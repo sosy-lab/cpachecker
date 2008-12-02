c9bf743269f45def14a9e9539fae0faed6d5bd59
Erkan <Erkan@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2008-12-02 19:02:49 +0000

    Alberto's test files and scripts are added to test directory.
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@197 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

---
+++ test/albertos_tests/do_run.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# template for the log files. Each log file will be called
+# $outfile.$cfg.log, where $cfg is the configuration used (see below)
+outfile=results/test_`date +%Y-%m-%d`
+
+# the various configurations to test
+configurations="summary explicit itpexplicit"
+
+# the benchmark instances
+#
+# this selects the "simplified" instances
+instances=`find test/ -regex ".+[^i]\.cil\.c$"`
+
+# this selects the "original" instances. For these, you should replace the
+#"summary" configuration with "summary_cex_suffix", as this works much
+# better. I'm still trying to understand why though
+#instances=`find test -name"*.i.cil.c"`
+
+# run the tests
+for cfg in $configurations; do 
+     ./run_tests.py --config=config/$cfg.properties --output=$outfile $instances --timeout=1800 --memlimit=1900000
+done
