bd5eb7dcdddaafc313318302ebcde30dae3d3ced
tautschnig <tautschnig@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2008-12-09 04:25:07 +0000

    Replaced dummy code in main testing loop where possible
    - Automata interfaces and code are already there
    
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@238 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/programtesting/QueryDrivenProgramTesting.java
+++ src/programtesting/QueryDrivenProgramTesting.java
@@ -4,17 +4,25 @@
 package programtesting;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
+import cfa.objectmodel.CFAEdge;
 import cfa.objectmodel.CFAFunctionDefinitionNode;
 
 import compositeCPA.CompositeCPA;
 
 import cpa.common.CPAAlgorithm;
+import cpa.common.CompositeElement;
+import cpa.common.automaton.Automaton;
+import cpa.common.interfaces.AbstractElement;
 import cpa.common.interfaces.ConfigurableProgramAnalysis;
 import cpa.location.LocationCPA;
-import cpa.scoperestrictionautomaton.ScopeRestrictionAutomatonCPA;
+import cpa.scoperestriction.ScopeRestrictionCPA;
 import cpa.symbpredabsCPA.SymbPredAbsCPA;
+import cpa.testgoal.TestGoalCPA;
 import exceptions.CPAException;
 
 /**
@@ -33,36 +41,47 @@ public class QueryDrivenProgramTesting {
       // for fixed values "sep", "sep" this is actually unreachable
       e.printStackTrace();
     }
-    ScopeRestrictionAutomatonCPA sraCPA = new ScopeRestrictionAutomatonCPA(null, null);
-    cpas.add(sraCPA);
+
+    Automaton<CFAEdge> passing = new Automaton<CFAEdge>();
+    cpas.add(new ScopeRestrictionCPA(passing));
+
+    Automaton<CFAEdge> cover = new Automaton<CFAEdge>();
+    cpas.add(new TestGoalCPA(cover));
+
     SymbPredAbsCPA predAbsCPA = new SymbPredAbsCPA("sep", "sep");
     cpas.add(predAbsCPA);
     ConfigurableProgramAnalysis cpa = CompositeCPA.createNewCompositeCPA(cpas, pMainFunction);
     CPAAlgorithm algo = new CPAAlgorithm();
-    /*
-    Set<AbstractElement> testGoals = new HashSet<AbstractElement>(sraCPA.getTestGoals());
+
+    Set<Automaton<CFAEdge>.State> testGoals = cover.getFinalStates();
+
     while (!testGoals.isEmpty()) {
       // testGoals to be passed in as precision
       AbstractElement initialElement =
-        cpa.getInitialElement(mainFunction);
-      Collection<AbstractElement> reached = algo.CPA(cpa, initialElement);
-      removeInfeasibleTestGoals(testGoals, reached);
+        cpa.getInitialElement(pMainFunction);
+      Collection<AbstractElement> reached = null;
+      try {
+        reached = algo.CPA(cpa, initialElement);
+      } catch (CPAException e1) {
+        // TODO Auto-generated catch block
+        e1.printStackTrace();
+      }
+      //removeInfeasibleTestGoals(testGoals, reached);
 
-      Set<String> paths = new HashSet<String>();
+      Set<ArrayList<CFAEdge>> paths = new HashSet<ArrayList<CFAEdge>>();
       for (AbstractElement e : reached) {
         CompositeElement comp = (CompositeElement)e;
         if (testGoals.isEmpty()) break;
-        if (!satisfiesTestGoal(testGoals, e)) continue;
-        if (predAbsCPA.isReachable(comp.get(2))) {
-          testGoals.remove(e);
-          paths.add(e);
-        } else {
+        //if (!satisfiesTestGoal(testGoals, e)) continue;
+        //if (predAbsCPA.isReachable(comp.get(2))) {
+          testGoals.iterator().next().unsetFinal();
+          //paths.add(e);
+        //} else {
           // refine predicate precision
-        }
+        //}
       }
 
-      runCBMC(paths);
+      //runCBMC(paths);
     }
-    */
   }
 }
