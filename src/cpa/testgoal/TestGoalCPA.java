926cc3214182573c09ed35f9b9ec57c30b76046a
tautschnig <tautschnig@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2008-12-13 01:44:21 +0000

    Converted TestGoalCPA to CPA+
    - Precision is ignored
    
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@295 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/cpa/testgoal/TestGoalCPA.java
+++ src/cpa/testgoal/TestGoalCPA.java
@@ -6,6 +6,8 @@ package cpa.testgoal;
 import java.util.Collection;
 import java.util.List;
 
+import common.Pair;
+
 import cfa.objectmodel.CFAEdge;
 import cfa.objectmodel.CFAFunctionDefinitionNode;
 import exceptions.CPATransferException;
@@ -15,6 +17,9 @@ import cpa.common.interfaces.AbstractElement;
 import cpa.common.interfaces.AbstractElementWithLocation;
 import cpa.common.interfaces.ConfigurableProgramAnalysis;
 import cpa.common.interfaces.MergeOperator;
+import cpa.common.interfaces.Precision;
+import cpa.common.interfaces.PrecisionAdjustment;
+import cpa.common.interfaces.PrecisionDomain;
 import cpa.common.interfaces.StopOperator;
 import cpa.common.interfaces.TransferRelation;
 import exceptions.CPAException;
@@ -24,38 +29,44 @@ import exceptions.CPAException;
  *
  */
 public class TestGoalCPA implements ConfigurableProgramAnalysis {
-  
+
+  public class TestGoalPrecisionDomain implements PrecisionDomain {
+
+  }
+
   public class TestGoalMergeOperator implements MergeOperator {
 
     @Override
     public AbstractElement merge(AbstractElement pElement1,
-                                 AbstractElement pElement2) throws CPAException {
+                                 AbstractElement pElement2,
+                                 Precision prec) throws CPAException {
       // no join
       return pElement2;
     }
-    
+
     public AbstractElementWithLocation merge(AbstractElementWithLocation pElement1,
-                                             AbstractElementWithLocation pElement2) throws CPAException {
+                                             AbstractElementWithLocation pElement2,
+                                             Precision prec) throws CPAException {
       throw new CPAException ("Cannot return element with location information");
     }
   }
-  
+
   public class TestGoalStopOperator implements StopOperator {
 
     @Override
     public <AE extends AbstractElement> boolean stop(AE pElement,
-                        Collection<AE> pReached)
-                                                             throws CPAException {
+                                                     Collection<AE> pReached, Precision prec)
+    throws CPAException {
       assert(pElement != null);
       assert(pReached != null);
-      
+
       // exists lElement in pReached with stop(pElement, lElement)?
       for (AbstractElement lElement : pReached) {
         if (stop(pElement, lElement)) {
           return true;
         }
       }
-      
+
       return false;
     }
 
@@ -64,49 +75,63 @@ public class TestGoalCPA implements ConfigurableProgramAnalysis {
                         AbstractElement pReachedElement) throws CPAException {
       return mDomain.getPartialOrder().satisfiesPartialOrder(pElement, pReachedElement);
     }
-    
+
   }
-  
+
+  public class TestGoalPrecisionAdjustment implements PrecisionAdjustment {
+
+    public <AE extends AbstractElement> Pair<AE, Precision> prec(
+        AE pElement,
+        Precision pPrecision,
+        Collection<Pair<AE, Precision>> pElements) {
+      return new Pair<AE,Precision> (pElement, pPrecision);
+    }
+
+  }
+
   public class TestGoalTransferRelation implements TransferRelation {
 
     @Override
     public AbstractElement getAbstractSuccessor(AbstractElement pElement,
-                                                CFAEdge pCfaEdge)
-                                                                 throws CPATransferException {
+                                                CFAEdge pCfaEdge, Precision prec)
+    throws CPATransferException {
       return mDomain.getSuccessor(pElement, pCfaEdge);
     }
 
     @Override
-    public List<AbstractElementWithLocation> getAllAbstractSuccessors(AbstractElementWithLocation pElement)
-                                                                                   throws CPAException,
-                                                                                   CPATransferException {
+    public List<AbstractElementWithLocation> getAllAbstractSuccessors(AbstractElementWithLocation pElement, Precision prec)
+    throws CPAException,
+    CPATransferException {
       // this method may not be called!
       assert(false);
-      
+
       return null;
     }
-    
+
   }
-  
+
   private AutomatonCPADomain<CFAEdge> mDomain;
+  private PrecisionDomain mPrecisionDomain;
+  private TestGoalTransferRelation mTransferRelation;
   private TestGoalMergeOperator mMergeOperator;
   private TestGoalStopOperator mStopOperator;
-  private TestGoalTransferRelation mTransferRelation;
-  
+  private PrecisionAdjustment mPrecisionAdjustment;
+
   public TestGoalCPA(Automaton<CFAEdge> pTestGoalAutomaton) {
     // Check for invariant: Final states will not be left once they are reached.
     for (Automaton<CFAEdge>.State lState : pTestGoalAutomaton.getFinalStates()) {
       // we ensure the property by a very strict assumption:
       assert(lState.hasUnconditionalSelfLoop());
     }
-    
+
     mDomain = new AutomatonCPADomain<CFAEdge>(pTestGoalAutomaton);
-    
+    mPrecisionDomain = new TestGoalPrecisionDomain();
+    mTransferRelation = new TestGoalTransferRelation();
     mMergeOperator = new TestGoalMergeOperator();
     mStopOperator = new TestGoalStopOperator();
-    mTransferRelation = new TestGoalTransferRelation();
+    mPrecisionAdjustment = new TestGoalPrecisionAdjustment();
   }
-  
+
   /* (non-Javadoc)
    * @see cpa.common.interfaces.ConfigurableProgramAnalysis#getAbstractDomain()
    */
@@ -115,12 +140,16 @@ public class TestGoalCPA implements ConfigurableProgramAnalysis {
     return mDomain;
   }
 
+  public PrecisionDomain getPrecisionDomain() {
+    return mPrecisionDomain;
+  }
+
   /* (non-Javadoc)
-   * @see cpa.common.interfaces.ConfigurableProgramAnalysis#getInitialElement(cfa.objectmodel.CFAFunctionDefinitionNode)
+   * @see cpa.common.interfaces.ConfigurableProgramAnalysis#getTransferRelation()
    */
   @Override
-  public AutomatonCPADomain<CFAEdge>.Element getInitialElement(CFAFunctionDefinitionNode pNode) {
-    return mDomain.getInitialElement();
+  public TestGoalTransferRelation getTransferRelation() {
+    return mTransferRelation;
   }
 
   /* (non-Javadoc)
@@ -139,12 +168,20 @@ public class TestGoalCPA implements ConfigurableProgramAnalysis {
     return mStopOperator;
   }
 
-  /* (non-Javadoc)
-   * @see cpa.common.interfaces.ConfigurableProgramAnalysis#getTransferRelation()
-   */
-  @Override
-  public TestGoalTransferRelation getTransferRelation() {
-    return mTransferRelation;
+  public PrecisionAdjustment getPrecisionAdjustment() {
+    return mPrecisionAdjustment;
   }
 
+  /* (non-Javadoc)
+   * @see cpa.common.interfaces.ConfigurableProgramAnalysis#getInitialElement(cfa.objectmodel.CFAFunctionDefinitionNode)
+   */
+  @Override
+  public AutomatonCPADomain<CFAEdge>.Element getInitialElement(CFAFunctionDefinitionNode pNode) {
+    return mDomain.getInitialElement();
+  }
+
+  public Precision getInitialPrecision(CFAFunctionDefinitionNode pNode) {
+    // TODO Auto-generated method stub
+    return null;
+  }
 }
