f3fd596c40064ba81f4c0fb4583646c26be8e297
holzer <holzer@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2008-12-20 16:53:06 +0000

    Added a "composite" cpa that produces bottom as soon as one of the composites produces bottom. Now, the examples for testing work again (using this composite cpa).
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@351 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/programtesting/QueryDrivenProgramTesting.java
+++ src/programtesting/QueryDrivenProgramTesting.java
@@ -42,13 +42,23 @@ import java.util.Map.Entry;
 import predicateabstraction.ThreeValuedBoolean;
 
 import cfa.objectmodel.CFAEdge;
+import cfa.objectmodel.CFAEdgeType;
 import cfa.objectmodel.CFAFunctionDefinitionNode;
+import cfa.objectmodel.CFANode;
+import cfa.objectmodel.c.CallToReturnEdge;
 
 import common.Pair;
 import compositeCPA.CompositeCPA;
+import compositeCPA.CompositeMergeOperator;
 import compositeCPA.CompositePrecision;
+import compositeCPA.CompositePrecisionAdjustment;
+import compositeCPA.CompositeStopOperator;
 
 import cpa.common.CPAAlgorithm;
+import cpa.common.CallElement;
+import cpa.common.CallStack;
+import cpa.common.CompositeBottomElement;
+import cpa.common.CompositeDomain;
 import cpa.common.CompositeElement;
 import cpa.common.automaton.Automaton;
 import cpa.common.automaton.AutomatonCPADomain;
@@ -72,6 +82,7 @@ import cpa.symbpredabs.explicit.ExplicitTransferRelation;
 import cpa.testgoal.TestGoalCPA;
 import cpa.testgoal.TestGoalCPA.TestGoalPrecision;
 import exceptions.CPAException;
+import exceptions.CPATransferException;
 import exceptions.RefinementNeededException;
 
 /**
@@ -79,6 +90,206 @@ import exceptions.RefinementNeededException;
  *
  */
 public class QueryDrivenProgramTesting {
+  
+  public static class MyCompositeCPA implements ConfigurableProgramAnalysis {
+
+    public class CompositeTransferRelation implements TransferRelation{
+
+      private final CompositeDomain compositeDomain;
+      private final List<TransferRelation> transferRelations;
+
+      // private LocationTransferRelation locationTransferRelation;
+
+      public CompositeTransferRelation (CompositeDomain compositeDomain, List<TransferRelation> transferRelations)
+      {
+        this.compositeDomain = compositeDomain;
+        this.transferRelations = transferRelations;
+
+        //TransferRelation first = transferRelations.get (0);
+        //if (first instanceof LocationTransferRelation)
+        //{
+        //  locationTransferRelation = (LocationTransferRelation) first;
+        //}
+      }
+
+      public AbstractDomain getAbstractDomain ()
+      {
+        return compositeDomain;
+      }
+
+      public AbstractElement getAbstractSuccessor (AbstractElement element, CFAEdge cfaEdge, Precision precision) throws CPATransferException
+      {
+        assert(precision instanceof CompositePrecision);
+        CompositePrecision lCompositePrecision = (CompositePrecision)precision;
+        
+        CompositeElement compositeElement = (CompositeElement) element;
+        List<AbstractElement> inputElements = compositeElement.getElements ();
+        List<AbstractElement> resultingElements = new ArrayList<AbstractElement> ();
+
+        CallStack updatedCallStack = compositeElement.getCallStack();
+
+        // TODO add some check here for unbounded recursive calls
+        if(cfaEdge.getEdgeType() == CFAEdgeType.FunctionCallEdge)
+        {
+          String functionName = cfaEdge.getSuccessor().getFunctionName();
+          CFANode callNode = cfaEdge.getPredecessor();
+          CallElement ce = new CallElement(functionName, callNode, compositeElement);
+          CallStack cs = compositeElement.getCallStack();
+          updatedCallStack = cs.clone();
+          updatedCallStack.push(ce);
+        }
+
+        // handling the return from a function
+        else if(cfaEdge.getEdgeType() == CFAEdgeType.ReturnEdge)
+        {
+          CallElement topCallElement = compositeElement.getCallStack().peek();
+          assert(cfaEdge.getPredecessor().getFunctionName().
+              equals(topCallElement.getFunctionName()));
+          CallElement returnElement = compositeElement.getCallStack().getSecondTopElement();
+
+          if(! topCallElement.isConsistent(cfaEdge.getSuccessor()) ||
+              ! returnElement.isConsistent(cfaEdge.getSuccessor().getFunctionName()) ){
+            return compositeDomain.getBottomElement();
+          }
+
+          // TODO we are saving the abstract state on summary edge, that works for
+          // now but this is a terrible design practice. Add another method
+          // getAbstractSuccessorOnReturn(subElement, prevElement, cfaEdge)
+          // and implement it for all CPAs later.
+          else{
+            CallStack cs = compositeElement.getCallStack();
+            updatedCallStack = cs.clone();
+            CallElement ce = updatedCallStack.pop();
+            CompositeElement compElemBeforeCall = ce.getState();
+            // TODO use summary edge as a cache later
+            CallToReturnEdge summaryEdge = cfaEdge.getSuccessor().getEnteringSummaryEdge();
+            summaryEdge.setAbstractElement(compElemBeforeCall);
+          }
+        }
+
+        for (int idx = 0; idx < transferRelations.size (); idx++)
+        {
+          TransferRelation transfer = transferRelations.get (idx);
+          AbstractElement subElement = null;
+          AbstractElement successor = null;
+          subElement = inputElements.get (idx);
+          // handling a call edge
+
+          Precision lPresicion = lCompositePrecision.get(idx);
+          
+          successor = transfer.getAbstractSuccessor (subElement, cfaEdge, lPresicion);
+          
+          // as soon as a component returns bottom we return composite bottom
+          if (compositeDomain.getDomains().get(idx).getBottomElement().equals(successor)) {
+            return compositeDomain.getBottomElement();
+          }
+          
+          resultingElements.add (successor);
+        }
+
+        CompositeElement successorState = new CompositeElement (resultingElements, updatedCallStack);
+        return successorState;
+      }
+
+      public List<AbstractElementWithLocation> getAllAbstractSuccessors(AbstractElementWithLocation element, Precision precision) throws CPAException, CPATransferException
+      {
+
+        //TODO CPACheckerStatistics.noOfTransferRelations++;
+
+        CompositeElement compositeElement = (CompositeElement) element;
+        CFANode node = compositeElement.getLocationNode();
+
+        List<AbstractElementWithLocation> results = new ArrayList<AbstractElementWithLocation> ();
+
+        for (int edgeIdx = 0; edgeIdx < node.getNumLeavingEdges (); edgeIdx++)
+        {
+          CFAEdge edge = node.getLeavingEdge (edgeIdx);
+          results.add ((CompositeElement) getAbstractSuccessor (element, edge, precision));
+        }
+
+        return results;
+      }
+    }
+    
+    private final CompositeDomain mDomain;
+    private final TransferRelation mTransferRelation;
+    private final MergeOperator mMergeOperator;
+    private final StopOperator mStopOperator;
+    private final PrecisionAdjustment mPrecisionAdjustment;
+    private final CompositeElement mInitialElement;
+    private final Precision mInitialPrecision;
+    
+    public MyCompositeCPA(List<ConfigurableProgramAnalysis> cpas, CFAFunctionDefinitionNode node) {
+      List<AbstractDomain> domains = new ArrayList<AbstractDomain> ();
+      List<TransferRelation> transferRelations = new ArrayList<TransferRelation> ();
+      List<MergeOperator> mergeOperators = new ArrayList<MergeOperator> ();
+      List<StopOperator> stopOperators = new ArrayList<StopOperator> ();
+      List<PrecisionAdjustment> precisionAdjustments = new ArrayList<PrecisionAdjustment> ();
+      List<AbstractElement> initialElements = new ArrayList<AbstractElement> ();
+      List<Precision> initialPrecisions = new ArrayList<Precision> ();
+
+      for(ConfigurableProgramAnalysis sp : cpas) {
+        domains.add(sp.getAbstractDomain());
+        transferRelations.add(sp.getTransferRelation());
+        mergeOperators.add(sp.getMergeOperator());
+        stopOperators.add(sp.getStopOperator());
+        precisionAdjustments.add(sp.getPrecisionAdjustment());
+        initialElements.add(sp.getInitialElement(node));
+        initialPrecisions.add(sp.getInitialPrecision(node));
+      }
+
+      mDomain = new CompositeDomain(domains);
+      mTransferRelation = new CompositeTransferRelation(mDomain, transferRelations);
+      mMergeOperator = new CompositeMergeOperator(mDomain, mergeOperators);
+      mStopOperator = new CompositeStopOperator(mDomain, stopOperators);
+      mPrecisionAdjustment = new CompositePrecisionAdjustment(precisionAdjustments);
+      mInitialElement = new CompositeElement(initialElements, null);
+      mInitialPrecision = new CompositePrecision(initialPrecisions);
+      
+      // set call stack
+      CallStack initialCallStack = new CallStack();
+      CallElement initialCallElement = new CallElement(node.getFunctionName(), node, mInitialElement);
+      initialCallStack.push(initialCallElement);
+      mInitialElement.setCallStack(initialCallStack);
+    }
+    
+    @Override
+    public AbstractDomain getAbstractDomain() {
+      return mDomain;
+    }
+
+    @Override
+    public CompositeElement getInitialElement(CFAFunctionDefinitionNode pNode) {
+      // TODO create an initial element
+      return mInitialElement;
+    }
+
+    @Override
+    public Precision getInitialPrecision(CFAFunctionDefinitionNode pNode) {
+      return mInitialPrecision;
+    }
+
+    @Override
+    public MergeOperator getMergeOperator() {
+      return mMergeOperator;
+    }
+
+    @Override
+    public PrecisionAdjustment getPrecisionAdjustment() {
+      return mPrecisionAdjustment;
+    }
+
+    @Override
+    public StopOperator getStopOperator() {
+      return mStopOperator;
+    }
+
+    @Override
+    public TransferRelation getTransferRelation() {
+      return mTransferRelation;
+    }
+    
+  }
   
   public static class MyCollection implements Collection<Pair<AbstractElementWithLocation,Precision>> {
     class MyIterator implements Iterator<Pair<AbstractElementWithLocation,Precision>> {
@@ -445,10 +656,13 @@ public class QueryDrivenProgramTesting {
   }
   
   public static class WrapperCPA implements ConfigurableProgramAnalysis {
-    private CompositeCPA mCompositeCPA;
+    private MyCompositeCPA mCompositeCPA;
     private AutomatonCPADomain<CFAEdge> mAutomatonDomain;
     
-    public WrapperCPA(CompositeCPA pCompositeCPA, AutomatonCPADomain<CFAEdge> pAutomatonDomain) {
+    public WrapperCPA(MyCompositeCPA pCompositeCPA, AutomatonCPADomain<CFAEdge> pAutomatonDomain) {
+      assert(pCompositeCPA != null);
+      assert(pAutomatonDomain != null);
+      
       mCompositeCPA = pCompositeCPA;
       mAutomatonDomain = pAutomatonDomain;
     }
@@ -580,7 +794,8 @@ public class QueryDrivenProgramTesting {
       cpas.add(lTestGoalCPA);
       
       // create composite cpa
-      CompositeCPA cpa = CompositeCPA.createNewCompositeCPA(cpas, pMainFunction);
+      //CompositeCPA cpa = CompositeCPA.createNewCompositeCPA(cpas, pMainFunction);
+      MyCompositeCPA cpa = new MyCompositeCPA(cpas, pMainFunction);
       WrapperCPA lWrapperCPA = new WrapperCPA(cpa, lTestGoalCPA.getAbstractDomain());
       
       
