ffc42ebf75ab975d40dd606fa7b1ef4501a5c3f8
tautschnig <tautschnig@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2009-01-11 00:09:57 +0000

    Improved auto-regression-test script
    - Check for running regression-tester processes
    - Exit with the exit code of regression-tester.sh
    
    
    
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@427 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- test/regression-test/test-HEAD.sh
+++ test/regression-test/test-HEAD.sh
@@ -4,6 +4,9 @@ cd `dirname $0`
 rep_dir=/localhome/dbeyer/SVN-software/cpachecker
 rev=`svnlook youngest $rep_dir`
 [ -d results.r$rev ] && exit 0
+if ps aux | grep regression-tester.sh | grep -q cpachecker ; then
+  echo "Regression test already running"
+  exit 1
+fi
 ./regression-tester.sh file://$rep_dir/trunk $rev > /dev/null
-exit 0
 
