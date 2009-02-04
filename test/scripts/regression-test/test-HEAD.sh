94aefabbdecac3aa58904693e0b36acba67d1898
tautschnig <tautschnig@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2009-02-04 17:51:44 +0000

    Improving the daily test script
    - Adapted to new naming schema of result directories
    - Make sure that people only try to run that on cs-sel-02
    
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@564 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- test/scripts/regression-test/test-HEAD.sh
+++ test/scripts/regression-test/test-HEAD.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+if [ "`hostname`" != "cs-sel-02" ] ; then
+  echo "This script only works on cs-sel-02.cs.surrey.sfu.ca"
+  exit 1
+fi
+
 cd `dirname $0`
 rep_dir=/localhome/dbeyer/SVN-software/cpachecker
 rev=`svnlook youngest $rep_dir`
-[ -d results.r$rev ] && exit 0
+[ -d results.lbe.r$rev ] && exit 0
 if ps aux | grep regression-tester.sh | grep -q cpachecker ; then
   echo "Regression test already running"
   exit 1
