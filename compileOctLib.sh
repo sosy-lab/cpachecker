e8c960c135140780768573f83231b7d1c1f9691b
tautschnig <tautschnig@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2009-02-04 14:18:52 +0000

    Added lib/native/source/ as a very special kind of architecture :-)
    - For the moment, we only provide source builds for libJOct.so
    - Moved the C sources and the compile script from the main directory to the new
      one
    - Made the compile script fully automatic (it downloads the sources, builds the
      oct library and compiles all the required stuff into libJOct.so)
    
    
    
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@535 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- compileOctLib.sh
+++
@@ -1,6 +0,0 @@
-gcc -I/localhome/erkan/jdk1.6.0_07/include/ -I/localhome/erkan/jdk1.6.0_07/include/linux/ -I/usr/local/include/oct -g -O2 -g -DOCT_HAS_MPFR -I/usr/lib/ocaml/3.10.0 -DOCT_HAS_MPFR -DOCT_HAS_GMP -DOCT_ENABLE_ASSERT -DOCT_NUM_FLOAT -DOCT_PREFIX=CAT\(octfag_ -c OctWrapper.c
-echo "compiled"
-ld -G -L/usr/local/lib -loct_fag -lmpfr -lgmp -lm OctWrapper.o -o libJOct.so -lm -lc -lpthread
-echo "linked"
-cp libJOct.so nativeLibs/
-echo "copied"
