1edc870b46f5bb40549961c4a08228d5a5262f54
holzer <holzer@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2009-03-09 18:35:05 +0000

    Added simple examples.
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/qdpt-hacking@625 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

---
+++ test/query-driven-program-testing/test/basic/test07.c
@@ -0,0 +1,12 @@
+int foo() {
+  return 10;
+}
+
+int main(void) {
+  int x;
+
+  x = foo();
+
+  return (x);
+}
+
---
+++ test/query-driven-program-testing/test/basic/test08.c
@@ -0,0 +1,14 @@
+int main(void) {
+  int x;
+
+  if (x == 0) {
+    goto END;
+  }
+
+  x = 10;
+
+END:
+
+  return (0);
+}
+
