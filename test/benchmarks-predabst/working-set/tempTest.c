ad4a9b7f55821770693138e13cc260ce2aa288b6
Erkan <Erkan@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2009-07-30 00:55:51 +0000

    updated config files
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@666 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- test/benchmarks-predabst/working-set/tempTest.c
+++ test/benchmarks-predabst/working-set/tempTest.c
@@ -6,15 +6,21 @@ int main(void){
  
   a = 5;
   b = a + 1;
-
-  if(a == 89){
-    b++;
+ 
+  c = asdad();
+  b = sdsd();
+ 
+  if(c == 89){
+    b = 90;
+  }
+  else{
+   b = 2000;
   }
 
   c = b;
   a = c - 1;
   
-  if(a == 4){
+  if(a == 89){
   errorFn();
   }
   return (0);
