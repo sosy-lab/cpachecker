32cbfcf55610517b246a175716ce497536687f00
dbeyer <dbeyer@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2009-09-09 13:34:52 +0000

    New test case added
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@682 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

---
+++ test/regression-tests/blast_array_1.c
@@ -0,0 +1,24 @@
+void f1() {
+    int x;
+    int *p= &x;
+    assert(p != 0); /* Blast says "safe" */
+}
+
+void f() {
+  int array[1];
+  int *p= &array[0]; /* Blast considers that p can have NULL value*/
+  assert(p != 0); /* Blast says "Error" */
+}
+
+void g() {
+  int array[1],i,j;
+  i=0; array[i]=1;
+  j=0; assert(array[j]==1);/* Blast says "Error"*/
+}
+
+void main(void) {
+  f1();
+  f();
+  g();
+}
+
