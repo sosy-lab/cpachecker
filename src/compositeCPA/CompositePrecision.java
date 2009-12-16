9d9204e3c63485903ef8ca50d6233c66d4eef720
pwendler <pwendler@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2009-12-16 16:26:25 +0000

    improving ARTPrecision and CompositePrecision
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@805 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/compositeCPA/CompositePrecision.java
+++ src/compositeCPA/CompositePrecision.java
@@ -26,7 +26,6 @@
  */
 package compositeCPA;
 
-import java.util.Iterator;
 import java.util.List;
 
 import cpa.common.interfaces.Precision;
@@ -38,52 +37,36 @@ import cpa.common.interfaces.Precision;
 public class CompositePrecision implements Precision {
   private final List<Precision> precisions;
 
-  public CompositePrecision (List<Precision> precisions)
-  {
+  public CompositePrecision (List<Precision> precisions) {
     this.precisions = precisions;
   }
 
-  public List<Precision> getPrecisions ()
-  {
+  public List<Precision> getPrecisions() {
     return precisions;
   }
 
   @Override
-  public boolean equals (Object other)
-  {
-    if (other == this)
+  public boolean equals(Object other) {
+    if (other == this) {
       return true;
-
-    if (!(other instanceof CompositePrecision))
+    } else if (other == null || !(other instanceof CompositePrecision)) {
       return false;
-
-    CompositePrecision otherPrecision = (CompositePrecision) other;
-
-    if (otherPrecision.precisions.size() != this.precisions.size ())
-      return false;
-    
-    Iterator<Precision> iter = precisions.iterator();
-    Iterator<Precision> otherIter = otherPrecision.precisions.iterator();
-    
-    while (iter.hasNext()) {
-      if (!iter.next().equals(otherIter.next())) return false;
     }
-
-    return true;
+    
+    return precisions.equals(((CompositePrecision)other).precisions);
   }
 
   @Override
   public int hashCode() {
-    int hashCode = 0;
-
-    for (Precision p : precisions) {
-      if (p != null) hashCode += p.hashCode();
-    }
-
-    return hashCode;
+    return precisions.hashCode();
   }
 
   public Precision get(int idx) {
     return precisions.get(idx);
+  }
+  
+  @Override
+  public String toString() {
+    return precisions.toString();
   }
 }
--- src/cpa/art/ARTPrecision.java
+++ src/cpa/art/ARTPrecision.java
@@ -4,9 +4,10 @@ import cpa.common.interfaces.Precision;
 
 public class ARTPrecision implements Precision {
   
-  private Precision wrappedPrec;
+  private final Precision wrappedPrec;
   
   public ARTPrecision(Precision pWrappedPrec) {
+    assert pWrappedPrec != null;
     wrappedPrec = pWrappedPrec;
   }
   
@@ -14,4 +15,24 @@ public class ARTPrecision implements Precision {
     return wrappedPrec;
   }
 
+  @Override
+  public boolean equals(Object pObj) {
+    if (pObj == this) {
+      return true;
+    } else if (pObj == null || !(pObj instanceof ARTPrecision)) {
+      return false;
+    } else {
+      return wrappedPrec.equals(((ARTPrecision)pObj).wrappedPrec);
+    }
+  }
+  
+  @Override
+  public int hashCode() {
+    return wrappedPrec.hashCode();
+  }
+  
+  @Override
+  public String toString() {
+    return String.valueOf(wrappedPrec);
+  }
 }
