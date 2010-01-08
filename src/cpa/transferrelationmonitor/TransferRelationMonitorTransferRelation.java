cdad712bfda54da8883e86e1c4522d446cbb7a1c
Erkan <Erkan@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2010-01-08 18:44:30 +0000

    we will monitor execution whenever this cpa is added, so config file check should be removed from this transfer relation and moved to an outer loop
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@882 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/cpa/transferrelationmonitor/TransferRelationMonitorTransferRelation.java
+++ src/cpa/transferrelationmonitor/TransferRelationMonitorTransferRelation.java
@@ -24,7 +24,7 @@ public class TransferRelationMonitorTransferRelation implements TransferRelation
 
   private final TransferRelation transferRelation;
   private TransferCallable tc = new TransferCallable();
-  
+
   public TransferRelationMonitorTransferRelation(TransferRelation pTransferRelation) {
     transferRelation = pTransferRelation;
   }
@@ -34,43 +34,37 @@ public class TransferRelationMonitorTransferRelation implements TransferRelation
       AbstractElement pElement, Precision pPrecision, CFAEdge pCfaEdge)
       throws CPATransferException, TransferTimeOutException {
     TransferRelationMonitorElement element = (TransferRelationMonitorElement)pElement;
-
     Collection<? extends AbstractElement> successors = null;
     
     AbstractElement wrappedElement = element.getWrappedElements().iterator().next();
     Precision wrappedPrecision = null;
+    
     if(pPrecision != null){
       wrappedPrecision = ((TransferRelationMonitorPrecision)pPrecision).getPrecision();
     }
 
-    if (CPAMain.cpaConfig.getBooleanValue("predicateabstraction.trackabstractioncomputation.enabled")){
-      // time limit is given in milliseconds
-      long timeLimit = Integer.parseInt(CPAMain.cpaConfig.getPropertiesArray
-          ("predicateabstraction.trackabstractioncomputation.limit")[0]);
-      // set the edge and element
-      tc.setEdge(pCfaEdge);
-      tc.setElement(wrappedElement);
-      tc.setPrecision(wrappedPrecision);
-      Future<Collection<? extends AbstractElement>> future = CEGARAlgorithm.executor.submit(tc);
-      try{
-        // here we get the result of the post computation but there is a time limit
-        // given to complete the task specified by timeLimit
-        successors = future.get(timeLimit, TimeUnit.MILLISECONDS);
-      } catch (TimeoutException exc){
-        throw new TransferTimeOutException(pCfaEdge, wrappedElement, wrappedPrecision);
-      } catch (InterruptedException exc) {
-        exc.printStackTrace();
-      } catch (ExecutionException exc) {
-        exc.printStackTrace();
-      }
-      //      executor.shutdown();
+    // time limit is given in milliseconds
+    long timeLimit = Integer.parseInt(CPAMain.cpaConfig.getPropertiesArray
+        ("predicateabstraction.trackabstractioncomputation.limit")[0]);
+    // set the edge and element
+    tc.setEdge(pCfaEdge);
+    tc.setElement(wrappedElement);
+    tc.setPrecision(wrappedPrecision);
+    Future<Collection<? extends AbstractElement>> future = CEGARAlgorithm.executor.submit(tc);
+    try{
+      // here we get the result of the post computation but there is a time limit
+      // given to complete the task specified by timeLimit
+      successors = future.get(timeLimit, TimeUnit.MILLISECONDS);
+    } catch (TimeoutException exc){
+      throw new TransferTimeOutException(pCfaEdge, wrappedElement, wrappedPrecision);
+    } catch (InterruptedException exc) {
+      exc.printStackTrace();
+    } catch (ExecutionException exc) {
+      exc.printStackTrace();
     }
-    else{
-      successors = transferRelation.getAbstractSuccessors(wrappedElement, wrappedPrecision, pCfaEdge);
-    }
-    
+
     assert(successors != null);
-    
+
     if (successors.isEmpty()) {
       return Collections.emptySet();
     }
@@ -89,34 +83,33 @@ public class TransferRelationMonitorTransferRelation implements TransferRelation
       Precision precision) {    
     return null;
   }
-  
+
   private class TransferCallable implements Callable<Collection<? extends AbstractElement>>{
 
     CFAEdge cfaEdge;
     AbstractElement abstractElement;
     Precision precision;
-    
+
     public TransferCallable() {
-      
+
     }
-    
+
     @Override
     public Collection<? extends AbstractElement> call() throws Exception {
       return transferRelation.getAbstractSuccessors(abstractElement, precision, cfaEdge);
     }
-    
+
     public void setEdge(CFAEdge pCfaEdge){
       cfaEdge = pCfaEdge;
     }
-    
+
     public void setElement(AbstractElement pAbstractElement){
       abstractElement = pAbstractElement;
     }
-    
+
     public void setPrecision(Precision pPrecision){
       precision = pPrecision;
     }
-    
   }
-  
+
 }
