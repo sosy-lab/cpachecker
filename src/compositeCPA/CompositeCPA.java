e7d3b8eb3878969947610dacb5b486cbbac7bab0
pwendler <pwendler@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2010-01-19 18:57:24 +0000

    forgotten change from last revision
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@925 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/compositeCPA/CompositeCPA.java
+++ src/compositeCPA/CompositeCPA.java
@@ -101,7 +101,7 @@ public class CompositeCPA implements ConfigurableProgramAnalysis, CPAWithStatist
     }
 
     CompositeDomain compositeDomain = new CompositeDomain (domains);
-    CompositeTransferRelation compositeTransfer = new CompositeTransferRelation (compositeDomain, transferRelations);
+    CompositeTransferRelation compositeTransfer = new CompositeTransferRelation (transferRelations);
     CompositeMergeOperator compositeMerge = new CompositeMergeOperator (compositeDomain, mergeOperators);
     CompositeStopOperator compositeStop = new CompositeStopOperator (compositeDomain, stopOperators);
     CompositePrecisionAdjustment compositePrecisionAdjustment = new CompositePrecisionAdjustment (precisionAdjustments);
