ad02005d2428a5de390ab29a0c9b1bc0470ada69
pwendler <pwendler@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2010-01-22 11:16:58 +0000

    another test cases for struct copies, this times with an articifial bug
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@946 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

---
+++ test/tests/single/struct_copy_BUG.c
@@ -0,0 +1,13 @@
+// this test case checks wether struct copies are handled
+struct s { int x; };
+int main() {
+	struct s a;
+	struct s b;
+	a.x = 4;
+	b.x = 8;
+	a = b;
+	if (a.x == 8) {
+ERROR:
+		goto ERROR;
+	}
+}
