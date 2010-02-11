688224575beac7ebd3b3ee36989f60cfd7865977
g.theoduloz <g.theoduloz@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2010-02-11 16:09:28 +0000

    New progress observer heuristics: number of repetitions of an edge along a path > threshold
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@1089 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

---
+++ src/cpa/assumptions/collector/progressobserver/RepetitionsInPathHeuristicsData.java
@@ -0,0 +1,125 @@
+/*
+ *  CPAchecker is a tool for configurable software verification.
+ *  This file is part of CPAchecker. 
+ *
+ *  Copyright (C) 2007-2008  Dirk Beyer and Erkan Keremoglu.
+ *  All rights reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *
+ *  CPAchecker web page:
+ *    http://www.cs.sfu.ca/~dbeyer/CPAchecker/
+ */
+package cpa.assumptions.collector.progressobserver;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import cfa.objectmodel.CFAEdge;
+import cfa.objectmodel.CFAEdgeType;
+
+/**
+ * @author g.theoduloz
+ */
+public class RepetitionsInPathHeuristicsData implements StopHeuristicsData {
+  
+  private static long threshold = -1;
+  
+  public static void setThreshold(long newThreshold)
+  {
+    threshold = newThreshold;
+  }
+  
+  public static long getThreshold()
+  {
+    return threshold;
+  }
+  
+  private final Map<CFAEdge, Integer> frequencyMap;
+  
+  public RepetitionsInPathHeuristicsData()
+  {
+    frequencyMap = new HashMap<CFAEdge, Integer>();
+  }
+  
+  /** The given map is copied to the new object's map */
+  public RepetitionsInPathHeuristicsData(Map<CFAEdge, Integer> map)
+  {
+    frequencyMap = new HashMap<CFAEdge, Integer>(map);
+  }
+  
+  @Override
+  public StopHeuristicsData collectData(ReachedHeuristicsDataSetView pReached) {
+    return this;
+  }
+
+  @Override
+  public boolean isBottom() {
+    return false;
+  }
+
+  @Override
+  public boolean isLessThan(StopHeuristicsData pD) {
+    return false;
+  }
+
+  @Override
+  public boolean isTop() {
+    return frequencyMap.isEmpty();
+  }
+
+  private boolean isInteresting(CFAEdge edge)
+  {
+    return (edge.getEdgeType() == CFAEdgeType.FunctionCallEdge)
+        && (edge.getPredecessor().isLoopStart());
+  }
+  
+  @Override
+  public StopHeuristicsData processEdge(CFAEdge edge) {
+    if (!isInteresting(edge)) return this;
+    
+    RepetitionsInPathHeuristicsData result = copy();
+    int oldValue = frequencyMap.get(edge);
+    result.frequencyMap.put(edge, oldValue+1);
+    return result;
+  }
+  
+  private RepetitionsInPathHeuristicsData copy() {
+    return new RepetitionsInPathHeuristicsData(frequencyMap);
+  }
+
+  @Override
+  public String toString() {
+    return super.toString();
+  }
+  
+  public static final RepetitionsInPathHeuristicsData TOP = new RepetitionsInPathHeuristicsData() {
+    @Override
+    public boolean isTop() { return true; }
+    @Override
+    public StopHeuristicsData processEdge(CFAEdge edge) { return this; }
+    @Override
+    public String toString() { return "TOP"; }
+  };
+  
+  public static final RepetitionsInPathHeuristicsData BOTTOM = new RepetitionsInPathHeuristicsData() {
+    @Override
+    public boolean isBottom() { return true; }
+    @Override
+    public StopHeuristicsData processEdge(CFAEdge edge) { return this; }
+    @Override
+    public String toString() { return "BOTTOM"; }
+  };
+  
+}
