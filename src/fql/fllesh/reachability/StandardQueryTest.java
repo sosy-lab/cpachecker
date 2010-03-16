0c6688b5a0ea7050879dd568dcfd79127e5d7c8d
holzer <holzer@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2010-03-16 12:32:05 +0000

    Test case that uses SymbPredAbsCPA as may analysis.
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@1232 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/fql/fllesh/reachability/StandardQueryTest.java
+++ src/fql/fllesh/reachability/StandardQueryTest.java
@@ -19,10 +19,12 @@ import compositeCPA.CompositePrecision;
 
 import cpa.alwaystop.AlwaysTopCPA;
 import cpa.common.LogManager;
+import cpa.common.interfaces.CPAFactory;
 import cpa.common.interfaces.ConfigurableProgramAnalysis;
 import cpa.concrete.ConcreteAnalysisCPA;
 import cpa.location.LocationCPA;
 import cpa.mustmay.MustMayAnalysisCPA;
+import cpa.symbpredabsCPA.SymbPredAbsCPA;
 import exceptions.CPAException;
 import fql.backend.pathmonitor.Automaton;
 import fql.backend.targetgraph.Node;
@@ -258,6 +260,101 @@ public class StandardQueryTest {
     
     
     AlwaysTopCPA lMayCPA = new AlwaysTopCPA();
+    ConcreteAnalysisCPA lMustCPA = new ConcreteAnalysisCPA();
+    
+    MustMayAnalysisCPA lMustMayAnalysisCPA = new MustMayAnalysisCPA(lMustCPA, lMayCPA);
+    
+    LocationCPA lLocationCPA = new LocationCPA();
+    
+    LinkedList<ConfigurableProgramAnalysis> lCPAs = new LinkedList<ConfigurableProgramAnalysis>();
+    
+    lCPAs.add(lLocationCPA);
+    lCPAs.add(lMustMayAnalysisCPA);
+    
+    ConfigurableProgramAnalysis lCompositeCPA = CompositeCPA.factory().setChildren(lCPAs).createInstance();
+    
+    CompositeElement lInitialDataSpaceElement = FeasibilityCheck.createInitialElement(lProgramEntry);
+    //CompositeElement lFinalDataSpaceElement = FeasibilityCheck.createNextElement(lProgramExit);
+    
+    CompositePrecision lDataSpacePrecision = (CompositePrecision)lCompositeCPA.getInitialPrecision(lMainFunction);
+    
+    Automaton lFirstAutomaton = Automaton.create(new LowerBound(Identity.getInstance(), 0), lTargetGraph);
+    Automaton lSecondAutomaton = Automaton.create(new LowerBound(Identity.getInstance(), 0), lTargetGraph);
+    
+    StandardQuery.Factory lQueryFactory = new StandardQuery.Factory(lLogManager);
+    
+    StandardQuery lQuery = lQueryFactory.create(lFirstAutomaton, lSecondAutomaton, lInitialDataSpaceElement, lDataSpacePrecision, lFirstAutomaton.getInitialStates(), lSecondAutomaton.getInitialStates(), lMainFunction, lFirstAutomaton.getFinalStates(), lSecondAutomaton.getFinalStates());
+
+    System.out.println("Automaton1: " + lFirstAutomaton.toString());
+    System.out.println("Automaton2: " + lSecondAutomaton.toString());
+    
+    System.out.println("Source: " + lQuery.getSource().toString());
+    System.out.println("Target: " + lQuery.getTarget().toString());
+    
+    System.out.println(lQuery.hasNext());
+  }
+  
+  @Test
+  public void test_04() throws IOException, InvalidCmdlineArgumentException, CPAException {
+    
+    String[] lArguments = new String[3];
+    
+    lArguments[0] = mConfig;
+    lArguments[1] = mPropertiesFile;
+    
+    // check cilly invariance of source file, i.e., is it changed when preprocessed by cilly?
+    Cilly lCilly = new Cilly();
+    
+    String lSourceFileName = "test/programs/simple/functionCall.c";
+    
+    if (!lCilly.isCillyInvariant(lSourceFileName)) {
+      File lCillyProcessedFile = lCilly.cillyfy(lSourceFileName);
+      
+      lSourceFileName = lCillyProcessedFile.getAbsolutePath();
+      
+      System.err.println("WARNING: Given source file is not CIL invariant ... did preprocessing!");
+    }
+    
+    // set source file name
+    lArguments[2] = lSourceFileName;
+    
+    Configuration lConfiguration = CPAMain.createConfiguration(lArguments);
+
+    
+    
+    LogManager lLogManager = new LogManager(lConfiguration);
+      
+    CPAchecker lCPAchecker = new CPAchecker(lConfiguration, lLogManager);
+        
+    CFAFunctionDefinitionNode lMainFunction = lCPAchecker.getMainFunction();
+    
+    TargetGraph lTargetGraph = TargetGraph.createTargetGraphFromCFA(lMainFunction);
+    
+    
+    // add self loops to CFA
+    AddSelfLoop.addSelfLoops(lMainFunction);
+    
+    
+    // TODO remove this output code
+    DOTBuilder dotBuilder = new DOTBuilder();
+    dotBuilder.generateDOT(lCPAchecker.getCFAMap().values(), lMainFunction, new File("/tmp/mycfa.dot"));
+    
+    
+    Node lProgramEntry = new Node(lMainFunction);
+    //Node lProgramExit = new Node(lMainFunction.getExitNode());
+    
+    
+    //AlwaysTopCPA lMayCPA = new AlwaysTopCPA();
+    
+    
+    CPAFactory lSymbPredAbsFactory = SymbPredAbsCPA.factory();
+    
+    //TODO: modify configuration such that SymbPredAbsCPA is properly configured 
+    lSymbPredAbsFactory.setConfiguration(lConfiguration);
+    lSymbPredAbsFactory.setLogger(lLogManager);
+    
+    ConfigurableProgramAnalysis lMayCPA = lSymbPredAbsFactory.createInstance();
+    
     ConcreteAnalysisCPA lMustCPA = new ConcreteAnalysisCPA();
     
     MustMayAnalysisCPA lMustMayAnalysisCPA = new MustMayAnalysisCPA(lMustCPA, lMayCPA);
