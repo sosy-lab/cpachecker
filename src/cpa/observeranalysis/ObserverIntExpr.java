58112ddb7dc3342193edb4a21e61e4f116c8ce99
rhein <rhein@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2010-04-21 19:12:51 +0000

    Usage of Transition-variables in IntExpressions
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@1324 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/cpa/observeranalysis/ObserverIntExpr.java
+++ src/cpa/observeranalysis/ObserverIntExpr.java
@@ -1,5 +1,7 @@
 package cpa.observeranalysis;
 
+import java.util.logging.Level;
+
 /**
  * Implements a integer expression that evaluates and returns a <code>int</code> value when <code>eval()</code> is called.
  * The Expression can be evaluated multiple times.
@@ -23,9 +25,35 @@ abstract class ObserverIntExpr {
      */
     static class VarAccess extends ObserverIntExpr {
       String varId;
-      public VarAccess(String pId) {this.varId = pId; }
+      public VarAccess(String pId) {
+        if (pId.startsWith("$$")) {
+          // throws a NumberFormatException and this is good!
+          Integer.parseInt(pId.substring(2));
+        }
+        this.varId = pId;
+      }
       @Override public int eval(ObserverExpressionArguments pArgs) {
-        return pArgs.getObserverVariables().get(varId).getValue(); // only ints supported so far
+        if (varId.matches("\\$\\d+")) { // $1  ObserverTransitionVariables
+          // no exception here (would have come in the constructor)
+          int key = Integer.parseInt(varId.substring(1));
+          String val = pArgs.getTransitionVariable(key);
+          if (val == null) {
+            pArgs.getLogger().log(Level.WARNING, "could not find the transition variable $" + key + ". Returning -1.");
+            return -1;
+          }
+          int value = -1;
+          try {
+            value = Integer.parseInt(val);
+          } catch (NumberFormatException e) {
+            pArgs.getLogger().log(Level.WARNING, "could not parse the contents of variable $" + key + "=\"" + val +"\". Returning -1.");
+            return -1;
+          }
+          return value;
+        } else if (varId.equals("$line")) { // $line  line number in sourcecode
+          return pArgs.getCfaEdge().getLineNumber();
+        } else {
+          return pArgs.getObserverVariables().get(varId).getValue(); // only ints supported so far
+        }
       }
     }
     /** Addition of {@link ObserverIntExpr} instances. 
