244d34eee2c854120adb2299c5546a6ca76dc70c
dbeyer <dbeyer@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2010-04-29 14:45:19 +0000

    example formatting
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@1383 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- test/programs/simple/simple_setuid_test.c
+++ test/programs/simple/simple_setuid_test.c
@@ -1,17 +1,16 @@
 // testcase for simple_setuid.txt
-main()
-{
-	// some user input so CPAchecker must check all if branches
-	i = someUserFunction();
-	if (i == 0) {
-		// this should be ok
-		// setting the userid
-		setuid(2);
-		// systemcall
-		i = system(20);
-	} else if (i == 1) {
-		// this should trigger an error
-		//systemcall without setting the userid
-		system(40);
-	}
+main() {
+  // some user input so CPAchecker must check all if branches
+  i = someUserFunction();
+  if (i == 0) {
+    // this should be ok
+    // setting the userid
+    setuid(2);
+    // systemcall
+    i = system(20);
+  } else if (i == 1) {
+    // this should trigger an error
+    //systemcall without setting the userid
+    system(40);
+  }
 }
