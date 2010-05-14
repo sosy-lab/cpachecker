b36f813df37feead9755cbdb7c173bbaa65960a9
pwendler <pwendler@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2010-05-14 06:32:03 +0000

    adjustments so that branch with old code and new libraries works
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/abe@1435 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- scripts/cpa.sh
+++ scripts/cpa.sh
@@ -11,8 +11,8 @@ JAVA=java
 # From here on you should not need to change anything
 #------------------------------------------------------------------------------
 
-if [ ! -e $PATH_TO_CPACHECKER/bin/org/sosy_lab/cpachecker/cmdline/CPAMain.class ] ; then
-  echo "bin/org/sosy_lab/cpachecker/cmdline/CPAMain.class not found, please check path to project directory" 1>&2
+if [ ! -e $PATH_TO_CPACHECKER/bin/cmdline/CPAMain.class ] ; then
+  echo "bin/cmdline/CPAMain.class not found, please check path to project directory" 1>&2
   exit 1
 fi
 
@@ -61,4 +61,4 @@ export PATH="$PATH:$arch_platform_path"
 if [ ! -z "$JAVA_VM_ARGUMENTS" ]; then
   echo "Running CPAchecker with the following extra VM options: $JAVA_VM_ARGUMENTS"
 fi
-$JAVA $JAVA_VM_ARGUMENTS -Djava.library.path=$arch_platform_path -Xmx1200m -ea org.sosy_lab.cpachecker.cmdline.CPAMain "$@"
+$JAVA $JAVA_VM_ARGUMENTS -Djava.library.path=$arch_platform_path -Xmx1200m -ea cmdline.CPAMain "$@"
