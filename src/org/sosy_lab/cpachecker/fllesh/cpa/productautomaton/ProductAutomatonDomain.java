14e27277dbefa7a85d12cfcad359b193cfc25720
holzer <holzer@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2010-05-27 09:24:22 +0000

    Added more adequate partial order and join operator.
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@1512 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/fllesh/cpa/productautomaton/ProductAutomatonDomain.java
+++ src/org/sosy_lab/cpachecker/fllesh/cpa/productautomaton/ProductAutomatonDomain.java
@@ -1,11 +1,36 @@
 package org.sosy_lab.cpachecker.fllesh.cpa.productautomaton;
 
-import org.sosy_lab.cpachecker.core.defaults.FlatLatticeDomain;
+import org.sosy_lab.cpachecker.core.interfaces.AbstractDomain;
+import org.sosy_lab.cpachecker.core.interfaces.JoinOperator;
 
-public class ProductAutomatonDomain extends FlatLatticeDomain {
-
+public class ProductAutomatonDomain implements AbstractDomain {
+  
+  private ProductAutomatonPartialOrder mPartialOrder;
+  private ProductAutomatonJoinOperator mJoinOperator;
+  
   public ProductAutomatonDomain() {
-    super(ProductAutomatonTopElement.getInstance(), ProductAutomatonBottomElement.getInstance());
+    mPartialOrder = new ProductAutomatonPartialOrder();
+    mJoinOperator = new ProductAutomatonJoinOperator();
+  }
+
+  @Override
+  public ProductAutomatonBottomElement getBottomElement() {
+    return ProductAutomatonBottomElement.getInstance();
+  }
+
+  @Override
+  public JoinOperator getJoinOperator() {
+    return mJoinOperator;
+  }
+
+  @Override
+  public ProductAutomatonPartialOrder getPartialOrder() {
+    return mPartialOrder;
+  }
+
+  @Override
+  public ProductAutomatonTopElement getTopElement() {
+    return ProductAutomatonTopElement.getInstance();
   }
   
 }
---
+++ src/org/sosy_lab/cpachecker/fllesh/cpa/productautomaton/ProductAutomatonPartialOrder.java
@@ -0,0 +1,48 @@
+package org.sosy_lab.cpachecker.fllesh.cpa.productautomaton;
+
+import org.sosy_lab.cpachecker.core.interfaces.AbstractElement;
+import org.sosy_lab.cpachecker.core.interfaces.PartialOrder;
+import org.sosy_lab.cpachecker.exceptions.CPAException;
+
+public class ProductAutomatonPartialOrder implements PartialOrder {
+
+  @Override
+  public boolean satisfiesPartialOrder(AbstractElement pElement1,
+      AbstractElement pElement2) throws CPAException {
+    
+    if (!(pElement1 instanceof ProductAutomatonElement)) {
+      throw new IllegalArgumentException();
+    }
+    
+    if (!(pElement2 instanceof ProductAutomatonElement)) {
+      throw new IllegalArgumentException();
+    }
+    
+    if (pElement1.equals(ProductAutomatonBottomElement.getInstance())) {
+      return true;
+    }
+    
+    if (pElement2.equals(ProductAutomatonTopElement.getInstance())) {
+      return true;
+    }
+    
+    if (pElement1.equals(pElement2)) {
+      return true;
+    }
+    
+    if (pElement1.equals(ProductAutomatonTopElement.getInstance())) {
+      return false;
+    }
+    
+    if (pElement2.equals(ProductAutomatonBottomElement.getInstance())) {
+      return false;
+    }
+    
+    if (pElement2.equals(ProductAutomatonUndeterminedElement.getInstance())) {
+      return true;
+    }
+    
+    return false;
+  }
+
+}
