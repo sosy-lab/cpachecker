b7e7fe3548f9063719810df8b68fac795375a0ce
holzer <holzer@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2010-06-07 15:44:16 +0000

    Nicer implementation of nondet. trick.
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@1585 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- test/programs/fql/using_random.c
+++ test/programs/fql/using_random.c
@@ -1,20 +1,24 @@
 
 extern int rand(int counter);
 
-int counter = 0;
-
 int myRand() {
+	/*
+	 * TODO: we need a counter since it is assumed that rand is a pure function
+	 */
+	static int counter = 0;
 	return rand(counter++);
 }
 
 int foo() {
 	int x;
 	int y;
+	int z;
 
 	x = myRand();
 	y = myRand();
+	z = myRand();
 
-	if (x != y) {
+	if (x != y && z == 10) {
 		return 1;
 	}
 	else {
