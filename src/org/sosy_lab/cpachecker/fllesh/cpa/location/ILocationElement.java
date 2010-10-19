478626f89f3ccfe261276faa2bc6770104684499
holzer <holzer@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2010-10-19 09:22:48 +0000

    Remove unused copy of location cpa.
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@2131 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/fllesh/cpa/location/ILocationElement.java
+++
@@ -1,7 +0,0 @@
-package org.sosy_lab.cpachecker.fllesh.cpa.location;
-
-import org.sosy_lab.cpachecker.core.interfaces.AbstractElementWithLocation;
-
-public interface ILocationElement extends AbstractElementWithLocation {
-
-}
