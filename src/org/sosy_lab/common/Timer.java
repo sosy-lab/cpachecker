3e3bd9c43864acdf7c216a91625189b43a16791a
friedber <friedber@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2010-10-28 15:13:55 +0000

    Class Timer updated, new function getAverageTime()
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@2226 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/common/Timer.java
+++ src/org/sosy_lab/common/Timer.java
@@ -1,20 +1,41 @@
+/*
+ *  CPAchecker is a tool for configurable software verification.
+ *  This file is part of CPAchecker.
+ *
+ *  Copyright (C) 2007-2010  Dirk Beyer
+ *  All rights reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *
+ *  CPAchecker web page:
+ *    http://cpachecker.sosy-lab.org
+ */
 package org.sosy_lab.common;
 
 /**
  * This Class represents a timer like a chronograph. It can be started and
- * stopped several times. It can return the sum of those intervals and the
- * maximal interval.
+ * stopped several times. It can return the sum, the average and the maximum 
+ * of those intervals.
+ * 
+ * @author Karlheinz Friedberger
  */
 public class Timer {
 
-  private long startTime = 0;
-  private long endTime   = 0;
-  private long sumTime   = 0;
-  private long maxTime   = 0;
-
-  /** Constructor of Timer */
-  public Timer() {
-  }
+  private long startTime         = 0;
+  private long sumTime           = 0;
+  private long maxTime           = 0;
+  private int  numberOfIntervals = 0;
 
   /** start the timer if it is not running, else start new interval */
   public void start() {
@@ -29,7 +50,8 @@ public class Timer {
 
   /** stop the timer if it is not running, else do nothing */
   public void stop() {
-    endTime = System.currentTimeMillis();
+    long endTime = System.currentTimeMillis();
+
     if (isRunning()) {
       long intervallTime = endTime - startTime;
       sumTime += intervallTime;
@@ -37,16 +59,18 @@ public class Timer {
 
       // reset startTime for isRunning()
       startTime = 0;
+      // one more interval is finished
+      numberOfIntervals++;
     }
   }
 
   /**
    * returns the sum of all intervals. if timer is running, it returns the sum
-   * of the intarvals plus the time since the timer has been started
+   * of the intervals plus the time since the timer has been started
    */
   public long getSumTime() {
     if (isRunning()) {
-      return System.currentTimeMillis() - startTime + sumTime;
+      return sumTime + System.currentTimeMillis() - startTime;
     } else {
       return sumTime;
     }
@@ -56,8 +80,25 @@ public class Timer {
     return maxTime;
   }
 
+  /**
+   * returns the average of all intervals. if timer is running, it returns the average
+   * of the intervals plus one interval with the time since the timer has been started
+   */
+  public long getAvgTime() {
+    if (isRunning()) {
+      return getSumTime() / (numberOfIntervals + 1);
+    } else {
+      return getSumTime() / numberOfIntervals;
+    }
+  }
+
+  /**some methods for printing*/
   public String printMaxTime() {
-    return formatTime(maxTime);
+    return formatTime(getMaxTime());
+  }
+
+  public String printAvgTime() {
+    return formatTime(getAvgTime());
   }
 
   @Override
