1c79cc1a7099a7881384f11bd8a8e81feb20c807
friedber <friedber@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2010-10-28 15:20:03 +0000

    replace time-calculations (starts, stops, differences) with Timer-Objects, remove function "private String toTime(long time)", because it is not needed any more here
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@2227 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/core/algorithm/CPAAlgorithm.java
+++ src/org/sosy_lab/cpachecker/core/algorithm/CPAAlgorithm.java
@@ -32,6 +32,7 @@ import java.util.logging.Level;
 import org.sosy_lab.common.LogManager;
 import org.sosy_lab.common.Pair;
 import org.sosy_lab.common.Triple;
+import org.sosy_lab.common.Timer;
 
 import org.sosy_lab.cpachecker.core.CPAchecker;
 import org.sosy_lab.cpachecker.core.CPAcheckerResult.Result;
@@ -52,21 +53,21 @@ public class CPAAlgorithm implements Algorithm, StatisticsProvider {
 
   private static class CPAStatistics implements Statistics {
 
-    private long totalTime = 0;
-    private long chooseTime = 0;
-    private long precisionTime = 0;
-    private long transferTime = 0;
-    private long mergeTime = 0;
-    private long stopTime = 0;
+    private Timer totalTime         = new Timer();
+    private Timer chooseTime        = new Timer();
+    private Timer precisionTime     = new Timer();
+    private Timer transferTime      = new Timer();
+    private Timer mergeTime         = new Timer();
+    private Timer stopTime          = new Timer();
 
-    private int countIterations = 0;
-    private int maxWaitlistSize = 0;
-    private int countWaitlistSize = 0;
-    private int countSuccessors = 0;
-    private int maxSuccessors = 0;
-    private int countMerge = 0;
-    private int countStop = 0;
-    private int countBreak = 0;
+    private int   countIterations   = 0;
+    private int   maxWaitlistSize   = 0;
+    private int   countWaitlistSize = 0;
+    private int   countSuccessors   = 0;
+    private int   maxSuccessors     = 0;
+    private int   countMerge        = 0;
+    private int   countStop         = 0;
+    private int   countBreak        = 0;
 
     @Override
     public String getName() {
@@ -78,31 +79,28 @@ public class CPAAlgorithm implements Algorithm, StatisticsProvider {
         ReachedSet pReached) {
       out.println("Number of iterations:            " + countIterations);
       out.println("Max size of waitlist:            " + maxWaitlistSize);
-      out.println("Average size of waitlist:        " + countWaitlistSize/countIterations);
+      out.println("Average size of waitlist:        " + countWaitlistSize
+          / countIterations);
       out.println("Number of computed successors:   " + countSuccessors);
       out.println("Max successors for one element:  " + maxSuccessors);
       out.println("Number of times merged:          " + countMerge);
       out.println("Number of times stopped:         " + countStop);
       out.println("Number of times breaked:         " + countBreak);
       out.println();
-      out.println("Total time for CPA algorithm:   " + toTime(totalTime));
-      out.println("Time for choose from waitlist:  " + toTime(chooseTime));
-      out.println("Time for precision adjustment:  " + toTime(precisionTime));
-      out.println("Time for transfer relation:     " + toTime(transferTime));
-      out.println("Time for merge operator:        " + toTime(mergeTime));
-      out.println("Time for stop operator:         " + toTime(stopTime));
-    }
-
-    private String toTime(long timeMillis) {
-      return String.format("% 5d.%03ds", timeMillis/1000, timeMillis%1000);
+      out.println("Total time for CPA algorithm:   " + totalTime);
+      out.println("Time for choose from waitlist:  " + chooseTime);
+      out.println("Time for precision adjustment:  " + precisionTime);
+      out.println("Time for transfer relation:     " + transferTime);
+      out.println("Time for merge operator:        " + mergeTime);
+      out.println("Time for stop operator:         " + stopTime);
     }
   }
 
-  private final CPAStatistics stats = new CPAStatistics();
+  private final CPAStatistics               stats = new CPAStatistics();
 
   private final ConfigurableProgramAnalysis cpa;
 
-  private final LogManager logger;
+  private final LogManager                  logger;
 
   public CPAAlgorithm(ConfigurableProgramAnalysis cpa, LogManager logger) {
     this.cpa = cpa;
@@ -111,11 +109,12 @@ public class CPAAlgorithm implements Algorithm, StatisticsProvider {
 
   @Override
   public void run(final ReachedSet reachedSet) throws CPAException {
-    long startTotalTime = System.currentTimeMillis();
+    stats.totalTime.start();
     final TransferRelation transferRelation = cpa.getTransferRelation();
     final MergeOperator mergeOperator = cpa.getMergeOperator();
     final StopOperator stopOperator = cpa.getStopOperator();
-    final PrecisionAdjustment precisionAdjustment = cpa.getPrecisionAdjustment();
+    final PrecisionAdjustment precisionAdjustment =
+        cpa.getPrecisionAdjustment();
 
     while (reachedSet.hasWaitingElement()) {
       CPAchecker.stopIfNecessary();
@@ -130,35 +129,36 @@ public class CPAAlgorithm implements Algorithm, StatisticsProvider {
       }
       stats.countWaitlistSize += size;
 
-      long start = System.currentTimeMillis();
+      stats.chooseTime.start();
       final AbstractElement element = reachedSet.popFromWaitlist();
       final Precision precision = reachedSet.getPrecision(element);
-      long end = System.currentTimeMillis();
-      stats.chooseTime += (end - start);
+      stats.chooseTime.stop();
 
       logger.log(Level.FINER, "Retrieved element from waitlist");
-      logger.log(Level.ALL, "Current element is", element, "with precision", precision);
+      logger.log(Level.ALL, "Current element is", element, "with precision",
+          precision);
 
-      start = System.currentTimeMillis();
-      Collection<? extends AbstractElement> successors = transferRelation.getAbstractSuccessors(element, precision, null);
-      end = System.currentTimeMillis();
-      stats.transferTime += (end - start);
-      // TODO When we have a nice way to mark the analysis result as incomplete, we could continue analysis on a CPATransferException with the next element from waitlist.
+      stats.transferTime.start();
+      Collection<? extends AbstractElement> successors =
+          transferRelation.getAbstractSuccessors(element, precision, null);
+      stats.transferTime.stop();
+      // TODO When we have a nice way to mark the analysis result as incomplete, 
+      // we could continue analysis on a CPATransferException with the next element from waitlist.
 
       int numSuccessors = successors.size();
-      logger.log(Level.FINER, "Current element has", numSuccessors, "successors");
+      logger.log(Level.FINER, "Current element has", numSuccessors,
+          "successors");
       stats.countSuccessors += numSuccessors;
       stats.maxSuccessors = Math.max(numSuccessors, stats.maxSuccessors);
-      
+
       for (AbstractElement successor : successors) {
         logger.log(Level.FINER, "Considering successor of current element");
         logger.log(Level.ALL, "Successor of", element, "\nis", successor);
 
-        start = System.currentTimeMillis();
-        Triple<AbstractElement, Precision, Action> precAdjustmentResult = 
-          precisionAdjustment.prec(successor, precision, reachedSet);
-        end = System.currentTimeMillis();
-        stats.precisionTime += (end - start);
+        stats.precisionTime.start();
+        Triple<AbstractElement, Precision, Action> precAdjustmentResult =
+            precisionAdjustment.prec(successor, precision, reachedSet);
+        stats.precisionTime.stop();
 
         successor = precAdjustmentResult.getFirst();
         Precision successorPrecision = precAdjustmentResult.getSecond();
@@ -170,59 +170,67 @@ public class CPAAlgorithm implements Algorithm, StatisticsProvider {
           // successors left that otherwise would be forgotten
           reachedSet.add(element, precision);
           reachedSet.add(successor, successorPrecision);
-          
-          stats.totalTime += (System.currentTimeMillis() - startTotalTime);
+
+          stats.totalTime.stop();
           return;
         }
         assert action == Action.CONTINUE : "Enum Action has unhandled values!";
-        
+
         Collection<AbstractElement> reached = reachedSet.getReached(successor);
 
         // AG as an optimization, we allow the mergeOperator to be null,
         // as a synonym of a trivial operator that never merges
         if (mergeOperator != null && !reached.isEmpty()) {
-          start = System.currentTimeMillis();
+          stats.mergeTime.start();
 
           List<AbstractElement> toRemove = new ArrayList<AbstractElement>();
-          List<Pair<AbstractElement, Precision>> toAdd = new ArrayList<Pair<AbstractElement, Precision>>();
+          List<Pair<AbstractElement, Precision>> toAdd =
+              new ArrayList<Pair<AbstractElement, Precision>>();
 
-          logger.log(Level.FINER, "Considering", reached.size(), "elements from reached set for merge");
+          logger.log(Level.FINER, "Considering", reached.size(),
+              "elements from reached set for merge");
           for (AbstractElement reachedElement : reached) {
-            AbstractElement mergedElement = mergeOperator.merge(successor, reachedElement, successorPrecision);
+            AbstractElement mergedElement =
+                mergeOperator.merge(successor, reachedElement,
+                    successorPrecision);
 
             if (!mergedElement.equals(reachedElement)) {
-              logger.log(Level.FINER, "Successor was merged with element from reached set");
-              logger.log(Level.ALL, "Merged", successor, "\nand", reachedElement, "\n-->", mergedElement);
+              logger.log(Level.FINER,
+                  "Successor was merged with element from reached set");
+              logger.log(Level.ALL, "Merged", successor, "\nand",
+                  reachedElement, "\n-->", mergedElement);
               stats.countMerge++;
 
               toRemove.add(reachedElement);
-              toAdd.add(new Pair<AbstractElement, Precision>(mergedElement, successorPrecision));
+              toAdd.add(new Pair<AbstractElement, Precision>(mergedElement,
+                  successorPrecision));
             }
           }
           reachedSet.removeAll(toRemove);
           reachedSet.addAll(toAdd);
 
-          end = System.currentTimeMillis();
-          stats.mergeTime += (end - start);
+          stats.mergeTime.stop();
         }
 
-        start = System.currentTimeMillis();
-        boolean stop = stopOperator.stop(successor, reached, successorPrecision);
-        end = System.currentTimeMillis();
-        stats.stopTime += (end - start);
+        stats.stopTime.start();
+        boolean stop =
+            stopOperator.stop(successor, reached, successorPrecision);
+        stats.stopTime.stop();
 
         if (stop) {
-          logger.log(Level.FINER, "Successor is covered or unreachable, not adding to waitlist");
+          logger.log(Level.FINER,
+              "Successor is covered or unreachable, not adding to waitlist");
           stats.countStop++;
 
         } else {
-          logger.log(Level.FINER, "No need to stop, adding successor to waitlist");
+          logger.log(Level.FINER,
+              "No need to stop, adding successor to waitlist");
 
           reachedSet.add(successor, successorPrecision);
         }
       }
     }
-    stats.totalTime += System.currentTimeMillis() - startTotalTime;
+    stats.totalTime.stop();
   }
 
   @Override
