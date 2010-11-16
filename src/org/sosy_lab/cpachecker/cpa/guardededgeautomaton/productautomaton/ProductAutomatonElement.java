7d4e5c18fed44b14d9e88ae617c2ea4249c6db5d
holzer <holzer@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2010-11-16 21:29:41 +0000

    Remove unused classes.
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@2478 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/cpa/guardededgeautomaton/productautomaton/ProductAutomatonElement.java
+++
@@ -1,7 +0,0 @@
-package org.sosy_lab.cpachecker.cpa.guardededgeautomaton.productautomaton;
-
-import org.sosy_lab.cpachecker.core.interfaces.AbstractElement;
-
-public interface ProductAutomatonElement extends AbstractElement {
-
-}
