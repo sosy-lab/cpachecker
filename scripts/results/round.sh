9346cb53c100fc610afa81034b09d634063f9cdb
pwendler <pwendler@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2010-11-18 00:21:08 +0000

    fix result processing scripts
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@2490 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- scripts/results/round.sh
+++ scripts/results/round.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 BC_SCRIPT='
+	if (n < 0) {
+		print "-";
+		n *= -1;
+	}
 	if (n >= 1000) {
 		print (n+5)/10*10;
 	} else {
@@ -20,11 +24,13 @@ IFS="$(echo -e '\n\t\n')"
 while read -a LINE ; do
 	echo -n "${LINE[0]}"
 	for i in `seq 1 $(( ${#LINE[@]} - 1))` ; do
+		echo -en "\t"
 		NUMBER="${LINE[$i]}"
-		echo "$BC_SCRIPT" | bc
-		#[[ "$NUMBER" == *.* ]] && NUMBER="$(echo "n=$NUMBER; $BC_SCRIPT" | bc )"
-		#[ "${NUMBER:0:1}" = "." ] && NUMBER="0$NUMBER"
-		echo -en "\t$NUMBER"
+		if [[ "$NUMBER" == *.* ]] ; then
+			echo "n=$NUMBER; $BC_SCRIPT" | bc
+		else
+			echo -n "$NUMBER"
+		fi
 	done
 	echo ""
 done
