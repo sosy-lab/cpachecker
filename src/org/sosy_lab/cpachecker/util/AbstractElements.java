a98287d506e75c47dc5a645d8b3127c0d7d46116
pwendler <pwendler@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2010-12-08 11:23:54 +0000

    more helper methods for abstract elements
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@2620 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/util/AbstractElements.java
+++ src/org/sosy_lab/cpachecker/util/AbstractElements.java
@@ -23,14 +23,21 @@
  */
 package org.sosy_lab.cpachecker.util;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Iterator;
+
 import org.sosy_lab.cpachecker.cfa.objectmodel.CFANode;
+import org.sosy_lab.cpachecker.core.defaults.AbstractSingleWrapperElement;
 import org.sosy_lab.cpachecker.core.interfaces.AbstractElement;
 import org.sosy_lab.cpachecker.core.interfaces.AbstractElementWithLocation;
 import org.sosy_lab.cpachecker.core.interfaces.AbstractWrapperElement;
 import org.sosy_lab.cpachecker.core.interfaces.Targetable;
 
 import com.google.common.base.Function;
+import com.google.common.base.Preconditions;
 import com.google.common.base.Predicate;
+import com.google.common.collect.Iterators;
 
 /**
  * Helper class that provides several useful methods for handling AbstractElements.
@@ -42,11 +49,21 @@ public final class AbstractElements {
   public static <T extends AbstractElement> T extractElementByType(AbstractElement pElement, Class<T> pType) {
     if (pType.isInstance(pElement)) {
       return pType.cast(pElement);
+    
+    } else if (pElement instanceof AbstractSingleWrapperElement) {
+      AbstractElement wrapped = ((AbstractSingleWrapperElement)pElement).getWrappedElement();
+      return extractElementByType(wrapped, pType);
+    
     } else if (pElement instanceof AbstractWrapperElement) {
-      return ((AbstractWrapperElement)pElement).retrieveWrappedElement(pType);
-    } else {
-      return null;
+      for (AbstractElement wrapped : ((AbstractWrapperElement)pElement).getWrappedElements()) {
+        T result = extractElementByType(wrapped, pType);
+        if (result != null) {
+          return result;
+        }
+      }
     }
+
+    return null;
   }
   
   public static CFANode extractLocation(AbstractElement pElement) {
@@ -74,5 +91,58 @@ public final class AbstractElements {
         return extractElementByType(ae, pType);
       }
     };
+  }
+  
+  /**
+   * Creates an iterable that enumerates all the AbstractElements contained in
+   * a single element, including the root element itself.
+   * The tree of elements is traversed in pre-order.
+   */
+  public static Iterable<AbstractElement> asIterable(final AbstractElement ae) {
+    
+    return new Iterable<AbstractElement>() {
+      @Override
+      public Iterator<AbstractElement> iterator() {
+        
+        return new Iterator<AbstractElement>() {
+
+          private final Deque<Iterator<? extends AbstractElement>> stack = new ArrayDeque<Iterator<? extends AbstractElement>>();
+          {
+            stack.push(Iterators.singletonIterator(ae));
+          }
+          
+          @Override
+          public boolean hasNext() {
+            return !stack.isEmpty();
+          }
+
+          @Override
+          public AbstractElement next() {
+            Iterator<? extends AbstractElement> currentIterator = stack.peek();
+            Preconditions.checkState(currentIterator.hasNext());
+            AbstractElement current = currentIterator.next();
+            
+            if (!currentIterator.hasNext()) {
+              stack.pop();
+            }
+
+            if (current instanceof AbstractSingleWrapperElement) {
+              AbstractElement wrapped = ((AbstractSingleWrapperElement)current).getWrappedElement();
+              stack.push(Iterators.singletonIterator(wrapped));
+            
+            } else if (current instanceof AbstractWrapperElement) {
+              stack.push(((AbstractWrapperElement)current).getWrappedElements().iterator());
+            }
+            
+            return current;
+          }
+
+          @Override
+          public void remove() {
+            throw new UnsupportedOperationException();
+          }
+        };
+      }
+    };    
   }
 }
