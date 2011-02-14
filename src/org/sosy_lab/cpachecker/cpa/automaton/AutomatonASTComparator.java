6b09f54e12ef02f0215d867723f7d6c0b88d13e6
pwendler <pwendler@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2011-02-14 14:49:51 +0000

    move parsing-related code into class CParser so that it is easier to switch to our own AST data structure
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@2841 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/cpa/automaton/AutomatonASTComparator.java
+++ src/org/sosy_lab/cpachecker/cpa/automaton/AutomatonASTComparator.java
@@ -27,18 +27,14 @@ import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import org.eclipse.cdt.core.dom.ast.IASTCompoundStatement;
-import org.eclipse.cdt.core.dom.ast.IASTDeclaration;
 import org.eclipse.cdt.core.dom.ast.IASTExpressionStatement;
-import org.eclipse.cdt.core.dom.ast.IASTFunctionDefinition;
 import org.eclipse.cdt.core.dom.ast.IASTIdExpression;
 import org.eclipse.cdt.core.dom.ast.IASTLiteralExpression;
 import org.eclipse.cdt.core.dom.ast.IASTName;
 import org.eclipse.cdt.core.dom.ast.IASTNode;
-import org.eclipse.cdt.core.dom.ast.IASTProblem;
 import org.eclipse.cdt.core.dom.ast.IASTStatement;
-import org.eclipse.cdt.core.dom.ast.IASTTranslationUnit;
 import org.eclipse.core.runtime.CoreException;
+import org.sosy_lab.cpachecker.exceptions.UnrecognizedCCodeException;
 import org.sosy_lab.cpachecker.util.CParser;
 import org.sosy_lab.cpachecker.util.CParser.Dialect;
 
@@ -83,15 +79,13 @@ public class AutomatonASTComparator {
     // $?-Jokers, $1-Jokers and function declaration
     String tmp = addFunctionDeclaration(replaceJokersInPattern(pPattern));
 
-    IASTTranslationUnit ast = AutomatonASTComparator.parse(tmp);
-    return stripFunctionDeclaration(ast);
+    return AutomatonASTComparator.parse(tmp);
   }
 
   static IASTNode generateSourceAST(String pSource) throws InvalidAutomatonException {
     String tmp = addFunctionDeclaration(pSource);
 
-    IASTTranslationUnit ast = AutomatonASTComparator.parse(tmp);
-    return stripFunctionDeclaration(ast);
+    return AutomatonASTComparator.parse(tmp);
   }
 
   /**
@@ -105,32 +99,6 @@ public class AutomatonASTComparator {
       return "void test() { " + pBody + "}";
     } else {
       return "void test() { " + pBody + ";}";
-    }
-  }
-
-  private static IASTNode stripFunctionDeclaration(IASTTranslationUnit ast) throws InvalidAutomatonException {
-    IASTDeclaration[] declarations = ast.getDeclarations();
-    if (   declarations == null
-        || declarations.length != 1
-        || !(declarations[0] instanceof IASTFunctionDefinition)) {
-      throw new InvalidAutomatonException("Error: AST does not match the expectations");
-    }
-
-    IASTFunctionDefinition func = (IASTFunctionDefinition)declarations[0];
-    if (   !func.getDeclarator().getName().getRawSignature().equals("test")
-        || !(func.getBody() instanceof IASTCompoundStatement)) {
-      throw new InvalidAutomatonException("Error: AST does not match the expectations");
-    }
-
-    IASTStatement[] body = ((IASTCompoundStatement)func.getBody()).getStatements();
-    if (!(body.length == 2 && body[1] == null || body.length == 1)) {
-      throw new InvalidAutomatonException("Error: AST does not match the expectations");
-    }
-
-    if (body[0] instanceof IASTExpressionStatement) {
-      return ((IASTExpressionStatement)body[0]).getExpression();
-    } else {
-      return body[0];
     }
   }
 
@@ -235,28 +203,23 @@ public class AutomatonASTComparator {
    * @return The AST.
    * @throws InvalidAutomatonException
    */
-  private static IASTTranslationUnit parse(String code) throws InvalidAutomatonException {
-    IASTTranslationUnit ast;
+  private static IASTNode parse(String code) throws InvalidAutomatonException {
+    IASTStatement statement;
     try {
-      ast = CParser.parseString(code, Dialect.C99);
+      statement = CParser.parseSingleStatement(code, Dialect.C99);
     } catch (CoreException e) {
+      throw new InvalidAutomatonException("Error during parsing C code \""
+          + code + "\": " + e.getMessage());
+    } catch (UnrecognizedCCodeException e) {
       throw new InvalidAutomatonException("Error during parsing C code \""
           + code + "\": " + e.getMessage());
     }
 
-    checkForASTProblems(ast); // will throw an exception if ast contains a problem
-
-    return ast;
-  }
-
-  private static void checkForASTProblems(IASTNode pAST) throws InvalidAutomatonException {
-    if (pAST instanceof IASTProblem) {
-      throw new InvalidAutomatonException("Error during parsing C code \""
-          + pAST.getRawSignature() + "\": " + ((IASTProblem)pAST).getMessage());
+    if (statement instanceof IASTExpressionStatement) {
+      return ((IASTExpressionStatement)statement).getExpression();
     } else {
-      for (IASTNode n : pAST.getChildren()) {
-        checkForASTProblems(n);
-      }
+      return statement;
     }
   }
+
 }
