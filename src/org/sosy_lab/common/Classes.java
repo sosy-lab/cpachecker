c38b7b88327c8012df4c6a83b889d976bd9293ce
pwendler <pwendler@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2011-02-21 09:50:34 +0000

    refactoring (split methods into smaller pieces)
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@2882 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/common/Classes.java
+++ src/org/sosy_lab/common/Classes.java
@@ -60,24 +60,69 @@ public final class Classes {
   public static <T> T createInstance(String className, String prefix,
       Class<?>[] argumentTypes, Object[] argumentValues, Class<T> type)
       throws ClassInstantiationException, InvocationTargetException {
+    
+    return createInstance(className, prefix, argumentTypes, argumentValues, type, null);
+  }
+  
+  /**
+   * Creates an instance of class className, passing the objects from argumentList
+   * to the constructor and casting the object to class type.
+   *
+   * @param className The class name.
+   * @param prefix An optional package name that is prefixed to the className if the class is not found.
+   * @param argumentTypes Array with the types of the parameters of the desired constructor.
+   * @param argumentValues Array with the values that will be passed to the constructor.
+   * @param type The return type (has to be a super type of the class, of course).
+   * @param cl An optional class loader to load the class (may be null).
+   * @throws ClassInstantiationException If something goes wrong (like class cannot be found or has no constructor).
+   * @throws InvocationTargetException If the constructor throws an exception.  
+   */
+  public static <T> T createInstance(String className, String prefix,
+      Class<?>[] argumentTypes, Object[] argumentValues, Class<T> type,
+      ClassLoader cl)
+      throws ClassInstantiationException, InvocationTargetException {
     try {
-      Class<?> cls = forName(className, prefix);
-      Constructor<?> ct = cls.getConstructor(argumentTypes);
-      Object obj = ct.newInstance(argumentValues);
-      return type.cast(obj);
-
+      Class<?> cls = forName(className, prefix, cl);
+      return createInstance(cls, argumentTypes, argumentValues, type);
+      
     } catch (ClassNotFoundException e) {
       throw new ClassInstantiationException(className, "Class not found!");
     } catch (SecurityException e) {
       throw new ClassInstantiationException(className, e.getMessage());
+    }
+  }
+  
+  
+  /**
+   * Creates an instance of class cls, passing the objects from argumentList
+   * to the constructor and casting the object to class type.
+   *
+   * @param cls The class to instantiate.
+   * @param argumentTypes Array with the types of the parameters of the desired constructor.
+   * @param argumentValues Array with the values that will be passed to the constructor.
+   * @param type The return type (has to be a super type of the class, of course).
+   * @param cl An optional class loader to load the class (may be null).
+   * @throws ClassInstantiationException If something goes wrong (like class cannot be found or has no constructor).
+   * @throws InvocationTargetException If the constructor throws an exception.  
+   */
+  public static <T> T createInstance(Class<?> cls,
+      Class<?>[] argumentTypes, Object[] argumentValues, Class<T> type)
+      throws ClassInstantiationException, InvocationTargetException {
+    try {
+      Constructor<?> ct = cls.getConstructor(argumentTypes);
+      Object obj = ct.newInstance(argumentValues);
+      return type.cast(obj);
+
+    } catch (SecurityException e) {
+      throw new ClassInstantiationException(cls.getCanonicalName(), e.getMessage());
     } catch (NoSuchMethodException e) {
-      throw new ClassInstantiationException(className, "Matching constructor not found!");
+      throw new ClassInstantiationException(cls.getCanonicalName(), "Matching constructor not found!");
     } catch (InstantiationException e) {
-      throw new ClassInstantiationException(className, e.getMessage());
+      throw new ClassInstantiationException(cls.getCanonicalName(), e.getMessage());
     } catch (IllegalAccessException e) {
-      throw new ClassInstantiationException(className, e.getMessage());
+      throw new ClassInstantiationException(cls.getCanonicalName(), e.getMessage());
     } catch (ClassCastException e) {
-      throw new ClassInstantiationException(className, "Not an instance of " + type.getCanonicalName());
+      throw new ClassInstantiationException(cls.getCanonicalName(), "Not an instance of " + type.getCanonicalName());
     }
   }
   
@@ -90,19 +135,39 @@ public final class Classes {
    * @return The class object for  name  or  prefix + "." + name  
    * @throws ClassNotFoundException If none of the two classes can be found.
    */
-  public static Class<?> forName(String name, String prefix) throws ClassNotFoundException {
+  public static Class<?> forName(String name, String prefix) throws ClassNotFoundException, SecurityException {
+    return forName(name, prefix, null);
+  }
+  
+  /**
+   * Similar to {@link Class#forName(String)} and {@link ClassLoader#loadClass(String)},
+   * but if the class is not found this
+   * method re-tries with a package name prefixed.
+   * 
+   * @param name The class name.
+   * @param prefix An optional package name as prefix.
+   * @param cl An optional class loader to load the class (may be null).
+   * @return The class object for  name  or  prefix + "." + name  
+   * @throws ClassNotFoundException If none of the two classes can be found.
+   */
+  private static Class<?> forName(String name, String prefix, ClassLoader cl) throws ClassNotFoundException, SecurityException {
+    if (cl == null) {
+      // use the class loader of this class to simulate the behaviour
+      // of Class#forName(String)
+      cl = Classes.class.getClassLoader();
+    }
     if (prefix == null || prefix.isEmpty()) {
-      return Class.forName(name);
+      return cl.loadClass(name);
     }
     
     try {
-      return Class.forName(name);
+      return cl.loadClass(name);
       
-    } catch (ClassNotFoundException e1) {
+    } catch (ClassNotFoundException e) {
       try {
-        return Class.forName(prefix + "." + name); // try with prefix added
-      } catch (ClassNotFoundException e2) {
-        throw e1; // re-throw original exception to get correct error message
+        return cl.loadClass(prefix + "." + name); // try with prefix added
+      } catch (ClassNotFoundException _) {
+        throw e; // re-throw original exception to get correct error message
       }
     }
   }
