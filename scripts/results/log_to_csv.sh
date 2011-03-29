60eaeb88fff3dbf4546495455a9bd59891e767a5
pwendler <pwendler@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2011-03-29 10:17:22 +0000

    Remove some outdated scripts. run_test* and log_to_csv.sh were replaced by benchmark.py, and the rest doesn't work for a long time already.
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@3204 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- scripts/results/log_to_csv.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-sed -e 's%test/programs/[^/]*/%%g' -e 's/ *\t */\t/g' -e 's/[0-9.-]\+\([[:space:]]OUT OF MEMORY\)/MO\1/' -e 's/-1\([[:space:]]KILLED\)/> 1800\1/' -e 's/\([0-9][0-9]\)s/\1/' | tail -n +3
