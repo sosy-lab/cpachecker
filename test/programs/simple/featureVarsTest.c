bf6fc707dbaec9eb61eed61bac756dd8a1de89f3
rhein <rhein@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2011-04-08 11:39:40 +0000

    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@3273 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- test/programs/simple/featureVarsTest.c
+++ test/programs/simple/featureVarsTest.c
@@ -1,3 +1,4 @@
+
 int featureModelValid() {
   if (! __SELECTED_FEATURE_Verify)
 	  if (__SELECTED_FEATURE_Forward)
@@ -9,24 +10,8 @@ int featureModelValid() {
 }
 
 int main() {
-	int tmp = featureModelValid();
+	int tmp;
+	tmp = featureModelValid();
 	if (! tmp)
 		error: fail();
-/*
-	int tmp = 0;
-	  if (! __SELECTED_FEATURE_Verify)
-		  if (__SELECTED_FEATURE_Forward)
-			  if (! __SELECTED_FEATURE_Sign)
-				  tmp = 1;
-			   else tmp = 1;
-		   else tmp = 1;
-	   else tmp = 1;
-
-	  if (0) {}
-
-	  if (__SELECTED_FEATURE_Sign) {
-		  error: fail();
-	  }
-*/
-
 }
