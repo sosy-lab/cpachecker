3e21536f532a4e01e00472cbb6ecb6d3f6d02007
holzer <holzer@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2011-04-12 22:24:28 +0000

    adapted kbfilter for test generation
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@3291 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- test/programs/fql/ntdrivers-simplified/kbfiltr_simpl2.cil.c
+++ test/programs/fql/ntdrivers-simplified/kbfiltr_simpl2.cil.c
@@ -120,6 +120,7 @@ int KbFilter_PnP(int DeviceObject , int Irp )
   Irp__Tail__Overlay__CurrentStackLocation = __BLAST_NONDET;
   irpStack__MinorFunction = __BLAST_NONDET;
   event = __BLAST_NONDET;
+  Irp__CurrentLocation = __BLAST_NONDET;
 
   {
 #line 107
@@ -891,6 +892,7 @@ int KbFilter_DispatchPassThrough(int DeviceObject , int Irp )
 
   // initialization added by ah
   Irp__Tail__Overlay__CurrentStackLocation = __BLAST_NONDET;
+  Irp__CurrentLocation = __BLAST_NONDET;
 
   {
 #line 691
@@ -933,6 +935,8 @@ int KbFilter_Power(int DeviceObject , int Irp )
   irpStack__MinorFunction = __BLAST_NONDET;
   powerType = __BLAST_NONDET;
   powerState__DeviceState = __BLAST_NONDET;
+  Irp__CurrentLocation = __BLAST_NONDET;
+  Irp__Tail__Overlay__CurrentStackLocation = __BLAST_NONDET;
 
   {
 #line 718
