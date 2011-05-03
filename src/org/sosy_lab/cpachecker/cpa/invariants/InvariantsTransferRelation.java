a2cee25f94af9202bcb56070880ded195f151bed
pwendler <pwendler@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2011-05-03 11:41:37 +0000

    add a CPA for finding simple syntactic invariants about integer variables
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@3373 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

---
+++ src/org/sosy_lab/cpachecker/cpa/invariants/InvariantsTransferRelation.java
@@ -0,0 +1,243 @@
+/*
+ *  CPAchecker is a tool for configurable software verification.
+ *  This file is part of CPAchecker.
+ *
+ *  Copyright (C) 2007-2010  Dirk Beyer
+ *  All rights reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *
+ *  CPAchecker web page:
+ *    http://cpachecker.sosy-lab.org
+ */
+package org.sosy_lab.cpachecker.cpa.invariants;
+
+import static com.google.common.base.Objects.firstNonNull;
+
+import java.math.BigInteger;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+import org.sosy_lab.cpachecker.cfa.ast.DefaultExpressionVisitor;
+import org.sosy_lab.cpachecker.cfa.ast.IASTAssignment;
+import org.sosy_lab.cpachecker.cfa.ast.IASTBinaryExpression;
+import org.sosy_lab.cpachecker.cfa.ast.IASTBinaryExpression.BinaryOperator;
+import org.sosy_lab.cpachecker.cfa.ast.IASTCastExpression;
+import org.sosy_lab.cpachecker.cfa.ast.IASTCharLiteralExpression;
+import org.sosy_lab.cpachecker.cfa.ast.IASTCompositeTypeSpecifier;
+import org.sosy_lab.cpachecker.cfa.ast.IASTDeclaration;
+import org.sosy_lab.cpachecker.cfa.ast.IASTExpression;
+import org.sosy_lab.cpachecker.cfa.ast.IASTFunctionCallExpression;
+import org.sosy_lab.cpachecker.cfa.ast.IASTFunctionTypeSpecifier;
+import org.sosy_lab.cpachecker.cfa.ast.IASTIdExpression;
+import org.sosy_lab.cpachecker.cfa.ast.IASTInitializerExpression;
+import org.sosy_lab.cpachecker.cfa.ast.IASTIntegerLiteralExpression;
+import org.sosy_lab.cpachecker.cfa.ast.IASTParameterDeclaration;
+import org.sosy_lab.cpachecker.cfa.ast.IASTRightHandSide;
+import org.sosy_lab.cpachecker.cfa.ast.IASTSimpleDeclaration;
+import org.sosy_lab.cpachecker.cfa.ast.IASTUnaryExpression;
+import org.sosy_lab.cpachecker.cfa.ast.RightHandSideVisitor;
+import org.sosy_lab.cpachecker.cfa.ast.StorageClass;
+import org.sosy_lab.cpachecker.cfa.objectmodel.CFAEdge;
+import org.sosy_lab.cpachecker.cfa.objectmodel.c.DeclarationEdge;
+import org.sosy_lab.cpachecker.cfa.objectmodel.c.StatementEdge;
+import org.sosy_lab.cpachecker.core.interfaces.AbstractElement;
+import org.sosy_lab.cpachecker.core.interfaces.Precision;
+import org.sosy_lab.cpachecker.core.interfaces.TransferRelation;
+import org.sosy_lab.cpachecker.exceptions.CPATransferException;
+import org.sosy_lab.cpachecker.exceptions.UnrecognizedCCodeException;
+import org.sosy_lab.cpachecker.exceptions.UnrecognizedCFAEdgeException;
+
+enum InvariantsTransferRelation implements TransferRelation {
+
+  INSTANCE;
+  
+  @Override
+  public Collection<? extends AbstractElement> getAbstractSuccessors(
+      AbstractElement pElement, Precision pPrecision, CFAEdge edge)
+      throws CPATransferException {
+
+    InvariantsElement element = (InvariantsElement)pElement;
+    
+    switch (edge.getEdgeType()) {
+    case AssumeEdge:
+    case BlankEdge:
+    case FunctionCallEdge:
+    case FunctionReturnEdge:
+    case ReturnStatementEdge:
+      break;
+
+    case DeclarationEdge:
+      element = handleDeclaration(element, (DeclarationEdge)edge);
+      break;
+
+    case StatementEdge:
+      element = handleStatement(element, (StatementEdge)edge);
+      break;
+      
+    default:
+      throw new UnrecognizedCFAEdgeException(edge);
+    }
+    
+    return Collections.singleton(element);
+  }
+  
+  private InvariantsElement handleDeclaration(InvariantsElement element, DeclarationEdge edge) throws UnrecognizedCCodeException {
+    if (edge.getName() == null
+        || edge.getDeclSpecifier() instanceof IASTFunctionTypeSpecifier
+        || edge.getDeclSpecifier() instanceof IASTCompositeTypeSpecifier
+        || edge.getStorageClass() == StorageClass.TYPEDEF) {
+      
+      return element;
+    }
+    
+    String varName = edge.getName().getRawSignature();
+    if (!edge.isGlobal()) {
+      varName = edge.getSuccessor().getFunctionName() + "::" + varName;
+    }
+    
+    SimpleInterval value = SimpleInterval.infinite();
+    if (edge.getInitializer() != null && edge.getInitializer() instanceof IASTInitializerExpression) {
+      IASTExpression init = ((IASTInitializerExpression)edge.getInitializer()).getExpression();
+      value = init.accept(SimpleRightHandSideValueVisitor.VISITOR_INSTANCE);
+    }
+    
+    return element.copyAndSet(varName, value);
+  }
+  
+  private InvariantsElement handleStatement(InvariantsElement element, StatementEdge edge) throws UnrecognizedCCodeException {
+    
+    if (edge.getStatement() instanceof IASTAssignment) {
+      IASTAssignment assignment = (IASTAssignment)edge.getStatement();
+      
+      IASTExpression leftHandSide = assignment.getLeftHandSide();
+      if (leftHandSide instanceof IASTIdExpression) {
+        // a = ...
+
+        String varName = getVarName((IASTIdExpression)leftHandSide, edge);
+
+        IASTRightHandSide rightHandSide = assignment.getRightHandSide();
+        SimpleInterval rightHandValue = rightHandSide.accept(SimpleRightHandSideValueVisitor.VISITOR_INSTANCE);
+          
+        // the line above handles all "easy" assignments like a = 5
+        // now handle the special case "a = a + i" where is a literal
+        
+        if (rightHandSide instanceof IASTBinaryExpression) {
+          IASTBinaryExpression binExp = (IASTBinaryExpression)rightHandSide;
+          
+          if (binExp.getOperator() == BinaryOperator.PLUS) {
+            IASTExpression operand1 = binExp.getOperand1();
+            if (operand1 instanceof IASTIdExpression) {
+              String rightHandVar = getVarName((IASTIdExpression)operand1, edge);
+              
+              if (varName.equals(rightHandVar)) {
+                // now we are sure it's really "a = a + ..."
+                
+                rightHandValue = firstNonNull(element.get(varName), SimpleInterval.infinite());
+                SimpleInterval incrementValue = binExp.getOperand2().accept(SimpleRightHandSideValueVisitor.VISITOR_INSTANCE); 
+                
+                if (incrementValue.containsPositive()) {
+                  rightHandValue = rightHandValue.extendToPositiveInfinity();
+                }
+                if (incrementValue.containsNegative()) {
+                  rightHandValue = rightHandValue.extendToNegativeInfinity();
+                }
+              }
+            }
+          }
+        }
+        
+        element = element.copyAndSet(varName, rightHandValue);
+        
+      } else {
+        throw new UnrecognizedCCodeException("unknown left-hand side of assignment", edge, leftHandSide);
+      }
+      
+    }
+    
+    return element;
+  }
+  
+  private static String getVarName(IASTIdExpression var, CFAEdge edge) throws UnrecognizedCCodeException {
+    String varName = var.getName().getRawSignature();
+    if (var.getDeclaration() != null) {
+      IASTSimpleDeclaration decl = var.getDeclaration();
+     
+      if (!(decl instanceof IASTDeclaration || decl instanceof IASTParameterDeclaration)) {
+        throw new UnrecognizedCCodeException("unknown variable declaration", edge, var);
+      }
+      
+      if (decl instanceof IASTDeclaration && ((IASTDeclaration)decl).isGlobal()) {
+        
+      } else {
+        varName = edge.getSuccessor().getFunctionName() + "::" + varName;
+      }
+    }
+    return varName;
+  }
+  
+  private static class SimpleRightHandSideValueVisitor extends DefaultExpressionVisitor<SimpleInterval, UnrecognizedCCodeException>
+                                                       implements RightHandSideVisitor<SimpleInterval, UnrecognizedCCodeException> {
+    
+    private static SimpleRightHandSideValueVisitor VISITOR_INSTANCE = new SimpleRightHandSideValueVisitor();
+    
+    @Override
+    protected SimpleInterval visitDefault(IASTExpression pExp) {
+      return SimpleInterval.infinite();
+    }
+    
+    @Override
+    public SimpleInterval visit(IASTFunctionCallExpression pIastFunctionCallExpression) {
+      return visitDefault(null);
+    }
+    
+    @Override
+    public SimpleInterval visit(IASTIntegerLiteralExpression pE) {
+      return SimpleInterval.singleton(pE.getValue());
+    }
+    
+    @Override
+    public SimpleInterval visit(IASTCharLiteralExpression pE) {
+      return SimpleInterval.singleton(BigInteger.valueOf(pE.getCharacter()));
+    }
+    
+    @Override
+    public SimpleInterval visit(IASTCastExpression pE) throws UnrecognizedCCodeException {
+      SimpleInterval operand = pE.getOperand().accept(this);
+      return operand;
+    }
+    
+    @Override
+    public SimpleInterval visit(IASTUnaryExpression pE) throws UnrecognizedCCodeException {
+      
+      switch (pE.getOperator()) {
+      case MINUS:
+        SimpleInterval operand = pE.getOperand().accept(this);
+        return operand.negate();
+      
+      default:
+        return super.visit(pE);
+      }
+    }    
+  }
+
+  @Override
+  public Collection<? extends AbstractElement> strengthen(
+      AbstractElement pElement, List<AbstractElement> pOtherElements,
+      CFAEdge pCfaEdge, Precision pPrecision) {
+
+    return null;
+  }
+}
