8ccb9e326965e4c1907781469b3db0875945f9e8
pdaca <pdaca@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2011-07-08 14:09:03 +0000

    Commit before changes.
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/multi-threaded@3876 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

---
+++ lbe-rg-run.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+scripts/cpa.sh -concurrent -config test/config/rg-symbpredabsCPA-lbe.properties test/programs/multi-threaded/simple-thr1-safe.cil.c test/programs/multi-threaded/simple-thr2-safe.cil.c
---
+++ sbe-rg-run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+scripts/cpa.sh -concurrent -config test/config/rg-symbpredabsCPA-sbe.properties test/programs/multi-threaded/simple-thr1-safe.cil.c test/programs/multi-threaded/simple-thr2-safe.cil.c
+
