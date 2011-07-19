8b4b22b8b5fab14adaa7b36c7294bf6d6ebfdc6c
pdaca <pdaca@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2011-07-19 12:40:45 +0000

    Cleaning-up.
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/multi-threaded@3954 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

---
+++ test/run_scripts/various/dekker-it15.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+exec_path=".."
+$exec_path/scripts/cpa.sh -concurrent -config $exec_path/test/config/benchmarks/rg-dekker-abe-it15.properties $exec_path/test/programs/multi-threaded/threader_benchmarks/dekker-thr1.cil.c  $exec_path/test/programs/multi-threaded/threader_benchmarks/dekker-thr2.cil.c
+
