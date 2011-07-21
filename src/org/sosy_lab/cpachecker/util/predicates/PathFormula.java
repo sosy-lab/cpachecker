710d91d54d00ad7d3b783de51d89e6fe21d1f4ca
pwendler <pwendler@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2011-07-21 08:15:35 +0000

    Use length in equality check of PathFormulas. Otherwise the wrong path formula may get retrieved from the cache. (Thanks to pdaca for finding this).
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@3970 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/util/predicates/PathFormula.java
+++ src/org/sosy_lab/cpachecker/util/predicates/PathFormula.java
@@ -65,11 +65,12 @@ public class PathFormula {
 
     PathFormula other = (PathFormula)obj;
     return formula.equals(other.formula)
-        && ssa.equals(other.ssa);
+        && ssa.equals(other.ssa)
+        && (length == other.length);
   }
 
   @Override
   public int hashCode() {
-    return formula.hashCode() * 17 + ssa.hashCode();
+    return (formula.hashCode() * 17 + ssa.hashCode()) * 31 + length;
   }
 }
