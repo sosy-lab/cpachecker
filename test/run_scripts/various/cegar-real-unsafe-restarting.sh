6fb261d8e9857c725a984133a2a08844738f78a7
pdaca <pdaca@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2011-08-17 13:34:26 +0000

    Cleaning-up temp files.
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/multi-threaded@4154 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- test/run_scripts/various/cegar-real-unsafe-restarting.sh
+++ test/run_scripts/various/cegar-real-unsafe-restarting.sh
@@ -4,5 +4,5 @@ if [ -z "$CPAchecker_mt" ]; then
     exit 1
 fi  
 cd $CPAchecker_mt
-scripts/cpa.sh -concurrent -config test/config/various/rg-cegar-real-unsafe.properties test/programs/multi-threaded/various/real-unsafe-thr0.cil.c  test/programs/multi-threaded/various/real-unsafe-thr1.cil.c
+scripts/cpa.sh -concurrent -config test/config/various/rg-cegar-real-restarting.properties test/programs/multi-threaded/various/real-unsafe-thr0.cil.c  test/programs/multi-threaded/various/real-unsafe-thr1.cil.c
 
