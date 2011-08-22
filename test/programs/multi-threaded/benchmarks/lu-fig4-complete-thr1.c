ad21b1882381640786db255a9a9c1d9b21186972
pdaca <pdaca@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2011-08-22 14:32:12 +0000

    New benchmark.
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/multi-threaded@4182 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

---
+++ test/programs/multi-threaded/benchmarks/lu-fig4-complete-thr1.c
@@ -0,0 +1,48 @@
+#include <assert.h>
+#include "lu-fig4.h"
+
+bool __START_ASYNC__ = False; // models if the second thread can start
+int __COUNT__ = 0; // models a counter which monitors order violations
+
+
+
+void main (ExtendedParamBlock pbAsyncPtr_thread)
+{ // (mozilla/nsprpub/pr/src/md/mac/macthr.c 3.13)
+  while (__START_ASYNC__ == False) {}; // second thread waits until the async read is issued
+  PRThread thread = pbAsyncPtr_thread;
+
+  if (_PR_MD_GET_INTSOFF()) {
+    return;
+  }
+  //  _PR_MD_SET_INTSOFF(1);
+
+  DoneWaitingOnThisThread(thread);
+
+  //  _PR_MD_SET_INTSOFF(0);
+
+}
+
+inline void DoneWaitingOnThisThread(PRThread thread)
+{
+  int is;
+  int thread_md_asyncIOLock;
+  int thread_io_pending;
+  int thread_md_asyncIOCVar;
+
+  //  _PR_INTSOFF(is);
+  //PR_Lock(thread_md_asyncIOLock);
+
+  if (__COUNT__ == 1) {
+    thread_io_pending = PR_FALSE; // check for order violation
+    __COUNT__ = __COUNT__ + 1;
+  } else {
+    assert(0);
+  }
+
+  // let the waiting thread know that async IO completed
+  //  PR_NotifyCondVar(thread_md_asyncIOCVar);
+  //  PR_Unlock(thread_md_asyncIOLock);
+  //  _PR_FAST_INTSON(is);
+}
+
+inline bool _PR_MD_GET_INTSOFF() { return PR_FALSE; }
