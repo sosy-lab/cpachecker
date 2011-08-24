7e99694c2c88727f748708b2f03a788c2838228b
pwendler <pwendler@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2011-08-24 11:00:51 +0000

    typo
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@4195 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/cfa/CFACreator.java
+++ src/org/sosy_lab/cpachecker/cfa/CFACreator.java
@@ -201,7 +201,7 @@ public class CFACreator {
         CFACreator.loops = loops.build();
       } catch (ParserException e) {
         // don't abort here, because if the analysis doesn't need the loop information, we can continue
-        logger.logUserException(Level.WARNING, e, "Could not analze loop structure of program");
+        logger.logUserException(Level.WARNING, e, "Could not analyze loop structure of program");
       }
 
       // Insert call and return edges and build the supergraph
