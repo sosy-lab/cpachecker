133d4ddbcb43d06806baed050014d09ebd27112b
pwendler <pwendler@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2011-10-11 08:10:29 +0000

    make class immutable (at least as long as nobody writes to the stack it contains)
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@4507 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/core/algorithm/cbmctools/CBMCEdge.java
+++ src/org/sosy_lab/cpachecker/core/algorithm/cbmctools/CBMCEdge.java
@@ -28,12 +28,12 @@ import java.util.Stack;
 import org.sosy_lab.cpachecker.cfa.objectmodel.CFAEdge;
 import org.sosy_lab.cpachecker.cpa.art.ARTElement;
 
-public class CBMCEdge implements Comparable<CBMCEdge>{
+class CBMCEdge implements Comparable<CBMCEdge> {
 
-  private ARTElement parentElement;
-  private ARTElement childElement;
-  private CFAEdge edge;
-  private Stack<Stack<CBMCStackElement>> stack;
+  private final ARTElement parentElement;
+  private final ARTElement childElement;
+  private final CFAEdge edge;
+  private final Stack<Stack<CBMCStackElement>> stack;
 
   public CBMCEdge(ARTElement pParentElement, ARTElement pChildElement,
       CFAEdge pEdge, Stack<Stack<CBMCStackElement>> pStack) {
@@ -47,38 +47,16 @@ public class CBMCEdge implements Comparable<CBMCEdge>{
     return parentElement;
   }
 
-  public void setParentElement(ARTElement pParentElement) {
-    parentElement = pParentElement;
-  }
-
   public ARTElement getChildElement() {
     return childElement;
-  }
-
-  public void setChildElement(ARTElement pChildElement) {
-    childElement = pChildElement;
   }
 
   public CFAEdge getEdge() {
     return edge;
   }
 
-  public void setEdge(CFAEdge pEdge) {
-    edge = pEdge;
-  }
-
-  public void pushToStack(Stack<CBMCStackElement> pStack){
-    stack.push(pStack);
-  }
-
   public Stack<Stack<CBMCStackElement>> getStack() {
     return stack;
-  }
-
-  public void writeToTheLastStackElement(Object pStatement){
-    Stack<CBMCStackElement> lastFunctionStack = stack.peek();
-    CBMCStackElement lastStackElement = lastFunctionStack.peek();
-    lastStackElement.write(pStatement);
   }
 
   @Override
