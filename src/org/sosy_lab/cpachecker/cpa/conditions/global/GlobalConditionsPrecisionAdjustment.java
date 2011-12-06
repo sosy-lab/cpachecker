e25c4dd6aa0bf04f4dd25e10a68171ad91e74ec6
pwendler <pwendler@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2011-12-06 13:21:24 +0000

    Rename package cpa.conditions.globalconditions to cpa.conditions.global
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@5031 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/cpa/conditions/global/GlobalConditionsPrecisionAdjustment.java
+++ src/org/sosy_lab/cpachecker/cpa/conditions/global/GlobalConditionsPrecisionAdjustment.java
@@ -21,7 +21,7 @@
  *  CPAchecker web page:
  *    http://cpachecker.sosy-lab.org
  */
-package org.sosy_lab.cpachecker.cpa.conditions.globalconditions;
+package org.sosy_lab.cpachecker.cpa.conditions.global;
 
 import java.lang.management.ManagementFactory;
 import java.lang.management.MemoryMXBean;
