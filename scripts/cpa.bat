8a4de73ba665ad921d261d45067332fa162582c2
stieglma <stieglma@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2011-12-14 15:53:28 +0000

    cpa.bat file fixed
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@5158 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- scripts/cpa.bat
+++ scripts/cpa.bat
@@ -1,6 +1,7 @@
 @echo off
 
 :: parse the parameters into one string and give that to the cpaChecker
+set params=""
 :parameterparsing
 if "%1"=="" goto cont
 set params=%params% %1
@@ -14,5 +15,7 @@ set CLASSPATH=bin;cpachecker.jar
 for %%i in (lib\*.jar) do call scripts\classpath.bat %%i
 for %%i in (lib\java\runtime\*.jar) do call scripts\classpath.bat %%i
 
-
-java -Djava.library.path=/lib/native/x86-win32/ -Xmx1200m -ea org.sosy_lab.cpachecker.cmdline.CPAMain %params%
+set OLDPATH=%PATH%
+set PATH=%PATH%;lib/native/x86-win32
+java -Djava.library.path=lib/native/x86-win32/ -Xmx1200m -ea org.sosy_lab.cpachecker.cmdline.CPAMain %params%
+set PATH=%OLDPATH%
