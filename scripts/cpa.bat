c14d73775c1f755b044057a38368362da5f942ff
pwendler <pwendler@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2012-01-20 08:00:14 +0000

    Simplify scripts by using a feature of newer JVMs: The classpath variable can contain '*' and the JVM will expand this to all JARs in a folder. Now we don't need to do the expansion ourselves.
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@5307 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- scripts/cpa.bat
+++ scripts/cpa.bat
@@ -1,12 +1,10 @@
 @echo off
 
 
-:: build the Classpath: bin directory, every .jar in lib and every .jar in lib/eclipse/ 
-set CLASSPATH=bin;cpachecker.jar
-for %%i in (lib\*.jar) do call scripts\classpath.bat %%i
-for %%i in (lib\java\runtime\*.jar) do call scripts\classpath.bat %%i
+:: build the Classpath: bin directory, every .jar in lib and every .jar in lib\java\runtime
+set CLASSPATH="bin;cpachecker.jar:lib\*:lib\java\runtime\*"
 
 set OLDPATH=%PATH%
-set PATH=%PATH%;lib/native/x86-win32
-java -Djava.library.path=lib/native/x86-win32/ -Xmx1200m -ea org.sosy_lab.cpachecker.cmdline.CPAMain %*
+set PATH=%PATH%;lib\native\x86-win32
+java -Djava.library.path=lib\native\x86-win32 -Xmx1200m -ea org.sosy_lab.cpachecker.cmdline.CPAMain %*
 set PATH=%OLDPATH%
