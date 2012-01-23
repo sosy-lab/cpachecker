c510c46ef6b9ab08982b4bdfce420888ad562f75
stieglma <stieglma@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2012-01-23 15:20:13 +0000

    fixed startscript for Windows
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@5333 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- scripts/cpa.bat
+++ scripts/cpa.bat
@@ -1,10 +1,6 @@
 @echo off
 
-
-:: build the Classpath: bin directory, every .jar in lib and every .jar in lib\java\runtime
-set CLASSPATH="bin;cpachecker.jar:lib\*:lib\java\runtime\*"
-
 set OLDPATH=%PATH%
 set PATH=%PATH%;lib\native\x86-win32
-java -Djava.library.path=lib\native\x86-win32 -Xmx1200m -ea org.sosy_lab.cpachecker.cmdline.CPAMain %*
+java -Djava.library.path=lib\native\x86-win32 -cp bin;cpachecker.jar;lib\*;lib\java\runtime\* -Xmx1200m -ea org.sosy_lab.cpachecker.cmdline.CPAMain %*
 set PATH=%OLDPATH%
