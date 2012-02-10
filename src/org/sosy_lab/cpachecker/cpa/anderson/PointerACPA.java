63ce592c3cb033cf0dc0eb8b7863c2ac6956d589
kronawit <kronawit@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2012-02-10 08:40:49 +0000

    initial version of andersons points to analysis
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@5548 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

---
+++ src/org/sosy_lab/cpachecker/cpa/anderson/PointerACPA.java
@@ -0,0 +1,192 @@
+/*
+ *  CPAchecker is a tool for configurable software verification.
+ *  This file is part of CPAchecker.
+ *
+ *  Copyright (C) 2007-2011  Dirk Beyer
+ *  All rights reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *
+ *  CPAchecker web page:
+ *    http://cpachecker.sosy-lab.org
+ */
+package org.sosy_lab.cpachecker.cpa.anderson;
+
+import org.sosy_lab.common.LogManager;
+import org.sosy_lab.common.configuration.Configuration;
+import org.sosy_lab.common.configuration.InvalidConfigurationException;
+import org.sosy_lab.common.configuration.Option;
+import org.sosy_lab.common.configuration.Options;
+import org.sosy_lab.cpachecker.cfa.objectmodel.CFANode;
+import org.sosy_lab.cpachecker.core.defaults.AutomaticCPAFactory;
+import org.sosy_lab.cpachecker.core.defaults.MergeJoinOperator;
+import org.sosy_lab.cpachecker.core.defaults.MergeSepOperator;
+import org.sosy_lab.cpachecker.core.defaults.StaticPrecisionAdjustment;
+import org.sosy_lab.cpachecker.core.defaults.StopJoinOperator;
+import org.sosy_lab.cpachecker.core.defaults.StopNeverOperator;
+import org.sosy_lab.cpachecker.core.defaults.StopSepOperator;
+import org.sosy_lab.cpachecker.core.interfaces.AbstractDomain;
+import org.sosy_lab.cpachecker.core.interfaces.AbstractElement;
+import org.sosy_lab.cpachecker.core.interfaces.CPAFactory;
+import org.sosy_lab.cpachecker.core.interfaces.ConfigurableProgramAnalysisWithABM;
+import org.sosy_lab.cpachecker.core.interfaces.MergeOperator;
+import org.sosy_lab.cpachecker.core.interfaces.Precision;
+import org.sosy_lab.cpachecker.core.interfaces.PrecisionAdjustment;
+import org.sosy_lab.cpachecker.core.interfaces.Reducer;
+import org.sosy_lab.cpachecker.core.interfaces.StopOperator;
+import org.sosy_lab.cpachecker.core.interfaces.TransferRelation;
+import org.sosy_lab.cpachecker.cpa.pointerA.PointerACPA;
+import org.sosy_lab.cpachecker.cpa.pointerA.PointerADomain;
+import org.sosy_lab.cpachecker.cpa.pointerA.PointerAElement;
+import org.sosy_lab.cpachecker.cpa.pointerA.PointerAPrecision;
+import org.sosy_lab.cpachecker.cpa.pointerA.PointerATransferRelation;
+
+@Options(prefix="cpa.pointerA")
+public class PointerACPA implements ConfigurableProgramAnalysisWithABM {
+
+  public static CPAFactory factory()
+  {
+    return AutomaticCPAFactory.forType(PointerACPA.class);
+  }
+
+  @Option(name="merge", toUppercase=true, values={"SEP", "JOIN"},
+      description="which merge operator to use for PointerACPA")
+  private String mergeType = "SEP";
+
+  @Option(name="stop", toUppercase=true, values={"SEP", "JOIN", "NEVER"},
+      description="which stop operator to use for PointerACPA")
+  private String stopType = "SEP";
+
+//  @Option(name="variableBlacklist",
+//      description="blacklist regex for variables that won't be tracked by PointerACPA")
+//  private String variableBlacklist = "";
+
+//  private ExplicitPrecision precision;
+
+  private AbstractDomain abstractDomain;
+  private MergeOperator mergeOperator;
+  private StopOperator stopOperator;
+  private TransferRelation transferRelation;
+  private PrecisionAdjustment precisionAdjustment;
+//  private final ExplicitReducer reducer;
+
+  private final Configuration config;
+  private final LogManager logger;
+
+  private PointerACPA(Configuration config, LogManager logger) throws InvalidConfigurationException {
+    this.config = config;
+    this.logger = logger;
+
+    config.inject(this);
+
+    abstractDomain      = new PointerADomain();
+    transferRelation    = new PointerATransferRelation(config);
+//    precision           = initializePrecision();
+    mergeOperator       = initializeMergeOperator();
+    stopOperator        = initializeStopOperator();
+    precisionAdjustment = StaticPrecisionAdjustment.getInstance();
+//    reducer             = new ExplicitReducer();
+  }
+
+  private MergeOperator initializeMergeOperator() {
+    if(mergeType.equals("SEP")) {
+      return MergeSepOperator.getInstance();
+    }
+
+    else if(mergeType.equals("JOIN")) {
+      return new MergeJoinOperator(abstractDomain);
+    }
+
+    return null;
+  }
+
+  private StopOperator initializeStopOperator() {
+    if(stopType.equals("SEP")) {
+      return new StopSepOperator(abstractDomain);
+    }
+
+    else if(stopType.equals("JOIN")) {
+      return new StopJoinOperator(abstractDomain);
+    }
+
+    else if(stopType.equals("NEVER")) {
+      return new StopNeverOperator();
+    }
+
+    return null;
+  }
+
+//  private ExplicitPrecision initializePrecision() {
+//    HashMultimap<CFANode, String> whitelist = null;
+//
+//    if(this.useCegar()) {
+//      whitelist = HashMultimap.create();
+//    }
+//
+//    return new ExplicitPrecision(variableBlacklist, whitelist);
+//  }
+
+  @Override
+  public AbstractDomain getAbstractDomain() {
+    return abstractDomain;
+  }
+
+  @Override
+  public MergeOperator getMergeOperator() {
+    return mergeOperator;
+  }
+
+  @Override
+  public StopOperator getStopOperator() {
+    return stopOperator;
+  }
+
+  @Override
+  public TransferRelation getTransferRelation() {
+    return transferRelation;
+  }
+
+  @Override
+  public AbstractElement getInitialElement(CFANode node) {
+    return new PointerAElement();
+  }
+
+  @Override
+  public Precision getInitialPrecision(CFANode pNode) {
+    return new PointerAPrecision();
+  }
+
+  @Override
+  public PrecisionAdjustment getPrecisionAdjustment() {
+    return precisionAdjustment;
+  }
+
+  protected Configuration getConfiguration() {
+    return config;
+  }
+
+  protected LogManager getLogger() {
+    return logger;
+  }
+
+  private boolean useCegar() {
+    return this.config.getProperty("analysis.useRefinement") != null
+      && this.config.getProperty("cegar.refiner").equals("cpa.pointerA.PointerARefiner");
+  }
+
+  @Override
+  public Reducer getReducer() {
+    return null;
+  }
+}
---
+++ src/org/sosy_lab/cpachecker/cpa/anderson/PointerATransferRelation.java
@@ -0,0 +1,299 @@
+/*
+ *  CPAchecker is a tool for configurable software verification.
+ *  This file is part of CPAchecker.
+ *
+ *  Copyright (C) 2007-2011  Dirk Beyer
+ *  All rights reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *
+ *  CPAchecker web page:
+ *    http://cpachecker.sosy-lab.org
+ */
+package org.sosy_lab.cpachecker.cpa.anderson;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+import org.sosy_lab.common.configuration.Configuration;
+import org.sosy_lab.common.configuration.InvalidConfigurationException;
+import org.sosy_lab.common.configuration.Options;
+import org.sosy_lab.cpachecker.cfa.ast.IASTAssignment;
+import org.sosy_lab.cpachecker.cfa.ast.IASTCastExpression;
+import org.sosy_lab.cpachecker.cfa.ast.IASTExpression;
+import org.sosy_lab.cpachecker.cfa.ast.IASTExpressionStatement;
+import org.sosy_lab.cpachecker.cfa.ast.IASTFunctionCallExpression;
+import org.sosy_lab.cpachecker.cfa.ast.IASTFunctionCallStatement;
+import org.sosy_lab.cpachecker.cfa.ast.IASTIdExpression;
+import org.sosy_lab.cpachecker.cfa.ast.IASTInitializer;
+import org.sosy_lab.cpachecker.cfa.ast.IASTInitializerExpression;
+import org.sosy_lab.cpachecker.cfa.ast.IASTRightHandSide;
+import org.sosy_lab.cpachecker.cfa.ast.IASTStatement;
+import org.sosy_lab.cpachecker.cfa.ast.IASTUnaryExpression;
+import org.sosy_lab.cpachecker.cfa.ast.IASTUnaryExpression.UnaryOperator;
+import org.sosy_lab.cpachecker.cfa.ast.IASTVariableDeclaration;
+import org.sosy_lab.cpachecker.cfa.objectmodel.CFAEdge;
+import org.sosy_lab.cpachecker.cfa.objectmodel.c.DeclarationEdge;
+import org.sosy_lab.cpachecker.cfa.objectmodel.c.StatementEdge;
+import org.sosy_lab.cpachecker.core.interfaces.AbstractElement;
+import org.sosy_lab.cpachecker.core.interfaces.Precision;
+import org.sosy_lab.cpachecker.core.interfaces.TransferRelation;
+import org.sosy_lab.cpachecker.cpa.anderson.util.BaseConstraint;
+import org.sosy_lab.cpachecker.cpa.anderson.util.ComplexConstraint;
+import org.sosy_lab.cpachecker.cpa.anderson.util.SimpleConstraint;
+import org.sosy_lab.cpachecker.cpa.pointerA.PointerAElement;
+import org.sosy_lab.cpachecker.exceptions.CPATransferException;
+import org.sosy_lab.cpachecker.exceptions.UnrecognizedCCodeException;
+
+@Options(prefix = "cpa.pointerA")
+public class PointerATransferRelation implements TransferRelation {
+
+  public PointerATransferRelation(Configuration config) throws InvalidConfigurationException {
+    config.inject(this);
+  }
+
+  @Override
+  public Collection<AbstractElement> getAbstractSuccessors(AbstractElement element, Precision pPrecision,
+      CFAEdge cfaEdge)
+      throws CPATransferException {
+
+    AbstractElement successor = null;
+    PointerAElement pointerAElement = (PointerAElement) element;
+
+    // check the type of the edge
+    switch (cfaEdge.getEdgeType()) {
+
+    // if edge is a statement edge, e.g. a = b + c
+    case StatementEdge:
+      StatementEdge statementEdge = (StatementEdge) cfaEdge;
+      successor = handleStatement(pointerAElement, statementEdge.getStatement(), cfaEdge);
+      break;
+
+
+    //    case ReturnStatementEdge:
+    //      ReturnStatementEdge returnEdge = (ReturnStatementEdge)cfaEdge;
+    //      // this statement is a function return, e.g. return (a);
+    //      // note that this is different from return edge
+    //      // this is a statement edge which leads the function to the
+    //      // last node of its CFA, where return edge is from that last node
+    //      // to the return site of the caller function
+    //      successor = handleExitFromFunction(pointerAElement, returnEdge.getExpression(), returnEdge);
+    //      break;
+
+    // edge is a declaration edge, e.g. int a;
+    case DeclarationEdge:
+      DeclarationEdge declarationEdge = (DeclarationEdge) cfaEdge;
+      successor = handleDeclaration(pointerAElement, declarationEdge);
+      break;
+
+    // this is an assumption, e.g. if(a == b)
+    case AssumeEdge:
+      successor = pointerAElement.clone();
+//      AssumeEdge assumeEdge = (AssumeEdge) cfaEdge;
+//      successor =
+//          handleAssumption(pointerAElement.clone(), assumeEdge.getExpression(), cfaEdge,
+//              assumeEdge.getTruthAssumption());
+      break;
+
+    case BlankEdge:
+      successor = pointerAElement.clone();
+      break;
+
+    //    case FunctionCallEdge:
+    //      FunctionCallEdge functionCallEdge = (FunctionCallEdge) cfaEdge;
+    //      successor = handleFunctionCall(pointerAElement, functionCallEdge);
+    //      break;
+    //
+    //    // this is a return edge from function, this is different from return statement
+    //    // of the function. See case for statement edge for details
+    //    case FunctionReturnEdge:
+    //      FunctionReturnEdge functionReturnEdge = (FunctionReturnEdge) cfaEdge;
+    //      successor = handleFunctionReturn(pointerAElement, functionReturnEdge);
+    //      break;
+
+    default:
+      printWarning(cfaEdge);
+
+      //      throw new UnrecognizedCFAEdgeException(cfaEdge);
+    }
+
+    if (successor == null)
+      return Collections.emptySet();
+
+    else
+      return Collections.singleton(successor);
+  }
+
+  @Override
+  public Collection<? extends AbstractElement> strengthen(AbstractElement element, List<AbstractElement> elements,
+      CFAEdge cfaEdge, Precision precision)
+      throws UnrecognizedCCodeException {
+
+    return null;
+  }
+
+  private PointerAElement handleStatement(PointerAElement element, IASTStatement expression, CFAEdge cfaEdge)
+      throws UnrecognizedCCodeException {
+
+    // e.g. a = b;
+    if (expression instanceof IASTAssignment)
+      return handleAssignment(element, (IASTAssignment) expression, cfaEdge);
+
+    // external function call - do nothing
+    else if (expression instanceof IASTFunctionCallStatement)
+      return element.clone();
+
+    else if (expression instanceof IASTExpressionStatement)
+      return element.clone();
+
+    else
+      throw new UnrecognizedCCodeException(cfaEdge, expression);
+  }
+
+  private PointerAElement handleAssignment(PointerAElement element, IASTAssignment assignExpression, CFAEdge cfaEdge)
+      throws UnrecognizedCCodeException {
+
+    IASTExpression op1 = assignExpression.getLeftHandSide();
+    IASTRightHandSide op2 = assignExpression.getRightHandSide();
+
+    if (op1 instanceof IASTIdExpression) {
+
+      // a = ...
+
+      return handleAssignmentTo(op1.toASTString(), op2, element, cfaEdge);
+
+    } else if (op1 instanceof IASTUnaryExpression && ((IASTUnaryExpression) op1).getOperator() == UnaryOperator.STAR
+        && op2 instanceof IASTIdExpression) {
+
+      // *a = b; complex constraint
+
+      op1 = ((IASTUnaryExpression) op1).getOperand();
+
+      if (op1 instanceof IASTIdExpression) {
+
+        PointerAElement succ = element.clone();
+        succ.addConstraint(new ComplexConstraint(op2.toASTString(), op1.toASTString(), false));
+        return succ;
+
+      } else
+        throw new UnrecognizedCCodeException("not supported", cfaEdge, op2);
+
+    } else
+      throw new UnrecognizedCCodeException("not supported", cfaEdge, op1);
+  }
+
+  private PointerAElement handleAssignmentTo(String op1, IASTRightHandSide op2, PointerAElement element, CFAEdge cfaEdge)
+      throws UnrecognizedCCodeException {
+
+    // unpack cast if necessary
+    while (op2 instanceof IASTCastExpression)
+      op2 = ((IASTCastExpression) op2).getOperand();
+
+    if (op2 instanceof IASTIdExpression) {
+
+      // a = b; simple constraint
+
+      PointerAElement succ = element.clone();
+      succ.addConstraint(new SimpleConstraint(op2.toASTString(), op1));
+      return succ;
+
+    } else if (op2 instanceof IASTUnaryExpression && ((IASTUnaryExpression) op2).getOperator() == UnaryOperator.AMPER) {
+
+      // a = &b; base constraint
+
+      op2 = ((IASTUnaryExpression) op2).getOperand();
+
+      if (op2 instanceof IASTIdExpression) {
+
+        PointerAElement succ = element.clone();
+        succ.addConstraint(new BaseConstraint(op2.toASTString(), op1));
+        return succ;
+
+      } else
+        throw new UnrecognizedCCodeException("not supported", cfaEdge, op2);
+
+    } else if (op2 instanceof IASTUnaryExpression && ((IASTUnaryExpression) op2).getOperator() == UnaryOperator.STAR) {
+
+      // a = *b; complex constraint
+
+      op2 = ((IASTUnaryExpression) op2).getOperand();
+
+      if (op2 instanceof IASTIdExpression) {
+
+        PointerAElement succ = element.clone();
+        succ.addConstraint(new ComplexConstraint(op2.toASTString(), op1, true));
+        return succ;
+
+      } else
+        throw new UnrecognizedCCodeException("not supported", cfaEdge, op2);
+
+    } else if (op2 instanceof IASTFunctionCallExpression
+        && "malloc".equals(((IASTFunctionCallExpression) op2).getFunctionNameExpression().toASTString())) {
+
+      PointerAElement succ = element.clone();
+      succ.addConstraint(new BaseConstraint("malloc-" + cfaEdge.getLineNumber(), op1));
+      return succ;
+
+    }
+
+    // not implemented, or not interessing
+    printWarning(cfaEdge);
+    return element.clone();
+  }
+
+  private PointerAElement handleDeclaration(PointerAElement element, DeclarationEdge declarationEdge)
+      throws UnrecognizedCCodeException {
+
+    if (!(declarationEdge.getDeclaration() instanceof IASTVariableDeclaration)) {
+      // nothing interesting to see here, please move along
+      return element.clone();
+    }
+
+    IASTVariableDeclaration decl = (IASTVariableDeclaration)declarationEdge.getDeclaration();
+
+    // get the variable name in the declarator
+    String varName = decl.getName();
+    //    String functionName = declarationEdge.getPredecessor().getFunctionName();
+
+    //    // handle global variables
+    //    if(declarationEdge.isGlobal())
+    //    {
+    //      // if this is a global variable, add to the list of global variables
+    //      globalVariables.add(varName);
+    //
+    //      // global variables without initializer are set to 0 in C
+    //      initialValue = 0L;
+    //    }
+
+    // get initial value
+    IASTInitializer init = decl.getInitializer();
+    if (init instanceof IASTInitializerExpression) {
+
+      IASTRightHandSide exp = ((IASTInitializerExpression) init).getExpression();
+
+      return handleAssignmentTo(varName, exp, element, declarationEdge);
+    }
+
+    return element.clone();
+  }
+
+  private void printWarning(CFAEdge cfaEdge) {
+
+    StackTraceElement[] trace = Thread.currentThread().getStackTrace();
+
+    System.err.println("Warning! CFA Edge \"" + cfaEdge.getRawStatement() + "\" (line: " + cfaEdge.getLineNumber()
+        + ") not handled. [Method: " + trace[2].toString() + ']');
+  }
+}
