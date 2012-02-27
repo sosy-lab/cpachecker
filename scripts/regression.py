65151bc8a8c09ec1d5eea23ebd5fae5bf8e8786e
friedber <friedber@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2012-02-27 08:25:43 +0000

    bugfix in regression.py due to last change in table-generator.py.
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@5755 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- scripts/regression.py
+++ scripts/regression.py
@@ -26,7 +26,7 @@ CPAchecker web page:
 
 # import our own modules
 TableGenerator = __import__('table-generator') # for '-' in module-name
-FileUtil = TableGenerator # only for different names in programm
+Util = TableGenerator.Util # only for different names in programm
 
 import xml.etree.ElementTree as ET
 import sys
@@ -138,7 +138,7 @@ def getFilenameList(listOfSourcefileTags, listOfSourcefileDics, compare):
 
 def status(currentFile):
     fileName = currentFile.get('name').lower()
-    isSafeFile = not TableGenerator.containsAny(fileName, TableGenerator.BUG_SUBSTRING_LIST)
+    isSafeFile = not Util.containsAny(fileName, TableGenerator.BUG_SUBSTRING_LIST)
     status = [x for x in currentFile.findall("column") if x.get("title") == "status"][0].get("value").lower()
 
     if status not in ('safe', 'unsafe'):
@@ -149,7 +149,7 @@ def compareResults(xmlFiles, options):
     compare = options.compare
     print ('\ncomparing results ...')
 
-    resultFiles = FileUtil.extendFileList(xmlFiles)
+    resultFiles = Util.extendFileList(xmlFiles)
 
     if len(resultFiles) == 0:
         print ('Resultfile not found. Check your filenames!')
