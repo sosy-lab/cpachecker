c726dfe95e2c46b3d563c9a4895d22c3a13068bc
stieglma <stieglma@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2012-06-25 09:23:23 +0000

    fixed a bug in convert(iastunaryexpression) where temporary variables were created to often with statements like i++;
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@6249 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/cfa/parser/eclipse/ASTConverter.java
+++ src/org/sosy_lab/cpachecker/cfa/parser/eclipse/ASTConverter.java
@@ -41,7 +41,6 @@ import org.eclipse.cdt.core.dom.ast.IASTArraySubscriptExpression;
 import org.eclipse.cdt.core.dom.ast.IASTBinaryExpression;
 import org.eclipse.cdt.core.dom.ast.IASTCastExpression;
 import org.eclipse.cdt.core.dom.ast.IASTCompositeTypeSpecifier;
-import org.eclipse.cdt.core.dom.ast.IASTCompoundStatement;
 import org.eclipse.cdt.core.dom.ast.IASTConditionalExpression;
 import org.eclipse.cdt.core.dom.ast.IASTDeclSpecifier;
 import org.eclipse.cdt.core.dom.ast.IASTDeclaration;
@@ -767,7 +766,7 @@ class ASTConverter {
       default: throw new AssertionError();
       }
 
-      if (!(e.getParent() instanceof IASTCompoundStatement)
+      if (!(e.getParent() instanceof IASTExpressionStatement)
           && !(e.getParent() instanceof IASTExpressionList)) {
         return addSideAssignmentsForUnaryExpressions(e, operand, fileLoc, type, postOp);
       } else {
