9ca80eaf0befb903767d3ad860c434d447b74c07
pwendler <pwendler@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2012-07-30 12:54:28 +0000

    Use Tempita engine for other python script as well.
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@6382 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- scripts/report-generator.py
+++ scripts/report-generator.py
@@ -31,46 +31,16 @@ import sys
 sys.dont_write_bytecode = True # prevent creation of .pyc files
 
 import os
-import re
 import time
-import shutil
 import optparse
 import subprocess
+import tempita
 try:
     import json
 except ImportError:
     #for python 2.5 and below, hope that simplejson is available
     import simplejson as json
 
-SUBSTITUTION_RE = re.compile('\{\{\{(\w+)\}\}\}')
-
-class Template(object):
-    """
-    a pretty limited template "engine" :)
-    """
-
-    def __init__(self, infile, outfile):
-        self.infile = infile
-        self.outfile = outfile
-
-    def render(self, **kws):
-        """
-        pass kwargs here. The value should be a callable that returns the string to insert for the key.
-
-        render(test=lambda : "This is a test!!") will substitute {{{test}}} for "This is a test!!"
-
-        WARNING: currently only one template var e.g. {{{test}}} may appear on a single line in the template.
-
-        """
-        for line in self.infile:
-            match = SUBSTITUTION_RE.search(line)
-            if match and match.group(1) in kws:
-                subst = kws[match.group(1)]()
-                self.outfile.write(line[:match.start()])
-                self.outfile.write(subst)
-                self.outfile.write(line[match.end():])
-            else:
-                self.outfile.write(line)
 
 def call_dot(infile, outpath):
     (basefilename, ext) = os.path.splitext(os.path.basename(infile))
@@ -99,6 +69,17 @@ def call_dot(infile, outpath):
         sys.exit(1)
 
     return True
+
+
+def readfile(filepath, optional=False):
+    if not os.path.isfile(filepath):
+        if optional:
+            return None
+        raise Exception('File not found: ' + filepath)
+    print ('Reading: ' + filepath)
+    with open(filepath, 'r') as fp:
+        return fp.read()
+
 
 def main():
 
@@ -172,48 +153,8 @@ def main():
     logfile = options.logfile or os.path.join(cpaoutdir, 'CPALog.txt')
     statsfile = options.statsfile or os.path.join(cpaoutdir, 'Statistics.txt')
     conffile = options.conffile or os.path.join(cpacheckerdir, 'config', 'predicateAnalysis.properties')
-    cilfile = args[0]
+    sourcefile = args[0]
     time_generated = time.strftime("%a, %d %b %Y %H:%M", time.localtime())
-
-    def filecontents(filepath, encode=False, fallback=None):
-        def inner():
-            if not os.path.isfile(filepath):
-                if fallback:
-                    return fallback
-                if not encode:
-                    raise Exception('File not found: ' + filepath)
-                else:
-                    return 'Not found:' + filepath
-            print ('Reading: ' + filepath)
-            with open(filepath, 'r') as fp:
-                if encode:
-                    return fp.read().replace('<','&lt;').replace('>', '&gt;')
-                else:
-                    return fp.read()
-        return inner
-
-    def gen_functionlist():
-        funclist = [x[5:-4] for x in os.listdir(cpaoutdir) if x.startswith('cfa__') and x.endswith('.dot')]
-        print ('Generating SVGs for CFA')
-        for func in funclist:
-            call_dot(os.path.join(cpaoutdir, 'cfa__' + func + '.dot'), reportdir)
-        return json.dumps(funclist, indent=4)
-
-    def format_cil():
-        if not os.path.isfile(cilfile):
-            return '<h3>CIL file not found.</h3>'
-        else:
-            with open(cilfile, 'r') as fp:
-                buff = ['<table id="cil_holder">']
-                for no, line in enumerate(fp):
-                    buff.append('<tr id="cil_line_%d"><td><pre>%d</pre></td><td><pre class="prettyprint lang-c">%s</pre></td></tr>\n' % (
-                            no,
-                            no,
-                            line.replace('<','&lt;').replace('>', '&gt;').rstrip()
-                        )
-                    )
-                buff.append('</table>')
-                return ''.join(buff)
 
     #if there is an ARG.dot create an SVG in the report dir
     if os.path.isfile(argfilepath):
@@ -223,26 +164,32 @@ def main():
                 os.rename(os.path.join(reportdir, 'ErrorPath.svg'),
                           os.path.join(reportdir, 'ARG.svg'))
 
-    inf = open(tplfilepath, 'r')
-    outf = open(outfilepath, 'w')
+    functions = [x[5:-4] for x in os.listdir(cpaoutdir) if x.startswith('cfa__') and x.endswith('.dot')]
+    print ('Generating SVGs for CFA')
+    for func in functions:
+        call_dot(os.path.join(cpaoutdir, 'cfa__' + func + '.dot'), reportdir)
 
-    t = Template(inf, outf)
-    t.render(
-        logfile=filecontents(logfile, encode=True),
-        statistics=filecontents(statsfile, encode=True),
-        conffile=filecontents(conffile, encode=True),
-        errorpath=filecontents(errorpath, fallback='[]'),
-        functionlist=gen_functionlist,
-        combinednodes=filecontents(combinednodes),
-        cfainfo=filecontents(cfainfo),
-        fcalledges=filecontents(fcalledges),
-        time_generated=lambda: time_generated,
-        formatted_cil=format_cil,
-        sourcefile=lambda: cilfile[cilfile.rfind('/') + 1:], # get filename without path
-    )
+    template = tempita.HTMLTemplate.from_filename(tplfilepath, encoding='UTF-8')
 
-    inf.close()
-    outf.close()
+    # write file
+    with open(outfilepath, 'w') as outf:
+        outf.write(template.substitute(
+            time_generated   =time_generated,
+            sourcefilename   =os.path.basename(sourcefile),
+
+            sourcefilecontent=readfile(sourcefile, optional=True),
+            logfile          =readfile(logfile, optional=True),
+            statistics       =readfile(statsfile, optional=True),
+            conffile         =readfile(conffile, optional=True),
+
+            # JSON data for script
+            errorpath        =readfile(errorpath, optional=True) or '[]',
+            functionlist     =json.dumps(functions),
+            combinednodes    =readfile(combinednodes),
+            cfainfo          =readfile(cfainfo),
+            fcalledges       =readfile(fcalledges),
+        ))
+
     print ('Report generated in {0}'.format(outfilepath))
 
 if __name__ == '__main__':
