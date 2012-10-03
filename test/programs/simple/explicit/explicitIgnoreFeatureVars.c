46ed57fe2f243c08970ddd2418324596cd60dc21
pwendler <pwendler@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2012-10-03 17:15:18 +0000

    Fix the -explicitAnalysis-featureVars configuration and some unit tests so that they work as intended after the changes of r6649.
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@6651 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- test/programs/simple/explicit/explicitIgnoreFeatureVars.c
+++ test/programs/simple/explicit/explicitIgnoreFeatureVars.c
@@ -1,7 +1,7 @@
 /*
  * This is a test to determine if explicit analysis can be configured to ignore the feature variable.
  * tested by cpa.explicit.ExplicitTest
- * The error should not be found if the option 
+ * The error should only be found if the option
  * "cpa.explicit.variableBlacklist" is set to "__SELECTED_FEATURE_(\\w)*" .
  */
 void error()
@@ -12,7 +12,7 @@ int __SELECTED_FEATURE_base;
 
 int main()
 {
-	__SELECTED_FEATURE_base = 1;
+	__SELECTED_FEATURE_base = 0;
 	if (__SELECTED_FEATURE_base) {
 		error();
 	}
