968a9b7b476ef2ad9f0d2f339328543e0b458503
muller <muller@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2013-01-31 16:18:20 +0000

    cpalien: add a heap of comments and TODO items
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@7328 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/cpa/cpalien/CLangSMG.java
+++ src/org/sosy_lab/cpachecker/cpa/cpalien/CLangSMG.java
@@ -36,16 +36,62 @@ import org.sosy_lab.common.LogManager;
 import org.sosy_lab.cpachecker.cfa.ast.c.CFunctionDeclaration;
 import org.sosy_lab.cpachecker.cfa.ast.c.CIdExpression;
 
-
+/**
+ * Extending SMG with notions specific for programs in C language:
+ *  - separation of global, heap and stack objects
+ *  - null object and value
+ *
+ * TODO: Implement configurable consistency checking on various places:
+ *  - none
+ *  - on interesting places
+ *  - on every operation (=debug)
+ */
 public class CLangSMG extends SMG {
+  /**
+   * A container for object found on the stack:
+   *  - local variables
+   *  - parameters
+   *
+   * TODO: Perhaps it could be wrapped in a class?
+   */
   final private ArrayDeque<CLangStackFrame> stack_objects = new ArrayDeque<>();
+
+  /**
+   * A container for objects allocated on heap
+   */
   final private HashSet<SMGObject> heap_objects = new HashSet<>();
+
+  /**
+   * A container for global objects
+   *
+   * TODO: Full consistency check
+   *  - all three container classes are disjunct
+   *  - union of all containers is the generic object container in parent class
+   */
   final private HashMap<String, SMGObject> global_objects = new HashMap<>();
 
+  /**
+   * A special object representing NULL
+   */
   final private SMGObject nullObject = new SMGObject();
+  /**
+   * An adress of the special object representing null
+   */
   final private int nullAddress = 0;
+
+  /**
+   * A flag signifying the edge leading to this state caused memory to be leaked
+   */
   private boolean has_leaks = false;
 
+  /**
+   * Constructor
+   *
+   * Newly constructed CLangSMG contains a single nullObject with an address
+   * pointing to it, and is empty otherwise.
+   *
+   * TODO: Test
+   */
   public CLangSMG() {
     SMGEdgePointsTo nullPointer = new SMGEdgePointsTo(nullAddress, nullObject, 0);
 
@@ -54,6 +100,13 @@ public class CLangSMG extends SMG {
     addPointsToEdge(nullPointer);
   }
 
+  /**
+   * @param pHeap original CLangSMG
+   *
+   * Copy constructor.
+   *
+   * TODO: Test
+   */
   public CLangSMG(CLangSMG pHeap) {
     super(pHeap);
 
@@ -66,16 +119,72 @@ public class CLangSMG extends SMG {
     global_objects.putAll(pHeap.global_objects);
   }
 
+  /**
+   * @param pObject
+   *
+   * Adds an object to the heap.
+   *
+   * TODO: Test adding an object
+   * TODO: Test adding an object already present
+   * TODO: Test consistency after both cases above
+   *
+   * TODO: Consistency check: same object on heap and global
+   * TODO: Test for this consistency check
+   *
+   * TODO: Consistency check: same object on heap and stack
+   * TODO: Test for this consistency check
+   */
   public void addHeapObject(SMGObject pObject) {
     this.heap_objects.add(pObject);
     super.addObject(pObject);
   }
 
+  /**
+   * @param pObject
+   *
+   * Adds a global object
+   *
+   * TODO: Test adding an object
+   * TODO: Test adding an object twice
+   * TODO: Test consistency after both cases above
+   *
+   * TODO: Consistency check: same object on global and stack
+   * TODO: Test for this consistency check
+   *
+   * TODO: Consistency check: different objects with same label (invalid C)
+   * TODO: Test for this consistency check
+   */
   private void addGlobalObject(SMGObject pObject) {
     this.global_objects.put(pObject.getLabel(), pObject);
     super.addObject(pObject);
   }
 
+  /**
+   * @param pObject
+   * @throws IllegalAccessException
+   *
+   * Adds an object to the current stack frame
+   *
+   * TODO: Scope visibility vs. stack frame issues: handle cases where a variable is visible
+   * but is is allowed to override (inner blocks)
+   *
+   *
+   * TODO: Shall we need an extension for putting objects to upper frames?
+   * TODO: Test adding an object: successful
+   * TODO: Test adding an object twice to the same stack frame: exc
+   *
+   * TODO: Consistency check (deny): same object in different stack frames
+   * TODO: Test for this consistency check
+   *
+   * TODO: Consistency check (allow): different objects with same label inside a frame
+   * TODO: Test for this consistency check
+   *
+   * TODO: Consistency check (allow): different objects with same label across frames
+   * TODO: Test for this consistency check
+   *
+   * TODO: Consistency check (allow): different objects with same label as global object
+   * TODO: Test for this consistency check
+   */
   public void addStackObject(SMGObject pObject) throws IllegalAccessException {
     super.addObject(pObject);
     stack_objects.peek().addStackVariable(pObject.getLabel(), pObject);
@@ -87,6 +196,21 @@ public class CLangSMG extends SMG {
         + global_objects + "\n " + this.valuesToString() + "\n " + this.ptToString() + "\n " + this.hvToString() + "\n]";
   }
 
+  /**
+   * @param pVariableName
+   * @return
+   *
+   * Returns an SMGObject tied to the variable name. The name must be visible in
+   * the current scope: it needs to be visible either in the current frame, or it
+   * is a global variable.
+   *
+   * TODO: Test for getting visible local object
+   * TODO: Test for getting visible local object hiding other local object
+   * TODO: Test for getting visible local object hiding global object
+   * TODO: Test for getting visible local object with invisible object on stack
+   * TODO: Test for getting visible global object
+   * TODO: Test for getting visible global object with invisible object on stack
+   */
   public SMGObject getObjectForVisibleVariable(CIdExpression pVariableName) {
     // Look in the local frame
     if (stack_objects.peek().containsVariable(pVariableName.getName())){
@@ -98,11 +222,27 @@ public class CLangSMG extends SMG {
     return null;
   }
 
+  /**
+   * @param pFunctionDeclaration
+   *
+   * Add a new stack frame for the passed function
+   *
+   * TODO: Test for stack frame addition
+   *  - hides current objects
+   *  - does not hide global objects
+   */
   public void addStackFrame(CFunctionDeclaration pFunctionDeclaration) {
     CLangStackFrame newFrame = new CLangStackFrame(pFunctionDeclaration);
     stack_objects.push(newFrame);
   }
 
+  /**
+   * @return
+   *
+   * Returns the (modifiable) stack of frames containing objects.
+   *
+   * TODO: Test
+   */
   public ArrayDeque<CLangStackFrame> getStackFrames() {
     //TODO: This still allows modification, as queues do not have
     // the appropriate unmodifiable method. There is probably some good
@@ -110,14 +250,36 @@ public class CLangSMG extends SMG {
     return stack_objects;
   }
 
+  /**
+   * @return
+   *
+   * Returns an unmodifiable set of objects on the heap.
+   *
+   * TODO: Test
+   */
   public Set<SMGObject> getHeapObjects() {
     return Collections.unmodifiableSet(heap_objects);
   }
 
+  /**
+   * @return
+   *
+   * Returns an unmodifiable map from variable names to global objects
+   *
+   * TODO: Test
+   */
   public Map<String, SMGObject> getGlobalObjects() {
     return Collections.unmodifiableMap(global_objects);
   }
 
+  /**
+   * @return
+   *
+   * Returns true if the SMG is a successor over the edge causing some memory
+   * to be leaked. Returns false otherwise.
+   *
+   * TODO: Test
+   */
   public boolean hasMemoryLeaks() {
     // TODO: There needs to be a proper graph algorithm in the future
     //       Right now, we can discover memory leaks only after unassigned
@@ -125,6 +287,12 @@ public class CLangSMG extends SMG {
     return has_leaks;
   }
 
+  /**
+   * Sets a flag indicating this SMG is a successor over the edge causing a
+   * memory leak.
+   *
+   * TODO: Test
+   */
   public void setMemoryLeak() {
     has_leaks = true;
   }
