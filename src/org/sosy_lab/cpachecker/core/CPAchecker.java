64b798855f599bae8e7b3baec4430608b77d11d3
pwendler <pwendler@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2013-02-07 08:46:04 +0000

    Improve error message when parsing fails.
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@7414 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/core/CPAchecker.java
+++ src/org/sosy_lab/cpachecker/core/CPAchecker.java
@@ -39,6 +39,7 @@ import org.sosy_lab.common.configuration.Option;
 import org.sosy_lab.common.configuration.Options;
 import org.sosy_lab.cpachecker.cfa.CFA;
 import org.sosy_lab.cpachecker.cfa.CFACreator;
+import org.sosy_lab.cpachecker.cfa.Language;
 import org.sosy_lab.cpachecker.cfa.model.FunctionEntryNode;
 import org.sosy_lab.cpachecker.core.CPAcheckerResult.Result;
 import org.sosy_lab.cpachecker.core.algorithm.Algorithm;
@@ -49,8 +50,6 @@ import org.sosy_lab.cpachecker.core.interfaces.ConfigurableProgramAnalysis;
 import org.sosy_lab.cpachecker.core.interfaces.Precision;
 import org.sosy_lab.cpachecker.core.reachedset.ReachedSet;
 import org.sosy_lab.cpachecker.exceptions.CPAException;
-import org.sosy_lab.cpachecker.exceptions.CParserException;
-import org.sosy_lab.cpachecker.exceptions.JParserException;
 import org.sosy_lab.cpachecker.exceptions.ParserException;
 import org.sosy_lab.cpachecker.util.globalinfo.GlobalInfo;
 
@@ -205,27 +204,16 @@ public class CPAchecker {
     } catch (IOException e) {
       logger.logUserException(Level.SEVERE, e, "Could not read file");
 
-    } catch (CParserException e) {
-      // only log message, not whole exception because this is a C problem,
-      // not a CPAchecker problem
+    } catch (ParserException e) {
       logger.logUserException(Level.SEVERE, e, "Parsing failed");
-      logger.log(Level.INFO, "Make sure that the code was preprocessed using Cil (HowTo.txt).\n"
-          + "If the error still occurs, please send this error message together with the input file to cpachecker-users@sosy-lab.org.");
+      StringBuilder msg = new StringBuilder();
+      msg.append("Please make sure that the code can be compiled by a compiler.\n");
+      if (e.getLanguage() == Language.C) {
+        msg.append("If the code was not preprocessed, please use a C preprocessor\nor specify the -preprocess command-line argument.\n");
+      }
+      msg.append("If the error still occurs, please send this error message\ntogether with the input file to cpachecker-users@sosy-lab.org.\n");
+      logger.log(Level.INFO, msg);
 
-    } catch (JParserException e) {
-        // only log message, not whole exception because this is a Java problem,
-        // not a CPAchecker problem
-        logger.logUserException(Level.SEVERE, e, "Parsing failed");
-        logger.log(Level.INFO, "Make sure that the Java code could be compiled by a Compiler," +
-        		"and the Java Version is supported.\n"
-            + "The Java Parser of cpaChecker does not yet support the complete Java language.\n"
-            + "If the error still occurs, please send this error message together with the input file to cpachecker-users@sosy-lab.org.");
-   } catch (ParserException e) {
-          // only log message, not whole exception because this is a C problem,
-          // not a CPAchecker problem
-          logger.logUserException(Level.SEVERE, e, "Parsing failed");
-          logger.log(Level.INFO, "Make sure that the code was preprocessed using Cil (HowTo.txt).\n"
-              + "If the error still occurs, please send this error message together with the input file to cpachecker-users@sosy-lab.org.");
     } catch (InvalidConfigurationException e) {
       logger.logUserException(Level.SEVERE, e, "Invalid configuration");
 
