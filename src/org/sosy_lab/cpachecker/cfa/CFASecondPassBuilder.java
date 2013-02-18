ae24a448e4e7858e272e01af6eb81d95d7af81a1
mutilin <mutilin@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2013-02-18 10:50:27 +0000

    fix iteration on leaving edges
    if edges are changing then we should store them in immutable list
    
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@7518 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/cfa/CFASecondPassBuilder.java
+++ src/org/sosy_lab/cpachecker/cfa/CFASecondPassBuilder.java
@@ -112,7 +112,7 @@ public class CFASecondPassBuilder {
         continue;
       }
 
-      for (CFAEdge edge : leavingEdges(node)) {
+      for (CFAEdge edge : leavingEdges(node).toImmutableList()) {
         if (edge instanceof AStatementEdge) {
           AStatementEdge statement = (AStatementEdge)edge;
           IAStatement expr = statement.getStatement();
