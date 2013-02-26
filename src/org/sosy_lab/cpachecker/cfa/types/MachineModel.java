a3167e1b9f7ea151db5b23b62488083462bf5502
driemey <driemey@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2013-02-26 15:07:50 +0000

    Machine Model allows now to get Sizes of ArrayTypes who's length is specified without variables.
    For the size of Array Types which can't be calculated, the size of a pointer (4) is given.
    
    (There is a possibility to implement another getSize method in the future which takes an abstract State, e. g. explicitState and evaluates Pointer Types for all Arrays with a known size)
    
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@7562 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/cfa/types/MachineModel.java
+++ src/org/sosy_lab/cpachecker/cfa/types/MachineModel.java
@@ -23,6 +23,16 @@
  */
 package org.sosy_lab.cpachecker.cfa.types;
 
+import org.sosy_lab.cpachecker.cfa.ast.c.CBinaryExpression;
+import org.sosy_lab.cpachecker.cfa.ast.c.CBinaryExpression.BinaryOperator;
+import org.sosy_lab.cpachecker.cfa.ast.c.CCastExpression;
+import org.sosy_lab.cpachecker.cfa.ast.c.CExpression;
+import org.sosy_lab.cpachecker.cfa.ast.c.CIntegerLiteralExpression;
+import org.sosy_lab.cpachecker.cfa.ast.c.CTypeIdExpression;
+import org.sosy_lab.cpachecker.cfa.ast.c.CTypeIdExpression.TypeIdOperator;
+import org.sosy_lab.cpachecker.cfa.ast.c.CUnaryExpression;
+import org.sosy_lab.cpachecker.cfa.ast.c.CUnaryExpression.UnaryOperator;
+import org.sosy_lab.cpachecker.cfa.ast.c.DefaultCExpressionVisitor;
 import org.sosy_lab.cpachecker.cfa.types.c.CArrayType;
 import org.sosy_lab.cpachecker.cfa.types.c.CCompositeType;
 import org.sosy_lab.cpachecker.cfa.types.c.CCompositeType.CCompositeTypeMemberDeclaration;
@@ -218,7 +228,26 @@ public enum MachineModel {
     @Override
     public Integer visit(CArrayType pArrayType) throws IllegalArgumentException {
       // TODO: This has to be checked (Example: Char pathbuf[1 + 1];)
-      // We do not support arrays, so treat them as pointer.
+
+      CExpression arrayLength = pArrayType.getLength();
+
+      Integer length = null;
+
+      if (arrayLength != null) {
+        length = arrayLength.accept(new ArrayLengthVisitor());
+      }
+
+      if (length != null) {
+
+        Integer sizeOfType = model.getSizeof(pArrayType.getType());
+
+        if (sizeOfType != null) {
+          return length * sizeOfType;
+        }
+      }
+
+
+      // We do not support arrays with variable length, so treat them as pointer.
       return model.getSizeofPtr();
       //return getSizeofInt();
     }
@@ -305,6 +334,173 @@ public enum MachineModel {
     @Override
     public Integer visit(CTypedefType pTypedefType) throws IllegalArgumentException {
       return pTypedefType.getRealType().accept(this);
+    }
+
+    private class ArrayLengthVisitor extends DefaultCExpressionVisitor<Integer, IllegalArgumentException>
+    {
+
+      @Override
+      protected Integer visitDefault(CExpression pExp) {
+        return null;
+      }
+
+      @Override
+      public Integer visit(CIntegerLiteralExpression exp) throws IllegalArgumentException {
+        return (int) exp.asLong();
+      }
+
+      @Override
+      public Integer visit(CBinaryExpression pE) throws IllegalArgumentException {
+        BinaryOperator binaryOperator = pE.getOperator();
+        CExpression lVarInBinaryExp = pE.getOperand1();
+        CExpression rVarInBinaryExp = pE.getOperand2();
+
+        switch (binaryOperator) {
+        case PLUS:
+        case MINUS:
+        case DIVIDE:
+        case MULTIPLY:
+        case SHIFT_LEFT:
+        case BINARY_AND:
+        case BINARY_OR:
+        case BINARY_XOR: {
+          Integer lVal = lVarInBinaryExp.accept(this);
+          if (lVal == null) { return null; }
+
+          Integer rVal = rVarInBinaryExp.accept(this);
+          if (rVal == null) { return null; }
+
+          switch (binaryOperator) {
+          case PLUS:
+            return lVal + rVal;
+
+          case MINUS:
+            return lVal - rVal;
+
+          case DIVIDE:
+            // TODO maybe we should signal a division by zero error?
+            if (rVal == 0) { return null; }
+
+            return lVal / rVal;
+
+          case MULTIPLY:
+            return lVal * rVal;
+
+          case SHIFT_LEFT:
+            return lVal << rVal;
+
+          case BINARY_AND:
+            return lVal & rVal;
+
+          case BINARY_OR:
+            return lVal | rVal;
+
+          case BINARY_XOR:
+            return lVal ^ rVal;
+
+          default:
+            throw new AssertionError();
+          }
+        }
+
+        case EQUALS:
+        case NOT_EQUALS:
+        case GREATER_THAN:
+        case GREATER_EQUAL:
+        case LESS_THAN:
+        case LESS_EQUAL: {
+
+          Integer lVal = lVarInBinaryExp.accept(this);
+          if (lVal == null) { return null; }
+
+          Integer rVal = rVarInBinaryExp.accept(this);
+          if (rVal == null) { return null; }
+
+          boolean result;
+          switch (binaryOperator) {
+          case EQUALS:
+            result = (lVal.equals(rVal));
+            break;
+          case NOT_EQUALS:
+            result = !(lVal.equals(rVal));
+            break;
+          case GREATER_THAN:
+            result = (lVal > rVal);
+            break;
+          case GREATER_EQUAL:
+            result = (lVal >= rVal);
+            break;
+          case LESS_THAN:
+            result = (lVal < rVal);
+            break;
+          case LESS_EQUAL:
+            result = (lVal <= rVal);
+            break;
+
+          default:
+            throw new AssertionError();
+          }
+
+          // return 1 if expression holds, 0 otherwise
+          return result ? 1 : 0;
+        }
+
+        case MODULO:
+        case SHIFT_RIGHT:
+        default:
+          // TODO check which cases can be handled
+          return null;
+        }
+      }
+
+      @Override
+      public Integer visit(CUnaryExpression unaryExpression) throws IllegalArgumentException {
+        UnaryOperator unaryOperator = unaryExpression.getOperator();
+        CExpression unaryOperand = unaryExpression.getOperand();
+
+        Integer value = null;
+
+        switch (unaryOperator) {
+        case MINUS:
+          value = unaryOperand.accept(this);
+          return (value != null) ? -value : null;
+
+        case NOT:
+          value = unaryOperand.accept(this);
+
+          if (value == null) {
+            return null;
+          } else {
+            return value == 0 ? 1 : 0;
+          }
+
+        case SIZEOF:
+          return model.getSizeof(unaryOperand.getExpressionType());
+        case TILDE:
+        default:
+          return null;
+        }
+      }
+
+      @Override
+      public Integer visit(CTypeIdExpression typeIdExp) throws IllegalArgumentException {
+
+        TypeIdOperator typeOperator = typeIdExp.getOperator();
+        CType type = typeIdExp.getType();
+
+        switch (typeOperator) {
+        case SIZEOF:
+          return model.getSizeof(type);
+        default:
+          return null;
+          //TODO Investigate the other Operators.
+        }
+      }
+
+      @Override
+      public Integer visit(CCastExpression pE) throws IllegalArgumentException {
+        return pE.getOperand().accept(this);
+      }
     }
   }
 
