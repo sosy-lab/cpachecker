b65c1a1ebf73ac29b21bdba88df346cd7f62cc59
loewe <loewe@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2013-02-27 23:49:55 +0000

    updated eVolveCheck adapter
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@7573 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- scripts/benchmark/tools/evolcheck.py
+++ scripts/benchmark/tools/evolcheck.py
@@ -11,69 +11,39 @@ import benchmark.tools.template
 
 class Tool(benchmark.tools.template.BaseTool):
     
-    lastSourcefile = None
-    gotosourcefileDict = {}
-
     def getExecutable(self):
-        return Util.findExecutable('evolcheck')
+        return Util.findExecutable('lib/native/x86_64-linux/evolcheck')
 
 
     def getVersion(self, executable):
         return subprocess.Popen([executable, '--version'],
                                 stdout=subprocess.PIPE).communicate()[0].strip()
 
-
     def getName(self):
         return 'eVolCheck'
 
-    def filehash(self, filepath):
-        sha1 = hashlib.sha1()
-        f = open(filepath, 'rb')
-        try:
-            sha1.update(f.read())
-        finally:
-            f.close()
-        return sha1.hexdigest()
-
-    
     def preprocessSourcefile(self, sourcefile):
-        if not sourcefile in Tool.gotosourcefileDict:
-            sourcefileHash = self.filehash(sourcefile)
-            gotosourcefile = '/tmp/{}.goto'.format(sourcefileHash)
-        
-            if not os.path.isfile(gotosourcefile):
-                gotoccExecutable = Util.findExecutable('make-goto.sh')
-                subprocess.check_call([gotoccExecutable, sourcefile, gotosourcefile])
-        
-            Tool.gotosourcefileDict[sourcefile] = gotosourcefile
+        gotoCcExecutable  = Util.findExecutable('lib/native/x86_64-linux/goto-cc')
+        gotoCcFilename    = sourcefile + ".cc"
+
+        subprocess.Popen([gotoCcExecutable,
+                            sourcefile,
+                            '-o',
+                            gotoCcFilename],
+                          stdout=subprocess.PIPE).wait()
             
-        return Tool.gotosourcefileDict[sourcefile]
+        return gotoCcFilename
             
 
     def getCmdline(self, executable, options, sourcefile):
-        dirNameSourcefile = os.path.dirname(sourcefile)
-        if not Tool.lastSourcefile is None:
-            dirNameLastSourcefile = os.path.dirname(Tool.lastSourcefile)
-        else:
-            dirNameLastSourcefile = None
+        sourcefile = self.preprocessSourcefile(sourcefile)
 
-        gotosourcefile = self.preprocessSourcefile(sourcefile)
-
-        if dirNameSourcefile != dirNameLastSourcefile:
-            Tool.lastSourcefile = None
-
-        if Tool.lastSourcefile is None:
-            args = options + ["--init-upgrade-check", gotosourcefile]
+        if ("--init-upgrade-check" in options):
             subprocess.check_call(["rm -f", "__summaries", "__omega"], shell=True)
+            return [executable] + options + [sourcefile]
+
         else:
-            lastGotosourcefile = self.preprocessSourcefile(Tool.lastSourcefile)
-            args = options + ["--do-upgrade-check", gotosourcefile, lastGotosourcefile]
-
-        self.options = args
-
-        Tool.lastSourcefile = sourcefile
-        return [executable] + args
-
+            return [executable] + ['--do-upgrade-check'] + [sourcefile] + options
 
     def getStatus(self, returncode, returnsignal, output, isTimeout):
         status = None
