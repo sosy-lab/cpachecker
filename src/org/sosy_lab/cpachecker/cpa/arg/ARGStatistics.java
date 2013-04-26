5b73e800d22ea9d3c3df0c423a62f767da62405f
pwendler <pwendler@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2013-04-26 07:04:13 +0000

    Write error path as an automaton to a file.
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@7950 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/cpa/arg/ARGStatistics.java
+++ src/org/sosy_lab/cpachecker/cpa/arg/ARGStatistics.java
@@ -118,6 +118,11 @@ public class ARGStatistics implements Statistics {
   @FileOption(FileOption.Type.OUTPUT_FILE)
   private Path errorPathGraphFile = Paths.get("ErrorPath.dot");
 
+  @Option(name="errorPath.automaton",
+      description="export error path to file as an automaton")
+  @FileOption(FileOption.Type.OUTPUT_FILE)
+  private Path errorPathAutomatonFile = Paths.get("ErrorPath.spc");
+
   private final ARGCPA cpa;
 
   private Writer refinementGraphUnderlyingWriter = null;
@@ -249,6 +254,13 @@ public class ARGStatistics implements Statistics {
               ARGUtils.CHILDREN_OF_STATE,
               Predicates.in(pathElements),
               isTargetPathEdge);
+        }
+      });
+
+      writeErrorPathFile(errorPathAutomatonFile, new Appender() {
+        @Override
+        public void appendTo(Appendable pAppendable) throws IOException {
+          ARGUtils.producePathAutomaton(pAppendable, rootState, pathElements);
         }
       });
 
