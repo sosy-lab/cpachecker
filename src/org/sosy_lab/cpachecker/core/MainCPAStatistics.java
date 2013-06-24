74b00ff3f2edd6809d67d085d53a6d28fb444a6f
pwendler <pwendler@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2013-06-24 14:02:20 +0000

    Refactor writing of ReachedSet statistics.
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@8121 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/core/MainCPAStatistics.java
+++ src/org/sosy_lab/cpachecker/core/MainCPAStatistics.java
@@ -260,64 +260,61 @@ class MainCPAStatistics implements Statistics {
 
       out.println("Size of reached set:             " + reachedSize);
 
-      Set<CFANode> locations;
+      if (!reached.isEmpty()) {
 
-      if (reached instanceof LocationMappedReachedSet) {
-        LocationMappedReachedSet l = (LocationMappedReachedSet)reached;
-        locations = l.getLocations();
-        int locs = l.getNumberOfPartitions();
-        if (locs > 0) {
-          out.println("  Number of locations:           " + locs);
-          out.println("    Avg states per location:     " + reachedSize / locs);
+        Set<CFANode> locations;
+        CFANode mostFrequentLocation = null;
+        int mostFrequentLocationCount = 0;
+
+        if (reached instanceof LocationMappedReachedSet) {
+          LocationMappedReachedSet l = (LocationMappedReachedSet)reached;
+          locations = l.getLocations();
+
           Map.Entry<Object, Collection<AbstractState>> maxPartition = l.getMaxPartition();
-          out.println("    Max states per location:     " + maxPartition.getValue().size() + " (at node " + maxPartition.getKey() + ")");
-        }
+          mostFrequentLocation = (CFANode)maxPartition.getKey();
+          mostFrequentLocationCount = maxPartition.getValue().size();
 
-      } else {
-        HashMultiset<CFANode> allLocations = HashMultiset.create(from(reached)
-                                                                      .transform(EXTRACT_LOCATION)
-                                                                      .filter(notNull()));
+        } else {
+          HashMultiset<CFANode> allLocations = HashMultiset.create(from(reached)
+                                                                        .transform(EXTRACT_LOCATION)
+                                                                        .filter(notNull()));
 
-        locations = allLocations.elementSet();
-        int locs = allLocations.entrySet().size();
-        if (locs > 0) {
-          out.println("  Number of reached locations:   " + locs + " (" + StatisticsUtils.toPercent(locs, cfa.getAllNodes().size()) + ")");
-          out.println("    Avg states per location:     " + reachedSize / locs);
+          locations = allLocations.elementSet();
 
-          int max = 0;
-          CFANode maxLoc = null;
           for (Multiset.Entry<CFANode> location : allLocations.entrySet()) {
             int size = location.getCount();
-            if (size > max) {
-              max = size;
-              maxLoc = location.getElement();
+            if (size > mostFrequentLocationCount) {
+              mostFrequentLocationCount = size;
+              mostFrequentLocation = location.getElement();
             }
           }
-          out.println("    Max states per location:     " + max + " (at node " + maxLoc + ")");
         }
-      }
 
-      if (!locations.isEmpty()) {
+        int locs = locations.size();
+        out.println("  Number of reached locations:   " + locs + " (" + StatisticsUtils.toPercent(locs, cfa.getAllNodes().size()) + ")");
+        out.println("    Avg states per location:     " + reachedSize / locs);
+        out.println("    Max states per location:     " + mostFrequentLocationCount + " (at node " + mostFrequentLocation + ")");
+
         Set<String> functions = from(locations).transform(CFAUtils.GET_FUNCTION).toSet();
         out.println("  Number of reached functions:   " + functions.size() + " (" + StatisticsUtils.toPercent(functions.size(), cfa.getNumberOfFunctions()) + ")");
-      }
 
-      if (reached instanceof PartitionedReachedSet) {
-        PartitionedReachedSet p = (PartitionedReachedSet)reached;
-        int partitions = p.getNumberOfPartitions();
-        out.println("  Number of partitions:          " + partitions);
-        out.println("    Avg size of partitions:      " + reachedSize / partitions);
-        Map.Entry<Object, Collection<AbstractState>> maxPartition = p.getMaxPartition();
-        out.print  ("    Max size of partitions:      " + maxPartition.getValue().size());
-        if (maxPartition.getValue().size() > 1) {
-          out.println(" (with key " + maxPartition.getKey() + ")");
-        } else {
-          out.println();
+        if (reached instanceof PartitionedReachedSet) {
+          PartitionedReachedSet p = (PartitionedReachedSet)reached;
+          int partitions = p.getNumberOfPartitions();
+          out.println("  Number of partitions:          " + partitions);
+          out.println("    Avg size of partitions:      " + reachedSize / partitions);
+          Map.Entry<Object, Collection<AbstractState>> maxPartition = p.getMaxPartition();
+          out.print  ("    Max size of partitions:      " + maxPartition.getValue().size());
+          if (maxPartition.getValue().size() > 1) {
+            out.println(" (with key " + maxPartition.getKey() + ")");
+          } else {
+            out.println();
+          }
         }
-      }
-      out.println("  Number of target states:       " + from(reached).filter(IS_TARGET_STATE).size());
-      if (reached.hasWaitingState()) {
-        out.println("  Size of final wait list        " + reached.getWaitlistSize());
+        out.println("  Number of target states:       " + from(reached).filter(IS_TARGET_STATE).size());
+        if (reached.hasWaitingState()) {
+          out.println("  Size of final wait list        " + reached.getWaitlistSize());
+        }
       }
     }
 
