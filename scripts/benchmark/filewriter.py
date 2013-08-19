04c3dbf211083ad296f5bee1da32dd80e5176163
friedber <friedber@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2013-08-19 11:39:42 +0000

    refactoring benchmark-script: - splitting into several files for OutputHandler, BenchmarkDataStructures. - minor changes.
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@8421 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- scripts/benchmark/filewriter.py
+++ scripts/benchmark/filewriter.py
@@ -1,11 +1,39 @@
+"""
+CPAchecker is a tool for configurable software verification.
+This file is part of CPAchecker.
+
+Copyright (C) 2007-2013  Dirk Beyer
+All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+
+CPAchecker web page:
+  http://cpachecker.sosy-lab.org
+"""
+
 import os
 
-def writeFile(name, content):
+def writeFile(content, *path):
     """
     Simply write some content to a file, overriding the file if necessary.
     """
-    with open(name, "w") as file:
-        file.write(content)
+    with open(os.path.join(*path), "w") as file:
+        return file.write(content)
+
+def readFile(*path):
+    with open(os.path.join(*path)) as f:
+        return f.read().strip()
 
 class FileWriter:
     """
@@ -23,7 +51,7 @@ class FileWriter:
         self.__content = content
 
         # Open file with "w" at least once so it will be overwritten.
-        writeFile(self.__filename, content)
+        writeFile(content, self.__filename)
 
     def append(self, newContent, keep=True):
         """
@@ -42,7 +70,7 @@ class FileWriter:
             """
             tmpFilename = self.__filename + ".tmp"
 
-            writeFile(tmpFilename, content)
+            writeFile(content, tmpFilename)
 
             os.rename(tmpFilename, self.__filename)
         else:
