cf344a51ea8f65e624255c415f02b4aefd51fdc2
mandrykin <mandrykin@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2013-10-11 13:15:17 +0000

    Preliminary CFA transformations for the new analysis: finished initial implementation
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/predicateAnalysisWithUF@8894 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/cfa/transformers/for_uif_analysis/CFATransformer.java
+++ src/org/sosy_lab/cpachecker/cfa/transformers/for_uif_analysis/CFATransformer.java
@@ -23,36 +23,44 @@
  */
 package org.sosy_lab.cpachecker.cfa.transformers.for_uif_analysis;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import org.sosy_lab.cpachecker.cfa.ast.c.CDeclaration;
 import org.sosy_lab.cpachecker.cfa.ast.c.CExpression;
+import org.sosy_lab.cpachecker.cfa.ast.c.CFunctionCall;
+import org.sosy_lab.cpachecker.cfa.ast.c.CFunctionCallAssignmentStatement;
+import org.sosy_lab.cpachecker.cfa.ast.c.CFunctionCallExpression;
+import org.sosy_lab.cpachecker.cfa.ast.c.CFunctionCallStatement;
 import org.sosy_lab.cpachecker.cfa.ast.c.CInitializer;
+import org.sosy_lab.cpachecker.cfa.ast.c.CReturnStatement;
 import org.sosy_lab.cpachecker.cfa.ast.c.CStatement;
 import org.sosy_lab.cpachecker.cfa.ast.c.CStatementVisitor;
 import org.sosy_lab.cpachecker.cfa.ast.c.CVariableDeclaration;
 import org.sosy_lab.cpachecker.cfa.model.CFAEdge;
 import org.sosy_lab.cpachecker.cfa.model.CFANode;
+import org.sosy_lab.cpachecker.cfa.model.MultiEdge;
 import org.sosy_lab.cpachecker.cfa.model.c.CAssumeEdge;
 import org.sosy_lab.cpachecker.cfa.model.c.CDeclarationEdge;
 import org.sosy_lab.cpachecker.cfa.model.c.CFunctionCallEdge;
+import org.sosy_lab.cpachecker.cfa.model.c.CReturnStatementEdge;
+import org.sosy_lab.cpachecker.cfa.model.c.CStatementEdge;
 import org.sosy_lab.cpachecker.exceptions.UnrecognizedCCodeException;
 import org.sosy_lab.cpachecker.util.CFATraversal.DefaultCFAVisitor;
 import org.sosy_lab.cpachecker.util.CFATraversal.TraversalProcess;
 
 public class CFATransformer extends DefaultCFAVisitor {
 
-  @Override
-  public TraversalProcess visitEdge(final CFAEdge oldEdge) {
-    final CFANode predecessor = oldEdge.getPredecessor();
-    final CFANode successor = oldEdge.getSuccessor();
+  private CFAEdge transformEdge(CFAEdge oldEdge) {
 
     CFAEdge edge = null;
+
     expressionVisitor.setCurrentEdge(oldEdge);
     try {
       switch (oldEdge.getEdgeType()) {
-      case AssumeEdge:
+      case AssumeEdge: {
         final CAssumeEdge assumeEdge = (CAssumeEdge) oldEdge;
         final CExpression oldExpression = assumeEdge.getExpression();
         final CExpression expression = (CExpression) oldExpression.accept(expressionVisitor);
@@ -65,9 +73,11 @@ public class CFATransformer extends DefaultCFAVisitor {
                                  assumeEdge.getTruthAssumption());
         }
         break;
-      case BlankEdge:
+      }
+      case BlankEdge: {
         break;
-      case DeclarationEdge:
+      }
+      case DeclarationEdge: {
         final CDeclarationEdge declarationEdge = (CDeclarationEdge) oldEdge;
         final CDeclaration declaration = declarationEdge.getDeclaration();
         if (declaration instanceof CVariableDeclaration) {
@@ -92,13 +102,133 @@ public class CFATransformer extends DefaultCFAVisitor {
           }
         }
         break;
-      case FunctionCallEdge:
+      }
+      case FunctionCallEdge: {
         final CFunctionCallEdge functionCallEdge = (CFunctionCallEdge) oldEdge;
-
+        List<CExpression> arguments = null;
+        int i = 0;
+        for (CExpression oldArgument : functionCallEdge.getArguments()) {
+          final CExpression argument = (CExpression) oldArgument.accept(expressionVisitor);
+          if (argument != oldArgument && arguments == null) {
+            arguments = new ArrayList<>();
+            arguments.addAll(functionCallEdge.getArguments().subList(0, i));
+          }
+          if (arguments != null) {
+            arguments.add(argument);
+          }
+          ++i;
+        }
+        if (arguments != null) {
+          final CFunctionCall oldFunctionCall = functionCallEdge.getSummaryEdge().getExpression();
+          final CFunctionCallExpression oldFunctionCallExpression = oldFunctionCall.getFunctionCallExpression();
+          final CFunctionCallExpression functionCallExpression = new CFunctionCallExpression(
+                                                                       oldFunctionCallExpression.getFileLocation(),
+                                                                       oldFunctionCallExpression.getExpressionType(),
+                                                                       oldFunctionCallExpression
+                                                                         .getFunctionNameExpression(),
+                                                                       arguments,
+                                                                       oldFunctionCallExpression.getDeclaration());
+          final CFunctionCall functionCall;
+          if (oldFunctionCall instanceof CFunctionCallStatement) {
+            functionCall = new CFunctionCallStatement(((CFunctionCallStatement) oldFunctionCall).getFileLocation(),
+                                                      functionCallExpression);
+          } else /*oldFunctionCall instanceof CFunctionCallAssignmentStatement*/ {
+            functionCall = new CFunctionCallAssignmentStatement(((CFunctionCallAssignmentStatement) oldFunctionCall)
+                                                                 .getFileLocation(),
+                                                                 ((CFunctionCallAssignmentStatement) oldFunctionCall)
+                                                                 .getLeftHandSide(),
+                                                                  functionCallExpression);
+          }
+          edge = new CFunctionCallEdge(functionCallEdge.getRawStatement(),
+                                       functionCallEdge.getLineNumber(),
+                                       functionCallEdge.getPredecessor(),
+                                       functionCallEdge.getSuccessor(),
+                                       functionCall,
+                                       functionCallEdge.getSummaryEdge());
+        }
         break;
+      }
+      case FunctionReturnEdge: {
+        break;
+      }
+      case ReturnStatementEdge: {
+        final CReturnStatementEdge returnStatementEdge = (CReturnStatementEdge) oldEdge;
+        if (returnStatementEdge.getRawAST().isPresent()) {
+          final CExpression oldExpression = returnStatementEdge.getExpression();
+          final CExpression expression = (CExpression) oldExpression.accept(expressionVisitor);
+          if (expression != oldExpression) {
+            edge = new CReturnStatementEdge(returnStatementEdge.getRawStatement(),
+                                            new CReturnStatement(returnStatementEdge.getRawAST()
+                                                                                    .get()
+                                                                                    .getFileLocation(),
+                                                                 expression),
+                                            returnStatementEdge.getLineNumber(),
+                                            returnStatementEdge.getPredecessor(),
+                                            returnStatementEdge.getSuccessor());
+          }
+        }
+        break;
+      }
+      case StatementEdge: {
+        final CStatementEdge statementEdge = (CStatementEdge) oldEdge;
+        final CStatement oldStatement = statementEdge.getStatement();
+        final CStatement statement = oldStatement.accept(statementVisitor);
+        if (statement != oldStatement) {
+          edge = new CStatementEdge(statementEdge.getRawStatement(),
+                                    statement,
+                                    statementEdge.getLineNumber(),
+                                    statementEdge.getPredecessor(),
+                                    statementEdge.getSuccessor());
+        }
+      }
       }
     } catch (UnrecognizedCCodeException e) {
       logger.log(Level.WARNING, "UnrecognizedCCodeException while pre-processing edge", new Object[]{edge, e});
+    }
+
+    if (edge != null) {
+      return edge;
+    } else {
+      return oldEdge;
+    }
+  }
+
+  @Override
+  public TraversalProcess visitEdge(final CFAEdge oldEdge) {
+    final CFANode predecessor = oldEdge.getPredecessor();
+    final CFANode successor = oldEdge.getSuccessor();
+
+    CFAEdge edge = null;
+    if (oldEdge instanceof MultiEdge) {
+      List<CFAEdge> edges = null;
+      int i = 0;
+      for (CFAEdge oldSubEdge : ((MultiEdge) oldEdge).getEdges()) {
+        final CFAEdge subEdge = transformEdge(oldSubEdge);
+        if (edges == null && subEdge != oldSubEdge) {
+          edges = new ArrayList<>();
+          edges.addAll(((MultiEdge) oldEdge).getEdges().subList(0, i));
+        }
+        if (edges != null) {
+          edges.add(subEdge);
+        }
+        ++i;
+      }
+      if (edges != null) {
+        edge = new MultiEdge(predecessor, successor, edges);
+      }
+    } else {
+      final CFAEdge newEdge = transformEdge(oldEdge);
+      if (newEdge != oldEdge) {
+        edge = newEdge;
+      }
+    }
+
+    if (edge != null) {
+      predecessor.removeLeavingEdge(oldEdge);
+      successor.removeEnteringEdge(oldEdge);
+
+      predecessor.addLeavingEdge(edge);
+      successor.addEnteringEdge(edge);
     }
 
     return TraversalProcess.CONTINUE;
