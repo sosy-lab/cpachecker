57a4029351f502f39d3d0277de5f658973762458
pwendler <pwendler@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2013-11-02 15:10:13 +0000

    fix exception during statistics writing if target state is at the end of the CFA
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@9299 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/cpa/arg/ARGStatistics.java
+++ src/org/sosy_lab/cpachecker/cpa/arg/ARGStatistics.java
@@ -24,6 +24,7 @@
 package org.sosy_lab.cpachecker.cpa.arg;
 
 import static com.google.common.base.Preconditions.checkNotNull;
+import static com.google.common.base.Predicates.notNull;
 import static com.google.common.collect.FluentIterable.from;
 import static org.sosy_lab.cpachecker.util.AbstractStates.IS_TARGET_STATE;
 
@@ -317,7 +318,7 @@ public class ARGStatistics implements Statistics {
       public void appendTo(Appendable out) throws IOException {
         // Write edges mixed with assigned values.
 
-        for (CFAEdge edge : targetPath.asEdgesList()) {
+        for (CFAEdge edge : from(targetPath.asEdgesList()).filter(notNull())) {
           out.append(edge.toString());
           out.append(System.lineSeparator());
           if (model != null) {
