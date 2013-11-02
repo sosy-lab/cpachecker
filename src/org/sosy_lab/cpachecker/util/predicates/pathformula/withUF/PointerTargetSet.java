bbdc174bbd88d34e552582abf563d0971af9f23c
pwendler <pwendler@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2013-11-02 12:47:36 +0000

    Extract class DeferredAllocationPool to its own file.
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@9295 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/util/predicates/pathformula/withUF/PointerTargetSet.java
+++ src/org/sosy_lab/cpachecker/util/predicates/pathformula/withUF/PointerTargetSet.java
@@ -138,126 +138,6 @@ public class PointerTargetSet implements Serializable {
   }
 
   /**
-   * This class is used to keep information about already performed memory allocations of unknown type, e.g.
-   *
-   *   <pre>
-   *   void *tmp_0 = malloc(size); // tmp_0 is a pointer variable, allocation type is unknown
-   *   ...
-   *   void *tmp_2 = tmp_0; // Now tmp_2 is also a pointer variable corresponding to the same allocation
-   *   struct s* ps = (struct s*)tmp_2; // Now the actual type of the allocation is revealed
-   *   </pre>
-   *
-   * <p>
-   * When the type of the allocation is revealed (by the context in which one of the pointer variables is used),
-   * the actual allocation occurs (pointer targets are added to the set).
-   * </p>
-   * <p>
-   * Several base variables can fall within the same pool in case of merging, e.g.:
-   *
-   *   <pre>
-   *   void *tmp_0;
-   *   if (condition) {
-   *     tmp_0 = malloc(size1); // Corresponds to a fake base variable __VERIFIER_successfull_alloc0
-   *   } else {
-   *     tmp_0 = malloc(size2); // Corresponds to another fake base variable __VERIFIER_successfull_alloc1
-   *   }
-   *   ... (struct s*) tmp_0 // Both base variables are allocated here as (struct s)
-   *                         // (but their addresses can be different!)
-   *   </pre>
-   */
-   public static class DeferredAllocationPool {
-
-    private DeferredAllocationPool(final PersistentList<String> pointerVariables,
-                                   final boolean isZeroing,
-                                   final CIntegerLiteralExpression size,
-                                   final PersistentList<String> baseVariables) {
-      this.pointerVariables = pointerVariables;
-      this.isZeroing = isZeroing;
-      this.size = size;
-      this.baseVariables = baseVariables;
-    }
-
-    DeferredAllocationPool(final String pointerVariable,
-                           final boolean isZeroing,
-                           final CIntegerLiteralExpression size,
-                           final String baseVariable) {
-      this(PersistentList.of(pointerVariable), isZeroing, size, PersistentList.of(baseVariable));
-    }
-
-    private DeferredAllocationPool(final DeferredAllocationPool predecessor,
-                                   final PersistentList<String> pointerVariables) {
-      this(pointerVariables,
-           predecessor.isZeroing,
-           predecessor.size,
-           predecessor.baseVariables);
-    }
-
-    public PersistentList<String> getPointerVariables() {
-      return pointerVariables;
-    }
-
-    public PersistentList<String> getBaseVariables() {
-      return baseVariables;
-    }
-
-    public boolean wasAllocationZeroing() {
-      return isZeroing;
-    }
-
-    public CIntegerLiteralExpression getSize() {
-      return size;
-    }
-
-    DeferredAllocationPool addPointerVariable(final String pointerVariable) {
-      return new DeferredAllocationPool(this, this.pointerVariables.with(pointerVariable));
-    }
-
-    DeferredAllocationPool removePointerVariable(final String pointerVariable) {
-      return new DeferredAllocationPool(this, pointerVariables.without(pointerVariable));
-    }
-
-    DeferredAllocationPool mergeWith(final DeferredAllocationPool other) {
-      return new DeferredAllocationPool(mergePersistentLists(this.pointerVariables, other.pointerVariables),
-                                        this.isZeroing && other.isZeroing,
-                                        this.size != null && other.size != null ?
-                                          this.size.getValue().equals(other.size.getValue()) ? this.size : null :
-                                          this.size != null ? this.size : other.size,
-                                        mergePersistentLists(this.baseVariables, other.baseVariables));
-    }
-
-    @Override
-    public boolean equals(final Object other) {
-      if (this == other) {
-        return true;
-      }
-      if (!(other instanceof DeferredAllocationPool)) {
-        return false;
-      }
-      final DeferredAllocationPool otherPool = (DeferredAllocationPool) other;
-      if (pointerVariables.containsAll(otherPool.pointerVariables) &&
-          otherPool.pointerVariables.containsAll(pointerVariables)) {
-        return true;
-      } else {
-        return false;
-      }
-    }
-
-    @Override
-    public int hashCode() {
-      int result = 0;
-      for (final String s : pointerVariables) {
-        result += s.hashCode();
-      }
-      return result;
-    }
-
-    private final PersistentList<String> pointerVariables;
-    private final boolean isZeroing;
-    private final CIntegerLiteralExpression size;
-    private final PersistentList<String> baseVariables;
-  }
-
-  /**
    * Return the length of an array if statically given, or null.
    */
   public static Integer getArrayLength(CArrayType t) {
@@ -1087,7 +967,7 @@ public class PointerTargetSet implements Serializable {
                                      Pair.of(mergedBases.getSecond(), mergedBases.getFirst()));
   }
 
-  private static <T> PersistentList<T> mergePersistentLists(final PersistentList<T> list1,
+  static <T> PersistentList<T> mergePersistentLists(final PersistentList<T> list1,
                                                             final PersistentList<T> list2) {
     final int size1 = list1.size();
     final ArrayList<T> arrayList1 = new ArrayList<>(size1);
