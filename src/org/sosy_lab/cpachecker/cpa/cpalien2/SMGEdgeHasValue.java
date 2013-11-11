cdf25d63d15534d62c714ecf2ebd3dc1a41a1edb
dresler <dresler@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2013-11-11 21:21:19 +0000

    Copy everything from the branch SMG_Explicitly into the trunk. This includes: - The CPAs CPAlien and ExplicitCPA. - Some config files and test sets. - C programs for testing.
    Because the code in the branch and the trunk were not yet successfully integrated,
    but we want to have the code in the branch now,
    I made copies of both CPAs and their config files instead of merging them into the existing ones.
    This means, we now have (temporarily) two CPAlien and two ExplicitCPAs in the trunk.
    All files in the packages of these CPAs were duplicated,
    even those that were equal in both trunk and branch.
    The CPAs from the trunk were untouched.
    The CPAs from the branch SMG_Explicitly have gotten the suffix "2".
    I added the suffix in the following places:
    - at the end of the package name of the CPAs
    - in the names of the config files
    - in all places where these CPAs are referenced (e.g., in their config files)
    I did *NOT* add the suffix anywhere else, including not in the following places:
    - any class, type, method, or variable name
    - the name of the configuration options
      (so both versions of the CPAs listen to the same configuration options!)
    
    It is not possible to use both versions of ExplicitCPA or CPAlien
    together in the same configuration,
    you have to either use the one from the trunk or the one from the branch.
    It is also not possible to mix between CPAs,
    if you use both CPAlien and ExplicitCPA,
    they have to be from the same source.
    
    The rest of CPAchecker should work with both versions of the CPA,
    I integrated them in all places where the compiler enforced it
    (that's why there are a few changes in classes outside the CPAlien/ExplicitCPA packages).
    
    The unit tests run through, but I did not ran any further test.
    
    I added the mergeinfo property for SVN
    so that SVN now believes this branch was cleanly integrated into the trunk
    and won't propose any further merges.
    The SVN history for all files copied was also kept intact.
    
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/appengine@9685 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

---
+++ src/org/sosy_lab/cpachecker/cpa/cpalien2/SMGEdgeHasValue.java
@@ -0,0 +1,164 @@
+/*
+ *  CPAchecker is a tool for configurable software verification.
+ *  This file is part of CPAchecker.
+ *
+ *  Copyright (C) 2007-2013  Dirk Beyer
+ *  All rights reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *
+ *  CPAchecker web page:
+ *    http://cpachecker.sosy-lab.org
+ */
+package org.sosy_lab.cpachecker.cpa.cpalien2;
+
+import java.math.BigInteger;
+
+import org.sosy_lab.cpachecker.cfa.ast.c.CIntegerLiteralExpression;
+import org.sosy_lab.cpachecker.cfa.types.MachineModel;
+import org.sosy_lab.cpachecker.cfa.types.c.CArrayType;
+import org.sosy_lab.cpachecker.cfa.types.c.CBasicType;
+import org.sosy_lab.cpachecker.cfa.types.c.CSimpleType;
+import org.sosy_lab.cpachecker.cfa.types.c.CType;
+
+public class SMGEdgeHasValue extends SMGEdge {
+  final private CType type;
+  final private int offset;
+
+  final private CSimpleType dummyChar = new CSimpleType(false, false, CBasicType.CHAR, false, false, true, false, false, false, false);
+  final private CSimpleType dummyInt = new CSimpleType(false, false, CBasicType.INT, true, false, false, true, false, false, false);
+
+  public SMGEdgeHasValue(CType pType, int pOffset, SMGObject pObject, int pValue) {
+    super(pValue, pObject);
+    type = pType;
+    offset = pOffset;
+  }
+
+  public SMGEdgeHasValue(int pSizeInBytes, int pOffset, SMGObject pObject, int pValue) {
+    super(pValue, pObject);
+    CIntegerLiteralExpression arrayLen = new CIntegerLiteralExpression(null, dummyInt, BigInteger.valueOf(pSizeInBytes));
+    type = new CArrayType(false, false, dummyChar, arrayLen);
+    offset = pOffset;
+  }
+
+  @Override
+  public String toString() {
+    return "sizeof(" + type.toASTString("foo") + ")b @ " + object.getLabel() + "+" + offset + "b has value " + value;
+  }
+
+  public int getOffset() {
+    return offset;
+  }
+
+  public CType getType() {
+    return type;
+  }
+
+  public int getSizeInBytes(MachineModel pMachineModel) {
+    return pMachineModel.getSizeof(type);
+  }
+
+  @Override
+  public boolean isConsistentWith(SMGEdge other) {
+    if (! (other instanceof SMGEdgeHasValue)) {
+      return false;
+    }
+
+    if ((this.object == other.object) &&
+        (this.offset == ((SMGEdgeHasValue)other).offset) &&
+        (this.type == ((SMGEdgeHasValue)other).type)) {
+      return (this.value == other.value);
+    }
+
+    return true;
+  }
+
+  public boolean overlapsWith(SMGEdgeHasValue other, MachineModel pModel) {
+    if (this.object != other.object) {
+      throw new IllegalArgumentException("Call of overlapsWith() on Has-Value edges pair not originating from the same object");
+    }
+
+    int otStart = other.getOffset();
+
+    int otEnd = otStart + pModel.getSizeof(other.getType());
+
+    return overlapsWith(otStart, otEnd, pModel);
+  }
+
+  public boolean overlapsWith(int pOtStart, int pOtEnd, MachineModel pModel) {
+
+    int myStart = offset;
+
+    int myEnd = myStart + pModel.getSizeof(type);
+
+    if (myStart < pOtStart) {
+      return (myEnd > pOtStart);
+
+    } else if (pOtStart < myStart) {
+      return (pOtEnd > myStart);
+    }
+
+    // Start offsets are equal, always overlap
+    return true;
+  }
+
+  public boolean isCompatibleField(SMGEdgeHasValue other, MachineModel pModel) {
+    return this.type.equals(other.type) && (this.offset == other.offset);
+  }
+
+  public boolean isCompatibleFieldOnSameObject(SMGEdgeHasValue other, MachineModel pModel) {
+    // return (this.type.equals(other.type)) && (this.offset == other.offset) && (this.object == other.object);
+    return pModel.getSizeof(type) == pModel.getSizeof(other.type) && (this.offset == other.offset) && this.object == other.object;
+  }
+
+  /* (non-Javadoc)
+   * @see java.lang.Object#hashCode()
+   */
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = super.hashCode();
+    result = prime * result + offset;
+    result = prime * result + ((type == null) ? 0 : type.hashCode());
+    return result;
+  }
+
+  /* (non-Javadoc)
+   * @see java.lang.Object#equals(java.lang.Object)
+   */
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) {
+      return true;
+    }
+    if (!super.equals(obj)) {
+      return false;
+    }
+    if (getClass() != obj.getClass()) {
+      return false;
+    }
+    SMGEdgeHasValue other = (SMGEdgeHasValue) obj;
+    if (offset != other.offset) {
+      return false;
+    }
+    if (type == null) {
+      if (other.type != null) {
+        return false;
+      }
+    } else if (!type.getCanonicalType().equals(other.type.getCanonicalType())) {
+      return false;
+    }
+    return true;
+  }
+}
