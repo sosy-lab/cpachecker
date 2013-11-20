d7b7d8631af0b4d282bf16a756d31e7f5aec69ce
mczech <mczech@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2013-11-20 13:05:55 +0000

    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@9866 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/cpa/sign/SignState.java
+++ src/org/sosy_lab/cpachecker/cpa/sign/SignState.java
@@ -43,7 +43,7 @@ public class SignState implements AbstractState, TargetableWithPredicatedAnalysi
   private static SignTargetChecker targetChecker;
 
   static void init(Configuration config) throws InvalidConfigurationException{
-    targetChecker = new SignTargetChecker(config);
+    //targetChecker = new SignTargetChecker(config);
   }
 
   private SignMap signMap;
