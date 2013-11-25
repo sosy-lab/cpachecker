e22d84c6866d27a3479f5e9526bd2dd9ae2800a6
mcjakobs <mcjakobs@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2013-11-25 08:26:20 +0000

    ensured that CPA behaves correctly e.g. in case of recursion but requires CallstackCPA for correct behavior to get information about possible recursion
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@9899 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/cpa/validvars/ValidVarsTransferRelation.java
+++ src/org/sosy_lab/cpachecker/cpa/validvars/ValidVarsTransferRelation.java
@@ -32,11 +32,14 @@ import org.sosy_lab.cpachecker.cfa.ast.c.CDeclaration;
 import org.sosy_lab.cpachecker.cfa.ast.c.CVariableDeclaration;
 import org.sosy_lab.cpachecker.cfa.model.CFAEdge;
 import org.sosy_lab.cpachecker.cfa.model.FunctionEntryNode;
+import org.sosy_lab.cpachecker.cfa.model.FunctionReturnEdge;
 import org.sosy_lab.cpachecker.cfa.model.MultiEdge;
 import org.sosy_lab.cpachecker.cfa.model.c.CDeclarationEdge;
 import org.sosy_lab.cpachecker.core.interfaces.AbstractState;
 import org.sosy_lab.cpachecker.core.interfaces.Precision;
 import org.sosy_lab.cpachecker.core.interfaces.TransferRelation;
+import org.sosy_lab.cpachecker.cpa.callstack.CallstackState;
+import org.sosy_lab.cpachecker.cpa.callstackPCC.CallstackPccState;
 import org.sosy_lab.cpachecker.exceptions.CPATransferException;
 
 
@@ -74,9 +77,6 @@ public class ValidVarsTransferRelation implements TransferRelation{
       validVariables.extendLocalVars(pCfaEdge.getSuccessor().getFunctionName(),
           ((FunctionEntryNode) pCfaEdge.getSuccessor()).getFunctionParameterNames());
       break;
-    case FunctionReturnEdge:
-      validVariables = validVariables.removeVarsOfFunction(pCfaEdge.getPredecessor().getFunctionName());
-      break;
     case DeclarationEdge:
       CDeclaration declaration = ((CDeclarationEdge) pCfaEdge).getDeclaration();
       if (declaration instanceof CVariableDeclaration) {
@@ -112,17 +112,54 @@ public class ValidVarsTransferRelation implements TransferRelation{
       CFAEdge pCfaEdge, Precision pPrecision) throws CPATransferException, InterruptedException {
 
     ValidVarsState state = (ValidVarsState) pState;
+    ValidVars vars = state.getValidVariables();
+
+    if(pCfaEdge instanceof FunctionReturnEdge){
+      String funName = pCfaEdge.getPredecessor().getFunctionName();
+      boolean containsFunction = false;
+      boolean foundCss = false;
+
+      for(AbstractState otherS :pOtherStates){
+        if (otherS instanceof CallstackState) {
+          foundCss = true;
+          CallstackState css = (CallstackState) otherS;
+          for (int i = css.getDepth(); i > 0 & !containsFunction; i--) {
+            containsFunction = containsFunction && css.getCurrentFunction().equals(funName);
+            css = css.getPreviousState();
+          }
+        }
+        if(otherS instanceof CallstackPccState){
+          foundCss = true;
+          CallstackPccState css = (CallstackPccState) otherS;
+          for (int i = css.getDepth(); i > 0 & !containsFunction; i--) {
+            containsFunction = containsFunction && css.getCurrentFunction().equals(funName);
+            css = css.getPreviousState();
+          }
+        }
+      }
+      if(!foundCss){
+        throw new CPATransferException("Require CallstackCPA or CallstackPccCPA to securely remove variables of a function "
+            +"after function return. Otherwise e.g. recursion cannot be handled.");
+      }
+      if(!containsFunction){
+        vars = vars.removeVarsOfFunction(funName);
+      }
+    }
+
     Collection<? extends AbstractState> wrappedStrengthen =
         wrappedTransfer.strengthen(state.getWrappedState(), pOtherStates, pCfaEdge, pPrecision);
 
     if(wrappedStrengthen == null || wrappedStrengthen.size()==0){
+      if(pCfaEdge instanceof FunctionReturnEdge && vars!=state.getValidVariables()){
+        return Collections.singleton(new ValidVarsState(state.getWrappedState(), vars));
+      }
       return null;
     }
 
     ArrayList<AbstractState> successors = new ArrayList<>(wrappedStrengthen.size());
 
     for (AbstractState successor : wrappedStrengthen) {
-      successors.add(new ValidVarsState(successor, state.getValidVariables()));
+      successors.add(new ValidVarsState(successor, vars));
     }
 
     return successors;
