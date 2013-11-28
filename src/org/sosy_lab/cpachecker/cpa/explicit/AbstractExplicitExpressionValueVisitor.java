86dda7f319ceb56ca829eaa4f36c2708c3ebc71d
cib <cib@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2013-11-28 13:24:12 +0000

    AEEVV arithmeticOperation für double implementiert.
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/explicitfloat@9923 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/cpa/explicit/AbstractExplicitExpressionValueVisitor.java
+++ src/org/sosy_lab/cpachecker/cpa/explicit/AbstractExplicitExpressionValueVisitor.java
@@ -332,6 +332,41 @@ public abstract class AbstractExplicitExpressionValueVisitor
 
   }
 
+  private static double arithmeticOperation(final double l, final double r,
+      final BinaryOperator op, final CType calculationType,
+      final MachineModel machineModel, final LogManager logger) {
+
+    switch (op) {
+    case PLUS:
+      return l + r;
+    case MINUS:
+      return l - r;
+    case DIVIDE:
+      if (r == 0) {
+        logger.logf(Level.SEVERE, "Division by Zero (%d / %d)", l, r);
+        return 0;
+      }
+      return l / r;
+    case MODULO:
+      return l % r; // TODO in C always sign of first operand?
+    case MULTIPLY:
+      return l * r;
+    case SHIFT_LEFT:
+      throw new AssertionError("trying to perform shift on floating point operands");
+    case SHIFT_RIGHT:
+      throw new AssertionError("trying to perform shift on floating point operands");
+    case BINARY_AND:
+      throw new AssertionError("trying to perform binary and on floating point operands");
+    case BINARY_OR:
+      throw new AssertionError("trying to perform binary or on floating point operands");
+    case BINARY_XOR:
+      throw new AssertionError("trying to perform binary xor on floating point operands");
+    default:
+      throw new AssertionError("unknown binary operation: " + op);
+    }
+
+  }
+
   private static NumberContainer arithmeticOperation(final NumberContainer l, final NumberContainer r,
       final BinaryOperator op, final CType calculationType,
       final MachineModel machineModel, final LogManager logger) {
@@ -345,10 +380,12 @@ public abstract class AbstractExplicitExpressionValueVisitor
         long lVal = l.getNumber().longValue();
         long rVal = l.getNumber().longValue();
         long result = arithmeticOperation(lVal, rVal, op, calculationType, machineModel, logger);
-        return new NumberContainer(CNumericTypes.INT, new BigDecimal(result));
-      } else if(type.getType() == CBasicType.FLOAT) {
-        // TODO: implement arithmetic operations for floats
-        throw new AssertionError("floats not implemented yet!");
+        return new NumberContainer(CNumericTypes.INT, result);
+      } else if(type.getType() == CBasicType.DOUBLE) {
+        double lVal = l.doubleValue();
+        double rVal = r.doubleValue();
+        double result = arithmeticOperation(lVal, rVal, op, calculationType, machineModel, logger);
+        return new NumberContainer(CNumericTypes.DOUBLE, result);
       } else {
         throw new AssertionError("unsupported type of result of binary operation: "+type);
       }
