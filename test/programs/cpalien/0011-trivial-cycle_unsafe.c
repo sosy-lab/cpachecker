aaec0e6418f186a0308af5322f2b0a7032a7ddef
muller <muller@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2013-12-11 14:28:07 +0000

    CPAlien: Two new testcases
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@10023 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

---
+++ test/programs/cpalien/0011-trivial-cycle_unsafe.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdbool.h>
+
+struct node;
+extern bool __VERIFIER_nondet_bool();
+
+typedef struct node {
+  struct node *next;
+  struct node *prev;
+} node;
+
+int main() {
+  node* nd = malloc(sizeof(node));
+  nd->next = NULL;
+
+  while (__VERIFIER_nondet_bool()) {
+    if (nd->next == NULL) {
+      nd->next = nd;
+    }
+    else {
+      nd->next = NULL;
+    }
+  }
+
+  free(nd->next);
+  return 0;
+}
