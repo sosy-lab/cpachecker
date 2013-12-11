ee92e979216bf0684d3f13d5df0caf910f3a2869
muller <muller@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2013-12-11 14:28:18 +0000

    CPAlien: Add few testcases for assumeEdge processing
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@10025 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

---
+++ test/programs/cpalien/0017-assume-edge-deref-dangling_unsafe.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdbool.h>
+
+extern bool __VERIFIER_nondet_bool();
+
+int main() {
+  bool selector = __VERIFIER_nondet_bool();
+
+  int *ptr;
+
+  if (!selector) {
+    *ptr = malloc(sizeof(int));
+  }
+
+  if (selector) {
+    *ptr = 666;
+    ptr = NULL;
+    free(ptr);
+  }
+
+  return;
+}
---
+++ test/programs/cpalien/0018-builtins_safe.c
@@ -0,0 +1,9 @@
+#include <stdbool.h>
+
+extern bool __VERIFIER_nondet_bool();
+extern bool __VERIFIER_nondet_int();
+
+int main() {
+  int a = __VERIFIER_nondet_int();
+  bool b = __VERIFIER_nondet_bool();
+}
