582b0678de850de7220aff611f398e170f06a11b
mandrykin <mandrykin@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2013-12-13 15:45:10 +0000

    Several minor fixes in PredicateAnalysisWithUFs:  -- empty list initializer for unions of primitive types  -- initializers for nested unions/structures  -- a bug with const and volatile elimination
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@10069 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/util/predicates/pathformula/ctoformula/CToFormulaWithUFConverter.java
+++ src/org/sosy_lab/cpachecker/util/predicates/pathformula/ctoformula/CToFormulaWithUFConverter.java
@@ -175,6 +175,12 @@ public class CToFormulaWithUFConverter extends CtoFormulaConverter {
     return efmgr.getFormulaType(size * bitsPerByte);
   }
 
+  @Override
+  Variable scopedIfNecessary(CIdExpression var, SSAMapBuilder ssa, String function) {
+    return Variable.create(var.getDeclaration().getQualifiedName(),
+                           PointerTargetSet.simplifyType(var.getExpressionType()));
+  }
+
   private void checkSsaSavedType(final String name, final CType type, final SSAMapBuilder ssa) {
     CType ssaSavedType = ssa.getType(name);
     if (ssaSavedType != null) {
