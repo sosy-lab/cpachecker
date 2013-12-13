f59af02258930873e298019c411fd13a5febb1df
quadt <quadt@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2013-12-13 17:36:36 +0000

    Merged
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/testgen@10074 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/core/algorithm/testgen/dummygen/ARGStateDummyCreator.java
+++ src/org/sosy_lab/cpachecker/core/algorithm/testgen/dummygen/ARGStateDummyCreator.java
@@ -24,40 +24,40 @@
 package org.sosy_lab.cpachecker.core.algorithm.testgen.dummygen;
 
 import java.util.Collection;
-import java.util.HashSet;
-import java.util.Set;
 
 import org.sosy_lab.common.LogManager;
-import org.sosy_lab.common.Pair;
-import org.sosy_lab.common.Triple;
 import org.sosy_lab.common.configuration.Configuration;
 import org.sosy_lab.common.configuration.InvalidConfigurationException;
 import org.sosy_lab.cpachecker.cfa.CFA;
 import org.sosy_lab.cpachecker.cfa.model.CFAEdge;
 import org.sosy_lab.cpachecker.cfa.model.CFANode;
-import org.sosy_lab.cpachecker.core.defaults.SingletonPrecision;
-import org.sosy_lab.cpachecker.core.reachedset.ReachedSet;
-import org.sosy_lab.cpachecker.cpa.arg.ARGPath;
+import org.sosy_lab.cpachecker.core.CoreComponentsFactory;
+import org.sosy_lab.cpachecker.core.ShutdownNotifier;
+import org.sosy_lab.cpachecker.core.interfaces.AbstractState;
+import org.sosy_lab.cpachecker.core.interfaces.ConfigurableProgramAnalysis;
+import org.sosy_lab.cpachecker.core.interfaces.Precision;
+import org.sosy_lab.cpachecker.core.interfaces.TransferRelation;
 import org.sosy_lab.cpachecker.cpa.arg.ARGState;
-import org.sosy_lab.cpachecker.cpa.arg.ARGTransferRelation;
-import org.sosy_lab.cpachecker.cpa.arg.ARGUtils;
-import org.sosy_lab.cpachecker.cpa.explicit.ExplicitTransferRelation;
+import org.sosy_lab.cpachecker.exceptions.CPAException;
 import org.sosy_lab.cpachecker.exceptions.CPATransferException;
 import org.sosy_lab.cpachecker.util.AbstractStates;
-
-import com.google.common.base.Function;
 
 
 public class ARGStateDummyCreator {
 
-  private ARGTransferRelation transferRelation;
+  private TransferRelation transferRelation;
   private CFA cfa;
+  private Precision prec;
 //  private final static ImmutableList<PrecisionAdjustment> precs = ImmutableList.of();
 //  private ARGPrecisionAdjustment precision;
 
-  public ARGStateDummyCreator(CFA pCfa, Configuration config, LogManager pLogger) throws InvalidConfigurationException {
+  public ARGStateDummyCreator(CFA pCfa, Configuration config, LogManager pLogger) throws InvalidConfigurationException, CPAException {
     cfa = pCfa;
-    transferRelation = new ARGTransferRelation(new ExplicitTransferRelation(config, pLogger, pCfa));
+    ShutdownNotifier notifier = ShutdownNotifier.create();
+    ConfigurableProgramAnalysis cpa = new CoreComponentsFactory(config, pLogger,notifier).createCPA(pCfa, null);
+    transferRelation = cpa.getTransferRelation();
+    prec = cpa.getInitialPrecision(cfa.getMainFunction());
+    //transferRelation = new ARGTransferRelation(new ExplicitTransferRelation(config, pLogger, pCfa));
 //    precision = new ARGPrecisionAdjustment(
 //        new ComponentAwareExplicitPrecisionAdjustment(precs, config, pCfa),false);
   }
@@ -72,61 +72,33 @@ public class ARGStateDummyCreator {
    */
   public ARGState computeOtherSuccessor(ARGState pState, ARGState pNotToChildState) throws CPATransferException, InterruptedException{
     CFAEdge edgeToWrongChild = pState.getEdgeToChild(pNotToChildState);
+    CFAEdge correctEdge = null;
     CFANode location = AbstractStates.extractLocation(pState);
+    for (int i = 0; i < location.getNumLeavingEdges(); i++) {
+      CFAEdge edge = location.getLeavingEdge(i);
+      if(edge.equals(edgeToWrongChild)) {
+        continue;
+      } else {
+        correctEdge = edge;
+      }
+
+    }
     if(location.getNumLeavingEdges() > 2) {
       throw new IllegalStateException("Node has more than two leaving edges, which is unsupported.");
     }
-    Collection<ARGState> successors = transferRelation.getAbstractSuccessors(pState, SingletonPrecision.getInstance(), null);
+    Collection<? extends AbstractState> successors = transferRelation.getAbstractSuccessors(pState, prec, null);
     ARGState newState = null;
-    for (ARGState argState : successors) {
+    for (AbstractState argState : successors) {
       CFANode l = AbstractStates.extractLocation(argState);
       if(location.getEdgeTo(l).equals(edgeToWrongChild)) {
         continue;
       } else {
-        newState = argState;
+        newState = (ARGState)argState;
       }
     }
 
     return newState;
 
-  }
-
-  /**
-   * @param pReached The ReachedSet to work on
-   * @param pIsTraget Function that identifies the target State to switch
-   * @return Triple(the new path, the root ARGState, the target ARGState identified by pIsTarget)
-   */
-  public Triple<Set<ARGState>, ARGState, ARGState> produceOtherPath(ReachedSet pReached,
-      Function<ARGState, Boolean> pIsTarget) throws CPATransferException, InterruptedException {
-    ARGState lastState = (ARGState) pReached.getLastState();
-    ARGPath path = ARGUtils.getOnePathTo(lastState);
-
-    ARGState targetState = null;
-    ARGState targetSuccessorInPath = null;
-
-    Set<ARGState> newPath = new HashSet<>();
-
-    for (Pair<ARGState, CFAEdge> stateEdgePair : path) {
-      ARGState curState = stateEdgePair.getFirst();
-      if (targetState != null) {
-        targetSuccessorInPath = curState;
-        break;
-      } else if (pIsTarget.apply(curState)) {
-        targetState = curState;
-      }
-
-      newPath.add(curState);
-    }
-
-    assert targetState != null : "Target State not found";
-    assert targetSuccessorInPath != null : "Target State found but has no successor in path";
-
-    ARGState targetOtherSuccessor = computeOtherSuccessor(targetState, targetSuccessorInPath);
-    targetSuccessorInPath.removeFromARG();
-    targetOtherSuccessor.addParent(targetState);
-    newPath.add(targetOtherSuccessor);
-
-    return Triple.of(newPath, path.getFirst().getFirst(), targetState);
   }
 
 }
