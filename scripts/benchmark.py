cf654f5321472094db30d09173ed0279a17faf53
pwendler <pwendler@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2013-12-17 10:08:28 +0000

    re-enable code that was disabled only because of the problem with hanging processes, this is fixed now
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@10105 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- scripts/benchmark.py
+++ scripts/benchmark.py
@@ -625,15 +625,14 @@ def main(argv=None):
         if not os.path.exists(arg) or not os.path.isfile(arg):
             parser.error("File {0} does not exist.".format(repr(arg)))
 
-    # Temporarily disabled because of problems with hanging ps processes.
-    #if not config.cloud:
-    #    try:
-    #        processes = subprocess.Popen(['ps', '-eo', 'cmd'], stdout=subprocess.PIPE).communicate()[0]
-    #        if len(re.findall("python.*benchmark\.py", Util.decodeToString(processes))) > 1:
-    #            logging.warn("Already running instance of this script detected. " + \
-    #                         "Please make sure to not interfere with somebody else's benchmarks.")
-    #    except OSError:
-    #        pass # this does not work on Windows
+    if not config.cloud:
+        try:
+            processes = subprocess.Popen(['ps', '-eo', 'cmd'], stdout=subprocess.PIPE).communicate()[0]
+            if len(re.findall("python.*benchmark\.py", Util.decodeToString(processes))) > 1:
+                logging.warn("Already running instance of this script detected. " + \
+                             "Please make sure to not interfere with somebody else's benchmarks.")
+        except OSError:
+            pass # this does not work on Windows
 
     for arg in config.files:
         if STOPPED_BY_INTERRUPT: break
