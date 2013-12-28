752329e551c296eb0d60bbbada57b2ba4002da3e
dresler <dresler@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2013-12-28 15:30:30 +0000

    Add display of status.
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@10199 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/appengine/server/resource/JobServerResource.java
+++ src/org/sosy_lab/cpachecker/appengine/server/resource/JobServerResource.java
@@ -23,6 +23,7 @@
  */
 package org.sosy_lab.cpachecker.appengine.server.resource;
 
+import org.restlet.data.MediaType;
 import org.restlet.ext.wadl.WadlServerResource;
 import org.restlet.representation.Representation;
 import org.restlet.representation.StringRepresentation;
@@ -36,7 +37,7 @@ public class JobServerResource extends WadlServerResource implements JobResource
   @Override
   public Representation jobAsHtml() {
     Job job = JobDAO.load(getAttribute("jobKey"));
-    return new StringRepresentation(job.getLog());
+    return new StringRepresentation("Status: "+job.getStatus()+"<br><br><pre>"+job.getLog()+"</pre>", MediaType.TEXT_HTML);
   }
 
 }
