669e8a71e4e49c3a57eb03024f151d7835df020b
stieglma <stieglma@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2014-01-14 12:42:21 +0000

    in #getCalculationTypeForBinaryOperation() CArrayType is now treated like CPointer or CFunctionType
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@10384 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/cfa/parser/eclipse/c/CBinaryExpressionBuilder.java
+++ src/org/sosy_lab/cpachecker/cfa/parser/eclipse/c/CBinaryExpressionBuilder.java
@@ -299,8 +299,8 @@ public class CBinaryExpressionBuilder {
 
 
     // both are pointer or function-pointer
-    if ((pType1 instanceof CPointerType || pType1 instanceof CFunctionType)
-        && (pType2 instanceof CPointerType || pType2 instanceof CFunctionType)) {
+    if ((pType1 instanceof CPointerType || pType1 instanceof CFunctionType || pType1 instanceof CArrayType)
+        && (pType2 instanceof CPointerType || pType2 instanceof CFunctionType || pType2 instanceof CArrayType)) {
 
       if (pBinOperator == BinaryOperator.MINUS) { return machineModel.getPointerDiffType(); }
 
@@ -327,7 +327,6 @@ public class CBinaryExpressionBuilder {
 
 
     // TODO check if there are other types, that can be used in binaryExp
-
     throw new AssertionError(String.format(
         "unhandled type-conversion: %s (%s, %s) of (%s, %s)",
         pBinOperator, pType1, pType2, pType1.getClass(), pType2.getClass()));
