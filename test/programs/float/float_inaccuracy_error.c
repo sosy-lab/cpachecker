223ae8eae33ddfa715a52432aea5d53c8690afc3
cib <cib@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2014-01-16 11:42:44 +0000

    Corrected the example where the ExplicitCPA currently behaves differently from C.
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/explicitfloat@10427 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- test/programs/float/float_inaccuracy_error.c
+++ test/programs/float/float_inaccuracy_error.c
@@ -1,15 +1,15 @@
 int main() {
+   int i = 0;
    int j = 123;
    float b = 2.2;
-   float h = j + b; // 125.2
-   double c = 125.2;
+   double h = j + b; // 125.2
    
-   if (h - c != 0) {
+   double x = 125.2 - h;
+   if (x > 0.0) {
       goto Error;
    }
-   
    return 0;
 
-   Error:
    return -1;
+
 }
