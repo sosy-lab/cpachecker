09a4019c240e06abd856ac8d5272b0077ac20d92
stieglma <stieglma@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2014-01-26 03:24:17 +0000

    revert accidentially committed changes
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@10581 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/cfa/parser/eclipse/c/GlobalScope.java
+++ src/org/sosy_lab/cpachecker/cfa/parser/eclipse/c/GlobalScope.java
@@ -228,10 +228,7 @@ class GlobalScope implements Scope {
       // We now have a real declaration for a type for which we have seen a forward declaration
       // We set a reference to the full type in the old type
       // and update the types map with the full type.
-      if (type != ((CElaboratedType)oldType).getRealType()) {
-        ((CElaboratedType)oldType).setRealType(type);
-      }
-
+      ((CElaboratedType)oldType).setRealType(type);
     }
 
     types.put(name, declaration);
