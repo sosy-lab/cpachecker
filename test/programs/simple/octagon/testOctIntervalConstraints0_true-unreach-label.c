bc29fc0c7b5f3d06b7f7c43b70d47b9cef8ab76b
stieglma <stieglma@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2014-01-27 17:06:19 +0000

    some more tests
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@10639 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

---
+++ test/programs/simple/octagon/testOctIntervalConstraints0_true-unreach-label.c
@@ -0,0 +1,17 @@
+void __VERIFIER_assert(int cond) {
+  if (!(cond)) {
+    ERROR: goto ERROR;
+  }
+  return;
+}
+
+int main(void) {
+  int a = __VERIFIER_nondet_uint(); //interval from 0 to infinity
+
+  __VERIFIER_assert(a >= 0);
+  __VERIFIER_assert(a > -1);
+  __VERIFIER_assert(!(a < 0));
+
+  return a;
+}
+
