141f0a46fd67aa5a68829c21cf7d34ad0038ea91
pwendler <pwendler@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2014-01-31 11:31:34 +0000

    Using "-spec file.prp" now automatically uses the correct specification automaton if the .prp file specifies the reachability property.
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@10758 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/cmdline/CmdLineArguments.java
+++ src/org/sosy_lab/cpachecker/cmdline/CmdLineArguments.java
@@ -89,6 +89,7 @@ class CmdLineArguments {
 
   private static final Pattern SPECIFICATION_FILES_PATTERN = DEFAULT_CONFIG_FILES_PATTERN;
   private static final String SPECIFICATION_FILES_TEMPLATE = "config/specification/%s.spc";
+  private static final String REACHABILITY_SPECIFICATION_FILE = "config/specification/sv-comp.spc";
 
   private static final Pattern PROPERTY_FILE_PATTERN = Pattern.compile("(.)+\\.prp");
 
@@ -357,15 +358,15 @@ class CmdLineArguments {
                                                PropertyType.VALID_FREE,
                                                PropertyType.VALID_MEMTRACK))) {
                 putIfNotExistent(options, "memorysafety.check", "true");
+                newValue = null;
 
               } else if (properties.equals(EnumSet.of(PropertyType.REACHABILITY))) {
-                // no change needed
+                newValue = REACHABILITY_SPECIFICATION_FILE;
 
               } else {
                 System.err.println("Checking for the properties " + properties + " is currently not supported by CPAchecker.");
                 System.exit(0);
               }
-              newValue = null;
             }
 
             else {
