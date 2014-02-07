5eb087ea7134c33c2716f55f655cb3c74f3131ac
stahlbauer <stahlbauer@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2014-02-07 16:13:10 +0000

    Added a statistic-value class to describe distributions of integers.
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@10944 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

---
+++ src/org/sosy_lab/cpachecker/util/statistics/StatIntHist.java
@@ -0,0 +1,53 @@
+/*
+ *  CPAchecker is a tool for configurable software verification.
+ *  This file is part of CPAchecker.
+ *
+ *  Copyright (C) 2007-2013  Dirk Beyer
+ *  All rights reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *
+ *  CPAchecker web page:
+ *    http://cpachecker.sosy-lab.org
+ */
+package org.sosy_lab.cpachecker.util.statistics;
+
+import com.google.common.collect.HashMultiset;
+import com.google.common.collect.Multiset;
+
+
+public class StatIntHist extends StatInt {
+
+  private Multiset<Integer> hist = HashMultiset.create();
+
+  public StatIntHist(StatKind pMainStatisticKind, String pTitle) {
+    super(pMainStatisticKind, pTitle);
+  }
+
+  public int getTimesWithValue(Integer value) {
+    return hist.count(value);
+  }
+
+  @Override
+  public void setNextValue(int pNewValue) {
+    super.setNextValue(pNewValue);
+    hist.add(pNewValue);
+  }
+
+  @Override
+  public String toString() {
+    return super.toString() + " " + hist.toString();
+  }
+
+}
