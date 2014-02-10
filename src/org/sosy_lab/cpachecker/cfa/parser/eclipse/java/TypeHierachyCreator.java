8ea3ec2abf419829a9467d6f522129e5e0f69bcf
pwendler <pwendler@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2014-02-10 08:00:09 +0000

    Update copyright information.
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@10954 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/cfa/parser/eclipse/java/TypeHierachyCreator.java
+++ src/org/sosy_lab/cpachecker/cfa/parser/eclipse/java/TypeHierachyCreator.java
@@ -1,9 +1,8 @@
 /*
-
  *  CPAchecker is a tool for configurable software verification.
  *  This file is part of CPAchecker.
  *
- *  Copyright (C) 2007-2013  Dirk Beyer
+ *  Copyright (C) 2007-2014  Dirk Beyer
  *  All rights reserved.
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
