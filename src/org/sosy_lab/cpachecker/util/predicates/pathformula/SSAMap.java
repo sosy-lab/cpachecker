eab6cbb8afe199cbe14d40c7c4384220df64fa70
pwendler <pwendler@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2014-02-12 09:24:51 +0000

    Remove the possibility to store SSA indices for uninterpreted functions from the SSAMap.
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@10995 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/util/predicates/pathformula/SSAMap.java
+++ src/org/sosy_lab/cpachecker/util/predicates/pathformula/SSAMap.java
@@ -24,13 +24,11 @@
 package org.sosy_lab.cpachecker.util.predicates.pathformula;
 
 import static com.google.common.base.Preconditions.checkNotNull;
-import static com.google.common.base.Predicates.notNull;
 import static com.google.common.collect.Iterators.*;
 
 import java.io.Serializable;
 import java.util.AbstractMap.SimpleImmutableEntry;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
@@ -51,22 +49,11 @@ import org.sosy_lab.cpachecker.cfa.types.c.CElaboratedType;
 import org.sosy_lab.cpachecker.cfa.types.c.CFunctionType;
 import org.sosy_lab.cpachecker.cfa.types.c.CPointerType;
 import org.sosy_lab.cpachecker.cfa.types.c.CType;
-import org.sosy_lab.cpachecker.util.predicates.interfaces.FormulaList;
 
 import com.google.common.base.Equivalence;
-import com.google.common.base.Function;
 import com.google.common.base.Joiner;
-import com.google.common.base.Objects;
 import com.google.common.base.Preconditions;
-import com.google.common.collect.FluentIterable;
 import com.google.common.collect.ImmutableList;
-import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.ImmutableMultiset;
-import com.google.common.collect.LinkedHashMultiset;
-import com.google.common.collect.MapDifference;
-import com.google.common.collect.Maps;
-import com.google.common.collect.Multiset;
-import com.google.common.collect.Multiset.Entry;
 
 /**
  * Maps a variable name to its latest "SSA index", that should be used when
@@ -91,8 +78,6 @@ public class SSAMap implements Serializable {
     private SSAMap ssa;
     private PersistentSortedMap<String, Integer> vars; // Do not update without updating varsHashCode!
     private PersistentSortedMap<String, CType> varTypes;
-    private Multiset<Pair<String, FormulaList>> funcsBuilder = null;
-    private Map<Pair<String, FormulaList>, CType> funcTypesBuilder = null;
 
     // Instead of computing vars.hashCode(),
     // we calculate the hashCode ourselves incrementally
@@ -108,11 +93,6 @@ public class SSAMap implements Serializable {
 
     public int getIndex(String variable) {
       return SSAMap.getIndex(variable, vars);
-    }
-
-    public int getIndex(String name, FormulaList args) {
-      return SSAMap.getIndex(Pair.<String, FormulaList>of(name, args),
-          Objects.firstNonNull(funcsBuilder, ssa.funcs));
     }
 
     public CType getType(String name) {
@@ -149,35 +129,6 @@ public class SSAMap implements Serializable {
       }
     }
 
-    public void setIndex(String name, FormulaList args, CType type, int idx) {
-      Preconditions.checkArgument(idx > 0, "Indices need to be positive for this SSAMap implementation!");
-
-      if (funcsBuilder == null) {
-        funcsBuilder = LinkedHashMultiset.create(ssa.funcs);
-      }
-      Pair<String, FormulaList> key = Pair.of(name, args);
-      Preconditions.checkArgument(idx >= funcsBuilder.count(key), "SSAMap updates need to be strictly monotone!");
-
-      checkNotNull(type);
-      CType oldType = Objects.firstNonNull(funcTypesBuilder, ssa.types).get(key);
-      if (oldType != null) {
-        oldType = oldType.getCanonicalType();
-        type = type.getCanonicalType();
-        Preconditions.checkArgument(
-               type instanceof CFunctionType || oldType instanceof CFunctionType
-            || (isEnumPointerType(type) && isEnumPointerType(oldType))
-            || oldType.equals(type)
-            , "Cannot change type of variable %s in SSAMap from %s to %s", key, oldType, type);
-      } else {
-        if (funcTypesBuilder == null) {
-          funcTypesBuilder = new HashMap<>(ssa.types);
-        }
-        funcTypesBuilder.put(key, type);
-      }
-
-      funcsBuilder.setCount(key, idx);
-    }
-
     private static boolean isEnumPointerType(CType type) {
       if (type instanceof CPointerType) {
         type = ((CPointerType) type).getType();
@@ -197,10 +148,6 @@ public class SSAMap implements Serializable {
       }
     }
 
-    public Iterable<Pair<Variable, FormulaList>> allFunctions() {
-      return SSAMap.allFunctions(Objects.firstNonNull(funcTypesBuilder, ssa.types));
-    }
-
     public SortedSet<String> allVariables() {
       return varTypes.keySet();
     }
@@ -217,15 +164,11 @@ public class SSAMap implements Serializable {
      * Returns an immutable SSAMap with all the changes made to the builder.
      */
     public SSAMap build() {
-      if (vars == ssa.vars && funcsBuilder == null) {
+      if (vars == ssa.vars) {
         return ssa;
       }
 
-      ssa = new SSAMap(vars, varsHashCode,
-                       Objects.firstNonNull(funcsBuilder, ssa.funcs),
-                       varTypes,
-                       Objects.firstNonNull(funcTypesBuilder, ssa.types));
-      funcsBuilder = null;
+      ssa = new SSAMap(vars, varsHashCode, varTypes);
       return ssa;
     }
 
@@ -241,9 +184,7 @@ public class SSAMap implements Serializable {
   private static final SSAMap EMPTY_SSA_MAP = new SSAMap(
       PathCopyingPersistentTreeMap.<String, Integer>of(),
       0,
-      ImmutableMultiset.<Pair<String, FormulaList>>of(),
-      PathCopyingPersistentTreeMap.<String, CType>of(),
-      ImmutableMap.<Pair<String, FormulaList>, CType>of());
+      PathCopyingPersistentTreeMap.<String, CType>of());
 
   /**
    * Returns an empty immutable SSAMap.
@@ -253,20 +194,13 @@ public class SSAMap implements Serializable {
   }
 
   public SSAMap withDefault(final int defaultValue) {
-    return new SSAMap(this.vars, this.varsHashCode, this.funcs, this.varTypes, this.types) {
+    return new SSAMap(this.vars, this.varsHashCode, this.varTypes) {
 
       private static final long serialVersionUID = -5638018887478723717L;
 
       @Override
       public int getIndex(String pVariable) {
         int result = super.getIndex(pVariable);
-
-        return (result < 0) ? defaultValue : result;
-      }
-
-      @Override
-      public int getIndex(String pName, FormulaList pArgs) {
-        int result = super.getIndex(pName, pArgs);
 
         return (result < 0) ? defaultValue : result;
       }
@@ -291,25 +225,13 @@ public class SSAMap implements Serializable {
     List<Triple<String, Integer, Integer>> differences;
     if (s1.vars == s2.vars) {
       differences = ImmutableList.of();
-      if (s1.funcs == s2.funcs && s1.types == s2.types) {
-        // both are absolutely identical
-        return Pair.of(s1, differences);
-      }
-      vars = s1.vars;
+      // both are absolutely identical
+      return Pair.of(s1, differences);
 
     } else {
       differences = new ArrayList<>();
       vars = merge(s1.vars, s2.vars, Equivalence.equals(),
           MAXIMUM_ON_CONFLICT, differences);
-    }
-
-    Multiset<Pair<String, FormulaList>> funcs;
-    if ((s1.funcs == s2.funcs) || s2.funcs.isEmpty()) {
-      funcs = s1.funcs;
-    } else if (s1.funcs.isEmpty()) {
-      funcs = s2.funcs;
-    } else {
-      funcs = merge(s1.funcs, s2.funcs);
     }
 
     PersistentSortedMap<String, CType> varTypes;
@@ -334,35 +256,7 @@ public class SSAMap implements Serializable {
           exceptionOnConflict, null);
     }
 
-    Map<Pair<String, FormulaList>, CType> funcTypes;
-    if (s1.types == s2.types) {
-      funcTypes = s1.types;
-    } else {
-
-      MapDifference<Pair<String, FormulaList>, CType> diff = Maps.difference(s1.types, s2.types);
-      if (!diff.entriesDiffering().isEmpty()) {
-        throw new IllegalArgumentException("Cannot merge SSAMaps that contain the same variable {0} with differing types: " + diff.entriesDiffering());
-      }
-
-      if (diff.entriesOnlyOnLeft().isEmpty()) {
-        assert s2.types.size() >= s1.types.size();
-        funcTypes = s2.types;
-
-      } else if (diff.entriesOnlyOnRight().isEmpty()) {
-        assert s1.types.size() >= s2.types.size();
-        funcTypes = s1.types;
-
-      } else {
-        funcTypes = new HashMap<>(diff.entriesInCommon().size()
-                            + diff.entriesOnlyOnLeft().size()
-                            + diff.entriesOnlyOnRight().size());
-        funcTypes.putAll(diff.entriesInCommon());
-        funcTypes.putAll(diff.entriesOnlyOnLeft());
-        funcTypes.putAll(diff.entriesOnlyOnRight());
-      }
-    }
-
-    return Pair.of(new SSAMap(vars, 0, funcs, varTypes, funcTypes), differences);
+    return Pair.of(new SSAMap(vars, 0, varTypes), differences);
   }
 
   /**
@@ -505,49 +399,17 @@ public class SSAMap implements Serializable {
     }
   };
 
-  private static <T> Multiset<T> merge(Multiset<T> s1, Multiset<T> s2) {
-    Multiset<T> result = LinkedHashMultiset.create(Math.max(s1.elementSet().size(), s2.elementSet().size()));
-    for (Entry<T> entry : s1.entrySet()) {
-      T key = entry.getElement();
-      int i1 = entry.getCount();
-      int i2 = s2.count(key);
-      result.setCount(key, Math.max(i1, i2));
-    }
-    for (Entry<T> entry : s2.entrySet()) {
-      T key = entry.getElement();
-      if (!s1.contains(key)) {
-        result.setCount(key, entry.getCount());
-      }
-    }
-
-    return result;
-  }
-
-  /*
-   * Use Multiset<String> instead of Map<String, Integer> because it is more
-   * efficient. The integer value is stored as the number of instances of any
-   * element in the Multiset. So instead of calling map.get(key) we just use
-   * Multiset.count(key). This is better because the Multiset internally uses
-   * modifiable integers instead of the immutable Integer class.
-   */
   private final PersistentSortedMap<String, Integer> vars;
-  private final Multiset<Pair<String, FormulaList>> funcs;
-
   private final PersistentSortedMap<String, CType> varTypes;
-  private final Map<Pair<String, FormulaList>, CType> types;
 
-  // Cache hashCode of potentiall big map
+  // Cache hashCode of potentially big map
   private final int varsHashCode;
 
   private SSAMap(PersistentSortedMap<String, Integer> vars,
                  int varsHashCode,
-                 Multiset<Pair<String, FormulaList>> funcs,
-                 PersistentSortedMap<String, CType> varTypes,
-                 Map<Pair<String, FormulaList>, CType> types) {
+                 PersistentSortedMap<String, CType> varTypes) {
     this.vars = vars;
-    this.funcs = funcs;
     this.varTypes = varTypes;
-    this.types = types;
 
     if (varsHashCode == 0) {
       this.varsHashCode = vars.hashCode();
@@ -562,16 +424,6 @@ public class SSAMap implements Serializable {
    */
   public SSAMapBuilder builder() {
     return new SSAMapBuilder(this);
-  }
-
-  private static <T> int getIndex(T key, Multiset<T> map) {
-    int i = map.count(key);
-    if (i != 0) {
-      return i;
-    } else {
-      // no index found, return -1
-      return INDEX_NOT_CONTAINED;
-    }
   }
 
   private static <T> int getIndex(T key, PersistentMap<T, Integer> map) {
@@ -590,11 +442,6 @@ public class SSAMap implements Serializable {
   public int getIndex(String variable) {
     return getIndex(variable, vars);
   }
-
-  public int getIndex(String name, FormulaList args) {
-    return getIndex(Pair.<String, FormulaList>of(name, args), funcs);
-  }
-
   public CType getType(String name) {
     return varTypes.get(name);
   }
@@ -607,32 +454,16 @@ public class SSAMap implements Serializable {
     return varTypes.entrySet();
   }
 
-  public Iterable<Pair<Variable, FormulaList>> allFunctions() {
-    return allFunctions(types);
-  }
-
-  static Iterable<Pair<Variable, FormulaList>> allFunctions(final Map<Pair<String, FormulaList>, CType> types) {
-    return FluentIterable.from(types.entrySet())
-        .transform(
-            new Function<Map.Entry<Pair<String, FormulaList>, CType>, Pair<Variable, FormulaList>>() {
-              @Override
-              public Pair<Variable, FormulaList> apply(Map.Entry<Pair<String, FormulaList>, CType> pInput) {
-                return Pair.of(Variable.create(pInput.getKey().getFirst(), pInput.getValue()), pInput.getKey().getSecond());
-              }
-            })
-        .filter(notNull());
-  }
-
   private static final Joiner joiner = Joiner.on(" ");
 
   @Override
   public String toString() {
-    return joiner.join(vars.entrySet()) + " " + joiner.join(funcs.entrySet());
+    return joiner.join(vars.entrySet());
   }
 
   @Override
   public int hashCode() {
-    return (31 + funcs.hashCode()) * 31 + varsHashCode;
+    return varsHashCode;
   }
 
   @Override
@@ -645,9 +476,7 @@ public class SSAMap implements Serializable {
       SSAMap other = (SSAMap)obj;
       // Do a few cheap checks before the expensive ones.
       return varsHashCode == other.varsHashCode
-          && funcs.entrySet().size() == other.funcs.entrySet().size()
-          && vars.equals(other.vars)
-          && funcs.equals(other.funcs);
+          && vars.equals(other.vars);
     }
   }
 }
