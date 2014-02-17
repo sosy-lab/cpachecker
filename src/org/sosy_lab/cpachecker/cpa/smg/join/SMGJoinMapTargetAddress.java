04bcca427a2704442ec82a9fb5990a59de8aa944
muller <muller@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2014-02-17 17:59:40 +0000

    CPAlien: Rename SMGJoin to less redundant name
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@11176 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/cpa/smg/join/SMGJoinMapTargetAddress.java
+++ src/org/sosy_lab/cpachecker/cpa/smg/join/SMGJoinMapTargetAddress.java
@@ -21,7 +21,7 @@
  *  CPAchecker web page:
  *    http://cpachecker.sosy-lab.org
  */
-package org.sosy_lab.cpachecker.cpa.smg.SMGJoin;
+package org.sosy_lab.cpachecker.cpa.smg.join;
 
 import java.util.Collection;
 
