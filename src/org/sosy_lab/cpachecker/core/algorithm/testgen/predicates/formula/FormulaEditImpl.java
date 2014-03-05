b9980380d16763da2efd5c67da380b616b4ed067
dahlberg <dahlberg@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2014-03-05 18:46:45 +0000

    add implementation of FormulaEditFunction
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/testgen@11518 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

---
+++ src/org/sosy_lab/cpachecker/core/algorithm/testgen/predicates/formula/FormulaEditImpl.java
@@ -0,0 +1,77 @@
+/*
+ *  CPAchecker is a tool for configurable software verification.
+ *  This file is part of CPAchecker.
+ *
+ *  Copyright (C) 2007-2014  Dirk Beyer
+ *  All rights reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *
+ *  CPAchecker web page:
+ *    http://cpachecker.sosy-lab.org
+ */
+package org.sosy_lab.cpachecker.core.algorithm.testgen.predicates.formula;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.sosy_lab.cpachecker.util.predicates.interfaces.BooleanFormula;
+import org.sosy_lab.cpachecker.util.predicates.interfaces.view.FormulaManagerView;
+
+
+public class FormulaEditImpl implements FormulaEditFunction {
+
+  @Override
+  public BooleanFormula negateAtomOfFormula(BooleanFormula pF, int pPos, FormulaManagerView fmv) {
+    //      PredicateCPA predCPA = CPAs.retrieveCPA(cpa, PredicateCPA.class);
+    //      Formula f;
+    //      fm = predCPA.getFormulaManager();
+
+    // TODO get a useful FMV
+
+    /*
+     * negiere eine teilformel einer gegebenen Formel
+     */
+    //      zerlege formel in teilformeln
+
+    // boolean splitArithEqualities, boolean conjunctionsOnly
+    Collection<BooleanFormula> formulaAtoms = fmv.extractAtoms(pF, false, false);
+    //    wähle eine teilformel nach BlubStrategie aus
+    BooleanFormula fTeil = ((List<BooleanFormula>) formulaAtoms).get(pPos);
+    //    negiere die gewählte teilformel
+    BooleanFormula negatedFTeil = fmv.makeNot(fTeil);
+    //    schmeiß die ausgewählte Formel aus der Menge der Formeln raus
+    BooleanFormula sum = null;
+
+    for (BooleanFormula t : formulaAtoms) {
+      if (t == fTeil) {
+        if (sum == null) {
+          sum = negatedFTeil;
+        } else {
+          sum = fmv.makeAnd(sum, negatedFTeil);
+        }
+
+      } else {
+        if (sum == null) {
+          sum = t;
+        } else {
+          sum = fmv.makeAnd(sum, t);
+        }
+      }
+    }
+
+    return sum;
+
+  }
+}
