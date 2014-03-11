4bd754bc67a9e46b5090ac986494c4f921b55365
pwendler <pwendler@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2014-03-11 12:19:18 +0000

    Fix executability of cpachecker.py
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@11585 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- scripts/benchmark/tools/cpachecker.py
+++ scripts/benchmark/tools/cpachecker.py
@@ -1,3 +1,29 @@
+#!/usr/bin/env python
+
+"""
+CPAchecker is a tool for configurable software verification.
+This file is part of CPAchecker.
+
+Copyright (C) 2007-2014  Dirk Beyer
+All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+
+CPAchecker web page:
+  http://cpachecker.sosy-lab.org
+"""
+
 # prepare for Python 3
 from __future__ import absolute_import, print_function, unicode_literals
 
@@ -7,13 +33,13 @@ import sys
 import string
 import os
 import re
-import benchmark.result as result
 
 sys.dont_write_bytecode = True # prevent creation of .pyc files
 
 if __name__ == "__main__":
     sys.path.append(os.path.join(os.path.dirname(__file__), os.path.pardir, os.path.pardir))
 
+import benchmark.result as result
 import benchmark.util as Util
 import benchmark.tools.template
 
