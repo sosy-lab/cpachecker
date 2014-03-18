dc4d446935357c6a86437035781941b12e44d9b3
pwendler <pwendler@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2014-03-18 14:13:04 +0000

    Rename package cpachecker.coverage into cpachecker.util.coverage.
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@11780 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/util/coverage/CoveragePrinter.java
+++ src/org/sosy_lab/cpachecker/util/coverage/CoveragePrinter.java
@@ -21,7 +21,7 @@
  *  CPAchecker web page:
  *    http://cpachecker.sosy-lab.org
  */
-package org.sosy_lab.cpachecker.coverage;
+package org.sosy_lab.cpachecker.util.coverage;
 
 import java.io.IOException;
 
