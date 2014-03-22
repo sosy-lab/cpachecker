4d69faf11544abf687237d069e87f7421ad2c8e5
friedlto <friedlto@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2014-03-22 03:43:42 +0000

    Fix for Python 3 in SympyCaller
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/symbolicvalue@11890 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- lib/python/sympy/sympyCaller.py
+++ lib/python/sympy/sympyCaller.py
@@ -2,6 +2,12 @@
 
 import sys
 
+# Fix for Python 3 (where "execfile" was removed)
+# Taken from http://code.activestate.com/recipes/577942-execfile-is-back-backwards-compatibility-part-5/
+if sys.version[0] == "3":
+  def execfile(STRING_some_path_and_file, globals={}, locals={}):
+    exec(open(STRING_some_path_and_file).read()) in globals, locals
+
 if __name__ == "__main__":
   tmpFilename = sys.stdin.read()
   execfile(tmpFilename)
