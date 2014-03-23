ddb50826af2e2651a2a75e742c5e365405a3a4b2
cib <cib@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2014-03-23 15:22:15 +0000

    Fixes to the external simplifier.
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/symbolicvalue@11903 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/cpa/value/simplifier/ExternalSimplifier.java
+++ src/org/sosy_lab/cpachecker/cpa/value/simplifier/ExternalSimplifier.java
@@ -42,28 +42,6 @@ import edu.jas.kern.ComputerThreads;
  * Uses an external library/application to simplify symbolic formulas.
  */
 public class ExternalSimplifier {
-  public static void main(String[] args) {
-
-    // Static initialization of the MathEclipse engine instead of null
-    // you can set a file name to overload the default initial
-    // rules. This step should be called only once at program setup:
-    F.initSymbols(null);
-    EvalUtilities util = new EvalUtilities();
-
-    IExpr result;
-
-    try {
-      String input = "Expand[X1-X1]";
-      result = util.evaluate(input);
-      System.out.println("isplus: "+result.isPlus());
-      System.out.println(result.toString());
-    } catch (final Exception e) {
-      e.printStackTrace();
-    } finally {
-      // Call terminate() only one time at the end of the program
-      ComputerThreads.terminate();
-    }
-  }
 
   public static ExpressionBase simplify(ExpressionBase expr) {
     // Static initialization of the MathEclipse engine instead of null
@@ -75,11 +53,12 @@ public class ExternalSimplifier {
     IExpr result;
 
     try {
-      String input = "Simplify["+convertFormulaToString(expr)+"]";
+      List<SymbolicValue> usedVariables = new ArrayList<SymbolicValue>();
+      String input = "Simplify["+convertFormulaToString(expr, usedVariables)+"]";
       System.out.println(input);
       result = util.evaluate(input);
       System.out.println(result.toString());
-      return recursiveConvertExpressionToFormula(result);
+      return recursiveConvertExpressionToFormula(result, usedVariables);
     } catch (final Exception e) {
       e.printStackTrace();
     } finally {
@@ -90,8 +69,8 @@ public class ExternalSimplifier {
     return expr;
   }
 
-  private static String convertFormulaToString(ExpressionBase expr) {
-    return recursiveConvertFormulaToString(expr, new ArrayList<SymbolicValue>());
+  private static String convertFormulaToString(ExpressionBase expr, List<SymbolicValue> usedVariables) {
+    return recursiveConvertFormulaToString(expr, usedVariables);
   }
 
   private static String recursiveConvertFormulaToString(ExpressionBase formulaExpr, List<SymbolicValue> usedVariables) {
@@ -106,30 +85,45 @@ public class ExternalSimplifier {
     } else if(formulaExpr instanceof BinaryExpression) {
       BinaryExpression expr = (BinaryExpression) formulaExpr;
       String leftHand = recursiveConvertFormulaToString(expr.getOperand1(), usedVariables);
-      String rightHand = recursiveConvertFormulaToString(expr.getOperand1(), usedVariables);
+      String rightHand = recursiveConvertFormulaToString(expr.getOperand2(), usedVariables);
       String operator = operatorIdentifierToOperator(expr.getOperator().toString());
       return "(" + leftHand + " " + operator + " " + rightHand + ")";
     } else if(formulaExpr instanceof ConstantValue) {
-      return formulaExpr.toString();
+      return ((ConstantValue)formulaExpr).getValue().asNumericValue().getNumber().toString();
     } else {
       // This should never happen, as the above list should cover all symbolic value formulas.
       throw new RuntimeException("Unsupported formula.");
     }
   }
 
-  private static ExpressionBase recursiveConvertExpressionToFormula(IExpr expression) {
+  private static ExpressionBase recursiveConvertExpressionToFormula(IExpr expression, List<SymbolicValue> usedVariables) {
     if(expression.isPlus()) {
-      ExpressionBase leftHand = recursiveConvertExpressionToFormula(expression.getAt(0));
-      ExpressionBase rightHand = recursiveConvertExpressionToFormula(expression.getAt(1));
+      ExpressionBase leftHand = recursiveConvertExpressionToFormula(expression.getAt(0), usedVariables);
+      ExpressionBase rightHand = recursiveConvertExpressionToFormula(expression.getAt(1), usedVariables);
       return new BinaryExpression(leftHand, rightHand, BinaryExpression.BinaryOperator.PLUS, CNumericTypes.INT, CNumericTypes.INT);
     } else if(expression.isTimes()) {
-      ExpressionBase leftHand = recursiveConvertExpressionToFormula(expression.getAt(0));
-      ExpressionBase rightHand = recursiveConvertExpressionToFormula(expression.getAt(1));
+      ExpressionBase leftHand = recursiveConvertExpressionToFormula(expression.getAt(1), usedVariables);
+      ExpressionBase rightHand = recursiveConvertExpressionToFormula(expression.getAt(2), usedVariables);
       return new BinaryExpression(leftHand, rightHand, BinaryExpression.BinaryOperator.MULTIPLY, CNumericTypes.INT, CNumericTypes.INT);
+    } else if(expression.isPower()) {
+      // TODO: find a way to tell symja not to generate power expressions
+      ExpressionBase base = recursiveConvertExpressionToFormula(expression.getAt(0), usedVariables);
+      ExpressionBase power = recursiveConvertExpressionToFormula(expression.getAt(1), usedVariables);
+
+      // TODO: build an expression for any power, not just 2
+      if(power instanceof ConstantValue && ((ConstantValue)power).getValue().asLong(CNumericTypes.INT) == 2) {
+        return new BinaryExpression(base, base, BinaryExpression.BinaryOperator.MULTIPLY, CNumericTypes.INT, CNumericTypes.INT);
+      } else {
+        throw new RuntimeException("Power larger than 2 not yet supported.");
+      }
     } else if(expression.isInteger()) {
       return new ConstantValue(new NumericValue(Integer.parseInt(expression.toString())));
+    } else if(expression.toString().startsWith("X")) {
+      String indexString = expression.toString().substring(1);
+      int index = Integer.parseInt(indexString);
+      return usedVariables.get(index);
     } else {
-      throw new RuntimeException("Unsupported formula.");
+      throw new RuntimeException("Unsupported formula: "+expression.toString());
     }
   }
 
