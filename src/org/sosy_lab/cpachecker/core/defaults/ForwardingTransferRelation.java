bbea9005b11ad87a292541c0c30b85be7942a88e
friedber <friedber@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2014-03-28 15:31:28 +0000

    set edge to inner edge whileiterating over MultiEdges.
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@12004 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/core/defaults/ForwardingTransferRelation.java
+++ src/org/sosy_lab/cpachecker/core/defaults/ForwardingTransferRelation.java
@@ -251,8 +251,10 @@ public abstract class ForwardingTransferRelation<S extends AbstractState, P exte
   /** This method just forwards the handling to every inner edge. */
   protected S handleMultiEdge(MultiEdge cfaEdge) throws CPATransferException {
     for (final CFAEdge innerEdge : cfaEdge) {
+      edge = innerEdge;
       state = handleSimpleEdge(innerEdge);
     }
+    edge = cfaEdge; // reset edge
     return state;
   }
 
