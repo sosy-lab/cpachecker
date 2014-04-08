89558aa0f319aef2ee47e140c722e3235a75b544
pwendler <pwendler@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2014-04-08 18:53:25 +0000

    Revert "CPAlien: Unit test for freeing a SLL"
    This reverts commit 11470.
    
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@12151 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/cpa/smg/objects/sll/SMGSingleLinkedListTest.java
+++ src/org/sosy_lab/cpachecker/cpa/smg/objects/sll/SMGSingleLinkedListTest.java
@@ -23,84 +23,13 @@
  */
 package org.sosy_lab.cpachecker.cpa.smg.objects.sll;
 
-import static org.mockito.Mockito.mock;
-
 import org.junit.Assert;
 import org.junit.Test;
-import org.sosy_lab.common.LogManager;
-import org.sosy_lab.cpachecker.cfa.types.MachineModel;
-import org.sosy_lab.cpachecker.cpa.smg.AnonymousTypes;
-import org.sosy_lab.cpachecker.cpa.smg.SMG;
-import org.sosy_lab.cpachecker.cpa.smg.SMGConsistencyVerifier;
-import org.sosy_lab.cpachecker.cpa.smg.SMGEdgePointsTo;
-import org.sosy_lab.cpachecker.cpa.smg.SMGInconsistentException;
-import org.sosy_lab.cpachecker.cpa.smg.SMGState;
-import org.sosy_lab.cpachecker.cpa.smg.SMGTransferRelation.SMGKnownSymValue;
 import org.sosy_lab.cpachecker.cpa.smg.objects.DummyAbstraction;
-import org.sosy_lab.cpachecker.cpa.smg.objects.SMGObject;
 import org.sosy_lab.cpachecker.cpa.smg.objects.SMGRegion;
-import org.sosy_lab.cpachecker.exceptions.InvalidQueryException;
 
 
 public class SMGSingleLinkedListTest {
-  static private final  LogManager logger = mock(LogManager.class);
-  static private MachineModel mm = MachineModel.LINUX64;
-
-  private SMGRegion prepareSingleLinkedList(SMGState pState) throws SMGInconsistentException {
-    SMGRegion pointer = new SMGRegion(mm.getSizeofPtr(), "pointer");
-    Integer listAddress = pState.getAddress(pState.getNullObject(), 0);
-    pState.addGlobalObject(pointer);
-    for (int i = 0; i < 15; i++) {
-      SMGEdgePointsTo pt = pState.addNewHeapAllocation(mm.getSizeofPtr(), "node");
-      pState.writeValue(pt.getObject(), 0, AnonymousTypes.dummyPointer, SMGKnownSymValue.valueOf(listAddress));
-      listAddress = pt.getValue();
-      pState.writeValue(pointer, 0, AnonymousTypes.dummyPointer,SMGKnownSymValue.valueOf(listAddress));
-    }
-    pState.attemptAbstraction();
-
-    Integer sllAddress = pState.readValue(pointer, 0, AnonymousTypes.dummyPointer);
-    SMGEdgePointsTo pt = pState.getPointerFromValue(sllAddress);
-    SMGObject object = pt.getObject();
-
-    Assert.assertTrue(object instanceof SMGSingleLinkedList);
-    SMGSingleLinkedList sll = (SMGSingleLinkedList) object;
-    Assert.assertEquals(15, sll.getLength());
-    Assert.assertEquals(0, sll.getOffset());
-
-    return pointer;
-  }
-
-  @Test
-  public void freeTest() throws SMGInconsistentException, InvalidQueryException {
-    SMGState state = new SMGState(logger, mm);
-    SMGRegion variable = prepareSingleLinkedList(state);
-
-    Integer address = state.readValue(variable, 0, AnonymousTypes.dummyPointer);
-    SMGEdgePointsTo pt = state.getPointerFromValue(address);
-    SMGSingleLinkedList sll = (SMGSingleLinkedList)(pt.getObject());
-    int originalLength = sll.getLength();
-
-    state.free(pt.getValue(), pt.getOffset(), sll);
-
-    Integer addressAfterFree = state.getAddress(sll, 0);
-    Assert.assertNotNull(addressAfterFree);
-    Assert.assertEquals(originalLength - 1, sll.getLength());
-    state.pruneUnreachable();
-
-    Integer addressAfterPrune = state.getAddress(sll, 0);
-    Assert.assertNull(addressAfterPrune);
-    Assert.assertTrue(state.checkProperty("has-leaks"));
-  }
-
-  @Test
-  public void consistencyInvalidSLLTest() {
-    SMG smg = new SMG(mm);
-    SMGRegion prototype = new SMGRegion(16, "prototype");
-    SMGSingleLinkedList sll = new SMGSingleLinkedList(prototype, 2, 4);
-    smg.addObject(sll, false);
-
-    Assert.assertFalse(SMGConsistencyVerifier.verifySMG(logger, smg));
-  }
 
   @Test
   public void basicsTest() {
