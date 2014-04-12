84a0aa710cd9d1832d83c98e0433820c57ac0f4e
friedber <friedber@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2014-04-12 15:21:49 +0000

    bugfix for benchmark-script
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@12239 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- scripts/benchmark/benchmarkDataStructures.py
+++ scripts/benchmark/benchmarkDataStructures.py
@@ -599,8 +599,8 @@ class Run():
         if returnvalue is not None \
                 and returnsignal == 9 \
                 and MEMLIMIT in rlimits \
-                and self.memUsage \
-                and int(self.memUsage) >= (rlimits[MEMLIMIT] * 1024 * 1024 * 0.999):
+                and 'memUsage' in self.values \
+                and int(self.values['memUsage']) >= (rlimits[MEMLIMIT] * 1024 * 1024 * 0.999):
             self.status = 'OUT OF MEMORY'
             self.category = result.CATEGORY_ERROR
 
