54ca6c6c779e457ee7dfda7e2d8f891e518c08b9
mcjakobs <mcjakobs@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2014-05-16 09:27:20 +0000

    included dummy error state if checking fails to see false instead of unknown as CPAchecker result
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@12546 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/core/algorithm/ProofCheckAlgorithm.java
+++ src/org/sosy_lab/cpachecker/core/algorithm/ProofCheckAlgorithm.java
@@ -35,6 +35,7 @@ import org.sosy_lab.common.log.LogManager;
 import org.sosy_lab.common.time.Timer;
 import org.sosy_lab.cpachecker.core.CPAcheckerResult.Result;
 import org.sosy_lab.cpachecker.core.ShutdownNotifier;
+import org.sosy_lab.cpachecker.core.defaults.SingletonPrecision;
 import org.sosy_lab.cpachecker.core.interfaces.ConfigurableProgramAnalysis;
 import org.sosy_lab.cpachecker.core.interfaces.Statistics;
 import org.sosy_lab.cpachecker.core.interfaces.StatisticsProvider;
@@ -42,6 +43,7 @@ import org.sosy_lab.cpachecker.core.interfaces.pcc.PCCStrategy;
 import org.sosy_lab.cpachecker.core.reachedset.ReachedSet;
 import org.sosy_lab.cpachecker.exceptions.CPAException;
 import org.sosy_lab.cpachecker.pcc.strategy.PCCStrategyBuilder;
+import org.sosy_lab.cpachecker.util.error.DummyErrorState;
 
 @Options
 public class ProofCheckAlgorithm implements Algorithm, StatisticsProvider {
@@ -128,6 +130,10 @@ public class ProofCheckAlgorithm implements Algorithm, StatisticsProvider {
 
     stats.totalTimer.stop();
     logger.log(Level.INFO, "Proof check algorithm finished.");
+
+    if (!result) {
+      reachedSet.add(new DummyErrorState(reachedSet.getFirstState()), SingletonPrecision.getInstance());
+    }
 
     return result;
   }
