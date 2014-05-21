d81643d9c90e17a55a21971bcc642c878df1dcd4
mcjakobs <mcjakobs@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2014-05-21 07:06:32 +0000

    graph balancer, exponentially computing optimal solution
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@12568 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/pcc/strategy/partitioning/ExponentialOptimalBalancedGraphPartitioner.java
+++ src/org/sosy_lab/cpachecker/pcc/strategy/partitioning/ExponentialOptimalBalancedGraphPartitioner.java
@@ -23,6 +23,9 @@
  */
 package org.sosy_lab.cpachecker.pcc.strategy.partitioning;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
 
@@ -31,19 +34,28 @@ import org.sosy_lab.cpachecker.core.interfaces.pcc.BalancedGraphPartitioner;
 import org.sosy_lab.cpachecker.pcc.strategy.partialcertificate.PartialReachedSetDirectedGraph;
 
 
-public class ExponentialOptimalBalancedGraphPartitioner implements BalancedGraphPartitioner{
+public class ExponentialOptimalBalancedGraphPartitioner implements BalancedGraphPartitioner {
+
+  private final ShutdownNotifier shutdown;
 
   public ExponentialOptimalBalancedGraphPartitioner(ShutdownNotifier pShutdownNotifier) {
-    // TODO Auto-generated constructor stub
+    shutdown = pShutdownNotifier;
   }
 
   @Override
-  public List<Set<Integer>> computePartitioning(int pNumPartitions, PartialReachedSetDirectedGraph pGraph) {
-    long currentNumCrossingEdges, minNumCrossingEdges = -1;
-    List<Set<Integer>> currentPartitioning, bestPartitioning = null;
+  public List<Set<Integer>> computePartitioning(final int pNumPartitions, final PartialReachedSetDirectedGraph pGraph)
+      throws InterruptedException {
+    List<Integer> permutationIndices = initPermutationIndices(pGraph.getNumNodes());
 
-    while (hasNextPermutation()) {
-      currentPartitioning = computeNextPermutation();
+    // compute partition for current order of nodes
+    List<Set<Integer>> currentPartitioning, bestPartitioning =
+        computePermutation(permutationIndices, pNumPartitions);
+
+    long currentNumCrossingEdges, minNumCrossingEdges = getNumberOfPartitionCrossingEdges(pGraph, bestPartitioning);
+
+    while (hasNextPermutation(permutationIndices)) {
+      shutdown.shutdownIfNecessary();
+      currentPartitioning = computeNextPermutation(permutationIndices, pNumPartitions);
       currentNumCrossingEdges = getNumberOfPartitionCrossingEdges(pGraph, currentPartitioning);
 
       if (minNumCrossingEdges >= currentNumCrossingEdges || minNumCrossingEdges == -1) {
@@ -54,14 +66,70 @@ public class ExponentialOptimalBalancedGraphPartitioner implements BalancedGraph
     return bestPartitioning;
   }
 
-  private boolean hasNextPermutation(){
-    // TODO implement
+  private List<Integer> initPermutationIndices(final int pNumIndices) {
+    List<Integer> permutationIndices = new ArrayList<>(pNumIndices);
+    for (int i = 0; i < pNumIndices; i++) {
+      permutationIndices.add(0);
+    }
+    return permutationIndices;
+  }
+
+  private List<Set<Integer>> initPartition(final int pNumPartitions, final int maxSize) {
+    List<Set<Integer>> result = new ArrayList<>(pNumPartitions);
+    for (int i = 0; i < pNumPartitions; i++) {
+      result.add(i, new HashSet<Integer>(maxSize));
+    }
+    return result;
+  }
+
+  private List<Set<Integer>> computePermutation(final List<Integer> pPermutationIndices, final int pNumPartitions) {
+
+    List<Integer> orderedNodes = new LinkedList<>();
+    for (int j = pPermutationIndices.size() - 1; j >= 0; j--) {
+      orderedNodes.add(pPermutationIndices.get(j), j);
+    }
+
+    int maxSize = (int) Math.ceil(pPermutationIndices.size() / ((double) pNumPartitions));
+    int numSmaller = maxSize * pNumPartitions - pPermutationIndices.size();
+
+    List<Set<Integer>> result = initPartition(pNumPartitions, maxSize);
+
+    Set<Integer> currentPartition;
+
+    for (int i = 0; i < numSmaller; i++) {
+      currentPartition = result.get(i);
+      for (int j = i * (maxSize - 1); j < (i + 1) * (maxSize - 1); j++) {
+        currentPartition.add(orderedNodes.get(j));
+      }
+    }
+
+    for (int i = 0; i < pPermutationIndices.size() - numSmaller; i++) {
+      currentPartition = result.get(i);
+      for (int j = i * maxSize + numSmaller * (maxSize - 1); j < (i + 1) * maxSize + numSmaller * (maxSize - 1); j++) {
+        currentPartition.add(orderedNodes.get(j));
+      }
+    }
+
+    return result;
+  }
+
+  private boolean hasNextPermutation(final List<Integer> pPermutationIndices) {
+    for (int i = 0, j = pPermutationIndices.size() - 1; i < pPermutationIndices.size(); i++, j--) {
+      if (pPermutationIndices.get(i) < j) { return true; }
+    }
     return false;
   }
 
-  private List<Set<Integer>> computeNextPermutation(){
-    // TODO implement or replace
-    return null;
+  private List<Set<Integer>> computeNextPermutation(final List<Integer> pPermutationIndices, final int pNumPartitions) {
+    for (int j = pPermutationIndices.size() - 1, i = 0; j >= 0; j--, i++) {
+      if (pPermutationIndices.get(j) < i) {
+        pPermutationIndices.set(j, pPermutationIndices.get(j) + 1);
+        break;
+      } else {
+        pPermutationIndices.set(j, 0);
+      }
+    }
+    return computePermutation(pPermutationIndices, pNumPartitions);
   }
 
   private long getNumberOfPartitionCrossingEdges(final PartialReachedSetDirectedGraph pGraph,
