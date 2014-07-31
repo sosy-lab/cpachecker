155197d286cc031cd7144e7c37d8996bfc3464cc
friedber <friedber@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2014-07-31 15:00:51 +0000

    adding some recursive sourcefiles, all of the same structure.
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/bamRecursion@13013 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

---
+++ test/programs/simple/recursive/id_o200_false-unreach-call.c
@@ -0,0 +1,15 @@
+extern int __VERIFIER_nondet_int();
+extern void __VERIFIER_error();
+
+int id(int x) {
+  if (x==0) return 0;
+  return id(x-1) + 1;
+}
+
+void main() {
+  int input = __VERIFIER_nondet_int();
+  int result = id(input);
+  if (result == 200) {
+    __VERIFIER_error();
+  }
+}
