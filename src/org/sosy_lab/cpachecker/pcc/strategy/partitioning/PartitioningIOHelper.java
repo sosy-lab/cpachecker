cc5954a8121febc96274be75445b812e93af0a30
mcjakobs <mcjakobs@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2014-08-05 12:46:33 +0000

    FindBugs suggestion
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@13064 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/pcc/strategy/partitioning/PartitioningIOHelper.java
+++ src/org/sosy_lab/cpachecker/pcc/strategy/partitioning/PartitioningIOHelper.java
@@ -249,8 +249,8 @@ public class PartitioningIOHelper {
     @Override
     public void printStatistics(PrintStream pOut, Result pResult, ReachedSet pReached) {
       if (numPartitions > 0 && partitions != null) {
-        pOut.format("Number of partitions: %d\n", numPartitions);
-        pOut.format("The following numbers are given in number of states.\n");
+        pOut.format("Number of partitions: %d%n", numPartitions);
+        pOut.format("The following numbers are given in number of states.%n");
         computeAndPrintDetailedPartitioningStats(pOut);
       }
     }
@@ -270,14 +270,14 @@ public class PartitioningIOHelper {
         totalS+=current;
       }
 
-      pOut.format("Certificate size: %d \n", totalS);
-      pOut.format("Total overhead:  %d\n", totalO);
-      pOut.format(Locale.ENGLISH,"Avg. partition size:  %.2e\n", ((double)totalS)/numPartitions);
-      pOut.format(Locale.ENGLISH,"Avg. partition overhead: %.2e\n", ((double)totalO)/numPartitions);
-      pOut.format("Max partition size: %d\n", maxP);
-      pOut.format("Max partition overhead: %d\n", maxO);
-      pOut.format("Min partition size: %d\n", minP);
-      pOut.format("Min partition overhead: %d\n", minO);
+      pOut.format("Certificate size: %d %n", totalS);
+      pOut.format("Total overhead:  %d%n", totalO);
+      pOut.format(Locale.ENGLISH,"Avg. partition size:  %.2e%n", ((double)totalS)/numPartitions);
+      pOut.format(Locale.ENGLISH,"Avg. partition overhead: %.2e%n", ((double)totalO)/numPartitions);
+      pOut.format("Max partition size: %d%n", maxP);
+      pOut.format("Max partition overhead: %d%n", maxO);
+      pOut.format("Min partition size: %d%n", minP);
+      pOut.format("Min partition overhead: %d%n", minO);
     }
 
     @Override
