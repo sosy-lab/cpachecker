4a5d9e069f526ac9ec37bb4eba116e029c8ac5f1
karpenkov <karpenkov@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2014-08-18 10:58:13 +0000

    Refactoring out CPA testing
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@13203 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

---
+++ src/org/sosy_lab/cpachecker/util/test/CPATestRunner.java
@@ -0,0 +1,58 @@
+/*
+ *  CPAchecker is a tool for configurable software verification.
+ *  This file is part of CPAchecker.
+ *
+ *  Copyright (C) 2007-2014  Dirk Beyer
+ *  All rights reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *
+ *  CPAchecker web page:
+ *    http://cpachecker.sosy-lab.org
+ */
+package org.sosy_lab.cpachecker.util.test;
+
+import org.sosy_lab.common.configuration.Configuration;
+import org.sosy_lab.common.configuration.FileOption;
+import org.sosy_lab.common.configuration.converters.FileTypeConverter;
+import org.sosy_lab.common.log.BasicLogManager;
+import org.sosy_lab.common.log.LogManager;
+import org.sosy_lab.common.log.StringBuildingLogHandler;
+import org.sosy_lab.cpachecker.core.CPAchecker;
+import org.sosy_lab.cpachecker.core.CPAcheckerResult;
+import org.sosy_lab.cpachecker.core.ShutdownNotifier;
+
+import java.util.Map;
+
+/**
+ * Helper class for running CPA tests.
+ */
+public class CPATestRunner {
+  public static TestResults run(
+      Map<String, String> pProperties,
+      String pSourceCodeFilePath) throws Exception {
+
+    Configuration config = Configuration.builder()
+        .addConverter(FileOption.class, new FileTypeConverter(Configuration.defaultConfiguration()))
+        .setOptions(pProperties).build();
+
+    StringBuildingLogHandler stringLogHandler = new StringBuildingLogHandler();
+    LogManager logger = new BasicLogManager(config, stringLogHandler);
+    ShutdownNotifier shutdownNotifier = ShutdownNotifier.create();
+    CPAchecker cpaChecker = new CPAchecker(config, logger, shutdownNotifier);
+    CPAcheckerResult results = cpaChecker.run(pSourceCodeFilePath);
+
+    return new TestResults(stringLogHandler.getLog(), results);
+  }
+}
