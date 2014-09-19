1ea76d28a9c61783abe7ad85b03ca9a14b1a1e9c
mcjakobs <mcjakobs@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2014-09-19 13:56:30 +0000

    javadoc comments
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@13644 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/core/interfaces/pcc/PartitioningCheckingHelper.java
+++ src/org/sosy_lab/cpachecker/core/interfaces/pcc/PartitioningCheckingHelper.java
@@ -26,8 +26,23 @@ package org.sosy_lab.cpachecker.core.interfaces.pcc;
 
 public interface PartitioningCheckingHelper {
 
+  /**
+   * Does the necessary actions to abort the complete certificate checking process.
+   * Informs all certificate checking components that certificate check failed.
+   * Possibly does more actions like stops checking of other partitions,
+   * prohibits start of a partition check.
+   */
   public void abortCheckingPreparation();
 
+  /**
+   * Returns intermediate size of the certificate. The returned size
+   * contains all elements which belong to an already checked partition
+   * as well as those elements recomputed in an already checked partition.
+   * The size may or may not include elements already explored in a
+   * partition check which is not completed.
+   *
+   * @return current size of certificate
+   */
   public int getCurrentCertificateSize();
 
 }
