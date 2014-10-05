63d59cb0478c901abc18a8eb70c887f23324287d
George Karpenkov <true.cheshire@gmail.com>
2014-10-05 18:38:09 +0000

    Adds PathFormulaReportingState interface,
    which allows abstract states to report their
    invariants using a combination of a formula and a SSAMap.
    
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@13788 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

---
+++ src/org/sosy_lab/cpachecker/core/interfaces/PathFormulaReportingState.java
@@ -0,0 +1,24 @@
+package org.sosy_lab.cpachecker.core.interfaces;
+
+import org.sosy_lab.cpachecker.util.predicates.interfaces.view.FormulaManagerView;
+import org.sosy_lab.cpachecker.util.predicates.pathformula.PathFormula;
+import org.sosy_lab.cpachecker.util.predicates.pathformula.SSAMap;
+
+/**
+ * Report the over-approximation of the abstract state using the formula
+ * language.
+ *
+ * Similar to {@link FormulaReportingState}, but uses {@link PathFormula} instead.
+ */
+public interface PathFormulaReportingState extends AbstractState{
+  /**
+   *
+   * @param manager Manager used to create the formulas.
+   * @param outMap {@link SSAMap} on the output constraints.
+   * @param inputMap {@link SSAMap} on the input constraints.
+   * @return Formula together with an updated output {@link SSAMap}
+   */
+  public PathFormula getFormulaApproximation(
+      FormulaManagerView manager, SSAMap outMap, SSAMap inputMap);
+
+}
