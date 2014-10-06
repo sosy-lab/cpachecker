b040014f9028746f5e6c70d8f4821b1a1b2ff3a3
George Karpenkov <true.cheshire@gmail.com>
2014-10-06 16:34:43 +0000

    Moved pretty-printing of the PolicyAbstractState
    to a separate class.
    
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@13800 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/cpa/stator/policy/PolicyAbstractState.java
+++ src/org/sosy_lab/cpachecker/cpa/stator/policy/PolicyAbstractState.java
@@ -1,16 +1,13 @@
 package org.sosy_lab.cpachecker.cpa.stator.policy;
 
-import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
 
-import org.sosy_lab.common.Pair;
 import org.sosy_lab.cpachecker.cfa.model.CFANode;
 import org.sosy_lab.cpachecker.core.interfaces.AbstractState;
 import org.sosy_lab.cpachecker.core.interfaces.Graphable;
-import org.sosy_lab.cpachecker.util.rationals.ExtendedRational;
 import org.sosy_lab.cpachecker.util.rationals.LinearExpression;
 
 import com.google.common.base.Optional;
@@ -81,7 +78,8 @@ public class PolicyAbstractState implements AbstractState,
    * @return {@link PolicyTemplateBound} for the given {@link LinearExpression}
    * or an empty optional if it is unbounded.
    */
-  public Optional<PolicyTemplateBound> getBound(LinearExpression e) {
+  public Optional<PolicyTemplateBound> getPolicyTemplateBound(
+      LinearExpression e) {
     return Optional.fromNullable(data.get(e));
   }
 
@@ -132,90 +130,7 @@ public class PolicyAbstractState implements AbstractState,
 
   @Override
   public String toDOTLabel() {
-    StringBuilder b = new StringBuilder();
-    b.append("\n");
-
-    // TODO: might want to do it as a pre-processing rather than
-    // only for printing.
-
-    // Convert to the readable format.
-    Map<LinearExpression, PolicyTemplateBound> toSort = new HashMap<>();
-
-    // Pretty-printing is tricky.
-    Map<LinearExpression, ExtendedRational> lessThan = new HashMap<>();
-    Map<LinearExpression, Pair<ExtendedRational, ExtendedRational>> bounded
-        = new HashMap<>();
-    Map<LinearExpression, ExtendedRational> equal = new HashMap<>();
-
-    toSort.putAll(data);
-    while (toSort.size() > 0) {
-      LinearExpression template, negTemplate;
-      template = toSort.keySet().iterator().next();
-      ExtendedRational upperBound = toSort.get(template).bound;
-
-      negTemplate = template.negate();
-
-      toSort.remove(template);
-
-      if (toSort.containsKey(negTemplate)) {
-        ExtendedRational lowerBound = toSort.get(negTemplate).bound.negate();
-        toSort.remove(negTemplate);
-
-        // Rotate the pair if necessary.
-        boolean negated = false;
-        if (template.toString().startsWith("-")) {
-          negated = true;
-        }
-
-        if (lowerBound.equals(upperBound)) {
-          if (negated) {
-            equal.put(template.negate(), lowerBound.negate());
-          } else {
-            equal.put(template, lowerBound);
-          }
-        } else {
-          if (negated) {
-            bounded.put(
-                template.negate(), Pair.of(upperBound.negate(), lowerBound.negate()));
-          } else {
-            bounded.put(
-                template, Pair.of(lowerBound, upperBound));
-          }
-        }
-      } else {
-        lessThan.put(template, upperBound);
-      }
-    }
-
-    // Print equals.
-    for (Entry<LinearExpression, ExtendedRational> entry : equal.entrySet()) {
-      b.append(entry.getKey())
-          .append("=")
-          .append(entry.getValue())
-          .append("\n");
-    }
-
-    // Print bounded.
-    for (Entry<LinearExpression, Pair<ExtendedRational, ExtendedRational>> entry
-        : bounded.entrySet()) {
-      b
-          .append(entry.getValue().getFirst())
-          .append("≤")
-          .append(entry.getKey())
-          .append("≤")
-          .append(entry.getValue().getSecond())
-          .append("\n");
-    }
-
-    // Print less-than.
-    for (Entry<LinearExpression, ExtendedRational> entry : lessThan.entrySet()) {
-      b.append(entry.getKey())
-          .append("≤")
-          .append(entry.getValue())
-          .append("\n");
-    }
-
-    return b.toString();
+    return (new PolicyDotWriter()).toDOTLabel(data);
   }
 
   @Override
