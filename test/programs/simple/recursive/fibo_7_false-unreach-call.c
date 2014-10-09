f48279df61c23f022c35da870e32b3e651614d74
friedber <friedber@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2014-10-09 11:47:50 +0000

    adding recursive examples
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/bamRecursion@13834 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

---
+++ test/programs/simple/recursive/fibo_7_false-unreach-call.c
@@ -0,0 +1,32 @@
+extern int __VERIFIER_nondet_int();
+extern void __VERIFIER_error();
+
+
+int fibo(int n) {
+    if (n < 1) {
+        return 0;
+    } else if (n == 1) {
+        return 1;
+    } else {
+        return fibo(n-1) + fibo(n-2);
+    }
+}
+
+// fibo 1-30
+// 1, 1, 2, 3, 5,
+// 8, 13, 21, 34, 55, 
+// 89, 144, 233, 377, 610,
+// 987, 1597, 2584, 4181, 6765,
+// 10946, 17711, 28657, 46368, 75025,
+// 121393, 196418, 317811, 514229, 832040
+
+int main() {
+    int x = 7;
+    int result = fibo(x);
+    if (result == 13) {
+        __VERIFIER_error();
+    }
+    return 0;
+}
+    
+
---
+++ test/programs/simple/recursive/sum_non_eq_true-unreach-call.c
@@ -0,0 +1,16 @@
+int sum(int n, int m) {
+    if (n == 0) {
+      return m;
+    } else {
+      return sum(n - 1, m + 1);
+    }
+}
+
+void main() {
+  int a;
+  int b;
+  int result = sum(a, b);
+  if (result != a + b) {
+    __VERIFIER_error();
+  }
+}
