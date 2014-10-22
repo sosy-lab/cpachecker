7dc876b882a265997420d80b864b4e1603b9c666
lemberge <lemberge@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2014-10-22 16:37:49 +0000

    Expand Java test programs for arrays and lazy boolean evaluation.
    Merge test programs for array initialization and array length into one program.
    Add code for testing correct handling of call by reference of Java arrays.
    
    In addition to code for testing casual lazy boolean statements, add code for different uses of the ternary operator and side effects.
    
    
    
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@14004 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- test/programs/java/Statements/Arrays_true_assert.java
+++ test/programs/java/Statements/Arrays_true_assert.java
@@ -7,5 +7,14 @@ public class Arrays_true_assert {
 
     assert numberArray[1][1] == 3;
     assert subArray[0] == 1;
+    assert numberArray[1].length == 2;
+
+    subArray[0] = 5;
+    assert numberArray[0][0] == 5;
+    
+    subArray = new int[5];
+    subArray[0] = 1;
+    assert numberArray[0][0] == 5;
+    assert subArray[0] == 1;
   }
 }
