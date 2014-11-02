f9214a58f40439067d35b0fea5b38768e20d973c
lemberge <lemberge@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2014-11-02 16:09:37 +0000

    Refactor Java test program: Divide different tests for type casts into methods.
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@14222 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- test/programs/java/Statements/Casts_true_assert.java
+++ test/programs/java/Statements/Casts_true_assert.java
@@ -1,10 +1,19 @@
 public class Casts_true_assert {
 
   public static void main(String[] args) {
-    int n1 = (int) 1.00001;
-    byte n2 = (byte) 257; // n2 = 1
+    checkFloatCast();
+    checkCastWithOverflow();
+  }
 
-    assert n1 == 1; //always true
-    assert n2 == 1; // always true
+  private static void checkFloatCast() {
+    int n = (int) 1.00001;
+
+    assert n == 1; //always true
+  }
+
+  private static void checkCastWithOverflow() {
+    byte n = (byte) 257; // n2 = 1
+
+    assert n == 1; // always true
   }
 }
