826f666553bfa1e9e2b423d6487d83cada9cd2a3
stieglma <stieglma@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2014-11-03 13:22:42 +0000

    Remove unnecessary parameter precision from some functions in BDD's PredicateManager.
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@14250 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/cpa/bdd/PredicateManager.java
+++ src/org/sosy_lab/cpachecker/cpa/bdd/PredicateManager.java
@@ -68,13 +68,12 @@ public class PredicateManager {
   private final NamedRegionManager rmgr;
 
   public PredicateManager(final Configuration config, final NamedRegionManager pRmgr,
-                          final BDDPrecision pPrecision, final CFA pCfa
-                          ) throws InvalidConfigurationException {
+                          final CFA pCfa) throws InvalidConfigurationException {
     config.inject(this);
     this.rmgr = pRmgr;
 
     if (initPartitions) {
-      initVars(pPrecision, pCfa);
+      initVars(pCfa);
     }
   }
 
@@ -96,7 +95,7 @@ public class PredicateManager {
    *  (later vars are deeper in the BDD).
    *  This function declares those vars in the beginning of the analysis,
    *  so that we can choose between some orders. */
-  protected void initVars(BDDPrecision precision, CFA cfa) {
+  protected void initVars(CFA cfa) {
     List<VariableClassification.Partition> partitions;
     if (initPartitionsOrdered) {
       BDDPartitionOrderer d = new BDDPartitionOrderer(cfa);
@@ -109,8 +108,7 @@ public class PredicateManager {
     MachineModel machineModel = cfa.getMachineModel();
     for (VariableClassification.Partition partition : partitions) {
       // maxBitSize is too much for most variables. we only create an order here, so this should not matter.
-      createPredicates(partition.getVars(), precision,
-              machineModel.getSizeofLongLongInt() * machineModel.getSizeofCharInBits());
+      createPredicates(partition.getVars(), machineModel.getSizeofLongLongInt() * machineModel.getSizeofCharInBits());
     }
   }
 
@@ -118,7 +116,7 @@ public class PredicateManager {
    *
    * The value 'bitsize' chooses how much bits are used for each var.
    * The varname is build as "varname@pos". */
-  public void createPredicates(final Collection<String> vars, final BDDPrecision precision, final int bitsize) {
+  public void createPredicates(final Collection<String> vars, final int bitsize) {
 
     assert bitsize >= 1 : "you need at least one bit for a variable.";
 
