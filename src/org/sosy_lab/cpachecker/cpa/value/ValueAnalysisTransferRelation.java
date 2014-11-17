sosy-lab/cpachecker

commit db2a6990bf8860600e9f6d82701c87d35a2ec0aa

Store the expected type of symbolic values.
git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@14438 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

src/org/sosy_lab/cpachecker/cpa/value/ValueAnalysisTransferRelation.java
@@ -540,6 +540,7 @@ public class ValueAnalysisTransferRelation extends ForwardingTransferRelation<Va
 
     ValueAnalysisState newElement = ValueAnalysisState.copyOf(state);
     AVariableDeclaration decl = (AVariableDeclaration) declaration;
+    Type declarationType = decl.getType();
 
     // get the variable name in the declarator
     String varName = decl.getName();
@@ -567,15 +568,15 @@ public class ValueAnalysisTransferRelation extends ForwardingTransferRelation<Va
     }
 
     if (addressedVariables.contains(decl.getQualifiedName())
-        && decl.getType() instanceof CType
-        && ((CType)decl.getType()).getCanonicalType() instanceof CPointerType) {
+        && declarationType instanceof CType
+        && ((CType) declarationType).getCanonicalType() instanceof CPointerType) {
       ValueAnalysisState.addToBlacklist(memoryLocation);
     }
 
     if (init instanceof AInitializerExpression) {
       ExpressionValueVisitor evv = getVisitor();
       IAExpression exp = ((AInitializerExpression) init).getExpression();
-      initialValue = getExpressionValue(exp, decl.getType(), evv);
+      initialValue = getExpressionValue(exp, declarationType, evv);
 
       if (isMissingCExpressionInformation(evv, exp)) {
         addMissingInformation(memoryLocation, exp);
@@ -583,14 +584,14 @@ public class ValueAnalysisTransferRelation extends ForwardingTransferRelation<Va
     }
 
     if (initialValue.isUnknown()) {
-      initialValue = SymbolicIdentifier.getInstance();
+      initialValue = SymbolicIdentifier.getInstance(declarationType);
     }
 
     if (isTrackedField(decl, initialValue)) {
       if (missingFieldVariableObject) {
         fieldNameAndInitialValue = Pair.of(varName, initialValue);
       } else if (missingInformationRightJExpression == null) {
-        newElement.assignConstant(memoryLocation, initialValue, decl.getType());
+        newElement.assignConstant(memoryLocation, initialValue, declarationType);
       } else {
         missingInformationLeftJVariable = memoryLocation.getAsSimpleString();
       }
@@ -855,17 +856,17 @@ public class ValueAnalysisTransferRelation extends ForwardingTransferRelation<Va
 //      newElement.forgetAllWithPrefix(assignedVar + ".");
 //      newElement.forgetAllWithPrefix(assignedVar + "->");
 
-      if (value.isUnknown()) {
-        // Don't erase it when there if it has yet to be evaluated
-        if (missingInformationRightJExpression == null) {
-          // TODO HasToBeErased Later
-         newElement.forget(assignedVar);
-        }
-      } else {
-        newElement.assignConstant(assignedVar, value, lType);
+      // if there is no information left to evaluate but the value is unknown, we assign a symbolic
+      // identifier to keep track of the variable.
+      if (value.isUnknown() && missingInformationRightJExpression == null) {
+        value = SymbolicIdentifier.getInstance(lType);
       }
 
+      if (!value.isUnknown()) {
+        newElement.assignConstant(assignedVar, value, lType);
+      }
     }
+
     return newElement;
   }
 

src/org/sosy_lab/cpachecker/cpa/value/type/SymbolicIdentifier.java
@@ -23,6 +23,7 @@
  */
 package org.sosy_lab.cpachecker.cpa.value.type;
 
+import org.sosy_lab.cpachecker.cfa.types.Type;
 import org.sosy_lab.cpachecker.cfa.types.c.CType;
 
 /**
@@ -52,9 +53,11 @@ public class SymbolicIdentifier implements SymbolicValue {
 
   // this objects unique id for identifying it
   private final long id;
+  private final Type type;
 
-  protected SymbolicIdentifier(long pId) {
+  protected SymbolicIdentifier(long pId, Type pType) {
     id = pId;
+    type = pType;
   }
 
   /**
@@ -64,8 +67,8 @@ public class SymbolicIdentifier implements SymbolicValue {
    *
    * @return a new instance of a <code>SymbolicIdentifier</code>
    */
-  public static SymbolicIdentifier getInstance() {
-    return new SymbolicIdentifier(nextId++);
+  public static SymbolicIdentifier getInstance(Type pType) {
+    return new SymbolicIdentifier(nextId++, pType);
   }
 
   @Override

commit 2206d5af4d2e982aaf21e4cac49826485786e379

Remove memory locations with unknown values from the state in Value Analysis of C programs.
This fixes a bug introduced in r14438.




git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@14455 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

src/org/sosy_lab/cpachecker/cpa/value/ValueAnalysisTransferRelation.java
@@ -876,8 +876,12 @@ public class ValueAnalysisTransferRelation extends ForwardingTransferRelation<Va
 
       // if there is no information left to evaluate but the value is unknown, we assign a symbolic
       // identifier to keep track of the variable.
-      if (value.isUnknown() && lType instanceof JType && missingInformationRightJExpression == null) {
-        value = getSymbolicIdentifier(lType);
+      if (value.isUnknown() && missingInformationRightJExpression == null) {
+        if (lType instanceof JType) {
+          value = getSymbolicIdentifier(lType);
+        } else {
+          newElement.forget(assignedVar);
+        }
       }
 
       if (!value.isUnknown()) {