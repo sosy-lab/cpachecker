22120377f322b108213c1315d6671d3eb7b31f83
stahlbauer-grammatech <stahlbauer-grammatech@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2014-12-15 17:12:10 +0000

    A CPA for partitioning the state space... less code than expected.
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/arrays@14945 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

---
+++ src/org/sosy_lab/cpachecker/cpa/partitioning/PartitioningCPA.java
@@ -0,0 +1,107 @@
+/*
+ *  CPAchecker is a tool for configurable software verification.
+ *  This file is part of CPAchecker.
+ *
+ *  Copyright (C) 2007-2014  Dirk Beyer
+ *  All rights reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *
+ *  CPAchecker web page:
+ *    http://cpachecker.sosy-lab.org
+ */
+package org.sosy_lab.cpachecker.cpa.partitioning;
+
+import org.sosy_lab.cpachecker.cfa.model.CFANode;
+import org.sosy_lab.cpachecker.core.defaults.FlatLatticeDomain;
+import org.sosy_lab.cpachecker.core.defaults.IdentityTransferRelation;
+import org.sosy_lab.cpachecker.core.defaults.MergeJoinOperator;
+import org.sosy_lab.cpachecker.core.defaults.SingletonPrecision;
+import org.sosy_lab.cpachecker.core.defaults.StaticPrecisionAdjustment;
+import org.sosy_lab.cpachecker.core.defaults.StopSepOperator;
+import org.sosy_lab.cpachecker.core.interfaces.AbstractDomain;
+import org.sosy_lab.cpachecker.core.interfaces.AbstractState;
+import org.sosy_lab.cpachecker.core.interfaces.ConfigurableProgramAnalysis;
+import org.sosy_lab.cpachecker.core.interfaces.MergeOperator;
+import org.sosy_lab.cpachecker.core.interfaces.Precision;
+import org.sosy_lab.cpachecker.core.interfaces.PrecisionAdjustment;
+import org.sosy_lab.cpachecker.core.interfaces.StateSpacePartition;
+import org.sosy_lab.cpachecker.core.interfaces.StopOperator;
+import org.sosy_lab.cpachecker.core.interfaces.TransferRelation;
+
+
+public class PartitioningCPA implements ConfigurableProgramAnalysis {
+
+  private final AbstractDomain abstractDomain = new FlatLatticeDomain();
+  private final StopOperator stopOperator = new StopSepOperator(abstractDomain);
+  private final MergeOperator mergeOperator = new MergeJoinOperator(abstractDomain);
+  private final TransferRelation transferRelation = IdentityTransferRelation.INSTANCE;
+
+  public static class PartitionState implements AbstractState {
+    private final Object partition;
+
+    public PartitionState(Object pPartition) {
+      this.partition = pPartition;
+    }
+
+    @Override
+    public int hashCode() {
+      final int prime = 31;
+      int result = 1;
+      result = prime * result + ((partition == null) ? 0 : partition.hashCode());
+      return result;
+    }
+
+    @Override
+    public boolean equals(Object pObj) {
+      return ((PartitionState) pObj).partition.equals(this.partition);
+    }
+  }
+
+  @Override
+  public AbstractDomain getAbstractDomain() {
+    return abstractDomain;
+  }
+
+  @Override
+  public TransferRelation getTransferRelation() {
+    return transferRelation;
+  }
+
+  @Override
+  public MergeOperator getMergeOperator() {
+    return mergeOperator;
+  }
+
+  @Override
+  public StopOperator getStopOperator() {
+    return stopOperator;
+  }
+
+  @Override
+  public PrecisionAdjustment getPrecisionAdjustment() {
+    return StaticPrecisionAdjustment.getInstance();
+  }
+
+  @Override
+  public AbstractState getInitialState(CFANode pNode, StateSpacePartition pPartition) {
+    return new PartitionState(pPartition);
+  }
+
+  @Override
+  public Precision getInitialPrecision(CFANode pNode, StateSpacePartition pPartition) {
+    return SingletonPrecision.getInstance();
+  }
+
+}
