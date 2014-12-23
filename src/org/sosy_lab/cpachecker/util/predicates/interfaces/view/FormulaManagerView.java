sosy-lab/cpachecker

commit 6985363e64f315be8f3c39efeb65d09d1d06fd54

Added a parameter to myExtractAtoms so that it does not uninstantiate the atoms.
git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@15122 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

src/org/sosy_lab/cpachecker/util/predicates/interfaces/view/FormulaManagerView.java
@@ -1015,12 +1015,17 @@ public class FormulaManagerView {
 
   public Collection<BooleanFormula> extractAtoms(BooleanFormula f, boolean splitArithEqualities, boolean conjunctionsOnly) {
     return unwrapFormulasOfList(
-        myExtractAtoms(f, splitArithEqualities, conjunctionsOnly, FormulaStructure.ATOM));
+        myExtractAtoms(f, splitArithEqualities, conjunctionsOnly, FormulaStructure.ATOM, false));
   }
 
-  public Collection<BooleanFormula> extractLiterals(BooleanFormula f, boolean splitArithEqualities, boolean conjunctionsOnly) {
+  public Collection<BooleanFormula> extractLiterals(
+      BooleanFormula f,
+      boolean splitArithEqualities,
+      boolean conjunctionsOnly,
+      boolean uninstanciate) {
+
     return unwrapFormulasOfList(
-        myExtractAtoms(f, splitArithEqualities, conjunctionsOnly, FormulaStructure.LITERAL));
+        myExtractAtoms(f, splitArithEqualities, conjunctionsOnly, FormulaStructure.LITERAL, uninstanciate));
   }
 
   private List<BooleanFormula> unwrapFormulasOfList(Collection<BooleanFormula> unwrapped) {
@@ -1042,7 +1047,7 @@ public class FormulaManagerView {
   }
 
   private Collection<BooleanFormula> myExtractAtoms(BooleanFormula f, boolean splitArithEqualities,
-      boolean conjunctionsOnly, FormulaStructure breakdownTo) {
+      boolean conjunctionsOnly, FormulaStructure breakdownTo, boolean uninstanciate) {
     Set<BooleanFormula> handled = new HashSet<>();
     List<BooleanFormula> atoms = new ArrayList<>();
 
@@ -1069,7 +1074,9 @@ public class FormulaManagerView {
               && (ttStructure == FormulaStructure.ATOM));
 
       if (isSmallesConsidered) {
-        tt = myUninstantiate(tt);
+        if (uninstanciate) {
+          tt = myUninstantiate(tt);
+        }
 
         if (splitArithEqualities
             && myIsPurelyArithmetic(tt)) {
@@ -1108,7 +1115,10 @@ public class FormulaManagerView {
           && !(booleanFormulaManager.isNot(tt) || booleanFormulaManager.isAnd(tt))) {
         // conjunctions only, but formula is neither "not" nor "and"
         // treat this as atomic
-        atoms.add(myUninstantiate(tt));
+        if (uninstanciate) {
+          tt = myUninstantiate(tt);
+        }
+        atoms.add(tt);
 
       } else {
         // ok, go into this formula

commit b3f39dedd71b8ab7727af745660324021e6afb97

Fixed a bug that was introduced with r15122. Extracted atoms must be uninstantiated!
git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@15130 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

src/org/sosy_lab/cpachecker/util/predicates/interfaces/view/FormulaManagerView.java
@@ -1034,9 +1034,13 @@ public class FormulaManagerView {
     return result;
   }
 
+  /**
+   * Extract all atoms of a given boolean formula.
+   * All atoms get un-instantiated!!
+   */
   public Collection<BooleanFormula> extractAtoms(BooleanFormula f, boolean splitArithEqualities, boolean conjunctionsOnly) {
     return unwrapFormulasOfList(
-        myExtractAtoms(f, splitArithEqualities, conjunctionsOnly, FormulaStructure.ATOM, false));
+        myExtractAtoms(f, splitArithEqualities, conjunctionsOnly, FormulaStructure.ATOM, true));
   }
 
   public Collection<BooleanFormula> extractLiterals(