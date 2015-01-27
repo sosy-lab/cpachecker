ddf42d6d6b85ef37f08e440459b1069eb377890f
mcjakobs <mcjakobs@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2015-01-27 07:09:02 +0000

    add method to build path formula implication check for PCC
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@15500 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/util/predicates/interfaces/PathFormulaManager.java
+++ src/org/sosy_lab/cpachecker/util/predicates/interfaces/PathFormulaManager.java
@@ -113,4 +113,19 @@ public interface PathFormulaManager {
   public Formula expressionToFormula(PathFormula pFormula,
       CIdExpression expr,
       CFAEdge edge) throws UnrecognizedCCodeException;
+
+  /**
+   * Builds test for PCC that pF1 is covered by more abstract path formula pF2.
+   * Assumes that the SSA indices of pF1 are smaller or equal than those of pF2.
+   * Since pF1 may be merged with other path formulas resulting in pF2, needs to
+   * add assumptions about the connection between indexed variables as included by
+   * {@link PathFormulaManager#makeOr(PathFormula, PathFormula)}. Returns negation of
+   * implication to check if it is unsatisfiable (implication is valid).
+   *
+   * @param pF1 path formula which should be covered
+   * @param pF2 path formula which covers
+   * @return pF1.getFormula() and assumptions and not pF2.getFormula()
+   * @throws InterruptedException
+   */
+  public BooleanFormula buildImplicationTestAsUnsat(PathFormula pF1, PathFormula pF2) throws InterruptedException;
 }
