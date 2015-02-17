0e58a7955b9e18cb7ea4063fc427fd1654b3e5b5
lemberge <lemberge@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2015-02-17 16:09:50 +0000

    Use correct variable in assert
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/symbolic@15707 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/cpa/constraints/ConstraintsState.java
+++ src/org/sosy_lab/cpachecker/cpa/constraints/ConstraintsState.java
@@ -305,7 +305,7 @@ public class ConstraintsState extends ForwardingSet<Constraint> implements Latti
         SymbolicIdentifier identifier = toSymbolicIdentifier(term.getName());
         Value concreteValue = convertToValue(termAssignment, term.getType());
 
-        assert !definiteAssignment.containsKey(identifier) || definiteAssignment.get(identifier).equals(termAssignment);
+        assert !definiteAssignment.containsKey(identifier) || definiteAssignment.get(identifier).equals(concreteValue);
         if (!definiteAssignment.containsKey(identifier)
             && isOnlySatisfyingAssignment(term, termAssignment)) {
 
