da75f5bfaa84d29903f3f0de0006c591e7fb51ab
stieglma <stieglma@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2015-03-02 09:33:20 +0000

    Refactoring for unnecessary calling a method twice.
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@15927 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/cfa/parser/eclipse/c/ASTConverter.java
+++ src/org/sosy_lab/cpachecker/cfa/parser/eclipse/c/ASTConverter.java
@@ -1522,7 +1522,9 @@ class ASTConverter {
         if (initializer != null) {
           throw new CFAGenerationRuntimeException("Typedef with initializer", d, niceFileNameFunction);
         }
-        return new CTypeDefDeclaration(fileLoc, isGlobal, type, scope.getFileSpecificTypeName(name), scope.getFileSpecificTypeName(scope.createScopedNameOf(name)));
+
+        name = scope.getFileSpecificTypeName(name);
+        return new CTypeDefDeclaration(fileLoc, isGlobal, type, name, scope.createScopedNameOf(name));
       }
 
       // We need to resolve typedefs, but we cannot call getCanonicalType()
