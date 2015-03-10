sosy-lab/cpachecker

commit a0d76564a8007e957e6bb755a66db7965c1ddb09

Changed CFASecondPassBuilder such that the argument of __VERIFIER_assume() is (like other assume expressions in if clauses) converted to a BinaryExpression with a logial operator (if there is not already one, we just append != 0)
git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@16075 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

src/org/sosy_lab/cpachecker/cfa/CFASecondPassBuilder.java
@@ -32,14 +32,18 @@ import org.sosy_lab.common.configuration.InvalidConfigurationException;
 import org.sosy_lab.common.configuration.Option;
 import org.sosy_lab.common.configuration.Options;
 import org.sosy_lab.common.log.LogManager;
+import org.sosy_lab.cpachecker.cfa.ast.ADeclaration;
+import org.sosy_lab.cpachecker.cfa.ast.AExpression;
 import org.sosy_lab.cpachecker.cfa.ast.AFunctionCall;
 import org.sosy_lab.cpachecker.cfa.ast.AFunctionCallAssignmentStatement;
 import org.sosy_lab.cpachecker.cfa.ast.AFunctionCallExpression;
 import org.sosy_lab.cpachecker.cfa.ast.FileLocation;
-import org.sosy_lab.cpachecker.cfa.ast.ADeclaration;
-import org.sosy_lab.cpachecker.cfa.ast.AExpression;
+import org.sosy_lab.cpachecker.cfa.ast.c.CBinaryExpression;
+import org.sosy_lab.cpachecker.cfa.ast.c.CBinaryExpression.BinaryOperator;
+import org.sosy_lab.cpachecker.cfa.ast.c.CBinaryExpressionBuilder;
 import org.sosy_lab.cpachecker.cfa.ast.c.CExpression;
 import org.sosy_lab.cpachecker.cfa.ast.c.CFunctionCall;
+import org.sosy_lab.cpachecker.cfa.ast.c.CIntegerLiteralExpression;
 import org.sosy_lab.cpachecker.cfa.ast.java.JMethodOrConstructorInvocation;
 import org.sosy_lab.cpachecker.cfa.model.AStatementEdge;
 import org.sosy_lab.cpachecker.cfa.model.AssumeEdge;
@@ -309,6 +313,13 @@ public class CFASecondPassBuilder {
     }
 
     CExpression assumeExp = (CExpression)f.getParameterExpressions().get(0);
+
+    if (!(assumeExp instanceof CBinaryExpression && !((CBinaryExpression)assumeExp).getOperator().isLogicalOperator())) {
+      assumeExp = new CBinaryExpressionBuilder(cfa.getMachineModel(), logger)
+                      .buildBinaryExpressionUnchecked(assumeExp,
+                                                      CIntegerLiteralExpression.ZERO,
+                                                      BinaryOperator.NOT_EQUALS);
+    }
 
     AssumeEdge trueEdge = new CAssumeEdge(edge.getRawStatement(), edge.getFileLocation(),
         edge.getPredecessor(), edge.getSuccessor(), assumeExp, true);

commit 2279145abadccce06df1334c09fa2482721f5e21

Fix bug in condition introduced in r16075.
git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@16078 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

src/org/sosy_lab/cpachecker/cfa/CFASecondPassBuilder.java
@@ -314,7 +314,7 @@ public class CFASecondPassBuilder {
 
     CExpression assumeExp = (CExpression)f.getParameterExpressions().get(0);
 
-    if (!(assumeExp instanceof CBinaryExpression && !((CBinaryExpression)assumeExp).getOperator().isLogicalOperator())) {
+    if (!(assumeExp instanceof CBinaryExpression && ((CBinaryExpression)assumeExp).getOperator().isLogicalOperator())) {
       assumeExp = new CBinaryExpressionBuilder(cfa.getMachineModel(), logger)
                       .buildBinaryExpressionUnchecked(assumeExp,
                                                       CIntegerLiteralExpression.ZERO,