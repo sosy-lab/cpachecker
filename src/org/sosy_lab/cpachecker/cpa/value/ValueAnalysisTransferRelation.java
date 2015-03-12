sosy-lab/cpachecker

commit 44569bf5c6587ee4e732b8cee944071f285c957f

ValueAnalysisTransferRelation drops frame only at blank edge of function exit node, not also in return statement edge.
Simplification of some expressions, also.



git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@16176 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

src/org/sosy_lab/cpachecker/cpa/value/ValueAnalysisTransferRelation.java
@@ -324,29 +324,28 @@ public class ValueAnalysisTransferRelation extends ForwardingTransferRelation<Va
   protected ValueAnalysisState handleReturnStatementEdge(AReturnStatementEdge returnEdge)
       throws UnrecognizedCCodeException {
 
-    // visitor must use the initial (previous) state, because there we have all information about variables
     ExpressionValueVisitor evv = new ExpressionValueVisitor(state, functionName, machineModel, logger);
-
-    // clone state, because will be changed through removing all variables of current function's scope
-    state = ValueAnalysisState.copyOf(state);
-    state.dropFrame(functionName);
 
     AExpression expression = returnEdge.getExpression().orNull();
     if (expression == null && returnEdge instanceof CReturnStatementEdge) {
       expression = CIntegerLiteralExpression.ZERO; // this is the default in C
     }
 
-    FunctionEntryNode functionEntryNode = returnEdge.getSuccessor().getEntryNode();
+    final FunctionEntryNode functionEntryNode = returnEdge.getSuccessor().getEntryNode();
 
+    final Optional<? extends AVariableDeclaration> optionalReturnVarDeclaration
+        = functionEntryNode.getReturnVariable();
     MemoryLocation functionReturnVar = null;
-    if(functionEntryNode.getReturnVariable().isPresent()) {
-      functionReturnVar = MemoryLocation.valueOf(functionEntryNode.getReturnVariable().get().getQualifiedName());
+
+    if(optionalReturnVarDeclaration.isPresent()) {
+      functionReturnVar = MemoryLocation.valueOf(optionalReturnVarDeclaration.get().getQualifiedName());
     }
 
     if (expression != null && functionReturnVar != null) {
+      final Type functionReturnType = functionEntryNode.getFunctionDefinition().getType().getReturnType();
 
       return handleAssignmentToVariable(functionReturnVar,
-          functionEntryNode.getFunctionDefinition().getType().getReturnType(), // TODO easier way to get type?
+          functionReturnType,
           expression,
           evv);
     } else {

commit 2348ed0d19d3fa2f8f3eb7875b58d05d90178543

ValueAnalysisTransferRelation drops frame after handling of FunctionReturnEdge.
This fixes a bug introduced with r16176.



git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@16196 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

src/org/sosy_lab/cpachecker/cpa/value/ValueAnalysisTransferRelation.java
@@ -438,10 +438,9 @@ public class ValueAnalysisTransferRelation extends ForwardingTransferRelation<Va
       }
     }
 
-    if(returnVarName.isPresent()) {
-      newElement.forget(functionReturnVar);
-    }
+    newElement.dropFrame(functionName);
 
+    assert !returnVarName.isPresent() || !newElement.contains(functionReturnVar);
     return newElement;
   }
 