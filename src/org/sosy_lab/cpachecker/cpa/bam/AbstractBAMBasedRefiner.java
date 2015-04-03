9c20d26e4b6d14ffce668a19504bc2d195c1fa08
friedber <friedber@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2015-04-03 12:05:02 +0000

    re-enable support for recursion in BAM+Value.
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@16538 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/cpa/bam/AbstractBAMBasedRefiner.java
+++ src/org/sosy_lab/cpachecker/cpa/bam/AbstractBAMBasedRefiner.java
@@ -152,7 +152,9 @@ public abstract class AbstractBAMBasedRefiner extends AbstractARGBasedRefiner {
       @Nullable
       @Override
       public Precision apply(@Nullable AbstractState state) {
-        return transfer.getPrecisionForState(Preconditions.checkNotNull(subgraphStatesToReachedState.get(state)), delegate.asReachedSet());
+        return delegate.asReachedSet().getPrecision(delegate.asReachedSet().getLastState());
+        // TODO do we really need the target-precision for refinements and not the actual one?
+        // return transfer.getPrecisionForState(Preconditions.checkNotNull(subgraphStatesToReachedState.get(state)), delegate.asReachedSet());
       }
     };
 
@@ -168,13 +170,6 @@ public abstract class AbstractBAMBasedRefiner extends AbstractARGBasedRefiner {
 
     @Override
     public UnmodifiableReachedSet asReachedSet() {
-      if (true) {
-        return delegate.asReachedSet();
-      }
-
-      // TODO there is a bug with precision handling in BAM-PredicateAnalysis,
-      //      when using the new ReachedSet instead of only the MainReachedSet.
-      //      However for ValuaAnalysis-refinement we need the new ReachedSet.
       return new UnmodifiableReachedSet() {
         @Override
         public Collection<AbstractState> asCollection() {
