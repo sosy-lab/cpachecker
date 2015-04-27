2277d9993a2df4df2b5e4f678966673afaacde6a
stieglma <stieglma@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2015-04-27 14:31:07 +0000

    re-added the possibility of compiling the z3 library with FOCI for interpolation to the compile script
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/z3interpol@16740 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- lib/native/source/libz3j/compile.sh
+++ lib/native/source/libz3j/compile.sh
@@ -14,26 +14,35 @@ set -o errexit
 set -o pipefail
 
 JNI_HEADERS="$(../get_jni_headers.sh)"
+OUTFILENAME="libz3j"
+Z3_SO_FILENAME="libz3"
 
 if [[ `uname` == CYGWIN* ]] ; then
-    OUTFILENAME="libz3j.dll"
-    Z3_SO_FILENAME="libz3.dll"
+    FILE_SUFFIX=".dll"
     # the 64bit environment of Cygwin is not yet stable. We have to use a cross compiler.
     COMPILER_CMD="./gcc64/bin/x86_64-pc-mingw32-gcc.exe"
-    LIB_SHARING="-shared"
 else
-    OUTFILENAME="libz3j.so"
-    Z3_SO_FILENAME="libz3.so"
+    FILE_SUFFIX=".so"
     COMPILER_CMD="gcc"
-    LIB_SHARING="-shared"
 fi
+
 Z3_LIBNAME="-lz3"
-CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 Z3_DIR="$1"
 Z3_SRC_DIR="$Z3_DIR"/src/api
 Z3_LIB_DIR="$Z3_DIR"/build
-if [ ! -f "$Z3_LIB_DIR/$Z3_SO_FILENAME" ]; then
+
+USE_FOCI="$2"
+
+if [[ $USE_FOCI == "-interp" ]] ; then
+  echo "Using interpolation with external library FOCI"
+  OUTFILENAME="${OUTFILENAME}_interp"
+  Z3_LIBNAME="${Z3_LIBNAME}_interp"
+  cp ${Z3_LIB_DIR}/${Z3_SO_FILENAME}${FILE_SUFFIX} ${Z3_LIB_DIR}/${Z3_SO_FILENAME}_interp${FILE_SUFFIX}
+  Z3_SO_FILENAME="${Z3_SO_FILENAME}_interp"
+fi
+
+if [ ! -f "${Z3_LIB_DIR}/${Z3_SO_FILENAME}${FILE_SUFFIX}" ]; then
 	echo "You need to specify the directory with the successfully built Z3 on the command line!"
 	exit 1
 fi
@@ -57,29 +66,29 @@ if [[ `uname` == CYGWIN* ]] ; then
     echo "A failure in the following steps might be due to a wrong compiler environment. Has libz3.dll been compiled for 64bit?"
 fi
 
-$COMPILER_CMD -Wall -g -o $OUTFILENAME $LIB_SHARING -Wl,-soname,$OUTFILENAME -Wl,-rpath,'$ORIGIN' -L. -L$Z3_LIB_DIR -L$Z3_DIR org_sosy_lab_cpachecker_util_predicates_z3_Z3NativeApi.o $Z3_LIBNAME
+$COMPILER_CMD -Wall -g -o ${OUTFILENAME}${FILE_SUFFIX} -shared -Wl,-soname,${OUTFILENAME}${FILE_SUFFIX} -Wl,-rpath,'$ORIGIN' -L. -L$Z3_LIB_DIR -L$Z3_DIR org_sosy_lab_cpachecker_util_predicates_z3_Z3NativeApi.o $Z3_LIBNAME
 
 
 if [[ ! `uname` == CYGWIN* ]] ; then
 
     if [ $? -eq 0 ]; then
-    	strip $OUTFILENAME
+    	strip ${OUTFILENAME}$FILE_SUFFIX
     else
     	echo "There was a problem during compilation of \"org_sosy_lab_cpachecker_util_predicates_z3_Z3NativeApi.c\""
     	exit 1
     fi
 
-    MISSING_SYMBOLS="$(readelf -Ws $OUTFILENAME | grep NOTYPE | grep GLOBAL | grep UND)"
+    MISSING_SYMBOLS="$(readelf -Ws ${OUTFILENAME}$FILE_SUFFIX | grep NOTYPE | grep GLOBAL | grep UND)"
     if [ ! -z "$MISSING_SYMBOLS" ]; then
     	echo "Warning: There are the following unresolved dependencies in libz3j.so:"
-    	readelf -Ws $OUTFILENAME | grep NOTYPE | grep GLOBAL | grep UND
+    	readelf -Ws ${OUTFILENAME}$FILE_SUFFIX | grep NOTYPE | grep GLOBAL | grep UND
     	exit 1
     fi
 
-    MISSING_SYMBOLS="$(readelf -Ws $Z3_LIB_DIR/$Z3_SO_FILENAME | grep NOTYPE | grep GLOBAL | grep UND)"
+    MISSING_SYMBOLS="$(readelf -Ws $Z3_LIB_DIR/${Z3_SO_FILENAME}$FILE_SUFFIX | grep NOTYPE | grep GLOBAL | grep UND)"
     if [ ! -z "$MISSING_SYMBOLS" ]; then
     	echo "Warning: There are the following unresolved dependencies in libz3.so:"
-    	readelf -Ws $Z3_LIB_DIR/$Z3_SO_FILENAME | grep NOTYPE | grep GLOBAL | grep UND
+    	readelf -Ws $Z3_LIB_DIR/${Z3_SO_FILENAME}$FILE_SUFFIX | grep NOTYPE | grep GLOBAL | grep UND
     	exit 1
     fi
 fi
