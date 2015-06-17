c9612cd046db30ffef288997f3c17d49b425a987
mcjakobs <mcjakobs@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2015-06-17 08:16:23 +0000

    implement hashCode, equals for CVoidType required for PCC and predicate analysis
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@17066 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/cfa/types/c/CVoidType.java
+++ src/org/sosy_lab/cpachecker/cfa/types/c/CVoidType.java
@@ -106,4 +106,34 @@ public final class CVoidType implements CType, Serializable {
   public CVoidType getCanonicalType(boolean pForceConst, boolean pForceVolatile) {
     return create(isConst || pForceConst, isVolatile || pForceVolatile);
   }
+
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + (isConst ? 1231 : 1237);
+    result = prime * result + (isVolatile ? 1231 : 1237);
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) {
+      return true;
+    }
+    if (obj == null) {
+      return false;
+    }
+    if (getClass() != obj.getClass()) {
+      return false;
+    }
+    CVoidType other = (CVoidType) obj;
+    if (isConst != other.isConst) {
+      return false;
+    }
+    if (isVolatile != other.isVolatile) {
+      return false;
+    }
+    return true;
+  }
 }
