064e66bf34f45a6098a6f5711bfd2d8c219aa7fe
lemberge <lemberge@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2015-06-25 12:11:58 +0000

    SymbolicValues.getPossibleAliases(..) always returns all possible aliases.
    Before, the identity function was the only possible 'alias function' when using the subset less-or-equal operator in ConstraintsCPA.
    It is not necessary to do so, though, since the subset operator in the ConstraintsCPA prevents a positive termination check in case
    the symbolic identifiers _in use_ are not the same.
    
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/symbolic-cegar@17154 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/cpa/value/symbolic/util/SymbolicValues.java
+++ src/org/sosy_lab/cpachecker/cpa/value/symbolic/util/SymbolicValues.java
@@ -27,7 +27,6 @@ import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
 
-import org.sosy_lab.cpachecker.core.counterexample.Memory;
 import org.sosy_lab.cpachecker.cpa.constraints.ConstraintsCPA.ComparisonType;
 import org.sosy_lab.cpachecker.cpa.value.symbolic.type.BinarySymbolicExpression;
 import org.sosy_lab.cpachecker.cpa.value.symbolic.type.ConstantSymbolicExpression;
@@ -50,17 +49,7 @@ public class SymbolicValues {
   private static SymbolicIdentifierLocator identifierLocator;
 
   public static void initialize(final ComparisonType pLessOrEqualComparison) {
-    switch (pLessOrEqualComparison) {
-      case SUBSET:
-      case IMPLICATION:
-        aliasCreator = new IdentityAliasCreator();
-        break;
-      case ALIASED_SUBSET:
-        aliasCreator = new RealAliasCreator();
-        break;
-      default:
-        throw new AssertionError("Unhandled comparison type: " + pLessOrEqualComparison);
-    }
+    aliasCreator = new RealAliasCreator();
 
     identifierLocator = SymbolicIdentifierLocator.getInstance();
   }
