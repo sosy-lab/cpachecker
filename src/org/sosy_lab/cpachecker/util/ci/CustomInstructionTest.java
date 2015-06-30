2e5653af6d935c61f491c204fd0e421a66418d56
mcjakobs <mcjakobs@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2015-06-30 13:27:43 +0000

    rewrote test
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@17188 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/util/ci/CustomInstructionTest.java
+++ src/org/sosy_lab/cpachecker/util/ci/CustomInstructionTest.java
@@ -26,34 +26,36 @@ package org.sosy_lab.cpachecker.util.ci;
 import java.io.IOException;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
-import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Queue;
-import java.util.Set;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.sosy_lab.common.Pair;
 import org.sosy_lab.common.ShutdownNotifier;
 import org.sosy_lab.cpachecker.cfa.CFA;
 import org.sosy_lab.cpachecker.cfa.model.CFAEdge;
 import org.sosy_lab.cpachecker.cfa.model.CFANode;
 import org.sosy_lab.cpachecker.core.interfaces.AbstractState;
 import org.sosy_lab.cpachecker.cpa.arg.ARGState;
+import org.sosy_lab.cpachecker.cpa.callstack.CallstackState;
 import org.sosy_lab.cpachecker.cpa.location.LocationState;
 import org.sosy_lab.cpachecker.exceptions.CPAException;
 import org.sosy_lab.cpachecker.exceptions.ParserException;
 import org.sosy_lab.cpachecker.util.CFAUtils;
+import org.sosy_lab.cpachecker.util.predicates.pathformula.SSAMap;
 import org.sosy_lab.cpachecker.util.test.TestDataTools;
 
 import com.google.common.truth.Truth;
 
 public class CustomInstructionTest {
   private CustomInstructionApplications cia;
+  private AppliedCustomInstruction aci;
   private Map<CFANode, AppliedCustomInstruction> cis;
   private CustomInstruction ci;
   private Constructor<?extends AbstractState> locConstructor;
@@ -64,14 +66,14 @@ public class CustomInstructionTest {
   private ARGState end;
 
   @Before
-  public void init() throws NoSuchMethodException, SecurityException, InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, AppliedCustomInstructionParsingFailedException, IOException, ParserException, InterruptedException {
-    // TestDataTools nutzen um CFA zu bekommen
+  public void init() throws NoSuchMethodException, SecurityException, InstantiationException, IllegalAccessException,
+      IllegalArgumentException, InvocationTargetException, AppliedCustomInstructionParsingFailedException, IOException,
+      ParserException, InterruptedException {
     String testProgram = ""
         + "void main(int a){"
-          + "return a;"
+        + "int a;"
+        + "if(a>0) {a=a+1;} else {a=a-1;}"
         + "}";
-    cfa = null;
-    new TestDataTools();
     cfa = TestDataTools.makeCFA(testProgram);
 
     locConstructor = LocationState.class.getDeclaredConstructor(CFANode.class, boolean.class);
@@ -79,20 +81,8 @@ public class CustomInstructionTest {
 
     startNode = cfa.getMainFunction();
     endNodes = new HashSet<>();
-
-    Queue<CFANode> queue = new ArrayDeque<>();
-    queue.add(startNode);
-    Set<CFANode> visitedNode = new HashSet<>();
-    while (!queue.isEmpty()) {
-      CFANode tmp = queue.poll();
-      if (tmp.getNumLeavingEdges()==0) {
-        endNodes.add(tmp);
-      } else if (!visitedNode.contains(tmp)) {
-        visitedNode.add(tmp);
-        for (CFAEdge edge : CFAUtils.allLeavingEdges(tmp)) {
-          queue.add(edge.getSuccessor());
-        }
-      }
+    for (CFAEdge edge : CFAUtils.allEnteringEdges(cfa.getMainFunction().getExitNode())) {
+      endNodes.add(edge.getPredecessor());
     }
 
     List<String> input = new ArrayList<>();
@@ -102,67 +92,81 @@ public class CustomInstructionTest {
     ci = new CustomInstruction(startNode, endNodes, input, output, ShutdownNotifier.create());
 
     cis = new HashMap<>();
-    AppliedCustomInstruction aci = null;
-    try {
-      aci = ci.inspectAppliedCustomInstruction(startNode);
-    } catch (AppliedCustomInstructionParsingFailedException | InterruptedException e) {
-      // TODO
-    }
+    aci = new AppliedCustomInstruction(startNode, endNodes,
+        Pair.of(Collections.<String> emptyList(), ""),
+        SSAMap.emptySSAMap());
 
-    // TODO
-//    Truth.assertThat(aci).isNotNull();
     cis.put(startNode, aci);
 
     cia = new CustomInstructionApplications(cis, ci);
     start = new ARGState(locConstructor.newInstance(startNode, true), null);
+    end = new ARGState(locConstructor.newInstance(endNodes.iterator().next(), true), null);
   }
 
   @Test
-  public void testIsStartState() throws CPAException, InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
-    Truth.assertThat(cia.isStartState(start)).isTrue();
-    ARGState notStart = new ARGState(locConstructor.newInstance(startNode.getLeavingEdge(0).getSuccessor(), true), start);
-    Truth.assertThat(cia.isStartState(notStart)).isFalse();
-  }
+  public void testIsStartState() throws CPAException, InstantiationException, IllegalAccessException,
+      IllegalArgumentException, InvocationTargetException {
+    ARGState notStart =
+        new ARGState(locConstructor.newInstance(startNode.getLeavingEdge(0).getSuccessor(), true), start);
+    ARGState noLocation = new ARGState(new CallstackState(null, "main", startNode), null);
 
-  @Test
-  public void testIsEndState() throws CPAException, InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
-    CFANode endNode = null;
-    CFANode predEnd = null;
-    CFANode predPredEnd = null;
-
-    Queue<CFANode> queue = new ArrayDeque<>();
-    queue.add(startNode);
-    Set<CFANode> visitedNodes = new HashSet<>();
-    while (endNode != null) {
-      predPredEnd = predEnd;
-      predEnd = endNode;
-      CFANode tmp = queue.poll();
-      if (tmp.getNumLeavingEdges()==0) {
-        endNode = tmp;
-        queue.clear();
-      } else if (!visitedNodes.contains(tmp)) {
-        visitedNodes.add(tmp);
-        for (CFAEdge edge : CFAUtils.allLeavingEdges(tmp)) {
-          queue.add(edge.getSuccessor());
-        }
-      }
+    // test applied custom instruction
+    Truth.assertThat(aci.isStartState(start)).isTrue();
+    Truth.assertThat(aci.isStartState(notStart)).isFalse();
+    try {
+      aci.isStartState(noLocation);
+    } catch (CPAException e) {
+      Truth.assertThat(e).isInstanceOf(CPAException.class);
     }
 
-    ARGState pred = new ARGState(locConstructor.newInstance(predEnd, true), new ARGState(locConstructor.newInstance(predPredEnd, true), null));
-    end = new ARGState(locConstructor.newInstance(endNode, true), pred);
-
-    // TODO
-//    Truth.assertThat(cia.isEndState(end, startNode)).isTrue();
-//    Truth.assertThat(cia.isEndState(pred, startNode)).isFalse();
+    // test custom instruction application
+    Truth.assertThat(cia.isStartState(start)).isTrue();
+    Truth.assertThat(cia.isStartState(notStart)).isFalse();
+    try {
+      cia.isStartState(noLocation);
+    } catch (CPAException e) {
+      Truth.assertThat(e).isInstanceOf(CPAException.class);
+    }
   }
 
   @Test
-  public void testGetAppliedCustomInstruction() throws InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, CPAException {
-    Truth.assertThat(cia.getAppliedCustomInstructionFor(start)).isEqualTo(cis.get(cfa));
-      try {
-        cia.getAppliedCustomInstructionFor(end);
-      } catch (CPAException e) {
-        Truth.assertThat(e).isInstanceOf(CPAException.class);
-      }
+  public void testIsEndState() throws CPAException, InstantiationException, IllegalAccessException,
+      IllegalArgumentException, InvocationTargetException {
+    ARGState noLocation = new ARGState(new CallstackState(null, "main", startNode), null);
+
+    // test applied custom instruction
+    Truth.assertThat(aci.isEndState(end)).isTrue();
+    Truth.assertThat(aci.isEndState(start)).isFalse();
+    try {
+      aci.isEndState(noLocation);
+    } catch (CPAException e) {
+      Truth.assertThat(e).isInstanceOf(CPAException.class);
+    }
+
+    // test custom instruction application
+    Truth.assertThat(cia.isEndState(end, startNode)).isTrue();
+    Truth.assertThat(cia.isEndState(start, startNode)).isFalse();
+
+    Truth.assertThat(cia.isEndState(end, start)).isTrue();
+    Truth.assertThat(cia.isEndState(start, start)).isFalse();
+  }
+
+  @Test
+  public void testGetAppliedCustomInstruction() throws InstantiationException, IllegalAccessException,
+      IllegalArgumentException, InvocationTargetException, CPAException {
+    Truth.assertThat(cia.getAppliedCustomInstructionFor(start)).isEqualTo(cis.get(startNode));
+    // test if input parameter not a start state
+    try {
+      cia.getAppliedCustomInstructionFor(end);
+    } catch (CPAException e) {
+      Truth.assertThat(e).isInstanceOf(CPAException.class);
+      Truth.assertThat(e).hasMessage("The state does not represent start of known custom instruction");
+    }
+    // test if input parameter does not contain location state
+    try {
+      cia.getAppliedCustomInstructionFor(new ARGState(new CallstackState(null, "main", startNode), null));
+    } catch (CPAException e) {
+      Truth.assertThat(e).isInstanceOf(CPAException.class);
+    }
   }
 }
