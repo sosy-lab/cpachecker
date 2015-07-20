148a53e30152eb54dbce88ee87043035759f5a7d
friedber <friedber@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2015-07-20 10:33:55 +0000

    adding new CPA for replaying a reached-set.
    The new CPA allows to traverse an old ARG (reachedset with ARGStates)
    parallel to a new analysis. The idea is to re-use information from the
    old analysis for the new analysis. For example, strenghening can access
    the old states.
    
    The domain of the new CPA is a powerset-domain, where the elements
    consist of a set of matching old ARGstates, i.e. with the same location
    and edges from root. If the new analysis leaves the state space of the
    old analysis (for example through a previously not visited branch in the
    program), the ARGReplay-state is expected to be empty.
    
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@17332 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

---
+++ src/org/sosy_lab/cpachecker/cpa/argReplay/ARGReplayCPA.java
@@ -0,0 +1,116 @@
+/*
+ *  CPAchecker is a tool for configurable software verification.
+ *  This file is part of CPAchecker.
+ *
+ *  Copyright (C) 2007-2015  Dirk Beyer
+ *  All rights reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *
+ *  CPAchecker web page:
+ *    http://cpachecker.sosy-lab.org
+ */
+package org.sosy_lab.cpachecker.cpa.argReplay;
+
+import java.util.Collections;
+
+import org.sosy_lab.common.log.LogManager;
+import org.sosy_lab.cpachecker.cfa.model.CFANode;
+import org.sosy_lab.cpachecker.core.defaults.AutomaticCPAFactory;
+import org.sosy_lab.cpachecker.core.defaults.DelegateAbstractDomain;
+import org.sosy_lab.cpachecker.core.defaults.MergeJoinOperator;
+import org.sosy_lab.cpachecker.core.defaults.SingletonPrecision;
+import org.sosy_lab.cpachecker.core.defaults.StaticPrecisionAdjustment;
+import org.sosy_lab.cpachecker.core.defaults.StopJoinOperator;
+import org.sosy_lab.cpachecker.core.interfaces.AbstractDomain;
+import org.sosy_lab.cpachecker.core.interfaces.AbstractState;
+import org.sosy_lab.cpachecker.core.interfaces.CPAFactory;
+import org.sosy_lab.cpachecker.core.interfaces.ConfigurableProgramAnalysis;
+import org.sosy_lab.cpachecker.core.interfaces.MergeOperator;
+import org.sosy_lab.cpachecker.core.interfaces.Precision;
+import org.sosy_lab.cpachecker.core.interfaces.PrecisionAdjustment;
+import org.sosy_lab.cpachecker.core.interfaces.StateSpacePartition;
+import org.sosy_lab.cpachecker.core.interfaces.StopOperator;
+import org.sosy_lab.cpachecker.core.interfaces.TransferRelation;
+import org.sosy_lab.cpachecker.core.reachedset.ReachedSet;
+import org.sosy_lab.cpachecker.cpa.arg.ARGState;
+
+import com.google.common.base.Preconditions;
+
+/** This CPA explores the state space of a powerset domain, backed by an old reached-set.
+ * Each abstract state contains the corresponding states of the old reached-set.
+ * The program location of new abstract state (in the current analysis)
+ * is equal to the abstract state from the reached-set. */
+public class ARGReplayCPA implements ConfigurableProgramAnalysis {
+
+  public static CPAFactory factory() {
+    return AutomaticCPAFactory.forType(ARGReplayCPA.class);
+  }
+
+  private final ARGReplayTransferRelation transferRelation;
+  private final AbstractDomain abstractDomain;
+  private final MergeOperator mergeOperator;
+  private final StopOperator stopOperator;
+  private ReachedSet reached = null;
+
+  public ARGReplayCPA(LogManager pLogger) {
+    transferRelation = new ARGReplayTransferRelation();
+    abstractDomain = DelegateAbstractDomain.<ARGReplayState>getInstance();
+    mergeOperator = new MergeJoinOperator(abstractDomain);
+    stopOperator = new StopJoinOperator(abstractDomain);
+  }
+
+  /** This method should be run directly after the constructor. */
+  public void setARG(ReachedSet pReached) {
+    Preconditions.checkNotNull(pReached);
+    Preconditions.checkState(this.reached == null, "ReachedSet should only be set once.");
+    this.reached  = pReached;
+  }
+
+  @Override
+  public AbstractDomain getAbstractDomain() {
+    return abstractDomain;
+  }
+
+  @Override
+  public TransferRelation getTransferRelation() {
+    return transferRelation;
+  }
+
+  @Override
+  public MergeOperator getMergeOperator() {
+    return mergeOperator;
+  }
+
+  @Override
+  public StopOperator getStopOperator() {
+    return stopOperator;
+  }
+
+  @Override
+  public PrecisionAdjustment getPrecisionAdjustment() {
+    return StaticPrecisionAdjustment.getInstance();
+  }
+
+  @Override
+  public AbstractState getInitialState(CFANode pNode, StateSpacePartition pPartition) {
+    Preconditions.checkNotNull(reached);
+    return new ARGReplayState(Collections.singleton((ARGState)reached.getFirstState()));
+  }
+
+  @Override
+  public Precision getInitialPrecision(CFANode pNode, StateSpacePartition pPartition) {
+    return SingletonPrecision.getInstance();
+  }
+}
---
+++ src/org/sosy_lab/cpachecker/cpa/argReplay/ARGReplayTransferRelation.java
@@ -0,0 +1,83 @@
+/*
+ *  CPAchecker is a tool for configurable software verification.
+ *  This file is part of CPAchecker.
+ *
+ *  Copyright (C) 2007-2015  Dirk Beyer
+ *  All rights reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *
+ *  CPAchecker web page:
+ *    http://cpachecker.sosy-lab.org
+ */
+package org.sosy_lab.cpachecker.cpa.argReplay;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.sosy_lab.cpachecker.cfa.model.CFAEdge;
+import org.sosy_lab.cpachecker.core.defaults.SingleEdgeTransferRelation;
+import org.sosy_lab.cpachecker.core.interfaces.AbstractState;
+import org.sosy_lab.cpachecker.core.interfaces.Precision;
+import org.sosy_lab.cpachecker.cpa.arg.ARGState;
+import org.sosy_lab.cpachecker.exceptions.CPATransferException;
+
+public class ARGReplayTransferRelation extends SingleEdgeTransferRelation {
+
+  public ARGReplayTransferRelation(){}
+
+  @Override
+  public Collection<? extends AbstractState> getAbstractSuccessorsForEdge(
+      AbstractState pState, Precision pPrecision, CFAEdge pCfaEdge) {
+
+    Set<ARGState> baseStates = ((ARGReplayState)pState).getStates();
+    Set<ARGState> successors = new HashSet<>();
+
+    // collect all states reachable via the edge
+    for (ARGState baseState : baseStates) {
+
+      // direct children
+      getChildren(pCfaEdge, baseState, successors);
+
+      // children of covering state
+      if (baseState.isCovered()) {
+        ARGState coveringState = baseState.getCoveringState();
+        // logger.log(Level.INFO, "jumping from", pState, "to covering state", coveringState, "because of edge", pCfaEdge);
+        getChildren(pCfaEdge, coveringState, successors);
+      }
+    }
+
+    return Collections.singleton(new ARGReplayState(successors));
+  }
+
+  private void getChildren(CFAEdge pCfaEdge, ARGState baseState, Set<ARGState> successors) {
+    for (ARGState child : baseState.getChildren()) {
+      if (pCfaEdge.equals(baseState.getEdgeToChild(child))) {
+        successors.add(child);
+      }
+    }
+  }
+
+  @Override
+  public Collection<? extends AbstractState> strengthen(
+      AbstractState pState, List<AbstractState> pOtherStates,
+      CFAEdge pCfaEdge, Precision pPrecision) throws CPATransferException, InterruptedException {
+
+    return Collections.singleton(pState);
+  }
+
+}
