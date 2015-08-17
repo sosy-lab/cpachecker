6e92a231f7341fc1400183e365c72cf429a736ce
stahlbauer <stahlbauer@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2015-08-17 16:00:17 +0000

    Get sets of component states
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/muauto@17601 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/util/AbstractStates.java
+++ src/org/sosy_lab/cpachecker/util/AbstractStates.java
@@ -26,6 +26,8 @@ package org.sosy_lab.cpachecker.util;
 import static com.google.common.base.Predicates.*;
 import static com.google.common.collect.FluentIterable.from;
 
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Set;
 
 import org.sosy_lab.cpachecker.cfa.model.CFANode;
@@ -46,6 +48,7 @@ import com.google.common.base.Predicate;
 import com.google.common.base.Predicates;
 import com.google.common.collect.FluentIterable;
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Lists;
 import com.google.common.collect.TreeTraverser;
 
 /**
@@ -90,6 +93,29 @@ public final class AbstractStates {
 
     return null;
   }
+
+  public static <T extends AbstractState> Collection<T> extractStatesByType(AbstractState pState, Class<T> pType) {
+    // TODO: Code duplication!!!! Refactor!
+
+    if (pType.isInstance(pState)) {
+      return ImmutableList.of(pType.cast(pState));
+
+    } else if (pState instanceof AbstractSingleWrapperState) {
+      AbstractState wrapped = ((AbstractSingleWrapperState)pState).getWrappedState();
+      return extractStatesByType(wrapped, pType);
+
+    } else if (pState instanceof AbstractWrapperState) {
+      Collection<T> result = Lists.newArrayList();
+      for (AbstractState wrapped : ((AbstractWrapperState)pState).getWrappedStates()) {
+        result.addAll(extractStatesByType(wrapped, pType));
+      }
+      return result;
+
+    } else {
+      return Collections.emptyList();
+    }
+  }
+
 
   /**
    * Applies {@link #extractStateByType(AbstractState, Class)} to all states
