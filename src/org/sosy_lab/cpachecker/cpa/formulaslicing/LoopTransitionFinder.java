24b4384b86e758a016a5d37149c5dee38e4869de
George Karpenkov <true.cheshire@gmail.com>
2015-08-25 15:25:36 +0000

    Faster LBE encoding for formula slicing.
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@17678 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/cpa/formulaslicing/LoopTransitionFinder.java
+++ src/org/sosy_lab/cpachecker/cpa/formulaslicing/LoopTransitionFinder.java
@@ -2,8 +2,10 @@ package org.sosy_lab.cpachecker.cpa.formulaslicing;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.logging.Level;
 
@@ -28,11 +30,13 @@ import org.sosy_lab.cpachecker.util.predicates.pathformula.SSAMap;
 import org.sosy_lab.cpachecker.util.predicates.pathformula.pointeraliasing.PointerTargetSet;
 
 import com.google.common.base.Preconditions;
-import com.google.common.collect.HashMultimap;
+import com.google.common.collect.HashBasedTable;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Iterables;
 import com.google.common.collect.Sets;
+import com.google.common.collect.Table;
+import com.google.common.collect.Table.Cell;
 
 /**
  * Return a path-formula describing all possible transitions inside the loop.
@@ -42,11 +46,7 @@ public class LoopTransitionFinder {
 
   @Option(secure=true, description="Apply AND- LBE transformation to loop "
       + "transition relation.")
-  private boolean applyANDtransformation = true;
-
-  @Option(secure=true, description="Apply OR- LBE transformation to loop "
-      + "transition relation.")
-  private boolean applyORtransformation = true;
+  private boolean applyLBETransformation = true;
 
   @Option(secure=true, description="Instead of considering the entire SCC, "
       + "ignore the function nested in the loop. UNSOUND!")
@@ -59,7 +59,7 @@ public class LoopTransitionFinder {
   private final FormulaSlicingStatistics statistics;
   private final ShutdownNotifier shutdownNotifier;
 
-  private final HashMultimap<CFANode, EdgeWrapper> cache;
+  private final Map<CFANode, Table<CFANode, CFANode, EdgeWrapper>> LBEcache;
 
   public LoopTransitionFinder(
       Configuration config,
@@ -75,7 +75,7 @@ public class LoopTransitionFinder {
     logger = pLogger;
     loopStructure = pCfa.getLoopStructure().get();
 
-    cache = HashMultimap.create();
+    LBEcache = new HashMap<>();
   }
 
   public PathFormula generateLoopTransition(
@@ -138,7 +138,8 @@ public class LoopTransitionFinder {
    *
    * 2) A - s_1 -> B, A - s_2 -> B is converted to A - s_1 \/ s_2 -> B.
    *
-   * Runs in cubic time, uses fixpoint computation.
+   * Runs in (at most) quadratic time.
+   *
    */
   private PathFormula LBE(
       CFANode loopHead,
@@ -146,11 +147,14 @@ public class LoopTransitionFinder {
       PointerTargetSet pts)
       throws CPATransferException, InterruptedException {
 
-    Set<EdgeWrapper> out;
-    if (cache.containsKey(loopHead)) {
+    // predecessor -> successor -> EdgeWrapper
+    Table<CFANode, CFANode, EdgeWrapper> out;
 
-      out = cache.get(loopHead);
+    if (LBEcache.containsKey(loopHead)) {
+
+      out = LBEcache.get(loopHead);
     } else {
+
       // Looping edges: intersection of forwards-reachable
       // and backwards-reachable.
       List<CFAEdge> edgesInLoop = getEdgesInSCC(loopHead);
@@ -162,13 +166,12 @@ public class LoopTransitionFinder {
       do {
         shutdownNotifier.shutdownIfNecessary();
         changed = false;
-        if (applyANDtransformation && andLBETransformation(out) ||
-            applyORtransformation && orLBETransformation(out)) {
+        if (applyLBETransformation && applyLBETransformation(out)) {
           changed = true;
         }
       } while (changed);
 
-      cache.putAll(loopHead, out);
+      LBEcache.put(loopHead, out);
     }
 
     PathFormula empty = new PathFormula(
@@ -176,94 +179,62 @@ public class LoopTransitionFinder {
         start, pts, 0);
     EdgeWrapper outEdge;
     if (out.size() == 1) {
-      outEdge = out.iterator().next();
+      outEdge = out.values().iterator().next();
     } else {
-      outEdge = new OrEdge(ImmutableList.copyOf(out));
+      outEdge = new OrEdge(ImmutableList.copyOf(out.values()));
     }
     return outEdge.toPathFormula(empty);
   }
 
   /**
-   * Apply and- transformation, return whether the passed set was changed.
+   * Apply and- and or- LBE transformation,
+   * return whether the passed table was changed.
    */
-  private boolean andLBETransformation(
-      Set<EdgeWrapper> out) {
-    for (EdgeWrapper e : out) {
+  private boolean applyLBETransformation(
+      Table<CFANode, CFANode, EdgeWrapper> out) {
 
+    // predecessor (row) -> successor (column) -> EdgeWrapper (value)
+    for (Cell<CFANode, CFANode, EdgeWrapper> cell : out.cellSet()) {
+
+      EdgeWrapper e = cell.getValue();
       CFANode predecessor = e.getPredecessor();
 
       // Do not perform reduction on nodes ending in a loop-head.
       if (loopStructure.getAllLoopHeads().contains(predecessor)) continue;
 
-      EdgeWrapper candidate = null;
-
-      for (EdgeWrapper other : out) {
-        if (e == other) continue;
-
-        if (other.getSuccessor() == predecessor) {
-          if (candidate == null) {
-            candidate = other;
-          } else {
-
-            // Do not perform replacement,
-            // if there are two candidates we should stop.
-            candidate = null;
-            break;
-          }
-        }
-      }
-
-      if (candidate != null) {
-        out.remove(e);
-        out.remove(candidate);
+      // Successor equivalent to our predecessor.
+      Collection<EdgeWrapper> candidates = out.column(predecessor).values();
+      if (candidates.size() == 1) {
+        EdgeWrapper candidate = candidates.iterator().next();
         EdgeWrapper added = new AndEdge(ImmutableList.of(candidate, e));
-        out.add(added);
 
-        logger.log(Level.ALL, "Removing", e, "and", candidate,
-            "adding", added);
+        // We need to check whether adding "added" would create a double entry.
+        EdgeWrapper alternative = out.get(added.getPredecessor(),
+            added.getSuccessor());
+
+        out.remove(candidate.getPredecessor(), candidate.getSuccessor());
+        out.remove(e.getPredecessor(), e.getSuccessor());
+        logger.log(Level.ALL, "Removing", e, "and", candidate);
+
+        if (alternative != null) {
+          added = new OrEdge(ImmutableList.of(added, alternative));
+          out.remove(alternative.getPredecessor(), alternative.getSuccessor());
+          logger.log(Level.ALL, "Removing", alternative);
+        }
+        out.put(added.getPredecessor(), added.getSuccessor(), added);
+        logger.log(Level.ALL, "Adding", added);
+
+        // Terminate the iteration on first change.
         return true;
       }
     }
     return false;
   }
 
-  /**
-   * Apply or- transformation, return whether the passed set was changed.
-   */
-  private boolean orLBETransformation(Set<EdgeWrapper> out) {
-    for (EdgeWrapper e : out) {
-
-      List<EdgeWrapper> candidates = new ArrayList<>();
-      candidates.add(e);
-
-      for (EdgeWrapper other : out) {
-        if (e == other) continue;
-
-        if (other.getPredecessor() == e.getPredecessor() &&
-            other.getSuccessor() == e.getSuccessor()) {
-
-          candidates.add(other);
-        }
-      }
-
-      if (candidates.size() > 1) {
-        EdgeWrapper added = new OrEdge(candidates);
-        for (EdgeWrapper toRemove : candidates) {
-          out.remove(toRemove);
-        }
-        out.add(added);
-        logger.log(Level.ALL, "Removing", candidates,
-            "adding", added);
-        return true;
-      }
-    }
-    return false;
-  }
-
-  private Set<EdgeWrapper> convert(Collection<CFAEdge> edges) {
-    Set<EdgeWrapper> out = new HashSet<>(edges.size());
+  private Table<CFANode, CFANode, EdgeWrapper> convert(Collection<CFAEdge> edges) {
+    Table<CFANode, CFANode, EdgeWrapper> out = HashBasedTable.create();
     for (CFAEdge e : edges) {
-      out.add(new SingleEdge(e));
+      out.put(e.getPredecessor(), e.getSuccessor(), new SingleEdge(e));
     }
     return out;
   }
