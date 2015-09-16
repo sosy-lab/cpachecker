4d8135b9137e3f71b97ff0dfab8b823a903e8e72
pwendler <pwendler@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2015-09-16 09:16:53 +0000

    Make logger field private, subclasses should use the logger from their own CPA such that log messages get appropriately marked with the correct CPA name.
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@17833 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/cpa/bam/AbstractBAMBasedRefiner.java
+++ src/org/sosy_lab/cpachecker/cpa/bam/AbstractBAMBasedRefiner.java
@@ -130,7 +130,7 @@ public abstract class AbstractBAMBasedRefiner extends AbstractARGBasedRefiner {
     // cleanup old states from last refinement
     subgraphStatesToReachedState.clear();
 
-    final BAMCEXSubgraphComputer cexSubgraphComputer = new BAMCEXSubgraphComputer(bamCpa, logger, subgraphStatesToReachedState);
+    final BAMCEXSubgraphComputer cexSubgraphComputer = new BAMCEXSubgraphComputer(bamCpa, subgraphStatesToReachedState);
     return cexSubgraphComputer.computeCounterexampleSubgraph(target, reachedSet);
   }
 }
