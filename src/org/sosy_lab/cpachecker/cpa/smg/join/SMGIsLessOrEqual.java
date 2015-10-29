91a2641495a5a1acd0f4f82a480797b71b59502c
driemey <driemey@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2015-10-29 11:56:53 +0000

    SMGCPA: Also compare return value in isLessOrEqual if applicable.
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@18220 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/cpa/smg/join/SMGIsLessOrEqual.java
+++ src/org/sosy_lab/cpachecker/cpa/smg/join/SMGIsLessOrEqual.java
@@ -30,6 +30,7 @@ import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
 
+import org.sosy_lab.cpachecker.cfa.types.c.CVoidType;
 import org.sosy_lab.cpachecker.cpa.smg.CLangStackFrame;
 import org.sosy_lab.cpachecker.cpa.smg.SMGEdgeHasValue;
 import org.sosy_lab.cpachecker.cpa.smg.SMGEdgeHasValueFilter;
@@ -107,8 +108,20 @@ public class SMGIsLessOrEqual {
       CLangStackFrame frameInSMG1 = smg1stackIterator.next();
       CLangStackFrame frameInSMG2 = smg2stackIterator.next();
 
+      //check, whether it is the same stack
+      if (!frameInSMG1.getFunctionDeclaration().getOrigName()
+          .equals(frameInSMG2.getFunctionDeclaration().getOrigName())) {
+        return false;
+      }
+
       //technically, one should look if any SMGHVE exist in additional region in SMG1
       if (frameInSMG1.getAllObjects().size() > frameInSMG2.getAllObjects().size()) {
+        return false;
+      }
+
+      // check, whether they have different return values if present
+      if (!(frameInSMG1.getFunctionDeclaration().getType().getReturnType().getCanonicalType() instanceof CVoidType)
+          && !isLessOrEqualFields(pSMG1, pSMG2, frameInSMG1.getReturnObject(), frameInSMG2.getReturnObject())) {
         return false;
       }
 
