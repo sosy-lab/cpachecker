db02c6a433ad365a2c5ec18afd1dac463e6ca96b
stieglma <stieglma@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2015-11-19 16:12:20 +0000

    Replace #asEdgesList with #getInnerEdges, it makes no difference in this case and should be preferred.
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@18535 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/util/refinement/GenericPrefixProvider.java
+++ src/org/sosy_lab/cpachecker/util/refinement/GenericPrefixProvider.java
@@ -146,7 +146,7 @@ public class GenericPrefixProvider<S extends ForgetfulState<?>> implements Prefi
           // for interpolation, one transition after the infeasible transition is needed,
           // so we add exactly that extra transition to the successor state
           ARGState lastState = path.asStatesList().get(feasiblePrefixBuilder.size());
-          CFAEdge lastEdge = path.asEdgesList().get(feasiblePrefixBuilder.size());
+          CFAEdge lastEdge = path.getInnerEdges().get(feasiblePrefixBuilder.size());
           ARGPath infeasiblePrefix = feasiblePrefixBuilder.build(lastState,
                                                                  BlankEdge.buildNoopEdge(lastEdge.getPredecessor(),
                                                                                          lastEdge.getSuccessor()));
