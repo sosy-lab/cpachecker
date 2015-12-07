e9c9abaaf6fe01a19cba7ade9f9ec33fa21b56e7
pwendler <pwendler@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2015-12-07 16:11:01 +0000

    fix compile error with javac,     no idea why this is necessary
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/cfaSequencer@18834 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/cfa/postprocessing/sequencer/utils/CFAEdgeUtilsTest.java
+++ src/org/sosy_lab/cpachecker/cfa/postprocessing/sequencer/utils/CFAEdgeUtilsTest.java
@@ -31,7 +31,7 @@ public class CFAEdgeUtilsTest {
   @Test
   public void testInjectEasySequnceInBetween() {
     MutableCFA cfa = Mockito.mock(MutableCFA.class);
-    
+
     // origin code
     CFANode startOriginNode = new CFANode("Node 1");
     CFANode middleOriginNode = new CFANode("Node 2");
@@ -40,7 +40,7 @@ public class CFAEdgeUtilsTest {
     CFAEdge originSecondEdge = new BlankEdge("", FileLocation.DUMMY, middleOriginNode, endOriginNode, "Edge 2");
     CFACreationUtils.addEdgeUnconditionallyToCFA(originFirstEdge);
     CFACreationUtils.addEdgeUnconditionallyToCFA(originSecondEdge);
-    
+
     // injected sequence
     CFANode startSequenceNode = new CFANode("Node 4");
     CFANode middleSequenceNode = new CFANode("Node 5");
@@ -49,26 +49,26 @@ public class CFAEdgeUtilsTest {
     CFAEdge secondSequenceEdge = new BlankEdge("", FileLocation.DUMMY, middleSequenceNode, endSequenceNode, "Edge 4");
     CFACreationUtils.addEdgeUnconditionallyToCFA(firstSequenceEdge);
     CFACreationUtils.addEdgeUnconditionallyToCFA(secondSequenceEdge);
-    
+
     // BEFORE INJECTING
     //connections which should still stay
     assertSuccessorOf(originFirstEdge, startOriginNode);
     assertSuccessorOf(originSecondEdge, middleOriginNode);
     assertSuccessorOf(endOriginNode, originSecondEdge);
-    
+
     assertSuccessorOf(firstSequenceEdge, startSequenceNode);
     assertSuccessorOf(middleSequenceNode, firstSequenceEdge);
     assertSuccessorOf(secondSequenceEdge, middleSequenceNode);
-    
+
     //connection which should desire
     assertSuccessorOf(middleOriginNode, originFirstEdge);
     assertSuccessorOf(endSequenceNode, secondSequenceEdge);
-    
+
     // desired connections should be replaced by
     assertNotSuccessorOf(startSequenceNode, originFirstEdge);
     assertNotSuccessorOf(middleOriginNode, secondSequenceEdge);
-    
-    
+
+
     // INJECTING
     CFAEdgeUtils.injectInBetween(middleOriginNode, startSequenceNode, endSequenceNode, cfa);
 
@@ -80,27 +80,27 @@ public class CFAEdgeUtilsTest {
     CFAEdge newSecondSequenceEdge = middleSequenceNode.getLeavingEdge(0);
     CFAEdge newOriginSecondEdge = endOriginNode.getEnteringEdge(0);
     CFAEdge newFirstSequenceEdge = startSequenceNode.getLeavingEdge(0);
-    
+
     // AFTER INJECTING
     //connections which should still stay
     assertSuccessorOf(newOriginFirstEdge, startOriginNode);
     assertSuccessorOf(newOriginSecondEdge, middleOriginNode);
     assertSuccessorOf(endOriginNode, newOriginSecondEdge);
-    
+
     assertSuccessorOf(newFirstSequenceEdge, startSequenceNode);
     assertSuccessorOf(middleSequenceNode, newFirstSequenceEdge);
     assertSuccessorOf(newSecondSequenceEdge, middleSequenceNode);
-    
+
     //connection which should desire
     assertNotSuccessorOf(middleOriginNode, newOriginFirstEdge);
     assertNotSuccessorOf(endSequenceNode, newSecondSequenceEdge);
-    
+
     // desired connections should be replaced by
     assertSuccessorOf(startSequenceNode, newOriginFirstEdge);
     assertSuccessorOf(middleOriginNode, newSecondSequenceEdge);
-    
+
   }
-  
+
   @Test
   public void testSimpleBypassCEdgeNodes() {
     // origin code
@@ -109,28 +109,28 @@ public class CFAEdgeUtilsTest {
     CFANode newEndNode = new CFANode("Node 3");
     CFAEdge originEdge = new BlankEdge("", FileLocation.DUMMY, startOriginNode, endOriginNode, "Edge 1");
     CFACreationUtils.addEdgeUnconditionallyToCFA(originEdge);
-    
+
     CFAEdge resultEdge = CFAEdgeUtils.bypassCEdgeNodes(originEdge, startOriginNode, newEndNode);
-    
+
     Assert.assertEquals(endOriginNode.getNumEnteringEdges(), 0);
     Assert.assertEquals(startOriginNode.getNumLeavingEdges(), 1);
     Assert.assertNotEquals(startOriginNode.getLeavingEdge(0), originEdge);
-    
-    
+
+
     Assert.assertEquals(resultEdge.getPredecessor(), startOriginNode);
     Assert.assertEquals(resultEdge.getSuccessor(), newEndNode);
-    
+
     assertSuccessorOf(resultEdge, startOriginNode);
     assertSuccessorOf(newEndNode, resultEdge);
   }
-  
+
   @Test
   public void testFullBypassCEdgeNodes() {
     //    * *
     //     *
-    //     * 
+    //     *
     //    * *
-    
+
     // origin code
     CFANode leftStartOriginNode = new CFANode("Node 1");
     CFANode rightStartOriginNode = new CFANode("Node 2");
@@ -138,79 +138,79 @@ public class CFAEdgeUtilsTest {
     CFANode secondMiddleNode = new CFANode("Node 4");
     CFANode leftEndOriginNode = new CFANode("Node 5");
     CFANode rightEndOriginNode = new CFANode("Node 6");
-    
+
     CFAEdge originEdge1 = new BlankEdge("", FileLocation.DUMMY, leftStartOriginNode, firstMiddleNode, "Edge 1");
     CFAEdge originEdge2 = new BlankEdge("", FileLocation.DUMMY, rightStartOriginNode, firstMiddleNode, "Edge 2");
     CFAEdge originEdge3 = new BlankEdge("", FileLocation.DUMMY, firstMiddleNode, secondMiddleNode, "Edge 3");  // will be replaced
     CFAEdge originEdge4 = new BlankEdge("", FileLocation.DUMMY, secondMiddleNode, leftEndOriginNode, "Edge 4");
     CFAEdge originEdge5 = new BlankEdge("", FileLocation.DUMMY, secondMiddleNode, rightEndOriginNode, "Edge 5");
-    
+
     CFACreationUtils.addEdgeUnconditionallyToCFA(originEdge1);
     CFACreationUtils.addEdgeUnconditionallyToCFA(originEdge2);
     CFACreationUtils.addEdgeUnconditionallyToCFA(originEdge3);
     CFACreationUtils.addEdgeUnconditionallyToCFA(originEdge4);
     CFACreationUtils.addEdgeUnconditionallyToCFA(originEdge5);
-    
+
     CFANode newPredesessor = new CFANode("Node 7");
     CFANode newSuccessor = new CFANode("Node 8");
-    
+
     CFAEdge resultEdge = CFAEdgeUtils.bypassCEdgeNodes(originEdge3, newPredesessor, newSuccessor);
-    
+
     Assert.assertEquals(firstMiddleNode.getNumLeavingEdges(), 0);
     Assert.assertEquals(firstMiddleNode.getNumEnteringEdges(), 2);
 
     Assert.assertEquals(secondMiddleNode.getNumEnteringEdges(), 0);
     Assert.assertEquals(secondMiddleNode.getNumLeavingEdges(), 2);
-    
-    
+
+
     Assert.assertEquals(resultEdge.getPredecessor(), newPredesessor);
     Assert.assertEquals(resultEdge.getSuccessor(), newSuccessor);
-    
+
     assertSuccessorOf(resultEdge, newPredesessor);
     assertSuccessorOf(newSuccessor, resultEdge);
-  
+
   }
-  
+
   @Test
   public void testXBypassCEdgeNodes() {
     //    * *
     //     *
     //    * *
-    
+
     // origin code
     CFANode leftStartOriginNode = new CFANode("Node 1");
     CFANode rightStartOriginNode = new CFANode("Node 2");
     CFANode middleNode = new CFANode("Node 3");
     CFANode leftEndOriginNode = new CFANode("Node 5");
     CFANode rightEndOriginNode = new CFANode("Node 6");
-    
+
     CFAEdge originEdge1 = new BlankEdge("", FileLocation.DUMMY, leftStartOriginNode, middleNode, "Edge 1");
     CFAEdge originEdge2 = new BlankEdge("", FileLocation.DUMMY, rightStartOriginNode, middleNode, "Edge 2");
     CFAEdge originEdge3 = new BlankEdge("", FileLocation.DUMMY, middleNode, leftEndOriginNode, "Edge 3");
     CFAEdge originEdge4 = new BlankEdge("", FileLocation.DUMMY, middleNode, rightEndOriginNode, "Edge 4");
-    
+
     CFACreationUtils.addEdgeUnconditionallyToCFA(originEdge1);
     CFACreationUtils.addEdgeUnconditionallyToCFA(originEdge2);
     CFACreationUtils.addEdgeUnconditionallyToCFA(originEdge3);
     CFACreationUtils.addEdgeUnconditionallyToCFA(originEdge4);
-    
+
     CFANode newPredesessor = new CFANode("Node 5");
-    
+
     CFAEdge resultEdge = CFAEdgeUtils.bypassCEdgeNodes(originEdge3, newPredesessor, rightEndOriginNode);
-    
+
     Assert.assertEquals(middleNode.getNumLeavingEdges(), 1);
     Assert.assertEquals(middleNode.getNumEnteringEdges(), 2);
 
     Assert.assertEquals(resultEdge.getPredecessor(), newPredesessor);
     Assert.assertEquals(resultEdge.getSuccessor(), rightEndOriginNode);
-    
+
     Assert.assertEquals(resultEdge.getPredecessor(), newPredesessor);
     Assert.assertEquals(resultEdge.getSuccessor(), rightEndOriginNode);
-    
+
     assertSuccessorOf(resultEdge, newPredesessor);
     assertSuccessorOf(rightEndOriginNode, resultEdge);
   }
-  
+
   private static void assertSuccessorOf(CFANode successor, CFAEdge of) {
     Assert.assertThat(of.getSuccessor(), isSameNode(successor));
     Assert.assertThat(CFAUtils.enteringEdges(successor), hasItem(isSameEdge(of)));
@@ -220,23 +220,23 @@ public class CFAEdgeUtilsTest {
     Assert.assertThat(successor.getPredecessor(), isSameNode(of));
     Assert.assertThat(CFAUtils.allLeavingEdges(of), hasItem(isSameEdge(successor)));
   }
-  
+
   private static void assertNotSuccessorOf(CFANode successor, CFAEdge of) {
     Assert.assertThat(of.getSuccessor(), not(isSameNode(successor)));
     Assert.assertThat(CFAUtils.enteringEdges(successor), everyItem(not(isSameEdge(of))));
   }
-  
+
   private static void assertNotSuccessorOf(CFAEdge successor, CFANode of) {
     Assert.assertThat(successor.getPredecessor(), not(isSameNode(of)));
     Assert.assertThat(CFAUtils.allLeavingEdges(of), everyItem(not(isSameEdge(successor))));
   }
-  
-  
+
+
   /**
    * This checks equality of an edge via it's class and it's description. A edge
    * with an null description will never match another edge
    */
-  public static class IsSameEdgeCheckedByClassAndDescritpion extends BaseMatcher<CFAEdge> {
+  public static class IsSameEdgeCheckedByClassAndDescritpion extends org.hamcrest.BaseMatcher<org.sosy_lab.cpachecker.cfa.model.CFAEdge> {
 
     private CFAEdge edge;
 
@@ -269,19 +269,19 @@ public class CFAEdgeUtilsTest {
     public void describeTo(Description arg0) {
 
     }
-    
+
     public static Matcher<CFAEdge> isSameEdge(CFAEdge edge) {
       return new IsSameEdgeCheckedByClassAndDescritpion(edge);
     }
 
   }
-  
-  
+
+
   /**
    * This checks equality of an edge via it's class and it's function name. A edge
    * with an null description will never match another edge
    */
-  public static class IsSameNodeCheckedByClassAndDescritpion extends BaseMatcher<CFANode> {
+  public static class IsSameNodeCheckedByClassAndDescritpion extends org.hamcrest.BaseMatcher<org.sosy_lab.cpachecker.cfa.model.CFANode> {
 
     private CFANode node;
 
@@ -320,7 +320,7 @@ public class CFAEdgeUtilsTest {
       return new IsSameNodeCheckedByClassAndDescritpion(node);
     }
   }
-  
+
   /**
    * This matcher will check if the node is reachable by an start node
    */
@@ -346,13 +346,13 @@ public class CFAEdgeUtilsTest {
       }
       return false;
     }
-    
+
     public boolean reachableBy(CFANode startNode) {
       NodeReachable reachableChecker = new NodeReachable(this.node);
       CFATraversal.dfs().traverseOnce(startNode, reachableChecker);
       return reachableChecker.isReachable();
     }
-    
+
     public boolean reachableBy(CFAEdge edge) {
       return reachableBy(edge.getSuccessor());
     }
@@ -367,7 +367,7 @@ public class CFAEdgeUtilsTest {
       return new IsNodeReachableBy(node);
     }
   }
-  
+
   public static class IsReachable extends BaseMatcher<CFAEdge> {
 
     private CFAEdge edge = null;
@@ -376,10 +376,10 @@ public class CFAEdgeUtilsTest {
     public IsReachable(CFAEdge edge) {
       this.edge = edge;
     }
-    
+
     public IsReachable(CFANode node) {
       this.node = node;
-    }    
+    }
 
     @Override
     public boolean matches(Object arg0) {
@@ -400,11 +400,11 @@ public class CFAEdgeUtilsTest {
     public void describeTo(Description arg0) {
 
     }
-    
+
     public static Matcher<CFAEdge> isEdgeReachable(CFAEdge edge) {
       return new IsReachable(edge);
     }
-    
+
     public static Matcher<CFAEdge> isNodeReachable(CFANode node) {
       return new IsReachable(node);
     }
