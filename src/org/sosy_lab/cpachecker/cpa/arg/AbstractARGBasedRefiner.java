cb02ce4b45ece2617c727abce9a0a3fbd438617b
stahlbauer <stahlbauer@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2015-12-08 15:16:51 +0000

    Removed debug log outputs.
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/checkerInclTiger@18880 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/cpa/arg/AbstractARGBasedRefiner.java
+++ src/org/sosy_lab/cpachecker/cpa/arg/AbstractARGBasedRefiner.java
@@ -129,8 +129,6 @@ public abstract class AbstractARGBasedRefiner implements Refiner {
       // Print error trace if cpa.arg.printErrorPath = true
       argCpa.exportCounterexampleOnTheFly(lastElement, counterexample, counterexamplesCounter);
       counterexamplesCounter++;
-    } else {
-      logger.log(Level.INFO, "NO Counterexample has been found.");
     }
 
     logger.log(Level.FINEST, "ARG based refinement finished, result is", counterexample.isSpurious());
