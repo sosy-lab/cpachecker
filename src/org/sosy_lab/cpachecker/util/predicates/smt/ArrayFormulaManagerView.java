eccb34a61eab2a19cd0262e4136cb52ac3e0fa64
lukasczyk <lukasczyk@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2015-12-09 12:52:30 +0000

    Documentation of ArrayFormulaManagerView
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/heaparray-integration@18893 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/util/predicates/smt/ArrayFormulaManagerView.java
+++ src/org/sosy_lab/cpachecker/util/predicates/smt/ArrayFormulaManagerView.java
@@ -34,34 +34,50 @@ import org.sosy_lab.solver.api.FormulaType;
 import org.sosy_lab.solver.api.FormulaType.ArrayFormulaType;
 import org.sosy_lab.solver.api.NumeralFormulaManager;
 
-public class ArrayFormulaManagerView
-  extends BaseManagerView
-  implements ArrayFormulaManager {
+/**
+ * Implements some methods for easier interaction with the formula manager for
+ * array formulas.
+ */
+public class ArrayFormulaManagerView extends BaseManagerView implements ArrayFormulaManager {
 
   private ArrayFormulaManager manager;
 
-  ArrayFormulaManagerView(FormulaWrappingHandler pWrappingHandler, ArrayFormulaManager pManager) {
+  /**
+   * Creates the new formula manager view for arrays.
+   *
+   * @param pWrappingHandler A handler for wrapping and unwrapping of formulae.
+   * @param pManager The formula manager capable of the SMT theory of arrays.
+   */
+  ArrayFormulaManagerView(
+      final FormulaWrappingHandler pWrappingHandler, final ArrayFormulaManager pManager) {
     super(pWrappingHandler);
     this.manager = pManager;
   }
 
+  /**
+   * {@inheritDoc}
+   */
   @Override
-  public <TI extends Formula, TE extends Formula> TE select (
-      ArrayFormula<TI, TE> pArray, Formula pIndex) {
+  public <TI extends Formula, TE extends Formula> TE select(
+      final ArrayFormula<TI, TE> pArray, final Formula pIndex) {
 
     @SuppressWarnings("unchecked")
     final ArrayFormula<TI, TE> declaredArray = (ArrayFormula<TI, TE>) unwrap(pArray);
     final TE selectResult = manager.select(declaredArray, unwrap(pIndex));
     final FormulaType<TE> resultType = getElementType(pArray);
 
-    // the result of a select can also be a reference to an array! (multi-dimensional arrays)
+    // the result of a select can also be a reference to an array!
+    // (multi-dimensional arrays)
     // example: returns an array
     return wrap(resultType, selectResult);
   }
 
+  /**
+   * {@inheritDoc}
+   */
   @Override
-  public <TI extends Formula, TE extends Formula> ArrayFormula<TI, TE> store (
-      ArrayFormula<TI, TE> pArray, Formula pIndex, Formula pValue) {
+  public <TI extends Formula, TE extends Formula> ArrayFormula<TI, TE> store(
+      final ArrayFormula<TI, TE> pArray, final Formula pIndex, final Formula pValue) {
 
     @SuppressWarnings("unchecked")
     final ArrayFormula<TI, TE> declaredArray = (ArrayFormula<TI, TE>) unwrap(pArray);
@@ -69,43 +85,63 @@ public class ArrayFormulaManagerView
     return manager.store(declaredArray, unwrap(pIndex), unwrap(pValue));
   }
 
-  @SuppressWarnings("unchecked")
+  /**
+   * {@inheritDoc}
+   */
   @Override
-  public <TI extends Formula, TE extends Formula, FTI extends FormulaType<TI>, FTE extends FormulaType<TE>> ArrayFormula<TI, TE> makeArray(
-      String pName, FTI pIndexType, FTE pElementType) {
+  public <TI extends Formula, TE extends Formula, FTI extends FormulaType<TI>,
+          FTE extends FormulaType<TE>>
+      ArrayFormula<TI, TE> makeArray(
+          final String pName, final FTI pIndexType, final FTE pElementType) {
 
-    final ArrayFormulaType<TI, TE> inputArrayType = new ArrayFormulaType<>(pIndexType, pElementType);
+    final ArrayFormulaType<TI, TE> inputArrayType =
+        new ArrayFormulaType<>(pIndexType, pElementType);
+
+    @SuppressWarnings("unchecked")
     final FTI unwrappedIndexType = (FTI) unwrapType(pIndexType);
+    @SuppressWarnings("unchecked")
     final FTE unwrappedElementType = (FTE) unwrapType(pElementType);
 
-    final ArrayFormula<TI, TE> result = manager.makeArray(pName, unwrappedIndexType, unwrappedElementType);
+    final ArrayFormula<TI, TE> result =
+        manager.makeArray(pName, unwrappedIndexType, unwrappedElementType);
 
     return wrap(inputArrayType, result);
   }
 
+  /**
+   * {@inheritDoc}
+   */
   @Override
-  public <TI extends Formula> FormulaType<TI> getIndexType(ArrayFormula<TI, ?> pArray) {
+  public <TI extends Formula> FormulaType<TI> getIndexType(final ArrayFormula<TI, ?> pArray) {
     if (pArray instanceof WrappingFormula<?,?>) {
       @SuppressWarnings("unchecked")
-      ArrayFormulaType<TI, ?> t = (ArrayFormulaType<TI, ?>) ((WrappingFormula<?, ?>) pArray).getType();
+      ArrayFormulaType<TI, ?> t =
+          (ArrayFormulaType<TI, ?>) ((WrappingFormula<?, ?>) pArray).getType();
       return t.getIndexType();
     }
     return manager.getIndexType(pArray);
   }
 
+  /**
+   * {@inheritDoc}
+   */
   @Override
-  public <TE extends Formula> FormulaType<TE> getElementType(ArrayFormula<?, TE> pArray) {
+  public <TE extends Formula> FormulaType<TE> getElementType(final ArrayFormula<?, TE> pArray) {
     if (pArray instanceof WrappingFormula<?,?>) {
       @SuppressWarnings("unchecked")
-      ArrayFormulaType<?, TE> t = (ArrayFormulaType<?, TE>) ((WrappingFormula<?, ?>) pArray).getType();
+      ArrayFormulaType<?, TE> t =
+          (ArrayFormulaType<?, TE>) ((WrappingFormula<?, ?>) pArray).getType();
       return t.getElementType();
     }
     return manager.getElementType(pArray);
   }
 
+  /**
+   * {@inheritDoc}
+   */
   @Override
-  public <TI extends Formula, TE extends Formula> BooleanFormula equivalence(ArrayFormula<TI, TE> pArray1,
-      ArrayFormula<TI, TE> pArray2) {
+  public <TI extends Formula, TE extends Formula> BooleanFormula equivalence(
+      final ArrayFormula<TI, TE> pArray1, final ArrayFormula<TI, TE> pArray2) {
 
     @SuppressWarnings("unchecked")
     final ArrayFormula<TI, TE> declaredArray1 = (ArrayFormula<TI, TE>) unwrap(pArray1);
@@ -115,36 +151,91 @@ public class ArrayFormulaManagerView
     BooleanFormula result = manager.equivalence(declaredArray1, declaredArray2);
     return wrap(FormulaType.BooleanType, result);
   }
-  public <T extends Formula> T declareAndCallArray(String pName,
-      NumeralFormulaManager<?, ?> pIntegerFormulaManager,
-      FormulaType<T> pReturnType,
-      Formula... pArgs) {
+
+  /**
+   * Returns a formula representing the declaration of an SMT array and a call
+   * to it.
+   *
+   * @param pName The name of the array variable.
+   * @param pIntegerFormulaManager The formula manager for integer formulae.
+   * @param pReturnType The return type of the array call, i.e. the element type
+   *                    of the array.
+   * @param pArgs A var-args list of formulae that get stored in the array.
+   * @param <T> The formula type of the elements of the array.
+   * @return A formula for the array and the call to it.
+   */
+  public <T extends Formula> T declareAndCallArray(
+      final String pName,
+      final NumeralFormulaManager<?, ?> pIntegerFormulaManager,
+      final FormulaType<T> pReturnType,
+      final Formula... pArgs) {
     return declareAndCallArray(pName, pIntegerFormulaManager, pReturnType,
         Arrays.asList(pArgs));
   }
 
-  public <T extends Formula> T declareAndCallArray(String pName,
-      int pIdx,
-      NumeralFormulaManager<?, ?> pIntegerFormulaManager,
-      FormulaType<T> pReturnType,
-      Formula... pArgs) {
+  /**
+   * Returns a formula representing the declaration of an SMT array and a call
+   * to it.
+   *
+   * @param pName The name of the array variable.
+   * @param pIdx An additional index.
+   * @param pIntegerFormulaManager The formula manager for integer formulae.
+   * @param pReturnType The return type of the array call, i.e. the element type
+   *                    of the array.
+   * @param pArgs A var-args list of formulae that get stored in the array.
+   * @param <T> The formula type of the elements of the array.
+   * @return A formula for the array and the call to it.
+   */
+  public <T extends Formula> T declareAndCallArray(
+      final String pName,
+      final int pIdx,
+      final NumeralFormulaManager<?, ?> pIntegerFormulaManager,
+      final FormulaType<T> pReturnType,
+      final Formula... pArgs) {
     return declareAndCallArray(pName, pIdx, pIntegerFormulaManager, pReturnType,
         Arrays.asList(pArgs));
   }
 
-  private <T extends Formula> T declareAndCallArray(String pName,
-      int pIdx,
-      NumeralFormulaManager<?, ?> pIntegerFormulaManager,
-      FormulaType<T> pReturnType,
-      List<Formula> pArgs) {
+  /**
+   * Returns a formula representing the declaration of an SMT array and a call
+   * to it.
+   *
+   * @param pName The name of the array variable.
+   * @param pIdx An additional index.
+   * @param pIntegerFormulaManager The formula manager for integer formulae.
+   * @param pReturnType The return type of the array call, i.e. the element type
+   *                    of the array.
+   * @param pArgs A list of formulae that get stored in the array.
+   * @param <T> The formula type of the elements of the array.
+   * @return A formula for the array and the call to it.
+   */
+  private <T extends Formula> T declareAndCallArray(
+      final String pName,
+      final int pIdx,
+      final NumeralFormulaManager<?, ?> pIntegerFormulaManager,
+      final FormulaType<T> pReturnType,
+      final List<Formula> pArgs) {
     String name = FormulaManagerView.makeName(pName, pIdx);
     return declareAndCallArray(name, pIntegerFormulaManager, pReturnType, pArgs);
   }
 
-  private <T extends Formula> T declareAndCallArray(String pName,
-      NumeralFormulaManager<?, ?> pIntegerFormulaManager,
-      FormulaType<T> pReturnType,
-      List<Formula> pArgs) {
+  /**
+   * Returns a formula representing the declaration of an SMT array and a call
+   * to it.
+   *
+   * @param pName The name of the array variable.
+   * @param pIntegerFormulaManager The formula manager for integer formulae.
+   * @param pReturnType The return type of the array call, i.e. the element type
+   *                    of the array.
+   * @param pArgs A list of formulae that get stored in the array.
+   * @param <T> The formula type of the elements of the array.
+   * @return A formula for the array and the call to it.
+   */
+  private <T extends Formula> T declareAndCallArray(
+      final String pName,
+      final NumeralFormulaManager<?, ?> pIntegerFormulaManager,
+      final FormulaType<T> pReturnType,
+      final List<Formula> pArgs) {
     // todo does the magic...
     ArrayFormula<?, ?> arrayFormula = makeArray(pName,
         FormulaType.IntegerType, pReturnType);
@@ -155,7 +246,6 @@ public class ArrayFormulaManagerView
       arrayFormula = store(arrayFormula, index, pArgs.get(i));
     }
 
-    return wrap(pReturnType,
-        select(arrayFormula, pIntegerFormulaManager.makeNumber(i)));
+    return wrap(pReturnType, select(arrayFormula, pIntegerFormulaManager.makeNumber(i - 1)));
   }
 }
