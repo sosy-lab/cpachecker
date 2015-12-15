c94916773273a9538a9416f8ccff7e66c1d246fc
friedber <friedber@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2015-12-15 07:36:07 +0000

    ThreadingCPA: small formatting and cleanup unused method.
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@18944 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/cpa/threading/ThreadingTransferRelation.java
+++ src/org/sosy_lab/cpachecker/cpa/threading/ThreadingTransferRelation.java
@@ -123,14 +123,16 @@ public final class ThreadingTransferRelation extends SingleEdgeTransferRelation 
         throws CPATransferException, InterruptedException {
     Preconditions.checkNotNull(cfaEdge);
     Preconditions.checkArgument(!(cfaEdge instanceof MultiEdge),
-        "MultiEdges cannot be supported by ThreadingCPA, because we need interleaving steps for chains of edges.");
+        "MultiEdges cannot be supported by ThreadingCPA, "
+        + "because we need interleaving steps for chains of edges.");
 
     final ThreadingState threadingState = exitThreads((ThreadingState) state);
 
     final String activeThread = getActiveThread(cfaEdge, threadingState);
 
     // check if atomic lock exists and is set for current thread
-    if (useAtomicLocks && threadingState.hasLock(ATOMIC_LOCK) && !threadingState.hasLock(activeThread, ATOMIC_LOCK)) {
+    if (useAtomicLocks && threadingState.hasLock(ATOMIC_LOCK)
+        && !threadingState.hasLock(activeThread, ATOMIC_LOCK)) {
       return Collections.emptySet();
     }
 
@@ -143,7 +145,8 @@ public final class ThreadingTransferRelation extends SingleEdgeTransferRelation 
     }
 
     // get all possible successors
-    Collection<ThreadingState> results = getAbstractSuccessorsFromWrappedCPAs(activeThread, threadingState, precision, cfaEdge);
+    Collection<ThreadingState> results = getAbstractSuccessorsFromWrappedCPAs(
+        activeThread, threadingState, precision, cfaEdge);
 
     if (useLocalAccessLocks) {
       results = handleLocalAccessLock(cfaEdge, threadingState, activeThread, results);
@@ -296,23 +299,6 @@ public final class ThreadingTransferRelation extends SingleEdgeTransferRelation 
       }
     }
     return tmp;
-  }
-
-  private Collection<ThreadingState> exitThread(
-      final ThreadingState threadingState, final String activeThread,
-      final Collection<ThreadingState> results) {
-
-    // update all successors
-    final Collection<ThreadingState> newResults = new ArrayList<>();
-    for (ThreadingState ts : results) {
-      ts = exitThread(ts, activeThread);
-      if (ts.getThreadIds().isEmpty()) {
-        // we have exited all threads, no successor
-      } else {
-        newResults.add(ts);
-      }
-    }
-    return newResults;
   }
 
   /** remove the thread-id from the state, and cleanup remaining locks of this thread. */
