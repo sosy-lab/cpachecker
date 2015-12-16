2c472a9f267db52890674796eaa90c365ca1eac1
buerdek <buerdek@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2015-12-16 14:19:56 +0000

    added small variability-aware c programm for testing tiger
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/checkerInclTiger@19001 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

---
+++ test/programs/tiger/simulator/simple.c
@@ -0,0 +1,15 @@
+extern int __VERIFIER_nondet_int();
+extern int input();
+
+int __SELECTED_FEATURE_GR;
+
+int main() {
+    int a;
+	__SELECTED_FEATURE_GR = __VERIFIER_nondet_int();
+	
+	if (__SELECTED_FEATURE_GR) {
+	  a = 10;
+	}
+	
+	return a;
+}
