5bdd1c53837fd6e047489824826852832156b876
friedber <friedber@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2015-12-22 21:43:44 +0000

    reverting r19108 because of interleaved update that already fixed this case.
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@19109 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/util/predicates/smt/FormulaManagerViewTest.java
+++ src/org/sosy_lab/cpachecker/util/predicates/smt/FormulaManagerViewTest.java
@@ -40,7 +40,6 @@ import org.sosy_lab.common.log.TestLogManager;
 import org.sosy_lab.cpachecker.cfa.types.c.CNumericTypes;
 import org.sosy_lab.cpachecker.util.predicates.pathformula.SSAMap;
 import org.sosy_lab.cpachecker.util.predicates.pathformula.SSAMap.SSAMapBuilder;
-import org.sosy_lab.cpachecker.util.predicates.smt.BooleanFormulaManagerView.DefaultBooleanFormulaVisitor;
 import org.sosy_lab.solver.FormulaManagerFactory.Solvers;
 import org.sosy_lab.solver.SolverException;
 import org.sosy_lab.solver.api.ArrayFormula;
@@ -93,17 +92,7 @@ public class FormulaManagerViewTest extends SolverBasedTest0 {
 
   /** strip the most outer NOT, if there is one, else return the formula unchanged. */
   private BooleanFormula stripNot(final BooleanFormula f) {
-    return new DefaultBooleanFormulaVisitor<BooleanFormula>(mgrv) {
-      @Override
-      public BooleanFormula visitDefault() {
-        return f;
-      }
-      @Override
-      public BooleanFormula visitNot(BooleanFormula notExpr) {
-        // no recursive visit, not needed for testing
-        return notExpr;
-      }
-    }.visit(f);
+    return mgrv.stripNegation(f).or(f);
   }
 
   @Test
