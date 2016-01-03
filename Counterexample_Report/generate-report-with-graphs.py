82c469757b657a2e00988f2872a124cbc19607fb
murr <murr@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2016-01-03 06:17:37 +0000

    jump with doubleclick
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/counterexample-report@19158 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- Counterexample_Report/generate-report-with-graphs.py
+++ Counterexample_Report/generate-report-with-graphs.py
@@ -119,7 +119,7 @@ def writeARG(argfilepath, outf):
             start = True
         if start:
             line = line.replace('class="node"','class="node" ng-dblclick="report.clickedARGElement($event)"')
-            line = line.replace('class="edge"','class="edge" ng-dblclick="report.clickedARGElement($event)"')
+            line = line.replace('class="edge"','class="edge" ng-dbllclick="report.clickedARGElement($event)"')
             outf.write(line)
     argfile.close()
 
