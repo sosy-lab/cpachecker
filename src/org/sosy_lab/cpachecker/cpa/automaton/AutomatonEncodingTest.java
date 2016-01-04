346e98fc52df6b66966e9563593f5e74c10ab786
stahlbauer <stahlbauer@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2016-01-04 10:42:35 +0000

    A first automated test for encoding properties using an automaton.
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/muauto@19165 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

---
+++ src/org/sosy_lab/cpachecker/cpa/automaton/AutomatonEncodingTest.java
@@ -0,0 +1,65 @@
+/*
+ *  CPAchecker is a tool for configurable software verification.
+ *  This file is part of CPAchecker.
+ *
+ *  Copyright (C) 2007-2014  Dirk Beyer
+ *  All rights reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *
+ *  CPAchecker web page:
+ *    http://cpachecker.sosy-lab.org
+ */
+package org.sosy_lab.cpachecker.cpa.automaton;
+
+import java.util.Map;
+
+import org.junit.Test;
+import org.sosy_lab.common.configuration.Configuration;
+import org.sosy_lab.cpachecker.util.test.CPATestRunner;
+import org.sosy_lab.cpachecker.util.test.StatisticsParser;
+import org.sosy_lab.cpachecker.util.test.TestDataTools;
+import org.sosy_lab.cpachecker.util.test.TestResults;
+
+import com.google.common.collect.ImmutableMap;
+
+public class AutomatonEncodingTest {
+
+  @Test
+  public void cyclicInclusionTest() throws Exception {
+    final String specFile = "test/config/automata/encode/LDV_118_1a_encode.spc";
+    final String programFile = "test/config/automata/encode/ldv_118_test.c";
+
+    Map<String, String> prop = ImmutableMap.of(
+        "specification",                    specFile,
+        "cpa.predicate.ignoreIrrelevantVariables", "FALSE",
+        "cfa.useMultiEdges",                "FALSE",
+        "automata.properties.granularity",  "BASENAME"
+      );
+
+    Configuration cfg = TestDataTools.configurationForTest()
+        .loadFromFile("config/predicateAnalysis-PredAbsRefiner-ABEl.properties")
+        .setOptions(prop)
+        .build();
+
+      TestResults results = CPATestRunner.run(cfg, programFile, false);
+      results.assertIsSafe();
+
+      StatisticsParser tgt = new StatisticsParser();
+      results.getCheckerResult().printStatistics(tgt.getPrintStream());
+
+      tgt.dumpStatistics(System.out);
+  }
+
+}
---
+++ test/config/automata/encode/LDV_118_1a_encode.spc
@@ -0,0 +1,19 @@
+OBSERVER AUTOMATON AUTOMATON_118_1a
+INITIAL STATE Init;
+
+STATE USEALL Init :
+  MATCH ENTRY -> ENCODE {int rlock = 1; int wlock = 1;} GOTO Init;
+
+  MATCH CALL {ldv_read_lock($?)} -> SPLIT {wlock == 1;} ENCODE {rlock=rlock+1;} GOTO Init NEGATION ERROR;
+  MATCH CALL {ldv_read_unlock($?)} -> SPLIT {rlock != 1;} ENCODE {rlock=rlock-1;} GOTO Init NEGATION ERROR;
+
+  MATCH CALL {ldv_write_lock($?)} -> SPLIT {wlock == 1;} ENCODE {wlock=wlock+1;} GOTO Init NEGATION ERROR;
+  MATCH CALL {ldv_write_unlock($?)} -> SPLIT {wlock == 2;} ENCODE {wlock=wlock-1;} GOTO Init NEGATION ERROR;
+  
+  MATCH RETURN {$1=ldv_read_trylock($?)} -> ASSUME {((int)$1)==1; wlock == 1;} ENCODE {rlock=rlock+1;} GOTO Init;
+  MATCH RETURN {$1=ldv_write_trylock($?)} -> ASSUME {((int)$1)==1; wlock == 2;} ENCODE {wlock=wlock+1;} GOTO Init;
+
+  MATCH EXIT -> SPLIT {rlock == 1;} GOTO Init NEGATION ERROR;
+  MATCH EXIT -> SPLIT {wlock == 1;} GOTO Init NEGATION ERROR;
+
+END AUTOMATON
