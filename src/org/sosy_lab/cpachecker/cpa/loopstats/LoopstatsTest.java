19840175dff4f7a2c2e1de5eee973b0136eedb0f
Stephan Lukasczyk <stephan@lukasczyk.me>
2016-01-07 15:10:18 +0000

    Implemented minimal test for Loopstats CPA.
    This is basically a copy of the AutomatonEncodingTest. Will be improved.
    
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/muauto@19199 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/cpa/loopstats/LoopstatsTest.java
+++ src/org/sosy_lab/cpachecker/cpa/loopstats/LoopstatsTest.java
@@ -1,7 +1,92 @@
+/*
+ * CPAchecker is a tool for configurable software verification.
+ *  This file is part of CPAchecker.
+ *
+ *  Copyright (C) 2007-2016  Dirk Beyer
+ *  All rights reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *
+ *  CPAchecker web page:
+ *    http://cpachecker.sosy-lab.org
+ */
 package org.sosy_lab.cpachecker.cpa.loopstats;
 
-/**
- * Created by sl on 1/7/16.
- */
+import java.util.Map;
+
+import org.junit.Test;
+import org.sosy_lab.common.configuration.Configuration;
+import org.sosy_lab.cpachecker.util.test.CPATestRunner;
+import org.sosy_lab.cpachecker.util.test.TestDataTools;
+import org.sosy_lab.cpachecker.util.test.TestResults;
+import org.sosy_lab.cpachecker.util.test.TestRunStatisticsParser;
+
+import com.google.common.collect.ImmutableMap;
+
 public class LoopstatsTest {
+
+  @Test
+  public void testEncodingOfLdvRule118_Safe() throws Exception {
+    final String specFile = "test/config/automata/encode/LDV_118_1a_encode.spc";
+    final String programFile = "test/config/automata/encode/ldv_118_test.c";
+
+    TestResults results = runWithSetup(specFile, programFile);
+
+    TestRunStatisticsParser stat = new TestRunStatisticsParser();
+    results.getCheckerResult().printStatistics(stat.getPrintStream());
+
+    stat.assertThatNumber("Number of times merged").isAtLeast(2);
+    stat.assertThatNumber("Number of refinements").isAtMost(3);
+    stat.assertThatNumber("Max. unrollings of a loop").isAtMost(2);
+  }
+
+  @Test
+  public void testEncodingOfLdvRule118_Unsafe() throws Exception {
+    final String specFile = "test/config/automata/encode/LDV_118_1a_encode.spc";
+    final String programFile = "test/config/automata/encode/ldv_118_test_false.c";
+
+    TestResults results = runWithSetup(specFile, programFile);
+
+    results.assertIsUnsafe();
+
+    TestRunStatisticsParser stat = new TestRunStatisticsParser();
+    results.getCheckerResult().printStatistics(stat.getPrintStream());
+
+    stat.assertThatNumber("Number of times merged").isAtLeast(0);
+    stat.assertThatNumber("Number of successful refinements").isAtMost(3);
+    stat.assertThatNumber("Max states per location").isAtMost(2);
+  }
+
+  private TestResults runWithSetup(final String pSpecFile, final String pProgramFile)
+      throws Exception {
+    Map<String, String> prop = ImmutableMap.<String, String>builder()
+        .put("specification", pSpecFile)
+        .put("cpa.predicate.ignoreIrrelevantVariables", "false")
+        .put("cfa.useMultiEdges", "false")
+        .put("automata.properties.granularity", "BASENAME")
+        .put("analysis.checkCounterexamples", "false")
+        .put("CompositeCPA.cpas", "cpa.location.LocationCPA, cpa.callstack.CallstackCPA, "
+            + "cpa.loopstats.LoopstatsCPA, cpa.functionpointer.FunctionPointerCPA, "
+            + "cpa.predicate.PredicateCPA, cpa.coverage.CoverageCPA")
+        .build();
+
+    Configuration cfg = TestDataTools.configurationForTest()
+        .loadFromFile("config/predicateAnalysis-PredAbsRefiner-ABEl.properties")
+        .setOptions(prop)
+        .build();
+
+    return CPATestRunner.run(cfg, pProgramFile, false);
+  }
+
 }
