4dd539b618607a1c2d45031a99f4428293ffe624
buerdek <buerdek@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2016-01-12 14:50:23 +0000

    update of mini fase
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/checkerInclTiger@19249 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- test/programs/tiger/simulator/Mini_FASE2015.c
+++ test/programs/tiger/simulator/Mini_FASE2015.c
@@ -1,44 +1,21 @@
 extern int __VERIFIER_nondet_int();
 
 int __SELECTED_FEATURE_PLUS;
-int __SELECTED_FEATURE_MINUS;
-int __SELECTED_FEATURE_NOTNEGATIVE;
 
 int validProduct() {
-	if (((__SELECTED_FEATURE_PLUS || __SELECTED_FEATURE_MINUS) && !(__SELECTED_FEATURE_PLUS && __SELECTED_FEATURE_MINUS))
-		&& (!__SELECTED_FEATURE_NOTNEGATIVE || __SELECTED_FEATURE_MINUS)) {
+	if (__SELECTED_FEATURE_PLUS) {
 		return 1;
 	}
 	return 0;
 }
 
-int foobar(int x, int y, int z) {
-	int a;
-	
-	if (__SELECTED_FEATURE_PLUS) {
-		G1: z += a;
-	} else if (__SELECTED_FEATURE_MINUS) {
-		if (__SELECTED_FEATURE_NOTNEGATIVE) {
-			if ((z - a) < 0)
-				G3: a *= -1;
-		}
-		G2: z -= a;
-	}
-	return z;
-}
-
-
 int main() {
-	int x = __VERIFIER_nondet_int();
-	int y = __VERIFIER_nondet_int();
-	int z = __VERIFIER_nondet_int();
+	int x = 0;
 	
 	__SELECTED_FEATURE_PLUS = __VERIFIER_nondet_int();
-	__SELECTED_FEATURE_MINUS = __VERIFIER_nondet_int();
-	__SELECTED_FEATURE_NOTNEGATIVE = __VERIFIER_nondet_int();
 
 	if (validProduct()) {
-		foobar(x, y, z);
+		G1: x = 10;	
 	}
 
 	return 0;
