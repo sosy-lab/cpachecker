2b55ee2ead662ff98ecc0f49504bb221ffdc6f04
sell <sell@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2016-01-19 14:47:05 +0000

    reverted MainCPAStatistics changed to temporiaily fix errors
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@19324 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/core/MainCPAStatistics.java
+++ src/org/sosy_lab/cpachecker/core/MainCPAStatistics.java
@@ -58,7 +58,6 @@ import org.sosy_lab.cpachecker.cfa.export.DOTBuilder;
 import org.sosy_lab.cpachecker.cfa.model.CFANode;
 import org.sosy_lab.cpachecker.core.CPAcheckerResult.Result;
 import org.sosy_lab.cpachecker.core.algorithm.Algorithm;
-import org.sosy_lab.cpachecker.core.counterexample.GenerateReportWithoutGraphs;
 import org.sosy_lab.cpachecker.core.interfaces.AbstractState;
 import org.sosy_lab.cpachecker.core.interfaces.AlgorithmIterationListener;
 import org.sosy_lab.cpachecker.core.interfaces.Graphable;
@@ -108,10 +107,6 @@ class MainCPAStatistics implements Statistics, AlgorithmIterationListener {
   @Option(secure=true, name="statistics.memory",
     description="track memory usage of JVM during runtime")
   private boolean monitorMemoryUsage = true;
-
-  @Option(secure=true, name="newCounterexampleReport",
-    description="insert all files except cfa/arg-graphs in html/js-template")
-  private boolean generateNewCounterexampleReport = true;
 
   private final LogManager logger;
   private final Collection<Statistics> subStats;
@@ -279,20 +274,6 @@ class MainCPAStatistics implements Statistics, AlgorithmIterationListener {
     out.println();
 
     printMemoryStatistics(out);
-
-    if (generateNewCounterexampleReport) {
-      GenerateReportWithoutGraphs.getFiles("output/UsedConfiguration.properties");
-      int amountOfErrorPaths = GenerateReportWithoutGraphs.getErrorpathFiles();
-      if(amountOfErrorPaths != 0) {
-        for(int i = 0; i < amountOfErrorPaths; i++) {
-          GenerateReportWithoutGraphs.fillOutHTMLTemplate("Counterexample_Report/index_template.html", "Counterexample_Report/index_withoutGraphs_" + i + ".html", i);
-          GenerateReportWithoutGraphs.fillOutJSTemplate("Counterexample_Report/app/app_template.js","Counterexample_Report/app/app_" + i + ".js", i);
-        }
-      } else {
-        GenerateReportWithoutGraphs.fillOutHTMLTemplate("Counterexample_Report/index_template.html", "Counterexample_Report/index_withoutGraphs.html", -1);
-        GenerateReportWithoutGraphs.fillOutJSTemplate("Counterexample_Report/app/app_template.js","Counterexample_Report/app/app.js", -1);
-      }
-    }
   }
 
 
