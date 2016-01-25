sosy-lab/cpachecker

commit 2cb37253438bf2123a1359cdc4e7c05b326b4644

Fixed export of invariants for k-Induction with KInductionInvariantGenerator
git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@19434 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

src/org/sosy_lab/cpachecker/core/algorithm/bmc/KInductionProver.java
@@ -42,6 +42,7 @@ import java.util.logging.Level;
 import org.sosy_lab.common.ShutdownNotifier;
 import org.sosy_lab.common.log.LogManager;
 import org.sosy_lab.cpachecker.cfa.CFA;
+import org.sosy_lab.cpachecker.cfa.model.BlankEdge;
 import org.sosy_lab.cpachecker.cfa.model.CFAEdge;
 import org.sosy_lab.cpachecker.cfa.model.CFANode;
 import org.sosy_lab.cpachecker.cfa.postprocessing.global.singleloop.CFASingleLoopTransformation;
@@ -71,6 +72,7 @@ import org.sosy_lab.cpachecker.util.LoopStructure.Loop;
 import org.sosy_lab.cpachecker.util.Precisions;
 import org.sosy_lab.cpachecker.util.expressions.And;
 import org.sosy_lab.cpachecker.util.expressions.ExpressionTree;
+import org.sosy_lab.cpachecker.util.expressions.ExpressionTrees;
 import org.sosy_lab.cpachecker.util.predicates.pathformula.PathFormula;
 import org.sosy_lab.cpachecker.util.predicates.pathformula.PathFormulaManager;
 import org.sosy_lab.cpachecker.util.predicates.pathformula.SSAMap;
@@ -90,6 +92,8 @@ import com.google.common.base.Predicates;
 import com.google.common.collect.FluentIterable;
 import com.google.common.collect.Iterables;
 import com.google.common.collect.Maps;
+import com.google.common.collect.Queues;
+import com.google.common.collect.Sets;
 
 /**
  * Instances of this class are used to prove the safety of a program by
@@ -275,12 +279,19 @@ class KInductionProver implements AutoCloseable {
     InvariantSupplier currentInvariantsSupplier = getCurrentInvariantSupplier();
     BooleanFormulaManager bfmgr = pFMGR.getBooleanFormulaManager();
 
-    BooleanFormula invariant = currentInvariantsSupplier.getInvariantFor(pLocation, pFMGR, pPFMGR);
+    BooleanFormula invariant = bfmgr.makeBoolean(true);
 
-    for (LocationFormulaInvariant confirmedCandidate : from(this.confirmedCandidates).filter(LocationFormulaInvariant.class)) {
-      if (confirmedCandidate.getLocations().contains(pLocation)) {
-        invariant = bfmgr.and(invariant, confirmedCandidate.getFormula(pFMGR, pPFMGR));
+
+    for (CFANode location : getBlankNeighbors(pLocation)) {
+
+      BooleanFormula locationInvariant = currentInvariantsSupplier.getInvariantFor(location, pFMGR, pPFMGR);
+
+      for (LocationFormulaInvariant confirmedCandidate : from(this.confirmedCandidates).filter(LocationFormulaInvariant.class)) {
+        if (confirmedCandidate.getLocations().contains(location)) {
+          locationInvariant = bfmgr.and(locationInvariant, confirmedCandidate.getFormula(pFMGR, pPFMGR));
+        }
       }
+      invariant = bfmgr.and(invariant, locationInvariant);
     }
 
     return invariant;
@@ -290,12 +301,19 @@ class KInductionProver implements AutoCloseable {
       throws InterruptedException {
     ExpressionTreeSupplier currentInvariantsSupplier = getCurrentExpressionTreeInvariantSupplier();
 
-    ExpressionTree<Object> invariant = currentInvariantsSupplier.getInvariantFor(pLocation);
+    ExpressionTree<Object> invariant = ExpressionTrees.getTrue();
 
-    for (ExpressionTreeLocationInvariant confirmedCandidate : FluentIterable.from(this.confirmedCandidates).filter(ExpressionTreeLocationInvariant.class)) {
-      if (confirmedCandidate.getLocations().contains(pLocation)) {
-        invariant = And.of(invariant, confirmedCandidate.asExpressionTree());
+    for (CFANode location : getBlankNeighbors(pLocation)) {
+      ExpressionTree<Object> locationInvariant = currentInvariantsSupplier.getInvariantFor(location);
+
+      for (ExpressionTreeCandidateInvariant confirmedCandidate : FluentIterable.from(this.confirmedCandidates).filter(ExpressionTreeCandidateInvariant.class)) {
+        if (confirmedCandidate instanceof LocationFormulaInvariant) {
+          if (((LocationFormulaInvariant) confirmedCandidate).getLocations().contains(location)) {
+            locationInvariant = And.of(locationInvariant, confirmedCandidate.asExpressionTree());
+          }
+        }
       }
+      invariant = And.of(invariant, locationInvariant);
     }
 
     return invariant;
@@ -589,6 +607,29 @@ class KInductionProver implements AutoCloseable {
 
   private static Set<CFANode> getStopLocations(ReachedSet pReachedSet) {
     return from(pReachedSet).filter(BMCAlgorithm.IS_STOP_STATE).transform(AbstractStates.EXTRACT_LOCATION).toSet();
+  }
+
+  private static Set<CFANode> getBlankNeighbors(CFANode pNode) {
+    Set<CFANode> visited = Sets.newHashSet();
+    Queue<CFANode> waitlist = Queues.newArrayDeque();
+    waitlist.offer(pNode);
+    visited.add(pNode);
+    while (!waitlist.isEmpty()) {
+      CFANode current = waitlist.poll();
+      for (BlankEdge enteringEdge : CFAUtils.enteringEdges(current).filter(BlankEdge.class)) {
+        CFANode predecessor = enteringEdge.getPredecessor();
+        if (visited.add(predecessor)) {
+          waitlist.offer(predecessor);
+        }
+      }
+      for (BlankEdge leavingEdge : CFAUtils.leavingEdges(current).filter(BlankEdge.class)) {
+        CFANode successor = leavingEdge.getSuccessor();
+        if (visited.add(successor)) {
+          waitlist.offer(successor);
+        }
+      }
+    }
+    return visited;
   }
 
 }

src/org/sosy_lab/cpachecker/cpa/arg/ARGPathExporter.java
@@ -247,7 +247,7 @@ public class ARGPathExporter {
         pRootState,
         pIsRelevantState,
         pIsRelevantEdge,
-        GraphBuilder.CFA,
+        GraphBuilder.CFA_FROM_ARG,
         new InvariantProvider() {
 
           @Override

src/org/sosy_lab/cpachecker/core/algorithm/bmc/BMCAlgorithm.java
@@ -61,6 +61,7 @@ import org.sosy_lab.cpachecker.core.reachedset.ReachedSetFactory;
 import org.sosy_lab.cpachecker.cpa.arg.ARGCPA;
 import org.sosy_lab.cpachecker.cpa.arg.ARGPath;
 import org.sosy_lab.cpachecker.cpa.arg.ARGPathExporter;
+import org.sosy_lab.cpachecker.cpa.arg.ARGPathExporter.ExtractionState;
 import org.sosy_lab.cpachecker.cpa.arg.ARGState;
 import org.sosy_lab.cpachecker.cpa.arg.ARGUtils;
 import org.sosy_lab.cpachecker.cpa.arg.GraphBuilder;
@@ -334,17 +335,18 @@ public class BMCAlgorithm extends AbstractBMCAlgorithm implements Algorithm {
                     rootState,
                     Predicates.alwaysTrue(),
                     Predicates.alwaysTrue(),
-                    GraphBuilder.CFA,
+                    GraphBuilder.CFA_FULL,
                     new InvariantProvider() {
 
                       @Override
                       public ExpressionTree<Object> provideInvariantFor(
                           CFAEdge pCFAEdge,
-                          Optional<? extends Collection<? extends ARGState>> pStates) {
+                          Optional<? extends Collection<? extends ExtractionState>> pStates) {
                         try {
                           CFANode node = pCFAEdge.getSuccessor();
                           ExpressionTree<Object> result =
                               invariantGenerator.getAsExpressionTree().getInvariantFor(node);
+
 
                           return result;
 

src/org/sosy_lab/cpachecker/core/algorithm/bmc/EdgeFormulaNegation.java
@@ -117,6 +117,6 @@ public class EdgeFormulaNegation extends LocationFormulaInvariant implements Exp
 
   @Override
   public ExpressionTree<Object> asExpressionTree() {
-    return LeafExpression.<Object>of((Object) getNegatedAssumeEdge().getExpression());
+    return LeafExpression.<Object>of((Object) getNegatedAssumeEdge().getExpression(), getNegatedAssumeEdge().getTruthAssumption());
   }
 }

src/org/sosy_lab/cpachecker/core/algorithm/invariants/KInductionInvariantGenerator.java
@@ -29,6 +29,7 @@ import java.io.PrintStream;
 import java.util.ArrayDeque;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Queue;
@@ -39,6 +40,7 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import org.sosy_lab.common.Classes.UnexpectedCheckedException;
 import org.sosy_lab.common.LazyFutureTask;
@@ -91,11 +93,11 @@ import org.sosy_lab.cpachecker.util.CPAs;
 import org.sosy_lab.cpachecker.util.automaton.TargetLocationProvider;
 import org.sosy_lab.cpachecker.util.expressions.ExpressionTree;
 import org.sosy_lab.cpachecker.util.expressions.ExpressionTrees;
-import org.sosy_lab.cpachecker.util.expressions.Or;
 import org.sosy_lab.solver.SolverException;
 
 import com.google.common.base.Throwables;
-import com.google.common.collect.Maps;
+import com.google.common.collect.HashMultimap;
+import com.google.common.collect.Multimap;
 import com.google.common.collect.Sets;
 
 /**
@@ -245,17 +247,20 @@ public class KInductionInvariantGenerator extends AbstractInvariantGenerator imp
     shutdownManager.getNotifier().registerAndCheckImmediately(shutdownListener);
   }
 
+  private final AtomicBoolean cancelled = new AtomicBoolean();
+
   @Override
   public void cancel() {
     checkState(invariantGenerationFuture != null);
     shutdownManager.requestShutdown("Invariant generation cancel requested.");
+    cancelled.set(true);
   }
 
   @Override
   public InvariantSupplier get() throws CPAException, InterruptedException {
     checkState(invariantGenerationFuture != null);
 
-    if (async && !invariantGenerationFuture.isDone()) {
+    if (async && (!invariantGenerationFuture.isDone()) || cancelled.get()) {
       // grab intermediate result that is available so far
       return algorithm.getCurrentInvariants();
 
@@ -346,6 +351,7 @@ public class KInductionInvariantGenerator extends AbstractInvariantGenerator imp
       }
     }
 
+    final Multimap<CFANode, ExpressionTree<AExpression>> candidatesByLocation = HashMultimap.create();
     if (options.invariantsAutomatonFile != null) {
       ConfigurationBuilder configBuilder = Configuration.builder();
       String machineModelOption = "analysis.machineModel";
@@ -379,22 +385,16 @@ public class KInductionInvariantGenerator extends AbstractInvariantGenerator imp
         // but instead of throwing the exception here,
         // let it be thrown by the invariant generator.
       }
-      Map<CFANode, ExpressionTree<AExpression>> candidatesByLocation = Maps.newLinkedHashMap();
       for (AbstractState abstractState : reachedSet) {
         Iterable<CFANode> locations = AbstractStates.extractLocations(abstractState);
         for (CFANode location : locations) {
           for (AutomatonState automatonState : AbstractStates.asIterable(abstractState).filter(AutomatonState.class)) {
             ExpressionTree<AExpression> candidate = automatonState.getCandidateInvariants();
-            ExpressionTree<AExpression> prev = candidatesByLocation.get(location);
-            if (prev == null) {
-              candidatesByLocation.put(location, candidate);
-            } else {
-              candidatesByLocation.put(location, Or.of(prev, candidate));
-            }
+            candidatesByLocation.put(location, candidate);
           }
         }
       }
-      for (Map.Entry<CFANode, ExpressionTree<AExpression>> entry : candidatesByLocation.entrySet()) {
+      for (Map.Entry<CFANode, ExpressionTree<AExpression>> entry : candidatesByLocation.entries()) {
         if (!entry.getValue().equals(ExpressionTrees.getTrue())) {
           candidates.add(new ExpressionTreeLocationInvariant(entry.getKey(), entry.getValue()));
         }
@@ -403,6 +403,8 @@ public class KInductionInvariantGenerator extends AbstractInvariantGenerator imp
 
     if (options.terminateOnCounterexample) {
       return new StaticCandidateProvider(candidates) {
+
+        private final Map<CFANode, Integer> disproved = new HashMap<>();
 
         @Override
         public Iterator<CandidateInvariant> iterator() {
@@ -423,7 +425,19 @@ public class KInductionInvariantGenerator extends AbstractInvariantGenerator imp
 
             @Override
             public void remove() {
-              pShutdownManager.requestShutdown("Incorrect invariant: " + candidate.toString());
+              if (candidate instanceof ExpressionTreeLocationInvariant) {
+                ExpressionTreeLocationInvariant expressionTreeLocationInvariant = (ExpressionTreeLocationInvariant) candidate;
+                CFANode location = expressionTreeLocationInvariant.getLocations().iterator().next();
+                Integer disprovedCount = disproved.get(location);
+                if (disprovedCount == null) {
+                  disprovedCount = 0;
+                }
+                ++disprovedCount;
+                disproved.put(location, disprovedCount);
+                if (disprovedCount == candidatesByLocation.get(location).size()) {
+                  //pShutdownManager.requestShutdown("Incorrect invariant: " + candidate.toString());
+                }
+              }
               iterator.remove();
             }
 

commit 6f53d837e381bf06e37671cdf0e8c2c39e524ba8

Fixed a bug in the k-Induction algorithm introduced in r19434
git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@19445 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

src/org/sosy_lab/cpachecker/core/algorithm/bmc/KInductionProver.java
@@ -33,6 +33,7 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Queue;
 import java.util.Set;
@@ -72,6 +73,7 @@ import org.sosy_lab.cpachecker.util.Precisions;
 import org.sosy_lab.cpachecker.util.expressions.And;
 import org.sosy_lab.cpachecker.util.expressions.ExpressionTree;
 import org.sosy_lab.cpachecker.util.expressions.ExpressionTrees;
+import org.sosy_lab.cpachecker.util.expressions.Or;
 import org.sosy_lab.cpachecker.util.predicates.pathformula.PathFormula;
 import org.sosy_lab.cpachecker.util.predicates.pathformula.PathFormulaManager;
 import org.sosy_lab.cpachecker.util.predicates.pathformula.SSAMap;
@@ -276,21 +278,22 @@ class KInductionProver implements AutoCloseable {
     InvariantSupplier currentInvariantsSupplier = getCurrentInvariantSupplier();
     BooleanFormulaManager bfmgr = pFMGR.getBooleanFormulaManager();
 
-    BooleanFormula invariant = bfmgr.makeBoolean(true);
+    BooleanFormula invariant = bfmgr.makeBoolean(false);
 
+    for (List<CFANode> path : CFAUtils.getBlankPaths(pLocation)) {
+      BooleanFormula pathInvariant = bfmgr.makeBoolean(true);
 
-    for (CFANode location : CFAUtils.getBlankNeighbors(pLocation)) {
-
-      BooleanFormula locationInvariant = currentInvariantsSupplier.getInvariantFor(location, pFMGR, pPFMGR);
-
-      for (LocationFormulaInvariant confirmedCandidate : from(this.confirmedCandidates).filter(LocationFormulaInvariant.class)) {
-        if (confirmedCandidate.getLocations().contains(location)) {
-          locationInvariant = bfmgr.and(locationInvariant, confirmedCandidate.getFormula(pFMGR, pPFMGR));
+      for (CFANode location : path) {
+        for (CandidateInvariant candidateInvariant :
+            getConfirmedCandidates(location).filter(CandidateInvariant.class)) {
+          pathInvariant = bfmgr.and(pathInvariant, candidateInvariant.getFormula(pFMGR, pPFMGR));
         }
+        pathInvariant =
+            bfmgr.and(
+                pathInvariant, currentInvariantsSupplier.getInvariantFor(location, pFMGR, pPFMGR));
       }
-      invariant = bfmgr.and(invariant, locationInvariant);
+      invariant = bfmgr.or(pathInvariant, invariant);
     }
-
     return invariant;
   }
 
@@ -298,22 +301,35 @@ class KInductionProver implements AutoCloseable {
       throws InterruptedException {
     ExpressionTreeSupplier currentInvariantsSupplier = getCurrentExpressionTreeInvariantSupplier();
 
-    ExpressionTree<Object> invariant = ExpressionTrees.getTrue();
+    ExpressionTree<Object> invariant = ExpressionTrees.getFalse();
+    for (List<CFANode> path : CFAUtils.getBlankPaths(pLocation)) {
+      ExpressionTree<Object> pathInvariant = ExpressionTrees.getTrue();
 
-    for (CFANode location : CFAUtils.getBlankNeighbors(pLocation)) {
-      ExpressionTree<Object> locationInvariant = currentInvariantsSupplier.getInvariantFor(location);
-
-      for (ExpressionTreeCandidateInvariant confirmedCandidate : FluentIterable.from(this.confirmedCandidates).filter(ExpressionTreeCandidateInvariant.class)) {
-        if (confirmedCandidate instanceof LocationFormulaInvariant) {
-          if (((LocationFormulaInvariant) confirmedCandidate).getLocations().contains(location)) {
-            locationInvariant = And.of(locationInvariant, confirmedCandidate.asExpressionTree());
-          }
+      for (CFANode location : path) {
+        for (ExpressionTreeCandidateInvariant expressionTreeCandidateInvariant :
+            getConfirmedCandidates(location).filter(ExpressionTreeCandidateInvariant.class)) {
+          pathInvariant =
+              And.of(pathInvariant, expressionTreeCandidateInvariant.asExpressionTree());
         }
-      }
-      invariant = And.of(invariant, locationInvariant);
-    }
 
+        pathInvariant = And.of(pathInvariant, currentInvariantsSupplier.getInvariantFor(location));
+      }
+      invariant = Or.of(pathInvariant, invariant);
+    }
     return invariant;
+  }
+
+  private FluentIterable<LocationFormulaInvariant> getConfirmedCandidates(final CFANode pLocation) {
+    return from(confirmedCandidates)
+        .filter(LocationFormulaInvariant.class)
+        .filter(
+            new Predicate<LocationFormulaInvariant>() {
+
+              @Override
+              public boolean apply(LocationFormulaInvariant pConfirmedCandidate) {
+                return pConfirmedCandidate.getLocations().contains(pLocation);
+              }
+            });
   }
 
   @Override

src/org/sosy_lab/cpachecker/core/algorithm/invariants/CPAInvariantGenerator.java
@@ -455,28 +455,25 @@ public class CPAInvariantGenerator extends AbstractInvariantGenerator implements
 
     @Override
     public ExpressionTree<Object> getInvariantFor(CFANode pLocation) {
+      ExpressionTree<Object> locationInvariant = ExpressionTrees.getFalse();
+      for (List<CFANode> path : CFAUtils.getBlankPaths(pLocation)) {
+        ExpressionTree<Object> pathInvariant = ExpressionTrees.getTrue();
 
-      ExpressionTree<Object> invariant = ExpressionTrees.getTrue();
-
-      for (CFANode location : CFAUtils.getBlankNeighbors(pLocation)) {
-
-        ExpressionTree<Object> locationInvariant = ExpressionTrees.getFalse();
-
-        for (AbstractState locState : AbstractStates.filterLocation(reached, location)) {
-          ExpressionTree<Object> stateInvariant = ExpressionTrees.getTrue();
-          for (ExpressionTreeReportingState expressionTreeReportingState : AbstractStates.asIterable(locState).filter(ExpressionTreeReportingState.class)) {
-            stateInvariant =
-                And.of(
-                    stateInvariant,
-                    expressionTreeReportingState.getFormulaApproximation(
-                        cfa.getFunctionHead(pLocation.getFunctionName()), location));
+        for (CFANode location : path) {
+          for (AbstractState locState : AbstractStates.filterLocation(reached, location)) {
+            for (ExpressionTreeReportingState expressionTreeReportingState :
+                AbstractStates.asIterable(locState).filter(ExpressionTreeReportingState.class)) {
+              pathInvariant =
+                  And.of(
+                      pathInvariant,
+                      expressionTreeReportingState.getFormulaApproximation(
+                          cfa.getFunctionHead(pLocation.getFunctionName()), location));
+            }
           }
-
-          locationInvariant = Or.of(locationInvariant, stateInvariant);
         }
-        invariant = And.of(invariant, locationInvariant);
+        locationInvariant = Or.of(pathInvariant, locationInvariant);
       }
-      return invariant;
+      return locationInvariant;
     }
   }
 

src/org/sosy_lab/cpachecker/cpa/arg/ARGPathExporter.java
@@ -973,13 +973,14 @@ public class ARGPathExporter {
       } else {
         newScope = null;
       }
-      ExpressionTree<Object> newTree = Or.of(sourceTree, targetTree);
-      if (newScope == null && !ExpressionTrees.isConstant(newTree)) {
-        putStateInvariant(source, ExpressionTrees.getTrue());
-        stateScopes.remove(source);
-      } else {
-        putStateInvariant(source, newTree);
-        stateScopes.put(source, newScope);
+      ExpressionTree<Object> newTree = mergeStateInvariantsInfoFirst(source, target);
+      if (newTree != null) {
+        if (newScope == null && !ExpressionTrees.isConstant(newTree)) {
+          putStateInvariant(source, ExpressionTrees.getTrue());
+          stateScopes.remove(source);
+        } else {
+          stateScopes.put(source, newScope);
+        }
       }
 
       // Merge the violated properties
@@ -1124,12 +1125,37 @@ public class ARGPathExporter {
      * @param pValue the invariant to be added.
      */
     private void putStateInvariant(String pStateId, ExpressionTree<Object> pValue) {
-      ExpressionTree<Object> result = stateInvariants.get(pStateId);
-      if (result == null) {
+      ExpressionTree<Object> prev = stateInvariants.get(pStateId);
+      if (prev == null) {
         stateInvariants.put(pStateId, pValue);
-      } else {
-        stateInvariants.put(pStateId, Or.of(result, pValue));
+        return;
       }
+      ExpressionTree<Object> result = Or.of(prev, pValue);
+      stateInvariants.put(pStateId, result);
+    }
+
+    /**
+     * Merges the invariants for the given state ids and stores it as the new invariant for the first of the given ids.
+     *
+     * @param pStateId the state id.
+     * @param pOtherStateId the other state id.
+     *
+     * @return the merged invariant. {@code null} if neither state had an invariant.
+     */
+    private @Nullable ExpressionTree<Object> mergeStateInvariantsInfoFirst(
+        String pStateId, String pOtherStateId) {
+      ExpressionTree<Object> prev = stateInvariants.get(pStateId);
+      ExpressionTree<Object> other = stateInvariants.get(pOtherStateId);
+      if (prev == null) {
+        stateInvariants.put(pStateId, other);
+        return other;
+      }
+      if (other == null) {
+        return prev;
+      }
+      ExpressionTree<Object> result = Or.of(prev, other);
+      stateInvariants.put(pStateId, result);
+      return result;
     }
 
     private ExpressionTree<Object> getStateInvariant(String pStateId) {

src/org/sosy_lab/cpachecker/util/CFAUtils.java
@@ -27,9 +27,11 @@ import static com.google.common.base.Preconditions.*;
 import static com.google.common.base.Predicates.not;
 
 import java.util.ArrayDeque;
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Queue;
 import java.util.Set;
 import java.util.SortedSet;
@@ -49,9 +51,9 @@ import com.google.common.base.Function;
 import com.google.common.base.Predicate;
 import com.google.common.base.Predicates;
 import com.google.common.collect.FluentIterable;
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Iterables;
 import com.google.common.collect.Queues;
-import com.google.common.collect.Sets;
 import com.google.common.collect.UnmodifiableIterator;
 
 public class CFAUtils {
@@ -383,31 +385,55 @@ public class CFAUtils {
   }
 
   /**
-   * Get all nodes transitively connected to this node via blank edges, including the node itself.
+   * Get all (sub)-paths through the given nodes connected only via blank edges.
    *
-   * @param pNode the node to get the neighbors for.
-   * @return all nodes connected to this node via blank edges, including the node itself.
+   * @param pNode the node to get the blank paths for.
+   * @return all (sub)-paths through the given nodes connected only via blank edges.
    */
-  public static Set<CFANode> getBlankNeighbors(CFANode pNode) {
-    Set<CFANode> visited = Sets.newHashSet();
-    Queue<CFANode> waitlist = Queues.newArrayDeque();
-    waitlist.offer(pNode);
-    visited.add(pNode);
+  public static Iterable<List<CFANode>> getBlankPaths(CFANode pNode) {
+    List<List<CFANode>> blankPaths = new ArrayList<>();
+    Queue<List<CFANode>> waitlist = Queues.newArrayDeque();
+    waitlist.offer(ImmutableList.of(pNode));
     while (!waitlist.isEmpty()) {
-      CFANode current = waitlist.poll();
-      for (BlankEdge enteringEdge : CFAUtils.enteringEdges(current).filter(BlankEdge.class)) {
-        CFANode predecessor = enteringEdge.getPredecessor();
-        if (visited.add(predecessor)) {
-          waitlist.offer(predecessor);
-        }
-      }
-      for (BlankEdge leavingEdge : CFAUtils.leavingEdges(current).filter(BlankEdge.class)) {
-        CFANode successor = leavingEdge.getSuccessor();
-        if (visited.add(successor)) {
-          waitlist.offer(successor);
+      List<CFANode> currentPath = waitlist.poll();
+      CFANode pathSucc = currentPath.get(currentPath.size() - 1);
+      List<BlankEdge> leavingBlankEdges =
+          CFAUtils.leavingEdges(pathSucc).filter(BlankEdge.class).toList();
+      if (pathSucc.getNumLeavingEdges() <= 0
+          || leavingBlankEdges.size() < pathSucc.getNumLeavingEdges()) {
+        blankPaths.add(currentPath);
+      } else {
+        for (CFAEdge leavingEdge : leavingBlankEdges) {
+          CFANode successor = leavingEdge.getSuccessor();
+          if (!currentPath.contains(successor)) {
+            List<CFANode> newPath =
+                ImmutableList.<CFANode>builder().addAll(currentPath).add(successor).build();
+            waitlist.offer(newPath);
+          }
         }
       }
     }
-    return visited;
+    waitlist.addAll(blankPaths);
+    blankPaths.clear();
+    while (!waitlist.isEmpty()) {
+      List<CFANode> currentPath = waitlist.poll();
+      CFANode pathPred = currentPath.get(0);
+      List<BlankEdge> enteringBlankEdges =
+          CFAUtils.enteringEdges(pathPred).filter(BlankEdge.class).toList();
+      if (pathPred.getNumEnteringEdges() <= 0
+          || enteringBlankEdges.size() < pathPred.getNumEnteringEdges()) {
+        blankPaths.add(currentPath);
+      } else {
+        for (CFAEdge enteringEdge : enteringBlankEdges) {
+          CFANode predecessor = enteringEdge.getPredecessor();
+          if (!currentPath.contains(predecessor)) {
+            List<CFANode> newPath =
+                ImmutableList.<CFANode>builder().add(predecessor).addAll(currentPath).build();
+            waitlist.offer(newPath);
+          }
+        }
+      }
+    }
+    return blankPaths;
   }
 }