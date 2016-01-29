c6435e6d1e22d9c77b644e76dce643a644f73239
pwendler <pwendler@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2016-01-29 11:19:07 +0000

    Fix #238: CDT gives incorrect types of expressions, we need to convert array types to pointer types ourselves, especially when declaring temporary variables.
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@19506 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/cfa/parser/eclipse/c/ASTConverter.java
+++ src/org/sosy_lab/cpachecker/cfa/parser/eclipse/c/ASTConverter.java
@@ -628,6 +628,14 @@ class ASTConverter {
     // TODO: consider always adding a const modifier if there is an initializer
     CType type = (initializer == null) ? CTypes.withoutConst(pType) : pType;
 
+    if (type instanceof CArrayType) {
+      // Replace with pointer type.
+      // This should actually be handled by Eclipse, because the C standard says in §5.4.2.1 (3)
+      // that array types of operands are converted to pointer types except in a very few
+      // specific cases (for which there will never be a temporary variable).
+      type = new CPointerType(type.isConst(), type.isVolatile(), ((CArrayType) type).getType());
+    }
+
     CVariableDeclaration decl = new CVariableDeclaration(loc,
                                                scope.isGlobalScope(),
                                                CStorageClass.AUTO,
