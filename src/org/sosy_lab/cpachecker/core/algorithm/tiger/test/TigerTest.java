810cf1299a89c4054c9d662fc2321a37c8b367d2
buerdek <buerdek@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2016-02-11 13:42:48 +0000

    added tiger test for testing TGAs without omega transition.
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/checkerInclTiger@19766 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/core/algorithm/tiger/test/TigerTest.java
+++ src/org/sosy_lab/cpachecker/core/algorithm/tiger/test/TigerTest.java
@@ -1034,4 +1034,33 @@ public class TigerTest {
     assertTrue(TigerTestHelper.validPresenceConditions(testSuite, faseTS, faseFm));
   }
 
+  /*
+   * Test various configurations - variants
+   */
+
+  /**
+   * Type:        variant
+   * Analysis:    predicate
+   * Specialties: multiple test goals
+   */
+  @Test
+  public void variants_example_noUseOfOmegaLabel() throws Exception {
+    Map<String, String> prop = TigerTestHelper.getConfigurationFromPropertiesFile(
+        new File("config/tiger-variants-powerset.properties"));
+    prop.put("tiger.numberOfTestGoalsPerRun", "-1");
+    prop.put("tiger.useOmegaLabel", "false");
+    prop.put("tiger.fqlQuery",
+        "COVER \"EDGES(ID)*\".(EDGES(@LABEL(G1))+EDGES(@LABEL(G2))+EDGES(@LABEL(G3))+EDGES(@LABEL(G4))).\"EDGES(ID)*\"");
+
+    TestResults results = CPATestRunner.run(prop, EXAMPLE_C);
+    AlgorithmResult result = results.getCheckerResult().getAlgorithmResult();
+
+    assertThat(result).isInstanceOf(TestSuite.class);
+    TestSuite testSuite = (TestSuite) result;
+
+    assertThat(testSuite.getNumberOfFeasibleTestGoals()).isEqualTo(3);
+    assertThat(testSuite.getNumberOfInfeasibleTestGoals()).isEqualTo(1);
+    assertThat(testSuite.getNumberOfTimedoutTestGoals()).isEqualTo(0);
+  }
+
 }
