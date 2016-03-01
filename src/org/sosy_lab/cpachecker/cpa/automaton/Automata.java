a0f09fe4e4e75d89a5dd87a65de0ba8c38cef999
dangl <dangl@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2016-03-01 12:18:52 +0000

    Add a utility automaton specifying loop heads.
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@19995 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

---
+++ src/org/sosy_lab/cpachecker/cpa/automaton/Automata.java
@@ -0,0 +1,74 @@
+/*
+ *  CPAchecker is a tool for configurable software verification.
+ *  This file is part of CPAchecker.
+ *
+ *  Copyright (C) 2007-2016  Dirk Beyer
+ *  All rights reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *
+ *  CPAchecker web page:
+ *    http://cpachecker.sosy-lab.org
+ */
+package org.sosy_lab.cpachecker.cpa.automaton;
+
+import java.util.Collections;
+import java.util.List;
+
+import com.google.common.collect.Lists;
+
+
+public class Automata {
+
+  private static final Automaton LOOP_HEAD_TARGET_AUTOMATON;
+
+  static {
+    String initStateName = "Init";
+    AutomatonTransition toInit = new AutomatonTransition(
+        AutomatonBoolExpr.TRUE,
+        Collections.<AutomatonBoolExpr>emptyList(),
+        Collections.<AutomatonAction>emptyList(),
+        initStateName);
+
+    AutomatonInternalState targetState = AutomatonInternalState.ERROR;
+    AutomatonTransition toTarget = new AutomatonTransition(
+        AutomatonBoolExpr.MatchLoopHead.INSTANCE,
+        Collections.<AutomatonBoolExpr>emptyList(),
+        Collections.<AutomatonAction>emptyList(),
+        targetState);
+
+    AutomatonInternalState initState = new AutomatonInternalState(initStateName, Lists.newArrayList(toInit, toTarget), false, true);
+
+    List<AutomatonInternalState> states = Lists.newArrayList(initState, targetState);
+
+    try {
+      LOOP_HEAD_TARGET_AUTOMATON = new Automaton(
+          "LoopHeadTarget",
+          Collections.<String, AutomatonVariable>emptyMap(),
+          states,
+          initStateName);
+    } catch (InvalidAutomatonException e) {
+      throw new AssertionError("Automaton built in code should be valid.");
+    }
+  }
+
+  public static Automaton getLoopHeadTargetAutomaton() {
+    return LOOP_HEAD_TARGET_AUTOMATON;
+  }
+
+  private Automata() {
+
+  }
+
+}
