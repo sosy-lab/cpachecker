eb56134d6be13347cd628aefb11bd3dcd943c538
andrianov <andrianov@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2016-03-11 12:22:14 +0000

    Implement new BlockPartitioningBuilder within an option
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/DelegatingBAMRefinerLDV@20200 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/cfa/blocks/builder/BlockPartitioningBuilder.java
+++ src/org/sosy_lab/cpachecker/cfa/blocks/builder/BlockPartitioningBuilder.java
@@ -27,8 +27,6 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Iterator;
-import java.util.LinkedList;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
@@ -44,9 +42,7 @@ import org.sosy_lab.cpachecker.cfa.model.c.CFunctionCallEdge;
 import org.sosy_lab.cpachecker.util.CFATraversal;
 import org.sosy_lab.cpachecker.util.CFAUtils;
 
-import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Iterables;
-import com.google.common.collect.Sets;
 
 
 /**
@@ -56,153 +52,54 @@ public class BlockPartitioningBuilder {
 
   private static final CFATraversal TRAVERSE_CFA_INSIDE_FUNCTION = CFATraversal.dfs().ignoreFunctionCalls();
 
-  private final Map<CFANode, Set<ReferencedVariable>> referencedVariablesMap = new HashMap<>();
-  private final Map<CFANode, Set<CFANode>> callNodesMap = new HashMap<>();
-  private final Map<CFANode, Set<CFANode>> returnNodesMap = new HashMap<>();
-  private final Map<CFANode, Set<FunctionEntryNode>> innerFunctionCallsMap = new HashMap<>();
-  private final Map<CFANode, Set<CFANode>> blockNodesMap = new HashMap<>();
+  protected final Map<CFANode, Set<ReferencedVariable>> referencedVariablesMap = new HashMap<>();
+  protected final Map<CFANode, Set<CFANode>> callNodesMap = new HashMap<>();
+  protected final Map<CFANode, Set<CFANode>> returnNodesMap = new HashMap<>();
+  protected final Map<CFANode, Set<FunctionEntryNode>> innerFunctionCallsMap = new HashMap<>();
+  protected final Map<CFANode, Set<CFANode>> blockNodesMap = new HashMap<>();
 
   public BlockPartitioningBuilder() {}
 
   public BlockPartitioning build(CFANode mainFunction) {
-
-    Map<CFANode, Set<FunctionEntryNode>> workCopyOfInnerFunctionCalls = new HashMap<>();
-
-    /* We chose one representative from every loop.
-     * This map stores for the node its representative.
-     */
-    Map<CFANode, CFANode> loopMapping = new HashMap<>();
-
-    //Deep clone, because we will delete nodes
-    for (CFANode node : innerFunctionCallsMap.keySet()) {
-      Set<FunctionEntryNode> newSet = Sets.newHashSet(innerFunctionCallsMap.get(node));
-      workCopyOfInnerFunctionCalls.put(node, newSet);
+    //fixpoint iteration to take inner function calls into account for referencedVariables and callNodesMap
+    boolean changed = true;
+outer: while (changed) {
+      changed = false;
+      for (Entry<CFANode, Set<ReferencedVariable>> entry : referencedVariablesMap.entrySet()) {
+        CFANode node = entry.getKey();
+        for (CFANode calledFun : innerFunctionCallsMap.get(node)) {
+          Set<ReferencedVariable> functionVars = referencedVariablesMap.get(calledFun);
+          Set<CFANode> functionBody = blockNodesMap.get(calledFun);
+          if (functionVars == null || functionBody == null) {
+            assert functionVars == null && functionBody == null;
+            //compute it only the fly
+            functionBody = TRAVERSE_CFA_INSIDE_FUNCTION.collectNodesReachableFrom(calledFun);
+            functionVars = collectReferencedVariables(functionBody);
+            //and save it
+            blockNodesMap.put(calledFun, functionBody);
+            referencedVariablesMap.put(calledFun, functionVars);
+            innerFunctionCallsMap.put(calledFun, collectInnerFunctionCalls(functionBody));
+            changed = true;
+            continue outer;
+          }
+          if (entry.getValue().addAll(functionVars)) {
+            changed = true;
+          }
+          if (blockNodesMap.get(node).addAll(functionBody)) {
+            changed = true;
+          }
+        }
+      }
     }
-
-    //Set of not handled CFANodes
-    Set<CFANode> workCopyOfCFANodes = Sets.newHashSet(referencedVariablesMap.keySet());
-    do {
-      //loop for finding single nodes
-      Set<CFANode> SingleNodes = new HashSet<>();
-      do {
-        SingleNodes.clear();
-        for (CFANode node : workCopyOfCFANodes) {
-          Set<FunctionEntryNode> calls = workCopyOfInnerFunctionCalls.get(node);
-          //Remove single-node loop
-          calls.remove(node);
-          if (calls.isEmpty()) {
-            SingleNodes.add(node);
-          }
-        }
-        if (!SingleNodes.isEmpty()) {
-          workCopyOfCFANodes.removeAll(SingleNodes);
-
-          for (CFANode node : workCopyOfCFANodes) {
-            Iterator<FunctionEntryNode> iterator = workCopyOfInnerFunctionCalls.get(node).iterator();
-            while (iterator.hasNext()) {
-              FunctionEntryNode calledFun = iterator.next();
-              if (SingleNodes.contains(calledFun)) {
-                iterator.remove();
-                joinFunctionPartitioning(node, calledFun);
-              }
-            }
-          }
-        }
-      } while (!SingleNodes.isEmpty());
-
-      if (workCopyOfCFANodes.isEmpty()) {
-        break;
-      }
-      //Detect a recursion loop
-      LinkedList<CFANode> foundedRecursionLoop = new LinkedList<>();
-      /* Create a random path by getting the first function call from every node
-       * and find the first node which is repeated
-       */
-      CFANode representativeNode = workCopyOfCFANodes.iterator().next();
-      while (!foundedRecursionLoop.contains(representativeNode)) {
-        foundedRecursionLoop.add(representativeNode);
-        representativeNode = workCopyOfInnerFunctionCalls.get(representativeNode).iterator().next();
-        assert (representativeNode != null);
-      }
-
-      Set<FunctionEntryNode> functionsCalledFromTheLoop = new HashSet<>();
-      //Remove the first elements, which are not included in the loop
-      while (!foundedRecursionLoop.pollFirst().equals(representativeNode)) {}
-      //Join all partitions of functions from the loop
-      for (CFANode recursiveCaller : foundedRecursionLoop) {
-        loopMapping.put(recursiveCaller, representativeNode);
-        joinFunctionPartitioning(representativeNode, recursiveCaller);
-        workCopyOfCFANodes.remove(recursiveCaller);
-        functionsCalledFromTheLoop.addAll(workCopyOfInnerFunctionCalls.get(recursiveCaller));
-        //Remove the handled node from others
-        for (CFANode node : workCopyOfCFANodes) {
-          Set<FunctionEntryNode> callers = workCopyOfInnerFunctionCalls.get(node);
-          if (callers.remove(recursiveCaller)) {
-            //Do not add single node loops
-            if (!node.equals(representativeNode)) {
-              // We should add to callers one node from the loop instead of removed one.
-              callers.add((FunctionEntryNode) representativeNode);
-            }
-          }
-        }
-      }
-      foundedRecursionLoop.add(representativeNode);
-      functionsCalledFromTheLoop.removeAll(foundedRecursionLoop);
-      //Add to chosen node (representative) all function calls from removed functions
-      workCopyOfInnerFunctionCalls.get(representativeNode).addAll(functionsCalledFromTheLoop);
-
-    } while (!workCopyOfCFANodes.isEmpty()) ;
-
-    //Try to optimize the memory
-    Map<CFANode, ImmutableSet<ReferencedVariable>> immutableVariablesMap = new HashMap<>();
-    Map<CFANode, ImmutableSet<CFANode>> immutableNodesMap = new HashMap<>();
-    //Resolve loop mapping
-    for (CFANode node : loopMapping.keySet()) {
-      CFANode mappedNode = loopMapping.get(node);
-      while (loopMapping.containsKey(mappedNode)) {
-        mappedNode = loopMapping.get(mappedNode);
-      }
-      /* We put the same object, because new Block() makes copy of these maps,
-       * so we do not care about this equality
-       */
-      ImmutableSet<ReferencedVariable> resultVars;
-      ImmutableSet<CFANode> resultNodes;
-      if (!immutableVariablesMap.containsKey(mappedNode)) {
-        resultVars = ImmutableSet.copyOf(referencedVariablesMap.get(mappedNode));
-        immutableVariablesMap.put(mappedNode, resultVars);
-      } else {
-        resultVars = immutableVariablesMap.get(mappedNode);
-      }
-      immutableVariablesMap.put(node, resultVars);
-      if (!immutableNodesMap.containsKey(mappedNode)) {
-        resultNodes = ImmutableSet.copyOf(blockNodesMap.get(mappedNode));
-        immutableNodesMap.put(mappedNode, resultNodes);
-      } else {
-        resultNodes = immutableNodesMap.get(mappedNode);
-      }
-      immutableNodesMap.put(node, resultNodes);
-    }
-
-    //now we can create the Blocks for the BlockPartitioning
+    //now we can create the Blocks   for the BlockPartitioning
     Collection<Block> blocks = new ArrayList<>(returnNodesMap.keySet().size());
-    for (CFANode key : returnNodesMap.keySet()) {
-      if (immutableVariablesMap.containsKey(key)) {
-        assert immutableNodesMap.containsKey(key);
-        blocks.add(new Block(immutableVariablesMap.get(key), callNodesMap.get(key), returnNodesMap.get(key), immutableNodesMap.get(key)));
-      } else {
-        blocks.add(new Block(ImmutableSet.copyOf(referencedVariablesMap.get(key)), callNodesMap.get(key),
-            returnNodesMap.get(key), ImmutableSet.copyOf(blockNodesMap.get(key))));
-      }
+    for (Entry<CFANode, Set<CFANode>> entry : returnNodesMap.entrySet()) {
+      CFANode key = entry.getKey();
+      blocks.add(new Block(referencedVariablesMap.get(key), callNodesMap.get(key), entry.getValue(), blockNodesMap.get(key)));
     }
     return new BlockPartitioning(blocks, mainFunction);
   }
 
-  private void joinFunctionPartitioning(CFANode node, CFANode caller) {
-    Set<ReferencedVariable> functionVars = referencedVariablesMap.get(caller);
-    Set<CFANode> functionBody = blockNodesMap.get(caller);
-    referencedVariablesMap.get(node).addAll(functionVars);
-    blockNodesMap.get(node).addAll(functionBody);
-  }
 
   /**
    * @param nodes Nodes from which Block should be created; if the set of nodes contains inner function calls, the called function body should NOT be included
