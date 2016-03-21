e1bb26edb5ca3641df03209028c497f254a5a132
andrianov <andrianov@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2016-03-21 12:50:34 +0000

    Fix expression simplification in ValueAnalysis
    simplifyAssumption() does not consider an operator in an expression. Thus, the analysis may convert the condition 'if(a != 0)' into 'if(!a)', if the variable 'a' has boolean type. 
    Add support of two operators: equals (==) and not_equals (!=). Let someone more confident adds something else.
    
    
    
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@20376 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/core/defaults/ForwardingTransferRelation.java
+++ src/org/sosy_lab/cpachecker/core/defaults/ForwardingTransferRelation.java
@@ -37,6 +37,7 @@ import org.sosy_lab.cpachecker.cfa.ast.AFunctionCall;
 import org.sosy_lab.cpachecker.cfa.ast.AParameterDeclaration;
 import org.sosy_lab.cpachecker.cfa.ast.AStatement;
 import org.sosy_lab.cpachecker.cfa.ast.c.CBinaryExpression;
+import org.sosy_lab.cpachecker.cfa.ast.c.CBinaryExpression.BinaryOperator;
 import org.sosy_lab.cpachecker.cfa.ast.c.CDeclaration;
 import org.sosy_lab.cpachecker.cfa.ast.c.CExpression;
 import org.sosy_lab.cpachecker.cfa.ast.c.CFunctionCall;
@@ -176,6 +177,7 @@ public abstract class ForwardingTransferRelation<S, T extends AbstractState, P e
 
     case AssumeEdge:
       final AssumeEdge assumption = (AssumeEdge) cfaEdge;
+      final AExpression aExpression = assumption.getExpression();
       successor = handleAssumption(assumption, assumption.getExpression(), assumption.getTruthAssumption());
       break;
 
@@ -660,12 +662,21 @@ public abstract class ForwardingTransferRelation<S, T extends AbstractState, P e
     if (isBooleanExpression(pExpression)) {
       if (pExpression instanceof CBinaryExpression) {
         CBinaryExpression binExp = (CBinaryExpression) pExpression;
+        BinaryOperator operator = binExp.getOperator();
         if (isBooleanExpression(binExp.getOperand1())
             && binExp.getOperand2().equals(CIntegerLiteralExpression.ZERO)) {
-          return simplifyAssumption(binExp.getOperand1(), !pAssumeTruth);
+          if (operator == BinaryOperator.EQUALS) {
+            return simplifyAssumption(binExp.getOperand1(), !pAssumeTruth);
+          } else if (operator == BinaryOperator.NOT_EQUALS) {
+            return simplifyAssumption(binExp.getOperand1(), pAssumeTruth);
+          } //TODO what else?
         } else if (isBooleanExpression(binExp.getOperand2())
             && binExp.getOperand1().equals(CIntegerLiteralExpression.ZERO)) {
-          return simplifyAssumption(binExp.getOperand2(), !pAssumeTruth);
+          if (operator == BinaryOperator.EQUALS) {
+            return simplifyAssumption(binExp.getOperand2(), !pAssumeTruth);
+          } else if (operator == BinaryOperator.NOT_EQUALS) {
+            return simplifyAssumption(binExp.getOperand2(), pAssumeTruth);
+          } //TODO what else?
         }
       }
     }
