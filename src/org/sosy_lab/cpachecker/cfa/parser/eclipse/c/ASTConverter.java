c43673c14c00c1db396794a87574c851c16d45df
pwendler <pwendler@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2016-04-05 14:02:14 +0000

    Fix expression type of _Alignof expressions, it is always int but CDT provides an incorrect type.
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@20582 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/cfa/parser/eclipse/c/ASTConverter.java
+++ src/org/sosy_lab/cpachecker/cfa/parser/eclipse/c/ASTConverter.java
@@ -23,18 +23,14 @@
  */
 package org.sosy_lab.cpachecker.cfa.parser.eclipse.c;
 
-import static org.sosy_lab.cpachecker.cfa.types.c.CTypes.*;
+import static org.sosy_lab.cpachecker.cfa.types.c.CTypes.withoutConst;
+import static org.sosy_lab.cpachecker.cfa.types.c.CTypes.withoutVolatile;
 
-import java.math.BigInteger;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Deque;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.logging.Level;
-
-import javax.annotation.Nullable;
+import com.google.common.base.Function;
+import com.google.common.base.Optional;
+import com.google.common.base.Strings;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Lists;
 
 import org.eclipse.cdt.core.dom.ast.IASTArrayDeclarator;
 import org.eclipse.cdt.core.dom.ast.IASTArrayModifier;
@@ -140,6 +136,7 @@ import org.sosy_lab.cpachecker.cfa.ast.c.CSimpleDeclaration;
 import org.sosy_lab.cpachecker.cfa.ast.c.CStatement;
 import org.sosy_lab.cpachecker.cfa.ast.c.CTypeDefDeclaration;
 import org.sosy_lab.cpachecker.cfa.ast.c.CTypeIdExpression;
+import org.sosy_lab.cpachecker.cfa.ast.c.CTypeIdExpression.TypeIdOperator;
 import org.sosy_lab.cpachecker.cfa.ast.c.CUnaryExpression;
 import org.sosy_lab.cpachecker.cfa.ast.c.CUnaryExpression.UnaryOperator;
 import org.sosy_lab.cpachecker.cfa.ast.c.CVariableDeclaration;
@@ -173,11 +170,16 @@ import org.sosy_lab.cpachecker.exceptions.UnrecognizedCCodeException;
 import org.sosy_lab.cpachecker.util.Pair;
 import org.sosy_lab.cpachecker.util.Triple;
 
-import com.google.common.base.Function;
-import com.google.common.base.Optional;
-import com.google.common.base.Strings;
-import com.google.common.collect.ImmutableList;
-import com.google.common.collect.Lists;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.logging.Level;
+
+import javax.annotation.Nullable;
 
 @Options(prefix="cfa")
 class ASTConverter {
@@ -1369,8 +1371,16 @@ class ASTConverter {
   }
 
   private CTypeIdExpression convert(IASTTypeIdExpression e) {
-    return new CTypeIdExpression(getLocation(e), typeConverter.convert(e.getExpressionType()),
-        operatorConverter.convertTypeIdOperator(e), convert(e.getTypeId()));
+    TypeIdOperator typeIdOperator = operatorConverter.convertTypeIdOperator(e);
+    CType expressionType;
+    if (typeIdOperator == TypeIdOperator.ALIGNOF || typeIdOperator == TypeIdOperator.SIZEOF) {
+      // sizeof and _Alignof always return int, CDT sometimes provides wrong type
+      expressionType = CNumericTypes.INT;
+    } else {
+      expressionType = typeConverter.convert(e.getExpressionType());
+    }
+    return new CTypeIdExpression(
+        getLocation(e), expressionType, typeIdOperator, convert(e.getTypeId()));
   }
 
   private CExpression convert(IASTTypeIdInitializerExpression e) {
