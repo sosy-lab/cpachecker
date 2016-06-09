704f0e169ebee41800e4f544fc86075e9f774871
pwendler <pwendler@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2016-06-09 15:49:38 +0000

    If cpa.predicate.trackFunctionPointers is disabled, do not add constraint about addresses of functions, otherwise we might still track some facts about function pointer. Fixes #193.
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@21651 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/util/predicates/pathformula/pointeraliasing/PointerTargetSetBuilder.java
+++ src/org/sosy_lab/cpachecker/util/predicates/pathformula/pointeraliasing/PointerTargetSetBuilder.java
@@ -41,6 +41,7 @@ import org.sosy_lab.cpachecker.cfa.types.c.CCompositeType;
 import org.sosy_lab.cpachecker.cfa.types.c.CCompositeType.CCompositeTypeMemberDeclaration;
 import org.sosy_lab.cpachecker.cfa.types.c.CElaboratedType;
 import org.sosy_lab.cpachecker.cfa.types.c.CType;
+import org.sosy_lab.cpachecker.cfa.types.c.CTypes;
 import org.sosy_lab.cpachecker.util.Pair;
 import org.sosy_lab.cpachecker.util.Triple;
 import org.sosy_lab.cpachecker.util.predicates.pathformula.pointeraliasing.PointerTargetSet.CompositeField;
@@ -257,7 +258,13 @@ public interface PointerTargetSetBuilder {
 
       final BooleanFormula nextInequality = ptsMgr.getNextBaseAddressInequality(name, bases, lastBase);
       lastBase = name;
-      return nextInequality;
+      if (!options.trackFunctionPointers() && CTypes.isFunctionPointer(type)) {
+        // Avoid adding constraints about function addresses,
+        // otherwise we might track facts about function pointers for code like "if (p == &f)".
+        return formulaManager.getBooleanFormulaManager().makeBoolean(true);
+      } else {
+        return nextInequality;
+      }
     }
 
     @Override
