6e7dca0f4e68ecf78017e9a4bbe5cd47cb233a72
pwendler <pwendler@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2016-06-24 08:52:52 +0000

    Refactor BaseVisitor by extracting common case.
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@21903 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/util/predicates/pathformula/pointeraliasing/BaseVisitor.java
+++ src/org/sosy_lab/cpachecker/util/predicates/pathformula/pointeraliasing/BaseVisitor.java
@@ -23,33 +23,32 @@
  */
 package org.sosy_lab.cpachecker.util.predicates.pathformula.pointeraliasing;
 
-import org.sosy_lab.cpachecker.cfa.ast.c.CAddressOfLabelExpression;
 import org.sosy_lab.cpachecker.cfa.ast.c.CArraySubscriptExpression;
 import org.sosy_lab.cpachecker.cfa.ast.c.CBinaryExpression;
 import org.sosy_lab.cpachecker.cfa.ast.c.CCastExpression;
-import org.sosy_lab.cpachecker.cfa.ast.c.CCharLiteralExpression;
 import org.sosy_lab.cpachecker.cfa.ast.c.CComplexCastExpression;
-import org.sosy_lab.cpachecker.cfa.ast.c.CExpressionVisitor;
+import org.sosy_lab.cpachecker.cfa.ast.c.CExpression;
 import org.sosy_lab.cpachecker.cfa.ast.c.CFieldReference;
-import org.sosy_lab.cpachecker.cfa.ast.c.CFloatLiteralExpression;
 import org.sosy_lab.cpachecker.cfa.ast.c.CIdExpression;
-import org.sosy_lab.cpachecker.cfa.ast.c.CImaginaryLiteralExpression;
 import org.sosy_lab.cpachecker.cfa.ast.c.CIntegerLiteralExpression;
 import org.sosy_lab.cpachecker.cfa.ast.c.CPointerExpression;
-import org.sosy_lab.cpachecker.cfa.ast.c.CStringLiteralExpression;
-import org.sosy_lab.cpachecker.cfa.ast.c.CTypeIdExpression;
-import org.sosy_lab.cpachecker.cfa.ast.c.CUnaryExpression;
+import org.sosy_lab.cpachecker.cfa.ast.c.DefaultCExpressionVisitor;
 import org.sosy_lab.cpachecker.cfa.model.CFAEdge;
 import org.sosy_lab.cpachecker.cfa.types.c.CType;
 import org.sosy_lab.cpachecker.exceptions.UnrecognizedCCodeException;
 
 import javax.annotation.Nullable;
 
-class BaseVisitor implements CExpressionVisitor<Variable, UnrecognizedCCodeException> {
+class BaseVisitor extends DefaultCExpressionVisitor<Variable, UnrecognizedCCodeException> {
 
   BaseVisitor(final CFAEdge cfaEdge, final PointerTargetSetBuilder pts) {
     this.cfaEdge = cfaEdge;
     this.pts = pts;
+  }
+
+  @Override
+  protected Variable visitDefault(CExpression pExp) throws UnrecognizedCCodeException {
+    throw new UnrecognizedCCodeException("unexpected expression in lvalue", cfaEdge, pExp);
   }
 
   @Override
@@ -99,58 +98,13 @@ class BaseVisitor implements CExpressionVisitor<Variable, UnrecognizedCCodeExcep
   }
 
   @Override
-  public Variable visit(final CCharLiteralExpression e) throws UnrecognizedCCodeException {
-    throw new UnrecognizedCCodeException("Char literal in place of lvalue", cfaEdge, e);
-  }
-
-  @Override
-  public Variable visit(final CFloatLiteralExpression e) throws UnrecognizedCCodeException {
-    throw new UnrecognizedCCodeException("Float literal in place of lvalue", cfaEdge, e);
-  }
-
-  @Override
   public Variable visit(final CIntegerLiteralExpression e) throws UnrecognizedCCodeException {
     return null;
   }
 
   @Override
-  public Variable visit(final CStringLiteralExpression e) throws UnrecognizedCCodeException {
-    throw new UnrecognizedCCodeException("String literal in place of lvalue", cfaEdge, e);
-  }
-
-  @Override
-  public Variable visit(CImaginaryLiteralExpression e) throws UnrecognizedCCodeException {
-    throw new UnrecognizedCCodeException("Imaginary literal in place of lvalue", cfaEdge, e);
-  }
-
-  @Override
-  public Variable visit(final CTypeIdExpression e) throws UnrecognizedCCodeException {
-    throw new UnrecognizedCCodeException("TypeId in place of lvalue", cfaEdge, e);
-  }
-
-  @Override
-  public Variable visit(final CUnaryExpression e) throws UnrecognizedCCodeException {
-    switch (e.getOperator()) {
-    case AMPER:
-      throw new UnrecognizedCCodeException("Address in place of lvalue", cfaEdge, e);
-    case TILDE:
-    case MINUS:
-      throw new UnrecognizedCCodeException("Arithmetic in place of lvalue", cfaEdge, e);
-    case SIZEOF:
-      throw new UnrecognizedCCodeException("Constant in place of lvalue", cfaEdge, e);
-    default:
-      throw new UnrecognizedCCodeException("Unrecognized code in place of lvalue", cfaEdge, e);
-    }
-  }
-
-  @Override
   public Variable visit(final CPointerExpression e) throws UnrecognizedCCodeException {
     return null;
-  }
-
-  @Override
-  public Variable visit(CAddressOfLabelExpression e) throws UnrecognizedCCodeException {
-    throw new UnrecognizedCCodeException("Addess of label in place of lvalue", cfaEdge, e);
   }
 
   @Nullable
