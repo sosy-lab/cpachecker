125846833d90d5cce47cff8921bfc6077f0efdcc
George Karpenkov <true.cheshire@gmail.com>
2016-07-12 18:00:51 +0000

    CPA for overflow tracking.
    Creates target states on overflows.
    
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@22253 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

---
+++ src/org/sosy_lab/cpachecker/cpa/overflow/OverflowCPA.java
@@ -0,0 +1,207 @@
+/*
+ * CPAchecker is a tool for configurable software verification.
+ *  This file is part of CPAchecker.
+ *
+ *  Copyright (C) 2007-2016  Dirk Beyer
+ *  All rights reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *
+ *  CPAchecker web page:
+ *    http://cpachecker.sosy-lab.org
+ */
+package org.sosy_lab.cpachecker.cpa.overflow;
+
+import com.google.common.collect.ImmutableList;
+
+import org.sosy_lab.common.log.LogManager;
+import org.sosy_lab.cpachecker.cfa.CFA;
+import org.sosy_lab.cpachecker.cfa.ast.FileLocation;
+import org.sosy_lab.cpachecker.cfa.ast.c.CBinaryExpression.BinaryOperator;
+import org.sosy_lab.cpachecker.cfa.ast.c.CBinaryExpressionBuilder;
+import org.sosy_lab.cpachecker.cfa.ast.c.CExpression;
+import org.sosy_lab.cpachecker.cfa.ast.c.CExpressionStatement;
+import org.sosy_lab.cpachecker.cfa.ast.c.CIntegerLiteralExpression;
+import org.sosy_lab.cpachecker.cfa.model.CFAEdge;
+import org.sosy_lab.cpachecker.cfa.model.CFANode;
+import org.sosy_lab.cpachecker.cfa.model.c.CAssumeEdge;
+import org.sosy_lab.cpachecker.core.defaults.AutomaticCPAFactory;
+import org.sosy_lab.cpachecker.core.defaults.FlatLatticeNoTopDomain;
+import org.sosy_lab.cpachecker.core.defaults.MergeSepOperator;
+import org.sosy_lab.cpachecker.core.defaults.SingleEdgeTransferRelation;
+import org.sosy_lab.cpachecker.core.defaults.SingletonPrecision;
+import org.sosy_lab.cpachecker.core.defaults.StaticPrecisionAdjustment;
+import org.sosy_lab.cpachecker.core.defaults.StopSepOperator;
+import org.sosy_lab.cpachecker.core.interfaces.AbstractDomain;
+import org.sosy_lab.cpachecker.core.interfaces.AbstractState;
+import org.sosy_lab.cpachecker.core.interfaces.CPAFactory;
+import org.sosy_lab.cpachecker.core.interfaces.ConfigurableProgramAnalysis;
+import org.sosy_lab.cpachecker.core.interfaces.MergeOperator;
+import org.sosy_lab.cpachecker.core.interfaces.Precision;
+import org.sosy_lab.cpachecker.core.interfaces.PrecisionAdjustment;
+import org.sosy_lab.cpachecker.core.interfaces.StateSpacePartition;
+import org.sosy_lab.cpachecker.core.interfaces.StopOperator;
+import org.sosy_lab.cpachecker.core.interfaces.TransferRelation;
+import org.sosy_lab.cpachecker.cpa.assumptions.genericassumptions.ArithmeticOverflowAssumptionBuilder;
+import org.sosy_lab.cpachecker.exceptions.CPATransferException;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * CPA for detecting overflows.
+ */
+public class OverflowCPA
+    extends SingleEdgeTransferRelation
+    implements ConfigurableProgramAnalysis{
+
+  private final CFA cfa;
+  private final LogManager logger;
+  private final CBinaryExpressionBuilder expressionBuilder;
+  private final AbstractDomain domain;
+
+  public static CPAFactory factory() {
+    return AutomaticCPAFactory.forType(OverflowCPA.class);
+  }
+
+  private OverflowCPA(CFA pCfa, LogManager pLogger) {
+    cfa = pCfa;
+    logger = pLogger;
+    expressionBuilder = new CBinaryExpressionBuilder(pCfa.getMachineModel(), pLogger);
+    domain = new FlatLatticeNoTopDomain();
+  }
+
+  @Override
+  public Collection<? extends AbstractState> getAbstractSuccessorsForEdge(
+      AbstractState state,
+      Precision precision,
+      CFAEdge cfaEdge
+  ) throws CPATransferException, InterruptedException {
+    OverflowState prev = (OverflowState) state;
+
+    if (prev.hasOverflow()) {
+
+      // Once we have an overflow there is no need to continue.
+      return Collections.emptyList();
+    }
+
+    ArithmeticOverflowAssumptionBuilder builder = new
+        ArithmeticOverflowAssumptionBuilder(cfa, logger);
+
+    List<CExpression> assumptions =
+        builder.assumptionsForEdge(cfaEdge);
+
+    // No overflows <=> all assumptions hold.
+    List<CExpression> noOverflows;
+    if (assumptions.isEmpty()) {
+      noOverflows = Collections.emptyList();
+    } else {
+      noOverflows = assumptions;
+    }
+
+    ImmutableList.Builder<OverflowState> outStates = ImmutableList.builder();
+    outStates.addAll(assumptions.stream().map(
+
+        // Overflow <=> there exists a violating assumption.
+        a -> mkState(Collections.singletonList(mkNot(a)), cfaEdge, true)
+    ).iterator());
+    outStates.add(mkState(noOverflows, cfaEdge, false));
+    return outStates.build();
+  }
+
+  private OverflowState mkState(
+      List<CExpression> expression,
+      CFAEdge pEdge,
+      boolean pHasOverflow
+      ) {
+    return new OverflowState(
+        expression.stream().map(e -> mkStatement(e)).collect(Collectors.toList()),
+        expression.stream().map(
+            e -> mkAssumeEdge(
+                pEdge.getPredecessor(),
+                e,
+                pEdge.getSuccessor()
+            )
+        ).collect(Collectors.toList()),
+        pHasOverflow);
+  }
+
+  private CAssumeEdge mkAssumeEdge(
+      CFANode predecessor,
+      CExpression pExpression,
+      CFANode successor) {
+    return new CAssumeEdge(
+        pExpression.toASTString(),
+        FileLocation.DUMMY,
+        predecessor,
+        successor,
+        pExpression,
+        true
+    );
+  }
+
+  private CExpressionStatement mkStatement(CExpression pExpression) {
+    return new CExpressionStatement(FileLocation.DUMMY, pExpression);
+  }
+
+  private CExpression mkNot(CExpression arg) {
+    return expressionBuilder.buildBinaryExpressionUnchecked(
+        arg,
+        CIntegerLiteralExpression.ZERO,
+        BinaryOperator.EQUALS
+    );
+  }
+
+  @Override
+  public AbstractDomain getAbstractDomain() {
+    return domain;
+  }
+
+  @Override
+  public TransferRelation getTransferRelation() {
+    return this;
+  }
+
+  @Override
+  public MergeOperator getMergeOperator() {
+    return MergeSepOperator.getInstance();
+  }
+
+  @Override
+  public StopOperator getStopOperator() {
+    return new StopSepOperator(domain);
+  }
+
+  @Override
+  public PrecisionAdjustment getPrecisionAdjustment() {
+    return StaticPrecisionAdjustment.getInstance();
+  }
+
+  @Override
+  public AbstractState getInitialState(
+      CFANode node, StateSpacePartition partition) throws InterruptedException {
+    return new OverflowState(
+        ImmutableList.of(),
+        ImmutableList.of(),
+        false);
+  }
+
+  @Override
+  public Precision getInitialPrecision(
+      CFANode node, StateSpacePartition partition) throws InterruptedException {
+    return SingletonPrecision.getInstance();
+  }
+}
---
+++ src/org/sosy_lab/cpachecker/cpa/overflow/OverflowState.java
@@ -0,0 +1,136 @@
+/*
+ * CPAchecker is a tool for configurable software verification.
+ *  This file is part of CPAchecker.
+ *
+ *  Copyright (C) 2007-2016  Dirk Beyer
+ *  All rights reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *
+ *  CPAchecker web page:
+ *    http://cpachecker.sosy-lab.org
+ */
+package org.sosy_lab.cpachecker.cpa.overflow;
+
+import com.google.common.base.Joiner;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
+
+import org.sosy_lab.cpachecker.cfa.ast.AStatement;
+import org.sosy_lab.cpachecker.cfa.ast.c.CStatement;
+import org.sosy_lab.cpachecker.cfa.model.AssumeEdge;
+import org.sosy_lab.cpachecker.core.interfaces.AbstractStateWithAssumptions;
+import org.sosy_lab.cpachecker.core.interfaces.Graphable;
+import org.sosy_lab.cpachecker.core.interfaces.Property;
+import org.sosy_lab.cpachecker.core.interfaces.Targetable;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+
+import javax.annotation.Nonnull;
+
+/**
+ * Abstract state for tracking overflows.
+ */
+class OverflowState implements AbstractStateWithAssumptions,
+                               Targetable,
+                               Graphable {
+
+  private final ImmutableList<CStatement> assumptions;
+  private final ImmutableList<AssumeEdge> assumeEdges;
+  private final boolean hasOverflow;
+  private static final String PROPERTY_OVERFLOW = "overflow";
+
+  public OverflowState(
+      List<CStatement> pAssumptions,
+      List<AssumeEdge> pAssumeEdges, boolean pHasOverflow) {
+    assumptions = ImmutableList.copyOf(pAssumptions);
+    assumeEdges = ImmutableList.copyOf(pAssumeEdges);
+    hasOverflow = pHasOverflow;
+  }
+
+  public boolean hasOverflow() {
+    return hasOverflow;
+  }
+
+  @Override
+  public List<? extends AStatement> getAssumptions() {
+    return assumptions;
+  }
+
+  @Override
+  public List<AssumeEdge> getAsAssumeEdges(String functionName) {
+    return assumeEdges;
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(assumptions, assumeEdges, hasOverflow);
+  }
+
+  @Override
+  public boolean equals(Object pO) {
+    if (this == pO) {
+      return true;
+    }
+    if (pO == null || getClass() != pO.getClass()) {
+      return false;
+    }
+    OverflowState that = (OverflowState) pO;
+    return hasOverflow == that.hasOverflow &&
+        Objects.equals(assumptions, that.assumptions) &&
+        Objects.equals(assumeEdges, that.assumeEdges);
+  }
+
+  @Override
+  public String toString() {
+    return "OverflowState{" +
+        "assumptions=" + assumptions +
+        ", assumeEdges=" + assumeEdges +
+        ", hasOverflow=" + hasOverflow +
+        '}';
+  }
+
+  @Override
+  public boolean isTarget() {
+    return hasOverflow;
+  }
+
+  @Nonnull
+  @Override
+  public Set<Property> getViolatedProperties() throws IllegalStateException {
+    return ImmutableSet.of(
+        new Property() {
+          @Override
+          public String toString() {
+            return PROPERTY_OVERFLOW;
+          }
+        }
+    );
+  }
+
+  @Override
+  public String toDOTLabel() {
+    if (hasOverflow) {
+      return Joiner.on('\n').join(assumptions);
+    }
+    return "";
+  }
+
+  @Override
+  public boolean shouldBeHighlighted() {
+    return false;
+  }
+}
