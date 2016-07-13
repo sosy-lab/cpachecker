bad5bf55d9277309c6eda0fa48beff2465dc7da3
pwendler <pwendler@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2016-07-13 15:01:51 +0000

    Revert "hashCode() and equals() for AssumeEdge"
    This reverts commit r22249.
    
    The current hashCode and equals is broken because it is not symmetric,
    and r22249 changed the semantics of these methods in a way
    that might be unexpected by other code.
    
    The underlying problem is better fixed by 22265.
    
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@22272 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/cfa/model/AssumeEdge.java
+++ src/org/sosy_lab/cpachecker/cfa/model/AssumeEdge.java
@@ -26,7 +26,6 @@ package org.sosy_lab.cpachecker.cfa.model;
 import org.sosy_lab.cpachecker.cfa.ast.AExpression;
 import org.sosy_lab.cpachecker.cfa.ast.FileLocation;
 
-import java.util.Objects;
 import java.util.Optional;
 
 
@@ -83,26 +82,5 @@ public class AssumeEdge extends AbstractCFAEdge {
   @Override
   public Optional<? extends AExpression> getRawAST() {
     return Optional.of(expression);
-  }
-
-  @Override
-  public boolean equals(Object pO) {
-    if (this == pO) {
-      return true;
-    }
-    if (pO == null || getClass() != pO.getClass()) {
-      return false;
-    }
-    if (!super.equals(pO)) {
-      return false;
-    }
-    AssumeEdge that = (AssumeEdge) pO;
-    return truthAssumption == that.truthAssumption &&
-        Objects.equals(expression, that.expression);
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(super.hashCode(), truthAssumption, expression);
   }
 }
