d6df9dbf872e1273f8dcdc4311fcbb6c8a9e742f
pwendler <pwendler@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2016-07-13 16:15:13 +0000

    Remove unused code of SourceLocationMapper and reduce visibility of remaining methods as far as possible.
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@22276 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/util/SourceLocationMapper.java
+++ src/org/sosy_lab/cpachecker/util/SourceLocationMapper.java
@@ -23,23 +23,14 @@
  */
 package org.sosy_lab.cpachecker.util;
 
-import java.util.Optional;
 import com.google.common.base.Preconditions;
 import com.google.common.base.Predicates;
-import com.google.common.collect.ComparisonChain;
 import com.google.common.collect.FluentIterable;
 import com.google.common.collect.Queues;
 import com.google.common.collect.Sets;
 
 import org.sosy_lab.cpachecker.cfa.ast.FileLocation;
 import org.sosy_lab.cpachecker.cfa.ast.c.CAstNode;
-import org.sosy_lab.cpachecker.cfa.ast.c.CDeclaration;
-import org.sosy_lab.cpachecker.cfa.ast.c.CExpression;
-import org.sosy_lab.cpachecker.cfa.ast.c.CIdExpression;
-import org.sosy_lab.cpachecker.cfa.ast.c.CIdExpressionCollectingVisitor;
-import org.sosy_lab.cpachecker.cfa.ast.c.CInitializer;
-import org.sosy_lab.cpachecker.cfa.ast.c.CInitializerList;
-import org.sosy_lab.cpachecker.cfa.ast.c.CVariableDeclaration;
 import org.sosy_lab.cpachecker.cfa.ast.c.FileLocationCollectingVisitor;
 import org.sosy_lab.cpachecker.cfa.model.CFAEdge;
 import org.sosy_lab.cpachecker.cfa.model.FunctionEntryNode;
@@ -53,9 +44,8 @@ import org.sosy_lab.cpachecker.cfa.model.c.CStatementEdge;
 import java.util.Collections;
 import java.util.Deque;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.Set;
-import java.util.SortedSet;
-import java.util.TreeSet;
 
 
 public class SourceLocationMapper {
@@ -66,17 +56,17 @@ public class SourceLocationMapper {
 
   }
 
-  public static abstract class FileNameDescriptor implements LocationDescriptor {
+  private static abstract class FileNameDescriptor implements LocationDescriptor {
 
     private final Optional<String> originFileName;
 
     private final boolean matchBaseName;
 
-    public FileNameDescriptor(String originFileName) {
+    private FileNameDescriptor(String originFileName) {
       this(originFileName, true);
     }
 
-    public FileNameDescriptor(String originFileName, boolean matchBaseName) {
+    private FileNameDescriptor(String originFileName, boolean matchBaseName) {
       this.originFileName = Optional.of(originFileName);
       this.matchBaseName = matchBaseName;
     }
@@ -131,13 +121,14 @@ public class SourceLocationMapper {
 
   public static class OriginLineDescriptor extends FileNameDescriptor implements LocationDescriptor {
 
-    public final int originLineNumber;
+    private final int originLineNumber;
 
     public OriginLineDescriptor(Optional<String> pOriginFileName, int pOriginLineNumber) {
       this(pOriginFileName, pOriginLineNumber, true);
     }
 
-    public OriginLineDescriptor(Optional<String> pOriginFileName, int pOriginLineNumber, boolean pMatchBaseName) {
+    private OriginLineDescriptor(
+        Optional<String> pOriginFileName, int pOriginLineNumber, boolean pMatchBaseName) {
       super(pOriginFileName, pMatchBaseName);
       this.originLineNumber = pOriginLineNumber;
     }
@@ -174,13 +165,14 @@ public class SourceLocationMapper {
 
   public static class OffsetDescriptor extends FileNameDescriptor implements LocationDescriptor {
 
-    public final int offset;
+    private final int offset;
 
     public OffsetDescriptor(Optional<String> pOriginFileName, int pOffset) {
       this(pOriginFileName, pOffset, true);
     }
 
-    public OffsetDescriptor(Optional<String> pOriginFileName, int pOffset, boolean pMatchBaseName) {
+    private OffsetDescriptor(
+        Optional<String> pOriginFileName, int pOffset, boolean pMatchBaseName) {
       super(pOriginFileName, pMatchBaseName);
       this.offset = pOffset;
     }
@@ -216,96 +208,12 @@ public class SourceLocationMapper {
     }
   }
 
-  public static Set<String> matchTokenNumbersToTokenStrings() {
-    return Collections.emptySet();
-  }
-
-  private static void collectLine(final SortedSet<Integer> target, final FileLocation loc, boolean overApproximateTokens) {
-    if (loc != null && !loc.equals(FileLocation.DUMMY)) {
-      if (overApproximateTokens) {
-        int lowerBound = loc.getStartingLineNumber();
-        int upperBound = loc.getEndingLineNumber();
-        if (target.size() > 0) {
-          lowerBound = Math.min(lowerBound, target.first());
-          upperBound = Math.max(upperBound, target.last());
-        }
-        for (int line=lowerBound; line<=upperBound; line++) {
-          target.add(line);
-        }
-      } else {
-        target.add(loc.getStartingLineNumber());
-      }
-    }
-  }
-
-  public static Set<Integer> collectTokensFrom(CAstNode astNode, boolean overApproximateTokens) {
-    final TreeSet<Integer> result = Sets.newTreeSet();
-    Set<FileLocation> locs = collectFileLocationsFrom(astNode);
-
-    for (FileLocation l: locs) {
-      collectLine(result, l, overApproximateTokens);
-    }
-
-    return result;
-  }
-
-  public static class RowAndColumn implements Comparable<RowAndColumn>{
-    public final int row;
-    public final int column;
-
-    public RowAndColumn(int row, int col) {
-      this.row = row;
-      this.column = col;
-    }
-
-    @Override
-    public String toString() {
-      return row + ":" + column;
-    }
-
-    @Override
-    public int compareTo(RowAndColumn pO) {
-      // Caution: inverted order here
-      return ComparisonChain.start().compare(pO.row, row).compare(pO.column, column).result();
-    }
-
-    @Override
-    public int hashCode() {
-      return 31 * (31 + column) + row;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-      if (this == obj) {
-        return true;
-      }
-      if (!(obj instanceof RowAndColumn)) {
-        return false;
-      }
-      RowAndColumn other = (RowAndColumn) obj;
-      return row == other.row
-          && column == other.column;
-    }
-  }
-
-  public static synchronized Set<RowAndColumn> collectRowsAndColsFrom(CAstNode astNode) {
-    final TreeSet<RowAndColumn> result = Sets.newTreeSet();
-    Set<FileLocation> locs = collectFileLocationsFrom(astNode);
-
-    for (FileLocation l: locs) {
-      RowAndColumn rc = new RowAndColumn(l.getStartingLineNumber(), l.getNodeOffset());
-      result.add(rc);
-    }
-
-    return result;
-  }
-
-  public static synchronized Set<FileLocation> collectFileLocationsFrom(CAstNode astNode) {
+  private static Set<FileLocation> collectFileLocationsFrom(CAstNode astNode) {
     final FileLocationCollectingVisitor visitor = new FileLocationCollectingVisitor();
     return astNode.accept(visitor);
   }
 
-  public static synchronized Set<CAstNode> getAstNodesFromCfaEdge(CFAEdge pEdge) {
+  private static Set<CAstNode> getAstNodesFromCfaEdge(CFAEdge pEdge) {
     final Set<CAstNode> result = Sets.newHashSet();
     final Deque<CFAEdge> edges = Queues.newArrayDeque();
 
@@ -357,7 +265,7 @@ public class SourceLocationMapper {
     return result;
   }
 
-  public static synchronized Set<FileLocation> getFileLocationsFromCfaEdge(CFAEdge pEdge) {
+  public static Set<FileLocation> getFileLocationsFromCfaEdge(CFAEdge pEdge) {
     Set<FileLocation> result = Sets.newHashSet();
 
     final Set<CAstNode> astNodes = getAstNodesFromCfaEdge(pEdge);
@@ -377,100 +285,4 @@ public class SourceLocationMapper {
     }
     return result;
   }
-
-  public static synchronized Set<RowAndColumn> getRowsAndColsFromCFAEdge(CFAEdge pEdge) {
-    final TreeSet<RowAndColumn> result = Sets.newTreeSet();
-    final Set<CAstNode> astNodes = getAstNodesFromCfaEdge(pEdge);
-
-    for (CAstNode n: astNodes) {
-      result.addAll(collectRowsAndColsFrom(n));
-    }
-
-    RowAndColumn rc = new RowAndColumn(
-        pEdge.getFileLocation().getStartingLineNumber(),
-        pEdge.getFileLocation().getNodeOffset());
-    result.add(rc);
-
-    return result;
-  }
-
-  public static Set<String> getEdgeVariableNames(CFAEdge subject) {
-    CIdExpressionCollectingVisitor visitor = new CIdExpressionCollectingVisitor();
-
-    final Set<String> result = Sets.newHashSet();
-    final Set<CIdExpression> idExs = Sets.newHashSet();
-    final Deque<CFAEdge> edges = Queues.newArrayDeque();
-
-    edges.add(subject);
-
-    while (!edges.isEmpty()) {
-      CFAEdge edge = edges.pop();
-
-      switch (edge.getEdgeType()) {
-      case AssumeEdge:
-        CAssumeEdge assumeEdge = ((CAssumeEdge) edge);
-        idExs.addAll(assumeEdge.getExpression().accept(visitor));
-      break;
-      case CallToReturnEdge:
-        CFunctionSummaryEdge fnSumEdge = (CFunctionSummaryEdge) edge;
-        idExs.addAll(fnSumEdge.getExpression().accept(visitor));
-      break;
-      case DeclarationEdge:
-        CDeclaration decl = ((CDeclarationEdge) edge).getDeclaration();
-        if (decl instanceof CVariableDeclaration) {
-          CVariableDeclaration varDecl = (CVariableDeclaration) decl;
-          result.add(varDecl.getQualifiedName());
-          if (varDecl.getInitializer() != null) {
-            CInitializer initializer = varDecl.getInitializer();
-
-            // CInitializerLists can be ridiculously long (1000s of entries),
-            // leading to stack overflow error when traversing the list via a visitor
-            // so rather visit each one by one
-            if(initializer instanceof CInitializerList) {
-              for(CInitializer c : ((CInitializerList)initializer).getInitializers()) {
-                idExs.addAll(c.accept(visitor));
-              }
-            }
-
-            else {
-              idExs.addAll(varDecl.getInitializer().accept(visitor));
-            }
-          }
-        }
-      break;
-      case FunctionCallEdge:
-        CFunctionCallEdge callEdge = (CFunctionCallEdge) edge;
-        for (CExpression e: callEdge.getArguments()) {
-          idExs.addAll(e.accept(visitor));
-        }
-      break ;
-      case FunctionReturnEdge:
-      break;
-      case ReturnStatementEdge:
-        Optional<CExpression> expr = ((CReturnStatementEdge) edge).getExpression();
-        if (expr.isPresent()) {
-          idExs.addAll(expr.get().accept(visitor));
-        }
-      break;
-      case StatementEdge:
-        idExs.addAll(((CStatementEdge) edge).getStatement().accept(visitor));
-      break;
-      case BlankEdge:
-        // do nothing
-        break;
-      default:
-        throw new AssertionError("Unhandled edge type in switch statement: " + edge.getEdgeType());
-      }
-    }
-
-    for (CIdExpression e: idExs) {
-      if (e.getDeclaration() != null) {
-        result.add(e.getDeclaration().getQualifiedName());
-      }
-    }
-
-    return result;
-  }
-
-
 }
