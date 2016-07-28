e6a087668cc0df324afe1271ac932a1e00d43ece
Karlheinz Friedberger <kfriedberger@gmail.com>
2016-07-28 21:12:37 +0000

    simplification of RTTCPA by extending AbstractCPA.
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@22417 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/cpa/rtt/RTTCPA.java
+++ src/org/sosy_lab/cpachecker/cpa/rtt/RTTCPA.java
@@ -23,95 +23,37 @@
  */
 package org.sosy_lab.cpachecker.cpa.rtt;
 
-import java.util.Collection;
-
 import org.sosy_lab.common.log.LogManager;
 import org.sosy_lab.cpachecker.cfa.model.CFANode;
+import org.sosy_lab.cpachecker.core.defaults.AbstractCPA;
 import org.sosy_lab.cpachecker.core.defaults.AutomaticCPAFactory;
 import org.sosy_lab.cpachecker.core.defaults.DelegateAbstractDomain;
-import org.sosy_lab.cpachecker.core.defaults.MergeSepOperator;
-import org.sosy_lab.cpachecker.core.defaults.SingletonPrecision;
-import org.sosy_lab.cpachecker.core.defaults.StaticPrecisionAdjustment;
-import org.sosy_lab.cpachecker.core.defaults.StopSepOperator;
-import org.sosy_lab.cpachecker.core.interfaces.AbstractDomain;
 import org.sosy_lab.cpachecker.core.interfaces.AbstractState;
 import org.sosy_lab.cpachecker.core.interfaces.CPAFactory;
-import org.sosy_lab.cpachecker.core.interfaces.ConfigurableProgramAnalysis;
-import org.sosy_lab.cpachecker.core.interfaces.MergeOperator;
-import org.sosy_lab.cpachecker.core.interfaces.Precision;
-import org.sosy_lab.cpachecker.core.interfaces.PrecisionAdjustment;
 import org.sosy_lab.cpachecker.core.interfaces.StateSpacePartition;
 import org.sosy_lab.cpachecker.core.interfaces.Statistics;
 import org.sosy_lab.cpachecker.core.interfaces.StatisticsProvider;
-import org.sosy_lab.cpachecker.core.interfaces.StopOperator;
-import org.sosy_lab.cpachecker.core.interfaces.TransferRelation;
 
-public class RTTCPA implements ConfigurableProgramAnalysis, StatisticsProvider {
+import java.util.Collection;
+
+public class RTTCPA extends AbstractCPA implements StatisticsProvider {
 
   public static CPAFactory factory() {
     return AutomaticCPAFactory.forType(RTTCPA.class);
   }
 
-
-  private Precision precision;
-
-  private  AbstractDomain abstractDomain;
-  private  MergeOperator mergeOperator;
-  private  StopOperator stopOperator;
-  private  TransferRelation transferRelation;
-  private  PrecisionAdjustment precisionAdjustment;
   private final RTTCPAStatistics statistics;
-
   private final LogManager logger;
 
-
   public RTTCPA(LogManager logger) {
+    super("sep", "sep", DelegateAbstractDomain.<RTTState>getInstance(), new RTTTransferRelation());
     this.logger = logger;
-
-    mergeOperator = new MergeSepOperator();
-    abstractDomain = DelegateAbstractDomain.<RTTState>getInstance();
-    stopOperator = new StopSepOperator(abstractDomain);
-    precision = SingletonPrecision.getInstance();
-    precisionAdjustment = StaticPrecisionAdjustment.getInstance();
-    transferRelation = new RTTTransferRelation();
     statistics = new RTTCPAStatistics();
-
-  }
-
-
-  @Override
-  public AbstractDomain getAbstractDomain() {
-    return abstractDomain;
-  }
-
-  @Override
-  public TransferRelation getTransferRelation() {
-    return transferRelation;
-  }
-
-  @Override
-  public MergeOperator getMergeOperator() {
-    return mergeOperator;
-  }
-
-  @Override
-  public StopOperator getStopOperator() {
-    return stopOperator;
-  }
-
-  @Override
-  public PrecisionAdjustment getPrecisionAdjustment() {
-    return precisionAdjustment;
   }
 
   @Override
   public AbstractState getInitialState(CFANode pNode, StateSpacePartition pPartition) {
     return new RTTState();
-  }
-
-  @Override
-  public Precision getInitialPrecision(CFANode pNode, StateSpacePartition pPartition) {
-    return precision;
   }
 
   @Override
