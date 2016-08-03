f137955402823da6fa4ce5970fd5ae3aa151e54a
mandrykin <mandrykin@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2016-08-03 15:56:17 +0000

    PredicateAnalysis: Added a test for arrays as function parameters
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@22490 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

---
+++ test/programs/simple/pointer_aliasing/array-as-function-parameter_true-unreach-label.c
@@ -0,0 +1,27 @@
+int test_fun(int a[], int N) {
+    int i;
+    int res = 0;
+    if (a[0] != 1) {
+        ERROR: goto ERROR;
+    }
+    for (i = 0; i < N; i++) {
+      while (a[i] > 0) {
+          a[i]--;
+          res++;
+       }
+    }
+    return res;
+}
+int main() {
+  int array_size = __VERIFIER_nondet_int();
+  if (array_size < 1) {
+     array_size = 1;
+  }
+  int* numbers = (int*) __builtin_alloca (array_size * sizeof(int));
+  numbers[0] = 1;
+  int res = test_fun(numbers, array_size);
+  if (res < 0) {
+      ERROR: goto ERROR;
+  }
+  return 0;
+}
