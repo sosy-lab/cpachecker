cfa950360a26aabb9d0e9bb6853c4097147142ff
pwendler <pwendler@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2016-08-12 12:50:40 +0000

    Let CAstNodeVisitor extend other visitors.
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@22703 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/cfa/ast/c/CAstNodeVisitor.java
+++ src/org/sosy_lab/cpachecker/cfa/ast/c/CAstNodeVisitor.java
@@ -23,43 +23,10 @@
  */
 package org.sosy_lab.cpachecker.cfa.ast.c;
 
-import org.sosy_lab.cpachecker.cfa.types.c.CEnumType.CEnumerator;
+public interface CAstNodeVisitor<R, X extends Exception>
+    extends CDesignatorVisitor<R, X>, CInitializerVisitor<R, X>, CRightHandSideVisitor<R, X>,
+        CSimpleDeclarationVisitor<R, X>, CStatementVisitor<R, X> {
 
-
-public interface CAstNodeVisitor<R, X extends Exception> {
-
-  R visit(CArrayDesignator pNode) throws X;
-  R visit(CArrayRangeDesignator pNode) throws X;
-  R visit(CFieldDesignator pNode) throws X;
-  R visit(CInitializerList pNode) throws X;
   R visit(CReturnStatement pNode) throws X;
-  R visit(CDesignatedInitializer pNode) throws X;
-  R visit(CInitializerExpression pNode) throws X;
-  R visit(CFunctionCallExpression pNode) throws X;
-  R visit(CBinaryExpression pNode) throws X;
-  R visit(CCastExpression pNode) throws X;
-  R visit(CTypeIdExpression pNode) throws X;
-  R visit(CUnaryExpression pNode) throws X;
-  R visit(CArraySubscriptExpression pNode) throws X;
-  R visit(CComplexCastExpression pNode) throws X;
-  R visit(CFieldReference pNode) throws X;
-  R visit(CIdExpression pNode) throws X;
-  R visit(CPointerExpression pNode) throws X;
-  R visit(CCharLiteralExpression pNode) throws X;
-  R visit(CFloatLiteralExpression pNode) throws X;
-  R visit(CImaginaryLiteralExpression pNode) throws X;
-  R visit(CIntegerLiteralExpression pNode) throws X;
-  R visit(CStringLiteralExpression pNode) throws X;
-  R visit(CAddressOfLabelExpression pNode) throws X;
-  R visit(CParameterDeclaration pNode) throws X;
-  R visit(CFunctionDeclaration pNode) throws X;
-  R visit(CComplexTypeDeclaration pNode) throws X;
-  R visit(CTypeDefDeclaration pNode) throws X;
-  R visit(CVariableDeclaration pNode) throws X;
-  R visit(CExpressionAssignmentStatement pNode) throws X;
-  R visit(CExpressionStatement pNode) throws X;
-  R visit(CFunctionCallAssignmentStatement pNode) throws X;
-  R visit(CFunctionCallStatement pNode) throws X;
-  R visit(CEnumerator pCEnumerator);
 
 }
