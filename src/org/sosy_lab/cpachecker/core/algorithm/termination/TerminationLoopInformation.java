7a0f673ce391eed2b1aeeecf14853b3157aed6c1
ottseb <ottseb@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2016-08-18 10:34:29 +0000

    Store the value at the pointer's target in a new variable when the lasso's loop starts.
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@22782 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/core/algorithm/termination/TerminationLoopInformation.java
+++ src/org/sosy_lab/cpachecker/core/algorithm/termination/TerminationLoopInformation.java
@@ -47,6 +47,7 @@ import org.sosy_lab.cpachecker.cfa.ast.c.CDeclaration;
 import org.sosy_lab.cpachecker.cfa.ast.c.CExpression;
 import org.sosy_lab.cpachecker.cfa.ast.c.CExpressionAssignmentStatement;
 import org.sosy_lab.cpachecker.cfa.ast.c.CIdExpression;
+import org.sosy_lab.cpachecker.cfa.ast.c.CPointerExpression;
 import org.sosy_lab.cpachecker.cfa.ast.c.CSimpleDeclaration;
 import org.sosy_lab.cpachecker.cfa.ast.c.CStatement;
 import org.sosy_lab.cpachecker.cfa.ast.c.CVariableDeclaration;
@@ -58,6 +59,8 @@ import org.sosy_lab.cpachecker.cfa.model.c.CDeclarationEdge;
 import org.sosy_lab.cpachecker.cfa.model.c.CLabelNode;
 import org.sosy_lab.cpachecker.cfa.model.c.CStatementEdge;
 import org.sosy_lab.cpachecker.cfa.types.MachineModel;
+import org.sosy_lab.cpachecker.cfa.types.c.CPointerType;
+import org.sosy_lab.cpachecker.cfa.types.c.CType;
 import org.sosy_lab.cpachecker.cpa.termination.TerminationARGPath;
 import org.sosy_lab.cpachecker.cpa.termination.TerminationTransferRelation;
 import org.sosy_lab.cpachecker.util.LoopStructure.Loop;
@@ -113,8 +116,7 @@ public class TerminationLoopInformation {
   /**
    * Mapping of relevant variables to the corresponding primed variable.
    */
-  private Map<CVariableDeclaration, CVariableDeclaration> relevantVariables =
-      Collections.emptyMap();
+  private Map<CExpression, CVariableDeclaration> relevantVariables = Collections.emptyMap();
 
   // reusing of intermediate location is required to build counter examples
   private List<CFANode> relevantVariablesInitializationIntermediateLocations =
@@ -193,12 +195,24 @@ public class TerminationLoopInformation {
 
     String functionName = pLoop.getLoopHeads().iterator().next().getFunctionName();
     ImmutableList.Builder<CFANode> intermediateStates = ImmutableList.builder();
-    Builder<CVariableDeclaration, CVariableDeclaration> builder = ImmutableMap.builder();
+    Builder<CExpression, CVariableDeclaration> builder = ImmutableMap.builder();
 
     for (CVariableDeclaration relevantVariable : pRelevantVariables) {
+      CExpression unprimedVariable = new CIdExpression(DUMMY, relevantVariable);
       CVariableDeclaration primedVariable = TerminationUtils.createPrimedVariable(relevantVariable);
-      builder.put(relevantVariable, primedVariable);
+      builder.put(unprimedVariable, primedVariable);
       intermediateStates.add(new CFANode(functionName));
+
+      // x__TERMINATION_PRIMED__TERMINATION_DEREFERENCED = *x;
+      CType type = relevantVariable.getType();
+      while (type instanceof CPointerType) {
+        type = ((CPointerType) type).getType();
+        unprimedVariable = new CPointerExpression(DUMMY, type, unprimedVariable);
+        primedVariable = TerminationUtils.createDereferencedVariable(primedVariable);
+
+        builder.put(unprimedVariable, primedVariable);
+        intermediateStates.add(new CFANode(functionName));
+      }
     }
 
     // Create a unique target node for each loop
@@ -250,10 +264,10 @@ public class TerminationLoopInformation {
   private List<CStatement> createPrimedVariableAssignments() {
     ImmutableList.Builder<CStatement> builder = ImmutableList.builder();
 
-    for (Entry<CVariableDeclaration, CVariableDeclaration> relevantVariable :
+    for (Entry<CExpression, CVariableDeclaration> relevantVariable :
         relevantVariables.entrySet()) {
 
-      CVariableDeclaration unprimedVariable = relevantVariable.getKey();
+      CExpression unprimedVariable = relevantVariable.getKey();
       CVariableDeclaration primedVariable = relevantVariable.getValue();
       CStatement assignment = createAssignmentStatement(primedVariable, unprimedVariable);
 
@@ -310,11 +324,11 @@ public class TerminationLoopInformation {
   }
 
   private CExpressionAssignmentStatement createAssignmentStatement(
-      CSimpleDeclaration pLeftHandSide, CSimpleDeclaration pRightHandSide) {
+      CSimpleDeclaration pLeftHandSide, CExpression pRightHandSide) {
     return new CExpressionAssignmentStatement(
         FileLocation.DUMMY,
         new CIdExpression(FileLocation.DUMMY, pLeftHandSide),
-        new CIdExpression(FileLocation.DUMMY, pRightHandSide));
+        pRightHandSide);
   }
 
   public BlankEdge createBlankEdge(CFANode pPredecessor, CFANode pSuccessor, String pDescription) {
