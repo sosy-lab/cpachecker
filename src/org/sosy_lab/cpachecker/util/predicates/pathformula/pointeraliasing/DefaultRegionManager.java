4f10e3554621885e8edf4038fd6f2b6b34cfa6df
mutilin <mutilin@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2016-09-09 07:49:14 +0000

    Fix test array-as-function-parameter_and_argument_true
    Add correct hashCode and equals
    
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/pointer_analysis_bnb_model2@23138 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/util/predicates/pathformula/pointeraliasing/DefaultRegionManager.java
+++ src/org/sosy_lab/cpachecker/util/predicates/pathformula/pointeraliasing/DefaultRegionManager.java
@@ -52,6 +52,36 @@ public class DefaultRegionManager implements MemoryRegionManager {
     public String toString() {
       return "DefaultMemoryRegion [type=" + type + "]";
     }
+
+    @Override
+    public int hashCode() {
+      final int prime = 31;
+      int result = 1;
+      result = prime * result + ((type == null) ? 0 : type.hashCode());
+      return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+      if (this == obj) {
+        return true;
+      }
+      if (obj == null) {
+        return false;
+      }
+      if (getClass() != obj.getClass()) {
+        return false;
+      }
+      DefaultMemoryRegion other = (DefaultMemoryRegion) obj;
+      if (type == null) {
+        if (other.type != null) {
+          return false;
+        }
+      } else if (!type.equals(other.type)) {
+        return false;
+      }
+      return true;
+    }
   }
 
   @Override
