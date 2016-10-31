adb2e8640d04cde05a216f689b695eac09ece9d4
weinzierl <weinzierl@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2016-10-31 09:33:25 +0000

    PointerStates updated (system.out.print-statements removed)
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/pointerAnalysis@23466 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

---
+++ src/org/sosy_lab/cpachecker/cpa/pointer2/AndersenState.java
@@ -0,0 +1,68 @@
+/*
+ *  CPAchecker is a tool for configurable software verification.
+ *  This file is part of CPAchecker.
+ *
+ *  Copyright (C) 2007-2016  Dirk Beyer
+ *  All rights reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *
+ *  CPAchecker web page:
+ *    http://cpachecker.sosy-lab.org
+ */
+package org.sosy_lab.cpachecker.cpa.pointer2;
+
+import org.sosy_lab.common.collect.PersistentSortedMap;
+import org.sosy_lab.cpachecker.cpa.pointer2.util.LocationSet;
+import org.sosy_lab.cpachecker.cpa.pointer2.util.LocationSetTop;
+
+public class AndersenState extends PointerState {
+
+  /**
+   * The initial empty pointer state.
+   */
+  public static final PointerState INITIAL_STATE = new AndersenState();
+
+  public AndersenState() {
+    super();
+  }
+
+  /**
+   * Creates a new pointer state from the given persistent points-to map.
+   *
+   * @param pPointsToMap the points-to map of this state.
+   */
+  private AndersenState(PersistentSortedMap<LocationSet, LocationSet> pPointsToMap) {
+    super(pPointsToMap);
+  }
+
+  @Override
+  public PointerState addPointsToInformation(LocationSet pSource, LocationSet pTargets) {
+    if (pTargets.isBot()) {
+      return this;
+    }
+    if (pTargets.isTop()) {
+      return new AndersenState(getPointsToMap().putAndCopy(pSource, LocationSetTop.INSTANCE));
+    }
+    LocationSet previousPointsToSet = getPointsToSet(pSource);
+    PointerState resultState = new AndersenState(getPointsToMap().putAndCopy(pSource, previousPointsToSet.addElements(pTargets)));
+    return resultState;
+  }
+
+  @Override
+  public String toString() {
+    return getPointsToMap().toString();
+  }
+
+}
---
+++ src/org/sosy_lab/cpachecker/cpa/pointer2/SteensgaardState.java
@@ -0,0 +1,125 @@
+/*
+ *  CPAchecker is a tool for configurable software verification.
+ *  This file is part of CPAchecker.
+ *
+ *  Copyright (C) 2007-2016  Dirk Beyer
+ *  All rights reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *
+ *  CPAchecker web page:
+ *    http://cpachecker.sosy-lab.org
+ */
+package org.sosy_lab.cpachecker.cpa.pointer2;
+
+import org.sosy_lab.common.collect.PersistentSortedMap;
+import org.sosy_lab.cpachecker.cpa.pointer2.util.ExplicitLocationSet;
+import org.sosy_lab.cpachecker.cpa.pointer2.util.LocationSet;
+import org.sosy_lab.cpachecker.cpa.pointer2.util.LocationSetBot;
+import org.sosy_lab.cpachecker.cpa.pointer2.util.LocationSetTop;
+
+public class SteensgaardState extends PointerState {
+
+  /**
+   * The initial empty pointer state.
+   */
+  public static final PointerState INITIAL_STATE = new SteensgaardState();
+
+  private SteensgaardState() {
+    super();
+  }
+
+  /**
+   * Creates a new pointer state from the given persistent points-to map.
+   *
+   * @param pPointsToMap the points-to map of this state.
+   */
+  private SteensgaardState(PersistentSortedMap<LocationSet, LocationSet> pPointsToMap) {
+    super(pPointsToMap);
+  }
+
+  /**
+   * Gets a pointer state representing the points to information of this state
+   * combined with the information that the first given identifier points to the
+   * given target identifiers.
+   *
+   * @param pSource the first identifier.
+   * @param pTargets the target identifiers.
+   * @return the pointer state.
+   */
+  @Override
+  public PointerState addPointsToInformation(LocationSet pSource, LocationSet pTargets) {
+    if (pTargets.isBot()) {
+      return this;
+    }
+    if (pTargets.isTop()) {
+      return new SteensgaardState(getPointsToMap().putAndCopy(pSource, LocationSetTop.INSTANCE));
+    }
+    LocationSet previousPointsToSet = getPointsToSet(pSource);
+    if (!(previousPointsToSet instanceof LocationSetBot)) {
+      PersistentSortedMap<LocationSet, LocationSet> updatedMap = join(getPointsToMap(), previousPointsToSet, pTargets);
+      PointerState resultState = new SteensgaardState(updatedMap);
+      return resultState;
+    }
+    SteensgaardState resultState = new SteensgaardState(getPointsToMap().putAndCopy(pSource, pTargets));
+    return resultState;
+    }
+
+  public PersistentSortedMap<LocationSet, LocationSet> join(PersistentSortedMap<LocationSet, LocationSet> currentMap, LocationSet pLhs, LocationSet pRhs) {
+    if (pLhs.equals(pRhs)) {
+      return currentMap;
+    }
+    LocationSet pLhsNext = getPointsToSet(pLhs);
+    LocationSet pRhsNext = getPointsToSet(pRhs);
+    currentMap = unify(currentMap, pLhs, pRhs);
+    if (!(pRhsNext instanceof LocationSetBot || pLhsNext instanceof LocationSetBot) && (!pRhsNext.containsAll(pLhsNext) && pLhs.containsAll(pRhsNext))) {
+    currentMap = join(currentMap, pLhsNext, pRhsNext);
+    }
+    return currentMap;
+  }
+
+  /**
+   * Updates the map accordingly to the new merged nodes.
+   */
+  private PersistentSortedMap<LocationSet, LocationSet> unify(PersistentSortedMap<LocationSet, LocationSet> currentMap, LocationSet pMergeLeft, LocationSet pMergeRight) {
+    LocationSet resultLocation = pMergeLeft.addElements(pMergeRight);
+    LocationSet referencedValues = new LocationSetBot();
+    for (LocationSet pKey : currentMap.keySet()) {
+      if ((currentMap.get(pKey).containsAll(pMergeLeft) || currentMap.get(pKey).containsAll(pMergeRight)) && !pKey.containsAll(resultLocation)) {
+        currentMap = currentMap.removeAndCopy(pKey);
+        currentMap = currentMap.putAndCopy(pKey, resultLocation);
+      }
+      if ((pKey.containsAll(pMergeLeft) || pKey.containsAll(pMergeRight)) && !pKey.containsAll(resultLocation)) {
+        referencedValues = referencedValues.addElements(currentMap.get(pKey));
+        currentMap = currentMap.removeAndCopy(pKey);
+      }
+    }
+    if (referencedValues instanceof ExplicitLocationSet || referencedValues instanceof LocationSetTop) {
+    currentMap = currentMap.putAndCopy(resultLocation, referencedValues);
+    }
+    return currentMap;
+  }
+
+  @Override
+  public int hashCode() {
+    return getPointsToMap().hashCode();
+  }
+
+  @Override
+  public String toString() {
+    return getPointsToMap().toString();
+  }
+
+
+}
