701b9812e4ece9a924b4dae735ab98e695f46e62
Karlheinz Friedberger <kfriedberger@gmail.com>
2016-11-16 22:03:38 +0000

    fix: remove unused import
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@23621 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/cpa/bam/BAMCEXSubgraphComputer.java
+++ src/org/sosy_lab/cpachecker/cpa/bam/BAMCEXSubgraphComputer.java
@@ -39,7 +39,6 @@ import org.sosy_lab.cpachecker.exceptions.CPAException;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
 import java.util.NavigableSet;
 import java.util.Set;
