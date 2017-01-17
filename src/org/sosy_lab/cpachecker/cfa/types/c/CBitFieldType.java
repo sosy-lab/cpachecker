0427c556ed3515f7590e5ced58716149f8a5e348
dangl <dangl@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2017-01-17 20:29:59 +0000

    Add documentation for CBitFieldType
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@24134 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/cfa/types/c/CBitFieldType.java
+++ src/org/sosy_lab/cpachecker/cfa/types/c/CBitFieldType.java
@@ -26,6 +26,7 @@ package org.sosy_lab.cpachecker.cfa.types.c;
 import com.google.common.base.Preconditions;
 import org.sosy_lab.cpachecker.cfa.types.c.CComplexType.ComplexTypeKind;
 
+/** Instances of this class represent C bit-field types. */
 public class CBitFieldType implements CType {
 
   private static final long serialVersionUID = 1L;
@@ -34,6 +35,22 @@ public class CBitFieldType implements CType {
 
   private final int bitFieldSize;
 
+  /**
+   * Creates a new bit-field type.
+   *
+   * @param pBitFieldType the base type of the bit field. The only allowed values are
+   *     <ul>
+   *       <li>{@link CSimpleType} with basic types {@link CBasicType#BOOL}, {@link
+   *           CBasicType#CHAR}, and {@link CBasicType#INT},
+   *       <li>{@link CEnumType}, and
+   *       <li>{@link CElaboratedType} with {@link CElaboratedType#getKind()} == {@link
+   *           ComplexTypeKind#ENUM}
+   *     </ul>
+   *
+   * @param pBitFieldSize the length of the field in bits. Must not be negative.
+   * @throws IllegalArgumentException if the given type is not a valid basic type for a bit field or
+   *     if the given size is negative.
+   */
   public CBitFieldType(CType pBitFieldType, int pBitFieldSize) {
     type = checkType(pBitFieldType);
     Preconditions.checkArgument(pBitFieldSize >= 0, "Bit-field size must not be negative, but was %s", pBitFieldSize);
@@ -103,10 +120,20 @@ public class CBitFieldType implements CType {
     return new CBitFieldType(canonicalBitFieldType, bitFieldSize);
   }
 
+  /**
+   * Gets the size of the bit field in bits.
+   *
+   * @return the size of the bit field in bits.
+   */
   public int getBitFieldSize() {
     return bitFieldSize;
   }
 
+  /**
+   * Gets the base type of the bit field.
+   *
+   * @return the base type of the bit field.
+   */
   public CType getType() {
     return type;
   }
