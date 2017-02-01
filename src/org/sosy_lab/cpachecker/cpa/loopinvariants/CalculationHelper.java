d04e6e69de5baac65051fa81822b1e85b177fc9f
dangl <dangl@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2017-02-01 16:24:42 +0000

    Refactor the inter-process communication with sympy performed in the loop-invariants CPA
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@24253 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/cpa/loopinvariants/CalculationHelper.java
+++ src/org/sosy_lab/cpachecker/cpa/loopinvariants/CalculationHelper.java
@@ -23,12 +23,10 @@
  */
 package org.sosy_lab.cpachecker.cpa.loopinvariants;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.nio.charset.Charset;
+import java.io.UncheckedIOException;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
@@ -37,6 +35,7 @@ import java.util.Set;
 import java.util.logging.Level;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 import org.sosy_lab.common.log.LogManager;
 import org.sosy_lab.cpachecker.cpa.loopinvariants.polynom.PolynomExpression;
 import org.sosy_lab.cpachecker.cpa.loopinvariants.polynom.visitors.CollectVariablesVisitor;
@@ -48,27 +47,9 @@ public class CalculationHelper {
    */
   public static List<Polynom> calculateGroebnerBasis(List<PolynomExpression> polynomials,
       Map<String, Double> valueMap, LogManager log) {
-    String sympyCommand = "isympy";
 
-    ProcessBuilder pb = new ProcessBuilder(sympyCommand);
-    Process sympy = null;
-
-    try {
-      sympy = pb.start();
-    } catch (IOException e) {
-      Error er = new Error("Failed to start the sympy subprocess.");
-      er.initCause(e);
-      throw er;
-    }
-
-    BufferedWriter stdin =
-        new BufferedWriter(
-            new OutputStreamWriter(sympy.getOutputStream(), Charset.defaultCharset()));
-    BufferedReader stdout =
-        new BufferedReader(new InputStreamReader(sympy.getInputStream(), Charset.defaultCharset()));
-
-    try {
-      return calculateGroebnerBasis(polynomials, valueMap, log, stdin, stdout);
+    try (SympyProcess sympy = SympyProcess.newProcess()) {
+      return calculateGroebnerBasis(polynomials, valueMap, log, sympy);
     } catch (IOException e) {
       Error er = new Error("Communication with the sympy subprocess failed.");
       er.initCause(e);
@@ -76,25 +57,25 @@ public class CalculationHelper {
     }
   }
 
-  private static List<Polynom> calculateGroebnerBasis(List<PolynomExpression> polynomials,
-      Map<String, Double> valueMap, LogManager log,
-      BufferedWriter stdin, BufferedReader stdout) throws IOException {
+  private static List<Polynom> calculateGroebnerBasis(
+      List<PolynomExpression> pPolynomials,
+      Map<String, Double> pValueMap,
+      LogManager pLog,
+      SympyProcess pSympy)
+      throws IOException {
 
     CollectVariablesVisitor collector = new CollectVariablesVisitor();
     Set<String> variables = new HashSet<>();
 
-    for (PolynomExpression polynom : polynomials) {
+    for (PolynomExpression polynom : pPolynomials) {
       variables.addAll(polynom.accept(collector));
     }
 
-    String polynomsStr =
-        preprocessingOfPolynomials(polynomials, valueMap, log).toString();
+    String polynomsStr = preprocessingOfPolynomials(pPolynomials, pValueMap, pLog).toString();
 
-    String outPrefix = "sympyout:";
-
-    writeToSympy(stdin, "n = Symbol('n')");
+    pSympy.sendLine("n = Symbol('n')");
     for (String var : variables) {
-      writeToSympy(stdin, var + " = Symbol('" + var + "')");
+      pSympy.sendLine(var + " = Symbol('" + var + "')");
     }
 
     StringBuilder sb = new StringBuilder();
@@ -107,13 +88,13 @@ public class CalculationHelper {
     }
     sb.append(")");
 
-    writeToSympy(stdin, sb.toString());
+    pSympy.sendLine(sb.toString());
 
-    writeToSympy(stdin, "print \"" + outPrefix + "\" + str(z) + \"\\n\"");
+    pSympy.sendLine("print str(z) + \"\\n\"");
 
-    stdin.close();
+    pSympy.commit();
 
-    List<String> groebnerBasis = getMatchesFromStreamWithGroebner(stdout, outPrefix);
+    List<String> groebnerBasis = getMatchesFromStreamWithGroebner(pSympy);
 
     List<Polynom> polynoms = new LinkedList<>();
 
@@ -126,65 +107,51 @@ public class CalculationHelper {
     return polynoms;
   }
 
-  private static void writeToSympy(BufferedWriter stdin, String msg) throws IOException {
-    stdin.write(msg);
-    stdin.newLine();
-  }
-
-  private static String readAllFromStream(BufferedReader s) throws IOException {
-    StringBuilder sb = new StringBuilder();
-    String line;
-    while ((line = s.readLine()) != null) {
-      sb.append(line);
-      sb.append('\n');
+  private static String readAllStderr(SympyProcess pSympy) throws IOException {
+    try {
+      return pSympy.readErrorLines().collect(Collectors.joining(System.lineSeparator()));
+    } catch (UncheckedIOException e) {
+      throw e.getCause();
     }
-    return sb.toString();
   }
 
   /**
-   * Gives the computed Groebner Base as Strings
+   * Attempts to compute Groebner Base as polynoms represented by Strings
    *
-   * @param s   input string
-   * @param prefix    start of the output
-   * @return polynoms as Strings
+   * @param pSympy the sympy process.
+   * @return the Groebner Base as polynoms represented by Strings.
+   * @throws IOException if an IOException occurred during the communication with the sympy process.
    */
-  private static List<String> getMatchesFromStreamWithGroebner(BufferedReader s, String prefix)
+  private static List<String> getMatchesFromStreamWithGroebner(SympyProcess pSympy)
       throws IOException {
     String line;
-    while ((line = s.readLine()) != null) {
-      if (line.contains(prefix)) {
-        line = line.replaceFirst(".*" + prefix, "");
-        Pattern p = Pattern.compile("GroebnerBasis\\(\\[([^\\[\\]]+)\\].*");
-        Matcher matsch = p.matcher(line);
-        if (matsch.matches()) {
-          String basisStr = matsch.group(1);
-          String[] basisPolyStrs = basisStr.split(", ");
-          List<String> res = new LinkedList<>();
-          for (String str : basisPolyStrs) {
-            res.add(str.replace("**", "^"));
-          }
-          return res;
-        } else {
-          throw new RuntimeException("Groebner Basis can not be calculated.");
+    while ((line = pSympy.readLine()) != null) {
+      Pattern p = Pattern.compile("GroebnerBasis\\(\\[([^\\[\\]]+)\\].*");
+      Matcher matcher = p.matcher(line);
+      if (matcher.matches()) {
+        String basisStr = matcher.group(1);
+        String[] basisPolyStrs = basisStr.split(", ");
+        List<String> res = new ArrayList<>(basisPolyStrs.length);
+        for (String str : basisPolyStrs) {
+          res.add(str.replace("**", "^"));
         }
+        return res;
+      } else {
+        throw new RuntimeException("Groebner Basis can not be calculated.");
       }
     }
-    return null;
+    return Collections.emptyList();
   }
 
-  private static List<String> getMatchesFromStream(BufferedReader s, String prefix, String var)
+  private static List<String> getMatchesFromSympy(SympyProcess pSympy, String pVar)
       throws IOException {
-    String line;
-    List<String> res = new LinkedList<>();
-    String output;
-    while ((line = s.readLine()) != null) {
-      if (line.contains(prefix)) {
-        output = line.replaceFirst(".*" + prefix, "");
-        res.add(output.replace("C0", var));
-      }
-
+    final List<String> result;
+    try {
+      result = pSympy.readLines().map(l -> l.replace("C0", pVar)).collect(Collectors.toList());
+    } catch (UncheckedIOException e) {
+      throw e.getCause();
     }
-    return res;
+    return result;
   }
 
   private static List<String> preprocessingOfPolynomials(List<PolynomExpression> polynomials,
@@ -200,39 +167,15 @@ public class CalculationHelper {
     }
   }
 
-  @SuppressWarnings("resource")
   private static List<String> preprocessingOfPolynomialsHelper(List<PolynomExpression> polynomials,
       Map<String, Double> valueMap, LogManager log)
       throws IOException {
-    String outPrefix = "sympyout:";
 
     CollectVariablesVisitor collector = new CollectVariablesVisitor();
     Set<String> variables;
     List<String> closedFormPolynomials = new LinkedList<>();
 
     for (PolynomExpression polynom : polynomials) {
-      String sympyCommand = "isympy";
-
-      ProcessBuilder pb = new ProcessBuilder(sympyCommand);
-      Process sympy = null;
-
-      try {
-        sympy = pb.start();
-      } catch (IOException e) {
-        Error er = new Error("Failed to start the sympy subprocess.");
-        er.initCause(e);
-        throw er;
-      }
-
-      BufferedWriter stdin =
-          new BufferedWriter(
-              new OutputStreamWriter(sympy.getOutputStream(), Charset.defaultCharset()));
-      BufferedReader stdout =
-          new BufferedReader(
-              new InputStreamReader(sympy.getInputStream(), Charset.defaultCharset()));
-      BufferedReader stderr =
-          new BufferedReader(
-              new InputStreamReader(sympy.getErrorStream(), Charset.defaultCharset()));
 
       variables = polynom.accept(collector);
       String polyStr = polynom.toString();
@@ -240,33 +183,40 @@ public class CalculationHelper {
       sb.append("z = rsolve(");
       sb.append(polyStr + ", ");
       String var = variables.iterator().next();
+      final List<String> closedFormPoly;
 
       int openBracket = var.indexOf("(");
       String startVar = var.substring(0, openBracket);
-      sb.append(startVar + "(n)");
 
-      //Startwert
-      if (valueMap.containsKey(startVar)) {
-        sb.append(", {" + startVar + "(0):" + valueMap.get(startVar) + "}");
+      try (SympyProcess sympy = SympyProcess.newProcess()) {
+
+        sb.append(startVar + "(n)");
+
+        //Startwert
+        if (valueMap.containsKey(startVar)) {
+          sb.append(", {" + startVar + "(0):" + valueMap.get(startVar) + "}");
+        }
+
+        sb.append(")");
+        sympy.sendLine(sb.toString());
+
+        sympy.sendLine("print str(z) + \"\\n\"");
+        sympy.commit();
+        closedFormPoly = getMatchesFromSympy(sympy, startVar);
+        if (log.wouldBeLogged(Level.INFO)) {
+          String errorOut = readAllStderr(sympy).trim();
+          if (!errorOut.isEmpty()) {
+            log.log(Level.INFO, errorOut);
+          }
+        }
       }
-
-      sb.append(")");
-      writeToSympy(stdin, sb.toString());
-
-      writeToSympy(stdin, "print \"" + outPrefix + "\" + str(z) + \"\\n\"");
-      stdin.close();
-      List<String> closedFormPoly = getMatchesFromStream(stdout, outPrefix, startVar);
       if (!closedFormPoly.isEmpty()) {
         closedFormPolynomials.add(closedFormPoly.get(0) + " - " + startVar + "(n)");
-        log.log(Level.INFO, "Closed form of " + polyStr + " : " + closedFormPoly.get(0).toString());
+        log.log(Level.FINE, "Closed form of " + polyStr + " : " + closedFormPoly.get(0).toString());
       } else {
         throw new RuntimeException(
             "The closed form of the polynomial '" + polyStr + "' can not be computed.");
       }
-      variables = null;
-      log.log(Level.INFO, readAllFromStream(stderr));
-      stderr.close();
-      stdout.close();
     }
     return closedFormPolynomials;
   }
