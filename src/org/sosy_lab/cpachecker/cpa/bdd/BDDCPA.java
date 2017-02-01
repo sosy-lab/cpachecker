191494cdcd30b783a1a446e87c76e2d429ddf546
Karlheinz Friedberger <kfriedberger@gmail.com>
2017-02-01 12:30:14 +0000

    BDDCPA: move statistics and options out of TransferRelation.
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@24251 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/cpa/bdd/BDDCPA.java
+++ src/org/sosy_lab/cpachecker/cpa/bdd/BDDCPA.java
@@ -23,13 +23,23 @@
  */
 package org.sosy_lab.cpachecker.cpa.bdd;
 
+import java.io.IOException;
 import java.io.PrintStream;
+import java.io.Writer;
+import java.nio.charset.Charset;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Collection;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.logging.Level;
 import org.sosy_lab.common.ShutdownNotifier;
 import org.sosy_lab.common.configuration.Configuration;
+import org.sosy_lab.common.configuration.FileOption;
 import org.sosy_lab.common.configuration.InvalidConfigurationException;
 import org.sosy_lab.common.configuration.Option;
 import org.sosy_lab.common.configuration.Options;
+import org.sosy_lab.common.io.MoreFiles;
 import org.sosy_lab.common.log.LogManager;
 import org.sosy_lab.cpachecker.cfa.CFA;
 import org.sosy_lab.cpachecker.cfa.model.CFANode;
@@ -53,6 +63,8 @@ import org.sosy_lab.cpachecker.core.interfaces.StatisticsProvider;
 import org.sosy_lab.cpachecker.core.interfaces.StopOperator;
 import org.sosy_lab.cpachecker.core.interfaces.TransferRelation;
 import org.sosy_lab.cpachecker.core.reachedset.UnmodifiableReachedSet;
+import org.sosy_lab.cpachecker.util.VariableClassification;
+import org.sosy_lab.cpachecker.util.VariableClassification.Partition;
 import org.sosy_lab.cpachecker.util.predicates.bdd.BDDManagerFactory;
 import org.sosy_lab.cpachecker.util.predicates.regions.NamedRegionManager;
 import org.sosy_lab.cpachecker.util.predicates.regions.RegionManager;
@@ -68,7 +80,6 @@ public class BDDCPA implements ConfigurableProgramAnalysisWithBAM, StatisticsPro
   private final BitvectorManager bvmgr;
   private final PredicateManager predmgr;
   private VariableTrackingPrecision precision;
-  private final BDDTransferRelation transferRelation;
   private final ShutdownNotifier shutdownNotifier;
   private final Configuration config;
   private final LogManager logger;
@@ -76,6 +87,19 @@ public class BDDCPA implements ConfigurableProgramAnalysisWithBAM, StatisticsPro
 
   @Option(secure=true, description="mergeType")
   private String merge = "join";
+
+  @Option(secure = true, name = "logfile", description = "Dump tracked variables to a file.")
+  @FileOption(FileOption.Type.OUTPUT_FILE)
+  private Path dumpfile = Paths.get("BDDCPA_tracked_variables.log");
+
+  @Option(secure = true, description = "max bitsize for values and vars, initial value")
+  private int bitsize = 64;
+
+  @Option(
+    secure = true,
+    description = "use a smaller bitsize for all vars, that have only intEqual values"
+  )
+  private boolean compressIntEqual = true;
 
   private BDDCPA(CFA pCfa, Configuration pConfig, LogManager pLogger, ShutdownNotifier pShutdownNotifier)
       throws InvalidConfigurationException {
@@ -93,7 +117,6 @@ public class BDDCPA implements ConfigurableProgramAnalysisWithBAM, StatisticsPro
     manager           = new NamedRegionManager(rmgr);
     bvmgr             = new BitvectorManager(rmgr);
     predmgr           = new PredicateManager(config, manager, cfa);
-    transferRelation  = new BDDTransferRelation(manager, bvmgr, predmgr, logger, config, cfa);
   }
 
   public void injectRefinablePrecision() throws InvalidConfigurationException {
@@ -123,7 +146,7 @@ public class BDDCPA implements ConfigurableProgramAnalysisWithBAM, StatisticsPro
 
   @Override
   public TransferRelation getTransferRelation() {
-    return transferRelation;
+    return new BDDTransferRelation(manager, bvmgr, predmgr, cfa, bitsize, compressIntEqual);
   }
 
   @Override
@@ -138,18 +161,86 @@ public class BDDCPA implements ConfigurableProgramAnalysisWithBAM, StatisticsPro
 
   @Override
   public void collectStatistics(Collection<Statistics> statsCollection) {
-    statsCollection.add(new Statistics() {
+    statsCollection.add(
+        new Statistics() {
 
-      @Override
-      public void printStatistics(PrintStream out, Result result, UnmodifiableReachedSet reached) {
-        transferRelation.printStatistics(out);
-      }
+          @Override
+          public void printStatistics(
+              PrintStream out, Result result, UnmodifiableReachedSet reached) {
+            VariableClassification varClass = cfa.getVarClassification().get();
+            final Set<Partition> intBool = varClass.getIntBoolPartitions();
+            int numOfBooleans = varClass.getIntBoolVars().size();
 
-      @Override
-      public String getName() {
-        return "BDDCPA";
-      }
-    });
+            int numOfIntEquals = 0;
+            final Set<Partition> intEq = varClass.getIntEqualPartitions();
+            for (Partition p : intEq) {
+              numOfIntEquals += p.getVars().size();
+            }
+
+            int numOfIntAdds = 0;
+            final Set<Partition> intAdd = varClass.getIntAddPartitions();
+            for (Partition p : intAdd) {
+              numOfIntAdds += p.getVars().size();
+            }
+
+            Collection<String> trackedIntBool =
+                new TreeSet<>(); // TreeSet for nicer output through ordering
+            Collection<String> trackedIntEq = new TreeSet<>();
+            Collection<String> trackedIntAdd = new TreeSet<>();
+            for (String var : predmgr.getTrackedVars().keySet()) {
+              if (varClass.getIntBoolVars().contains(var)) {
+                trackedIntBool.add(var);
+              } else if (varClass.getIntEqualVars().contains(var)) {
+                trackedIntEq.add(var);
+              } else if (varClass.getIntAddVars().contains(var)) {
+                trackedIntAdd.add(var);
+              } else {
+                // ignore other vars, they are either function_return_vars or tmp_vars
+              }
+            }
+
+            if (dumpfile != null) { // option -noout
+              try (Writer w = MoreFiles.openOutputFile(dumpfile, Charset.defaultCharset())) {
+                w.append("Boolean\n\n");
+                w.append(trackedIntBool.toString());
+                w.append("\n\nIntEq\n\n");
+                w.append(trackedIntEq.toString());
+                w.append("\n\nIntAdd\n\n");
+                w.append(trackedIntAdd.toString());
+              } catch (IOException e) {
+                logger.logUserException(
+                    Level.WARNING, e, "Could not write tracked variables for BDDCPA to file");
+              }
+            }
+
+            out.println(
+                String.format(
+                    "Number of boolean vars:           %d (of %d)",
+                    trackedIntBool.size(), numOfBooleans));
+            out.println(
+                String.format(
+                    "Number of intEqual vars:          %d (of %d)",
+                    trackedIntEq.size(), numOfIntEquals));
+            out.println(
+                String.format(
+                    "Number of intAdd vars:            %d (of %d)",
+                    trackedIntAdd.size(), numOfIntAdds));
+            out.println(
+                String.format(
+                    "Number of all vars:               %d",
+                    trackedIntBool.size() + trackedIntEq.size() + trackedIntAdd.size()));
+            out.println("Number of intBool partitions:     " + intBool.size());
+            out.println("Number of intEq partitions:       " + intEq.size());
+            out.println("Number of intAdd partitions:      " + intAdd.size());
+            out.println("Number of all partitions:         " + varClass.getPartitions().size());
+            manager.printStatistics(out);
+          }
+
+          @Override
+          public String getName() {
+            return "BDDCPA";
+          }
+        });
   }
 
   @Override
