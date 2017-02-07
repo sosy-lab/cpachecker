b3d8a77209b85b5de8d70d1955da109b6bdc8940
Karlheinz Friedberger <kfriedberger@gmail.com>
2017-02-07 13:54:59 +0000

    BAMCache: documentation.
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@24307 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/cpa/bam/BAMCache.java
+++ src/org/sosy_lab/cpachecker/cpa/bam/BAMCache.java
@@ -24,6 +24,7 @@
 package org.sosy_lab.cpachecker.cpa.bam;
 
 import java.util.Collection;
+import javax.annotation.Nullable;
 import org.sosy_lab.cpachecker.cfa.blocks.Block;
 import org.sosy_lab.cpachecker.core.interfaces.AbstractState;
 import org.sosy_lab.cpachecker.core.interfaces.Precision;
@@ -32,29 +33,57 @@ import org.sosy_lab.cpachecker.core.reachedset.ReachedSet;
 import org.sosy_lab.cpachecker.cpa.arg.ARGState;
 import org.sosy_lab.cpachecker.util.Pair;
 
-
+/**
+ * The BAMCache is the central storage for BAM. It is a map of a triple (state, precision, context)
+ * towards a rached-set (or its result-states).
+ *
+ * <p>In most cases the following is satisfied: The state is equal to first state of the
+ * reached-set. The precision is equal to the initial precision of the reached-set. The context is
+ * the block where a block-entry equals the initial location of the reached-set.
+ */
 public interface BAMCache extends Statistics {
 
+  /**
+   * Store a reached-set in the cache. Does not yet register the result-states for the reached-set.
+   *
+   * @param item reached-set to be inserted into the cache
+   */
   void put(AbstractState stateKey, Precision precisionKey, Block context, ReachedSet item);
 
-  void put(AbstractState stateKey, Precision precisionKey, Block context,
+  /**
+   * Store the result-states for the reached-set in the cache. Does not yet register the
+   * result-states for the reached-set.
+   *
+   * @param item result-states to be inserted into the cache
+   * @param rootOfBlock optional, can be {@code null}, only used for PCC
+   */
+  void put(
+      AbstractState stateKey,
+      Precision precisionKey,
+      Block context,
       Collection<AbstractState> item,
-      ARGState rootOfBlock);
+      @Nullable ARGState rootOfBlock);
 
+  /**
+   * Invalidate the result-states of the given key. Does not remove the reached-set, thus it can be
+   * used for re-exploration.
+   */
   void remove(AbstractState stateKey, Precision precisionKey, Block context);
 
   /**
-   * This function returns a Pair of the reached-set and the returnStates for the given keys.
-   * Both members of the returned Pair are NULL, if there is a cache miss.
-   * For a partial cache hit we return the partly computed reached-set and NULL as returnStates. */
+   * Return a Pair of the reached-set and the result-states for the given key. Both members of the
+   * returned Pair are NULL, if there is a cache miss. For a partial cache hit we return the partly
+   * computed reached-set and NULL as returnStates.
+   */
   Pair<ReachedSet, Collection<AbstractState>> get(
-      AbstractState stateKey,
-      Precision precisionKey,
-      Block context);
+      AbstractState stateKey, Precision precisionKey, Block context);
 
+  /** Return the root-state of the last analyzed block, based on the last cache-access. */
   ARGState getLastAnalyzedBlock();
 
+  /** Check whether a cache entry exists for a given key. */
   boolean containsPreciseKey(AbstractState stateKey, Precision precisionKey, Block context);
 
+  /** Return all cached reached-sets. Useful for statistics. */
   Collection<ReachedSet> getAllCachedReachedStates();
 }
