e4a5023b2cd5e1e74e587ba46a1a79f3369139c3
Karlheinz Friedberger <kfriedberger@gmail.com>
2017-02-08 22:34:19 +0000

    IntervalCPA: move class ExpressionVisitor into its own file.
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@24325 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

---
+++ src/org/sosy_lab/cpachecker/cpa/interval/ExpressionValueVisitor.java
@@ -0,0 +1,205 @@
+/*
+ *  CPAchecker is a tool for configurable software verification.
+ *  This file is part of CPAchecker.
+ *
+ *  Copyright (C) 2007-2017  Dirk Beyer
+ *  All rights reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *
+ *  CPAchecker web page:
+ *    http://cpachecker.sosy-lab.org
+ */
+package org.sosy_lab.cpachecker.cpa.interval;
+
+import org.sosy_lab.cpachecker.cfa.ast.c.CBinaryExpression;
+import org.sosy_lab.cpachecker.cfa.ast.c.CBinaryExpression.BinaryOperator;
+import org.sosy_lab.cpachecker.cfa.ast.c.CCastExpression;
+import org.sosy_lab.cpachecker.cfa.ast.c.CCharLiteralExpression;
+import org.sosy_lab.cpachecker.cfa.ast.c.CExpression;
+import org.sosy_lab.cpachecker.cfa.ast.c.CFunctionCallExpression;
+import org.sosy_lab.cpachecker.cfa.ast.c.CIdExpression;
+import org.sosy_lab.cpachecker.cfa.ast.c.CImaginaryLiteralExpression;
+import org.sosy_lab.cpachecker.cfa.ast.c.CIntegerLiteralExpression;
+import org.sosy_lab.cpachecker.cfa.ast.c.CRightHandSideVisitor;
+import org.sosy_lab.cpachecker.cfa.ast.c.CUnaryExpression;
+import org.sosy_lab.cpachecker.cfa.ast.c.DefaultCExpressionVisitor;
+import org.sosy_lab.cpachecker.cfa.model.CFAEdge;
+import org.sosy_lab.cpachecker.cfa.types.c.CEnumType.CEnumerator;
+import org.sosy_lab.cpachecker.exceptions.UnrecognizedCCodeException;
+
+/** Visitor that get's the interval from an expression, */
+class ExpressionValueVisitor extends DefaultCExpressionVisitor<Interval, UnrecognizedCCodeException>
+    implements CRightHandSideVisitor<Interval, UnrecognizedCCodeException> {
+
+  private final IntervalAnalysisState readableState;
+
+  private final CFAEdge cfaEdge;
+
+  public ExpressionValueVisitor(IntervalAnalysisState pState, CFAEdge edge) {
+    readableState = pState;
+    cfaEdge = edge;
+  }
+
+  @Override
+  protected Interval visitDefault(CExpression expression) {
+    return Interval.createUnboundInterval();
+  }
+
+  @Override
+  public Interval visit(CBinaryExpression binaryExpression) throws UnrecognizedCCodeException {
+    Interval interval1 = binaryExpression.getOperand1().accept(this);
+    Interval interval2 = binaryExpression.getOperand2().accept(this);
+
+    if (interval1 == null || interval2 == null) {
+      return Interval.createUnboundInterval();
+    }
+
+    BinaryOperator operator = binaryExpression.getOperator();
+    if (operator.isLogicalOperator()) {
+      return getLogicInterval(operator, interval1, interval2);
+    } else {
+      return getArithmeticInterval(operator, interval1, interval2);
+    }
+  }
+
+  private static Interval getLogicInterval(
+      BinaryOperator operator, Interval interval1, Interval interval2) {
+    switch (operator) {
+      case EQUALS:
+        if (!interval1.intersects(interval2)) {
+          return Interval.ZERO;
+        } else if (interval1.getLow().equals(interval1.getHigh()) && interval1.equals(interval2)) {
+          // singular interval, [5;5]==[5;5]
+          return Interval.ONE;
+        } else {
+          return Interval.createBooleanInterval();
+        }
+
+      case NOT_EQUALS:
+        if (!interval1.intersects(interval2)) {
+          return Interval.ONE;
+        } else if (interval1.getLow().equals(interval1.getHigh()) && interval1.equals(interval2)) {
+          // singular interval, [5;5]!=[5;5]
+          return Interval.ZERO;
+        } else {
+          return Interval.createBooleanInterval();
+        }
+
+      case GREATER_THAN:
+        if (interval1.isGreaterThan(interval2)) {
+          return Interval.ONE;
+        } else if (interval2.isGreaterOrEqualThan(interval1)) {
+          return Interval.ZERO;
+        } else {
+          return Interval.createBooleanInterval();
+        }
+
+      case GREATER_EQUAL: // a>=b == a+1>b, works only for integers
+        return getLogicInterval(
+            BinaryOperator.GREATER_THAN, interval1.plus(new Interval(1L)), interval2);
+
+      case LESS_THAN: // a<b == b>a
+        return getLogicInterval(BinaryOperator.GREATER_THAN, interval2, interval1);
+
+      case LESS_EQUAL: // a<=b == b+1>a, works only for integers
+        return getLogicInterval(
+            BinaryOperator.GREATER_THAN, interval2.plus(new Interval(1L)), interval1);
+
+      default:
+        throw new AssertionError("unknown binary operator: " + operator);
+    }
+  }
+
+  private static Interval getArithmeticInterval(
+      BinaryOperator operator, Interval interval1, Interval interval2) {
+    switch (operator) {
+      case PLUS:
+        return interval1.plus(interval2);
+      case MINUS:
+        return interval1.minus(interval2);
+      case MULTIPLY:
+        return interval1.times(interval2);
+      case DIVIDE:
+        return interval1.divide(interval2);
+      case SHIFT_LEFT:
+        return interval1.shiftLeft(interval2);
+      case SHIFT_RIGHT:
+        return interval1.shiftRight(interval2);
+      case MODULO:
+        return interval1.modulo(interval2);
+      case BINARY_AND:
+      case BINARY_OR:
+      case BINARY_XOR:
+        return Interval.createUnboundInterval();
+      default:
+        throw new AssertionError("unknown binary operator: " + operator);
+    }
+  }
+
+  @Override
+  public Interval visit(CCastExpression cast) throws UnrecognizedCCodeException {
+    return cast.getOperand().accept(this);
+  }
+
+  @Override
+  public Interval visit(CFunctionCallExpression functionCall) {
+    return Interval.createUnboundInterval();
+  }
+
+  @Override
+  public Interval visit(CCharLiteralExpression charLiteral) {
+    return new Interval((long) charLiteral.getCharacter());
+  }
+
+  @Override
+  public Interval visit(CImaginaryLiteralExpression exp) throws UnrecognizedCCodeException {
+    return exp.getValue().accept(this);
+  }
+
+  @Override
+  public Interval visit(CIntegerLiteralExpression integerLiteral) {
+    return new Interval(integerLiteral.asLong());
+  }
+
+  @Override
+  public Interval visit(CIdExpression identifier) {
+    if (identifier.getDeclaration() instanceof CEnumerator) {
+      return new Interval(((CEnumerator) identifier.getDeclaration()).getValue());
+    }
+
+    final String variableName = identifier.getDeclaration().getQualifiedName();
+    if (readableState.contains(variableName)) {
+      return readableState.getInterval(variableName);
+    } else {
+      return Interval.createUnboundInterval();
+    }
+  }
+
+  @Override
+  public Interval visit(CUnaryExpression unaryExpression) throws UnrecognizedCCodeException {
+    Interval interval = unaryExpression.getOperand().accept(this);
+    switch (unaryExpression.getOperator()) {
+      case MINUS:
+        return interval.negate();
+
+      case AMPER:
+      case TILDE:
+        return Interval.createUnboundInterval(); // valid expression, but it's a pointer value
+
+      default:
+        throw new UnrecognizedCCodeException("unknown unary operator", cfaEdge, unaryExpression);
+    }
+  }
+}
--- src/org/sosy_lab/cpachecker/cpa/interval/IntervalAnalysisTransferRelation.java
+++ src/org/sosy_lab/cpachecker/cpa/interval/IntervalAnalysisTransferRelation.java
@@ -2,7 +2,7 @@
  *  CPAchecker is a tool for configurable software verification.
  *  This file is part of CPAchecker.
  *
- *  Copyright (C) 2007-2014  Dirk Beyer
+ *  Copyright (C) 2007-2017  Dirk Beyer
  *  All rights reserved.
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
@@ -23,6 +23,13 @@
  */
 package org.sosy_lab.cpachecker.cpa.interval;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Optional;
+import java.util.logging.Level;
 import org.sosy_lab.common.configuration.Configuration;
 import org.sosy_lab.common.configuration.InvalidConfigurationException;
 import org.sosy_lab.common.configuration.Option;
@@ -31,26 +38,18 @@ import org.sosy_lab.common.log.LogManager;
 import org.sosy_lab.cpachecker.cfa.ast.c.CAssignment;
 import org.sosy_lab.cpachecker.cfa.ast.c.CBinaryExpression;
 import org.sosy_lab.cpachecker.cfa.ast.c.CBinaryExpression.BinaryOperator;
-import org.sosy_lab.cpachecker.cfa.ast.c.CCastExpression;
-import org.sosy_lab.cpachecker.cfa.ast.c.CCharLiteralExpression;
 import org.sosy_lab.cpachecker.cfa.ast.c.CDeclaration;
 import org.sosy_lab.cpachecker.cfa.ast.c.CExpression;
 import org.sosy_lab.cpachecker.cfa.ast.c.CFunctionCall;
 import org.sosy_lab.cpachecker.cfa.ast.c.CFunctionCallAssignmentStatement;
-import org.sosy_lab.cpachecker.cfa.ast.c.CFunctionCallExpression;
 import org.sosy_lab.cpachecker.cfa.ast.c.CFunctionCallStatement;
 import org.sosy_lab.cpachecker.cfa.ast.c.CIdExpression;
-import org.sosy_lab.cpachecker.cfa.ast.c.CImaginaryLiteralExpression;
 import org.sosy_lab.cpachecker.cfa.ast.c.CInitializer;
 import org.sosy_lab.cpachecker.cfa.ast.c.CInitializerExpression;
-import org.sosy_lab.cpachecker.cfa.ast.c.CIntegerLiteralExpression;
 import org.sosy_lab.cpachecker.cfa.ast.c.CParameterDeclaration;
 import org.sosy_lab.cpachecker.cfa.ast.c.CRightHandSide;
-import org.sosy_lab.cpachecker.cfa.ast.c.CRightHandSideVisitor;
 import org.sosy_lab.cpachecker.cfa.ast.c.CStatement;
-import org.sosy_lab.cpachecker.cfa.ast.c.CUnaryExpression;
 import org.sosy_lab.cpachecker.cfa.ast.c.CVariableDeclaration;
-import org.sosy_lab.cpachecker.cfa.ast.c.DefaultCExpressionVisitor;
 import org.sosy_lab.cpachecker.cfa.model.BlankEdge;
 import org.sosy_lab.cpachecker.cfa.model.CFAEdge;
 import org.sosy_lab.cpachecker.cfa.model.FunctionExitNode;
@@ -61,20 +60,11 @@ import org.sosy_lab.cpachecker.cfa.model.c.CFunctionReturnEdge;
 import org.sosy_lab.cpachecker.cfa.model.c.CFunctionSummaryEdge;
 import org.sosy_lab.cpachecker.cfa.model.c.CReturnStatementEdge;
 import org.sosy_lab.cpachecker.cfa.model.c.CStatementEdge;
-import org.sosy_lab.cpachecker.cfa.types.c.CEnumType.CEnumerator;
 import org.sosy_lab.cpachecker.cfa.types.c.CPointerType;
 import org.sosy_lab.cpachecker.core.defaults.ForwardingTransferRelation;
 import org.sosy_lab.cpachecker.core.interfaces.Precision;
 import org.sosy_lab.cpachecker.exceptions.UnrecognizedCCodeException;
 import org.sosy_lab.cpachecker.exceptions.UnrecognizedCodeException;
-
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Optional;
-import java.util.logging.Level;
 
 
 @Options(prefix="cpa.interval")
@@ -463,175 +453,6 @@ public class IntervalAnalysisTransferRelation extends ForwardingTransferRelation
 
   private Collection<IntervalAnalysisState> noSuccessors() {
     return Collections.emptySet();
-  }
-
-  /**
-   * Visitor that get's the interval from an expression,
-   */
-  private static class ExpressionValueVisitor
-      extends DefaultCExpressionVisitor<Interval, UnrecognizedCCodeException>
-      implements CRightHandSideVisitor<Interval, UnrecognizedCCodeException> {
-
-    private final IntervalAnalysisState readableState;
-
-    private final CFAEdge cfaEdge;
-
-    public ExpressionValueVisitor(IntervalAnalysisState pState, CFAEdge edge) {
-      readableState = pState;
-      cfaEdge = edge;
-    }
-
-    @Override
-    protected Interval visitDefault(CExpression expression) {
-      return Interval.createUnboundInterval();
-    }
-
-    @Override
-    public Interval visit(CBinaryExpression binaryExpression) throws UnrecognizedCCodeException {
-      Interval interval1 = binaryExpression.getOperand1().accept(this);
-      Interval interval2 = binaryExpression.getOperand2().accept(this);
-
-      if (interval1 == null || interval2 == null) {
-        return Interval.createUnboundInterval();
-      }
-
-      BinaryOperator operator = binaryExpression.getOperator();
-      if (operator.isLogicalOperator()) {
-        return getLogicInterval(operator, interval1, interval2);
-      } else {
-        return getArithmeticInterval(operator, interval1, interval2);
-      }
-    }
-
-    private static Interval getLogicInterval(BinaryOperator operator,
-        Interval interval1, Interval interval2) {
-      switch (operator) {
-      case EQUALS:
-        if (!interval1.intersects(interval2)) {
-          return Interval.ZERO;
-        } else if (interval1.getLow().equals(interval1.getHigh()) && interval1.equals(interval2)) {
-          // singular interval, [5;5]==[5;5]
-          return Interval.ONE;
-        } else {
-          return Interval.createBooleanInterval();
-        }
-
-      case NOT_EQUALS:
-        if (!interval1.intersects(interval2)) {
-          return Interval.ONE;
-        } else if (interval1.getLow().equals(interval1.getHigh()) && interval1.equals(interval2)) {
-          // singular interval, [5;5]!=[5;5]
-          return Interval.ZERO;
-        } else {
-          return Interval.createBooleanInterval();
-        }
-
-      case GREATER_THAN:
-        if (interval1.isGreaterThan(interval2)) {
-          return Interval.ONE;
-        } else if (interval2.isGreaterOrEqualThan(interval1)) {
-          return Interval.ZERO;
-        } else {
-          return Interval.createBooleanInterval();
-        }
-
-      case GREATER_EQUAL: // a>=b == a+1>b, works only for integers
-        return getLogicInterval(BinaryOperator.GREATER_THAN,
-            interval1.plus(new Interval(1L)), interval2);
-
-      case LESS_THAN: // a<b == b>a
-        return getLogicInterval(BinaryOperator.GREATER_THAN,
-            interval2, interval1);
-
-      case LESS_EQUAL: // a<=b == b+1>a, works only for integers
-        return getLogicInterval(BinaryOperator.GREATER_THAN,
-            interval2.plus(new Interval(1L)), interval1);
-
-      default:
-        throw new AssertionError("unknown binary operator: " + operator);
-      }
-    }
-
-    private static Interval getArithmeticInterval(BinaryOperator operator,
-        Interval interval1, Interval interval2) {
-      switch (operator) {
-      case PLUS:
-        return interval1.plus(interval2);
-      case MINUS:
-        return interval1.minus(interval2);
-      case MULTIPLY:
-        return interval1.times(interval2);
-      case DIVIDE:
-        return interval1.divide(interval2);
-      case SHIFT_LEFT:
-        return interval1.shiftLeft(interval2);
-      case SHIFT_RIGHT:
-        return interval1.shiftRight(interval2);
-      case MODULO:
-        return interval1.modulo(interval2);
-      case BINARY_AND:
-      case BINARY_OR:
-      case BINARY_XOR:
-        return Interval.createUnboundInterval();
-      default:
-        throw new AssertionError("unknown binary operator: " + operator);
-      }
-    }
-
-    @Override
-    public Interval visit(CCastExpression cast) throws UnrecognizedCCodeException {
-      return cast.getOperand().accept(this);
-    }
-
-    @Override
-    public Interval visit(CFunctionCallExpression functionCall) throws UnrecognizedCCodeException {
-      return Interval.createUnboundInterval();
-    }
-    @Override
-    public Interval visit(CCharLiteralExpression charLiteral) throws UnrecognizedCCodeException {
-      return new Interval((long)charLiteral.getCharacter());
-    }
-
-    @Override
-    public Interval visit(CImaginaryLiteralExpression exp) throws UnrecognizedCCodeException {
-      return exp.getValue().accept(this);
-    }
-
-    @Override
-    public Interval visit(CIntegerLiteralExpression integerLiteral) throws UnrecognizedCCodeException {
-      return new Interval(integerLiteral.asLong());
-    }
-
-    @Override
-    public Interval visit(CIdExpression identifier) throws UnrecognizedCCodeException {
-      if (identifier.getDeclaration() instanceof CEnumerator) {
-        return new Interval(((CEnumerator)identifier.getDeclaration()).getValue());
-      }
-
-      final String variableName = identifier.getDeclaration().getQualifiedName();
-      if (readableState.contains(variableName)) {
-        return readableState.getInterval(variableName);
-      } else {
-        return Interval.createUnboundInterval();
-      }
-    }
-
-    @Override
-    public Interval visit(CUnaryExpression unaryExpression) throws UnrecognizedCCodeException {
-      Interval interval = unaryExpression.getOperand().accept(this);
-      switch (unaryExpression.getOperator()) {
-
-      case MINUS:
-        return interval.negate();
-
-      case AMPER:
-      case TILDE:
-        return Interval.createUnboundInterval(); // valid expression, but it's a pointer value
-
-      default:
-        throw new UnrecognizedCCodeException("unknown unary operator", cfaEdge, unaryExpression);
-      }
-    }
   }
 }
 
