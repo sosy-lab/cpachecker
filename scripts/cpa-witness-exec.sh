503c44157d7d497a18fd21a601990f1d6911f65b
lemberge <lemberge@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2017-02-12 20:09:01 +0000

    Add executable for cpa-witness-exec validator
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@24344 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

---
+++ scripts/cpa-witness-exec.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+count() { echo $#; }
+
+CPA_EXEC=`which cpa.sh`
+[[ -z $CPA_EXEC ]] && [[ -f './cpa.sh' ]] && CPA_EXEC='./cpa.sh'
+[[ -z $CPA_EXEC ]] && [[ -f './scripts/cpa.sh' ]] && CPA_EXEC='./scripts/cpa.sh'
+
+EXPECTED_RETURN_CODE=107
+ARGS="$@"
+
+
+for file in $@; do true; done # store last argument into $file
+output_path='output'
+while [[ $# -gt 0 ]]
+do
+key="$1"
+case $key in
+    -outputpath)
+    output_path="$2"
+    shift
+    ;;
+    -spec|-config|-cp|-classpath|-cpas|-entryfunction|-logfile|-outputpath|-setprop|-sourcepath|-timelimit|-witness)
+    shift
+    ;;
+    -h|-help)
+    $CPA_EXEC -h
+    exit 0
+    ;;
+    -v|-version)
+    $CPA_EXEC -v
+    exit 0
+    ;;
+esac
+shift # past argument or value
+done
+
+
+harness_gen_output=`$CPA_EXEC $ARGS`
+harnesses=`find $output_path -name '*harness.c'` 
+echo "`count $harnesses` harnesses for witness."  
+for harness in $harnesses; do
+  echo "Looking at $harness"
+  gcc -o ${output_path}/test_suite "$harness" "$file"
+
+  ${output_path}/test_suite
+  test_return=$?
+  if [[ test_return -eq $EXPECTED_RETURN_CODE ]]; then
+    echo "Verification result: FALSE. Harness $harness reached expected error location."
+    exit 0
+
+  else 
+    echo "Run with harness $harness was not successful."
+  fi
+done 
+
+echo "Verification result: UNKOWN. No harness for witness was sucessful or no harness was produced."
