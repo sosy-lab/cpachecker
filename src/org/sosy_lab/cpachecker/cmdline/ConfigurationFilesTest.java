381bf8e020b47cd0873695d19476bc265a1520e0
mcjakobs <mcjakobs@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2017-03-15 20:25:29 +0000

    configurations for cmc proof checking that constructs condition after checking each ARG
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@24588 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/cmdline/ConfigurationFilesTest.java
+++ src/org/sosy_lab/cpachecker/cmdline/ConfigurationFilesTest.java
@@ -119,6 +119,9 @@ public class ConfigurationFilesTest {
           "witness.validation.violation.config",
           "witness.validation.correctness.config",
           "pcc.proofgen.doPCC",
+          "pcc.strategy",
+          "pcc.cmc.configFiles",
+          "pcc.cmc.file",
           // only handled if specification automaton is additionally specified
           "cpa.automaton.breakOnTargetState",
           "WitnessAutomaton.cpa.automaton.treatErrorsAsTargets",
