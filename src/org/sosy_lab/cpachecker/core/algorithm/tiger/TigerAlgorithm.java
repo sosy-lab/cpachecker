62934210e619b38eb9d34c8036e308f8fbf07490
hess <hess@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2017-05-29 11:54:31 +0000

    Analysis now running but returning incomplete result probably because of false setting of the precision adjustment in one of the used CPAs
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/tigerIntegration@24768 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/core/algorithm/tiger/TigerAlgorithm.java
+++ src/org/sosy_lab/cpachecker/core/algorithm/tiger/TigerAlgorithm.java
@@ -83,7 +83,6 @@ import org.sosy_lab.cpachecker.cpa.arg.ARGUtils;
 import org.sosy_lab.cpachecker.cpa.automaton.Automaton;
 import org.sosy_lab.cpachecker.cpa.automaton.ControlAutomatonCPA;
 import org.sosy_lab.cpachecker.cpa.composite.CompositeCPA;
-import org.sosy_lab.cpachecker.cpa.guardededgeautomaton.productautomaton.ProductAutomatonCPA;
 import org.sosy_lab.cpachecker.cpa.predicate.PredicateCPARefiner;
 import org.sosy_lab.cpachecker.exceptions.CPAEnabledAnalysisPropertyViolationException;
 import org.sosy_lab.cpachecker.exceptions.CPAException;
@@ -276,8 +275,6 @@ public class TigerAlgorithm implements Algorithm {
     }
 
     LinkedList<ConfigurableProgramAnalysis> lComponentAnalyses = new LinkedList<>();
-    int lProductAutomatonIndex = lComponentAnalyses.size();
-    lComponentAnalyses.add(ProductAutomatonCPA.create(lAutomatonCPAs, false, config));
 
     if (cpa instanceof CompositeCPA) {
       CompositeCPA compositeCPA = (CompositeCPA) cpa;
