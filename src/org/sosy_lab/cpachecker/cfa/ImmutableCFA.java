70bdf0c31ad9f43dd804187a3970fd882af0cad6
Karlheinz Friedberger <kfriedberger@gmail.com>
2017-06-01 13:47:26 +0000

    CFA-Serialization: add code for serializing the ImmutableCFA.
    As CFANodes do not serialize their edges,
    we dump the edges explicitly and
    link them into a full CFA after reading.
    
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@24797 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/cfa/ImmutableCFA.java
+++ src/org/sosy_lab/cpachecker/cfa/ImmutableCFA.java
@@ -26,12 +26,17 @@ package org.sosy_lab.cpachecker.cfa;
 import static com.google.common.base.Preconditions.*;
 
 import com.google.common.base.Optional;
+import com.google.common.collect.Iterables;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
-
 import org.sosy_lab.common.Optionals;
+import org.sosy_lab.cpachecker.cfa.model.CFAEdge;
 import org.sosy_lab.cpachecker.cfa.model.CFANode;
 import org.sosy_lab.cpachecker.cfa.model.FunctionEntryNode;
 import org.sosy_lab.cpachecker.cfa.types.MachineModel;
+import org.sosy_lab.cpachecker.util.CFAUtils;
 import org.sosy_lab.cpachecker.util.LiveVariables;
 import org.sosy_lab.cpachecker.util.LoopStructure;
 import org.sosy_lab.cpachecker.util.VariableClassification;
@@ -46,8 +51,9 @@ import com.google.common.collect.SetMultimap;
  * This class represents a CFA after it has been fully created (parsing, linking
  * of functions, etc.).
  */
-class ImmutableCFA implements CFA {
+class ImmutableCFA implements CFA, Serializable {
 
+  private static final long serialVersionUID = 5399965350156780812L;
   private final MachineModel machineModel;
   private final ImmutableSortedMap<String, FunctionEntryNode> functions;
   private final ImmutableSortedSet<CFANode> allNodes;
@@ -167,4 +173,42 @@ class ImmutableCFA implements CFA {
     return language;
   }
 
+
+  private void writeObject(java.io.ObjectOutputStream s) throws java.io.IOException {
+
+    // write default stuff
+    s.defaultWriteObject();
+
+    // we have to keep the order of edges 'AS IS'
+    final List<CFAEdge> enteringEdges = new ArrayList<>();
+    for (CFANode node : allNodes) {
+      Iterables.addAll(enteringEdges, CFAUtils.enteringEdges(node));
+    }
+    s.writeObject(enteringEdges);
+
+    // we have to keep the order of edges 'AS IS'
+    final List<CFAEdge> leavingEdges = new ArrayList<>();
+    for (CFANode node : allNodes) {
+      Iterables.addAll(leavingEdges, CFAUtils.leavingEdges(node));
+    }
+    s.writeObject(leavingEdges);
+  }
+
+  @SuppressWarnings("unchecked")
+  private void readObject(java.io.ObjectInputStream s)
+      throws java.io.IOException, ClassNotFoundException {
+
+    // read default stuff
+    s.defaultReadObject();
+
+    // read entering edges, we have to keep the order of edges 'AS IS'
+    for (CFAEdge edge : (List<CFAEdge>) s.readObject()) {
+      edge.getSuccessor().addEnteringEdge(edge);
+    }
+
+    // read leaving edges, we have to keep the order of edges 'AS IS'
+    for (CFAEdge edge : (List<CFAEdge>) s.readObject()) {
+      edge.getPredecessor().addLeavingEdge(edge);
+    }
+  }
 }
