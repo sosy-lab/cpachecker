45d07d80b2632da094f7c244d436e569c38fe55a
Karlheinz Friedberger <kfriedberger@gmail.com>
2017-07-25 19:50:24 +0000

    SMGCPA: refactoring, extract several nested classes into their own file.
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@25363 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

---
+++ src/org/sosy_lab/cpachecker/cpa/smg/evaluator/RHSLValueAssignmentVisitor.java
@@ -0,0 +1,88 @@
+/*
+ *  CPAchecker is a tool for configurable software verification.
+ *  This file is part of CPAchecker.
+ *
+ *  Copyright (C) 2007-2017  Dirk Beyer
+ *  All rights reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *
+ *  CPAchecker web page:
+ *    http://cpachecker.sosy-lab.org
+ */
+package org.sosy_lab.cpachecker.cpa.smg.evaluator;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import org.sosy_lab.cpachecker.cfa.ast.c.CArraySubscriptExpression;
+import org.sosy_lab.cpachecker.cfa.ast.c.CFieldReference;
+import org.sosy_lab.cpachecker.cfa.ast.c.CIdExpression;
+import org.sosy_lab.cpachecker.cfa.ast.c.CPointerExpression;
+import org.sosy_lab.cpachecker.cfa.model.CFAEdge;
+import org.sosy_lab.cpachecker.cpa.smg.SMGState;
+import org.sosy_lab.cpachecker.cpa.smg.evaluator.SMGAbstractObjectAndState.SMGAddressAndState;
+import org.sosy_lab.cpachecker.exceptions.CPATransferException;
+
+class RHSLValueAssignmentVisitor extends LValueAssignmentVisitor {
+
+  private final SMGRightHandSideEvaluator smgRightHandSideEvaluator;
+
+  public RHSLValueAssignmentVisitor(SMGRightHandSideEvaluator pSmgRightHandSideEvaluator, SMGExpressionEvaluator pSmgExpressionEvaluator, CFAEdge pEdge, SMGState pSmgState) {
+    super(pSmgExpressionEvaluator, pEdge, pSmgState);
+    smgRightHandSideEvaluator = pSmgRightHandSideEvaluator;
+  }
+
+  @Override
+  public List<SMGAddressAndState> visit(CIdExpression variableName) throws CPATransferException {
+    smgRightHandSideEvaluator.data.getLogger().log(Level.ALL, ">>> Handling statement: variable assignment");
+
+    // a = ...
+    return super.visit(variableName);
+  }
+
+  @Override
+  public List<SMGAddressAndState> visit(CPointerExpression pLValue)
+      throws CPATransferException {
+    smgRightHandSideEvaluator.data.getLogger().log(Level.ALL, ">>> Handling statement: assignment to dereferenced pointer");
+
+    List<SMGAddressAndState> addresses = super.visit(pLValue);
+
+    List<SMGAddressAndState> results = new ArrayList<>(addresses.size());
+
+    for (SMGAddressAndState address : addresses) {
+      if (address.getObject().isUnknown()) {
+        SMGState newState = address.getSmgState().setUnknownDereference();
+        results.add(SMGAddressAndState.of(newState));
+      } else {
+        results.add(address);
+      }
+    }
+    return results;
+  }
+
+  @Override
+  public List<SMGAddressAndState> visit(CFieldReference lValue) throws CPATransferException {
+    smgRightHandSideEvaluator.data.getLogger().log(Level.ALL, ">>> Handling statement: assignment to field reference");
+
+    return super.visit(lValue);
+  }
+
+  @Override
+  public List<SMGAddressAndState> visit(CArraySubscriptExpression lValue) throws CPATransferException {
+    smgRightHandSideEvaluator.data.getLogger().log(Level.ALL, ">>> Handling statement: assignment to array Cell");
+
+    return super.visit(lValue);
+  }
+}
