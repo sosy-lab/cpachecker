f77acafefc9e6cdba2810b63b35a1e2a1025501a
dunaev <dunaev@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2017-08-21 13:32:50 +0000

    Neues Interface wurde eingeführt. Klassen für Intervals und Values wurden angepasst. vierter Teil
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/unify_value_interval@25613 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/cpa/interval/ExpressionValueVisitor.java
+++ src/org/sosy_lab/cpachecker/cpa/interval/ExpressionValueVisitor.java
@@ -28,6 +28,7 @@ import org.sosy_lab.cpachecker.cfa.ast.c.CBinaryExpression.BinaryOperator;
 import org.sosy_lab.cpachecker.cfa.ast.c.CCastExpression;
 import org.sosy_lab.cpachecker.cfa.ast.c.CCharLiteralExpression;
 import org.sosy_lab.cpachecker.cfa.ast.c.CExpression;
+import org.sosy_lab.cpachecker.cfa.ast.c.CFloatLiteralExpression;
 import org.sosy_lab.cpachecker.cfa.ast.c.CFunctionCallExpression;
 import org.sosy_lab.cpachecker.cfa.ast.c.CIdExpression;
 import org.sosy_lab.cpachecker.cfa.ast.c.CImaginaryLiteralExpression;
@@ -40,166 +41,177 @@ import org.sosy_lab.cpachecker.cfa.types.c.CEnumType.CEnumerator;
 import org.sosy_lab.cpachecker.exceptions.UnrecognizedCCodeException;
 
 /** Visitor that get's the interval from an expression, */
-class ExpressionValueVisitor extends DefaultCExpressionVisitor<Interval, UnrecognizedCCodeException>
-    implements CRightHandSideVisitor<Interval, UnrecognizedCCodeException> {
+class ExpressionValueVisitor extends DefaultCExpressionVisitor<NumberInterface, UnrecognizedCCodeException>
+        implements CRightHandSideVisitor<NumberInterface, UnrecognizedCCodeException> {
 
-  private final IntervalAnalysisState readableState;
+    private final IntervalAnalysisState readableState;
 
-  private final CFAEdge cfaEdge;
+    private final CFAEdge cfaEdge;
 
-  public ExpressionValueVisitor(IntervalAnalysisState pState, CFAEdge edge) {
-    readableState = pState;
-    cfaEdge = edge;
-  }
-
-  @Override
-  protected Interval visitDefault(CExpression expression) {
-    return Interval.UNBOUND;
-  }
-
-  @Override
-  public Interval visit(CBinaryExpression binaryExpression) throws UnrecognizedCCodeException {
-    Interval interval1 = binaryExpression.getOperand1().accept(this);
-    Interval interval2 = binaryExpression.getOperand2().accept(this);
-
-    if (interval1 == null || interval2 == null) {
-      return Interval.UNBOUND;
+    public ExpressionValueVisitor(IntervalAnalysisState pState, CFAEdge edge) {
+        readableState = pState;
+        cfaEdge = edge;
     }
 
-    BinaryOperator operator = binaryExpression.getOperator();
-    if (operator.isLogicalOperator()) {
-      return getLogicInterval(operator, interval1, interval2);
-    } else {
-      return getArithmeticInterval(operator, interval1, interval2);
+    @Override
+    protected NumberInterface visitDefault(CExpression expression) {
+        return new CreatorIntegerInterval().factoryMethod(null).UNBOUND();
     }
-  }
 
-  private static Interval getLogicInterval(
-      BinaryOperator operator, Interval interval1, Interval interval2) {
-    switch (operator) {
-      case EQUALS:
-        if (!interval1.intersects(interval2)) {
-          return Interval.ZERO;
-        } else if (interval1.getLow().equals(interval1.getHigh()) && interval1.equals(interval2)) {
-          // singular interval, [5;5]==[5;5]
-          return Interval.ONE;
-        } else {
-          return Interval.BOOLEAN_INTERVAL;
+    @Override
+    public NumberInterface visit(CBinaryExpression binaryExpression) throws UnrecognizedCCodeException {
+        NumberInterface interval1 = binaryExpression.getOperand1().accept(this);
+        NumberInterface interval2 = binaryExpression.getOperand2().accept(this);
+
+        if (interval1 == null || interval2 == null) {
+            return new CreatorIntegerInterval().factoryMethod(null).UNBOUND();
         }
 
-      case NOT_EQUALS:
-        if (!interval1.intersects(interval2)) {
-          return Interval.ONE;
-        } else if (interval1.getLow().equals(interval1.getHigh()) && interval1.equals(interval2)) {
-          // singular interval, [5;5]!=[5;5]
-          return Interval.ZERO;
+        BinaryOperator operator = binaryExpression.getOperator();
+        if (operator.isLogicalOperator()) {
+            return getLogicInterval(operator, interval1, interval2);
         } else {
-          return Interval.BOOLEAN_INTERVAL;
+            return getArithmeticInterval(operator, interval1, interval2);
         }
+    }
 
-      case GREATER_THAN:
-        if (interval1.isGreaterThan(interval2)) {
-          return Interval.ONE;
-        } else if (interval2.isGreaterOrEqualThan(interval1)) {
-          return Interval.ZERO;
+    private static NumberInterface getLogicInterval(BinaryOperator operator, NumberInterface interval1,
+            NumberInterface interval2) {
+        switch (operator) {
+        case EQUALS:
+            if (!interval1.intersects(interval2)) {
+                return new CreatorIntegerInterval().factoryMethod(null).ZERO();
+            } else if (interval1.getLow().equals(interval1.getHigh()) && interval1.equals(interval2)) {
+                // singular interval, [5;5]==[5;5]
+                return new CreatorIntegerInterval().factoryMethod(null).ONE();
+            } else {
+                return new CreatorIntegerInterval().factoryMethod(null).BOOLEAN_INTERVAL();
+            }
+
+        case NOT_EQUALS:
+            if (!interval1.intersects(interval2)) {
+                return new CreatorIntegerInterval().factoryMethod(null).ONE();
+            } else if (interval1.getLow().equals(interval1.getHigh()) && interval1.equals(interval2)) {
+                // singular interval, [5;5]!=[5;5]
+                return new CreatorIntegerInterval().factoryMethod(null).ZERO();
+            } else {
+                return new CreatorIntegerInterval().factoryMethod(null).BOOLEAN_INTERVAL();
+            }
+
+        case GREATER_THAN:
+            if (interval1.isGreaterThan(interval2)) {
+                return new CreatorIntegerInterval().factoryMethod(null).ONE();
+            } else if (interval2.isGreaterOrEqualThan(interval1)) {
+                return new CreatorIntegerInterval().factoryMethod(null).ZERO();
+            } else {
+                return new CreatorIntegerInterval().factoryMethod(null).BOOLEAN_INTERVAL();
+            }
+
+        case GREATER_EQUAL: // a>=b == a+1>b, works only for integers
+            return getLogicInterval(BinaryOperator.GREATER_THAN,
+                    interval1.plus(new CreatorIntegerInterval().factoryMethod(null).ONE()), interval2);
+
+        case LESS_THAN: // a<b == b>a
+            return getLogicInterval(BinaryOperator.GREATER_THAN, interval2, interval1);
+
+        case LESS_EQUAL: // a<=b == b+1>a, works only for integers
+            return getLogicInterval(BinaryOperator.GREATER_THAN,
+                    interval2.plus(new CreatorIntegerInterval().factoryMethod(null).ONE()), interval1);
+
+        default:
+            throw new AssertionError("unknown binary operator: " + operator);
+        }
+    }
+
+    private static NumberInterface getArithmeticInterval(BinaryOperator operator, NumberInterface interval1,
+            NumberInterface interval2) {
+
+        switch (operator) {
+        case PLUS:
+            return interval1.plus(interval2);
+        case MINUS:
+            return interval1.minus(interval2);
+        case MULTIPLY:
+            return interval1.times(interval2);
+        case DIVIDE:
+            return interval1.divide(interval2);
+        case SHIFT_LEFT:
+            return interval1.shiftLeft(interval2);
+        case SHIFT_RIGHT:
+            return interval1.shiftRight(interval2);
+        case MODULO:
+            return interval1.modulo(interval2);
+        case BINARY_AND:
+        case BINARY_OR:
+        case BINARY_XOR:
+            return new CreatorIntegerInterval().factoryMethod(null).UNBOUND();
+        default:
+            throw new AssertionError("unknown binary operator: " + operator);
+        }
+    }
+
+    @Override
+    public NumberInterface visit(CCastExpression cast) throws UnrecognizedCCodeException {
+        return cast.getOperand().accept(this);
+    }
+
+    @Override
+    public NumberInterface visit(CFunctionCallExpression functionCall) {
+        return new CreatorIntegerInterval().factoryMethod(null).UNBOUND();
+    }
+
+    @Override
+    public NumberInterface visit(CCharLiteralExpression charLiteral) {
+        return new CreatorIntegerInterval().factoryMethod((long) charLiteral.getCharacter());
+    }
+
+    @Override
+    public NumberInterface visit(CImaginaryLiteralExpression exp) throws UnrecognizedCCodeException {
+        return exp.getValue().accept(this);
+    }
+
+    @Override
+    public NumberInterface visit(CIntegerLiteralExpression integerLiteral) {
+        // TODO BigInteger
+        return new CreatorIntegerInterval().factoryMethod(integerLiteral.asLong());
+    }
+
+    @Override
+    public NumberInterface visit(CFloatLiteralExpression floatLiteral) {
+        // TODO BigDecimal
+        return new CreatorDoubleInterval().factoryMethod(floatLiteral.getValue().doubleValue());
+    }
+
+    @Override
+    public NumberInterface visit(CIdExpression identifier) {
+        if (identifier.getDeclaration() instanceof CEnumerator) {
+            return new CreatorIntegerInterval().factoryMethod(((CEnumerator) identifier.getDeclaration()).getValue());
+        }
+        final String variableName = identifier.getDeclaration().getQualifiedName();
+        if (readableState.contains(variableName)) {
+            return readableState.getInterval(variableName);
         } else {
-          return Interval.BOOLEAN_INTERVAL;
+            return new CreatorIntegerInterval().factoryMethod(null).UNBOUND();
         }
-
-      case GREATER_EQUAL: // a>=b == a+1>b, works only for integers
-        return getLogicInterval(
-            BinaryOperator.GREATER_THAN, interval1.plus(Interval.ONE), interval2);
-
-      case LESS_THAN: // a<b == b>a
-        return getLogicInterval(BinaryOperator.GREATER_THAN, interval2, interval1);
-
-      case LESS_EQUAL: // a<=b == b+1>a, works only for integers
-        return getLogicInterval(
-            BinaryOperator.GREATER_THAN, interval2.plus(Interval.ONE), interval1);
-
-      default:
-        throw new AssertionError("unknown binary operator: " + operator);
-    }
-  }
-
-  private static Interval getArithmeticInterval(
-      BinaryOperator operator, Interval interval1, Interval interval2) {
-    switch (operator) {
-      case PLUS:
-        return interval1.plus(interval2);
-      case MINUS:
-        return interval1.minus(interval2);
-      case MULTIPLY:
-        return interval1.times(interval2);
-      case DIVIDE:
-        return interval1.divide(interval2);
-      case SHIFT_LEFT:
-        return interval1.shiftLeft(interval2);
-      case SHIFT_RIGHT:
-        return interval1.shiftRight(interval2);
-      case MODULO:
-        return interval1.modulo(interval2);
-      case BINARY_AND:
-      case BINARY_OR:
-      case BINARY_XOR:
-        return Interval.UNBOUND;
-      default:
-        throw new AssertionError("unknown binary operator: " + operator);
-    }
-  }
-
-  @Override
-  public Interval visit(CCastExpression cast) throws UnrecognizedCCodeException {
-    return cast.getOperand().accept(this);
-  }
-
-  @Override
-  public Interval visit(CFunctionCallExpression functionCall) {
-    return Interval.UNBOUND;
-  }
-
-  @Override
-  public Interval visit(CCharLiteralExpression charLiteral) {
-    return new Interval((long) charLiteral.getCharacter());
-  }
-
-  @Override
-  public Interval visit(CImaginaryLiteralExpression exp) throws UnrecognizedCCodeException {
-    return exp.getValue().accept(this);
-  }
-
-  @Override
-  public Interval visit(CIntegerLiteralExpression integerLiteral) {
-    return new Interval(integerLiteral.asLong());
-  }
-
-  @Override
-  public Interval visit(CIdExpression identifier) {
-    if (identifier.getDeclaration() instanceof CEnumerator) {
-      return new Interval(((CEnumerator) identifier.getDeclaration()).getValue());
     }
 
-    final String variableName = identifier.getDeclaration().getQualifiedName();
-    if (readableState.contains(variableName)) {
-      return readableState.getInterval(variableName);
-    } else {
-      return Interval.UNBOUND;
+    @Override
+    public NumberInterface visit(CUnaryExpression unaryExpression) throws UnrecognizedCCodeException {
+        NumberInterface interval = unaryExpression.getOperand().accept(this);
+        switch (unaryExpression.getOperator()) {
+        case MINUS:
+            return interval.negate();
+        case AMPER:
+        case TILDE:
+            return new CreatorIntegerInterval().factoryMethod(null).UNBOUND(); // valid
+                                                                               // expression,
+                                                                               // but
+                                                                               // it's
+                                                                               // a
+                                                                               // pointer
+                                                                               // value
+        default:
+            throw new UnrecognizedCCodeException("unknown unary operator", cfaEdge, unaryExpression);
+        }
     }
-  }
-
-  @Override
-  public Interval visit(CUnaryExpression unaryExpression) throws UnrecognizedCCodeException {
-    Interval interval = unaryExpression.getOperand().accept(this);
-    switch (unaryExpression.getOperator()) {
-      case MINUS:
-        return interval.negate();
-
-      case AMPER:
-      case TILDE:
-        return Interval.UNBOUND; // valid expression, but it's a pointer value
-
-      default:
-        throw new UnrecognizedCCodeException("unknown unary operator", cfaEdge, unaryExpression);
-    }
-  }
 }
