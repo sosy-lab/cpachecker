91e7acb9abbe0bcd5ca368b56619bc6b917eb2ba
andrianov <andrianov@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2017-09-08 13:13:01 +0000

    Add a TODO to a new option
    The idea was to have different lists of algorithm for pre-computation
    and for normal sequential composition
    
    
    
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@25799 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/core/algorithm/RestartAlgorithm.java
+++ src/org/sosy_lab/cpachecker/core/algorithm/RestartAlgorithm.java
@@ -186,6 +186,12 @@ public class RestartAlgorithm implements Algorithm, StatisticsProvider, ReachedS
   /* The option is useful for some preanalysis,
    * for instance, the first analysis is fast and provides some hints to the next ones
    * Is used, for example, in CPALockator
+   *
+   * TODO It might be better to have two lists of algorithms given to the RestartAlgorithm.
+   * One list for analysis with no result expected except information about the program
+   * and a second list for the real analyses to be restarted if necessary.
+   * This would allow to combine the pre-computation of information and
+   * the normal sequential composition of algorithms in a more flexible way.
    */
   @Option(
     secure = true,
