954d9356249d6c9032b6f480e6a0dc9e0eca72c1
Karlheinz Friedberger <kfriedberger@gmail.com>
2017-10-04 09:24:54 +0000

    BAM: move method into superclass.
    These methods are also usefull in COW-refinement.
    
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@26068 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/cpa/bam/ARGInPlaceSubtreeRemover.java
+++ src/org/sosy_lab/cpachecker/cpa/bam/ARGInPlaceSubtreeRemover.java
@@ -27,15 +27,12 @@ import static org.sosy_lab.cpachecker.util.AbstractStates.extractLocation;
 
 import com.google.common.base.Predicate;
 import com.google.common.base.Predicates;
-import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Iterables;
 import com.google.common.collect.LinkedHashMultimap;
 import com.google.common.collect.Multimap;
 import java.util.ArrayDeque;
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Deque;
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -187,10 +184,6 @@ public class ARGInPlaceSubtreeRemover extends ARGSubtreeRemover {
     // --> if second state of reached-set is removed, update parent-precision
   }
 
-  private ARGState getReachedState(ARGState state) {
-    return (ARGState) data.getInnermostState(((BackwardARGState) state).getARGState());
-  }
-
   /** just remove a state and its subtree from the given reachedSet. */
   static void removeSubtree(ARGReachedSet reachedSet, ARGState argElement) {
     if (BAMTransferRelation.isHeadOfMainFunction(extractLocation(argElement))) {
@@ -284,83 +277,6 @@ public class ARGInPlaceSubtreeRemover extends ARGSubtreeRemover {
 
     return Pair.of(
         reducedPrecision, Predicates.instanceOf(reducedPrecision.getClass()));
-  }
-
-  /**
-   * Returns a mapping of wrapped non-reduced init-state towards non-wrapped reduced exit-state
-   * along the path.
-   */
-  private Map<BackwardARGState, ARGState> getBlockInitAndExitStates(ImmutableList<ARGState> path) {
-    final Map<BackwardARGState, ARGState> blockInitAndExitStates = new LinkedHashMap<>();
-    final Deque<BackwardARGState> openCallStates = new ArrayDeque<>();
-    for (final ARGState bamState : path) {
-      final ARGState state = ((BackwardARGState) bamState).getARGState();
-
-      // ASSUMPTION: there can be several block-exits at once per location,
-      // but only one block-entry per location.
-
-      // we use a loop here, because a return-node can be the exit of several blocks at once.
-      for (AbstractState exitState : data.getExpandedStatesList(state)) {
-        // we are leaving a block, remove the start-state from the stack.
-        BackwardARGState initialState = openCallStates.pop();
-        AbstractState reducedExitState = data.getReducedStateForExpandedState(exitState);
-        assert null
-            != data.getReachedSetForInitialState(initialState.getWrappedState(), reducedExitState);
-        blockInitAndExitStates.put(initialState, (ARGState) reducedExitState);
-      }
-
-      if (data.hasInitialState(state)) {
-        assert partitioning.isCallNode(extractLocation(state))
-            : "the mapping of initial state to reached-set should only exist for block-start-locations";
-        // we start a new sub-reached-set, add state as start-state of a (possibly) open block.
-        // if we are at lastState, we do not want to enter the block
-        openCallStates.push((BackwardARGState) bamState);
-      }
-    }
-
-    assert openCallStates.isEmpty()
-        : "empty callstack expected after traversing the path: " + openCallStates;
-    return blockInitAndExitStates;
-  }
-
-  /** returns only those states, where a block starts that is 'open' at the cutState.
-   * main-RS-root is only included, if it was reduced in TransferRelation. */
-  private List<BackwardARGState> getRelevantCallStates(List<ARGState> path, ARGState bamCutState) {
-    final Deque<BackwardARGState> openCallStates = new ArrayDeque<>();
-    for (final ARGState pathState : path) {
-
-      final BackwardARGState bamState = (BackwardARGState) pathState;
-      final ARGState state = bamState.getARGState();
-
-      // ASSUMPTION: there can be several block-exits at once per location, but only one block-entry per location.
-
-      // we use a loop here, because a return-node can be the exit of several blocks at once.
-      ARGState tmp = state;
-      while (data.hasExpandedState(tmp) && bamCutState != bamState) {
-        assert partitioning.isReturnNode(extractLocation(tmp)) : "the mapping of expanded to reduced state should only exist for block-return-locations";
-        // we are leaving a block, remove the start-state from the stack.
-        tmp = (ARGState) data.getReducedStateForExpandedState(tmp);
-        openCallStates.removeLast();
-        // INFO:
-        // if we leave several blocks at once, we leave the blocks in reverse order,
-        // because the call-state of the most outer block is checked first.
-        // We ignore this here, because we just need the 'number' of block-exits.
-      }
-
-      if (bamCutState == bamState) {
-        // do not enter or leave a block, when we found the cutState.
-        break;
-      }
-
-      if (data.hasInitialState(state)) {
-        assert partitioning.isCallNode(extractLocation(state)) : "the mapping of initial state to reached-set should only exist for block-start-locations";
-        // we start a new sub-reached-set, add state as start-state of a (possibly) open block.
-        // if we are at lastState, we do not want to enter the block
-        openCallStates.addLast(bamState);
-      }
-    }
-
-    return new ArrayList<>(openCallStates);
   }
 
   /**
--- src/org/sosy_lab/cpachecker/cpa/bam/ARGSubtreeRemover.java
+++ src/org/sosy_lab/cpachecker/cpa/bam/ARGSubtreeRemover.java
@@ -23,15 +23,25 @@
  */
 package org.sosy_lab.cpachecker.cpa.bam;
 
+import static org.sosy_lab.cpachecker.util.AbstractStates.extractLocation;
+
 import com.google.common.base.Predicate;
+import com.google.common.collect.ImmutableList;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 import org.sosy_lab.common.log.LogManager;
 import org.sosy_lab.cpachecker.cfa.blocks.BlockPartitioning;
+import org.sosy_lab.cpachecker.core.interfaces.AbstractState;
 import org.sosy_lab.cpachecker.core.interfaces.Precision;
 import org.sosy_lab.cpachecker.core.interfaces.Reducer;
 import org.sosy_lab.cpachecker.cpa.arg.ARGPath;
 import org.sosy_lab.cpachecker.cpa.arg.ARGReachedSet;
 import org.sosy_lab.cpachecker.cpa.arg.ARGState;
+import org.sosy_lab.cpachecker.cpa.bam.BAMSubgraphComputer.BackwardARGState;
 import org.sosy_lab.cpachecker.util.statistics.StatTimer;
 
 public abstract class ARGSubtreeRemover {
@@ -64,4 +74,86 @@ public abstract class ARGSubtreeRemover {
                      List<Predicate<? super Precision>> pNewPrecisionTypes)
       throws InterruptedException;
 
+  protected ARGState getReachedState(ARGState state) {
+    return (ARGState) data.getInnermostState(((BackwardARGState) state).getARGState());
+  }
+
+  /**
+   * Returns a mapping of wrapped non-reduced init-state towards non-wrapped reduced exit-state
+   * along the path.
+   */
+  protected Map<BackwardARGState, ARGState> getBlockInitAndExitStates(ImmutableList<ARGState> path) {
+    final Map<BackwardARGState, ARGState> blockInitAndExitStates = new LinkedHashMap<>();
+    final Deque<BackwardARGState> openCallStates = new ArrayDeque<>();
+    for (final ARGState bamState : path) {
+      final ARGState state = ((BackwardARGState) bamState).getARGState();
+
+      // ASSUMPTION: there can be several block-exits at once per location,
+      // but only one block-entry per location.
+
+      // we use a loop here, because a return-node can be the exit of several blocks at once.
+      for (AbstractState exitState : data.getExpandedStatesList(state)) {
+        // we are leaving a block, remove the start-state from the stack.
+        BackwardARGState initialState = openCallStates.pop();
+        AbstractState reducedExitState = data.getReducedStateForExpandedState(exitState);
+        assert null
+            != data.getReachedSetForInitialState(initialState.getWrappedState(), reducedExitState);
+        blockInitAndExitStates.put(initialState, (ARGState) reducedExitState);
+      }
+
+      if (data.hasInitialState(state)) {
+        assert partitioning.isCallNode(extractLocation(state))
+            : "the mapping of initial state to reached-set should only exist for block-start-locations";
+        // we start a new sub-reached-set, add state as start-state of a (possibly) open block.
+        // if we are at lastState, we do not want to enter the block
+        openCallStates.push((BackwardARGState) bamState);
+      }
+    }
+
+    assert openCallStates.isEmpty()
+        : "empty callstack expected after traversing the path: " + openCallStates;
+    return blockInitAndExitStates;
+  }
+
+  /** returns only those states, where a block starts that is 'open' at the cutState.
+   * main-RS-root is only included, if it was reduced in TransferRelation. */
+  protected List<BackwardARGState> getRelevantCallStates(List<ARGState> path, ARGState bamCutState) {
+    final Deque<BackwardARGState> openCallStates = new ArrayDeque<>();
+    for (final ARGState pathState : path) {
+
+      final BackwardARGState bamState = (BackwardARGState) pathState;
+      final ARGState state = bamState.getARGState();
+
+      // ASSUMPTION: there can be several block-exits at once per location, but only one block-entry per location.
+
+      // we use a loop here, because a return-node can be the exit of several blocks at once.
+      ARGState tmp = state;
+      while (data.hasExpandedState(tmp) && bamCutState != bamState) {
+        assert partitioning.isReturnNode(extractLocation(tmp))
+            : "the mapping of expanded to reduced state should only exist for block-return-locations";
+        // we are leaving a block, remove the start-state from the stack.
+        tmp = (ARGState) data.getReducedStateForExpandedState(tmp);
+        openCallStates.removeLast();
+        // INFO:
+        // if we leave several blocks at once, we leave the blocks in reverse order,
+        // because the call-state of the most outer block is checked first.
+        // We ignore this here, because we just need the 'number' of block-exits.
+      }
+
+      if (bamCutState == bamState) {
+        // do not enter or leave a block, when we found the cutState.
+        break;
+      }
+
+      if (data.hasInitialState(state)) {
+        assert partitioning.isCallNode(extractLocation(state))
+            : "the mapping of initial state to reached-set should only exist for block-start-locations";
+        // we start a new sub-reached-set, add state as start-state of a (possibly) open block.
+        // if we are at lastState, we do not want to enter the block
+        openCallStates.addLast(bamState);
+      }
+    }
+
+    return new ArrayList<>(openCallStates);
+  }
 }
