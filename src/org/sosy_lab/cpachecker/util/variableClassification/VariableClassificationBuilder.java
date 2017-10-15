d2a157e19195300a0ccbf8a6ecad637b0ce36189
Karlheinz Friedberger <kfriedberger@gmail.com>
2017-10-15 22:24:34 +0000

    VariableClassification: move class "Dependencies" into its own file.
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@26135 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/util/variableClassification/VariableClassificationBuilder.java
+++ src/org/sosy_lab/cpachecker/util/variableClassification/VariableClassificationBuilder.java
@@ -33,8 +33,6 @@ import com.google.common.collect.HashMultiset;
 import com.google.common.collect.ImmutableMultimap;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Iterables;
-import com.google.common.collect.Lists;
-import com.google.common.collect.Maps;
 import com.google.common.collect.Multimap;
 import com.google.common.collect.Multiset;
 import com.google.common.collect.Sets;
@@ -47,9 +45,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Collection;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 import java.util.TreeSet;
 import java.util.logging.Level;
@@ -934,136 +930,6 @@ public class VariableClassificationBuilder {
     @Override
     public Set<String> visit(CAddressOfLabelExpression exp) {
       return null;
-    }
-  }
-
-
-  /** This class stores dependencies between variables.
-   * It sorts vars into partitions.
-   * Dependent vars are in the same partition. Partitions are independent. */
-  private static class Dependencies {
-
-    /** partitions, each of them contains vars */
-    private final List<Partition> partitions = Lists.newArrayList();
-
-    /** map to get partition of a var */
-    private final Map<String, Partition> varToPartition = Maps.newHashMap();
-
-    /** table to get a partition for a edge. */
-    private final Map<Pair<CFAEdge, Integer>, Partition> edgeToPartition = Maps.newHashMap();
-
-    /** This function returns a partition containing all vars,
-     * that are dependent with the given variable. */
-    public Partition getPartitionForVar(String var) {
-      return varToPartition.get(var);
-    }
-
-    /** This function creates a dependency between function1::var1 and function2::var2. */
-    public void add(String var1, String var2) {
-
-      // if both vars exists in some dependencies,
-      // either ignore them or merge their partitions
-      Partition partition1 = varToPartition.get(var1);
-      Partition partition2 = varToPartition.get(var2);
-      if (partition1 != null && partition2 != null) {
-
-        // swap partitions, we create partitions in the order they are used
-        if (partitions.lastIndexOf(partition1) > partitions.lastIndexOf(partition2)) {
-          Partition tmp = partition2;
-          partition2 = partition1;
-          partition1 = tmp;
-        }
-
-        if (!partition1.equals(partition2)) {
-          partition1.merge(partition2);
-          partitions.remove(partition2);
-        }
-
-        // if only left side of dependency exists, add right side into same partition
-      } else if (partition1 != null) {
-        partition1.add(var2);
-
-        // if only right side of dependency exists, add left side into same partition
-      } else if (partition2 != null) {
-        partition2.add(var1);
-
-        // if none side is in any existing partition, create new partition
-      } else {
-        Partition partition = new Partition(varToPartition, edgeToPartition);
-        partition.add(var1);
-        partition.add(var2);
-        partitions.add(partition);
-      }
-    }
-
-    /** This function adds a group of vars to exactly one partition.
-     * The values are stored in the partition.
-     * The partition is "connected" with the expression.
-     *
-     * @param vars group of variables tobe added
-     * @param values numbers, with are used in an expression together with the variables
-     * @param edge where is the expression
-     * @param index if an edge has several expressions, this index is the position ofthe expression
-     *  */
-    public void addAll(Collection<String> vars, Set<BigInteger> values,
-        CFAEdge edge, int index) {
-      if (vars == null || vars.isEmpty()) { return; }
-
-      Iterator<String> iter = vars.iterator();
-
-      // we use same varName for all other vars --> dependency
-      String var = iter.next();
-
-      // first add one single var
-      addVar(var);
-
-      // then add all other vars, they are dependent from the first var
-      while (iter.hasNext()) {
-        add(var, iter.next());
-      }
-
-      Partition partition = getPartitionForVar(var);
-      partition.addValues(values);
-      partition.addEdge(edge, index);
-    }
-
-    /** This function adds one single variable to the partitions.
-     * This is the only method to create a partition with only one element. */
-    public void addVar(String var) {
-
-      // if var exists, we can ignore it, otherwise create new partition for var
-      if (!varToPartition.containsKey(var)) {
-        Partition partition = new Partition(varToPartition, edgeToPartition);
-        partition.add(var);
-        partitions.add(partition);
-      }
-    }
-
-    /** This function adds all depending vars to the set, if necessary.
-     * If A depends on B and A is part of the set, B is added to the set, and vice versa.
-    * Example: If A is not boolean, B is not boolean. */
-    public void solve(final Set<String> vars) {
-      for (Partition partition : partitions) {
-
-        // is at least one var from the partition part of vars
-        if (!Sets.intersection(partition.getVars(), vars).isEmpty()) {
-          // add all dependend vars to vars
-          vars.addAll(partition.getVars());
-        }
-      }
-    }
-
-    @Override
-    public String toString() {
-      StringBuilder str = new StringBuilder("[");
-      Joiner.on(",\n").appendTo(str, partitions);
-      str.append("]\n\n");
-
-      //      for (Pair<CFAEdge, Integer> edge : edgeToPartition.keySet()) {
-      //        str.append(edge.getFirst().getRawStatement() + " :: "
-      //            + edge.getSecond() + " --> " + edgeToPartition.get(edge) + "\n");
-      //      }
-      return str.toString();
     }
   }
 
