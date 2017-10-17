23bcb3b4b7aad72d8929400e4cc85ce114d6b796
spiessl <spiessl@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2017-10-17 23:33:17 +0000

    Add overflow assumptions for multiplication
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@26170 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/util/ArithmeticOverflowAssumptionBuilder.java
+++ src/org/sosy_lab/cpachecker/util/ArithmeticOverflowAssumptionBuilder.java
@@ -240,6 +240,11 @@ public final class ArithmeticOverflowAssumptionBuilder implements
         if (upperBounds.get(typ) != null) {
           result.add(getUpperAssumption(op1, op2, binop, upperBounds.get(typ)));
         }
+      } else if (binop.equals(BinaryOperator.MULTIPLY)) {
+        if (lowerBounds.get(typ) != null && upperBounds.get(typ) != null) {
+          addMultiplicationAssumptions(op1, op2, lowerBounds.get(typ), upperBounds.get(typ),
+              result);
+        }
       }
     } else if (exp instanceof CUnaryExpression) {
       if (lowerBounds.get(typ) != null) {
@@ -274,9 +279,12 @@ public final class ArithmeticOverflowAssumptionBuilder implements
    * This helper method generates assumptions for checking overflows
    * in signed integer additions/subtractions. Since the assumptions
    * are {@link CExpression}s as well, they are structured in such a
-   * way that they do not contain overflows themselves (this is of
+   * way that they do not suffer* from overflows themselves (this is of
    * particular importance e.g. if bit vector theory is used for
    * representation!)
+   * *The assumptions contain overflows because the second part
+   * is always evaluated, but their resulting value will then
+   * not depend on the outcome of that part of the formula!
    *
    * For addition (operator = BinaryOperator.PLUS) these assumptions
    * are lower and upper limits:
@@ -337,6 +345,77 @@ public final class ArithmeticOverflowAssumptionBuilder implements
         cBinaryExpressionBuilder.buildBinaryExpression(term1, term3, BinaryOperator.BINARY_OR);
 
     return assumption;
+  }
+
+  /**
+   * This helper method generates assumptions for checking overflows
+   * in signed integer multiplications. Since the assumptions
+   * are {@link CExpression}s as well, they are structured in such a
+   * way that they do not suffer* from overflows themselves (this is of
+   * particular importance e.g. if bit vector theory is used for
+   * representation!)
+   * *The assumptions contain overflows because the second part
+   * is always evaluated, but their resulting value will then
+   * not depend on the outcome of that part of the formula!
+   *
+   * The necessary assumptions for multiplication to be free from
+   * overflows look as follows:
+   * (operand2 <= 0) | (operand1 <= pUpperLimit / operand2)
+   * (operand2 <= 0) | (operand1 >= pLowerLimit / operand2)
+   * (operand1 <= 0) | (operand2 >= pLowerLimit / operand1)
+   * (operand1 >= 0) | (operand2 >= pUpperLimit / operand1)
+   *
+   * @param operand1 first operand in the C Expression for which the
+   *        assumption should be generated
+   * @param operand2 second operand in the C Expression for which the
+   *        assumption should be generated
+   *
+   * @param pLowerLimit the {@link CLiteralExpression} representing the
+   *        overflow bound for the type of the expression
+   * @param pUpperLimit the {@link CLiteralExpression} representing the
+   *        overflow bound for the type of the expression
+   * @param result the set to which the generated assumptions are added
+   */
+  private void addMultiplicationAssumptions(CExpression operand1, CExpression operand2,
+      CLiteralExpression pLowerLimit, CLiteralExpression pUpperLimit, Set<CExpression> result)
+      throws UnrecognizedCCodeException {
+
+    for (boolean operand1isFirstOperand : new boolean[] { false, true }) {
+      CExpression firstOperand = operand1isFirstOperand ? operand1 : operand2;
+      CExpression secondOperand = operand1isFirstOperand ? operand2 : operand1;
+      for (boolean usesUpperLimit : new boolean[] { false, true }) {
+        CLiteralExpression limit = usesUpperLimit ? pUpperLimit : pLowerLimit;
+
+        // We construct assumption by writing each of the 4 possible assumptions as:
+        // term1 | term3
+
+        // where term1 is structured this way:
+        // firstOperand term1Operator 0
+        BinaryOperator term1Operator = usesUpperLimit && operand1isFirstOperand
+            ? BinaryOperator.GREATER_EQUAL : BinaryOperator.LESS_EQUAL;
+        CExpression term1 = cBinaryExpressionBuilder.buildBinaryExpression(firstOperand,
+            CIntegerLiteralExpression.ZERO, term1Operator);
+
+        // and term2 is structured this way:
+        // limit BinaryOperator.DIVIDE firstOperand
+        CExpression term2 = cBinaryExpressionBuilder.buildBinaryExpression(limit, firstOperand,
+            BinaryOperator.DIVIDE);
+
+        // and term3 is structured this way:
+        // secondOperand term3Operator term2
+        BinaryOperator term3Operator = usesUpperLimit && !operand1isFirstOperand
+            ? BinaryOperator.LESS_EQUAL : BinaryOperator.GREATER_EQUAL;
+        CExpression term3 =
+            cBinaryExpressionBuilder.buildBinaryExpression(secondOperand, term2, term3Operator);
+
+        // the final assumption will look like this:
+        // (firstOperand term1Operator 0) |
+        // ( secondOperand term3Operator (limit BinaryOperator.DIVIDE firstOperand) )
+        CExpression assumption =
+            cBinaryExpressionBuilder.buildBinaryExpression(term1, term3, BinaryOperator.BINARY_OR);
+        result.add(assumption);
+      }
+    }
   }
 
   private class AssumptionsFinder
