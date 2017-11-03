sosy-lab/cpachecker

commit b29a0bd7e2bfd64060597d510afcb60cfc0d9304

Replace Stack with ArrayDeque in cwriter code
git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@26386 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

src/org/sosy_lab/cpachecker/util/cwriter/FunctionBody.java
@@ -23,9 +23,9 @@
  */
 package org.sosy_lab.cpachecker.util.cwriter;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.Iterator;
-import java.util.Stack;
-
 import org.sosy_lab.cpachecker.cfa.model.c.CAssumeEdge;
 
 /**
@@ -34,7 +34,7 @@ import org.sosy_lab.cpachecker.cfa.model.c.CAssumeEdge;
  */
 class FunctionBody implements Iterable<BasicBlock> {
 
-  private final Stack<BasicBlock> stack = new Stack<>();
+  private final Deque<BasicBlock> stack = new ArrayDeque<>();
 
   public FunctionBody(int pElementId, String pFunctionName) {
     stack.push(new BasicBlock(pElementId, pFunctionName));
@@ -75,6 +75,6 @@ class FunctionBody implements Iterable<BasicBlock> {
   public String toString() {
     // To write the C code, we need only the outermost block of the function.
     // It will print its nested blocks automatically as needed.
-    return stack.get(0).getCode();
+    return stack.getFirst().getCode();
   }
 }

src/org/sosy_lab/cpachecker/util/cwriter/PathTranslator.java
@@ -32,14 +32,15 @@ import com.google.common.base.Joiner;
 import com.google.common.collect.FluentIterable;
 import com.google.common.collect.Iterables;
 import com.google.common.collect.Lists;
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Deque;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.Stack;
 import org.sosy_lab.common.Appender;
 import org.sosy_lab.common.Appenders;
 import org.sosy_lab.cpachecker.cfa.ast.c.CExpression;
@@ -96,11 +97,11 @@ public abstract class PathTranslator {
 
   /**
    * Translate a single linear path to code.
+   *
    * @param pPath the path to translate
-   * @param callback A callback that receives each <code>ARGState</code>
-   * along with their edges and can then determine what code to generate
-   * from it. The default behavior of a <code>ProcessEdgeFunction</code>
-   *  is to call {@link #processEdge(ARGState, CFAEdge, Stack)}
+   * @param callback A callback that receives each <code>ARGState</code> along with their edges and
+   *     can then determine what code to generate from it. The default behavior of a <code>
+   *     ProcessEdgeFunction</code> is to call {@link #processEdge(ARGState, CFAEdge, Deque)}
    */
   protected void translateSinglePath0(ARGPath pPath, EdgeVisitor callback) {
     assert pPath.size() >= 1;
@@ -108,7 +109,7 @@ public abstract class PathTranslator {
     PathIterator pathIt = pPath.fullPathIterator();
     ARGState firstElement = pathIt.getAbstractState();
 
-    Stack<FunctionBody> functionStack = new Stack<>();
+    Deque<FunctionBody> functionStack = new ArrayDeque<>();
 
     // create the first function and put in into the stack
     startFunction(firstElement, functionStack, extractFunctionCallLocation(firstElement));
@@ -137,7 +138,7 @@ public abstract class PathTranslator {
 
     // create initial element
     {
-      Stack<FunctionBody> newStack = new Stack<>();
+      Deque<FunctionBody> newStack = new ArrayDeque<>();
 
       // create the first function and put in into newStack
       startFunction(firstElement, newStack, extractFunctionCallLocation(firstElement));
@@ -164,7 +165,8 @@ public abstract class PathTranslator {
    * @param functionStack the current callstack
    * @param predecessor the previous node
    */
-  protected String startFunction(ARGState firstFunctionElement, Stack<FunctionBody> functionStack, CFANode predecessor) {
+  protected String startFunction(
+      ARGState firstFunctionElement, Deque<FunctionBody> functionStack, CFANode predecessor) {
     // create the first stack element using the first element of the function
     CFunctionEntryNode functionStartNode = extractFunctionCallLocation(firstFunctionElement);
     String freshFunctionName = getFreshFunctionName(functionStartNode);
@@ -185,11 +187,12 @@ public abstract class PathTranslator {
 
   /**
    * Processes an edge of the CFA and will write code to the output function body.
+   *
    * @param childElement the state after the given edge
    * @param edge the edge to process
    * @param functionStack the current callstack
    */
-  void processEdge(ARGState childElement, CFAEdge edge, Stack<FunctionBody> functionStack) {
+  void processEdge(ARGState childElement, CFAEdge edge, Deque<FunctionBody> functionStack) {
     FunctionBody currentFunction = functionStack.peek();
 
     if (childElement.isTarget()) {
@@ -221,7 +224,7 @@ public abstract class PathTranslator {
       EdgeVisitor callback) {
     ARGState childElement = nextEdge.getChildState();
     CFAEdge edge = nextEdge.getEdge();
-    Stack<FunctionBody> functionStack = nextEdge.getStack();
+    Deque<FunctionBody> functionStack = nextEdge.getStack();
 
     // clone stack to have a different representation of the function calls and conditions
     // for every element
@@ -286,8 +289,7 @@ public abstract class PathTranslator {
   }
 
   private Collection<Edge> getRelevantChildrenOfState(
-      ARGState currentElement, Stack<FunctionBody> functionStack,
-      Set<ARGState> elementsOnPath) {
+      ARGState currentElement, Deque<FunctionBody> functionStack, Set<ARGState> elementsOnPath) {
     // find the next elements to add to the waitlist
 
     List<ARGState> relevantChildrenOfElement = from(currentElement.getChildren()).filter(in(elementsOnPath)).toList();
@@ -308,7 +310,7 @@ public abstract class PathTranslator {
       Collection<Edge> result = new ArrayList<>(2);
       int ind = 0;
       for (ARGState elem : relevantChildrenOfElement) {
-        Stack<FunctionBody> newStack = cloneStack(functionStack);
+        Deque<FunctionBody> newStack = cloneStack(functionStack);
         CFAEdge e = currentElement.getEdgeToChild(elem);
         FunctionBody currentFunction = newStack.peek();
         assert e instanceof CAssumeEdge;
@@ -466,9 +468,9 @@ public abstract class PathTranslator {
     return functionStartNode.getFunctionName() + "_" + mFunctionIndex++;
   }
 
-  private Stack<FunctionBody> cloneStack(Stack<FunctionBody> pStack) {
+  private Deque<FunctionBody> cloneStack(Deque<FunctionBody> pStack) {
 
-    Stack<FunctionBody> ret = new Stack<>();
+    Deque<FunctionBody> ret = new ArrayDeque<>();
     for (FunctionBody functionBody : pStack) {
       ret.push(new FunctionBody(functionBody));
     }

src/org/sosy_lab/cpachecker/util/cwriter/ConcreteProgramEdgeVisitor.java
@@ -26,6 +26,7 @@ package org.sosy_lab.cpachecker.util.cwriter;
 import static com.google.common.base.Predicates.notNull;
 import static com.google.common.collect.FluentIterable.from;
 
+import java.util.Deque;
 import org.sosy_lab.cpachecker.cfa.ast.AExpressionStatement;
 import org.sosy_lab.cpachecker.cfa.ast.c.CBinaryExpression;
 import org.sosy_lab.cpachecker.cfa.ast.c.CExpressionAssignmentStatement;
@@ -39,8 +40,6 @@ import org.sosy_lab.cpachecker.core.counterexample.CFAEdgeWithAssumptions;
 import org.sosy_lab.cpachecker.core.counterexample.CFAPathWithAssumptions;
 import org.sosy_lab.cpachecker.cpa.arg.ARGState;
 
-import java.util.Stack;
-
 
 public class ConcreteProgramEdgeVisitor extends DefaultEdgeVisitor {
 
@@ -52,15 +51,14 @@ public class ConcreteProgramEdgeVisitor extends DefaultEdgeVisitor {
   }
 
   @Override
-  public void visit(ARGState pChildElement, CFAEdge pEdge, Stack<FunctionBody> pFunctionStack) {
+  public void visit(ARGState pChildElement, CFAEdge pEdge, Deque<FunctionBody> pFunctionStack) {
     translator.processEdge(pChildElement, pEdge, pFunctionStack);
 
     createAssumedAssigmentString(pFunctionStack, pEdge);
   }
 
-
-  private void createAssumedAssigmentString(Stack<FunctionBody> functionStack,
-      CFAEdge currentCFAEdge) {
+  private void createAssumedAssigmentString(
+      Deque<FunctionBody> functionStack, CFAEdge currentCFAEdge) {
     CFAEdgeWithAssumptions e = findMatchingEdge(currentCFAEdge);
     if (e != null &&
         e.getCFAEdge() instanceof CStatementEdge) {

src/org/sosy_lab/cpachecker/util/cwriter/DefaultEdgeVisitor.java
@@ -23,8 +23,7 @@
  */
 package org.sosy_lab.cpachecker.util.cwriter;
 
-import java.util.Stack;
-
+import java.util.Deque;
 import org.sosy_lab.cpachecker.cfa.model.CFAEdge;
 import org.sosy_lab.cpachecker.cpa.arg.ARGState;
 
@@ -38,7 +37,7 @@ public class DefaultEdgeVisitor implements EdgeVisitor {
   }
 
   @Override
-  public void visit(ARGState childElement, CFAEdge edge, Stack<FunctionBody> functionStack) {
+  public void visit(ARGState childElement, CFAEdge edge, Deque<FunctionBody> functionStack) {
     translator.processEdge(childElement, edge, functionStack);
   }
 

src/org/sosy_lab/cpachecker/util/cwriter/Edge.java
@@ -23,20 +23,19 @@
  */
 package org.sosy_lab.cpachecker.util.cwriter;
 
+import java.util.Deque;
 import org.sosy_lab.cpachecker.cfa.model.CFAEdge;
 import org.sosy_lab.cpachecker.cpa.arg.ARGState;
-
-import java.util.Stack;
 
 class Edge implements Comparable<Edge> {
 
   private final ARGState parentState;
   private final ARGState childState;
   private final CFAEdge edge;
-  private final Stack<FunctionBody> stack;
+  private final Deque<FunctionBody> stack;
 
   public Edge(
-      ARGState pChildElement, ARGState pParentElement, CFAEdge pEdge, Stack<FunctionBody> pStack) {
+      ARGState pChildElement, ARGState pParentElement, CFAEdge pEdge, Deque<FunctionBody> pStack) {
     childState = pChildElement;
     parentState = pParentElement;
     edge = pEdge;
@@ -55,7 +54,7 @@ class Edge implements Comparable<Edge> {
     return edge;
   }
 
-  public Stack<FunctionBody> getStack() {
+  public Deque<FunctionBody> getStack() {
     return stack;
   }
 

src/org/sosy_lab/cpachecker/util/cwriter/EdgeVisitor.java
@@ -23,14 +23,12 @@
  */
 package org.sosy_lab.cpachecker.util.cwriter;
 
-import java.util.Stack;
-
+import java.util.Deque;
 import org.sosy_lab.cpachecker.cfa.model.CFAEdge;
 import org.sosy_lab.cpachecker.cpa.arg.ARGState;
 
 
 public interface EdgeVisitor {
 
-  public void visit(ARGState childElement, CFAEdge edge, Stack<FunctionBody> functionStack);
-
+  public void visit(ARGState childElement, CFAEdge edge, Deque<FunctionBody> functionStack);
 }

src/org/sosy_lab/cpachecker/util/cwriter/LoopCollectingEdgeVisitor.java
@@ -26,6 +26,15 @@ package org.sosy_lab.cpachecker.util.cwriter;
 import static com.google.common.collect.FluentIterable.from;
 import static org.sosy_lab.cpachecker.util.AbstractStates.extractLocation;
 
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.HashSet;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.Map;
+import java.util.Set;
 import org.sosy_lab.common.configuration.Configuration;
 import org.sosy_lab.common.configuration.InvalidConfigurationException;
 import org.sosy_lab.common.configuration.Option;
@@ -37,17 +46,6 @@ import org.sosy_lab.cpachecker.cpa.arg.ARGState;
 import org.sosy_lab.cpachecker.util.LoopStructure;
 import org.sosy_lab.cpachecker.util.LoopStructure.Loop;
 import org.sosy_lab.cpachecker.util.Pair;
-
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Deque;
-import java.util.HashSet;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.ListIterator;
-import java.util.Map;
-import java.util.Set;
-import java.util.Stack;
 
 
 @Options(prefix="cwriter.withLoops")
@@ -89,7 +87,7 @@ public class LoopCollectingEdgeVisitor implements EdgeVisitor {
   }
 
   @Override
-  public void visit(ARGState childElement, CFAEdge edge, Stack<FunctionBody> functionStack) {
+  public void visit(ARGState childElement, CFAEdge edge, Deque<FunctionBody> functionStack) {
     cfaPath.add(Pair.of(edge, childElement));
   }
 

src/org/sosy_lab/cpachecker/util/cwriter/PathToCWithLoopsTranslator.java
@@ -34,7 +34,15 @@ import static org.sosy_lab.cpachecker.util.cwriter.LoopCollectingEdgeVisitor.get
 import com.google.common.base.Joiner;
 import com.google.common.collect.FluentIterable;
 import com.google.common.collect.ImmutableSet;
-
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.regex.Pattern;
 import org.sosy_lab.common.Appender;
 import org.sosy_lab.common.configuration.Configuration;
 import org.sosy_lab.common.configuration.InvalidConfigurationException;
@@ -56,17 +64,6 @@ import org.sosy_lab.cpachecker.util.CFAUtils;
 import org.sosy_lab.cpachecker.util.LoopStructure;
 import org.sosy_lab.cpachecker.util.LoopStructure.Loop;
 import org.sosy_lab.cpachecker.util.Pair;
-
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Deque;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.Stack;
-import java.util.regex.Pattern;
 
 /**
  * This class translates a given ARGpath into c-code. The created code consists
@@ -162,7 +159,10 @@ public class PathToCWithLoopsTranslator extends PathTranslator {
   }
 
   @Override
-  protected String startFunction(final ARGState firstFunctionElement, Stack<FunctionBody> functionStack, final CFANode predecessor) {
+  protected String startFunction(
+      final ARGState firstFunctionElement,
+      Deque<FunctionBody> functionStack,
+      final CFANode predecessor) {
     // create the first stack element using the first element of the function
     CFunctionEntryNode functionStartNode = extractFunctionCallLocation(firstFunctionElement);
     String functionName = functionStartNode.getFunctionName();
@@ -203,7 +203,8 @@ public class PathToCWithLoopsTranslator extends PathTranslator {
   }
 
   @Override
-  protected void processEdge(ARGState childElement, CFAEdge edge, Stack<FunctionBody> functionStack) {
+  protected void processEdge(
+      ARGState childElement, CFAEdge edge, Deque<FunctionBody> functionStack) {
     // we don't need to handle this edge again
     if (handledFunctions.contains(callStack.peek()) && edge instanceof CFunctionReturnEdge) {
       callStack.pop();
@@ -223,10 +224,9 @@ public class PathToCWithLoopsTranslator extends PathTranslator {
     }
   }
 
-  /**
-   * Processes an edge of the CFA and will write code to the output function body.
-   */
-  private final void processEdge0(ARGState childElement, CFAEdge edge, Stack<FunctionBody> functionStack) {
+  /** Processes an edge of the CFA and will write code to the output function body. */
+  private final void processEdge0(
+      ARGState childElement, CFAEdge edge, Deque<FunctionBody> functionStack) {
     FunctionBody currentFunction = functionStack.peek();
 
     if (childElement.isTarget()) {

commit daee6cb8851e724ed04973b8829fe7ca3e4b9412

Fix a regression in cwriter package introduced in r26386.
Stack and ArrayDeque have different iteration order.

git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@26415 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

src/org/sosy_lab/cpachecker/util/cwriter/FunctionBody.java
@@ -29,8 +29,8 @@ import java.util.Iterator;
 import org.sosy_lab.cpachecker.cfa.model.c.CAssumeEdge;
 
 /**
- * A function is basically a stack of blocks, where the first element is the
- * outermost block of the function and the last element is the current block.
+ * A function is basically a stack of blocks, where the first element is the outermost block of the
+ * function and the last element is the current block.
  */
 class FunctionBody implements Iterable<BasicBlock> {
 
@@ -46,16 +46,16 @@ class FunctionBody implements Iterable<BasicBlock> {
 
   public void enterBlock(int pElementId, CAssumeEdge pEdge, String pConditionString) {
     BasicBlock block = new BasicBlock(pElementId, pEdge, pConditionString);
-    stack.peek().write(block); // register the inner block in its outer block
-    stack.push(block);
+    stack.getLast().write(block); // register the inner block in its outer block
+    stack.addLast(block);
   }
 
   public void leaveBlock() {
-    stack.pop();
+    stack.removeLast();
   }
 
   public BasicBlock getCurrentBlock() {
-    return stack.peek();
+    return stack.getLast();
   }
 
   public int size() {
@@ -68,7 +68,7 @@ class FunctionBody implements Iterable<BasicBlock> {
   }
 
   public void write(String s) {
-    stack.peek().write(s);
+    stack.getLast().write(s);
   }
 
   @Override

src/org/sosy_lab/cpachecker/util/cwriter/PathTranslator.java
@@ -61,7 +61,6 @@ import org.sosy_lab.cpachecker.cpa.arg.ARGPath;
 import org.sosy_lab.cpachecker.cpa.arg.ARGPath.PathIterator;
 import org.sosy_lab.cpachecker.cpa.arg.ARGState;
 
-
 public abstract class PathTranslator {
 
   protected final static CFunctionEntryNode extractFunctionCallLocation(ARGState state) {
@@ -472,7 +471,7 @@ public abstract class PathTranslator {
 
     Deque<FunctionBody> ret = new ArrayDeque<>();
     for (FunctionBody functionBody : pStack) {
-      ret.push(new FunctionBody(functionBody));
+      ret.add(new FunctionBody(functionBody));
     }
     return ret;
   }

src/org/sosy_lab/cpachecker/util/cwriter/FunctionBodyTest.java
@@ -1,0 +1,65 @@
+/*
+ *  CPAchecker is a tool for configurable software verification.
+ *  This file is part of CPAchecker.
+ *
+ *  Copyright (C) 2007-2017  Dirk Beyer
+ *  All rights reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *
+ *  CPAchecker web page:
+ *    http://cpachecker.sosy-lab.org
+ */
+package org.sosy_lab.cpachecker.util.cwriter;
+
+import static com.google.common.collect.FluentIterable.from;
+import static com.google.common.truth.Truth.assertThat;
+
+import java.math.BigInteger;
+import org.junit.Test;
+import org.sosy_lab.cpachecker.cfa.ast.FileLocation;
+import org.sosy_lab.cpachecker.cfa.ast.c.CIntegerLiteralExpression;
+import org.sosy_lab.cpachecker.cfa.model.CFANode;
+import org.sosy_lab.cpachecker.cfa.model.c.CAssumeEdge;
+import org.sosy_lab.cpachecker.cfa.types.c.CNumericTypes;
+
+public class FunctionBodyTest {
+
+  private static CAssumeEdge createAssumeEdge() {
+    return new CAssumeEdge(
+        "",
+        FileLocation.DUMMY,
+        new CFANode("dummy"),
+        new CFANode("dummy"),
+        new CIntegerLiteralExpression(FileLocation.DUMMY, CNumericTypes.INT, BigInteger.ZERO),
+        true);
+  }
+
+  @Test
+  public void test_enter_leave_iterationOrder() {
+    FunctionBody body = new FunctionBody(0, "dummy");
+    body.enterBlock(1, createAssumeEdge(), "if (0)");
+    body.enterBlock(2, createAssumeEdge(), "if (0)");
+    assertThat(body.getCurrentBlock().getStateId()).isEqualTo(2);
+    assertThat(from(body).transform(BasicBlock::getStateId)).containsExactly(0, 1, 2).inOrder();
+    body.leaveBlock();
+    body.enterBlock(3, createAssumeEdge(), "if (0");
+    assertThat(body.getCurrentBlock().getStateId()).isEqualTo(3);
+    assertThat(from(body).transform(BasicBlock::getStateId)).containsExactly(0, 1, 3).inOrder();
+    body.leaveBlock();
+    body.leaveBlock();
+    body.leaveBlock();
+    assertThat(body).isEmpty();
+  }
+}