61413ee1aad778fb29d4f4ca17922bb90d06c21f
lemberge <lemberge@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2017-11-22 19:35:58 +0000

    All classes in CPAchecker use RandomProvider instead of creating own random objects.
    Allows control over the random seed used at all locations.
    
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@26679 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/pcc/strategy/partitioning/RandomBalancedGraphPartitioner.java
+++ src/org/sosy_lab/cpachecker/pcc/strategy/partitioning/RandomBalancedGraphPartitioner.java
@@ -28,9 +28,9 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Random;
 import java.util.Set;
-
 import org.sosy_lab.cpachecker.core.interfaces.pcc.BalancedGraphPartitioner;
 import org.sosy_lab.cpachecker.pcc.strategy.partialcertificate.PartialReachedSetDirectedGraph;
+import org.sosy_lab.cpachecker.util.RandomProvider;
 
 
 public class RandomBalancedGraphPartitioner implements BalancedGraphPartitioner{
@@ -45,7 +45,7 @@ public class RandomBalancedGraphPartitioner implements BalancedGraphPartitioner{
       partitioning.add(new HashSet<Integer>());
     }
 
-    Random randomGen = new Random();
+    Random randomGen = RandomProvider.get();
 
     for (int i = 0; i < pGraph.getNumNodes(); i++) {
       partitioning.get(randomGen.nextInt(pNumPartitions)).add(i);
