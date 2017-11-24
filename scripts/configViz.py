d4205e6117dfa7095a6b087cafd69f65e5ef3bfe
spiessl <spiessl@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2017-11-24 11:36:25 +0000

    Add first version of config visualization script
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@26723 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

---
+++ scripts/configViz.py
@@ -0,0 +1,176 @@
+#!/usr/bin/env python3
+
+"""
+CPAchecker is a tool for configurable software verification.
+This file is part of CPAchecker.
+
+Copyright (C) 2007-2017  Dirk Beyer
+All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+
+CPAchecker web page:
+  http://cpachecker.sosy-lab.org
+"""
+
+import argparse
+import os
+import re
+import sys
+
+class Node:
+  nodes = dict()
+  def __init__(self,name):
+    if name in Node.nodes:
+      assert False
+      return
+    self.children = list()
+    self.parents = list()
+    self.name = name
+    self.path = os.path.split(name)[0]
+    self.recursionwarnings = 0
+    Node.nodes[name] = self
+    self.addChildren()
+  def contains(self, name, i = 0):
+    if i > 30:
+        raise Exception("RECURSION")
+    if self.name == name:
+      return True
+    for child in self.children:
+      try:
+        if child != self and child.contains(name, i + 1):
+          return True
+      except Exception as e:
+        if i==0:
+          if self.recursionwarnings == 0:
+            print("WARNING: Recursion in %s trying to find %s" % (self.name, name), file=sys.stderr)
+          self.recursionwarnings += 1
+        else:
+          raise e
+    return False
+  @staticmethod
+  def filter(filename):
+    if "README" in filename:
+      return False
+    return True
+  def addChildren(self):
+    if self.name ==".":
+      return
+    for line in open(self.name,"r"):
+      fname = None
+      if line[:8]=="#include":
+        fname = line.split()[1]
+      else:
+        m = re.search("^[a-zA-Z\.]*\.config(?:Files|)\s*=\s*(.*)\s*",line)
+        if m != None:
+          fname = m.group(1)
+        else:
+          m = re.search("^specification\s*=\s*(.*)\s*",line)
+          if m != None:
+            if m.group(1)=="":
+              fname = ""
+            else:
+              fname = m.group(1)
+      if fname == None:
+        continue
+      if fname.rstrip() != fname:
+        print("WARNING: trailing whitespace in config reference '%s' in %s" % (fname, self.name), file=sys.stderr)
+        fname = fname.rstrip()
+      fnames = fname.split(",")
+      for name in fnames:
+        name = name.strip().split("::")[0]
+        if name != "":
+          name = os.path.join(self.path,name)
+        name = os.path.normpath(name)
+        if self.filter(name):
+          if name not in Node.nodes:
+            if not os.path.exists(name):
+              print("WARNING: file %s referenced in %s does not exists" % (name, self.name), file=sys.stderr)
+              continue
+            else:
+              child = Node(name)
+          else:
+            child = Node.nodes[name]
+          self.children.append(child)
+          child.parents.append(self)
+
+def listfiles(path):
+  files =  [os.path.join(path,i) for i in os.listdir(path)]
+  f =  [os.path.normpath(i) for i in files if os.path.isfile(i)]
+  for j in [i for i in files if os.path.isdir(i)]:
+    f += listfiles(j)
+  return f
+
+
+
+def parseArgs():
+    parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter,description=
+"""Visualize config file dependencies. This will output a graphviz graph on stdout.
+The graphviz file contains tooltips displaying the content of each config file!
+(but xdot fails to show them, try converting it to svg and open it in a browser)
+Examples
+    # graph with all files in the configuration folder:
+    python3 scripts/configViz.py ./config > graph.dot && xdot graph.dot
+    # graph just showing everything on which svcomp18.properties depends:
+    python3 scripts/configViz.py ./config config/svcomp18.properties --ranksep 1 >graph.dot && xdot graph.dot""")
+    parser.add_argument("configdir", help = "directory where the configuration files reside")
+    parser.add_argument("rootfile", nargs = '?', help = "optional configuration file for which a graph should be generated. When specified, only files included (directly or indirectely) from this file are shown")
+    parser.add_argument("--filter" ,metavar="FILTER", help = "String to filter nodes. When specified, only matching nodes or nodes connected to matching nodes are shown")
+    parser.add_argument("--ranksep", help = "ranksep to use in the graphviz output file", default = 8)
+    return parser.parse_args()
+
+if __name__ == "__main__":
+  args = parseArgs()
+  trees  = list()
+  if len(sys.argv) <2:
+    print("specify path!")
+    exit()
+  files = listfiles(args.configdir)
+
+  for f in files:
+    if not Node.filter(f) or f in Node.nodes:
+      continue
+    tree = Node(f)
+    trees.append(tree)
+  for t in trees[::]:
+    for ot in trees:
+      if ot!=t and ot.contains(t.name):
+        trees.remove(t)
+        break
+
+  graph = sys.stdout #open("configViz.dot","w")
+  graph.write("digraph configs {\n")
+  graph.write('graph [ranksep="%s" rankdir="LR"];\n' % (args.ranksep))
+  graph.write('node [shape=box]\n')
+  for (k,v) in Node.nodes.items():
+    parentname = k
+    relparentname = os.path.relpath(parentname,args.configdir)
+    if args.filter != None:
+      if not args.filter in parentname and not any([args.filter in child.name for child in v.children]):
+        continue
+    if args.rootfile != None:
+      if not Node.nodes[args.rootfile].contains(parentname):
+        continue
+    if os.path.isfile(parentname):
+      parentcontent = re.sub("\n", "&#10;", open(parentname,"r").read())
+      parentcontent = re.sub('"','\\"',parentcontent)
+    if parentname.split(".")[-1] != "properties":
+      graph.write('"%s"[style=filled fillcolor="grey" color="dodgerblue"];\n' % relparentname)
+    else:
+      graph.write('"%s"[tooltip = "%s"];\n' % (relparentname, parentcontent))
+    for child in v.children:
+      childname = child.name
+      relchildname = os.path.relpath(childname,args.configdir)
+      graph.write('"%s" -> "%s";\n' % (relparentname, relchildname))
+  graph.write("}\n")
