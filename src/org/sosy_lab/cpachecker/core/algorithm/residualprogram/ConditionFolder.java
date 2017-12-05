7ba83b9e3cb01e957b31da5161f841861253d837
mcjakobs <mcjakobs@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2017-12-05 08:37:41 +0000

    fixed another problem with loop context folder
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@26799 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/core/algorithm/residualprogram/ConditionFolder.java
+++ src/org/sosy_lab/cpachecker/core/algorithm/residualprogram/ConditionFolder.java
@@ -532,17 +532,20 @@ public abstract class ConditionFolder {
       }
 
       if (pEdge instanceof FunctionReturnEdge && newLoopContext.contains("/")) {
-        newLoopContext =
-            newLoopContext.substring(newLoopContext.indexOf("/") + 1, newLoopContext.length());
+        newLoopContext = newLoopContext.substring(0, newLoopContext.lastIndexOf("/"));
       }
       if (pEdge instanceof FunctionCallEdge) {
         newLoopContext =
-            ((FunctionCallEdge) pEdge).getSuccessor().getFunctionName() + "/" + newLoopContext;
+            newLoopContext
+                + "/"
+                + "N"
+                + ((FunctionCallEdge) pEdge).getPredecessor().getNodeNumber()
+                + "N";
       }
 
       // enter loop or start next iteration
       if (cfa.getAllLoopHeads().get().contains(pEdge.getSuccessor())) {
-        newLoopContext += "|";
+        newLoopContext += "|L" + pEdge.getSuccessor().getNodeNumber() + "L";
       }
 
       if (pEdge instanceof AssumeEdge) {
