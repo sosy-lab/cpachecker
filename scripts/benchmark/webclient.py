49424a047ec35d5a82884f8918b25f5214c2d0ec
dangl <dangl@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2017-12-14 13:44:28 +0000

    Extend the handling of the -benchmark option in benchmark.py for benchmarks via the webclient to match the behavior of scripts/cpa.sh and the updated documentation in doc/Configuration.md from r26910 to disable java assertions
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@26911 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- scripts/benchmark/webclient.py
+++ scripts/benchmark/webclient.py
@@ -590,6 +590,7 @@ class WebInterface:
 
         if run.options:
             i = iter(run.options)
+            disableAssertions = False
             while True:
                 try:
                     option = next(i)
@@ -614,7 +615,7 @@ class WebInterface:
                         # ignore, is always set by this script
                         pass
                     elif option == "-disable-java-assertions":
-                        params.append(('disableJavaAssertions', 'true'))
+                        disableAssertions = True
                     elif option == "-java":
                         params.append(("option", "language=JAVA"))
                     elif option == "-32":
@@ -657,7 +658,7 @@ class WebInterface:
                         params.append(("option", "coverage.enabled=true"))
                         params.append(("option", "output.disable=true"))
                         params.append(("option", "statistics.memory=false"));
-
+                        disableAssertions = True
                     elif option[0] == '-':
                         if config:
                             raise WebClientError("More than one configuration: '{}' and '{}'".format(config, option[1:]))
@@ -669,6 +670,9 @@ class WebInterface:
 
                 except StopIteration:
                     break
+
+            if disableAssertions:
+              params.append(('disableJavaAssertions', 'true'))
 
         return (None, opened_files)
 
