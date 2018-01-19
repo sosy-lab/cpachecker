664baffc1ddefe0e4536f94054af995bce9bf56b
mcjakobs <mcjakobs@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2018-01-19 09:16:08 +0000

    mark configuration files in ConditionalVerifierAlgorithm secure and also write output files in statistics
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@27184 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/core/algorithm/residualprogram/ConditionalVerifierAlgorithm.java
+++ src/org/sosy_lab/cpachecker/core/algorithm/residualprogram/ConditionalVerifierAlgorithm.java
@@ -69,13 +69,14 @@ import org.sosy_lab.cpachecker.util.statistics.StatisticsWriter;
 public class ConditionalVerifierAlgorithm implements Algorithm, StatisticsProvider {
 
   @Option(
+    secure = true,
     description =
         "configuration for the verification of the residual program which is constructed from another verifier's condition"
   )
   @FileOption(FileOption.Type.REQUIRED_INPUT_FILE)
   private @Nullable Path verifierConfig;
 
-  @Option(description = "configuration of the residual program generator")
+  @Option(secure = true, description = "configuration of the residual program generator")
   @FileOption(FileOption.Type.REQUIRED_INPUT_FILE)
   private @Nullable Path generatorConfig;
 
@@ -232,6 +233,7 @@ public class ConditionalVerifierAlgorithm implements Algorithm, StatisticsProvid
         logger.log(Level.FINE, "Build configurable program analysis");
         ConfigurableProgramAnalysis cpa;
         cpa = coreComponents.createCPA(cfaResidProg, spec);
+        collectStatistics(cpa);
         shutdown.shutdownIfNecessary();
 
         logger.log(Level.FINE, "Get verification algorithm");
@@ -267,9 +269,9 @@ public class ConditionalVerifierAlgorithm implements Algorithm, StatisticsProvid
     }
   }
 
-  private void collectStatistics(final Algorithm pAlgorithm) {
-    if (pAlgorithm instanceof StatisticsProvider) {
-      ((StatisticsProvider) pAlgorithm).collectStatistics(stats.substats);
+  private void collectStatistics(final Object pStatisticsProviderCandidate) {
+    if (pStatisticsProviderCandidate instanceof StatisticsProvider) {
+      ((StatisticsProvider) pStatisticsProviderCandidate).collectStatistics(stats.substats);
     }
   }
 
@@ -298,6 +300,7 @@ public class ConditionalVerifierAlgorithm implements Algorithm, StatisticsProvid
 
       for (Statistics substat : substats) {
         substat.printStatistics(pOut, pResult, pReached);
+        substat.writeOutputFiles(pResult, pReached);
       }
     }
 
