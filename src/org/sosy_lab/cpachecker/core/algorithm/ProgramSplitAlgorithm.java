9bd775cdebc77d73633626c83d573283e0b42c6a
mcjakobs <mcjakobs@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2018-01-19 18:36:52 +0000

    fix configuration files test
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@27190 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/core/algorithm/ProgramSplitAlgorithm.java
+++ src/org/sosy_lab/cpachecker/core/algorithm/ProgramSplitAlgorithm.java
@@ -140,7 +140,7 @@ public class ProgramSplitAlgorithm implements Algorithm, StatisticsProvider, Sta
 
     // Algorithm is never meant to prove or falsify a property, only does splitting for later
     // (parallel) conditional model checking
-    return AlgorithmStatus.UNSOUND_AND_PRECISE.withPrecise(false);
+    return AlgorithmStatus.SOUND_AND_PRECISE.withPrecise(false);
   }
 
   private void extractSplitConditions(final ReachedSet pReached) throws InterruptedException {
