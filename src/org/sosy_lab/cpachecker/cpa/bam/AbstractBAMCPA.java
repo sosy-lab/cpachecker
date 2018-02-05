ec5dd49e187f4cf95c672e08a0eb718d95a7eea3
Karlheinz Friedberger <kfriedberger@gmail.com>
2018-02-05 08:39:03 +0000

    BAM refactoring: create a new reducer instance when possible.
    This change avoids synchronization for Timers,
    if TimerReducer is used concurrent context.
    
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@27221 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/cpa/bam/AbstractBAMCPA.java
+++ src/org/sosy_lab/cpachecker/cpa/bam/AbstractBAMCPA.java
@@ -51,6 +51,7 @@ import org.sosy_lab.cpachecker.core.interfaces.ConfigurableProgramAnalysis;
 import org.sosy_lab.cpachecker.core.interfaces.ConfigurableProgramAnalysisWithBAM;
 import org.sosy_lab.cpachecker.core.interfaces.Statistics;
 import org.sosy_lab.cpachecker.cpa.arg.ARGStatistics;
+import org.sosy_lab.cpachecker.cpa.bam.TimedReducer.ReducerStatistics;
 import org.sosy_lab.cpachecker.exceptions.CPAException;
 
 @Options(prefix = "cpa.bam")
@@ -105,11 +106,11 @@ public abstract class AbstractBAMCPA extends AbstractSingleWrapperCPA {
   private boolean useCopyOnWriteRefinement = false;
 
   final Timer blockPartitioningTimer = new Timer();
+  final ReducerStatistics reducerStatistics;
 
   protected final LogManager logger;
   protected final ShutdownNotifier shutdownNotifier;
   protected final BlockPartitioning blockPartitioning;
-  private final TimedReducer reducer;
   private final BAMCPAStatistics stats;
   private final BAMARGStatistics argStats;
   private final BAMReachedSetExporter exporter;
@@ -136,10 +137,14 @@ public abstract class AbstractBAMCPA extends AbstractSingleWrapperCPA {
     blockPartitioning = buildBlockPartitioning(pCfa, pConfig);
     blockPartitioningTimer.stop();
 
-    reducer = new TimedReducer(getWrappedCpa().getReducer());
     argStats = new BAMARGStatistics(pConfig, pLogger, this, pCpa, pSpecification, pCfa);
     exporter = new BAMReachedSetExporter(pConfig, pLogger, this);
     stats = new BAMCPAStatistics(this);
+
+    reducerStatistics = new TimedReducer.ReducerStatistics();
+
+    // create a reducer to throw exceptions directly, actually useless code
+    getWrappedCpa().getReducer();
   }
 
   private BlockPartitioning buildBlockPartitioning(CFA pCfa, Configuration pConfig)
@@ -175,7 +180,12 @@ public abstract class AbstractBAMCPA extends AbstractSingleWrapperCPA {
   }
 
   TimedReducer getReducer() {
-    return Preconditions.checkNotNull(reducer);
+    try {
+      return new TimedReducer(reducerStatistics, getWrappedCpa().getReducer());
+    } catch (InvalidConfigurationException e) {
+      // exception would already appear before, see constructor above
+      throw new AssertionError(e);
+    }
   }
 
   @Override
