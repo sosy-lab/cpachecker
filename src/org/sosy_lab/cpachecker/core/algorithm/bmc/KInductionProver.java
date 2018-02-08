sosy-lab/cpachecker

commit bd390ddd95223d614d687fe17eca8f48e5c6dd87

Switch to new JavaSMT function for obtaining a value assignment from a model directly as a boolean formula
git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@27257 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

src/org/sosy_lab/cpachecker/core/algorithm/bmc/KInductionProver.java
@@ -41,7 +41,6 @@ import com.google.common.collect.Iterables;
 import com.google.common.collect.LinkedHashMultimap;
 import com.google.common.collect.Maps;
 import com.google.common.collect.Multimap;
-import java.math.BigInteger;
 import java.util.ArrayDeque;
 import java.util.Arrays;
 import java.util.Collection;
@@ -53,14 +52,12 @@ import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import java.util.Optional;
 import java.util.OptionalInt;
 import java.util.Set;
 import java.util.function.Supplier;
 import java.util.logging.Level;
 import java.util.stream.Stream;
-import javax.annotation.Nullable;
 import org.sosy_lab.common.ShutdownNotifier;
 import org.sosy_lab.common.log.LogManager;
 import org.sosy_lab.cpachecker.cfa.CFA;
@@ -99,14 +96,8 @@ import org.sosy_lab.cpachecker.util.predicates.pathformula.PathFormulaManager;
 import org.sosy_lab.cpachecker.util.predicates.pathformula.SSAMap;
 import org.sosy_lab.cpachecker.util.predicates.smt.BooleanFormulaManagerView;
 import org.sosy_lab.cpachecker.util.predicates.smt.FormulaManagerView;
-import org.sosy_lab.java_smt.api.ArrayFormula;
-import org.sosy_lab.java_smt.api.BitvectorFormula;
-import org.sosy_lab.java_smt.api.BitvectorFormulaManager;
 import org.sosy_lab.java_smt.api.BooleanFormula;
 import org.sosy_lab.java_smt.api.Formula;
-import org.sosy_lab.java_smt.api.FormulaType;
-import org.sosy_lab.java_smt.api.FormulaType.ArrayFormulaType;
-import org.sosy_lab.java_smt.api.FormulaType.BitvectorType;
 import org.sosy_lab.java_smt.api.FunctionDeclaration;
 import org.sosy_lab.java_smt.api.Model.ValueAssignment;
 import org.sosy_lab.java_smt.api.QuantifiedFormulaManager.Quantifier;
@@ -744,14 +735,12 @@ class KInductionProver implements AutoCloseable {
                 && ssaMap.getIndex(actualName) == index.getAsInt()
                 && value instanceof Number
                 && !inputs.containsKey(actualName)) {
-              Formula formula = variableFormulas.get().get(fullName);
-              if (formula == null) {
-                formula = valueAssignment.getKey();
-              }
-              FormulaType<?> formulaType = fmgr.getFormulaType(formula);
+              BooleanFormula assignment =
+                  fmgr.uninstantiate(valueAssignment.getAssignmentAsFormula());
               modelBuilder.put(
                   actualName,
-                  new ModelValue(actualName, formulaType, (Number) valueAssignment.getValue()));
+                  new ModelValue(
+                      actualName, fmgr.dumpFormula(assignment).toString(), assignment::toString));
             }
           }
         }
@@ -764,15 +753,11 @@ class KInductionProver implements AutoCloseable {
             continue;
           }
 
-          Supplier<? extends Set<AbstractState>> endStates =
-              Suppliers.memoize(() -> BMCHelper.filterEndStates(pReached).toSet());
-
           for (ValueAssignment valueAssignment : pModelAssignments) {
             String fullName = valueAssignment.getName();
             Pair<String, OptionalInt> pair = FormulaManagerView.parseName(fullName);
             String actualName = pair.getSecond().isPresent() ? pair.getFirst() : fullName;
             OptionalInt index = pair.getSecond();
-            Object value = valueAssignment.getValue();
             boolean isUnconnected = false;
             if (index.isPresent()
                 && ssaMap.containsVariable(actualName)
@@ -780,80 +765,10 @@ class KInductionProver implements AutoCloseable {
               isUnconnected = !variableFormulas.get().containsKey(fullName);
             }
             if ((!index.isPresent()
-                    || (index.isPresent()
-                        && (isUnconnected || inputs.get(actualName).contains(index.getAsInt()))))
-                && value instanceof Number) {
-              Formula formula = variableFormulas.get().get(fullName);
-              if (formula == null) {
-                formula = valueAssignment.getKey();
-              }
-              FormulaType<?> formulaType = fmgr.getFormulaType(formula);
-              BooleanFormula assignment = bfmgr.makeTrue();
-              if (!valueAssignment.isFunction()) {
-                ModelValue modelValue =
-                    new ModelValue(actualName, formulaType, (Number) valueAssignment.getValue());
-                assignment = modelValue.toAssignment(fmgr);
-                if (index.isPresent()) {
-                  CType type = types.get(actualName);
-                  if (type == null && ssaMap.containsVariable(actualName)) {
-                    type = ssaMap.getType(actualName);
-                  }
-                  if (type == null) {
-                    for (PredicateAbstractState endState :
-                        AbstractStates.projectToType(
-                            endStates.get(), PredicateAbstractState.class)) {
-                      type = endState.getPathFormula().getSsa().getType(actualName);
-                      if (type != null) {
-                        break;
-                      }
-                    }
-                  }
-                  if (type != null) {
-                    SSAMap ssaForInstantiation =
-                        SSAMap.emptySSAMap()
-                            .builder()
-                            .setIndex(actualName, type, index.getAsInt())
-                            .build();
-                    assignment = fmgr.instantiate(assignment, ssaForInstantiation);
-                  }
-                }
-              } else {
-                for (PredicateAbstractState endState :
-                    AbstractStates.projectToType(endStates.get(), PredicateAbstractState.class)) {
-                  BooleanFormula pathFormula =
-                      bfmgr.and(
-                          endState.getAbstractionFormula().getBlockFormula().getFormula(),
-                          endState.getPathFormula().getFormula());
-                  VariableFinder variableFinder = new VariableFinder(fullName);
-                  fmgr.visitRecursively(pathFormula, variableFinder);
-                  if (variableFinder.variable != null) {
-                    formulaType = fmgr.getFormulaType(variableFinder.variable);
-                    if (formulaType instanceof ArrayFormulaType) {
-                      ArrayFormulaType<?, ?> arrayType = (ArrayFormulaType<?, ?>) formulaType;
-                      if (arrayType.getIndexType() instanceof BitvectorType
-                          && arrayType.getElementType() instanceof BitvectorType) {
-                        BitvectorFormulaManager bfvm = fmgr.getBitvectorFormulaManager();
-                        BitvectorType indexType = (BitvectorType) arrayType.getIndexType();
-                        BitvectorFormula arrayIndex =
-                            bfvm.makeBitvector(
-                                indexType.getSize(),
-                                (BigInteger) valueAssignment.getArgInterpretation(0));
-                        BitvectorType elementType = (BitvectorType) arrayType.getElementType();
-                        BitvectorFormula element =
-                            bfvm.makeBitvector(elementType.getSize(), (BigInteger) value);
-                        @SuppressWarnings("unchecked")
-                        ArrayFormula<BitvectorFormula, BitvectorFormula> arrayFormula =
-                            (ArrayFormula<BitvectorFormula, BitvectorFormula>)
-                                variableFinder.variable;
-                        BitvectorFormula select =
-                            fmgr.getArrayFormulaManager().select(arrayFormula, arrayIndex);
-                        assignment = bfvm.equal(select, element);
-                      }
-                    }
-                    break;
-                  }
-                }
-              }
+                || (index.isPresent()
+                    && (isUnconnected || inputs.get(actualName).contains(index.getAsInt()))))) {
+              BooleanFormula assignment =
+                  fmgr.uninstantiate(valueAssignment.getAssignmentAsFormula());
               input = bfmgr.and(input, assignment);
             }
           }
@@ -891,48 +806,6 @@ class KInductionProver implements AutoCloseable {
     }
 
     return stateViolationAssertionsBuilder.build();
-  }
-
-  private static class VariableFinder implements FormulaVisitor<TraversalProcess> {
-
-    private final String variableName;
-
-    private @Nullable Formula variable = null;
-
-    public VariableFinder(String pVariableName) {
-      variableName = Objects.requireNonNull(pVariableName);
-    }
-
-    @Override
-    public TraversalProcess visitQuantifier(
-        BooleanFormula pArg0, Quantifier pArg1, List<Formula> pArg2, BooleanFormula pArg3) {
-      return TraversalProcess.CONTINUE;
-    }
-
-    @Override
-    public TraversalProcess visitFunction(
-        Formula pArg0, List<Formula> pArg1, FunctionDeclaration<?> pArg2) {
-      return TraversalProcess.CONTINUE;
-    }
-
-    @Override
-    public TraversalProcess visitFreeVariable(Formula pArg0, String pArg1) {
-      if (pArg1.equals(variableName)) {
-        variable = pArg0;
-        return TraversalProcess.ABORT;
-      }
-      return TraversalProcess.CONTINUE;
-    }
-
-    @Override
-    public TraversalProcess visitConstant(Formula pArg0, Object pArg1) {
-      return TraversalProcess.CONTINUE;
-    }
-
-    @Override
-    public TraversalProcess visitBoundVariable(Formula pArg0, int pArg1) {
-      return TraversalProcess.CONTINUE;
-    }
   }
 
   private static class VariableMapper implements FormulaVisitor<TraversalProcess> {

src/org/sosy_lab/cpachecker/core/algorithm/bmc/PartialTransitionRelation.java
@@ -67,7 +67,6 @@ import org.sosy_lab.cpachecker.util.predicates.smt.FormulaManagerView;
 import org.sosy_lab.java_smt.api.BooleanFormula;
 import org.sosy_lab.java_smt.api.BooleanFormulaManager;
 import org.sosy_lab.java_smt.api.Formula;
-import org.sosy_lab.java_smt.api.FormulaType;
 import org.sosy_lab.java_smt.api.Model.ValueAssignment;
 
 public class PartialTransitionRelation implements Comparable<PartialTransitionRelation> {
@@ -427,17 +426,10 @@ public class PartialTransitionRelation implements Comparable<PartialTransitionRe
             && (actualName.equals(TotalTransitionRelation.getLocationVariableName())
                 || (variables.containsKey(actualName)
                     && !inputs.get(actualName).contains(index.getAsInt())))) {
-          Formula formula;
-          if (actualName.equals(TotalTransitionRelation.getLocationVariableName())) {
-            assert ((Number) value).intValue() == startLocation.getNodeNumber()
-                : "This is the wrong part of the transition to extract a CTI from this model.";
-            formula = valueAssignment.getKey();
-          } else {
-            formula = variables.get(actualName);
-          }
-          FormulaType<?> formulaType = fmgr.getFormulaType(formula);
+          BooleanFormula assignment = fmgr.uninstantiate(valueAssignment.getAssignmentAsFormula());
           ModelValue modelValue =
-              new ModelValue(actualName, formulaType, (Number) valueAssignment.getValue());
+              new ModelValue(
+                  actualName, fmgr.dumpFormula(assignment).toString(), assignment::toString);
           modelBuilder.put(actualName, modelValue);
         }
       }
@@ -469,30 +461,27 @@ public class PartialTransitionRelation implements Comparable<PartialTransitionRe
         Pair<String, OptionalInt> pair = FormulaManagerView.parseName(fullName);
         String actualName = pair.getFirst();
         OptionalInt index = pair.getSecond();
-        Object value = valueAssignment.getValue();
-        if (value instanceof Number) {
-          Formula formula = valueAssignment.getKey();
-          FormulaType<?> formulaType = pFmgr.getFormulaType(formula);
-          ModelValue modelValue =
-              new ModelValue(actualName, formulaType, (Number) valueAssignment.getValue());
+        BooleanFormula assignment = pFmgr.uninstantiate(valueAssignment.getAssignmentAsFormula());
+        ModelValue modelValue =
+            new ModelValue(
+                actualName, pFmgr.dumpFormula(assignment).toString(), assignment::toString);
 
-          // We consider as inputs
-          // a) those that have an SSA index and are contained in our list of input variables and
-          // b) those that have no SSA index and are not known as actual variables,
-          // such as __ADDRESS_OF:
-          if (index.isPresent() && inputs.get(actualName).contains(index.getAsInt())) {
-            SSAMap ssaForInstantiation =
-                SSAMap.emptySSAMap()
-                    .builder()
-                    .setIndex(actualName, types.get(actualName), index.getAsInt())
-                    .build();
-            inputAssignments =
-                bfmgr.and(
-                    inputAssignments,
-                    pFmgr.instantiate(modelValue.toAssignment(pFmgr), ssaForInstantiation));
-          } else if (!index.isPresent() && !pVariables.containsKey(actualName)) {
-            inputAssignments = bfmgr.and(inputAssignments, modelValue.toAssignment(pFmgr));
-          }
+        // We consider as inputs
+        // a) those that have an SSA index and are contained in our list of input variables and
+        // b) those that have no SSA index and are not known as actual variables,
+        // such as __ADDRESS_OF:
+        if (index.isPresent() && inputs.get(actualName).contains(index.getAsInt())) {
+          SSAMap ssaForInstantiation =
+              SSAMap.emptySSAMap()
+                  .builder()
+                  .setIndex(actualName, types.get(actualName), index.getAsInt())
+                  .build();
+          inputAssignments =
+              bfmgr.and(
+                  inputAssignments,
+                  pFmgr.instantiate(modelValue.toAssignment(pFmgr), ssaForInstantiation));
+        } else if (!index.isPresent() && !pVariables.containsKey(actualName)) {
+          inputAssignments = bfmgr.and(inputAssignments, modelValue.toAssignment(pFmgr));
         }
       }
     }

src/org/sosy_lab/cpachecker/core/algorithm/bmc/CounterexampleToInductivity.java
@@ -27,16 +27,12 @@ import com.google.common.collect.FluentIterable;
 import java.util.Collections;
 import java.util.Map;
 import java.util.Objects;
-import java.util.Optional;
-import java.util.Set;
-import java.util.stream.Collectors;
 import org.sosy_lab.common.collect.PathCopyingPersistentTreeMap;
 import org.sosy_lab.common.collect.PersistentSortedMap;
 import org.sosy_lab.cpachecker.cfa.model.CFANode;
 import org.sosy_lab.cpachecker.util.predicates.smt.FormulaManagerView;
 import org.sosy_lab.java_smt.api.BooleanFormula;
 import org.sosy_lab.java_smt.api.BooleanFormulaManager;
-import org.sosy_lab.java_smt.api.Formula;
 
 public class CounterexampleToInductivity {
 
@@ -116,15 +112,5 @@ public class CounterexampleToInductivity {
                   SingleLocationFormulaInvariant.makeLocationInvariant(
                       getLocation(), v.toAssignment(pFMGR), pFMGR));
             });
-  }
-
-  public Set<Formula> getVariables(FormulaManagerView pFMGR) {
-    return model
-        .values()
-        .stream()
-        .map(mv -> mv.getVariable(pFMGR))
-        .filter(Optional::isPresent)
-        .map(Optional::get)
-        .collect(Collectors.toSet());
   }
 }

src/org/sosy_lab/cpachecker/core/algorithm/bmc/ModelValue.java
@@ -23,54 +23,33 @@
  */
 package org.sosy_lab.cpachecker.core.algorithm.bmc;
 
-import java.math.BigDecimal;
-import java.math.BigInteger;
 import java.util.Objects;
-import java.util.Optional;
-import org.sosy_lab.common.rationals.Rational;
+import java.util.function.Supplier;
 import org.sosy_lab.cpachecker.util.predicates.smt.FormulaManagerView;
-import org.sosy_lab.cpachecker.util.predicates.smt.IntegerFormulaManagerView;
-import org.sosy_lab.java_smt.api.BitvectorFormula;
-import org.sosy_lab.java_smt.api.BitvectorFormulaManager;
 import org.sosy_lab.java_smt.api.BooleanFormula;
-import org.sosy_lab.java_smt.api.BooleanFormulaManager;
-import org.sosy_lab.java_smt.api.FloatingPointFormula;
-import org.sosy_lab.java_smt.api.FloatingPointFormulaManager;
-import org.sosy_lab.java_smt.api.Formula;
-import org.sosy_lab.java_smt.api.FormulaType;
-import org.sosy_lab.java_smt.api.FormulaType.BitvectorType;
-import org.sosy_lab.java_smt.api.FormulaType.FloatingPointType;
-import org.sosy_lab.java_smt.api.NumeralFormula.IntegerFormula;
 
 public class ModelValue {
 
   private final String variableName;
 
-  private final FormulaType<?> formulaType;
+  private final Supplier<String> textualRepresentation;
 
-  private final Number value;
+  private final String formula;
 
-  public ModelValue(String pVariableName, FormulaType<?> pFormulaType, Number pValue) {
-    variableName = pVariableName;
-    formulaType = pFormulaType;
-    value = pValue;
+  public ModelValue(
+      String pVariableName, String pFormula, Supplier<String> pTextualRepresentation) {
+    variableName = Objects.requireNonNull(pVariableName);
+    formula = Objects.requireNonNull(pFormula);
+    textualRepresentation = Objects.requireNonNull(pTextualRepresentation);
   }
 
   public String getVariableName() {
     return variableName;
   }
 
-  public FormulaType<?> getFormulaType() {
-    return formulaType;
-  }
-
-  public Object getValue() {
-    return value;
-  }
-
   @Override
   public String toString() {
-    return String.format("%s = <%s> %s", variableName, formulaType, value);
+    return textualRepresentation.get();
   }
 
   @Override
@@ -80,75 +59,17 @@ public class ModelValue {
     }
     if (pOther instanceof ModelValue) {
       ModelValue other = (ModelValue) pOther;
-      return variableName.equals(other.variableName)
-          && value.equals(other.value)
-          && formulaType.equals(other.formulaType);
+      return variableName.equals(other.variableName) && formula.equals(other.formula);
     }
     return false;
   }
 
   @Override
   public int hashCode() {
-    return Objects.hash(variableName, value, formulaType);
-  }
-
-  public Optional<Formula> getVariable(FormulaManagerView pFMGR) {
-    if (formulaType.isIntegerType()) {
-      IntegerFormulaManagerView ifm = pFMGR.getIntegerFormulaManager();
-      return Optional.of(ifm.makeVariable(variableName));
-    }
-    if (formulaType instanceof BitvectorType) {
-      BitvectorFormulaManager bvm = pFMGR.getBitvectorFormulaManager();
-      return Optional.of(bvm.makeVariable((BitvectorType) formulaType, variableName));
-    }
-    if (formulaType instanceof FloatingPointType) {
-      FloatingPointFormulaManager fpm = pFMGR.getFloatingPointFormulaManager();
-      return Optional.of(fpm.makeVariable(variableName, (FloatingPointType) formulaType));
-    }
-    return Optional.empty();
+    return Objects.hash(variableName, formula);
   }
 
   public BooleanFormula toAssignment(FormulaManagerView pFMGR) {
-    BooleanFormulaManager bfmgr = pFMGR.getBooleanFormulaManager();
-    BitvectorFormulaManager bvm = pFMGR.getBitvectorFormulaManager();
-    FloatingPointFormulaManager fpm = pFMGR.getFloatingPointFormulaManager();
-
-    if (formulaType.isIntegerType()) {
-      IntegerFormulaManagerView ifm = pFMGR.getIntegerFormulaManager();
-      IntegerFormula intVariable = ifm.makeVariable(variableName);
-      IntegerFormula intValue;
-      if (value instanceof BigInteger) {
-        intValue = ifm.makeNumber((BigInteger) value);
-      } else {
-        intValue = ifm.makeNumber(value.longValue());
-      }
-      return ifm.equal(intVariable, intValue);
-    } else if (formulaType instanceof BitvectorType) {
-      BitvectorFormula bvVariable = bvm.makeVariable((BitvectorType) formulaType, variableName);
-      BitvectorFormula bvValue;
-      if (value instanceof BigInteger) {
-        bvValue = bvm.makeBitvector(bvm.getLength(bvVariable), (BigInteger) value);
-      } else {
-        bvValue = bvm.makeBitvector(bvm.getLength(bvVariable), value.longValue());
-      }
-      return bvm.equal(bvVariable, bvValue);
-    } else if (formulaType instanceof FloatingPointType) {
-      FloatingPointFormula fpVariable = fpm.makeVariable(variableName, (FloatingPointType) formulaType);
-      FormulaType.FloatingPointType fpType = (FormulaType.FloatingPointType) pFMGR.getFormulaType(fpVariable);
-      FloatingPointFormula fpValue;
-      if (value instanceof BigDecimal) {
-        fpValue = fpm.makeNumber((BigDecimal) value, fpType);
-      } else if (value instanceof Double) {
-        fpValue = fpm.makeNumber((Double) value, fpType);
-      } else if (value instanceof Float) {
-        fpValue = fpm.makeNumber((Float) value, fpType);
-      } else if (value instanceof Rational) {
-        fpValue = fpm.makeNumber((Rational) value, fpType);
-      } else {
-        fpValue = fpm.makeNumber(value.toString(), fpType);
-      }
-      return fpm.assignment(fpVariable, fpValue);
-    }
-    return bfmgr.makeTrue();
+    return pFMGR.parse(formula);
   }
 }

commit 5fd0793fa49fb53d35ca9fafa19bd98acccc29a5

Fix a bug introduced in r27257 where input assertions lost their ssa indices, thus causing trivial contradictions
git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@27264 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

src/org/sosy_lab/cpachecker/core/algorithm/bmc/KInductionProver.java
@@ -767,8 +767,7 @@ class KInductionProver implements AutoCloseable {
             if ((!index.isPresent()
                 || (index.isPresent()
                     && (isUnconnected || inputs.get(actualName).contains(index.getAsInt()))))) {
-              BooleanFormula assignment =
-                  fmgr.uninstantiate(valueAssignment.getAssignmentAsFormula());
+              BooleanFormula assignment = valueAssignment.getAssignmentAsFormula();
               input = bfmgr.and(input, assignment);
             }
           }

src/org/sosy_lab/cpachecker/core/algorithm/bmc/PartialTransitionRelation.java
@@ -461,27 +461,14 @@ public class PartialTransitionRelation implements Comparable<PartialTransitionRe
         Pair<String, OptionalInt> pair = FormulaManagerView.parseName(fullName);
         String actualName = pair.getFirst();
         OptionalInt index = pair.getSecond();
-        BooleanFormula assignment = pFmgr.uninstantiate(valueAssignment.getAssignmentAsFormula());
-        ModelValue modelValue =
-            new ModelValue(
-                actualName, pFmgr.dumpFormula(assignment).toString(), assignment::toString);
 
         // We consider as inputs
         // a) those that have an SSA index and are contained in our list of input variables and
         // b) those that have no SSA index and are not known as actual variables,
         // such as __ADDRESS_OF:
-        if (index.isPresent() && inputs.get(actualName).contains(index.getAsInt())) {
-          SSAMap ssaForInstantiation =
-              SSAMap.emptySSAMap()
-                  .builder()
-                  .setIndex(actualName, types.get(actualName), index.getAsInt())
-                  .build();
-          inputAssignments =
-              bfmgr.and(
-                  inputAssignments,
-                  pFmgr.instantiate(modelValue.toAssignment(pFmgr), ssaForInstantiation));
-        } else if (!index.isPresent() && !pVariables.containsKey(actualName)) {
-          inputAssignments = bfmgr.and(inputAssignments, modelValue.toAssignment(pFmgr));
+        if ((index.isPresent() && inputs.get(actualName).contains(index.getAsInt()))
+            || (!index.isPresent() && !pVariables.containsKey(actualName))) {
+          inputAssignments = bfmgr.and(inputAssignments, valueAssignment.getAssignmentAsFormula());
         }
       }
     }