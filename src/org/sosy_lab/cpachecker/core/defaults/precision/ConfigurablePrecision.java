fa86295857ac0150b08c4cb384a4263032889a1e
Karlheinz Friedberger <kfriedberger@gmail.com>
2018-03-17 09:38:26 +0000

    VariableTrackingPrecision is serializable now.
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@27564 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/core/defaults/precision/ConfigurablePrecision.java
+++ src/org/sosy_lab/cpachecker/core/defaults/precision/ConfigurablePrecision.java
@@ -40,11 +40,14 @@ import org.sosy_lab.cpachecker.cfa.types.Type;
 import org.sosy_lab.cpachecker.cfa.types.c.CSimpleType;
 import org.sosy_lab.cpachecker.cfa.types.java.JSimpleType;
 import org.sosy_lab.cpachecker.core.interfaces.ConfigurableProgramAnalysis;
+import org.sosy_lab.cpachecker.util.globalinfo.GlobalInfo;
 import org.sosy_lab.cpachecker.util.states.MemoryLocation;
 import org.sosy_lab.cpachecker.util.variableclassification.VariableClassification;
 
 @Options(prefix = "precision")
 public class ConfigurablePrecision extends VariableTrackingPrecision {
+
+  private static final long serialVersionUID = 1L;
 
   @Option(
     secure = true,
@@ -105,7 +108,7 @@ public class ConfigurablePrecision extends VariableTrackingPrecision {
   )
   private boolean trackVariablesBesidesEqAddBool = true;
 
-  private final Optional<VariableClassification> vc;
+  private transient Optional<VariableClassification> vc;
   private final Class<? extends ConfigurableProgramAnalysis> cpaClass;
 
   ConfigurablePrecision(
@@ -293,5 +296,10 @@ public class ConfigurablePrecision extends VariableTrackingPrecision {
         .add("trackFloatVariables", trackFloatVariables)
         .add("trackAddressedVariables", trackAddressedVariables)
         .toString();
+  }
+
+  private void readObject(java.io.ObjectInputStream in) throws IOException, ClassNotFoundException {
+    in.defaultReadObject();
+    vc = GlobalInfo.getInstance().getCFAInfo().get().getCFA().getVarClassification();
   }
 }
