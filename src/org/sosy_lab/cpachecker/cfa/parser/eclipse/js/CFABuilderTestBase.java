9e0ceccdcd4fee35dbb27b0a6b1c0601ed36b1b0
maiermic <maiermic@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2018-04-04 14:24:44 +0000

    add base class for CFA builder tests
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/javascript@27634 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

---
+++ src/org/sosy_lab/cpachecker/cfa/parser/eclipse/js/CFABuilderTestBase.java
@@ -0,0 +1,57 @@
+/*
+ * CPAchecker is a tool for configurable software verification.
+ *  This file is part of CPAchecker.
+ *
+ *  Copyright (C) 2007-2018  Dirk Beyer
+ *  All rights reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *
+ *  CPAchecker web page:
+ *    http://cpachecker.sosy-lab.org
+ */
+package org.sosy_lab.cpachecker.cfa.parser.eclipse.js;
+
+import org.eclipse.wst.jsdt.core.dom.JavaScriptUnit;
+import org.junit.Before;
+import org.sosy_lab.common.configuration.InvalidConfigurationException;
+import org.sosy_lab.cpachecker.cfa.model.CFANode;
+
+abstract class CFABuilderTestBase<S> {
+
+  protected EclipseJavaScriptParser parser;
+  protected ConfigurableJavaScriptCFABuilder builder;
+  protected CFANode entryNode;
+
+  @Before
+  public void init() throws InvalidConfigurationException {
+    builder = JavaScriptCFABuilderFactory.createTestJavaScriptCFABuilder();
+    parser = new EclipseJavaScriptParser(builder.getLogger());
+    entryNode = builder.getExitNode();
+  }
+
+  protected JavaScriptUnit createAST(final String pCode) {
+    return (JavaScriptUnit) parser.createAST(builder.getBuilder().getFilename(), pCode);
+  }
+
+  protected S parseStatement(final String pCode) {
+    return parseStatement(pCode, 0);
+  }
+
+  @SuppressWarnings("unchecked")
+  protected S parseStatement(final String pCode, final int pIndex) {
+    return (S) createAST(pCode).statements().get(pIndex);
+  }
+
+}
