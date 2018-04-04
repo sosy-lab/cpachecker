4aa85e7f781705bcaef37a6c11f6bb6a514f1d0d
maiermic <maiermic@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2018-04-04 14:22:24 +0000

    implement variable assignment expression
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/javascript@27622 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

---
+++ src/org/sosy_lab/cpachecker/cfa/parser/eclipse/js/AssignmentCFABuilderTest.java
@@ -0,0 +1,101 @@
+/*
+ * CPAchecker is a tool for configurable software verification.
+ *  This file is part of CPAchecker.
+ *
+ *  Copyright (C) 2007-2018  Dirk Beyer
+ *  All rights reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *
+ *  CPAchecker web page:
+ *    http://cpachecker.sosy-lab.org
+ */
+package org.sosy_lab.cpachecker.cfa.parser.eclipse.js;
+
+import static org.mockito.Mockito.mock;
+
+import com.google.common.truth.Truth;
+import java.math.BigInteger;
+import org.eclipse.wst.jsdt.core.dom.Assignment;
+import org.eclipse.wst.jsdt.core.dom.ExpressionStatement;
+import org.eclipse.wst.jsdt.core.dom.JavaScriptUnit;
+import org.eclipse.wst.jsdt.core.dom.SimpleName;
+import org.junit.Before;
+import org.junit.Test;
+import org.sosy_lab.common.configuration.InvalidConfigurationException;
+import org.sosy_lab.cpachecker.cfa.ast.FileLocation;
+import org.sosy_lab.cpachecker.cfa.ast.js.JSAssignment;
+import org.sosy_lab.cpachecker.cfa.ast.js.JSIdExpression;
+import org.sosy_lab.cpachecker.cfa.ast.js.JSIntegerLiteralExpression;
+import org.sosy_lab.cpachecker.cfa.ast.js.JSSimpleDeclaration;
+import org.sosy_lab.cpachecker.cfa.model.CFANode;
+import org.sosy_lab.cpachecker.cfa.model.js.JSStatementEdge;
+import org.sosy_lab.cpachecker.cfa.types.js.JSAnyType;
+
+public class AssignmentCFABuilderTest {
+
+  private EclipseJavaScriptParser parser;
+  private ConfigurableJavaScriptCFABuilder builder;
+  private CFANode entryNode;
+
+  @Before
+  public void init() throws InvalidConfigurationException {
+    builder = JavaScriptCFABuilderFactory.createTestJavaScriptCFABuilder();
+    parser = new EclipseJavaScriptParser(builder.getLogger());
+    entryNode = builder.getExitNode();
+  }
+
+  private JavaScriptUnit createAST(final String pCode) {
+    return (JavaScriptUnit) parser.createAST(builder.getBuilder().getFilename(), pCode);
+  }
+
+  @SuppressWarnings("unchecked")
+  private Assignment parseAssignment(final String pCode) {
+    final ExpressionStatement expressionStatement =
+        (ExpressionStatement) createAST(pCode).statements().get(0);
+    return (Assignment) expressionStatement.getExpression();
+  }
+
+  @Test
+  public void testVariableAssignment() {
+    final Assignment assignmentExpression = parseAssignment("x = 42");
+
+    final String expectedVariableName = "x";
+    final JSIdExpression variableId =
+        new JSIdExpression(
+            FileLocation.DUMMY,
+            JSAnyType.ANY,
+            expectedVariableName,
+            mock(JSSimpleDeclaration.class));
+
+    final JSIntegerLiteralExpression expectedInitializerExpression =
+        new JSIntegerLiteralExpression(FileLocation.DUMMY, BigInteger.valueOf(42));
+    // expected CFA: entryNode -{x = 42}-> ()
+
+    builder.setExpressionAppendable(
+        (pBuilder, pExpression) ->
+            pExpression instanceof SimpleName ? variableId : expectedInitializerExpression);
+
+    final JSIdExpression result =
+        (JSIdExpression) new AssignmentCFABuilder().append(builder, assignmentExpression);
+
+    Truth.assertThat(result).isEqualTo(variableId);
+    final JSStatementEdge assignmentEdge = (JSStatementEdge) entryNode.getLeavingEdge(0);
+    final JSAssignment variableAssignment = (JSAssignment) assignmentEdge.getStatement();
+    Truth.assertThat(variableAssignment.getLeftHandSide()).isEqualTo(variableId);
+    Truth.assertThat(variableAssignment.getRightHandSide())
+        .isEqualTo(expectedInitializerExpression);
+    Truth.assertThat(assignmentEdge.getSuccessor().getNumLeavingEdges()).isEqualTo(0);
+  }
+}
