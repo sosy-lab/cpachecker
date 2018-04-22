9396a94226bb83a9ee6a73592e55da9740701a87
maiermic <maiermic@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2018-04-22 08:49:45 +0000

    test that ExpressionCFABuilder converts SimpleName correctly
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/javascript@27769 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

---
+++ src/org/sosy_lab/cpachecker/cfa/parser/eclipse/js/ExpressionCFABuilderTest.java
@@ -0,0 +1,62 @@
+/*
+ * CPAchecker is a tool for configurable software verification.
+ *  This file is part of CPAchecker.
+ *
+ *  Copyright (C) 2007-2018  Dirk Beyer
+ *  All rights reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *
+ *  CPAchecker web page:
+ *    http://cpachecker.sosy-lab.org
+ */
+package org.sosy_lab.cpachecker.cfa.parser.eclipse.js;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import com.google.common.truth.Truth;
+import org.eclipse.wst.jsdt.core.dom.Expression;
+import org.eclipse.wst.jsdt.core.dom.SimpleName;
+import org.junit.Test;
+import org.sosy_lab.cpachecker.cfa.ast.js.JSExpression;
+import org.sosy_lab.cpachecker.cfa.ast.js.JSIdExpression;
+import org.sosy_lab.cpachecker.cfa.ast.js.JSUndefinedLiteralExpression;
+
+public class ExpressionCFABuilderTest extends CFABuilderTestBase {
+
+  @Test
+  public void testConvertSimpleNameToUndefinedLiteral() {
+    final Expression undefinedExpression = parseExpression(SimpleName.class, "undefined");
+    final JSExpression result = new ExpressionCFABuilder().append(builder, undefinedExpression);
+    Truth.assertThat(result).isInstanceOf(JSUndefinedLiteralExpression.class);
+  }
+
+  @Test
+  public void testConvertSimpleNameToJSIdExpression() {
+    final Expression simpleNameExpression = parseExpression(SimpleName.class, "foo");
+    final ExpressionCFABuilder expressionCFABuilder = new ExpressionCFABuilder();
+    final SimpleNameResolver simpleNameResolver = mock(SimpleNameResolver.class);
+    final JSIdExpression jsIdExpression = mock(JSIdExpression.class, "foo");
+    when(simpleNameResolver.resolve(eq(builder), any())).thenReturn(jsIdExpression);
+    expressionCFABuilder.setSimpleNameResolver(simpleNameResolver);
+
+    final JSExpression result = expressionCFABuilder.append(builder, simpleNameExpression);
+
+    Truth.assertThat(result).isEqualTo(jsIdExpression);
+  }
+
+}
