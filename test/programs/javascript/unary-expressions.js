ae450f9820153fbba72a004d1cca5b8792b5e246
maiermic <maiermic@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2018-05-01 06:14:57 +0000

    test that boolean is converted to number by unary plus/minus operator
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/javascript@27836 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- test/programs/javascript/unary-expressions.js
+++ test/programs/javascript/unary-expressions.js
@@ -5,3 +5,8 @@ if (+3 !== 3) { __VERIFIER_error(); }
 if (3 === -3) { __VERIFIER_error(); }
 if (3.0 === -3.0) { __VERIFIER_error(); }
 if (+0.0 !== -0.0) { __VERIFIER_error(); }
+// boolean is converted to number
+if (+true !== 1) { __VERIFIER_error(); }
+if (-true !== -1) { __VERIFIER_error(); }
+if (+false !== 0) { __VERIFIER_error(); }
+if (-false !== -0) { __VERIFIER_error(); }
