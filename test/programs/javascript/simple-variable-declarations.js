64ff21c356dec4f65edd9c0bc3cc8f7a47f70116
maiermic <maiermic@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2018-05-04 11:28:41 +0000

    verify variable values
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/javascript@27857 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- test/programs/javascript/simple-variable-declarations.js
+++ test/programs/javascript/simple-variable-declarations.js
@@ -1,3 +1,5 @@
+function __VERIFIER_error() {}
+
 var int = 42, float = 3.14;
 var singleQuoted = 'bar', doubleQuoted = "foobar";
 var truthy = true, falsy = false;
@@ -5,3 +7,12 @@ var nullable = null;
 
 // Not working due to a bug in the Eclipse parser
 // var notDefined = undefined;
+
+if (int !== 42) { __VERIFIER_error(); }
+if (float !== 3.14) { __VERIFIER_error(); }
+// TODO uncomment when string value tracking is implemented
+// if (singleQuoted !== "bar") { __VERIFIER_error(); }
+// if (doubleQuoted !== 'foobar') { __VERIFIER_error(); }
+if (!truthy) { __VERIFIER_error(); }
+if (falsy) { __VERIFIER_error(); }
+if (nullable !== null) { __VERIFIER_error(); }
