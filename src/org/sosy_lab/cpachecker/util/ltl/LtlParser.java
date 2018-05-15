c72d0a64bb01a5be8330159622bae2c6a2702eda
tbunk <tbunk@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2018-05-15 18:06:03 +0000

    Add parser that translates a string into a strongly typed ltl-formula
    The format is specified as follows:
    \phi := AP | \phi OR \phi | \phi AND \phi | ! \phi | G \phi | F \phi | \phi U \phi
         | \phi W \phi | \phi R \phi | \phi S \phi
    where \phi is a temporal formula and AP an atomic proposition. The exact specification
    can be found at '/src/org/sosy_lab/cpachecker/util/ltl/generated/LtlGrammar.g4'
    
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/ltl-fairness-mt@27961 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

---
+++ src/org/sosy_lab/cpachecker/util/ltl/LtlParser.java
@@ -0,0 +1,65 @@
+/*
+ *  CPAchecker is a tool for configurable software verification.
+ *  This file is part of CPAchecker.
+ *
+ *  Copyright (C) 2007-2018  Dirk Beyer
+ *  All rights reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *
+ *  CPAchecker web page:
+ *    http://cpachecker.sosy-lab.org
+ */
+package org.sosy_lab.cpachecker.util.ltl;
+
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.ConsoleErrorListener;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.sosy_lab.cpachecker.util.ltl.formulas.Formula;
+import org.sosy_lab.cpachecker.util.ltl.generated.LtlGrammarBaseVisitor;
+import org.sosy_lab.cpachecker.util.ltl.generated.LtlGrammarLexer;
+import org.sosy_lab.cpachecker.util.ltl.generated.LtlGrammarParser;
+
+abstract class LtlParser extends LtlGrammarBaseVisitor<Formula> {
+
+  private final CharStream input;
+
+  LtlParser(CharStream input) {
+    this.input = input;
+  }
+
+  abstract ParseTree getParseTree(LtlGrammarParser parser);
+
+  Formula doParse() {
+    // Tokenize the stream
+    LtlGrammarLexer lexer = new LtlGrammarLexer(input);
+    // Raise an exception instead of printing long error messages on the console
+    // For more informations, see https://stackoverflow.com/a/26573239/8204996
+    lexer.removeErrorListener(ConsoleErrorListener.INSTANCE);
+    // Add a fail-fast behavior for token errors
+    lexer.addErrorListener(ParserErrorListener.INSTANCE);
+
+    CommonTokenStream tokens = new CommonTokenStream(lexer);
+
+    // Parse the tokens
+    LtlGrammarParser parser = new LtlGrammarParser(tokens);
+    parser.removeErrorListeners();
+    parser.addErrorListener(ParserErrorListener.INSTANCE);
+
+    LtlFormulaTreeVisitor visitor = new LtlFormulaTreeVisitor();
+    ParseTree tree = getParseTree(parser);
+    return visitor.visit(tree);
+  }
+}
