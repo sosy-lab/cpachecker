sosy-lab/cpachecker

commit ad324af37ed5c544582f9b9680f7ef860b12e984

Constraints State stores model of satisfiable constraints and tries to solve constraints with that first.
This should speed up constraints solving.
Unfortunately, we can't use Model.evaluate(BooleanFormula)
which should be the right way to do it, because we get Segmentation
Faults as soon as we close the corresponding prover - and we don't want
to keep that open.

git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@28026 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

src/org/sosy_lab/cpachecker/cpa/constraints/domain/ConstraintsState.java
@@ -26,6 +26,17 @@ package org.sosy_lab.cpachecker.cpa.constraints.domain;
 import static com.google.common.base.Preconditions.checkNotNull;
 
 import com.google.common.base.Joiner;
+import com.google.common.collect.ImmutableList;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 import org.sosy_lab.cpachecker.core.interfaces.AbstractState;
 import org.sosy_lab.cpachecker.core.interfaces.Graphable;
 import org.sosy_lab.cpachecker.cpa.constraints.FormulaCreator;
@@ -39,24 +50,13 @@ import org.sosy_lab.cpachecker.cpa.value.type.Value;
 import org.sosy_lab.cpachecker.exceptions.UnrecognizedCCodeException;
 import org.sosy_lab.cpachecker.util.predicates.smt.FormulaManagerView;
 import org.sosy_lab.cpachecker.util.predicates.smt.Solver;
-import org.sosy_lab.java_smt.api.SolverException;
 import org.sosy_lab.java_smt.api.BooleanFormula;
 import org.sosy_lab.java_smt.api.Formula;
 import org.sosy_lab.java_smt.api.Model;
 import org.sosy_lab.java_smt.api.Model.ValueAssignment;
 import org.sosy_lab.java_smt.api.ProverEnvironment;
 import org.sosy_lab.java_smt.api.SolverContext.ProverOptions;
-
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
-import java.util.Set;
+import org.sosy_lab.java_smt.api.SolverException;
 
 /**
  * State for Constraints Analysis. Stores constraints and whether they are solvable.
@@ -84,6 +84,7 @@ public class ConstraintsState implements AbstractState, Graphable, Set<Constrain
   private SymbolicIdentifierLocator locator;
 
   private IdentifierAssignment definiteAssignment;
+  private ImmutableList<ValueAssignment> lastModel;
 
   /**
    * Creates a new, initial <code>ConstraintsState</code> object.
@@ -127,6 +128,7 @@ public class ConstraintsState implements AbstractState, Graphable, Set<Constrain
 
     lastAddedConstraint = pState.lastAddedConstraint;
     definiteAssignment = new IdentifierAssignment(pState.definiteAssignment);
+    lastModel = pState.lastModel;
   }
 
   /**
@@ -274,25 +276,39 @@ public class ConstraintsState implements AbstractState, Graphable, Set<Constrain
    * @return <code>true</code> if this state is unsatisfiable, <code>false</code> otherwise
    */
   public boolean isUnsat() throws SolverException, InterruptedException, UnrecognizedCCodeException {
-    boolean unsat = false;
+    boolean unsat = true;
 
     try {
       if (!constraints.isEmpty()) {
-        prover = solver.newProverEnvironment(ProverOptions.GENERATE_MODELS);
         BooleanFormula constraintsAsFormula = getFullFormula();
-
+        prover = solver.newProverEnvironment(ProverOptions.GENERATE_MODELS);
         prover.push(constraintsAsFormula);
-        unsat = prover.isUnsat();
 
-        if (!unsat) {
-          // doing this while the complete formula is still on the prover environment stack is
-          // cheaper than performing another complete SAT check when the assignment is really requested
-          resolveDefiniteAssignments();
-
-        } else {
-          definiteAssignment = null;
+        if (lastModel != null) {
+          List<BooleanFormula> modelAssignments = new ArrayList<>(lastModel.size());
+          for (ValueAssignment a : lastModel) {
+            modelAssignments.add(a.getAssignmentAsFormula());
+          }
+          prover.push(formulaManager.getBooleanFormulaManager().and(modelAssignments));
+          unsat = prover.isUnsat();
         }
 
+        if (unsat) {
+          prover.pop(); // Remove model assignments
+          unsat = prover.isUnsat();
+          if (!unsat) {
+            lastModel = prover.getModelAssignments();
+            // doing this while the complete formula is still on the prover environment stack is
+            // cheaper than performing another complete SAT check when the assignment is really
+            // requested
+            resolveDefiniteAssignments();
+          }
+        }
+
+        if (unsat) {
+          definiteAssignment = null;
+          lastModel = null;
+        }
       }
     } finally {
       closeProver();

test/programs/simple/explicit/symbolic/constraintsCaching.c
@@ -1,0 +1,17 @@
+extern int __VERIFIER_nondet_int();
+extern void __VERIFIER_error();
+
+int main() {
+    int a = __VERIFIER_nondet_int();
+    int b = a + 5;
+
+    if (a > 0) {
+        a = a - 1;
+    }
+
+    if (b > -4) {
+ERROR:
+        __VERIFIER_error();
+        return -1;
+    }
+}

commit 74e78d4f632933452314e23d933fddb6b82dc014

Fix bug in ConstraintsState introduced in r28026: Only pop formula from solver stack if model assignment was actually pushed to it before.
git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@28028 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

src/org/sosy_lab/cpachecker/cpa/constraints/domain/ConstraintsState.java
@@ -291,10 +291,10 @@ public class ConstraintsState implements AbstractState, Graphable, Set<Constrain
           }
           prover.push(formulaManager.getBooleanFormulaManager().and(modelAssignments));
           unsat = prover.isUnsat();
+          prover.pop(); // Remove model assignment from prover
         }
 
         if (unsat) {
-          prover.pop(); // Remove model assignments
           unsat = prover.isUnsat();
           if (!unsat) {
             lastModel = prover.getModelAssignments();