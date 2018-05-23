bc04cbaa94ba170d1fadcd0b053ee23abfca70b3
tbunk <tbunk@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2018-05-23 12:19:29 +0000

    Change buchi-converter class to make use of jhoafparser-lib
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@28044 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/util/ltl/Ltl2BuechiConverter.java
+++ src/org/sosy_lab/cpachecker/util/ltl/Ltl2BuechiConverter.java
@@ -23,58 +23,53 @@
  */
 package org.sosy_lab.cpachecker.util.ltl;
 
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.nio.charset.Charset;
 import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Objects;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.function.Consumer;
-import java.util.stream.Stream;
+import jhoafparser.consumer.HOAConsumerPrint;
+import jhoafparser.consumer.HOAConsumerStore;
+import jhoafparser.parser.HOAFParser;
+import jhoafparser.parser.generated.ParseException;
+import jhoafparser.storage.StoredAutomaton;
 import org.sosy_lab.common.NativeLibraries;
+import org.sosy_lab.cpachecker.cpa.automaton.Automaton;
 import org.sosy_lab.cpachecker.util.ltl.formulas.LtlFormula;
 
 public class Ltl2BuechiConverter {
 
   private static final String LTL3BA = "./ltl3ba";
 
-  private static class StreamGobbler implements Runnable {
-
-    private InputStream input;
-    private Consumer<String> consumer;
-
-    public StreamGobbler(InputStream input, Consumer<String> consumer) {
-      this.input = input;
-      this.consumer = consumer;
-    }
-
-    @Override
-    public void run() {
-      InputStreamReader inputStreamReader = new InputStreamReader(input, Charset.defaultCharset());
-      BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-      bufferedReader.lines().forEach(consumer);
-    }
-  }
-
-  public static InputStream convertFormula(LtlFormula f) throws InterruptedException, IOException {
-    if (!System.getProperty("os.name").equals("Linux")) {
-      throw new UnsupportedOperationException("Only Linux is currently supported as OS");
-    }
-    Objects.requireNonNull(f);
-    return new Ltl2BuechiConverter(f).runLtlExec();
-  }
-
   private final LtlFormula ltlFormula;
   private final ProcessBuilder builder;
 
-  private Ltl2BuechiConverter(LtlFormula f) {
-    ltlFormula = f;
+  public static Automaton convertFormula(LtlFormula pFormula)
+      throws InterruptedException, IOException, LtlParseException {
+    Objects.requireNonNull(pFormula);
+
+    StoredAutomaton storedAutomaton;
+    Ltl2BuechiConverter converter = new Ltl2BuechiConverter(pFormula);
+
+    try (InputStream is = converter.runLtlExec()) {
+
+      HOAConsumerStore consumer = new HOAConsumerStore();
+      HOAFParser.parseHOA(is, consumer);
+      storedAutomaton = consumer.getStoredAutomaton();
+      storedAutomaton.feedToConsumer(new HOAConsumerPrint(System.out));
+
+    } catch (ParseException e) {
+      throw new LtlParseException(
+          String.format(
+              "An error occured while parsing the output from the external tool '%s'", LTL3BA),
+          e);
+    }
+
+    //    return LtlParserUtils.transform(storedAutomaton);
+    return null;
+  }
+
+  private Ltl2BuechiConverter(LtlFormula pFormula) {
+    ltlFormula = pFormula;
     builder = new ProcessBuilder();
 
     Path nativeLibraryPath = NativeLibraries.getNativeLibraryPath();
@@ -87,28 +82,16 @@ public class Ltl2BuechiConverter {
     builder.command(LTL3BA, "-H", "-f", ltlFormula.toString());
   }
 
-  private InputStream runLtlExec() throws IOException, InterruptedException {
+  private InputStream runLtlExec() throws IOException, InterruptedException, LtlParseException {
     Process process = builder.start();
     InputStream is = process.getInputStream();
 
     int exitvalue = process.waitFor();
     if (exitvalue != 0) {
-      // TODO: throw exception -- "Tool 'ltl3ba' exited with error code: exitvalue"
+      throw new LtlParseException(
+          String.format("Tool %s exited with error code: %d", LTL3BA, exitvalue));
     }
 
     return is;
-  }
-
-  public static Stream<String> convertProcessToStream(InputStream is)
-      throws InterruptedException, ExecutionException {
-    List<String> list = new ArrayList<>();
-
-    ExecutorService executor = Executors.newSingleThreadExecutor();
-    StreamGobbler gobbler = new StreamGobbler(is, (x) -> list.add(x));
-
-    executor.submit(gobbler).get();
-    executor.shutdown();
-
-    return list.stream();
   }
 }
