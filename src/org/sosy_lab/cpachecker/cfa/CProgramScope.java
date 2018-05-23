b13501a836eb1487b4cbe0ede41b03459d35b190
mordan <mordan@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2018-05-23 11:59:47 +0000

    Reverted r28033
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/automata_assumptions@28041 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/cfa/CProgramScope.java
+++ src/org/sosy_lab/cpachecker/cfa/CProgramScope.java
@@ -344,6 +344,9 @@ public class CProgramScope implements Scope {
     Iterator<CSimpleDeclaration> resultIt = results.iterator();
     if (resultIt.hasNext()) {
       result = resultIt.next();
+      if (resultIt.hasNext()) {
+        result = null;
+      }
     }
     return result;
   }
