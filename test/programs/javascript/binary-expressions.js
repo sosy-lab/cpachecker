1655c9d01dff6c165307d61beb1f95dad392d5ea
maiermic <maiermic@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2018-06-07 16:11:09 +0000

    add NaN and isNaN
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/javascript@28207 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- test/programs/javascript/binary-expressions.js
+++ test/programs/javascript/binary-expressions.js
@@ -1,5 +1,13 @@
 function __VERIFIER_error() {}
 
+// TODO use global object definition of NaN and Number.isNaN when it is implemented
+var NaN = -(-undefined);
+function isNaN(value) {
+  return value !== value;
+}
+if (!isNaN(NaN)) { __VERIFIER_error(); }
+if (isNaN(0)) { __VERIFIER_error(); }
+
 // strict (un-) equality
 if (2 === 3) { __VERIFIER_error(); }
 if (3 !== 3) { __VERIFIER_error(); }
