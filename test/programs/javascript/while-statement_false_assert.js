c77dd0d14b7b94420015a5d003344d3a3264749b
maiermic <maiermic@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2018-06-08 20:40:46 +0000

    add benchmark tests for while statement
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/javascript@28244 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

---
+++ test/programs/javascript/while-statement_false_assert.js
@@ -0,0 +1,7 @@
+function __VERIFIER_error() {}
+
+var x = 0;
+while (x === 0) {
+  __VERIFIER_error();
+  x++;
+}
---
+++ test/programs/javascript/while-statement_true_assert.js
@@ -0,0 +1,10 @@
+function __VERIFIER_error() {}
+
+var x = 0;
+while (x === 0) {
+  x++;
+}
+
+if (x !== 1) {
+  __VERIFIER_error();
+}
