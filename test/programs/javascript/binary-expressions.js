9162b5f5d459a4583d31a596228c0134e0ba19a0
maiermic <maiermic@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2018-06-08 20:39:33 +0000

    add further tests of multiplication
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/javascript@28237 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- test/programs/javascript/binary-expressions.js
+++ test/programs/javascript/binary-expressions.js
@@ -26,8 +26,42 @@ if (5 - 3 === 3) { __VERIFIER_error(); }
 // If either operand is NaN, the result is NaN.
 if (!isNaN(NaN * 3)) { __VERIFIER_error(); }
 if (!isNaN(3 * NaN)) { __VERIFIER_error(); }
-// TODO test other cases of the specification (i.a. multiplication of infinity)
-// Remaining cases
+// The sign of the result is positive if both operands have the same sign,
+// negative if the operands have different signs.
+if ( 2 *  3 !==  6) { __VERIFIER_error(); }
+if (-2 * -3 !==  6) { __VERIFIER_error(); }
+if ( 2 * -3 !== -6) { __VERIFIER_error(); }
+if (-2 *  3 !== -6) { __VERIFIER_error(); }
+// Multiplication of an infinity by a zero results in NaN.
+if (!isNaN( Infinity * 0)) { __VERIFIER_error(); }
+if (!isNaN(-Infinity * 0)) { __VERIFIER_error(); }
+if (!isNaN(0 *  Infinity)) { __VERIFIER_error(); }
+if (!isNaN(0 * -Infinity)) { __VERIFIER_error(); }
+// Multiplication of an infinity by an infinity results in an infinity.
+// The sign is determined by the rule already stated above.
+if ( Infinity *  Infinity !==  Infinity) { __VERIFIER_error(); }
+if (-Infinity * -Infinity !==  Infinity) { __VERIFIER_error(); }
+if (-Infinity *  Infinity !== -Infinity) { __VERIFIER_error(); }
+if ( Infinity * -Infinity !== -Infinity) { __VERIFIER_error(); }
+// Multiplication of an infinity by a finite nonzero value results in a signed infinity.
+// The sign is determined by the rule already stated above.
+if ( Infinity *  9 !==  Infinity) { __VERIFIER_error(); }
+if (-Infinity * -9 !==  Infinity) { __VERIFIER_error(); }
+if (-Infinity *  9 !== -Infinity) { __VERIFIER_error(); }
+if ( Infinity * -9 !== -Infinity) { __VERIFIER_error(); }
+if ( 9 *  Infinity !==  Infinity) { __VERIFIER_error(); }
+if (-9 * -Infinity !==  Infinity) { __VERIFIER_error(); }
+if ( 9 * -Infinity !== -Infinity) { __VERIFIER_error(); }
+if (-9 *  Infinity !== -Infinity) { __VERIFIER_error(); }
+// In the remaining cases, where neither an infinity or NaN is involved,
+// the product is computed and rounded to the nearest representable value using
+// IEEE 754 round-to-nearest mode.
+// If the magnitude is too large to represent,
+// the result is then an infinity of appropriate sign.
+// If the magnitude is too small to represent,
+// the result is then a zero of appropriate sign.
+// The ECMAScript language requires support of gradual underflow as defined by IEEE 754.
+// TODO test edge cases described in comment above
 if (0 * 3 !== 0) { __VERIFIER_error(); }
 if (2 * 3 !== 6) { __VERIFIER_error(); }
 if (-2 * 3 !== -6) { __VERIFIER_error(); }
