395c6b30c783e92f746681534791c7ba7fa9743c
maiermic <maiermic@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2018-06-17 23:06:31 +0000

    add benchmark tests for for statement
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/javascript@28349 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

---
+++ test/programs/javascript/for-statement_false_assert.js
@@ -0,0 +1,7 @@
+function __VERIFIER_error() {}
+
+for (var x = 0; x !== 3; x++) {
+  if (x === 2) {
+    __VERIFIER_error();
+  }
+}
---
+++ test/programs/javascript/for-statement_true_assert.js
@@ -0,0 +1,14 @@
+function __VERIFIER_error() {}
+
+for (var x = 0; x !== 3; x++) {
+  if (x < 0) {
+    __VERIFIER_error();
+  }
+  if (x >= 3) {
+    __VERIFIER_error();
+  }
+}
+
+if (x !== 3) {
+  __VERIFIER_error();
+}
