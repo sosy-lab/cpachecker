32f07db7386a3a669ca161b059e899f5b20c603b
maiermic <maiermic@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2018-06-17 23:05:53 +0000

    test that less- and greater-than evaluate to false for equal values
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/javascript@28345 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- test/programs/javascript/binary-expressions.js
+++ test/programs/javascript/binary-expressions.js
@@ -170,8 +170,10 @@ if (-3.2 % -2 !== -1.2000000000000002) { __VERIFIER_error(); }
 // The Less-than operator, see https://www.ecma-international.org/ecma-262/5.1/#sec-11.8.1
 // ---------------------------------------------------------------------------------------
 if (5 < 2) { __VERIFIER_error(); }
+if (5 < 5) { __VERIFIER_error(); }
 
 // ------------------------------------------------------------------------------------------
 // The Greater-than operator, see https://www.ecma-international.org/ecma-262/5.1/#sec-11.8.2
 // ------------------------------------------------------------------------------------------
 if (2 > 5) { __VERIFIER_error(); }
+if (5 > 5) { __VERIFIER_error(); }
