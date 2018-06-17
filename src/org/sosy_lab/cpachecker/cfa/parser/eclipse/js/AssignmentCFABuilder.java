3d1167afd27a76150317da2f11a71f3b5ab54829
maiermic <maiermic@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2018-06-17 23:08:00 +0000

    support JS assignment with operator
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/javascript@28356 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/cfa/parser/eclipse/js/AssignmentCFABuilder.java
+++ src/org/sosy_lab/cpachecker/cfa/parser/eclipse/js/AssignmentCFABuilder.java
@@ -23,8 +23,16 @@
  */
 package org.sosy_lab.cpachecker.cfa.parser.eclipse.js;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import javax.annotation.Nonnull;
 import org.eclipse.wst.jsdt.core.dom.Assignment;
+import org.eclipse.wst.jsdt.core.dom.Assignment.Operator;
 import org.sosy_lab.cpachecker.cfa.ast.FileLocation;
+import org.sosy_lab.cpachecker.cfa.ast.js.JSBinaryExpression;
+import org.sosy_lab.cpachecker.cfa.ast.js.JSBinaryExpression.BinaryOperator;
 import org.sosy_lab.cpachecker.cfa.ast.js.JSExpression;
 import org.sosy_lab.cpachecker.cfa.ast.js.JSExpressionAssignmentStatement;
 import org.sosy_lab.cpachecker.cfa.ast.js.JSIdExpression;
@@ -38,8 +46,13 @@ class AssignmentCFABuilder implements AssignmentAppendable {
     // TODO handle other left hand side expressions like field access
     final JSIdExpression varId = (JSIdExpression) pBuilder.append(pAssignment.getLeftHandSide());
     final JSExpression value = pBuilder.append(pAssignment.getRightHandSide());
+    final Optional<BinaryOperator> operator = binaryOperatorOf(pAssignment.getOperator());
+    final JSExpression newValue =
+        operator.isPresent()
+            ? new JSBinaryExpression(FileLocation.DUMMY, varId, value, operator.get())
+            : value;
     final JSExpressionAssignmentStatement assignmentStatement =
-        new JSExpressionAssignmentStatement(FileLocation.DUMMY, varId, value);
+        new JSExpressionAssignmentStatement(FileLocation.DUMMY, varId, newValue);
     pBuilder.appendEdge(
         (pPredecessor, pSuccessor) ->
             new JSStatementEdge(
@@ -49,5 +62,32 @@ class AssignmentCFABuilder implements AssignmentAppendable {
                 pPredecessor,
                 pSuccessor));
     return varId;
+  }
+
+  private static final @Nonnull Map<Operator, BinaryOperator> assignmentOperatorToBinaryOperator;
+
+  static {
+    final Map<Operator, BinaryOperator> m = new HashMap<>();
+    m.put(Operator.TIMES_ASSIGN, BinaryOperator.TIMES);
+    m.put(Operator.DIVIDE_ASSIGN, BinaryOperator.DIVIDE);
+    m.put(Operator.REMAINDER_ASSIGN, BinaryOperator.REMAINDER);
+    m.put(Operator.PLUS_ASSIGN, BinaryOperator.PLUS);
+    m.put(Operator.MINUS_ASSIGN, BinaryOperator.MINUS);
+    m.put(Operator.LEFT_SHIFT_ASSIGN, BinaryOperator.LEFT_SHIFT);
+    m.put(Operator.RIGHT_SHIFT_SIGNED_ASSIGN, BinaryOperator.RIGHT_SHIFT_SIGNED);
+    m.put(Operator.RIGHT_SHIFT_UNSIGNED_ASSIGN, BinaryOperator.RIGHT_SHIFT_UNSIGNED);
+    m.put(Operator.BIT_AND_ASSIGN, BinaryOperator.AND);
+    m.put(Operator.BIT_XOR_ASSIGN, BinaryOperator.XOR);
+    m.put(Operator.BIT_OR_ASSIGN, BinaryOperator.OR);
+    assignmentOperatorToBinaryOperator = Collections.unmodifiableMap(m);
+  }
+
+  private static Optional<BinaryOperator> binaryOperatorOf(final Operator pOperator) {
+    if (pOperator.equals(Operator.ASSIGN)) {
+      return Optional.empty();
+    }
+    assert assignmentOperatorToBinaryOperator.containsKey(pOperator)
+        : "Unhandled assignment operator " + pOperator;
+    return Optional.of(assignmentOperatorToBinaryOperator.get(pOperator));
   }
 }
--- src/org/sosy_lab/cpachecker/cfa/parser/eclipse/js/AssignmentCFABuilderTest.java
+++ src/org/sosy_lab/cpachecker/cfa/parser/eclipse/js/AssignmentCFABuilderTest.java
@@ -32,6 +32,9 @@ import org.eclipse.wst.jsdt.core.dom.SimpleName;
 import org.junit.Test;
 import org.sosy_lab.cpachecker.cfa.ast.FileLocation;
 import org.sosy_lab.cpachecker.cfa.ast.js.JSAssignment;
+import org.sosy_lab.cpachecker.cfa.ast.js.JSBinaryExpression;
+import org.sosy_lab.cpachecker.cfa.ast.js.JSBinaryExpression.BinaryOperator;
+import org.sosy_lab.cpachecker.cfa.ast.js.JSExpression;
 import org.sosy_lab.cpachecker.cfa.ast.js.JSIdExpression;
 import org.sosy_lab.cpachecker.cfa.ast.js.JSIntegerLiteralExpression;
 import org.sosy_lab.cpachecker.cfa.ast.js.JSSimpleDeclaration;
@@ -67,6 +70,95 @@ public class AssignmentCFABuilderTest extends CFABuilderTestBase {
     Truth.assertThat(variableAssignment.getLeftHandSide()).isEqualTo(variableId);
     Truth.assertThat(variableAssignment.getRightHandSide())
         .isEqualTo(expectedInitializerExpression);
+    Truth.assertThat(assignmentEdge.getSuccessor().getNumLeavingEdges()).isEqualTo(0);
+  }
+
+  @Test
+  public void testVariableTimesOperatorAssignment() {
+    testVariableOperatorAssignment("x *= 42", BinaryOperator.TIMES);
+  }
+
+  @Test
+  public void testVariableDivideOperatorAssignment() {
+    testVariableOperatorAssignment("x /= 42", BinaryOperator.DIVIDE);
+  }
+
+  @Test
+  public void testVariableRemainderOperatorAssignment() {
+    testVariableOperatorAssignment("x %= 42", BinaryOperator.REMAINDER);
+  }
+
+  @Test
+  public void testVariablePlusOperatorAssignment() {
+    testVariableOperatorAssignment("x += 42", BinaryOperator.PLUS);
+  }
+
+  @Test
+  public void testVariableMinusOperatorAssignment() {
+    testVariableOperatorAssignment("x -= 42", BinaryOperator.MINUS);
+  }
+
+  @Test
+  public void testVariableLeftShiftOperatorAssignment() {
+    testVariableOperatorAssignment("x <<= 42", BinaryOperator.LEFT_SHIFT);
+  }
+
+  @Test
+  public void testVariableRightShiftSignedOperatorAssignment() {
+    testVariableOperatorAssignment("x >>= 42", BinaryOperator.RIGHT_SHIFT_SIGNED);
+  }
+
+  @Test
+  public void testVariableRightShiftUnsignedOperatorAssignment() {
+    testVariableOperatorAssignment("x >>>= 42", BinaryOperator.RIGHT_SHIFT_UNSIGNED);
+  }
+
+  @Test
+  public void testVariableAndOperatorAssignment() {
+    testVariableOperatorAssignment("x &= 42", BinaryOperator.AND);
+  }
+
+  @Test
+  public void testVariableXorOperatorAssignment() {
+    testVariableOperatorAssignment("x ^= 42", BinaryOperator.XOR);
+  }
+
+  @Test
+  public void testVariableOrOperatorAssignment() {
+    testVariableOperatorAssignment("x |= 42", BinaryOperator.OR);
+  }
+
+  private void testVariableOperatorAssignment(
+      final String pCode, final BinaryOperator pExpectedOperator) {
+    final Assignment assignmentExpression = parseExpression(Assignment.class, pCode);
+
+    final String expectedVariableName = "x";
+    final JSIdExpression variableId =
+        new JSIdExpression(
+            FileLocation.DUMMY, expectedVariableName, mock(JSSimpleDeclaration.class));
+
+    final JSExpression assignmentInitializerExpression =
+        new JSIntegerLiteralExpression(FileLocation.DUMMY, BigInteger.valueOf(42));
+    // expected CFA: entryNode -{x = x + 42}-> ()
+
+    builder.setExpressionAppendable(
+        (pBuilder, pExpression) ->
+            pExpression instanceof SimpleName ? variableId : assignmentInitializerExpression);
+
+    final JSIdExpression result =
+        (JSIdExpression) new AssignmentCFABuilder().append(builder, assignmentExpression);
+
+    Truth.assertThat(result).isEqualTo(variableId);
+    final JSStatementEdge assignmentEdge = (JSStatementEdge) entryNode.getLeavingEdge(0);
+    final JSAssignment variableAssignment = (JSAssignment) assignmentEdge.getStatement();
+    Truth.assertThat(variableAssignment.getLeftHandSide()).isEqualTo(variableId);
+    Truth.assertThat(variableAssignment.getRightHandSide())
+        .isEqualTo(
+            new JSBinaryExpression(
+                FileLocation.DUMMY,
+                variableId,
+                assignmentInitializerExpression,
+                pExpectedOperator));
     Truth.assertThat(assignmentEdge.getSuccessor().getNumLeavingEdges()).isEqualTo(0);
   }
 }
