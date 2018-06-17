b4854e3eee6f1d8527fbd706645372bdcba19654
maiermic <maiermic@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2018-06-17 23:09:11 +0000

    test switch statement without default and no break or return statements
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/javascript@28363 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

---
+++ test/programs/javascript/switch-without-default-and-always-fall-through_true_assert.js
@@ -0,0 +1,56 @@
+function __VERIFIER_error() {}
+
+function switchTest(value){
+  var result = 0;
+
+  switch(value) {
+    case 0:
+      result += 2;
+    case 1:
+      result += 4;
+    case 2:
+      result += 8;
+  }
+
+  return result;
+}
+
+if(!(switchTest(0) === 14)){
+  __VERIFIER_error();
+}
+
+if(!(switchTest(1) === 12)){
+  __VERIFIER_error();
+}
+
+if(!(switchTest(2) === 8)){
+  __VERIFIER_error();
+}
+
+if(!(switchTest(3) === 0)){
+  __VERIFIER_error();
+}
+
+if(!(switchTest(4) === 0)){
+  __VERIFIER_error();
+}
+
+if(!(switchTest(true) === 0)){
+  __VERIFIER_error();
+}
+
+if(!(switchTest(false) === 0)){
+  __VERIFIER_error();
+}
+
+if(!(switchTest(null) === 0)){
+  __VERIFIER_error();
+}
+
+if(!(switchTest(void 0) === 0)){
+  __VERIFIER_error();
+}
+
+if(!(switchTest('0') === 0)){
+  __VERIFIER_error();
+}
