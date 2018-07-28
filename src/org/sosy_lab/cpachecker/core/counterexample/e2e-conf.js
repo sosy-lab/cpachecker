dc23b5b6fc6b4945409b018dd9020781d4ed1f1e
nandanwar <nandanwar@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2018-07-28 09:56:24 +0000

    Added E2E testing setup and base test case
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/angularjs-dev@28839 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

---
+++ src/org/sosy_lab/cpachecker/core/counterexample/e2e-conf.js
@@ -0,0 +1,29 @@
+var fs = require('fs');
+
+fs.createReadStream('../../../../../../output/Counterexample.1.html').pipe(fs.createWriteStream('e2e-tests/Counterexample.1.html'));
+
+exports.config = {
+
+    // Capabilities to be passed to the webdriver instance.
+    capabilities: {
+        'browserName': 'chrome',
+        chromeOptions: {
+            args: ["allow-file-access-from-files", "disable-web-security", "allow-file-access", "--disable-gpu", "--window-size=800x600", ]
+        }
+    },
+
+    directConnect: true,
+
+    baseUrl: 'file:///',
+    // Framework to use. Jasmine is recommended.
+    framework: 'jasmine',
+
+    // Spec patterns are relative to the current working directory when
+    // protractor is called.
+    specs: ['e2e-tests/*_spec.js'],
+
+    // Options to be passed to Jasmine.
+    jasmineNodeOpts: {
+        defaultTimeoutInterval: 30000
+    }
+};
