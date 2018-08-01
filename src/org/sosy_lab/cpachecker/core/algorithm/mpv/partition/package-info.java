0444c9f902013971443b5e707e52e523ab6b3215
mordan <mordan@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2018-08-01 11:35:48 +0000

    Add implementation of basic partitioning operators
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/multi-property_verification@28896 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

---
+++ src/org/sosy_lab/cpachecker/core/algorithm/mpv/partition/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * This package contains representation of partitions for multi-property verification. Each
+ * partition contains properties and resources. In order to create partitions partitioning operators
+ * are used.
+ */
+package org.sosy_lab.cpachecker.core.algorithm.mpv.partition;
