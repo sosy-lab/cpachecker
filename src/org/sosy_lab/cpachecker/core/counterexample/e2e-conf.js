a5ed435e774affe13fad82757af84ca7d34c6765
nandanwar <nandanwar@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2018-08-04 02:47:38 +0000

    Added e2e tests for error-path sections
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/angularjs-dev@28919 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/core/counterexample/e2e-conf.js
+++ src/org/sosy_lab/cpachecker/core/counterexample/e2e-conf.js
@@ -2,6 +2,13 @@ var fs = require('fs');
 
 fs.createReadStream('../../../../../../output/Counterexample.1.html').pipe(fs.createWriteStream('e2e-tests/Counterexample.1.html'));
 
+var HtmlScreenshotReporter = require('protractor-jasmine2-screenshot-reporter');
+
+var reporter = new HtmlScreenshotReporter({
+    dest: 'e2e-tests-report/screenshots',
+    filename: 'e2e-tests-report.html'
+});
+
 exports.config = {
 
     // Capabilities to be passed to the webdriver instance.
@@ -23,11 +30,29 @@ exports.config = {
     specs: ['e2e-tests/*_spec.js'],
 
     // Options to be passed to Jasmine.
+
     jasmineNodeOpts: {
-        defaultTimeoutInterval: 30000
+        showColors: true,
+        includeStackTrace: true,
+        defaultTimeoutInterval: 1440000
     },
 
+    beforeLaunch: function () {
+        return new Promise(function (resolve) {
+            reporter.beforeLaunch(resolve);
+        });
+    },
+
+    // Assign the test reporter to each running instance
     onPrepare: function () {
+        jasmine.getEnv().addReporter(reporter);
         browser.manage().window().setSize(1600, 1000);
+    },
+
+    // Close the report after all tests finish
+    afterLaunch: function (exitCode) {
+        return new Promise(function (resolve) {
+            reporter.afterLaunch(resolve.bind(this, exitCode));
+        });
     }
 };
