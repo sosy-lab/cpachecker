e450a0091462fc7f0e32457d71c5cc2720df7b74
nandanwar <nandanwar@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2018-08-04 03:37:08 +0000

    Addition of end to end tests in gitlab-ci.yml
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/angularjs-dev@28920 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/core/counterexample/e2e-conf.js
+++ src/org/sosy_lab/cpachecker/core/counterexample/e2e-conf.js
@@ -15,7 +15,7 @@ exports.config = {
     capabilities: {
         'browserName': 'chrome',
         chromeOptions: {
-            args: ["allow-file-access-from-files", "disable-web-security", "allow-file-access", "--disable-gpu", "--window-size=800x600", ]
+            args: ["allow-file-access-from-files", "--headless", "disable-web-security", "allow-file-access", "--disable-gpu", "--window-size=800x600", ]
         }
     },
 
