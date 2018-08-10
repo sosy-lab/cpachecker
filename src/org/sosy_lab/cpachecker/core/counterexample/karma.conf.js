d57302bf207819a7a8b8788ce6b8516e959b6d8b
nandanwar <nandanwar@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2018-08-10 02:39:20 +0000

    Added Unit testing report generator using karma-html-reporter
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@29027 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/core/counterexample/karma.conf.js
+++ src/org/sosy_lab/cpachecker/core/counterexample/karma.conf.js
@@ -57,7 +57,31 @@ module.exports = function (config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['progress', 'html'],
+
+    htmlReporter: {
+      outputDir: 'Unit_testing_report', // where to put the reports 
+      templatePath: null, // set if you moved jasmine_template.html
+      focusOnFailures: true, // reports show failures on start
+      namedFiles: false, // name files instead of creating sub-directories
+      pageTitle: null, // page title for reports; browser info by default
+      urlFriendlyName: false, // simply replaces spaces with _ for files/dirs
+      reportName: 'ChromeHeadless', // report summary filename; browser info by default
+
+
+      // experimental
+      preserveDescribeNesting: false, // folded suites stay folded 
+      foldAll: false, // reports start folded (only with preserveDescribeNesting)
+    },
+
+    // add to plugins
+    plugins: [
+      // other plugins
+      'karma-html-reporter',
+      'karma-chrome-launcher',
+      'karma-phantomjs-launcher',
+      'karma-jasmine'
+    ],
 
     // web server port
     port: 9876,
@@ -74,7 +98,7 @@ module.exports = function (config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['PhantomJS'],
+    browsers: ['ChromeHeadless'],
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
