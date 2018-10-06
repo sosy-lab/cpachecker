668e7888305602d23239d38620f22818ed71904e
maiermic <maiermic@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2018-10-06 17:08:16 +0000

    only update indices if object is a variable
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/javascript@29410 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/util/predicates/pathformula/jstoformula/JSToFormulaConverter.java
+++ src/org/sosy_lab/cpachecker/util/predicates/pathformula/jstoformula/JSToFormulaConverter.java
@@ -926,7 +926,11 @@ public class JSToFormulaConverter {
         afmgr.store(getObjectFields(objectId, pSsa), getStringFormula(fieldName), field),
         pSsa,
         pConstraints);
-    updateIndicesOfOtherScopeVariables(objectDeclaration, pLhsFunction, pSsa, pConstraints);
+    // only update indices if object is a variable (not a temporary variable introduced by
+    // getObjectDeclarationOfFieldAccess)
+    if (pLhs.getObject() instanceof JSIdExpression) {
+      updateIndicesOfOtherScopeVariables(objectDeclaration, pLhsFunction, pSsa, pConstraints);
+    }
     return makeAssignment(field, pRhsValue);
   }
 
