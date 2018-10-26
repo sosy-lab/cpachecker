1107d5333322e262663ebd55ddbcfa8306144cc6
mcjakobs <mcjakobs@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2018-10-26 07:03:33 +0000

    bug fix to avoid that constants, which are leafs are tried to cast to LeafExpression (causes an exception); for constants we do not need to check for unknown variables
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@29587 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/cpa/automaton/AutomatonGraphmlParser.java
+++ src/org/sosy_lab/cpachecker/cpa/automaton/AutomatonGraphmlParser.java
@@ -734,7 +734,9 @@ public class AutomatonGraphmlParser {
   private ExpressionTree<AExpression> logAndRemoveUnknown(ExpressionTree<AExpression> invariant) {
     FluentIterable<AExpression> expressions =
         FluentIterable.from(ExpressionTrees.traverseRecursively(invariant))
-            .filter(ExpressionTrees::isLeaf)
+            .filter(
+                Predicates.and(
+                    ExpressionTrees::isLeaf, Predicates.not(ExpressionTrees::isConstant)))
             .transform(leaf -> ((LeafExpression<AExpression>) leaf).getExpression());
     Multimap<AExpression, AIdExpression> invalid = LinkedHashMultimap.create();
     for (AExpression assumption : expressions) {
