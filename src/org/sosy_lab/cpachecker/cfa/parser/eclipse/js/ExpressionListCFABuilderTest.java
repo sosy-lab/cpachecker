45ebfa07550f4c5de280c1c04e22f8c0b069310f
maiermic <maiermic@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2018-10-28 16:09:37 +0000

    test second expression has side effect
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/javascript@29609 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/cfa/parser/eclipse/js/ExpressionListCFABuilderTest.java
+++ src/org/sosy_lab/cpachecker/cfa/parser/eclipse/js/ExpressionListCFABuilderTest.java
@@ -30,11 +30,15 @@ import com.google.common.truth.Truth;
 import java.util.List;
 import org.eclipse.wst.jsdt.core.dom.Expression;
 import org.eclipse.wst.jsdt.core.dom.PrefixExpression;
+import org.eclipse.wst.jsdt.core.dom.SimpleName;
 import org.junit.Test;
 import org.sosy_lab.common.configuration.InvalidConfigurationException;
 import org.sosy_lab.cpachecker.cfa.ast.js.JSExpression;
 import org.sosy_lab.cpachecker.cfa.ast.js.JSIdExpression;
+import org.sosy_lab.cpachecker.cfa.ast.js.JSInitializerExpression;
+import org.sosy_lab.cpachecker.cfa.ast.js.JSVariableDeclaration;
 import org.sosy_lab.cpachecker.cfa.model.CFAEdge;
+import org.sosy_lab.cpachecker.cfa.model.js.JSDeclarationEdge;
 import org.sosy_lab.cpachecker.util.test.ReturnValueCaptor;
 
 public class ExpressionListCFABuilderTest extends CFABuilderTestBase {
@@ -73,5 +77,48 @@ public class ExpressionListCFABuilderTest extends CFABuilderTestBase {
     Truth.assertThat(entryNode.getNumLeavingEdges()).isEqualTo(1);
     Truth.assertThat(entryNode.getLeavingEdge(0)).isEqualTo(sideEffectEdgeCaptor.getReturnValue(0));
     Truth.assertThat(entryNode.getLeavingEdge(0).getSuccessor()).isEqualTo(builder.getExitNode());
+  }
+
+  @Test
+  public final void testSecondExpressionHasSideEffect() {
+    // single expression should be appended without temporary assignment
+    final Expression first = parseExpression(SimpleName.class, "x");
+    final Expression second = parseExpression(PrefixExpression.class, "++x");
+    final JSIdExpression x = mock(JSIdExpression.class, "x");
+    builder.setExpressionAppendable(
+        (pBuilder, pExpression) -> {
+          if (pExpression == first) {
+            return x;
+          } else if (pExpression == second) {
+            appendSideEffectEdge(pBuilder);
+            return x;
+          } else {
+            throw new RuntimeException("unexpected expression");
+          }
+        });
+
+    final List<JSExpression> result =
+        new ExpressionListCFABuilder().append(builder, ImmutableList.of(first, second));
+
+    // TODO check result
+    //    Truth.assertThat(result).isEqualTo(ImmutableList.of(x, x));
+
+    Truth.assertThat(sideEffectEdgeCaptor.getTimesCalled()).isGreaterThan(0);
+
+    Truth.assertThat(entryNode.getNumLeavingEdges()).isEqualTo(1);
+    Truth.assertThat(entryNode.getLeavingEdge(0)).isInstanceOf(JSDeclarationEdge.class);
+    final JSDeclarationEdge tmpAssignmentEdge = (JSDeclarationEdge) entryNode.getLeavingEdge(0);
+    final JSVariableDeclaration tmpVariableDeclaration =
+        (JSVariableDeclaration) tmpAssignmentEdge.getDeclaration();
+    Truth.assertThat(
+            ((JSInitializerExpression) tmpVariableDeclaration.getInitializer()).getExpression())
+        .isEqualTo(x);
+
+    Truth.assertThat(tmpAssignmentEdge.getSuccessor().getNumLeavingEdges()).isEqualTo(1);
+    final CFAEdge sideEffectEdge = tmpAssignmentEdge.getSuccessor().getLeavingEdge(0);
+    Truth.assertThat(sideEffectEdge).isEqualTo(sideEffectEdgeCaptor.getReturnValue(0));
+    // no temporary variable assignment is required for the last expression.
+    // That's why there should be no further (declaration) edge.
+    Truth.assertThat(sideEffectEdge.getSuccessor()).isEqualTo(builder.getExitNode());
   }
 }
