773f5a940592169d2ebb86068f97b592f8d3849d
lemberge <lemberge@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2018-11-12 20:36:17 +0000

    Consider unresolved casts in counterexample export
    Since r29750 (217b329727), a value cast can return an unknown value
    (in favor of being unsound).
    Let the assumption-to-edge-allocator used in the counterexample export
    handle this, as well.
    
    Led to NPEs before.
    
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@29752 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/core/counterexample/AssumptionToEdgeAllocator.java
+++ src/org/sosy_lab/cpachecker/core/counterexample/AssumptionToEdgeAllocator.java
@@ -1586,15 +1586,18 @@ public class AssumptionToEdgeAllocator {
             logger instanceof LogManagerWithoutDuplicates
                 ? (LogManagerWithoutDuplicates) logger
                 : new LogManagerWithoutDuplicates(logger);
-        Number number =
+        Value castValue =
             AbstractExpressionValueVisitor.castCValue(
-                    new NumericValue(pIntegerValue),
-                    pType,
-                    machineModel,
-                    logManager,
-                    FileLocation.DUMMY)
-                .asNumericValue()
-                .getNumber();
+                new NumericValue(pIntegerValue),
+                pType,
+                machineModel,
+                logManager,
+                FileLocation.DUMMY);
+        if (castValue.isUnknown()) {
+          return UnknownValueLiteral.getInstance();
+        }
+
+        Number number = castValue.asNumericValue().getNumber();
         final BigInteger valueAsBigInt;
         if (number instanceof BigInteger) {
           valueAsBigInt = (BigInteger) number;
