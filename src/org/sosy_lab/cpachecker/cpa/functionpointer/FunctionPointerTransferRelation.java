c63ef851105f833a9b6c6429f8ea33022510f520
Karlheinz Friedberger <kfriedberger@gmail.com>
2018-11-23 16:01:01 +0000

    FunctionPointerCPA: add option to allow to change strictness of parameter-handling.
    The default case is similar to the behaviour before r29814 and only prints a warning.
    The strict case throws an exception.
    
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@29815 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/cpa/functionpointer/FunctionPointerTransferRelation.java
+++ src/org/sosy_lab/cpachecker/cpa/functionpointer/FunctionPointerTransferRelation.java
@@ -102,6 +102,13 @@ class FunctionPointerTransferRelation extends SingleEdgeTransferRelation {
   @Option(secure=true, description="When an unknown function pointer is called, do not assume all functions as possible targets and instead call no function (this is unsound).")
   private boolean ignoreUnknownFunctionPointerCalls = false;
 
+  @Option(
+      secure = true,
+      description =
+          "When a function pointer is given as parameter, assume that nothing else uses "
+              + "it any further. Otherwise we throw an exception.")
+  private boolean allowOverridingFunctionPointerParameters = true;
+
   private final LogManagerWithoutDuplicates logger;
 
   FunctionPointerTransferRelation(LogManager pLogger, Configuration config) throws InvalidConfigurationException {
@@ -427,7 +434,12 @@ class FunctionPointerTransferRelation extends SingleEdgeTransferRelation {
       if (target != pNewState.getTarget(paramName)) {
         // we abort the analysis, because the old function pointer assignment
         // will be lost when returning from the called function.
-        throw new UnsupportedCodeException("found recursion due to parameter collision", callEdge);
+        if (allowOverridingFunctionPointerParameters) {
+          logger.log(Level.WARNING, "found recursion due to parameter collision", callEdge);
+        } else {
+          throw new UnsupportedCodeException(
+              "found recursion due to parameter collision", callEdge);
+        }
       }
 
       pNewState.setTarget(paramName, target);
