89d195d776960bf58de3feb4c54c137449981aa2
Karlheinz Friedberger <kfriedberger@gmail.com>
2018-11-25 20:23:17 +0000

    FunctionPointerCPA: refactoring, extract nested class into separate file.
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@29822 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/cpa/functionpointer/FunctionPointerTransferRelation.java
+++ src/org/sosy_lab/cpachecker/cpa/functionpointer/FunctionPointerTransferRelation.java
@@ -2,7 +2,7 @@
  *  CPAchecker is a tool for configurable software verification.
  *  This file is part of CPAchecker.
  *
- *  Copyright (C) 2007-2014  Dirk Beyer
+ *  Copyright (C) 2007-2018  Dirk Beyer
  *  All rights reserved.
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
@@ -24,6 +24,7 @@
 package org.sosy_lab.cpachecker.cpa.functionpointer;
 
 import com.google.common.base.Optional;
+import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableSet;
 import java.util.Collection;
 import java.util.List;
@@ -39,32 +40,22 @@ import org.sosy_lab.cpachecker.cfa.ast.c.CAssignment;
 import org.sosy_lab.cpachecker.cfa.ast.c.CBinaryExpression;
 import org.sosy_lab.cpachecker.cfa.ast.c.CBinaryExpression.BinaryOperator;
 import org.sosy_lab.cpachecker.cfa.ast.c.CCastExpression;
-import org.sosy_lab.cpachecker.cfa.ast.c.CCharLiteralExpression;
-import org.sosy_lab.cpachecker.cfa.ast.c.CComplexCastExpression;
 import org.sosy_lab.cpachecker.cfa.ast.c.CExpression;
 import org.sosy_lab.cpachecker.cfa.ast.c.CExpressionStatement;
 import org.sosy_lab.cpachecker.cfa.ast.c.CFieldReference;
-import org.sosy_lab.cpachecker.cfa.ast.c.CFloatLiteralExpression;
 import org.sosy_lab.cpachecker.cfa.ast.c.CFunctionCall;
 import org.sosy_lab.cpachecker.cfa.ast.c.CFunctionCallAssignmentStatement;
 import org.sosy_lab.cpachecker.cfa.ast.c.CFunctionCallExpression;
 import org.sosy_lab.cpachecker.cfa.ast.c.CFunctionCallStatement;
-import org.sosy_lab.cpachecker.cfa.ast.c.CFunctionDeclaration;
 import org.sosy_lab.cpachecker.cfa.ast.c.CIdExpression;
-import org.sosy_lab.cpachecker.cfa.ast.c.CImaginaryLiteralExpression;
 import org.sosy_lab.cpachecker.cfa.ast.c.CInitializer;
 import org.sosy_lab.cpachecker.cfa.ast.c.CInitializerExpression;
 import org.sosy_lab.cpachecker.cfa.ast.c.CIntegerLiteralExpression;
 import org.sosy_lab.cpachecker.cfa.ast.c.CParameterDeclaration;
 import org.sosy_lab.cpachecker.cfa.ast.c.CPointerExpression;
 import org.sosy_lab.cpachecker.cfa.ast.c.CRightHandSide;
-import org.sosy_lab.cpachecker.cfa.ast.c.CRightHandSideVisitor;
 import org.sosy_lab.cpachecker.cfa.ast.c.CStatement;
-import org.sosy_lab.cpachecker.cfa.ast.c.CStringLiteralExpression;
-import org.sosy_lab.cpachecker.cfa.ast.c.CUnaryExpression;
-import org.sosy_lab.cpachecker.cfa.ast.c.CUnaryExpression.UnaryOperator;
 import org.sosy_lab.cpachecker.cfa.ast.c.CVariableDeclaration;
-import org.sosy_lab.cpachecker.cfa.ast.c.DefaultCExpressionVisitor;
 import org.sosy_lab.cpachecker.cfa.model.CFAEdge;
 import org.sosy_lab.cpachecker.cfa.model.CFAEdgeType;
 import org.sosy_lab.cpachecker.cfa.model.c.CAssumeEdge;
@@ -76,7 +67,6 @@ import org.sosy_lab.cpachecker.cfa.model.c.CFunctionSummaryEdge;
 import org.sosy_lab.cpachecker.cfa.model.c.CReturnStatementEdge;
 import org.sosy_lab.cpachecker.cfa.model.c.CStatementEdge;
 import org.sosy_lab.cpachecker.cfa.types.c.CFunctionType;
-import org.sosy_lab.cpachecker.cfa.types.c.CPointerType;
 import org.sosy_lab.cpachecker.core.defaults.SingleEdgeTransferRelation;
 import org.sosy_lab.cpachecker.core.interfaces.AbstractState;
 import org.sosy_lab.cpachecker.core.interfaces.Precision;
@@ -525,120 +515,11 @@ class FunctionPointerTransferRelation extends SingleEdgeTransferRelation {
     return exp.accept(new ExpressionValueVisitor(element, invalidFunctionPointerTarget));
   }
 
-  private static class ExpressionValueVisitor
-      extends DefaultCExpressionVisitor<FunctionPointerTarget, UnrecognizedCodeException>
-      implements CRightHandSideVisitor<FunctionPointerTarget, UnrecognizedCodeException> {
-
-    private final FunctionPointerState.Builder state;
-    private final FunctionPointerTarget targetForInvalidPointers;
-
-    private ExpressionValueVisitor(FunctionPointerState.Builder pElement, FunctionPointerTarget pTargetForInvalidPointers) {
-      state = pElement;
-      targetForInvalidPointers = pTargetForInvalidPointers;
-    }
-
-    @Override
-    public FunctionPointerTarget visit(CArraySubscriptExpression pE)
-        throws UnrecognizedCodeException {
-      if (pE.getSubscriptExpression() instanceof CIntegerLiteralExpression
-          && pE.getArrayExpression() instanceof CIdExpression) {
-
-        return state.getTarget(arrayElementVariable(pE));
-      }
-      return super.visit(pE);
-    }
-
-    @Override
-    public FunctionPointerTarget visit(CUnaryExpression pE) {
-      if ((pE.getOperator() == UnaryOperator.AMPER) && (pE.getOperand() instanceof CIdExpression)) {
-        return extractFunctionId((CIdExpression)pE.getOperand());
-      }
-      return visitDefault(pE);
-    }
-
-    @Override
-    public FunctionPointerTarget visit(CPointerExpression pE) {
-      if (pE.getOperand() instanceof CIdExpression) {
-        return extractFunctionId((CIdExpression)pE.getOperand());
-      }
-      return visitDefault(pE);
-    }
-
-    private FunctionPointerTarget extractFunctionId(CIdExpression operand) {
-      if ( (operand.getDeclaration()!=null && operand.getDeclaration().getType() instanceof CFunctionType)
-        || (operand.getExpressionType() instanceof CFunctionType)) {
-        return new NamedFunctionTarget(operand.getName());
-      }
-      if (operand.getExpressionType() instanceof CPointerType) {
-        CPointerType t = (CPointerType)operand.getExpressionType();
-        if (t.getType() instanceof CFunctionType) {
-          return state.getTarget(operand.getDeclaration().getQualifiedName());
-        }
-      }
-      return visitDefault(operand);
-    }
-
-    @Override
-    public FunctionPointerTarget visit(CIdExpression pE) {
-      if (pE.getDeclaration() instanceof CFunctionDeclaration
-          || pE.getExpressionType() instanceof CFunctionType) {
-        return new NamedFunctionTarget(pE.getName());
-      }
-
-      return state.getTarget(pE.getDeclaration().getQualifiedName());
-    }
-
-    @Override
-    public FunctionPointerTarget visit(CCastExpression pE) throws UnrecognizedCodeException {
-      return pE.getOperand().accept(this);
-    }
-
-    @Override
-    public FunctionPointerTarget visit(CComplexCastExpression pE) throws UnrecognizedCodeException {
-      // evaluation of complex numbers is not supported by now
-      return UnknownTarget.getInstance();
-    }
-
-    @Override
-    protected FunctionPointerTarget visitDefault(CExpression pExp) {
-      return UnknownTarget.getInstance();
-    }
-
-    @Override
-    public FunctionPointerTarget visit(CFunctionCallExpression pIastFunctionCallExpression) {
-      return UnknownTarget.getInstance();
-    }
-
-    @Override
-    public FunctionPointerTarget visit(CCharLiteralExpression pE) {
-      return targetForInvalidPointers;
-    }
-
-    @Override
-    public FunctionPointerTarget visit(CFloatLiteralExpression pE) {
-      return targetForInvalidPointers;
-    }
-
-    @Override
-    public FunctionPointerTarget visit(CIntegerLiteralExpression pE) {
-      return targetForInvalidPointers;
-    }
-
-    @Override
-    public FunctionPointerTarget visit(CStringLiteralExpression pE) {
-      return targetForInvalidPointers;
-    }
-
-    @Override
-    public FunctionPointerTarget visit(CImaginaryLiteralExpression pE) {
-      return targetForInvalidPointers;
-    }
-  }
-
-  private static String arrayElementVariable(CArraySubscriptExpression exp) {
-    assert exp.getSubscriptExpression() instanceof CIntegerLiteralExpression;
-    String name = ((CIdExpression)exp.getArrayExpression()).getDeclaration().getQualifiedName();
-    name += "[" + exp.getSubscriptExpression().toASTString() + "]";
-    return name;
+  static String arrayElementVariable(CArraySubscriptExpression exp) {
+    Preconditions.checkArgument(exp.getSubscriptExpression() instanceof CIntegerLiteralExpression);
+    return ((CIdExpression) exp.getArrayExpression()).getDeclaration().getQualifiedName()
+        + "["
+        + exp.getSubscriptExpression().toASTString()
+        + "]";
   }
 }
