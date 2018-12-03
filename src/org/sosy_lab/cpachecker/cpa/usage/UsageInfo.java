c6eb09ab1919fc8e59213d033df8ae25b749d0ea
andrianov <andrianov@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2018-12-03 11:02:43 +0000

    Fix a bug introduced in 29933
    Previous commit contains changes, which are correct in the branch.
    Remove the line, which triggers problems.
    
    
    
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@29934 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/cpa/usage/UsageInfo.java
+++ src/org/sosy_lab/cpachecker/cpa/usage/UsageInfo.java
@@ -102,7 +102,6 @@ public class UsageInfo implements Comparable<UsageInfo> {
                 AbstractStates.extractLocation(state),
                 (SingleIdentifier) ident,
                 states.transform(CompatibleState::prepareToStore).toList());
-        result.core.keyState = state;
         return result;
       }
     }
