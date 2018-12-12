baf21886e6826590a418412d1824e08831bcc994
andrianov <andrianov@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2018-12-12 10:47:12 +0000

    Fix a bug with equality of lock states
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/CPALockator-combat-mode@30047 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/cpa/usage/refinement/ReducedPathFilter.java
+++ src/org/sosy_lab/cpachecker/cpa/usage/refinement/ReducedPathFilter.java
@@ -19,10 +19,18 @@
  */
 package org.sosy_lab.cpachecker.cpa.usage.refinement;
 
+import static com.google.common.collect.FluentIterable.from;
+
+import com.google.common.base.Joiner;
 import com.google.common.collect.Iterables;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import org.sosy_lab.cpachecker.cfa.model.BlankEdge;
 import org.sosy_lab.cpachecker.cfa.model.CFAEdge;
+import org.sosy_lab.cpachecker.cfa.model.FunctionReturnEdge;
+import org.sosy_lab.cpachecker.cfa.model.c.CDeclarationEdge;
+import org.sosy_lab.cpachecker.cfa.model.c.CFunctionCallEdge;
 import org.sosy_lab.cpachecker.core.defaults.SingletonPrecision;
 import org.sosy_lab.cpachecker.core.interfaces.AbstractState;
 import org.sosy_lab.cpachecker.cpa.arg.ARGState;
@@ -52,7 +60,10 @@ public class ReducedPathFilter extends GenericSinglePathRefiner {
     // We cannot use last arg state, as it is not expanded!
     // LockState in Usage is correct
     AbstractLockState lastLockState = pPath.getUsageInfo().getLockState();
-    List<CFAEdge> edges = pPath.getInnerEdges();
+    List<CFAEdge> edges =
+        from(pPath.getInnerEdges())
+            .filter(e -> !(e instanceof CDeclarationEdge || e instanceof BlankEdge))
+            .toList();
 
     AbstractLockState currentState = initialLockState;
 
@@ -63,11 +74,34 @@ public class ReducedPathFilter extends GenericSinglePathRefiner {
 
       currentState = (AbstractLockState) Iterables.getOnlyElement(successors);
     }
-    if (currentState.equals(lastLockState)) {
+    // Not equals as it checks also previous state
+    if (currentState.compareTo(lastLockState) == 0) {
       return RefinementResult.createTrue();
     } else {
       return RefinementResult.createFalse();
     }
+  }
+
+  // For debugging
+  @SuppressWarnings("unused")
+  private String getStack(ExtendedARGPath pPath) {
+    List<CFAEdge> edges =
+        from(pPath.getInnerEdges())
+            .filter(e -> e instanceof CFunctionCallEdge || e instanceof FunctionReturnEdge)
+            .toList();
+
+    List<CFunctionCallEdge> filteredEdges = new ArrayList<>();
+
+    for (CFAEdge edge : edges) {
+      if (edge instanceof CFunctionCallEdge) {
+        filteredEdges.add((CFunctionCallEdge) edge);
+      } else if (edge instanceof FunctionReturnEdge) {
+        filteredEdges.remove(filteredEdges.size() - 1);
+      }
+    }
+    return
+        from(filteredEdges).transform(e -> e.getSuccessor().getFunctionName())
+            .join(Joiner.on(" -> "));
   }
 
 }
