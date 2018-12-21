sosy-lab/cpachecker

commit 0f175c732a983767b85ac0d37063b7a2128561dc

CPA-w2t: Improve error handling
Fix IndexError when invoked without arguments,
fix TypeError if no specification given,
make '-spec' a required argument
and add additional checks.

Instead of an assertion, throw an exception that gets logged properly
if no valid specification is given, and
make "-witness" a required argument for nice error messages.

git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@30110 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

scripts/cpa_witness2test.py
@@ -190,12 +190,21 @@ def create_parser():
                         dest='specification_file',
                         type=str,
                         action='store',
-                        help='specification file'
+                        help='specification file',
+                        required=True
+                        )
+
+    parser.add_argument('-witness',
+                        dest='witness_file',
+                        type=str,
+                        action='store',
+                        help='witness file',
+                        required=True
                         )
 
     parser.add_argument("file",
                         type=str,
-                        nargs='?',
+                        action='store',
                         help="file to validate witness for"
                         )
 
@@ -204,9 +213,7 @@ def create_parser():
 
 def _parse_args(argv=sys.argv[1:]):
     parser = create_parser()
-    args = parser.parse_known_args(argv[:-1])[0]
-    args_file = parser.parse_args([argv[-1]])  # Parse the file name
-    args.file = args_file.file
+    args = parser.parse_known_args(argv)[0]
 
     return args
 
@@ -406,9 +413,14 @@ def get_spec(specification_file):
 
     :param str specification_file: specification file to read.
     :return List[str]: list of specification properties
+    :raise ValidationError: if no specification file given or it doesn't exist
     """
+
+    if not specification_file:
+        raise ValidationError("No specification file given.")
     if not os.path.isfile(specification_file):
-        logging.error("Specification file does not exist: %s" % specification_file)
+        raise ValidationError("Specification file does not exist: %s" % specification_file)
+
     with open(specification_file, 'r') as inp:
         content = inp.read().strip()
 
@@ -419,7 +431,8 @@ def get_spec(specification_file):
             if regex.search(content):
                 specification.append(spec)
 
-    assert specification, "No specification found for specification string: %s" % content
+    if not specification:
+        raise ValidationError("No SV-COMP specification found in " + specification_file)
     return specification
 
 
@@ -525,4 +538,5 @@ if __name__ == "__main__":
         run()
     except ValidationError as e:
         logging.error(e.msg)
-    print("Verification result: ERROR.")
+        print("Verification result: ERROR.")
+

commit 8672475f2b0cafc9eb912eb291c8df3d9e1e4bf0

Fix bug in CPA-w2t introduced in r30110
If a timelimit was provided, the wrong argument
was used as input file.

git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@30147 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

scripts/cpa_witness2test.py
@@ -191,7 +191,6 @@ def create_parser():
                         type=str,
                         action='store',
                         help='specification file',
-                        required=True
                         )
 
     parser.add_argument('-witness',
@@ -199,12 +198,11 @@ def create_parser():
                         type=str,
                         action='store',
                         help='witness file',
-                        required=True
                         )
 
     parser.add_argument("file",
                         type=str,
-                        action='store',
+                        nargs="?",
                         help="file to validate witness for"
                         )
 
@@ -213,7 +211,9 @@ def create_parser():
 
 def _parse_args(argv=sys.argv[1:]):
     parser = create_parser()
-    args = parser.parse_known_args(argv)[0]
+    args = parser.parse_known_args(argv[:-1])[0]
+    args_file = parser.parse_args([argv[-1]])  # Parse the file name
+    args.file = args_file.file
 
     return args
 