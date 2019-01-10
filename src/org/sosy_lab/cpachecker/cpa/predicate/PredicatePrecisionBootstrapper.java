9ab827b94fa953303aeed1e17bce2a87e46886b7
mwiesholler <mwiesholler@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2019-01-10 16:54:43 +0000

    Get invariants from witness to make predicates
    If desired the candidates from the available witness are extracted and
    predicates are created in the PredicatePrecisionBootstrapper
    
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/correctnessWitnessValidationUsingPredicateAnalysis@30286 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/cpa/predicate/PredicatePrecisionBootstrapper.java
+++ src/org/sosy_lab/cpachecker/cpa/predicate/PredicatePrecisionBootstrapper.java
@@ -23,13 +23,21 @@
  */
 package org.sosy_lab.cpachecker.cpa.predicate;
 
+import com.google.common.collect.ArrayListMultimap;
+import com.google.common.collect.HashMultimap;
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ListMultimap;
+import com.google.common.collect.Multimap;
+import com.google.common.collect.Sets;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
+import java.util.Set;
 import java.util.logging.Level;
+import org.sosy_lab.common.ShutdownManager;
+import org.sosy_lab.common.ShutdownNotifier;
 import org.sosy_lab.common.configuration.Configuration;
 import org.sosy_lab.common.configuration.FileOption;
 import org.sosy_lab.common.configuration.InvalidConfigurationException;
@@ -37,11 +45,21 @@ import org.sosy_lab.common.configuration.Option;
 import org.sosy_lab.common.configuration.Options;
 import org.sosy_lab.common.log.LogManager;
 import org.sosy_lab.cpachecker.cfa.CFA;
+import org.sosy_lab.cpachecker.cfa.model.CFANode;
+import org.sosy_lab.cpachecker.core.Specification;
+import org.sosy_lab.cpachecker.core.algorithm.bmc.candidateinvariants.CandidateInvariant;
+import org.sosy_lab.cpachecker.core.algorithm.bmc.candidateinvariants.ExpressionTreeLocationInvariant;
+import org.sosy_lab.cpachecker.core.algorithm.invariants.KInductionInvariantGenerator;
+import org.sosy_lab.cpachecker.core.algorithm.invariants.KInductionInvariantGenerator.KInductionInvariantGeneratorOptions;
 import org.sosy_lab.cpachecker.core.interfaces.Statistics;
 import org.sosy_lab.cpachecker.core.interfaces.StatisticsProvider;
+import org.sosy_lab.cpachecker.core.reachedset.ReachedSet;
 import org.sosy_lab.cpachecker.cpa.predicate.persistence.PredicateMapParser;
 import org.sosy_lab.cpachecker.cpa.predicate.persistence.PredicatePersistenceUtils.PredicateParsingFailedException;
+import org.sosy_lab.cpachecker.exceptions.CPAException;
 import org.sosy_lab.cpachecker.util.predicates.AbstractionManager;
+import org.sosy_lab.cpachecker.util.predicates.AbstractionPredicate;
+import org.sosy_lab.cpachecker.util.predicates.pathformula.PathFormulaManager;
 import org.sosy_lab.cpachecker.util.predicates.smt.FormulaManagerView;
 import org.sosy_lab.cpachecker.util.statistics.KeyValueStatistics;
 
@@ -56,6 +74,18 @@ public class PredicatePrecisionBootstrapper implements StatisticsProvider {
   @Option(secure=true, description="always check satisfiability at end of block, even if precision is empty")
   private boolean checkBlockFeasibility = false;
 
+  @Option(
+      secure = true,
+      name = "correctnessWitness.validation",
+      description = "extract candidates witness from witness if correctness witness is validated")
+  private boolean correctnessWitnessValidation = false;
+
+  @Option(
+      secure = true,
+      name = "correctnessWitness.atomPredicatesFromFormula",
+      description = "extract atoms from candidates in witness and create predicates")
+  private boolean atomPredicatesFromFormula = false;
+
   private final FormulaManagerView formulaManagerView;
   private final AbstractionManager abstractionManager;
 
@@ -63,16 +93,36 @@ public class PredicatePrecisionBootstrapper implements StatisticsProvider {
   private final LogManager logger;
   private final CFA cfa;
 
+  // new Klassenvariablen
+  private final Specification specification;
+  private final ShutdownNotifier shutdownNotifier;
+  private final PathFormulaManager pathFormulaManager;
+  private final PredicateAbstractionManager predicateAbstractionManager;
+
   private final KeyValueStatistics statistics = new KeyValueStatistics();
 
-  public PredicatePrecisionBootstrapper(Configuration config, LogManager logger, CFA cfa,
-      AbstractionManager abstractionManager, FormulaManagerView formulaManagerView) throws InvalidConfigurationException {
+  public PredicatePrecisionBootstrapper(
+      Configuration config,
+      LogManager logger,
+      CFA cfa,
+      AbstractionManager abstractionManager,
+      FormulaManagerView formulaManagerView,
+      Specification specification,
+      ShutdownNotifier shutDownNotifier,
+      PathFormulaManager pathFormulaManager,
+      PredicateAbstractionManager predicateAbstractionManager)
+      throws InvalidConfigurationException {
     this.config = config;
     this.logger = logger;
     this.cfa = cfa;
 
     this.abstractionManager = abstractionManager;
     this.formulaManagerView = formulaManagerView;
+
+    this.specification = specification;
+    this.shutdownNotifier = shutDownNotifier;
+    this.pathFormulaManager = pathFormulaManager;
+    this.predicateAbstractionManager = predicateAbstractionManager;
 
     config.inject(this);
   }
@@ -99,6 +149,58 @@ public class PredicatePrecisionBootstrapper implements StatisticsProvider {
         } catch (PredicateParsingFailedException e) {
           logger.logUserException(Level.WARNING, e, "Could not read predicate map");
         }
+      }
+    }
+
+    if (correctnessWitnessValidation) {
+      try {
+        ShutdownManager sdm = ShutdownManager.createWithParent(shutdownNotifier);
+        KInductionInvariantGeneratorOptions options = new KInductionInvariantGeneratorOptions();
+        config.inject(options);
+        final Set<CandidateInvariant> candidates = Sets.newLinkedHashSet();
+        final Multimap<String, CFANode> candidateGroupLocations = HashMultimap.create();
+        ReachedSet reachedSet =
+            KInductionInvariantGenerator.analyzeWitness(
+                config, specification, logger, cfa, sdm, options);
+        KInductionInvariantGenerator.extractCandidatesFromReachedSet(
+            sdm, candidates, candidateGroupLocations, reachedSet);
+        for (CandidateInvariant candidate : candidates) {
+
+          ListMultimap<CFANode, AbstractionPredicate> localPredicates = ArrayListMultimap.create();
+
+          // get atom predicates from candidate
+          // in getPredicatesForAtomsOf splitItpAtoms has to be true
+          if (atomPredicatesFromFormula) {
+            Collection<AbstractionPredicate> atomPredicates =
+                predicateAbstractionManager.getPredicatesForAtomsOf(
+                    candidate.getFormula(formulaManagerView, pathFormulaManager, null));
+            if (candidate instanceof ExpressionTreeLocationInvariant) {
+              ExpressionTreeLocationInvariant e = (ExpressionTreeLocationInvariant) candidate;
+              for (AbstractionPredicate atomPredicate : atomPredicates) {
+                localPredicates.put(e.getLocation(), atomPredicate);
+              }
+            }
+          }
+          // get predicates from candidate
+          else {
+            Set<AbstractionPredicate> predicate =
+                Collections.singleton(
+                    abstractionManager.makePredicate(
+                        candidate.getFormula(formulaManagerView, pathFormulaManager, null)));
+
+            if (candidate instanceof ExpressionTreeLocationInvariant) {
+              ExpressionTreeLocationInvariant e = (ExpressionTreeLocationInvariant) candidate;
+              localPredicates.put(e.getLocation(), predicate.iterator().next());
+              // result = result.addLocalPredicates(localPredicates.entries());
+            }
+          }
+
+          // add all predicates
+          result = result.addLocalPredicates(localPredicates.entries());
+        }
+      } catch (CPAException | InterruptedException e) {
+        logger.logUserException(
+            Level.WARNING, e, "Predicate from correctness witness file could not be computed");
       }
     }
 
