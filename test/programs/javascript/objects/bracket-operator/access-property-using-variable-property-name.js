293bd1450763f0367284941cc8db03a0ecca6925
maiermic <maiermic@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2019-01-13 16:22:15 +0000

    reverse expected verification result
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/javascript@30306 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- test/programs/javascript/objects/bracket-operator/access-property-using-variable-property-name.js
+++ test/programs/javascript/objects/bracket-operator/access-property-using-variable-property-name.js
@@ -4,6 +4,6 @@ var a = {};
 var b = {};
 var propertyName = 'b';
 a[propertyName] = b;
-if (a[propertyName] !== b) {
+if (a[propertyName] === b) {
   __VERIFIER_error();
 }
--- test/programs/javascript/objects/bracket-operator/single-field-assignment.js
+++ test/programs/javascript/objects/bracket-operator/single-field-assignment.js
@@ -3,6 +3,6 @@ function __VERIFIER_error() {}
 var a = {};
 var b = {};
 a['b'] = b;
-if (a['b'] !== b) {
+if (a['b'] === b) {
   __VERIFIER_error();
 }
