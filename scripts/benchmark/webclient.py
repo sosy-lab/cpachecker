sosy-lab/cpachecker

commit 49b1a5740e3bfcbacb70a9c7b54e5326098acb91

Change output to give the actual information
The previous commit r30343 would just have resulted in an output like
<Request [POST]>, now we give an overview of the actual request
parameters that were used.

git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@30355 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

scripts/benchmark/webclient_benchexec.py
@@ -183,7 +183,12 @@ def _submitRunsParallel(runSet, benchmark, output_handler):
 
             except (HTTPError, WebClientError) as e:
                 output_handler.set_error("VerifierCloud problem", runSet)
-                logging.warning('Could not submit run %s, got error: "%s" for request %s', run.identifier, e, e.request)
+                logging.warning('Could not submit run %s, got error: "%s" for request with the following params %s',
+                                run.identifier,
+                                e,
+                                #this are the parameters, truncate content in case it is very large:
+                                [(k,v[:50]) for (k,v) in e.request.files]
+                               )
                 return result_futures # stop submitting runs
 
             finally:

commit 93dca87df76bafc4782bbf3c71dfc902202cc93d

Fix bug for benchmark script introduced in r30355
A PreparedRequest does not have a attribute files any more, this
information is now in the field body. We now fail graciously should
that field not be present. I wish there would be an easy way to test.

git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@30361 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

scripts/benchmark/webclient.py
@@ -903,7 +903,6 @@ class WebInterface:
                 # HTTPError.request is automatically filled with response.request so no need to pass it.
                 # Also HTTPError extends IOError, so there is a constructor IOError(errno, strerror, filename)
                 raise requests.HTTPError(response.status_code, message, path, response=response)
-                message = message + '(path: %s)' % path
 
 def _open_output_log(output_path):
     log_file_path = output_path + "output.log"

scripts/benchmark/webclient_benchexec.py
@@ -183,11 +183,10 @@ def _submitRunsParallel(runSet, benchmark, output_handler):
 
             except (HTTPError, WebClientError) as e:
                 output_handler.set_error("VerifierCloud problem", runSet)
-                logging.warning('Could not submit run %s, got error: "%s" for request with the following params %s',
+                logging.warning('Could not submit run %s, got error: "%s" for request with the following body %s',
                                 run.identifier,
                                 e,
-                                #this are the parameters, truncate content in case it is very large:
-                                [(k,v[:50]) for (k,v) in e.request.files]
+                                getattr(e.request, 'body', None)[:200]]
                                )
                 return result_futures # stop submitting runs
 