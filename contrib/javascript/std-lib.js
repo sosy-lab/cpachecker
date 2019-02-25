23ba1c4c120368c319c7dfa02c2ff1d6ba103505
maiermic <maiermic@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2019-02-25 15:00:05 +0000

    add dummy function Date
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/javascript@30690 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- contrib/javascript/std-lib.js
+++ contrib/javascript/std-lib.js
@@ -2,6 +2,7 @@ function isNaN(value) {
   return value === undefined || value !== value;
 }
 
+// constructor call not supported yet
 function Number(value) {
   return +value;
 }
@@ -11,3 +12,9 @@ Number.POSITIVE_INFINITY = Infinity;
 Number.NEGATIVE_INFINITY = -Infinity;
 Number.MAX_VALUE = 1.7976931348623157E308;
 Number.MIN_VALUE = 4.9E-324;
+
+// constructor call not supported yet
+function Date() {
+  // dummy value
+  return "Mon Feb 25 2019 12:06:04 GMT+0100 (Central European Standard Time)";
+}
