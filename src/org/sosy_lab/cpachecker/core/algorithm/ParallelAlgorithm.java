sosy-lab/cpachecker

commit 9d3c35b1f748b02337eebfbcddca45b7cef4f5e8

Avoid accidental correctness witness generation
Before this fix it could happen (e.g. for k-Induction) that a
subanalysis generates a correctness witness despite the overall result
being FALSE. This is because a correctness witness is also generated
if the result is UNKNOWN, and results for all subanalyses of an
ParallelAlgorithm that are not the successful analysis are set to
UNKNOWN. This commit changes the latter to DONE, which will then in
return not trigger the correctness witness generation.
The result value DONE was probably not available when the code for the
subanalyses' result was written, it seems like a much better choice
here than using UNKNOWN.

git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@30717 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

src/org/sosy_lab/cpachecker/core/algorithm/ParallelAlgorithm.java
@@ -645,7 +645,7 @@ public class ParallelAlgorithm implements Algorithm, StatisticsProvider {
     public void writeOutputFiles(Result pResult, UnmodifiableReachedSet pReached) {
       StatisticsEntry successfullAnalysisStats = null;
       for (StatisticsEntry subStats : allAnalysesStats) {
-        if (isSuccessfulAnalysis(subStats.name)) {
+        if (isSuccessfulAnalysis(subStats)) {
           successfullAnalysisStats = subStats;
         } else {
           writeSubOutputFiles(pResult, subStats);
@@ -665,13 +665,14 @@ public class ParallelAlgorithm implements Algorithm, StatisticsProvider {
       }
     }
 
-    private boolean isSuccessfulAnalysis(String pAnalysisName) {
-      return successfulAnalysisName != null && successfulAnalysisName.equals(pAnalysisName);
+    private boolean isSuccessfulAnalysis(StatisticsEntry pStatEntry) {
+      return successfulAnalysisName != null && successfulAnalysisName.equals(pStatEntry.name);
     }
 
     private Result determineAnalysisResult(Result pResult, String pActualAnalysisName) {
       if (successfulAnalysisName != null && !successfulAnalysisName.equals(pActualAnalysisName)) {
-        return Result.UNKNOWN;
+        // Signal that the result is not important for the final verdict:
+        return Result.DONE;
       }
       return pResult;
     }

src/org/sosy_lab/cpachecker/cpa/arg/ARGStatistics.java
@@ -48,6 +48,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.EnumSet;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
@@ -378,7 +379,7 @@ public class ARGStatistics implements Statistics {
         ARGUtils.projectARG(rootState, ARGState::getChildren, ARGUtils.RELEVANT_STATE);
     Function<ARGState, Collection<ARGState>> relevantSuccessorFunction = Functions.forMap(relevantSuccessorRelation.asMap(), ImmutableSet.<ARGState>of());
 
-    if (proofWitness != null && pResult != Result.FALSE) {
+    if (proofWitness != null && EnumSet.of(Result.TRUE, Result.UNKNOWN).contains(pResult)) {
       try {
         Path witnessFile = adjustPathNameForPartitioning(rootState, proofWitness);
         Appender content = pAppendable -> argWitnessExporter.writeProofWitness(pAppendable, rootState, Predicates.alwaysTrue(),

commit c723968d1412f112879a4dbf829a3a35d3eafb9f

Fix broken corr. witness generation of k-induction
I broke this with r30717. With k-induction, the correctness witness
is generated by the invariant-generation analysis, which actually
has no way of knowing whether k-induction proved the program to be
correct or not, so it did just output a correctness witness EVERY
time. For verdict FALSE, this was then overwritten by the violation
witness (if both file names match). With r30717, I tried to avoid the
useless, expensive generation of a correctness witness in the FALSE
case. I did not realize that this meant that k-induction whould not
output a correctness witness in the TRUE case. Now this commit should
fix that. This still has a certain code smell, but a better solution
would probably require larger changes to the code base.

git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@30911 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

src/org/sosy_lab/cpachecker/core/algorithm/ParallelAlgorithm.java
@@ -671,8 +671,15 @@ public class ParallelAlgorithm implements Algorithm, StatisticsProvider {
 
     private Result determineAnalysisResult(Result pResult, String pActualAnalysisName) {
       if (successfulAnalysisName != null && !successfulAnalysisName.equals(pActualAnalysisName)) {
-        // Signal that the result is not important for the final verdict:
-        return Result.DONE;
+        if (pResult == Result.TRUE) {
+          // we need this to let the invariant analysis write a correctness witness if we use
+          // k-induction. TODO: find a better fix for this mess.
+          return Result.UNKNOWN;
+        } else {
+          // Signal that this analysis is not important for the final verdict:
+          // (especially, do NOT generate a correctness witness)
+          return Result.DONE;
+        }
       }
       return pResult;
     }