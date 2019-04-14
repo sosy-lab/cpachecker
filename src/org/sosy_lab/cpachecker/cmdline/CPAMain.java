69c90f1cb9756b9d8b7f2423ca6531cde8f27c33
lemberge <lemberge@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2019-04-14 16:09:27 +0000

    Auto-detect Java language through java.sourcepath and java.classpath
    Rename the method to better reflect its meaning (language
    is only auto-detected if not explicitly set),
    update/add unit tests for the new behavior,
    make unit test names more speaking
    and rebuild configuration builder at each test
    (in case other tests get added, we may always want a clean builder)
    
    Closes #554.
    
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@31008 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/cmdline/CPAMain.java
+++ src/org/sosy_lab/cpachecker/cmdline/CPAMain.java
@@ -23,6 +23,7 @@
  */
 package org.sosy_lab.cpachecker.cmdline;
 
+import static com.google.common.base.Preconditions.checkArgument;
 import static java.util.stream.Collectors.toList;
 import static org.sosy_lab.common.io.DuplicateOutputStream.mergeStreams;
 
@@ -144,7 +145,7 @@ public class CPAMain {
       dumpConfiguration(options, cpaConfig, logManager);
 
       // generate correct frontend based on file language
-      cpaConfig = extractFrontendfromFileending(options, cpaConfig, logManager);
+      cpaConfig = detectFrontendLanguageIfNecessary(options, cpaConfig, logManager);
 
       limits = ResourceLimitChecker.fromConfiguration(cpaConfig, logManager, shutdownManager);
       limits.start();
@@ -378,43 +379,23 @@ public class CPAMain {
           " Please specify a language directly with the option 'language=%s'.",
           Arrays.toString(Language.values()));
 
-  /** determine the frontend language based on the file endings of the given programs. */
+  /**
+   * Determines the frontend language based on the file endings of the given programs, if no
+   * language is given by the user. If a language is detected, it is set in the given {@link
+   * MainOptions} object and a new configuration for that language, based on the given
+   * configuration, is returned.
+   */
   @VisibleForTesting
-  protected static Configuration extractFrontendfromFileending(
+  static Configuration detectFrontendLanguageIfNecessary(
       MainOptions pOptions, Configuration pConfig, LogManager pLogManager)
       throws InvalidConfigurationException {
     if (pOptions.language == null) {
       // if language was not specified by option, we determine the best matching language
-      Language frontendLanguage = null;
-      for (String program : pOptions.programs) {
-        Language language;
-        String suffix = program.substring(program.lastIndexOf(".") + 1);
-        switch (suffix) {
-          case "ll":
-          case "bc":
-            language = Language.LLVM;
-            break;
-          case "java":
-            language = Language.JAVA;
-            break;
-          case "c":
-          case "i":
-          case "h":
-          default:
-            language = Language.C;
-            break;
-        }
-        Preconditions.checkNotNull(language);
-        if (frontendLanguage == null) { // first iteration
-          frontendLanguage = language;
-        }
-        if (frontendLanguage != language) { // further iterations: check for conflicting endings
-          throw new InvalidConfigurationException(
-              String.format(
-                      "Differing file formats detected: %s and %s files are declared for analysis.",
-                      frontendLanguage, language)
-                  + LANGUAGE_HINT);
-        }
+      Language frontendLanguage;
+      if (areJavaOptionsSet(pConfig)) {
+        frontendLanguage = Language.JAVA;
+      } else {
+        frontendLanguage = detectFrontendLanguageFromFileEndings(pOptions.programs);
       }
       Preconditions.checkNotNull(frontendLanguage);
       ConfigurationBuilder configBuilder = Configuration.builder();
@@ -426,6 +407,46 @@ public class CPAMain {
     }
     Preconditions.checkNotNull(pOptions.language);
     return pConfig;
+  }
+
+  @SuppressWarnings("deprecation") // checking the properties directly is more maintainable
+  private static boolean areJavaOptionsSet(Configuration pConfig) {
+    // Make sure to keep this synchronized with EclipseJavaParser
+    return pConfig.hasProperty("java.sourcepath") || pConfig.hasProperty("java.classpath");
+  }
+
+  private static Language detectFrontendLanguageFromFileEndings(ImmutableList<String> pPrograms)
+      throws InvalidConfigurationException {
+    checkArgument(!pPrograms.isEmpty(), "Empty list of programs");
+    Language frontendLanguage = null;
+    for (String program : pPrograms) {
+      Language language;
+      String suffix = program.substring(program.lastIndexOf(".") + 1);
+      switch (suffix) {
+        case "ll":
+        case "bc":
+          language = Language.LLVM;
+          break;
+        case "c":
+        case "i":
+        case "h":
+        default:
+          language = Language.C;
+          break;
+      }
+      Preconditions.checkNotNull(language);
+      if (frontendLanguage == null) { // first iteration
+        frontendLanguage = language;
+      }
+      if (frontendLanguage != language) { // further iterations: check for conflicting endings
+        throw new InvalidConfigurationException(
+            String.format(
+                    "Differing file formats detected: %s and %s files are declared for analysis.",
+                    frontendLanguage, language)
+                + LANGUAGE_HINT);
+      }
+    }
+    return frontendLanguage;
   }
 
   private static final ImmutableMap<Property, TestTargetType> TARGET_TYPES =
