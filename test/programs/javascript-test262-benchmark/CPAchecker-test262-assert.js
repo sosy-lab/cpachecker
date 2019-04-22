f40d21d71e829a4491e6724e1601f3b0dc4fc6e6
maiermic <maiermic@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2019-04-22 08:22:59 +0000

    use assert._isSameValue definition of test262 harness
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/javascript@31050 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- test/programs/javascript-test262-benchmark/CPAchecker-test262-assert.js
+++ test/programs/javascript-test262-benchmark/CPAchecker-test262-assert.js
@@ -6,8 +6,18 @@ function assert(condition, message) {
   }
 }
 
+function __VERIFIER_isSameValue(a, b) {
+  if (a === b) {
+    // Handle +/-0 vs. -/+0
+    return a !== 0 || 1 / a === 1 / b;
+  }
+
+  // Handle NaN vs. NaN
+  return a !== a && b !== b;
+}
+
 assert.sameValue = function sameValue(actual, expected) {
-  if (actual !== expected) {
+  if (!__VERIFIER_isSameValue(actual, expected)) {
     __VERIFIER_error();
   }
 };
