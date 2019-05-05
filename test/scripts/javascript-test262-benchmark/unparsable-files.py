d8b06b3a50ece61b673874b6fa28564bbe59a773
maiermic <maiermic@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2019-05-05 09:30:52 +0000

    count files with/without esid
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/javascript@31126 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- test/scripts/javascript-test262-benchmark/unparsable-files.py
+++ test/scripts/javascript-test262-benchmark/unparsable-files.py
@@ -9,6 +9,8 @@ def main():
     counter = 0
     es5_counter = 0
     es6_counter = 0
+    esid_counter = 0
+    no_esid_counter = 0
     meta_data_counter = 0
     for file in get_test262_test_dir().glob('language/**/*.js'):
         if file.name.endswith('_FIXTURE.js'):
@@ -26,10 +28,17 @@ def main():
                 es5_counter += 1
             elif 'es6id' in meta_data:
                 es6_counter += 1
+            elif 'esid' in meta_data:
+                esid_counter += 1
+            else:
+                no_esid_counter += 1
+                # eprint(file)
             # eprint('could not parse {} due to {}'.format(file, sys.exc_info()[0]))
     print('counter: {}'.format(counter))
     print('es5_counter: {}'.format(es5_counter))
     print('es6_counter: {}'.format(es6_counter))
+    print('esid_counter: {}'.format(esid_counter))
+    print('no_esid_counter: {}'.format(no_esid_counter))
     print('meta_data_counter: {}'.format(meta_data_counter))
 
 
