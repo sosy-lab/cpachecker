d321fdb8ceb02ad35477133cb3883f07f1fa8a71
mtoews <mtoews@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2019-07-26 10:43:14 +0000

    somemore unrecoqnizable characters
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/ifc@31582 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/cpa/ifcsecurity/combet/Resources.java
+++ src/org/sosy_lab/cpachecker/cpa/ifcsecurity/combet/Resources.java
@@ -1,10 +1,3 @@
-//
-// Diese Datei wurde mit der JavaTM Architecture for XML Binding(JAXB) Reference Implementation, v2.2.8-b130911.1802 generiert 
-// Siehe <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
-// Änderungen an dieser Datei gehen bei einer Neukompilierung des Quellschemas verloren. 
-// Generiert: 2018.04.20 um 12:34:41 PM CEST 
-//
-
 
 package org.sosy_lab.cpachecker.cpa.ifcsecurity.combet;
 
@@ -18,10 +11,12 @@ import javax.xml.bind.annotation.XmlType;
 
 
 /**
- * <p>Java-Klasse für anonymous complex type.
- * 
- * <p>Das folgende Schemafragment gibt den erwarteten Content an, der in dieser Klasse enthalten ist.
- * 
+ * <p>
+ * Java-Klasse for anonymous complex type.
+ *
+ * <p>
+ * Das folgende Schemafragment gibt den erwarteten Content an, der in dieser Klasse enthalten ist.
+ *
  * <pre>
  * &lt;complexType>
  *   &lt;complexContent>
@@ -33,8 +28,8 @@ import javax.xml.bind.annotation.XmlType;
  *   &lt;/complexContent>
  * &lt;/complexType>
  * </pre>
- * 
- * 
+ *
+ *
  */
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlType(name = "", propOrder = {
@@ -48,25 +43,25 @@ public class Resources {
 
     /**
      * Gets the value of the resource property.
-     * 
+     *
      * <p>
      * This accessor method returns a reference to the live list,
      * not a snapshot. Therefore any modification you make to the
      * returned list will be present inside the JAXB object.
      * This is why there is not a <CODE>set</CODE> method for the resource property.
-     * 
+     *
      * <p>
      * For example, to add a new item, do as follows:
      * <pre>
      *    getResource().add(newItem);
      * </pre>
-     * 
-     * 
+     *
+     *
      * <p>
      * Objects of the following type(s) are allowed in the list
      * {@link Resource }
-     * 
-     * 
+     *
+     *
      */
     public List<Resource> getResource() {
         if (resource == null) {
