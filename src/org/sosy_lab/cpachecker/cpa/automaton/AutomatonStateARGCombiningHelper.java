dbf164ecd99be10901e7f34f265529c62a34b750
mcjakobs <mcjakobs@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2019-08-22 12:21:38 +0000

    get rid of use of AutomatonCPA access from automaton state
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@31770 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/cpa/automaton/AutomatonStateARGCombiningHelper.java
+++ src/org/sosy_lab/cpachecker/cpa/automaton/AutomatonStateARGCombiningHelper.java
@@ -26,10 +26,10 @@ package org.sosy_lab.cpachecker.cpa.automaton;
 import com.google.common.base.Preconditions;
 import java.util.HashMap;
 import java.util.Map;
+import org.sosy_lab.common.log.LogManager;
 import org.sosy_lab.cpachecker.cfa.model.CFAEdge;
-import org.sosy_lab.cpachecker.core.defaults.SingletonPrecision;
-import org.sosy_lab.cpachecker.core.interfaces.AbstractState;
 import org.sosy_lab.cpachecker.core.interfaces.Property;
+import org.sosy_lab.cpachecker.cpa.automaton.AutomatonExpression.ResultValue;
 import org.sosy_lab.cpachecker.exceptions.CPAException;
 import org.sosy_lab.cpachecker.exceptions.CPATransferException;
 
@@ -92,19 +92,40 @@ public class AutomatonStateARGCombiningHelper {
     return nameToCPA.containsKey(pAutomatonName);
   }
 
-  public static boolean endsInAssumptionTrueState(final AutomatonState pPredecessor, final CFAEdge pEdge) {
+  public static boolean endsInAssumptionTrueState(
+      final AutomatonState pPredecessor, final CFAEdge pEdge, final LogManager pLogger) {
     Preconditions.checkNotNull(pPredecessor);
+    Preconditions.checkArgument(!pPredecessor.getInternalState().isNonDetState());
+
+    AutomatonExpressionArguments exprArgs =
+        new AutomatonExpressionArguments(
+            pPredecessor, pPredecessor.getVars(), null, pEdge, pLogger);
     try {
-      for (AbstractState successor : pPredecessor.getAutomatonCPA().getTransferRelation()
-          .getAbstractSuccessorsForEdge(pPredecessor, SingletonPrecision.getInstance(), pEdge)) {
-        if (!((AutomatonState) successor).getInternalStateName().equals("__TRUE")) {
+      for (AutomatonTransition transition : pPredecessor.getInternalState().getTransitions()) {
+        exprArgs.clearTransitionVariables();
+        ResultValue<Boolean> match;
+
+        match = transition.getTrigger().eval(exprArgs);
+        if (match.canNotEvaluate()) {
+          return false;
+        }
+
+        if (match.getValue()) {
+          if (transition.getFollowState().getName().equals("__TRUE")) {
+            ResultValue<Boolean> assertionsHold = transition.assertionsHold(exprArgs);
+            if (!assertionsHold.canNotEvaluate()
+                && assertionsHold.getValue()
+                && transition.canExecuteActionsOn(exprArgs)) {
+              return true;
+            }
+          }
           return false;
         }
       }
     } catch (CPATransferException e) {
       return false;
     }
-    return true;
-  }
 
+    return pPredecessor.getInternalState().getName().equals("__TRUE");
+  }
 }
