0105deb385376480750beac90824cc9b41f2345c
vasilyev <vasilyev@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2019-09-05 13:35:53 +0000

    SMGCPA: Fix filtering by zero size values
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/SMG-has-value-edges-invariant@31873 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/cpa/smg/graphs/SMGHasValueEdgeSet.java
+++ src/org/sosy_lab/cpachecker/cpa/smg/graphs/SMGHasValueEdgeSet.java
@@ -202,7 +202,7 @@ public class SMGHasValueEdgeSet implements SMGHasValueEdges {
             if (filterSize != null && newSize > filterSize) {
               newSize = filterSize;
             }
-            if (newSize > 0) {
+            if (newSize > 0 || (newSize == 0 && candidate.getSizeInBits() == 0)) {
               candidate = new SMGEdgeHasValue(newSize, filterOffset, candidate.getObject(), candidate.getValue());
             } else {
               continue;
