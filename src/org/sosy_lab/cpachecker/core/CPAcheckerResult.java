sosy-lab/cpachecker

commit 829e2b76d924456182d05f70b1e59110a49aa1d1

Make statistics object available for HTML report
We can extract other useful information from the statistics object
like information about the witness.

git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@31927 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

src/org/sosy_lab/cpachecker/core/CPAcheckerResult.java
@@ -64,17 +64,21 @@ public class CPAcheckerResult {
 
   private @Nullable Statistics proofGeneratorStats = null;
 
+  private @Nullable Specification specification = null;
+
   CPAcheckerResult(
       Result result,
       String violatedPropertyDescription,
       @Nullable ReachedSet reached,
       @Nullable CFA cfa,
-      @Nullable Statistics stats) {
+      @Nullable Statistics stats,
+      @Nullable Specification pSpecification) {
     this.violatedPropertyDescription = checkNotNull(violatedPropertyDescription);
     this.result = checkNotNull(result);
     this.reached = reached;
     this.cfa = cfa;
     this.stats = stats;
+    this.specification = pSpecification;
   }
 
   /**
@@ -154,5 +158,9 @@ public class CPAcheckerResult {
       default:
         throw new AssertionError(result);
     }
+  }
+
+  public Statistics getStatistics() {
+    return stats;
   }
 }

src/org/sosy_lab/cpachecker/cmdline/CPAMain.java
@@ -747,7 +747,8 @@ public class CPAMain {
 
     // export report
     if (mResult.getResult() != Result.NOT_YET_STARTED) {
-      reportGenerator.generate(mResult.getCfa(), mResult.getReached(), statistics.toString());
+      reportGenerator.generate(
+          mResult.getCfa(), mResult.getReached(), mResult.getStatistics(), statistics.toString());
     }
   }
 

src/org/sosy_lab/cpachecker/core/CPAchecker.java
@@ -339,6 +339,7 @@ public class CPAchecker {
     CFA cfa = null;
     Result result = Result.NOT_YET_STARTED;
     String violatedPropertyDescription = "";
+    Specification specification = null;
 
     final ShutdownRequestListener interruptThreadOnShutdown = interruptCurrentThreadOnShutdown();
     shutdownNotifier.register(interruptThreadOnShutdown);
@@ -360,7 +361,6 @@ public class CPAchecker {
         shutdownNotifier.shutdownIfNecessary();
 
         ConfigurableProgramAnalysis cpa;
-        Specification specification;
         stats.cpaCreationTime.start();
         try {
           specification =
@@ -466,7 +466,8 @@ public class CPAchecker {
       CPAs.closeIfPossible(algorithm, logger);
       shutdownNotifier.unregister(interruptThreadOnShutdown);
     }
-    return new CPAcheckerResult(result, violatedPropertyDescription, reached, cfa, stats);
+    return new CPAcheckerResult(
+        result, violatedPropertyDescription, reached, cfa, stats, specification);
   }
 
   private Path checkIfOneValidFile(List<String> fileDenotation)

src/org/sosy_lab/cpachecker/core/counterexample/ReportGenerator.java
@@ -78,6 +78,7 @@ import org.sosy_lab.cpachecker.cfa.model.FunctionEntryNode;
 import org.sosy_lab.cpachecker.cfa.model.FunctionExitNode;
 import org.sosy_lab.cpachecker.core.CPAchecker;
 import org.sosy_lab.cpachecker.core.interfaces.AbstractState;
+import org.sosy_lab.cpachecker.core.interfaces.Statistics;
 import org.sosy_lab.cpachecker.core.reachedset.UnmodifiableReachedSet;
 import org.sosy_lab.cpachecker.cpa.arg.ARGState;
 import org.sosy_lab.cpachecker.cpa.arg.ARGUtils;
@@ -145,9 +146,11 @@ public class ReportGenerator {
     producer = htmlEscaper().escape(CPAchecker.getVersion(pConfig));
   }
 
-  public void generate(CFA pCfa, UnmodifiableReachedSet pReached, String pStatistics) {
+  public void generate(
+      CFA pCfa, UnmodifiableReachedSet pReached, Statistics pStats, String pStatistics) {
     checkNotNull(pCfa);
     checkNotNull(pReached);
+    checkNotNull(pStats);
     checkNotNull(pStatistics);
 
     if (!generateReport || (reportFile == null && counterExampleFiles == null)) {

commit 96b137ddfb2645c0cec95ad4703d410969324b7a

Fix compiler warnings caused by r31927
git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@31935 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

src/org/sosy_lab/cpachecker/core/CPAcheckerResult.java
@@ -64,21 +64,17 @@ public class CPAcheckerResult {
 
   private @Nullable Statistics proofGeneratorStats = null;
 
-  private @Nullable Specification specification = null;
-
   CPAcheckerResult(
       Result result,
       String violatedPropertyDescription,
       @Nullable ReachedSet reached,
       @Nullable CFA cfa,
-      @Nullable Statistics stats,
-      @Nullable Specification pSpecification) {
+      @Nullable Statistics stats) {
     this.violatedPropertyDescription = checkNotNull(violatedPropertyDescription);
     this.result = checkNotNull(result);
     this.reached = reached;
     this.cfa = cfa;
     this.stats = stats;
-    this.specification = pSpecification;
   }
 
   /**