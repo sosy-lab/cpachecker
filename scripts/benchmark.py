9a9bd60df93ee88073d1ab6198c4923fa8951ad9
pwendler <pwendler@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2019-10-22 13:30:11 +0000

    Remove manipulation of PATH variable by benchmark.py
    Previously, some directory under lib/native/ was added to the PATH.
    This was added in 2015 (when BenchExec got separated from CPAchecker)
    to keep the previous behavior (that was added in 2011 in r3202 (070ebcdb))
    of searching for CBMC binaries in lib/native/ in order to simplify
    benchmarking of CBMC.
    
    However, this is bad:
    - Execution of CBMC by CPAchecker itself must work without manipulation
    of PATH, because otherwise it would work only during benchmarking, not
    during manual execution of CPAchecker.
    - When users of benchmark.py want to benchmark CBMC, we should not
    silently default to the (probably outdated) binary under lib/native/,
    but let the users provide their own CBMC version.
    
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@32157 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- scripts/benchmark.py
+++ scripts/benchmark.py
@@ -196,16 +196,4 @@ class Benchmark(benchexec.benchexec.BenchExec):
 
 
 if __name__ == "__main__":
-    # Add directory with binaries to path.
-    bin_dir = (
-        "lib/native/x86_64-linux"
-        if platform.machine() == "x86_64"
-        else "lib/native/x86-linux"
-        if platform.machine() == "i386"
-        else None
-    )
-    if bin_dir:
-        bin_dir = os.path.join(os.path.dirname(__file__), os.pardir, bin_dir)
-        os.environ["PATH"] += os.pathsep + bin_dir
-
     benchexec.benchexec.main(Benchmark())
