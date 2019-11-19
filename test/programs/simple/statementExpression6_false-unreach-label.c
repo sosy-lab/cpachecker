95ea6fb6f3cc91e4d927f76293882374f40c3588
nweise <nweise@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2019-11-19 00:19:18 +0000

    Simplify statement expression test program 6
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/585-statement-expression-in-expressionlist@32275 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- test/programs/simple/statementExpression6_false-unreach-label.c
+++ test/programs/simple/statementExpression6_false-unreach-label.c
@@ -1,11 +1,10 @@
-// macro variable shadowing
-#define maxint(a,b) ({int _a = (a), _b = (b); _a > _b ? _a : _b; })
-
 int main() {
     int _a = 1, _b = 2, c;
-    c = maxint (_a, _b);
+    // _a and _b inside the statement expression are initialized with themselves
+    // and not with _a and _b from the main function
+    c = ({int _a = _a, _b = _b; _a > _b ? _a : _b; });
     
-    // maxint does not necessarily return the max of _a and _b (but it can)
+    // the value of c is undefined, therefore the label should be reachable
     if (c != _a && c != _b) {
         ERROR: // reachable
             return 1;
