d8fd531b5acde9d54adee81da1028835576e3cb1
mkettl <mkettl@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2019-12-10 17:25:46 +0000

    Added test case for float128 and enhanced existing test case for int128
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@32394 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- test/programs/simple/128bit-computations_true-unreach-label.c
+++ test/programs/simple/128bit-computations_true-unreach-label.c
@@ -3,13 +3,26 @@ int main() {
   unsigned __int128 ui128;
   __int128_t i128_t;
   __uint128_t ui128_t;
-  __float128 f128;
 
   if (((__int128)2 << 100) <= 0) goto ERROR;
 
   __uint128_t j = (__uint128_t)0xFFFFFFFFFFFFFFFF + (__uint128_t)0x1000000000000000;
   if ((j >> 1) != 0x87FFFFFFFFFFFFFF) goto ERROR;
+
+  __uint128_t op1 = 0xFFFFFFFFFFFFFFFF;
+  __uint128_t op2 = 0x0000000011111111;
   
+  //binary operations
+  if((op1&op2) != op2) goto ERROR;
+  if((op1|op2) != op1) goto ERROR;
+  if((op1^op2) != 0xFFFFFFFFEEEEEEEE) goto ERROR;
+
+  //arithmetic operations
+  if((op1%(op2-1)) != 0xFF) goto ERROR;
+  if(op1 - op2 != (op1^op2)) goto ERROR;
+  //only true if op1%op2 == 0
+  if((op1 / op2) * op2 != op1) goto ERROR;
+
   return;
 ERROR:
   return 1;
