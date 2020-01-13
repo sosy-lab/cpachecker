35826f5686dff27ba7c67ab12306d0299dcde34d
Karlheinz Friedberger <kfriedberger@gmail.com>
2020-01-13 12:04:15 +0000

    SMG: add new option for configuring safe unknown functions.
    See #692 for reasons.
    
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@32561 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/cpa/smg/SMGBuiltins.java
+++ src/org/sosy_lab/cpachecker/cpa/smg/SMGBuiltins.java
@@ -922,10 +922,14 @@ public class SMGBuiltins {
       throws CPATransferException, AssertionError {
     switch (options.getHandleUnknownFunctions()) {
       case STRICT:
-        throw new CPATransferException(
-            "Unknown function '"
-                + calledFunctionName
-                + "' may be unsafe. See the cpa.smg.handleUnknownFunctions option.");
+        if (!options.getSafeUnknownFunctions().contains(calledFunctionName)) {
+          throw new CPATransferException(
+              String.format(
+                  "Unknown function '%s' may be unsafe. See the "
+                      + "cpa.smg.handleUnknownFunctions or cpa.smg.option.safeUnknownFunctions",
+                  calledFunctionName));
+        }
+        // $FALL-THROUGH$ // for safe functions
       case ASSUME_SAFE:
         return ImmutableList.of(SMGAddressValueAndState.of(pState));
       case ASSUME_EXTERNAL_ALLOCATED:
