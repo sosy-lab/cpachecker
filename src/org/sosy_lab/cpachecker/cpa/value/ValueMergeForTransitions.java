7b29b251638be9a0d987d61ad5eb20434ea3c6a5
andrianov <andrianov@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2020-02-06 16:16:07 +0000

    Implementation of merge for value
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/CPALockator-theory5@32713 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/cpa/value/ValueMergeForTransitions.java
+++ src/org/sosy_lab/cpachecker/cpa/value/ValueMergeForTransitions.java
@@ -24,6 +24,7 @@ import java.util.Map;
 import java.util.Set;
 import org.sosy_lab.common.collect.PathCopyingPersistentTreeMap;
 import org.sosy_lab.common.collect.PersistentMap;
+import org.sosy_lab.common.configuration.Options;
 import org.sosy_lab.cpachecker.cfa.types.Type;
 import org.sosy_lab.cpachecker.core.defaults.EmptyEdge;
 import org.sosy_lab.cpachecker.core.defaults.WrapperCFAEdge;
@@ -37,14 +38,13 @@ import org.sosy_lab.cpachecker.cpa.value.type.Value.UnknownValue;
 import org.sosy_lab.cpachecker.exceptions.CPAException;
 import org.sosy_lab.cpachecker.util.states.MemoryLocation;
 
+@Options(prefix = "cpa.value")
 public class ValueMergeForTransitions implements MergeOperator {
 
-  private final boolean abstractionMerge;
-  private final boolean joinOnlyEqualMems;
+  private final boolean joinStates;
 
-  ValueMergeForTransitions() {
-    abstractionMerge = true;
-    joinOnlyEqualMems = false;
+  ValueMergeForTransitions(boolean pFlag) {
+    joinStates = pFlag;
   }
 
   @Override
@@ -70,7 +70,7 @@ public class ValueMergeForTransitions implements MergeOperator {
         ValueAbstractEdge vEdge2 = (ValueAbstractEdge) edge2;
 
         ValueAnalysisState base;
-        if (abstractionMerge) {
+        if (joinStates) {
           if (!state1.hasEqualStates(state2)) {
             return pState2;
           } else {
@@ -91,21 +91,13 @@ public class ValueMergeForTransitions implements MergeOperator {
 
         for (MemoryLocation mem : jointMems) {
           if (values1.containsKey(mem) && !values2.containsKey(mem)) {
-            if (joinOnlyEqualMems) {
-              return pState2;
-            } else {
-              Type newType = values1.get(mem).getType();
-              newValues =
+            Type newType = values1.get(mem).getType();
+            newValues =
                   newValues.putAndCopy(mem, new ValueAndType(UnknownValue.getInstance(), newType));
-            }
           } else if (values2.containsKey(mem) && !values1.containsKey(mem)) {
-            if (joinOnlyEqualMems) {
-              return pState2;
-            } else {
-              Type newType = values2.get(mem).getType();
-              newValues =
+            Type newType = values2.get(mem).getType();
+            newValues =
                   newValues.putAndCopy(mem, new ValueAndType(UnknownValue.getInstance(), newType));
-            }
           } else if (values1.containsKey(mem) && values2.containsKey(mem)) {
             ValueAndType newType = join(values1.get(mem), values2.get(mem));
             newValues =
