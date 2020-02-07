44472c07585ffe6aa351626a0a850a326a2c5d4e
vasilyev <vasilyev@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2020-02-07 12:23:06 +0000

    SMGCPA: Add missed replacing symbolic values inside errorPredicate on replaceValue
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/SMGCPA-merge-hvEdges@32740 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/cpa/smg/graphs/SMG.java
+++ src/org/sosy_lab/cpachecker/cpa/smg/graphs/SMG.java
@@ -570,6 +570,7 @@ public class SMG implements UnmodifiableSMG {
 
     neq = neq.replaceValueAndCopy(fresh, old);
     pathPredicate.replace(fresh, old);
+    errorPredicate.replace(fresh, old);
 
     removeValue(old);
 
