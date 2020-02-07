b2735545e4ded175a50831b488d37c7280df1e22
vasilyev <vasilyev@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2020-02-07 12:21:47 +0000

    SMGCPA: Fix offsets from byte to bit
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/SMGCPA-merge-hvEdges@32733 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/cpa/smg/SMGStateTest.java
+++ src/org/sosy_lab/cpachecker/cpa/smg/SMGStateTest.java
@@ -113,11 +113,11 @@ public class SMGStateTest {
     SMGEdgeHasValue l4fn = new SMGEdgeHasValue(ptrSize, 0, l4, value10);
     SMGEdgeHasValue l5fn = new SMGEdgeHasValue(ptrSize, 0, l5, value5);
 
-    SMGEdgeHasValue l1fp = new SMGEdgeHasValue(ptrSize, 4, l1, value5);
-    SMGEdgeHasValue l2fp = new SMGEdgeHasValue(ptrSize, 4, l2, value6);
-    SMGEdgeHasValue l3fp = new SMGEdgeHasValue(ptrSize, 4, l3, value7);
-    SMGEdgeHasValue l4fp = new SMGEdgeHasValue(ptrSize, 4, l4, value8);
-    SMGEdgeHasValue l5fp = new SMGEdgeHasValue(ptrSize, 4, l5, value9);
+    SMGEdgeHasValue l1fp = new SMGEdgeHasValue(ptrSize, 32, l1, value5);
+    SMGEdgeHasValue l2fp = new SMGEdgeHasValue(ptrSize, 32, l2, value6);
+    SMGEdgeHasValue l3fp = new SMGEdgeHasValue(ptrSize, 32, l3, value7);
+    SMGEdgeHasValue l4fp = new SMGEdgeHasValue(ptrSize, 32, l4, value8);
+    SMGEdgeHasValue l5fp = new SMGEdgeHasValue(ptrSize, 32, l5, value9);
 
     SMGEdgePointsTo l1t = new SMGEdgePointsTo(value6, l1, 0);
     SMGEdgePointsTo l2t = new SMGEdgePointsTo(value7, l2, 0);
@@ -197,9 +197,9 @@ public class SMGStateTest {
     SMGValue value12 = SMGKnownSymValue.valueOf(12);
     SMGValue value13 = SMGKnownSymValue.valueOf(13);
 
-    SMGObject dll = new SMGDoublyLinkedList(96, 0, 0, 4, 0, 0);
+    SMGObject dll = new SMGDoublyLinkedList(96, 0, 0, 32, 0, 0);
     SMGEdgeHasValue dllN = new SMGEdgeHasValue(ptrSize, 0, dll, value5);
-    SMGEdgeHasValue dllP = new SMGEdgeHasValue(ptrSize, 4, dll, value5);
+    SMGEdgeHasValue dllP = new SMGEdgeHasValue(ptrSize, 32, dll, value5);
     heap.addHeapObject(dll);
     heap.setValidity(dll, true);
     heap.addValue(value5);
