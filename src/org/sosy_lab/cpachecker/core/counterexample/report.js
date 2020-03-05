sosy-lab/cpachecker

commit caf595c7a0411c928b0ed352af60eec2b12d533d

Add witness as view of the ARG to the HTML report
The witness is created by taking the ARG and merging some of the nodes
together, e.g. those at the same CFA location. By adding the witness
as special view on the ARG we can sometimes get a more compact form
compared to the simplified ARG. This of course also helps to better
understand the generated witness and why it looks how it looks.

git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@31934 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

src/org/sosy_lab/cpachecker/core/counterexample/report.js
@@ -203,6 +203,7 @@ with considerably less effort */
 						d3.select("#arg-toolbar").style("visibility", "hidden");
 						d3.selectAll(".arg-graph").style("visibility", "hidden");
 						d3.selectAll(".arg-simplified-graph").style("visibility", "hidden");
+						d3.selectAll(".arg-reduced-graph").style("visibility", "hidden");
 						d3.selectAll(".arg-error-graph").style("visibility", "hidden");
 						if (d3.select("#arg-container").classed("arg-content")) {
 							d3.select("#arg-container").classed("arg-content", false);
@@ -227,6 +228,7 @@ with considerably less effort */
 						d3.select("#arg-container").classed("arg-content", true);
 					}
 					d3.selectAll(".arg-simplified-graph").style("display", "none");
+					d3.selectAll(".arg-reduced-graph").style("display", "none");
 					if ($rootScope.displayedARG.indexOf("error") !== -1) {
 						d3.selectAll(".arg-error-graph").style("visibility", "visible");
 						if ($("#arg-container").scrollTop() === 0) {
@@ -251,6 +253,7 @@ with considerably less effort */
 						d3.select("#arg-toolbar").style("visibility", "hidden");
 						d3.selectAll(".arg-graph").style("visibility", "hidden");
 						d3.selectAll(".arg-simplified-graph").style("visibility", "hidden");
+						d3.selectAll(".arg-reduced-graph").style("visibility", "hidden");
 						d3.selectAll(".arg-error-graph").style("visibility", "hidden");
 						if (d3.select("#arg-container").classed("arg-content")) {
 							d3.select("#arg-container").classed("arg-content", false);
@@ -771,6 +774,9 @@ with considerably less effort */
 			if (relevantEdges !== undefined) {
                         	$scope.argSelections.push("simplified");
                         }
+			if (reducedEdges !== undefined) {
+				$scope.argSelections.push("witness");
+			}
 			$rootScope.displayedARG = $scope.argSelections[0];
 
 			$scope.displayARG = function () {
@@ -778,8 +784,11 @@ with considerably less effort */
 					if ($rootScope.displayedARG.indexOf("error") !== -1) {
 						d3.selectAll(".arg-graph").style("display", "none");
 						if (!d3.select(".arg-simplified-graph").empty()) {
-                                                         d3.selectAll(".arg-simplified-graph").style("display", "none");
-                                                }
+							d3.selectAll(".arg-simplified-graph").style("display", "none");
+						}
+						if (!d3.select(".arg-reduced-graph").empty()) {
+							d3.selectAll(".arg-reduced-graph").style("display", "none");
+						}
 						$("#arg-container").scrollTop(0).scrollLeft(0);
 						if (d3.select(".arg-error-graph").empty()) {
 							argWorker.postMessage({
@@ -790,6 +799,7 @@ with considerably less effort */
 						}
 					} else if ($rootScope.displayedARG.indexOf("simplified") !== -1) {
 					        d3.selectAll(".arg-graph").style("display", "none");
+					        d3.selectAll(".arg-reduced-graph").style("display", "none");
 					        $("#arg-container").scrollTop(0).scrollLeft(0);
 					        if (!d3.select(".arg-error-graph").empty()) {
                                                 	d3.selectAll(".arg-error-graph").style("display", "none");
@@ -801,6 +811,21 @@ with considerably less effort */
                                                 } else {
                                                         d3.selectAll(".arg-simplified-graph").style("display", "inline-block").style("visibility", "visible");
                                                 }
+					} else if ($rootScope.displayedARG.indexOf("witness") !== -1) {
+						console.log("reduced graph selected");
+						d3.selectAll(".arg-graph").style("display", "none");
+						d3.selectAll(".arg-simplified-graph").style("display", "none");
+						$("#arg-container").scrollTop(0).scrollLeft(0);
+						if (!d3.select(".arg-error-graph").empty()) {
+							d3.selectAll(".arg-error-graph").style("display", "none");
+						}
+						if (d3.select(".arg-reduced-graph").empty()) {
+							argWorker.postMessage({
+								"reducedGraph": true
+							});
+						} else {
+							d3.selectAll(".arg-reduced-graph").style("display", "inline-block").style("visibility", "visible");
+						}
 					} else {
 						if (!d3.select(".arg-error-graph").empty()) {
 							d3.selectAll(".arg-error-graph").style("display", "none");
@@ -808,6 +833,9 @@ with considerably less effort */
 						if (!d3.select(".arg-simplified-graph").empty()) {
                                                 	d3.selectAll(".arg-simplified-graph").style("display", "none");
                                                 }
+						if (!d3.select(".arg-reduced-graph").empty()) {
+							d3.selectAll(".arg-reduced-graph").style("display", "none");
+						}
 						d3.selectAll(".arg-graph").style("display", "inline-block").style("visibility", "visible");
 						$("#arg-container").scrollLeft(d3.select(".arg-svg").attr("width") / 4);
 					}
@@ -845,6 +873,7 @@ with considerably less effort */
 				}
 				d3.selectAll(".arg-graph").remove();
 				d3.selectAll(".arg-simplified-graph").remove();
+				d3.selectAll(".arg-reduced-graph").remove();
 				d3.selectAll(".arg-error-graph").remove();
 				if ($scope.zoomEnabled) {
 					$scope.argZoomControl();
@@ -906,6 +935,10 @@ if (cfaJson.hasOwnProperty("errorPath")) {
 var relevantEdges;
 if (argJson.hasOwnProperty("relevantedges")) {
         relevantEdges = argJson.relevantedges;
+}
+var reducedEdges;
+if (argJson.hasOwnProperty("reducededges")) {
+        reducedEdges = argJson.reducededges;
 }
 var graphSplitThreshold = 700;
 var zoomEnabled = false;
@@ -1426,19 +1459,26 @@ function init() {
 			self.importScripts("https://www.sosy-lab.org/lib/d3js/5.4.0/d3.min.js", "https://www.sosy-lab.org/lib/dagre-d3/0.5.0/dagre-d3.min.js");
 			var json, nodes, edges, errorPath, relevantNodes, relevantEdges, errorGraphMap;
 			var graphSplitThreshold = 700;
-			var graphMap = [],
-				graphCounter = 0, simplifiedGraphMap, simplifiedGraphCounter = 0;
+			var graphMap = [], graphCounter = 0,
+			    simplifiedGraphMap, simplifiedGraphCounter = 0,
+			    reducedGraphMap, reducedGraphCounter = 0;
 			self.addEventListener("message", function (m) {
 				if (m.data.json !== undefined) {
 					json = JSON.parse(m.data.json);
 					nodes = json.nodes;
 					edges = json.edges;
-					buildGraphsAndPrepareResults(nodes, edges, false)
+					buildGraphsAndPrepareResults(nodes, edges, "default")
 					if(json.relevantedges !== undefined && json.relevantnodes !== undefined){
 					        relevantEdges = json.relevantedges;
 					        relevantNodes = json.relevantnodes;
 					        simplifiedGraphMap = [];
-					        buildGraphsAndPrepareResults(relevantNodes, relevantEdges, true);
+					        buildGraphsAndPrepareResults(relevantNodes, relevantEdges, "relevant");
+					}
+					if(json.reducededges !== undefined && json.reducednodes !== undefined){
+				        reducedEdges = json.reducededges;
+				        reducedNodes = json.reducednodes;
+				        reducedGraphMap = [];
+				        buildGraphsAndPrepareResults(reducedNodes, reducedEdges, "witness");
 					}
 				} else if (m.data.errorPath !== undefined) {
 					errorPath = [];
@@ -1468,6 +1508,15 @@ function init() {
 						        simplifiedGraphMap.shift();
 						        simplifiedGraphCounter++;
 						}
+						if (reducedGraphMap.length > 0) {
+					        self.postMessage({
+					              "graph": JSON.stringify(reducedGraphMap[0]),
+					              "id": reducedGraphCounter,
+					              "reducedGraph" : true
+					        });
+					        reducedGraphMap.shift();
+					        reducedGraphCounter++;
+						}
 						if (errorPath !== undefined) {
 							errorGraphMap = [];
 							graphCounter = 0;
@@ -1493,11 +1542,11 @@ function init() {
 				}
 			}, false);
 
-			function buildGraphsAndPrepareResults(nodes, edges, relevant) {
+			function buildGraphsAndPrepareResults(nodes, edges, graphLabel) {
 				if (nodes.length > graphSplitThreshold) {
-					buildMultipleGraphs(nodes, edges, relevant);
+					buildMultipleGraphs(nodes, edges, graphLabel);
 				} else {
-					buildSingleGraph(nodes, edges, relevant);
+					buildSingleGraph(nodes, edges, graphLabel);
 				}
 			}
 
@@ -1525,19 +1574,21 @@ function init() {
 				}
 			}
 
-			function buildSingleGraph(nodes, edges, relevant) {
+			function buildSingleGraph(nodes, edges, graphLabel) {
 				var g = createGraph();
 				setGraphNodes(g, nodes);
 				setGraphEdges(g, edges, false);
-				if(relevant){
-				      simplifiedGraphMap.push(g);
+				if(graphLabel === "relevant"){
+					simplifiedGraphMap.push(g);
+				} else if (graphLabel === "witness") {
+					reducedGraphMap.push(g);
 				} else {
-			              graphMap.push(g);
+					graphMap.push(g);
 				}
 			}
 
 			// Split the ARG graph honoring the split threshold
-			function buildMultipleGraphs(nodes, edges, relevant) {
+			function buildMultipleGraphs(nodes, edges, graphLabel) {
 				nodes.sort(function (firstNode, secondNode) {
 					return firstNode.index - secondNode.index;
 				})
@@ -1556,8 +1607,10 @@ function init() {
 						}
 					}
 					var graph = createGraph();
-					if (relevant) {
-					        simplifiedGraphMap.push(graph);
+					if (graphLabel === "relevant") {
+					    simplifiedGraphMap.push(graph);
+					} else if (graphLabel === "witness") {
+						reducedGraphMap.push(graph);
 					} else {
 						graphMap.push(graph);
 					}
@@ -1888,6 +1941,10 @@ function init() {
 				        id = "arg-simplified-graph" + m.data.id;
 				        argClass = "arg-simplified-graph";
 				}
+				if(m.data.reducedGraph !== undefined) {
+					id = "arg-reduced-graph" + m.data.id;
+					argClass = "arg-reduced-graph";
+				}
 				var g = createGraph();
 				g = Object.assign(g, JSON.parse(m.data.graph));
 				d3.select("#arg-container").append("div").attr("id", id).attr("class", argClass);
@@ -2131,7 +2188,7 @@ function init() {
 		addPanEvent(".arg-svg");
 		d3.selectAll(".arg-node")
 			.on("mouseover", function (d) {
-				var node = argJson.nodes.find(function (it) {
+				var node = [...argJson.nodes, ...argJson.relevantnodes,  ...argJson.reducednodes].find(function (it) {
 					return it.index === parseInt(d);
 				})
 				var message = "<span class=\" bold \">function</span>: " + node.func + "<br>";
@@ -2173,7 +2230,7 @@ function init() {
 		d3.selectAll(".arg-edge")
 			.on("mouseover", function (d) {
 				d3.select(this).select("path").style("stroke-width", "3px");
-				var edge = [ ...argJson.edges, ...argJson.relevantedges ].find(function (it) {
+				var edge = [...argJson.edges, ...argJson.relevantedges, ...argJson.reducededges].find(function (it) {
 					return it.source === parseInt(d.v) && it.target === parseInt(d.w);
 				})
 				var message = "";

src/org/sosy_lab/cpachecker/core/counterexample/ReportGenerator.java
@@ -51,11 +51,15 @@ import java.nio.file.Paths;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayDeque;
 import java.util.Collection;
+import java.util.Deque;
 import java.util.HashMap;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Optional;
 import java.util.Set;
 import java.util.regex.Pattern;
 import org.checkerframework.checker.nullness.qual.Nullable;
@@ -81,7 +85,10 @@ import org.sosy_lab.cpachecker.core.interfaces.AbstractState;
 import org.sosy_lab.cpachecker.core.interfaces.Statistics;
 import org.sosy_lab.cpachecker.core.reachedset.UnmodifiableReachedSet;
 import org.sosy_lab.cpachecker.cpa.arg.ARGState;
+import org.sosy_lab.cpachecker.cpa.arg.ARGStatistics;
 import org.sosy_lab.cpachecker.cpa.arg.ARGUtils;
+import org.sosy_lab.cpachecker.cpa.arg.witnessexport.Witness;
+import org.sosy_lab.cpachecker.cpa.arg.witnessexport.WitnessToOutputFormatsUtils;
 import org.sosy_lab.cpachecker.util.AbstractStates;
 
 @Options
@@ -125,6 +132,9 @@ public class ReportGenerator {
   private final Map<String, Object> argEdges;
   private final Map<String, Object> argRelevantEdges;
   private final Map<Integer, Object> argRelevantNodes;
+  private final Map<String, Object> argReducedEdges;
+  private final Map<Integer, Object> argReducedNodes;
+  private Optional<Witness> witnessOptional;
 
   private final String producer; // HTML-escaped producer string
 
@@ -143,6 +153,9 @@ public class ReportGenerator {
     argEdges = new HashMap<>();
     argRelevantEdges = new HashMap<>();
     argRelevantNodes = new HashMap<>();
+    argReducedEdges = new HashMap<>();
+    argReducedNodes = new HashMap<>();
+    witnessOptional = Optional.empty();
     producer = htmlEscaper().escape(CPAchecker.getVersion(pConfig));
   }
 
@@ -167,10 +180,13 @@ public class ReportGenerator {
       return;
     }
 
+    extractWitness(pStats);
+
     // we cannot export the graph for some special analyses, e.g., termination analysis
     if (!pReached.isEmpty() && pReached.getFirstState() instanceof ARGState) {
       buildArgGraphData(pReached);
       buildRelevantArgGraphData(pReached);
+      buildReducedArgGraphData();
     }
 
     DOTBuilder2 dotBuilder = new DOTBuilder2(pCfa);
@@ -203,6 +219,26 @@ public class ReportGenerator {
               counterExamples.transform(cex -> counterExampleFiles.getPath(cex.getUniqueId())));
       counterExFiles.append("\".");
       console.println(counterExFiles.toString());
+    }
+  }
+
+  private void extractWitness(Statistics pStats) {
+    Deque<Statistics> waitlist = new ArrayDeque<>();
+    Collection<Statistics> seenStats = new LinkedHashSet<>();
+    waitlist.add(pStats);
+    while (!waitlist.isEmpty()) {
+      Statistics currentStats = waitlist.pop();
+      seenStats.add(currentStats);
+      if (currentStats instanceof ARGStatistics &&( (ARGStatistics)currentStats).getWitnessIfAlreadyGenerated().isPresent()) {
+        witnessOptional = ((ARGStatistics) currentStats).getWitnessIfAlreadyGenerated();
+        break;
+      }
+      waitlist.addAll(
+          currentStats
+              .getSubStatistics()
+              .stream()
+              .filter(x -> !seenStats.contains(x))
+              .collect(ImmutableList.toImmutableList()));
     }
   }
 
@@ -327,6 +363,13 @@ public class ReportGenerator {
       JSON.writeJSONString(argRelevantNodes.values(), writer);
       writer.write(",\n\"relevantedges\":");
       JSON.writeJSONString(argRelevantEdges.values(), writer);
+      writer.write("\n");
+    }
+    if (!argReducedEdges.isEmpty() || !argReducedNodes.isEmpty()) {
+      writer.write(",\n\"reducednodes\":");
+      JSON.writeJSONString(argReducedNodes.values(), writer);
+      writer.write(",\n\"reducededges\":");
+      JSON.writeJSONString(argReducedEdges.values(), writer);
       writer.write("\n");
     }
     writer.write("}\n");
@@ -605,6 +648,15 @@ public class ReportGenerator {
     }
   }
 
+  /** Build graph data for the reduced ARG */
+  private void buildReducedArgGraphData() {
+    if (!witnessOptional.isPresent()) {
+      return;
+    }
+    Witness witness = witnessOptional.get();
+    WitnessToOutputFormatsUtils.witnessToMapsForHTMLReport(witness, argReducedNodes, argReducedEdges);
+  }
+
   private Map<String, Object> createArgNode(int parentStateId, CFANode node, ARGState argState) {
     String dotLabel =
         argState.toDOTLabel().length() > 2
@@ -670,7 +722,7 @@ public class ReportGenerator {
     argEdges.put("" + coveringStateId + "->" + parentStateId, coveredEdge);
   }
 
-  private Map<String, Object> createArgEdge(
+  public static Map<String, Object> createArgEdge(
       int parentStateId, int childStateId, List<CFAEdge> edges) {
     Map<String, Object> argEdge = new HashMap<>();
     argEdge.put("source", parentStateId);

src/org/sosy_lab/cpachecker/cpa/arg/witnessexport/WitnessExporter.java
@@ -123,15 +123,17 @@ public class WitnessExporter {
             defaultFileName,
             WitnessType.VIOLATION_WITNESS,
             InvariantProvider.TrueInvariantProvider.INSTANCE);
-    writer.writePath(
-        pTarget,
-        pRootState,
-        pIsRelevantState,
-        pIsRelevantEdge,
-        Predicates.alwaysFalse(),
-        Optional.empty(),
-        Optional.ofNullable(pCounterExample),
-        GraphBuilder.ARG_PATH);
+    Witness generatedWitness =
+        writer.produceWitness(
+            pRootState,
+            pIsRelevantState,
+            pIsRelevantEdge,
+            Predicates.alwaysFalse(),
+            Optional.empty(),
+            Optional.ofNullable(pCounterExample),
+            GraphBuilder.ARG_PATH);
+    WitnessToOutputFormatsUtils.writeToGraphMl(generatedWitness, pTarget);
+    witness = Optional.ofNullable(generatedWitness);
   }
 
   public void writeTerminationErrorWitness(
@@ -316,7 +318,7 @@ public class WitnessExporter {
             Optional.empty(),
             Optional.empty(),
             GraphBuilder.CFA_FULL);
-    WitnessToGraphMlUtils.writeToGraphMl(generatedWitness, pTarget);
+    WitnessToOutputFormatsUtils.writeToGraphMl(generatedWitness, pTarget);
     witness = Optional.ofNullable(generatedWitness);
   }
 

src/org/sosy_lab/cpachecker/cpa/arg/witnessexport/WitnessFactory.java
@@ -1140,7 +1140,7 @@ class WitnessFactory implements EdgeAppender {
             cycleHeadToQuasiInvariant,
             pCounterExample,
             pGraphBuilder);
-    WitnessToGraphMlUtils.writeToGraphMl(witness,pTarget);
+    WitnessToOutputFormatsUtils.writeToGraphMl(witness,pTarget);
     return witness;
   }
 

src/org/sosy_lab/cpachecker/cpa/arg/witnessexport/WitnessToOutputFormatsUtils.java
@@ -21,13 +21,23 @@ package org.sosy_lab.cpachecker.cpa.arg.witnessexport;
 
 import static com.google.common.base.Strings.isNullOrEmpty;
 
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Maps;
 import com.google.common.collect.Queues;
+import com.google.common.collect.Sets;
 import java.io.IOException;
+import java.util.Collection;
 import java.util.Deque;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import javax.xml.parsers.ParserConfigurationException;
+import org.sosy_lab.cpachecker.cfa.model.CFAEdge;
+import org.sosy_lab.cpachecker.core.counterexample.ReportGenerator;
 import org.sosy_lab.cpachecker.core.interfaces.Property;
+import org.sosy_lab.cpachecker.cpa.arg.ARGState;
+import org.sosy_lab.cpachecker.cpa.slab.SLARGToDotWriter;
+import org.sosy_lab.cpachecker.util.NumericIdProvider;
 import org.sosy_lab.cpachecker.util.automaton.AutomatonGraphmlCommon.ElementType;
 import org.sosy_lab.cpachecker.util.automaton.AutomatonGraphmlCommon.GraphMlBuilder;
 import org.sosy_lab.cpachecker.util.automaton.AutomatonGraphmlCommon.KeyDef;
@@ -37,7 +47,7 @@ import org.sosy_lab.cpachecker.util.expressions.ExpressionTree;
 import org.sosy_lab.cpachecker.util.expressions.ExpressionTrees;
 import org.w3c.dom.Element;
 
-public class WitnessToGraphMlUtils {
+public class WitnessToOutputFormatsUtils {
 
   /**
    * Appends the witness as GraphML to the supplied {@link Appendable}
@@ -58,8 +68,107 @@ public class WitnessToGraphMlUtils {
     } catch (ParserConfigurationException e) {
       throw new IOException(e);
     }
-    WitnessToGraphMlUtils.writeElementsOfGraphToDoc(doc, witness);
+    WitnessToOutputFormatsUtils.writeElementsOfGraphToDoc(doc, witness);
     doc.appendTo(pTarget);
+  }
+
+  /**
+   * This method can be used to extract information about the nodes and edges in the witness which
+   * then can be used e.g. for display in the HTML report (c.f. @link ReportGenerator).
+   *
+   * @param witness the information that will be extracted
+   * @param nodesMap map that will be filled with information about the nodes
+   * @param edgesMap map that will be filled with information about the edges
+   */
+  public static void witnessToMapsForHTMLReport(
+      Witness witness, Map<Integer, Object> nodesMap, Map<String, Object> edgesMap) {
+    NumericIdProvider idProvider = NumericIdProvider.create();
+    String entryStateNode = witness.getEntryStateNodeId();
+    Set<String> nodes = Sets.newHashSet();
+    Deque<String> waitlist = Queues.newArrayDeque();
+    waitlist.push(entryStateNode);
+    // Element entryNode = createNewNode(doc, entryStateNodeId, witness);
+    // addInvariantsData(doc, entryNode, entryStateNodeId, witness);
+    nodes.add(entryStateNode);
+
+    while (!waitlist.isEmpty()) {
+      String source = waitlist.pop();
+
+      @SuppressWarnings("unchecked")
+      Map<String, Object> sourceNode = (Map<String, Object>) nodesMap.get(source);
+      if (sourceNode == null) {
+        // targetNode = createNewNode(doc, edge.getTarget(), witness);
+        sourceNode = Maps.newHashMap();
+
+        List<Integer> nodeIds = witness
+            .getARGStatesFor(source)
+            .stream()
+            .map(ARGState::getStateId)
+            .collect(ImmutableList.toImmutableList());
+        String nodeString = SLARGToDotWriter.generateLocationString(nodeIds).toString();
+        StringBuilder labelBuilder = new StringBuilder(source);
+        if (!nodeString.isEmpty()) {
+          labelBuilder.append(String.format("\nARG node%s: ", nodeIds.size() == 1 ? "" : "s"));
+          labelBuilder.append(nodeString);
+        }
+
+        ExpressionTree<Object> tree = witness.getStateInvariant(source);
+        if (!tree.equals(ExpressionTrees.getTrue())) {
+          sourceNode.put(KeyDef.INVARIANT.toString(), tree.toString());
+          String scope = witness.getStateScopes().get(source);
+          labelBuilder.append(System.lineSeparator()).append(tree.toString());
+          if (!isNullOrEmpty(scope) && !tree.equals(ExpressionTrees.getFalse())) {
+            sourceNode.put(KeyDef.INVARIANTSCOPE.toString(), scope);
+            labelBuilder.append(System.lineSeparator()).append(scope);
+          }
+        }
+
+        sourceNode.put("index", idProvider.provideNumericId(source));
+        sourceNode.put("label", labelBuilder.toString());
+        sourceNode.put("type", determineNodeType(witness, source));
+        sourceNode.put("func", "main"); // TODO: add actual function here (but what if it's mixed?!)
+        nodesMap.put(idProvider.provideNumericId(source), sourceNode);
+      }
+
+      for (Edge edge : witness.getLeavingEdges().get(source)) {
+        ExpressionTree<Object> tree = witness.getStateInvariant(edge.getTarget());
+
+        List<CFAEdge> edges = witness.getCFAEdgeFor(edge);
+        Map<String, Object> edgeMap =
+            ReportGenerator.createArgEdge(
+                idProvider.provideNumericId(source),
+                idProvider.provideNumericId(edge.getTarget()),
+                edges);
+        for (java.util.Map.Entry<KeyDef, String> e : edge.getLabel().getMapping().entrySet()) {
+          edgeMap.put(e.getKey().toString(), e.getValue());
+        }
+        edgesMap.put(
+            edge.getSource() + "->" + edge.getTarget(),
+            edgeMap);
+
+        if (!nodes.contains(edge.getTarget())) {
+          nodes.add(edge.getTarget());
+          if (!ExpressionTrees.getFalse().equals(tree)) {
+          waitlist.push(edge.getTarget());
+          }
+        } else {
+          continue;
+        }
+      }
+    }
+  }
+
+  private static String determineNodeType(Witness witness, String source) {
+    Collection<ARGState> states = witness.getARGStatesFor(source);
+    if (!witness.getViolatedProperties().get(source).isEmpty()
+        || states.stream().anyMatch(ARGState::isTarget)) {
+      return "target";
+    } else if (!states.stream().allMatch(ARGState::wasExpanded)) {
+      return "not-expanded";
+    } else if (states.stream().anyMatch(ARGState::shouldBeHighlighted)){
+      return "highlighted";
+    }
+    return "";
   }
 
   private static void writeElementsOfGraphToDoc(GraphMlBuilder doc, Witness witness) {

commit a5d4c229b0cf71caab04c8b3f61c834d7eb57a5d

Fix bug in the HTML report introduced in r31934
This fixes #710

git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@32902 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

src/org/sosy_lab/cpachecker/core/counterexample/report.js
@@ -1508,7 +1508,7 @@ function init() {
 						        simplifiedGraphMap.shift();
 						        simplifiedGraphCounter++;
 						}
-						if (reducedGraphMap.length > 0) {
+						if (typeof reducedGraphMap !== 'undefined' && reducedGraphMap.length > 0) {
 					        self.postMessage({
 					              "graph": JSON.stringify(reducedGraphMap[0]),
 					              "id": reducedGraphCounter,