b74b12f0aaba39b410f0ee3572bd0e12bd14dd72
pwendler <pwendler@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2020-04-14 06:21:02 +0000

    Fix bug from 33167: dot output of SLARGToDotWriter was broken
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@33197 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/cpa/slab/SLARGToDotWriter.java
+++ src/org/sosy_lab/cpachecker/cpa/slab/SLARGToDotWriter.java
@@ -89,7 +89,7 @@ public class SLARGToDotWriter {
       builder.append("\"");
     }
 
-    builder.append("]%n");
+    builder.append("]").append(System.lineSeparator());
     return builder.toString();
   }
 
@@ -109,7 +109,8 @@ public class SLARGToDotWriter {
     builder.append("@N");
     builder.append(generateLocationString(locationNumbers));
     builder.append("\" ");
-    builder.append("id=\"").append(pState.getStateId()).append("\"]%n");
+    builder.append("id=\"").append(pState.getStateId());
+    builder.append("\"]").append(System.lineSeparator());
     return builder.toString();
   }
 
