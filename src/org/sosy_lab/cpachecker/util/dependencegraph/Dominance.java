b267d50afdae75919f14bc10338bea65c282ba67
vport <vport@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2020-04-27 16:32:44 +0000

    Add Dominance.DomTraversable
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/program-dependence-graph@33265 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/util/dependencegraph/Dominance.java
+++ src/org/sosy_lab/cpachecker/util/dependencegraph/Dominance.java
@@ -295,6 +295,22 @@ final class Dominance {
     return frontiers;
   }
 
+  /**
+   * Creates the corresponding {@link DomTraversable} for a specified {@link DomTree}. The resulting
+   * DomTraversable can be used to traverse the specified dominance tree.
+   *
+   * @param <T> the node-type of the original graph.
+   * @param pDomTree the {@link DomTree} (dominance tree) of the original graph.
+   * @throws NullPointerException if {@code pDomTree} is {@code null}.
+   * @return the created {@link DomTraversable}-object.
+   */
+  public static <T> DomTraversable<T> createDomTraversable(DomTree<T> pDomTree) {
+
+    Objects.requireNonNull(pDomTree, "pDomTree must not be null");
+
+    return DomTraversable.create(pDomTree);
+  }
+
   /** DomInput stores predecessors for all nodes and the number of nodes. */
   private static final class DomInput {
 
@@ -519,6 +535,114 @@ final class Dominance {
         if (id != nodes.length - 1) {
           sb.append(", ");
         }
+      }
+
+      sb.append("]");
+
+      return sb.toString();
+    }
+  }
+
+  /**
+   * A data structure representing a node in a dominance tree that can be used for tree traversal.
+   *
+   * <p>It's possible to iterate over all direct children of this tree node (see {@link
+   * DomTraversable#iterator()}). Recursive iteration over all descendants enables full tree
+   * traversal.
+   *
+   * @param <T> the node-type of the original graph.
+   */
+  public static final class DomTraversable<T> implements Iterable<DomTraversable<T>> {
+
+    private final T node;
+
+    private DomTraversable<T> parent;
+    private List<DomTraversable<T>> children;
+
+    private DomTraversable(T pNode) {
+      node = pNode;
+      children = new ArrayList<>();
+    }
+
+    private static <T> DomTraversable<T> create(DomTree<T> pDomTree) {
+
+      List<DomTraversable<T>> traversables = new ArrayList<>(pDomTree.getNodeCount());
+
+      for (T node : pDomTree) {
+        traversables.add(new DomTraversable<>(node));
+      }
+
+      for (int id = 0; id < pDomTree.getNodeCount(); id++) {
+        if (pDomTree.hasParent(id)) {
+          DomTraversable<T> traversable = traversables.get(id);
+          DomTraversable<T> parent = traversables.get(pDomTree.getParent(id));
+          traversable.parent = parent;
+          parent.children.add(traversable);
+        }
+      }
+
+      for (DomTraversable<T> traversable : traversables) {
+        traversable.children = Collections.unmodifiableList(traversable.children);
+      }
+
+      return traversables.get(traversables.size() - 1);
+    }
+
+    /**
+     * Returns the original graph node for this DomTraversable.
+     *
+     * @return the original graph node.
+     */
+    public T getNode() {
+      return node;
+    }
+
+    /**
+     * Returns the parent DomTraversable, if the DomTraversable has a parent (i.e. is not the root).
+     *
+     * @return if the DomTraversable has valid parent, the parent DomTraversable is returned;
+     *     otherwise {@code null} is returned.
+     */
+    public DomTraversable<T> getParent() {
+      return parent;
+    }
+
+    /**
+     * Returns an iterator over the direct children of this DomTraversable.
+     *
+     * @return an iterator over the direct children of this DomTraversable.
+     */
+    @Override
+    public Iterator<DomTraversable<T>> iterator() {
+      return children.iterator();
+    }
+
+    @Override
+    public String toString() {
+
+      StringBuilder sb = new StringBuilder();
+      sb.append("[");
+
+      boolean insertSeparator = false;
+
+      if (parent != null) {
+        sb.append(node);
+        sb.append(" --> ");
+        sb.append(parent.node);
+        insertSeparator = true;
+      }
+
+      for (DomTraversable<T> child : children) {
+
+        if (insertSeparator) {
+          sb.append(", ");
+        } else {
+          insertSeparator = true;
+        }
+
+        sb.append(child.node);
+        sb.append(" --> ");
+        sb.append(node);
       }
 
       sb.append("]");
