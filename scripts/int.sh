4bcddcea4b5e7329d4acc591cfa78f8530ba76bf
nzlee <nzlee@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2020-04-28 09:59:29 +0000

    change timeout to 10s due to the equivalence comparison of images
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/interpolation-model-checking@33299 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- scripts/int.sh
+++ scripts/int.sh
@@ -1,2 +1,2 @@
-./scripts/cpa.sh -timelimit 60s -preprocess -bmc-interpolation -setprop cpa.loopbound.maxLoopIterationsUpperBound=2 example/hand-crafted/even2.c
+./scripts/cpa.sh -timelimit 10s -preprocess -bmc-interpolation -setprop cpa.loopbound.maxLoopIterationsUpperBound=2 example/hand-crafted/even2.c
 firefox output/Report.html &
