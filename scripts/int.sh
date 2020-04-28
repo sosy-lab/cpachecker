99778ffbef22780be52de4061b14f3736f0380be
nzlee <nzlee@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2020-04-28 10:06:23 +0000

    update the int script: remove the limits of unrolling upper bound
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/interpolation-model-checking@33335 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- scripts/int.sh
+++ scripts/int.sh
@@ -1,4 +1,5 @@
-./scripts/cpa.sh -timelimit 10s -preprocess -bmc-interpolation -setprop cpa.loopbound.maxLoopIterationsUpperBound=3 -spec sv-comp-reachability $1
+./scripts/cpa.sh -timelimit 10s -preprocess -bmc-interpolation -spec sv-comp-reachability $1
+#./scripts/cpa.sh -timelimit 10s -preprocess -bmc-interpolation -setprop cpa.loopbound.maxLoopIterationsUpperBound=3 -spec sv-comp-reachability $1
 if [[ $? -eq 0 ]];then
    firefox output/Report.html &
 else
