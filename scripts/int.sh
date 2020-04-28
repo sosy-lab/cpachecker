108f4823ab92dda5a84832c6f6fbe67eb0c19bac
nzlee <nzlee@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2020-04-28 10:02:19 +0000

    add a buggy variant to test BMC
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/interpolation-model-checking@33315 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- scripts/int.sh
+++ scripts/int.sh
@@ -1,2 +1,3 @@
-./scripts/cpa.sh -timelimit 10s -preprocess -bmc-interpolation -setprop cpa.loopbound.maxLoopIterationsUpperBound=3 -spec sv-comp-reachability example/hand-crafted/even_LBE.c
+./scripts/cpa.sh -timelimit 10s -preprocess -bmc-interpolation -setprop cpa.loopbound.maxLoopIterationsUpperBound=3 -spec sv-comp-reachability $1
+#./scripts/cpa.sh -timelimit 10s -preprocess -bmc-interpolation -setprop cpa.loopbound.maxLoopIterationsUpperBound=3 -spec sv-comp-reachability example/hand-crafted/even_LBE.c
 firefox output/Report.html &
