5413c4c28ba754f5049472db35c4ff78d59f40ca
nzlee <nzlee@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2020-04-28 09:57:06 +0000

    set maxLoopIterationUpperBound to 2 for debugging
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/interpolation-model-checking@33285 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- scripts/int.sh
+++ scripts/int.sh
@@ -1,1 +1,1 @@
-./scripts/cpa.sh -timelimit 5s -preprocess -bmc-interpolation example/hand-crafted/even.c
+./scripts/cpa.sh -timelimit 5s -preprocess -bmc-interpolation -setprop cpa.loopbound.maxLoopIterationsUpperBound=2 example/hand-crafted/even.c
