2a4f3aacc94d1185c6a093f83c341e311af80bb2
nzlee <nzlee@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2020-04-28 10:04:23 +0000

    set the upper bound of unrolling to infinity
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/interpolation-model-checking@33325 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- scripts/bmc-LBE.sh
+++ scripts/bmc-LBE.sh
@@ -1,2 +1,2 @@
-./scripts/cpa.sh -heap 2000M -timelimit 10s -preprocess -bmc-LBE -setprop cpa.loopbound.maxLoopIterationsUpperBound=10 -stats -spec sv-comp-reachability $1
+./scripts/cpa.sh -heap 2000M -timelimit 10s -preprocess -bmc-LBE -stats -spec sv-comp-reachability $1
 firefox output/Report.html &
