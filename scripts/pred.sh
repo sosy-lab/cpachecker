0b7911b010f742881f88403991238138b57ee71f
nzlee <nzlee@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2020-04-28 10:00:22 +0000

    add -spec sv-comp-reachability to the scripts
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/interpolation-model-checking@33304 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- scripts/pred.sh
+++ scripts/pred.sh
@@ -1,2 +1,2 @@
-./scripts/cpa.sh -timelimit 60s -preprocess -predicateAnalysis example/hand-crafted/even2.c
+./scripts/cpa.sh -timelimit 60s -preprocess -predicateAnalysis -spec sv-comp-reachability example/hand-crafted/even2.c
 firefox output/Report.html &
