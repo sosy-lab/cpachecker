2abffbb8dec7d6f1c3e2e4650d4f452a29e59dc3
nzlee <nzlee@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2020-04-28 10:07:20 +0000

    update int script: additional input argument to specify log level
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/interpolation-model-checking@33340 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- scripts/int.sh
+++ scripts/int.sh
@@ -1,5 +1,4 @@
-./scripts/cpa.sh -timelimit 10s -preprocess -bmc-interpolation -spec sv-comp-reachability $1
-#./scripts/cpa.sh -timelimit 10s -setprop log.level=FINEST -preprocess -bmc-interpolation -spec sv-comp-reachability $1
+./scripts/cpa.sh -timelimit 10s -setprop log.level=$1 -preprocess -bmc-interpolation -spec sv-comp-reachability $2
 #./scripts/cpa.sh -timelimit 10s -preprocess -bmc-interpolation -setprop cpa.loopbound.maxLoopIterationsUpperBound=3 -spec sv-comp-reachability $1
 if [[ $? -eq 0 ]];then
    firefox output/Report.html &
