e2d4d021c8857e128d9fa0a815f827aef54af717
nzlee <nzlee@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2020-04-28 10:05:12 +0000

    update int script: do not open the old report if an execution throws exception
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/interpolation-model-checking@33329 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- scripts/int.sh
+++ scripts/int.sh
@@ -1,3 +1,6 @@
 ./scripts/cpa.sh -timelimit 10s -preprocess -bmc-interpolation -setprop cpa.loopbound.maxLoopIterationsUpperBound=3 -spec sv-comp-reachability $1
-#./scripts/cpa.sh -timelimit 10s -preprocess -bmc-interpolation -setprop cpa.loopbound.maxLoopIterationsUpperBound=3 -spec sv-comp-reachability example/hand-crafted/even_LBE.c
-firefox output/Report.html &
+if [[ $? -eq 0 ]];then
+   firefox output/Report.html &
+else
+   echo "The execution was terminated with error code $? and produced no report."
+fi
