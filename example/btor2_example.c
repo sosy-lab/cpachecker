530fb5e6ac7a0e23500a683a95b8be90edf742c5
nzlee <nzlee@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2020-04-28 09:53:53 +0000

    add an example c program for btor2 format
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/interpolation-model-checking@33268 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

---
+++ example/btor2_example.c
@@ -0,0 +1,19 @@
+extern void __VERIFIER_error() __attribute__ ((__noreturn__));
+extern void __VERIFIER_assume(int cond) { if(!cond) while(1){} }
+void __VERIFIER_assert(int cond) { if(!(cond)) { ERROR: __VERIFIER_error(); } }
+extern int __VERIFIER_nondet_int();
+
+#include <stdlib.h>
+
+int
+main() {
+   int reg = 0;
+   while (1) {
+      reg = !reg;
+      if (reg == 2) {
+         __VERIFIER_assert(0);
+         return 1;
+      }
+   }
+   return 0;
+}
