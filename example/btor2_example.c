f8231bf34f1c8add8b8607853de36f1bbd1f7656
nzlee <nzlee@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2020-04-28 10:04:34 +0000

    delete multi-loop examples
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/interpolation-model-checking@33326 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- example/btor2_example.c
+++ example/btor2_example.c
@@ -1,5 +1,4 @@
 extern void __VERIFIER_error() __attribute__ ((__noreturn__));
-extern void __VERIFIER_assume(int cond) { if(!cond) while(1){} }
 void __VERIFIER_assert(int cond) { if(!(cond)) { ERROR: __VERIFIER_error(); } }
 extern int __VERIFIER_nondet_int();
 
