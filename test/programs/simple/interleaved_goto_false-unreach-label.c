17d6c3a46d9ca3367d412ad58b4f9a6ec009c31b
spiessl <spiessl@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2020-05-14 23:12:24 +0000

    Add simple test programs for interleaved gotos
    Our BMC implementation has problems with interleaved loops
    created by gotos. These programs will help in spotting
    if such an error occurs again or elsewhere.
    
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@33542 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

---
+++ test/programs/simple/interleaved_goto_false-unreach-label.c
@@ -0,0 +1,19 @@
+extern void __VERIFIER_error();
+void main() {
+  int i = 0;
+  int j = 0;
+  FIRST:
+  j = j + 1;
+  SECOND:
+  if (j < 4) {
+    goto FIRST;
+  } else {
+    i = i + 1;
+    if (i > 6) {
+      ERROR:
+      goto ERROR;
+    } else {
+      goto SECOND;
+    }
+  }
+}
---
+++ test/programs/simple/interleaved_goto_true-unreach-label_true-termination.c
@@ -0,0 +1,21 @@
+extern void __VERIFIER_error();
+void main() {
+  int i = 0;
+  int j = 0;
+  FIRST:
+  j = j + 1;
+  SECOND:
+  if (j < 4) {
+    goto FIRST;
+  } else {
+    i = i + 1;
+    if (i > 6) {
+      ERROR:
+      goto ERROR;
+    } else if (i == 6) {
+      return;
+    } else {
+      goto SECOND;
+    }
+  }
+}
