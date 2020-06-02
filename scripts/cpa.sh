10948ec305174c19816966d624b27a30fecfef40
pwendler <pwendler@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2020-06-02 12:59:35 +0000

    Add machine-readable copyright header to our scripts
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@33730 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- scripts/cpa.sh
+++ scripts/cpa.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# This file is part of CPAchecker,
+# a tool for configurable software verification:
+# https://cpachecker.sosy-lab.org
+#
+# SPDX-FileCopyrightText: 2007-2020 Dirk Beyer <https://www.sosy-lab.org>
+#
+# SPDX-License-Identifier: Apache-2.0
+
 # the location of the java command
 [ -z "$JAVA" ] && JAVA=java
 
