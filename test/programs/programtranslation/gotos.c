f8e91a0ace00279260ca9eaa35efe545ac632794
pwendler <pwendler@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2020-06-03 05:47:19 +0000

    Declare copyright for remaining test programs written in C
    These were created by CPAchecker contributors
    and we use the same license and copyright header
    that these contributors also have used for their Java contributions
    (same as we did for related config files, documentation etc.).
    For most of the test programs we even have a copyright transfer
    or the contributors explicitly confirmed that this is correct.
    
    All files where I could find sources from other projects
    (e.g., Linux code) already got a specific copyright header
    in the previous commits.
    
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@33749 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- test/programs/programtranslation/gotos.c
+++ test/programs/programtranslation/gotos.c
@@ -1,3 +1,11 @@
+// This file is part of CPAchecker,
+// a tool for configurable software verification:
+// https://cpachecker.sosy-lab.org
+//
+// SPDX-FileCopyrightText: 2007-2020 Dirk Beyer <https://www.sosy-lab.org>
+//
+// SPDX-License-Identifier: Apache-2.0
+
 unsigned char __VERIFIER_nondet_uchar(void);
 void __VERIFIER_error();
 void __VERIFIER_assume(int cond);
