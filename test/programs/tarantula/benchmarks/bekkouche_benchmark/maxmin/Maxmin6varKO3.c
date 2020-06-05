8ed5f58410fd42d04a166f27aaabd7ca4eb0891d
sali <sali@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2020-06-05 22:15:28 +0000

    reposition the bug
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/tarantula@33821 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- test/programs/tarantula/benchmarks/bekkouche_benchmark/maxmin/Maxmin6varKO3.c
+++ test/programs/tarantula/benchmarks/bekkouche_benchmark/maxmin/Maxmin6varKO3.c
@@ -1,15 +1,4 @@
 /*
-The program Maxmin6var (implemented in function foo) takes 
-six poxisitve values as input and assigns to the variable max 
-the maximum value and to the variable min the minimum value. 
-
-There is two errors in this program, one in the conditions 
-((a>b) && (a>c) && (b>d)  && (a>e) && (a>f)) and in the 
-condition ((b<c) && (c<d) && (b<e) && (b<f)). 
-By taking as input {a=1, b=-3, c=0, d=-2, e=-1, f=-2}, 
-these errors trigger two wrong branching; the program 
-returns {max=0,min=-3} instead of {max=1,min=-3}.
-
 @author: Mohammed Bekkouche
 @Web:    http://www.i3s.unice.fr
 */
@@ -31,10 +20,10 @@ void __VERIFIER_assert(int cond) {
 void foo (int a, int b, int c, int d, int e, int f) {
 	int max;
     int min;
-	if ((a>b) && (a>c) && (b>d)  && (a>e) && (a>f)){ // error, the instruction should be ((a>b) && (a>c) && "(a>d)" && (a>e) && (a>f))
+	if ((a>b) && (a>c) && (b>d)  && (a>d) && (a>f)){ 
         max=a;
         if ((b<c) && (b<d) && (b<e) && (b<f)){
-            min=b;
+            min=e;// error this should be min = b;
         }
         else{
             if ((c<d) && (c<e) && (c<f)){
