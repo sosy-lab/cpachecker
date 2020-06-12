f19e3f1b3b97d976a0c8a3109c19b4411d4561ae
nzlee <nzlee@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2020-06-12 16:17:40 +0000

    Fix CI complaints
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/cfa-single-loop-transformation@33877 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/cfa/postprocessing/global/singleloop/CFASingleLoopTransformation.java
+++ src/org/sosy_lab/cpachecker/cfa/postprocessing/global/singleloop/CFASingleLoopTransformation.java
@@ -1,26 +1,11 @@
-/*
- *  CPAchecker is a tool for configurable software verification.
- *  This file is part of CPAchecker.
- *
- *  Copyright (C) 2007-2014  Dirk Beyer
- *  All rights reserved.
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- *
- *
- *  CPAchecker web page:
- *    http://cpachecker.sosy-lab.org
- */
+// This file is part of CPAchecker,
+// a tool for configurable software verification:
+// https://cpachecker.sosy-lab.org
+//
+// SPDX-FileCopyrightText: 2007-2020 Dirk Beyer <https://www.sosy-lab.org>
+//
+// SPDX-License-Identifier: Apache-2.0
+
 package org.sosy_lab.cpachecker.cfa.postprocessing.global.singleloop;
 
 import static com.google.common.base.Predicates.in;
@@ -60,7 +45,7 @@ import java.util.Queue;
 import java.util.Set;
 import java.util.TreeMap;
 import java.util.logging.Level;
-import javax.annotation.Nullable;
+import org.checkerframework.checker.nullness.qual.Nullable;
 import org.sosy_lab.common.ShutdownNotifier;
 import org.sosy_lab.common.collect.CopyOnWriteSortedMap;
 import org.sosy_lab.common.collect.PathCopyingPersistentTreeMap;
@@ -1273,7 +1258,7 @@ public class CFASingleLoopTransformation {
     } else if (pNode.getClass() != CFANode.class) {
 
       Class<? extends CFANode> clazz = pNode.getClass();
-      Class<?>[] requiredParameterTypes = new Class<?>[] { int.class, String.class };
+      Class<?>[] requiredParameterTypes = new Class<?>[] {int.class, String.class};
       for (Constructor<?> cons : clazz.getConstructors()) {
         if (Modifier.isPublic(cons.getModifiers())
             && Arrays.equals(cons.getParameterTypes(), requiredParameterTypes)) {
