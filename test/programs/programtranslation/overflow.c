66b218aae4d6084ec948a08144cf17a8e34c548f
sumbricht <sumbricht@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2020-06-16 08:34:29 +0000

    Add copyright headers to test files.
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/overflow2@33955 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- test/programs/programtranslation/overflow.c
+++ test/programs/programtranslation/overflow.c
@@ -1,4 +1,13 @@
+// This file is part of CPAchecker,
+// a tool for configurable software verification:
+// https://cpachecker.sosy-lab.org
+//
+// SPDX-FileCopyrightText: 2007-2020 Dirk Beyer <https://www.sosy-lab.org>
+//
+// SPDX-License-Identifier: Apache-2.0
+
 int main() {
     int i = 2147483647 + 1;
     int j = i * i;
+    return 0;
 }
