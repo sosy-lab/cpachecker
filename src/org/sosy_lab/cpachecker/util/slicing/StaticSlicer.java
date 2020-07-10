b6564af3c24935072ce4619e4dea03aa8b15c36e
vport <vport@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2020-07-10 16:39:42 +0000

    Remove unnecessary @Options annotation
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@34346 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/util/slicing/StaticSlicer.java
+++ src/org/sosy_lab/cpachecker/util/slicing/StaticSlicer.java
@@ -18,7 +18,6 @@ import java.util.Set;
 import org.sosy_lab.common.ShutdownNotifier;
 import org.sosy_lab.common.configuration.Configuration;
 import org.sosy_lab.common.configuration.InvalidConfigurationException;
-import org.sosy_lab.common.configuration.Options;
 import org.sosy_lab.common.log.LogManager;
 import org.sosy_lab.cpachecker.cfa.CFA;
 import org.sosy_lab.cpachecker.cfa.model.CFAEdge;
@@ -43,7 +42,6 @@ import org.sosy_lab.cpachecker.util.statistics.StatisticsWriter;
  *
  * @see SlicerFactory
  */
-@Options(prefix = "slicing")
 public class StaticSlicer extends AbstractSlicer implements StatisticsProvider {
 
   private DependenceGraph depGraph;
