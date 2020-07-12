7616bf2f4ffb7d2f9e27977c6cee2f599362d8ac
sumbricht <sumbricht@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2020-07-12 12:50:23 +0000

    Change default implementations and add documentation.
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/acsl-bachelor@34373 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/core/algorithm/acsl/ACSLAnnotation.java
+++ src/org/sosy_lab/cpachecker/core/algorithm/acsl/ACSLAnnotation.java
@@ -1,15 +1,43 @@
 package org.sosy_lab.cpachecker.core.algorithm.acsl;
 
 public interface ACSLAnnotation {
+
+  /**
+   * Returns a predicate that represents the semantics of the annotation as they could be used in an
+   * invariant.
+   *
+   * <p>An implementation of this interface should either implement <code>
+   * getPredicateRepresentation()</code> or both <code>getPreStateRepresentation()</code> and <code>
+   * getPostStateRepresentation()</code> depending on what makes sense for the specific kind of
+   * annotation.
+   */
   default ACSLPredicate getPredicateRepresentation() {
-    return getPreStateRepresentation();
+    throw new UnsupportedOperationException();
   }
 
+  /**
+   * Returns a predicate that represents the semantics of the annotation as they could be used in an
+   * invariant. Only properties that should be evaluated in the pre-state are considered.
+   *
+   * <p>An implementation of this interface should either implement <code>
+   * getPredicateRepresentation()</code> or both <code>getPreStateRepresentation()</code> and <code>
+   * getPostStateRepresentation()</code> depending on what makes sense for the specific kind of
+   * annotation.
+   */
   default ACSLPredicate getPreStateRepresentation() {
-    return getPredicateRepresentation();
+    throw new UnsupportedOperationException();
   }
 
+  /**
+   * Returns a predicate that represents the semantics of the annotation as they could be used in an
+   * invariant. Only properties that should be evaluated in the post-state are considered.
+   *
+   * <p>An implementation of this interface should either implement <code>
+   * getPredicateRepresentation()</code> or both <code>getPreStateRepresentation()</code> and <code>
+   * getPostStateRepresentation()</code> depending on what makes sense for the specific kind of
+   * annotation.
+   */
   default ACSLPredicate getPostStateRepresentation() {
-    return getPredicateRepresentation();
+    throw new UnsupportedOperationException();
   }
 }
