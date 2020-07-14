7ff173d87e9d5284fe7514796bea5db20d89038c
mbeck <mbeck@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2020-07-14 12:01:36 +0000

    Add statistics with solver time
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/sl-integration0@34409 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

---
+++ src/org/sosy_lab/cpachecker/cpa/sl/SLStatistics.java
@@ -0,0 +1,44 @@
+// This file is part of CPAchecker,
+// a tool for configurable software verification:
+// https://cpachecker.sosy-lab.org
+//
+// SPDX-FileCopyrightText: 2020 Dirk Beyer <https://www.sosy-lab.org>
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package org.sosy_lab.cpachecker.cpa.sl;
+
+import java.io.PrintStream;
+import org.checkerframework.checker.nullness.qual.Nullable;
+import org.sosy_lab.common.time.Timer;
+import org.sosy_lab.cpachecker.core.CPAcheckerResult.Result;
+import org.sosy_lab.cpachecker.core.interfaces.Statistics;
+import org.sosy_lab.cpachecker.core.reachedset.UnmodifiableReachedSet;
+
+public class SLStatistics implements Statistics {
+
+
+  private Timer solverTime;
+
+  public SLStatistics() {
+    solverTime = new Timer();
+  }
+
+  @Override
+  public void printStatistics(PrintStream pOut, Result pResult, UnmodifiableReachedSet pReached) {
+    put(pOut, "Solver Time", solverTime);
+  }
+
+  @Override
+  public @Nullable String getName() {
+    return "SLCPA";
+  }
+
+  public void startSolverTime() {
+    solverTime.start();
+  }
+
+  public void stopSolverTime() {
+    solverTime.stop();
+  }
+}
