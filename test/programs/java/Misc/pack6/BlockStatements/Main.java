b7d29631e8bbd770d530a7845e816ef381156910
smassard <smassard@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2020-07-15 20:11:39 +0000

    Add BlockStatements test
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/java-development@34439 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

---
+++ test/programs/java/Misc/pack6/BlockStatements/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+
+  public static void main(String[] args) {
+    int[] a = new int[3];
+    if (a.length == 0) {
+      System.out.println("test");
+      int b = 1;
+      System.out.println(b);
+    } else if (a.length == 0) {
+      System.out.println("test2");
+      int b = 2;
+      System.out.println(b);
+    } else {
+      System.out.println("test3");
+      int b = 3;
+      System.out.println(b);
+    }
+  }
+}
+
