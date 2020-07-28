02d87046296c7024bd6f31d6261b50a4045881a2
sumbricht <sumbricht@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2020-07-28 17:13:02 +0000

    Add copyright headers, make ACSLPredicate constructor protected and loop over entrySet in ACSLLoopAnnotation.
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/acsl-bachelor@34587 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/core/algorithm/acsl/ACSLLoopAnnotation.java
+++ src/org/sosy_lab/cpachecker/core/algorithm/acsl/ACSLLoopAnnotation.java
@@ -1,3 +1,11 @@
+// This file is part of CPAchecker,
+// a tool for configurable software verification:
+// https://cpachecker.sosy-lab.org
+//
+// SPDX-FileCopyrightText: 2020 Dirk Beyer <https://www.sosy-lab.org>
+//
+// SPDX-License-Identifier: Apache-2.0
+
 package org.sosy_lab.cpachecker.core.algorithm.acsl;
 
 import com.google.common.base.Joiner;
@@ -28,7 +36,8 @@ public class ACSLLoopAnnotation implements ACSLAnnotation {
   @Override
   public ACSLPredicate getPredicateRepresentation() {
     ACSLPredicate predicateRepresentation = lInvariant.getPredicate();
-    for (List<Behavior> behaviors : additionalInvariants.keySet()) {
+    for (Entry<List<Behavior>, LoopInvariant> entry : additionalInvariants.entrySet()) {
+      List<Behavior> behaviors = entry.getKey();
       ACSLPredicate enclosingConjunction = ACSLPredicate.getTrue();
       ACSLPredicate enclosingDisjunction = ACSLPredicate.getFalse();
       for (Behavior behavior : behaviors) {
@@ -41,7 +50,7 @@ public class ACSLLoopAnnotation implements ACSLAnnotation {
       ACSLPredicate behaviorRepresentation =
           new ACSLLogicalPredicate(
               enclosingDisjunction,
-              additionalInvariants.get(behaviors).getPredicate(),
+              entry.getValue().getPredicate(),
               BinaryOperator.AND);
       behaviorRepresentation =
           new ACSLLogicalPredicate(behaviorRepresentation, enclosingConjunction, BinaryOperator.OR);
