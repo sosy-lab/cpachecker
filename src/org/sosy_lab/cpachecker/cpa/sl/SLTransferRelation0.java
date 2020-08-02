0ce1a3e4e9e59e66f487ec05a840b3bf9154fbf1
mbeck <mbeck@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2020-08-02 14:47:50 +0000

    removed termination check cause of value analysis
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/sl-integration0@34608 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/cpa/sl/SLTransferRelation0.java
+++ src/org/sosy_lab/cpachecker/cpa/sl/SLTransferRelation0.java
@@ -14,7 +14,6 @@ import java.util.Collections;
 import java.util.List;
 import java.util.logging.Level;
 import org.sosy_lab.common.log.LogManager;
-import org.sosy_lab.cpachecker.cfa.model.AssumeEdge;
 import org.sosy_lab.cpachecker.cfa.model.CFAEdge;
 import org.sosy_lab.cpachecker.core.defaults.SingleEdgeTransferRelation;
 import org.sosy_lab.cpachecker.core.interfaces.AbstractState;
@@ -57,11 +56,11 @@ public class SLTransferRelation0 extends SingleEdgeTransferRelation {
     info += state + "\n";
     info += "---------------------------";
     logger.log(Level.INFO, info);
-    if (pCfaEdge instanceof AssumeEdge) {
-      return handleAssumption();
-    } else {
-      return ImmutableList.of(state);
-    }
+    // if (pCfaEdge instanceof AssumeEdge) {
+    // return handleAssumption();
+    // }
+    return ImmutableList.of(state);
+
   }
 
   private List<SLState>
