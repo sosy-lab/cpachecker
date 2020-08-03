b9df0751d7e128c21a05b7ca85bc9fa3c28855dd
mbeck <mbeck@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2020-08-03 13:42:41 +0000

    no need to copy already checked constraints
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/sl-integration0@34636 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/cpa/sl/SLState.java
+++ src/org/sosy_lab/cpachecker/cpa/sl/SLState.java
@@ -168,7 +168,7 @@ public class SLState implements AbstractState, AbstractQueryableState {
         newFormula,
             new LinkedHashMap<>(heap),
             new LinkedHashMap<>(stack),
-            new LinkedHashSet<>(constraints),
+            new LinkedHashSet<>(),
             new HashMap<>(allocationSizes),
             new HashSet<>(inScopePtrs),
             new HashSet<>(declarations),
