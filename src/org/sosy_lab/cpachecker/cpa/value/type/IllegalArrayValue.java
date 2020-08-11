4fef068fedf684819812498adc29a5355d92a4e8
smassard <smassard@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2020-08-11 16:56:33 +0000

    AbstractExpressionValueVisitor will now create an IllegalArrayValue for arrays with negative index instead of throwing an assertion error
    NegativeArraySizeException can be caught, thus just throwing an assertion error is not the right choice. Implementing a backend check of whether the exception is caught will be implemented later.
    
    Can be tested with java/jbmc-regression/NegativeArraySizeException1 of the sv-benchmarks suite
    
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/java-development@34712 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

---
+++ src/org/sosy_lab/cpachecker/cpa/value/type/IllegalArrayValue.java
@@ -0,0 +1,21 @@
+// This file is part of CPAchecker,
+// a tool for configurable software verification:
+// https://cpachecker.sosy-lab.org
+//
+// SPDX-FileCopyrightText: 2020 Dirk Beyer <https://www.sosy-lab.org>
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package org.sosy_lab.cpachecker.cpa.value.type;
+
+import org.sosy_lab.cpachecker.cfa.types.java.JArrayType;
+
+/**
+ * This class is created for illegal array values, e.g. int a[] = new int[-1];
+ */
+public class IllegalArrayValue extends ArrayValue {
+
+  public IllegalArrayValue(JArrayType pType) {
+    super(pType, 0);
+  }
+}
