bea11ce61765030c8edc4f26c4a3797de74473d8
smassard <smassard@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2020-08-12 14:32:39 +0000

    Move and integrate try catch test files into integration-value-java test job
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/java-development@34720 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- test/programs/java/Misc/pack4/BlockStatements.java
+++ test/programs/java/Misc/pack4/BlockStatements.java
@@ -1,4 +1,14 @@
-public class Main {
+// This file is part of CPAchecker,
+// a tool for configurable software verification:
+// https://cpachecker.sosy-lab.org
+//
+// SPDX-FileCopyrightText: 2020 Dirk Beyer <https://www.sosy-lab.org>
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package pack4;
+
+public class BlockStatements {
 
   public static void main(String[] args) {
     int[] a = new int[3];
