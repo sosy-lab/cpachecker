5d23b610a6bf03a4972e837eeebbf4c516e774c5
holzners <holzners@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2020-08-21 09:43:22 +0000

    Refactor HeapTest to use SolverBasedTest0 as baseclass
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/byte_level_heap@34782 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/util/predicates/pathformula/pointeraliasing/SMTHeapTest.java
+++ src/org/sosy_lab/cpachecker/util/predicates/pathformula/pointeraliasing/SMTHeapTest.java
@@ -8,97 +8,70 @@
 
 package org.sosy_lab.cpachecker.util.predicates.pathformula.pointeraliasing;
 
-import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
-import org.sosy_lab.common.ShutdownNotifier;
-import org.sosy_lab.common.configuration.Configuration;
 import org.sosy_lab.common.configuration.InvalidConfigurationException;
-import org.sosy_lab.common.log.LogManager;
 import org.sosy_lab.cpachecker.cfa.types.MachineModel;
 import org.sosy_lab.cpachecker.util.predicates.smt.FormulaManagerView;
-import org.sosy_lab.java_smt.SolverContextFactory;
 import org.sosy_lab.java_smt.SolverContextFactory.Solvers;
 import org.sosy_lab.java_smt.api.BitvectorFormula;
-import org.sosy_lab.java_smt.api.BitvectorFormulaManager;
 import org.sosy_lab.java_smt.api.BooleanFormula;
 import org.sosy_lab.java_smt.api.FormulaType;
-import org.sosy_lab.java_smt.api.ProverEnvironment;
-import org.sosy_lab.java_smt.api.SolverContext;
-import org.sosy_lab.java_smt.api.SolverContext.ProverOptions;
 import org.sosy_lab.java_smt.api.SolverException;
+import org.sosy_lab.java_smt.test.SolverBasedTest0;
 
-public class SMTHeapTest {
+public class SMTHeapTest extends SolverBasedTest0 {
 
-  private SMTHeap heap;
-  private FormulaManagerView fmgr;
-  private BitvectorFormulaManager bvfmgr;
-  private SolverContext solvingContext;
-
-  @Before
-  public void init() throws InvalidConfigurationException {
-    Solvers solver = Solvers.MATHSAT5;
-
-    LogManager logger = LogManager.createNullLogManager();
-
-    Configuration config = Configuration.defaultConfiguration();
-
-    ShutdownNotifier notifier = ShutdownNotifier.createDummy();
-
-    SolverContextFactory solverFactory = new SolverContextFactory(config, logger, notifier);
-
-    solvingContext = solverFactory.generateContext(solver);
-    fmgr = new FormulaManagerView(solvingContext.getFormulaManager(), config, logger);
-    bvfmgr = fmgr.getBitvectorFormulaManager();
+  private SMTHeap init() throws InvalidConfigurationException {
+    requireUnsatCore();
+    requireBitvectors();
     MachineModel model = MachineModel.LINUX64;
     FormulaEncodingWithPointerAliasingOptions options =
         new FormulaEncodingWithPointerAliasingOptions(config);
     TypeHandlerWithPointerAliasing handler =
         new TypeHandlerWithPointerAliasing(logger, model, options);
-    heap = new SMTHeapWithByteArray(fmgr, handler, model);
+    return new SMTHeapWithByteArray(new FormulaManagerView(mgr, config, logger), handler, model);
+  }
+
+  @Override
+  protected Solvers solverToUse(){
+    return Solvers.MATHSAT5;
   }
 
   @Test
-  public void test64BitVector() throws InterruptedException, SolverException {
+  public void test64BitVector()
+      throws InterruptedException, SolverException, InvalidConfigurationException {
 
-    final BitvectorFormula value = storeBitVector(64, 0x1122334455667788L);
+    SMTHeap heap = init();
 
-    final BitvectorFormula result = readBitVector(64);
 
-    BooleanFormula testEquality = bvfmgr.equal(value, result);
-    ProverEnvironment prover1 = solvingContext.newProverEnvironment(ProverOptions.GENERATE_ALL_SAT);
+    final BitvectorFormula value = storeBitVector(heap, 64, 0x1122334455667788L);
 
-    prover1.addConstraint(testEquality);
+    final BitvectorFormula result = readBitVector(heap,64);
 
-    Assert.assertFalse(prover1.isUnsat());
-    prover1.close();
+    BooleanFormula testEquality = bvmgr.equal(value, result);
+    this.assertThatFormula(testEquality).isTautological();
   }
 
   @Test
-  public void test32BitVector() throws InterruptedException, SolverException {
+  public void test32BitVector()
+      throws InterruptedException, SolverException, InvalidConfigurationException {
 
+    SMTHeap heap = init();
+    final BitvectorFormula value = storeBitVector(heap,32, 0x112233L);
 
-    final BitvectorFormula value = storeBitVector(32, 0x112233L);
-
-    final BitvectorFormula result = readBitVector(32);
-
-    BooleanFormula testEquality = bvfmgr.equal(value, result);
-    ProverEnvironment prover1 = solvingContext.newProverEnvironment(ProverOptions.GENERATE_ALL_SAT);
-
-    prover1.addConstraint(testEquality);
-
-    Assert.assertFalse(prover1.isUnsat());
-    prover1.close();
+    final BitvectorFormula result = readBitVector(heap, 32);
+    BooleanFormula testEquality = bvmgr.equal(value, result);
+    this.assertThatFormula(testEquality).isTautological();
   }
 
-  private BitvectorFormula storeBitVector(int length, long value){
+  private BitvectorFormula storeBitVector(SMTHeap heap, int length, long value){
     final String targetName = "testTarget"+length;
     final FormulaType<BitvectorFormula> pTargetType =
-        fmgr.getFormulaType(bvfmgr.makeBitvector(length, 0));
+        mgr.getFormulaType(bvmgr.makeBitvector(length, 0));
     final int oldIndex = 0;
     final int newIndex = 1;
-    final BitvectorFormula address = bvfmgr.makeBitvector(64, 1234+length);
-    final BitvectorFormula bvValue = bvfmgr.makeBitvector(length, value);
+    final BitvectorFormula address = bvmgr.makeBitvector(64, 1234+length);
+    final BitvectorFormula bvValue = bvmgr.makeBitvector(length, value);
 
     @SuppressWarnings("unused")
     BooleanFormula bf = heap.makePointerAssignment(targetName, pTargetType, oldIndex, newIndex, address, bvValue);
@@ -106,56 +79,40 @@ public class SMTHeapTest {
     return bvValue;
   }
 
-  private BitvectorFormula readBitVector(int length){
+  private BitvectorFormula readBitVector(SMTHeap heap, int length){
     final String targetName = "testTarget"+length;
     final FormulaType<BitvectorFormula> pTargetType =
-        fmgr.getFormulaType(bvfmgr.makeBitvector(length, 0));
+        mgr.getFormulaType(bvmgr.makeBitvector(length, 0));
     final int newIndex = 1;
-    final BitvectorFormula address = bvfmgr.makeBitvector(64, 1234+length);
+    final BitvectorFormula address = bvmgr.makeBitvector(64, 1234+length);
     return heap.makePointerDereference(targetName, pTargetType, newIndex, address);
 }
 
 
   @Test
-  public void testMixedBitVectors() throws InterruptedException, SolverException {
+  public void testMixedBitVectors()
+      throws InterruptedException, SolverException, InvalidConfigurationException {
 
-    final BitvectorFormula value8 = storeBitVector(8, 0x1);
-    final BitvectorFormula value16 = storeBitVector(16, 0x112);
-    final BitvectorFormula value32 = storeBitVector(32, 0x112233L);
-    final BitvectorFormula value64 = storeBitVector(64, 0x1122334455667788L);
-    final BitvectorFormula result8 = readBitVector(8);
-    final BitvectorFormula result16 = readBitVector(16);
-    final BitvectorFormula result32 = readBitVector(32);
-    final BitvectorFormula result64 = readBitVector(64);
+    SMTHeap heap = init();
+    final BitvectorFormula value8 = storeBitVector(heap, 8, 0x1);
+    final BitvectorFormula value16 = storeBitVector(heap, 16, 0x112);
+    final BitvectorFormula value32 = storeBitVector(heap, 32, 0x112233L);
+    final BitvectorFormula value64 = storeBitVector(heap, 64, 0x1122334455667788L);
+    final BitvectorFormula result8 = readBitVector(heap, 8);
+    final BitvectorFormula result16 = readBitVector(heap, 16);
+    final BitvectorFormula result32 = readBitVector(heap, 32);
+    final BitvectorFormula result64 = readBitVector(heap, 64);
 
-    BooleanFormula testEquality8 = bvfmgr.equal(value8, result8);
-    ProverEnvironment prover8 = solvingContext.newProverEnvironment(ProverOptions.GENERATE_ALL_SAT);
-    prover8.addConstraint(testEquality8);
-    Assert.assertFalse(prover8.isUnsat());
-    prover8.close();
+    BooleanFormula testEquality8 = bvmgr.equal(value8, result8);
+    this.assertThatFormula(testEquality8).isTautological();
 
+    BooleanFormula testEquality16 = bvmgr.equal(value16, result16);
+    this.assertThatFormula(testEquality16).isTautological();
 
-    BooleanFormula testEquality16 = bvfmgr.equal(value16, result16);
-    ProverEnvironment prover16 = solvingContext.newProverEnvironment(ProverOptions.GENERATE_ALL_SAT);
-    prover16.addConstraint(testEquality16);
-    Assert.assertFalse(prover16.isUnsat());
-    prover16.close();
+    BooleanFormula testEquality32 = bvmgr.equal(value32, result32);
+    this.assertThatFormula(testEquality32).isTautological();
 
-
-    BooleanFormula testEquality32 = bvfmgr.equal(value32, result32);
-    ProverEnvironment prover32 = solvingContext.newProverEnvironment(ProverOptions.GENERATE_ALL_SAT);
-    prover32.addConstraint(testEquality32);
-    Assert.assertFalse(prover32.isUnsat());
-    prover32.close();
-
-
-
-    BooleanFormula testEquality64 = bvfmgr.equal(value64, result64);
-    ProverEnvironment prover64 = solvingContext.newProverEnvironment(ProverOptions.GENERATE_ALL_SAT);
-    prover64.addConstraint(testEquality64);
-    Assert.assertFalse(prover64.isUnsat());
-    prover64.close();
-
-
+    BooleanFormula testEquality64 = bvmgr.equal(value64, result64);
+    this.assertThatFormula(testEquality64).isTautological();
   }
 }
