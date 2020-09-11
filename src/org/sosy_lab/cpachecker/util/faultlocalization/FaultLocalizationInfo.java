3fceee67768871acef3d3b2aa9d0d702d7de2d76
mkettl <mkettl@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2020-09-11 10:41:57 +0000

    Removed unnecessary imports
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/kettl-fault-localization@34940 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/util/faultlocalization/FaultLocalizationInfo.java
+++ src/org/sosy_lab/cpachecker/util/faultlocalization/FaultLocalizationInfo.java
@@ -23,7 +23,6 @@ import org.sosy_lab.common.JSON;
 import org.sosy_lab.cpachecker.cfa.model.CFAEdge;
 import org.sosy_lab.cpachecker.core.counterexample.CFAPathWithAdditionalInfo;
 import org.sosy_lab.cpachecker.core.counterexample.CounterexampleInfo;
-import org.sosy_lab.cpachecker.util.faultlocalization.appendables.FaultInfo.InfoType;
 
 public class FaultLocalizationInfo extends CounterexampleInfo {
 
