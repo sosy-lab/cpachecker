fd3b54a6892addf26ef05e7e02686bbe350a3418
mkettl <mkettl@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2020-09-28 07:25:31 +0000

    Add missing licences
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/kettl-fault-localization@35274 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/core/algorithm/fault_localization/by_unsatisfiability/trace_formula/package-info.java
+++ src/org/sosy_lab/cpachecker/core/algorithm/fault_localization/by_unsatisfiability/trace_formula/package-info.java
@@ -2,12 +2,8 @@
 // a tool for configurable software verification:
 // https://cpachecker.sosy-lab.org
 //
-// SPDX-FileCopyrightText: 2007-2020 Dirk Beyer <https://www.sosy-lab.org>
+// SPDX-FileCopyrightText: 2020 Dirk Beyer <https://www.sosy-lab.org>
 //
 // SPDX-License-Identifier: Apache-2.0
 
-/**
- * Implementation of a trace formula. A trace formula is the conjunction of every edge from the
- * start to the error location.
- */
 package org.sosy_lab.cpachecker.core.algorithm.fault_localization.by_unsatisfiability.trace_formula;
