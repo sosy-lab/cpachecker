4cd3a8e142ddbebb919b4acccf105fed5ffe0631
lemberge <lemberge@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2020-10-07 08:34:37 +0000

    Remove unused import
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/distance_metrics@35379 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/core/algorithm/explainer/AbstractDistanceMetric.java
+++ src/org/sosy_lab/cpachecker/core/algorithm/explainer/AbstractDistanceMetric.java
@@ -11,7 +11,6 @@ import com.google.common.base.Preconditions;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 import org.sosy_lab.cpachecker.cfa.model.CFAEdge;
