093b31f898b50bfa6901433fe22bbb8fbeb24e3c
cgirstenbrei <cgirstenbrei@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2020-10-11 12:28:31 +0000

    Improve code of LtlFormula sublasses
    For example, remove duplicate code and use collection helpers.
    Also remove all public fields (some where even mutable).
    
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/legion_dev@35445 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/util/ltl/formulas/Disjunction.java
+++ src/org/sosy_lab/cpachecker/util/ltl/formulas/Disjunction.java
@@ -25,6 +25,7 @@ package org.sosy_lab.cpachecker.util.ltl.formulas;
 
 import com.google.common.collect.ImmutableSet;
 import java.util.Arrays;
+import org.sosy_lab.common.collect.Collections3;
 import org.sosy_lab.cpachecker.util.ltl.LtlFormulaVisitor;
 
 public final class Disjunction extends PropositionalFormula {
@@ -59,7 +60,7 @@ public final class Disjunction extends PropositionalFormula {
       }
 
       if (child instanceof Disjunction) {
-        builder.addAll(((Disjunction) child).children);
+        builder.addAll(((Disjunction) child).getChildren());
       } else {
         builder.add(child);
       }
@@ -84,15 +85,8 @@ public final class Disjunction extends PropositionalFormula {
 
   @Override
   public LtlFormula not() {
-    ImmutableSet.Builder<LtlFormula> builder = ImmutableSet.builder();
-
-    for (LtlFormula child : children) {
-      builder.add(child.not());
-    }
-
-    ImmutableSet<LtlFormula> set = builder.build();
-
-    return new Conjunction(set);
+    return new Conjunction(
+        Collections3.transformedImmutableSetCopy(getChildren(), LtlFormula::not));
   }
 
   @Override
