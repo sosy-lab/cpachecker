9172ab2e6945e638c49c6091e9fd3ea3ffe8ccd4
cgirstenbrei <cgirstenbrei@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2020-10-13 07:13:14 +0000

    Just cleanup unused
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/legion-integration@35515 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/core/algorithm/legion/LegionAlgorithm.java
+++ src/org/sosy_lab/cpachecker/core/algorithm/legion/LegionAlgorithm.java
@@ -19,12 +19,10 @@
  */
 package org.sosy_lab.cpachecker.core.algorithm.legion;
 
-import static org.junit.Assert.assertThat;
 import static org.sosy_lab.java_smt.test.ProverEnvironmentSubject.assertThat;
 
 import java.util.Collection;
 import java.util.LinkedList;
-import java.util.NavigableSet;
 import java.util.Random;
 import java.util.logging.Level;
 
@@ -38,8 +36,6 @@ import org.sosy_lab.cpachecker.core.interfaces.AbstractState;
 import org.sosy_lab.cpachecker.core.interfaces.ConfigurableProgramAnalysis;
 import org.sosy_lab.cpachecker.core.interfaces.Property;
 import org.sosy_lab.cpachecker.core.reachedset.ReachedSet;
-import org.sosy_lab.cpachecker.cpa.arg.ARGState;
-import org.sosy_lab.cpachecker.cpa.composite.CompositeState;
 import org.sosy_lab.cpachecker.cpa.predicate.PredicateAbstractState;
 import org.sosy_lab.cpachecker.cpa.predicate.PredicateCPA;
 import org.sosy_lab.cpachecker.cpa.value.RandomValueAssigner;
@@ -49,8 +45,6 @@ import org.sosy_lab.cpachecker.exceptions.CPAEnabledAnalysisPropertyViolationExc
 import org.sosy_lab.cpachecker.exceptions.CPAException;
 import org.sosy_lab.cpachecker.util.AbstractStates;
 import org.sosy_lab.cpachecker.util.CPAs;
-import org.sosy_lab.cpachecker.util.predicates.pathformula.PathFormula;
-import org.sosy_lab.cpachecker.util.predicates.pathformula.SSAMap;
 import org.sosy_lab.cpachecker.util.predicates.smt.Solver;
 import org.sosy_lab.java_smt.api.BooleanFormula;
 import org.sosy_lab.java_smt.api.Model;
@@ -63,11 +57,14 @@ public class LegionAlgorithm implements Algorithm {
     private final Algorithm algorithm;
     private final LogManager logger;
     private final int maxIterations;
+    @SuppressWarnings("unused")
     private final ConfigurableProgramAnalysis cpa;
     private RandomValueAssigner unknownValueHandler;
     private Solver solver;
     private int initialPasses;
     private int fuzzingPasses;
+    @SuppressWarnings("unused")
+    private ShutdownNotifier shutdownNotifier;
 
     public LegionAlgorithm(
             final Algorithm algorithm,
@@ -78,6 +75,7 @@ public class LegionAlgorithm implements Algorithm {
             throws InvalidConfigurationException {
         this.algorithm = algorithm;
         this.logger = pLogger;
+        this.shutdownNotifier = shutdownNotifier;
         this.initialPasses = 1;
         this.fuzzingPasses = 2;
         this.maxIterations = 10;
@@ -214,7 +212,7 @@ public class LegionAlgorithm implements Algorithm {
         logger.log(Level.INFO, "Fuzzing target.");
         for (int i = 0; i < pPasses; i++) {
             // Run algorithm and collect result
-            AlgorithmStatus status = pAlgorithm.run(pReachedSet);
+            pAlgorithm.run(pReachedSet);
 
             // If an error was found, stop execution
             Collection<Property> violatedProperties = pReachedSet.getViolatedProperties();
@@ -229,27 +227,5 @@ public class LegionAlgorithm implements Algorithm {
         }
 
         return pReachedSet;
-    }
-
-    void predicateInfo(AbstractState as) {
-        CompositeState cs = (CompositeState) as;
-        PredicateAbstractState ps = (PredicateAbstractState) cs.getWrappedStates().asList().get(4);
-        if (ps == null) {
-            this.logger.log(Level.SEVERE, "No ps");
-            return;
-        }
-        PathFormula f = ps.getPathFormula();
-        this.logger.log(Level.SEVERE, "Formula: " + f);
-        NavigableSet<String> vars = f.getSsa().allVariables();
-        if (!vars.isEmpty()) {
-            SSAMap map = f.getSsa();
-            int index = map.getIndex("main::i");
-            // m
-
-            // this.logger.log(Level.SEVERE, f.getSsa().getIndex("main::i"));
-            // this.logger.log(Level.SEVERE, vars.toArray()[0]);
-        }
-        // if (f.getSsa().containsVariable("main::i@2")){
-        // }
     }
 }
