29dec387e1599515d73c7a535289a3693e2ddaa3
cgirstenbrei <cgirstenbrei@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2020-10-13 07:07:06 +0000

    ~ 1/8ths probability of hitting error
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/legion-integration@35494 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- doc/examples/legion/example.c
+++ doc/examples/legion/example.c
@@ -2,7 +2,7 @@ extern void __VERIFIER_error() __attribute__ ((__noreturn__));
 
 int main() {
   int a = __VERIFIER_nondet_int();
-  if (a > 10){
+  if (a < -1610612736){
     __VERIFIER_error();
   }
 }
