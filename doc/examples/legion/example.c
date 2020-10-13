121874942057e2f5cf5b655376260c8df8f8ff69
cgirstenbrei <cgirstenbrei@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2020-10-13 07:07:47 +0000

    Add svcomp launch
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/legion-integration@35498 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- doc/examples/legion/example.c
+++ doc/examples/legion/example.c
@@ -1,8 +1,36 @@
 extern void __VERIFIER_error() __attribute__ ((__noreturn__));
 
 int main() {
-  int a = __VERIFIER_nondet_int();
-  if (a < -1610612736){
-    __VERIFIER_error();
-  }
+  
+    // int a = __VERIFIER_nondet_int();
+    // int b = 0;
+    int b;
+    b = 0;
+    // int c;
+    // if (a < -1610612736){
+    //   b = __VERIFIER_nondet_int();
+    // } else {
+    //   b = 2;
+    // }
+
+    // int x[10];
+    // x = {1,2,3,4,5,6,7,8,9,10};
+    // for (int i = 0; i < 10; i++) {
+    //   x[i] = 3;
+    // }
+    // a = a + b;
+    // int x = a;
+
+    // for (int i=0; i < 8; i=i+2){
+    //   x = x + 2;
+    //   a = a + 1;
+    // }
+
+  // a = __VERIFIER_nondet_int();
+  // if (a < x){  
+  //   int b = __VERIFIER_nondet_int();
+  //   if (b < -1610612736){
+  //     __VERIFIER_error();
+  //   }
+  // }
 }
