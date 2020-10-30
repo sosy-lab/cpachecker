08ca845c605c25a1442d0b3ec58e0154b09b16ea
pwaldinger <pwaldinger@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2020-10-30 08:58:06 +0000

    add package info for 'algorithm.summaries'
    #796
    
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/simple-code-summaries-for-loop-free-programs@35709 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

---
+++ src/org/sosy_lab/cpachecker/core/algorithm/summaries/package-info.java
@@ -0,0 +1,14 @@
+// This file is part of CPAchecker,
+// a tool for configurable software verification:
+// https://cpachecker.sosy-lab.org
+//
+// SPDX-FileCopyrightText: 2020 Dirk Beyer <https://www.sosy-lab.org>
+//
+// SPDX-License-Identifier: Apache-2.0
+
+/**
+ * Classes which represent algorithms and data structures related to Configurable Code Summaries.
+ * <br/>
+ * Configurable Code Summaries are an experimental feature to be added to CPAchecker (see #796).
+ */
+package org.sosy_lab.cpachecker.core.algorithm.summaries;
