c8d4e24fae0a6a44c40b0b831448644de41cae21
spiessl <spiessl@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2020-12-03 07:47:12 +0000

    Improve readability of string manipulation
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@36250 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/cmdline/CPAMain.java
+++ src/org/sosy_lab/cpachecker/cmdline/CPAMain.java
@@ -15,7 +15,6 @@ import static org.sosy_lab.common.collect.Collections3.transformedImmutableSetCo
 import static org.sosy_lab.common.io.DuplicateOutputStream.mergeStreams;
 
 import com.google.common.annotations.VisibleForTesting;
-import com.google.common.base.Joiner;
 import com.google.common.base.Preconditions;
 import com.google.common.base.Splitter;
 import com.google.common.collect.ImmutableList;
@@ -715,7 +714,7 @@ public class CPAMain {
       WitnessOptions pOptions, Map<String, String> pOverrideOptions) {
     String specs = pOverrideOptions.get(SPECIFICATION_OPTION);
     String witnessSpec = pOptions.witness.toString();
-    specs = specs == null ? witnessSpec : Joiner.on(',').join(specs, witnessSpec.toString());
+    specs = specs == null ? witnessSpec : (specs + "," + witnessSpec.toString());
     pOverrideOptions.put(SPECIFICATION_OPTION, specs);
   }
 
