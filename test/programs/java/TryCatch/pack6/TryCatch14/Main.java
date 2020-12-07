13afe203ca99d8784f9d0baeab9ce3d4e5dcd35d
smassard <smassard@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2020-12-07 15:31:23 +0000

    Update License headers
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/java-development@36279 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- test/programs/java/TryCatch/pack6/TryCatch14/Main.java
+++ test/programs/java/TryCatch/pack6/TryCatch14/Main.java
@@ -1,3 +1,11 @@
+// This file is part of CPAchecker,
+// a tool for configurable software verification:
+// https://cpachecker.sosy-lab.org
+//
+// SPDX-FileCopyrightText: 2020 Dirk Beyer <https://www.sosy-lab.org>
+//
+// SPDX-License-Identifier: Apache-2.0
+
 /*
  * Origin of the benchmark:
  *     license: 4-clause BSD (see /java/jbmc-regression/LICENSE)
