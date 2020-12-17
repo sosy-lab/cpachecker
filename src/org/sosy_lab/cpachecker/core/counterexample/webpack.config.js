fe8f06fe05b9b42c3f04efa6c9ae7a50a7070e24
mlachner <mlachner@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2020-12-17 16:05:16 +0000

    Separate bundling of dependencies into vendor files
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/report-js-build@36449 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/core/counterexample/webpack.config.js
+++ src/org/sosy_lab/cpachecker/core/counterexample/webpack.config.js
@@ -1,3 +1,11 @@
+// This file is part of CPAchecker,
+// a tool for configurable software verification:
+// https://cpachecker.sosy-lab.org
+//
+// SPDX-FileCopyrightText: 2020 Dirk Beyer <https://www.sosy-lab.org>
+//
+// SPDX-License-Identifier: Apache-2.0
+
 const HtmlWebpackPlugin = require('html-webpack-plugin');;
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const TerserPlugin = require("terser-webpack-plugin");
@@ -10,6 +18,7 @@ module.exports = {
     entry: './report.js',
     output: {
         filename: 'bundle.js',
+        chunkFilename: '[name].js',
         path: path.join(__dirname + '/build')
     },
     devServer: {
@@ -22,7 +31,8 @@ module.exports = {
             "template": devFolder + "index.html"
         }),
         new MiniCssExtractPlugin({
-            "filename": "bundle.css"
+            "filename": "bundle.css",
+            "chunkFilename": "[name].css",
         })
     ],
     module: {
@@ -44,9 +54,25 @@ module.exports = {
     optimization: {
         minimize: true,
         minimizer: [
-            new TerserPlugin(),
+            new TerserPlugin({
+              // Don't extract license comments, we bundle them separately
+              extractComments: false,
+            }),
             new CssMinimizerPlugin(),
         ],
+        runtimeChunk: false,
+        splitChunks: {
+          chunks: "all",
+          cacheGroups: {
+            vendors: {
+              chunks: "all",
+              name: "vendors",
+              test: /(node_modules|src\/dependency_licenses\/licenses\.json)/,
+            },
+          },
+        },
+        // Make vendor bundle change less often even if our own code changes.
+        occurrenceOrder: false,
     },
     resolve: {
         alias: {
