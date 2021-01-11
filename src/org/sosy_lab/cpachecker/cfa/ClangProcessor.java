b322814dabc0d316b633b26bd00ea303db374d30
kcimbalnik <kcimbalnik@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2021-01-11 18:24:12 +0000

    See #354: Remove duplicate code in CPreprocessor and ClangProcessor
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/354-clang-llvm_clean_history@36601 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/cfa/ClangProcessor.java
+++ src/org/sosy_lab/cpachecker/cfa/ClangProcessor.java
@@ -9,30 +9,19 @@
 
 package org.sosy_lab.cpachecker.cfa;
 
-import com.google.common.base.CharMatcher;
-import com.google.common.base.Splitter;
-import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.Lists;
-import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
-import java.io.IOException;
-import java.nio.charset.Charset;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.List;
-import java.util.logging.Level;
-import org.sosy_lab.common.ProcessExecutor;
 import org.sosy_lab.common.configuration.Configuration;
 import org.sosy_lab.common.configuration.FileOption;
 import org.sosy_lab.common.configuration.FileOption.Type;
 import org.sosy_lab.common.configuration.InvalidConfigurationException;
 import org.sosy_lab.common.configuration.Option;
 import org.sosy_lab.common.configuration.Options;
-import org.sosy_lab.common.io.IO;
 import org.sosy_lab.common.log.LogManager;
 import org.sosy_lab.cpachecker.exceptions.CParserException;
 
-@Options(prefix="parser")
-public class ClangProcessor {
+@Options(prefix = "parser")
+public class ClangProcessor extends Preprocessor {
 
   @Option(description="The command line for calling the clang processor. " +
                       "May contain binary name and arguments, but won't be expanded by a shell. " +
@@ -57,36 +46,12 @@ public class ClangProcessor {
   @FileOption(Type.OUTPUT_DIRECTORY)
   private Path dumpDirectory = Paths.get("processed");
 
-  private final LogManager logger;
-
   public ClangProcessor(Configuration config, LogManager pLogger) throws InvalidConfigurationException {
+    super(pLogger);
     config.inject(this);
-    logger = pLogger;
     if (dumpDirectory != null) {
       dumpDirectory = dumpDirectory.toAbsolutePath().normalize();
     }
-  }
-
-  public Path getAndWriteDumpedFile(String programCode, String file) {
-    if (dumpResults && dumpDirectory != null) {
-      final Path dumpFile = dumpDirectory.resolve(file.replaceAll("\\.c", ".ll")).normalize();
-      if (dumpFile.startsWith(dumpDirectory)) {
-        try {
-          IO.writeFile(dumpFile, Charset.defaultCharset(), programCode);
-        } catch (IOException e) {
-          logger.logUserException(Level.WARNING, e, "Cannot write result of preprocessing to file");
-        }
-      } else {
-        logger.logf(
-            Level.WARNING,
-            "Cannot dump result of preprocessing file %s, because path is outside the current"
-                + " directory and the result would end up outside the output directory.",
-            file);
-      }
-      return dumpFile;
-    }
-
-    return null;
   }
 
   public Path preprocessAndGetDumpedFile(String file) throws CParserException, InterruptedException {
@@ -94,87 +59,28 @@ public class ClangProcessor {
     return getAndWriteDumpedFile(result, file);
   }
 
-  public String preprocess(String file) throws CParserException, InterruptedException {
-    String result = preprocess0(file);
-    getAndWriteDumpedFile(result, file);
-    return result;
+  @Override
+  protected String getName() {
+    return "clang";
   }
 
-  @SuppressWarnings("JdkObsolete") // buffer is accessed from several threads
-  private String preprocess0(String file) throws CParserException, InterruptedException {
-    // create command line
-    List<String> argList =
-        Lists.newArrayList(
-            Splitter.on(CharMatcher.whitespace()).omitEmptyStrings().split(clang));
-    argList.add(file);
-    String[] args = argList.toArray(new String[0]);
-
-    logger.log(Level.FINE, "Running clang", argList);
-    try {
-      ClangProcessorExecutor executor = new ClangProcessorExecutor(logger, args);
-      executor.sendEOF();
-      int exitCode = executor.join();
-      logger.log(Level.FINE, "Clang finished");
-
-      if (exitCode != 0) {
-        throw new CParserException("Clang failed with exit code " + exitCode);
-      }
-
-      if (executor.errorOutputCount > 0) {
-        logger.log(
-            Level.WARNING,
-            "Clang returned successfully, but printed warnings. Please check the log above!");
-      }
-
-      if (executor.buffer == null) {
-        return "";
-      }
-      return executor.buffer.toString();
-
-    } catch (IOException e) {
-      throw new CParserException("Clang failed", e);
-    }
+  @Override
+  protected String getCommandLine() {
+    return clang;
   }
 
+  @Override
+  protected boolean dumpResults() {
+    return dumpResults;
+  }
 
-  private static class ClangProcessorExecutor extends ProcessExecutor<IOException> {
+  @Override
+  protected Path getDumpDirectory() {
+    return dumpDirectory;
+  }
 
-    private static final int MAX_ERROR_OUTPUT_SHOWN = 10;
-    private static final ImmutableMap<String, String> ENV_VARS = ImmutableMap.of("LANG", "C");
-
-    @SuppressFBWarnings(value = "VO_VOLATILE_INCREMENT",
-        justification = "Written only by one thread")
-    private volatile int errorOutputCount = 0;
-    private volatile StringBuffer buffer;
-
-    public ClangProcessorExecutor(LogManager logger, String[] args) throws IOException {
-      super(logger, IOException.class, ENV_VARS, args);
-    }
-
-    @Override
-    @SuppressWarnings("NonAtomicVolatileUpdate") // errorOutputCount written only by one thread
-    protected void handleErrorOutput(String pLine) throws IOException {
-      if (errorOutputCount == MAX_ERROR_OUTPUT_SHOWN) {
-        logger.log(Level.WARNING, "Skipping further clang error output...");
-        errorOutputCount++;
-
-      } else if (errorOutputCount < MAX_ERROR_OUTPUT_SHOWN) {
-        errorOutputCount++;
-        super.handleErrorOutput(pLine);
-      }
-    }
-
-    @Override
-    @SuppressWarnings("JdkObsolete") // buffer is accessed from several threads
-    protected void handleOutput(String pLine) {
-      if (buffer == null) {
-        buffer = new StringBuffer();
-      }
-      buffer.append(pLine);
-      buffer.append('\n');
-    }
-
-    @Override
-    protected void handleExitCode(int pCode) {}
+  @Override
+  protected String getDumpFileOfFile(String file) {
+    return file.replaceAll("\\.c", ".ll");
   }
 }
