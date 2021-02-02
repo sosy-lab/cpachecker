a0ffa88e77ffee99bd64d648a3eb7300c41feb9a
spiessl <spiessl@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2021-02-02 10:42:33 +0000

    Add some example code for how to use CPAs/options
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/invariantsampling@36722 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/cpa/invariantsampling/TestCPA.java
+++ src/org/sosy_lab/cpachecker/cpa/invariantsampling/TestCPA.java
@@ -8,25 +8,62 @@
 
 package org.sosy_lab.cpachecker.cpa.invariantsampling;
 
-import org.checkerframework.checker.nullness.qual.Nullable;
+import com.google.common.collect.ImmutableList;
+import java.util.Collection;
+import org.sosy_lab.common.configuration.Configuration;
+import org.sosy_lab.common.configuration.InvalidConfigurationException;
+import org.sosy_lab.common.configuration.Option;
+import org.sosy_lab.common.configuration.Options;
+import org.sosy_lab.cpachecker.cfa.model.CFAEdge;
 import org.sosy_lab.cpachecker.cfa.model.CFANode;
 import org.sosy_lab.cpachecker.core.defaults.AbstractCPA;
+import org.sosy_lab.cpachecker.core.defaults.AutomaticCPAFactory;
+import org.sosy_lab.cpachecker.core.defaults.SingleEdgeTransferRelation;
 import org.sosy_lab.cpachecker.core.interfaces.AbstractState;
+import org.sosy_lab.cpachecker.core.interfaces.CPAFactory;
+import org.sosy_lab.cpachecker.core.interfaces.Precision;
 import org.sosy_lab.cpachecker.core.interfaces.StateSpacePartition;
 import org.sosy_lab.cpachecker.core.interfaces.TransferRelation;
+import org.sosy_lab.cpachecker.exceptions.CPATransferException;
 
+@Options(prefix = "invariantsampling")
 public class TestCPA extends AbstractCPA {
 
-  protected TestCPA(
-      String mergeType,
-      String stopType,
-      @Nullable TransferRelation transfer) {
-    super(mergeType, stopType, transfer);
+  @Option(
+      secure = true,
+      // name = "whichStopType",
+      description = "Determines which stop operator to use for the invariant sampling analysis.")
+  private String stopType = "sep";
+
+  public static CPAFactory factory() {
+    return AutomaticCPAFactory.forType(TestCPA.class);
+  }
+
+  protected TestCPA(Configuration pConfig) throws InvalidConfigurationException {
+    super("sep", "sep", new InvSamplingTransferRelation());
+    pConfig.inject(this);
   }
 
   @Override
   public AbstractState getInitialState(
       CFANode node, StateSpacePartition partition) throws InterruptedException {
-    return null;
+    return new InvSamplingState();
+  }
+
+  private static class InvSamplingState implements AbstractState {
+    public InvSamplingState() {}
+  }
+
+  private static class InvSamplingTransferRelation extends SingleEdgeTransferRelation implements TransferRelation {
+
+    public InvSamplingTransferRelation() {}
+
+    @Override
+    public Collection<? extends AbstractState> getAbstractSuccessorsForEdge(
+        AbstractState pState, Precision pPrecision, CFAEdge pCfaEdge)
+        throws CPATransferException, InterruptedException {
+      // TODO Auto-generated method stub
+      return ImmutableList.of(pState);
+    }
   }
 }
