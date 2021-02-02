9a3098e8fd69cdef6c043390f14f113a086569f7
dmoessner <dmoessner@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2021-02-02 10:04:08 +0000

    Make TestCPA extend AbstractCPA
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/invariantsampling@36720 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/cpa/invariantsampling/TestCPA.java
+++ src/org/sosy_lab/cpachecker/cpa/invariantsampling/TestCPA.java
@@ -8,8 +8,25 @@
 
 package org.sosy_lab.cpachecker.cpa.invariantsampling;
 
-public class TestCPA {
+import org.checkerframework.checker.nullness.qual.Nullable;
+import org.sosy_lab.cpachecker.cfa.model.CFANode;
+import org.sosy_lab.cpachecker.core.defaults.AbstractCPA;
+import org.sosy_lab.cpachecker.core.interfaces.AbstractState;
+import org.sosy_lab.cpachecker.core.interfaces.StateSpacePartition;
+import org.sosy_lab.cpachecker.core.interfaces.TransferRelation;
 
+public class TestCPA extends AbstractCPA {
 
+  protected TestCPA(
+      String mergeType,
+      String stopType,
+      @Nullable TransferRelation transfer) {
+    super(mergeType, stopType, transfer);
+  }
 
+  @Override
+  public AbstractState getInitialState(
+      CFANode node, StateSpacePartition partition) throws InterruptedException {
+    return null;
+  }
 }
