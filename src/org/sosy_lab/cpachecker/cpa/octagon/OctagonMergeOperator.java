sosy-lab/cpachecker

commit 378ce078920b11eec39c871d18154d2ed53663da

refactor the creation and usage of the merge operator, the naming should now be more consistent
git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@12847 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

src/org/sosy_lab/cpachecker/cpa/octagon/OctagonMergeOperator.java
@@ -1,0 +1,99 @@
+/*
+ *  CPAchecker is a tool for configurable software verification.
+ *  This file is part of CPAchecker.
+ *
+ *  Copyright (C) 2007-2014  Dirk Beyer
+ *  All rights reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *
+ *  CPAchecker web page:
+ *    http://cpachecker.sosy-lab.org
+ */
+package org.sosy_lab.cpachecker.cpa.octagon;
+
+import org.sosy_lab.common.configuration.Configuration;
+import org.sosy_lab.common.configuration.InvalidConfigurationException;
+import org.sosy_lab.common.configuration.Option;
+import org.sosy_lab.common.configuration.Options;
+import org.sosy_lab.cpachecker.core.defaults.MergeSepOperator;
+import org.sosy_lab.cpachecker.core.interfaces.AbstractState;
+import org.sosy_lab.cpachecker.core.interfaces.MergeOperator;
+import org.sosy_lab.cpachecker.core.interfaces.Precision;
+import org.sosy_lab.cpachecker.exceptions.CPAException;
+
+@Options(prefix="cpa.octagon.mergeop")
+public class OctagonMergeOperator {
+
+  private final OctagonDomain domain;
+
+  @Option(name="type", toUppercase=true, values={"SEP", "JOIN", "WIDENING"},
+      description="of which type should the merge be?")
+  private String type = "SEP";
+
+  @Option(name="onlyMergeAtLoopHeads", description="with this option enabled"
+      + " the states are only merged at loop heads")
+  private boolean onlyMergeAtLoopHeads = false;
+
+  public static MergeOperator getInstance(OctagonDomain domain, Configuration config) throws InvalidConfigurationException {
+    OctagonMergeOperator mergeOp = new OctagonMergeOperator(domain, config);
+
+    switch (mergeOp.type) {
+    case "SEP": return MergeSepOperator.getInstance();
+    case "JOIN": return mergeOp.new OctagonMergeJoinOperator(domain, config);
+    case "WIDENING": return mergeOp.new OctagonMergeWideningOperator(domain, config);
+    default:
+      throw new InvalidConfigurationException("Unknown type for merge operator");
+    }
+  }
+
+  private OctagonMergeOperator(OctagonDomain domain, Configuration config) throws InvalidConfigurationException {
+    config.inject(this);
+    this.domain = domain;
+  }
+
+  class OctagonMergeJoinOperator extends OctagonMergeOperator implements MergeOperator {
+
+    private OctagonMergeJoinOperator(OctagonDomain domain, Configuration config) throws InvalidConfigurationException {
+      super(domain, config);
+    }
+
+    @Override
+    public AbstractState merge(AbstractState el1, AbstractState el2, Precision p) throws CPAException {
+      if (onlyMergeAtLoopHeads) {
+        if (!(((OctagonState)el1).isLoopHead() && ((OctagonState)el2).isLoopHead())) {
+          return el2;
+        }
+      }
+      return domain.join(el1, el2);
+    }
+  }
+
+  class OctagonMergeWideningOperator extends OctagonMergeOperator implements MergeOperator {
+
+    private OctagonMergeWideningOperator(OctagonDomain domain, Configuration config) throws InvalidConfigurationException {
+      super(domain, config);
+    }
+
+    @Override
+    public AbstractState merge(AbstractState el1, AbstractState el2, Precision p) throws CPAException {
+      if (onlyMergeAtLoopHeads) {
+        if (!(((OctagonState)el1).isLoopHead() && ((OctagonState)el2).isLoopHead())) {
+          return el2;
+        }
+      }
+      return domain.widening((OctagonState)el1, (OctagonState)el2);
+    }
+  }
+}

config/octagonAnalysis-mergeJoin.properties
@@ -2,5 +2,4 @@
 
 analysis.traversal.useReversePostorder = true
 
-cpa.octagon.merge        = JOIN
-cpa.octagon.mergeop.type = NORMAL
+cpa.octagon.mergeop.type = JOIN

config/octagonAnalysis-mergeWidening.properties
@@ -2,5 +2,4 @@
 
 analysis.traversal.useReversePostorder = true
 
-cpa.octagon.merge        = JOIN
 cpa.octagon.mergeop.type = WIDENING

src/org/sosy_lab/cpachecker/cpa/octagon/OctagonCPA.java
@@ -32,7 +32,6 @@ import org.sosy_lab.cpachecker.cfa.CFA;
 import org.sosy_lab.cpachecker.cfa.model.CFANode;
 import org.sosy_lab.cpachecker.core.ShutdownNotifier;
 import org.sosy_lab.cpachecker.core.defaults.AutomaticCPAFactory;
-import org.sosy_lab.cpachecker.core.defaults.MergeSepOperator;
 import org.sosy_lab.cpachecker.core.defaults.StaticPrecisionAdjustment;
 import org.sosy_lab.cpachecker.core.defaults.StopSepOperator;
 import org.sosy_lab.cpachecker.core.interfaces.AbstractDomain;
@@ -57,10 +56,6 @@ public final class OctagonCPA implements ConfigurableProgramAnalysis {
   public static CPAFactory factory() {
     return AutomaticCPAFactory.forType(OctagonCPA.class).withOptions(OctagonOptions.class);
   }
-
-  @Option(name="merge", toUppercase=true, values={"SEP", "JOIN"},
-      description="which merge operator to use for OctagonCPA?")
-  private String mergeType = "SEP";
 
   @Option(name="octagonLibrary", toUppercase=true, values={"INT", "FLOAT"},
       description="with this option the number representation in the"
@@ -117,13 +112,7 @@ public final class OctagonCPA implements ConfigurableProgramAnalysis {
 
     this.transferRelation = new OctagonTransferRelation(logger, cfa, octagonOptions);
 
-    MergeOperator octagonMergeOp = null;
-    if (mergeType.equals("JOIN")) {
-      octagonMergeOp = new OctagonMergeJoinOperator(octagonDomain, config);
-    } else {
-      // default is sep
-      octagonMergeOp = MergeSepOperator.getInstance();
-    }
+    MergeOperator octagonMergeOp = OctagonMergeOperator.getInstance(octagonDomain, config);
 
     StopOperator octagonStopOp = new StopSepOperator(octagonDomain);
 

src/org/sosy_lab/cpachecker/cpa/octagon/OctagonDomain.java
@@ -110,7 +110,7 @@ class OctagonDomain implements AbstractDomain {
     }
   }
 
-  public AbstractState joinWidening(OctagonState successorOct, OctagonState reachedOct) {
+  public AbstractState widening(OctagonState successorOct, OctagonState reachedOct) {
     Pair<OctagonState, OctagonState> shrinkedStates = getShrinkedStates(successorOct, reachedOct);
     successorOct = shrinkedStates.getFirst();
     reachedOct = shrinkedStates.getSecond();

src/org/sosy_lab/cpachecker/cpa/octagon/OctagonMergeJoinOperator.java
@@ -1,70 +1,0 @@
-/*
- *  CPAchecker is a tool for configurable software verification.
- *  This file is part of CPAchecker.
- *
- *  Copyright (C) 2007-2014  Dirk Beyer
- *  All rights reserved.
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- *
- *
- *  CPAchecker web page:
- *    http://cpachecker.sosy-lab.org
- */
-package org.sosy_lab.cpachecker.cpa.octagon;
-
-import org.sosy_lab.common.configuration.Configuration;
-import org.sosy_lab.common.configuration.InvalidConfigurationException;
-import org.sosy_lab.common.configuration.Option;
-import org.sosy_lab.common.configuration.Options;
-import org.sosy_lab.cpachecker.core.interfaces.AbstractState;
-import org.sosy_lab.cpachecker.core.interfaces.MergeOperator;
-import org.sosy_lab.cpachecker.core.interfaces.Precision;
-import org.sosy_lab.cpachecker.exceptions.CPAException;
-
-@Options(prefix="cpa.octagon.mergeop")
-public class OctagonMergeJoinOperator implements MergeOperator {
-
-  private final OctagonDomain domain;
-
-  @Option(name="type", toUppercase=true, values={"NORMAL", "WIDENING"},
-      description="of which type should the merge be? normal, for usual join, widening for"
-                + " a widening instead of a join")
-  private String joinType = "NORMAL";
-
-  @Option(name="onlyJoinEdgesInSameBlock", description="with this option enabled"
-      + "mergeJoin is only used on edges within the same block, i.e. each iteration"
-      + "of a loop is a different block, thus the precision of the analysis increases")
-  private boolean onlyJoinEdgesInSameBlock = false;
-
-  public OctagonMergeJoinOperator(OctagonDomain domain, Configuration config) throws InvalidConfigurationException {
-    config.inject(this);
-    this.domain = domain;
-  }
-
-  @Override
-  public AbstractState merge(AbstractState el1, AbstractState el2, Precision p) throws CPAException {
-    if (onlyJoinEdgesInSameBlock) {
-      if (!(((OctagonState)el1).isLoopHead() && ((OctagonState)el2).isLoopHead())) {
-        return el2;
-      }
-    }
-    if (joinType.equals("NORMAL")) {
-      return domain.join(el1, el2);
-    } else if (joinType.equals("WIDENING")) {
-        return domain.joinWidening((OctagonState)el1, (OctagonState)el2);
-    } else {
-      throw new CPAException("Invalid join type in Octagon merge join operator");
-    }
-  }
-}

commit 6a81807c6a92c65e2925fa2c7d86fcd9beed8812

Fix injection of configuration options of OctagonMergeOperator
This was broken since r12847 (378ce078) from 2014
and prevented the use of any other merge operator than merge-sep
for the OctagonCPA, so the configurations -octagonAnalysis-mergeJoin and
-octagonAnalysis-mergeWidening were silently broken.

git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@36728 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

src/org/sosy_lab/cpachecker/cpa/octagon/OctagonMergeOperator.java
@@ -44,11 +44,10 @@ public class OctagonMergeOperator {
   }
 
   private OctagonMergeOperator(OctagonDomain domain, Configuration config) throws InvalidConfigurationException {
-    config.inject(this);
+    config.inject(this, OctagonMergeOperator.class);
     this.domain = domain;
   }
 
-  @Options(prefix="cpa.octagon.mergeop")
   class OctagonMergeJoinOperator extends OctagonMergeOperator implements MergeOperator {
 
     private OctagonMergeJoinOperator(OctagonDomain domain, Configuration config) throws InvalidConfigurationException {
@@ -66,7 +65,6 @@ public class OctagonMergeOperator {
     }
   }
 
-  @Options(prefix="cpa.octagon.mergeop")
   class OctagonMergeWideningOperator extends OctagonMergeOperator implements MergeOperator {
 
     private OctagonMergeWideningOperator(OctagonDomain domain, Configuration config) throws InvalidConfigurationException {