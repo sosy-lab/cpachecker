346c1812367165abf986f8566096a8b5245e9550
mlingsch <mlingsch@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2021-03-20 14:26:45 +0000

    Added Refiner and Predicate Config for Loopsummary and refactored Configs
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/loopsummary@36982 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

---
+++ src/org/sosy_lab/cpachecker/cpa/predicate/LoopSummaryPredicateRefiner.java
@@ -0,0 +1,49 @@
+// This file is part of CPAchecker,
+// a tool for configurable software verification:
+// https://cpachecker.sosy-lab.org
+//
+// SPDX-FileCopyrightText: 2007-2020 Dirk Beyer <https://www.sosy-lab.org>
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package org.sosy_lab.cpachecker.cpa.predicate;
+
+import org.sosy_lab.common.configuration.Configuration;
+import org.sosy_lab.common.configuration.InvalidConfigurationException;
+import org.sosy_lab.common.log.LogManager;
+import org.sosy_lab.cpachecker.core.interfaces.ConfigurableProgramAnalysis;
+import org.sosy_lab.cpachecker.core.interfaces.Refiner;
+import org.sosy_lab.cpachecker.cpa.arg.ARGBasedRefiner;
+import org.sosy_lab.cpachecker.cpa.bam.BAMBasedRefiner;
+import org.sosy_lab.cpachecker.util.CPAs;
+import org.sosy_lab.cpachecker.util.predicates.pathformula.PathFormulaManager;
+import org.sosy_lab.cpachecker.util.predicates.smt.Solver;
+
+public abstract class LoopSummaryPredicateRefiner implements Refiner {
+
+  public static Refiner create(ConfigurableProgramAnalysis pCpa)
+      throws InvalidConfigurationException {
+    return BAMBasedRefiner.forARGBasedRefiner(create0(pCpa), pCpa);
+  }
+
+  @SuppressWarnings("resource")
+  public static ARGBasedRefiner create0(ConfigurableProgramAnalysis pCpa)
+      throws InvalidConfigurationException {
+    BAMPredicateCPA predicateCpa =
+        CPAs.retrieveCPAOrFail(pCpa, BAMPredicateCPA.class, BAMPredicateRefiner.class);
+    Configuration config = predicateCpa.getConfiguration();
+    LogManager logger = predicateCpa.getLogger();
+    Solver solver = predicateCpa.getSolver();
+    PathFormulaManager pfmgr = predicateCpa.getPathFormulaManager();
+
+    BlockFormulaStrategy blockFormulaStrategy = new BAMBlockFormulaStrategy(pfmgr);
+
+    RefinementStrategy strategy =
+        new BAMPredicateAbstractionRefinementStrategy(
+            config, logger, solver, predicateCpa.getPredicateManager());
+
+    return new PredicateCPARefinerFactory(pCpa)
+        .setBlockFormulaStrategy(blockFormulaStrategy)
+        .create(strategy);
+  }
+}
