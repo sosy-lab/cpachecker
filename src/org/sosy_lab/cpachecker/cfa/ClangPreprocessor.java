e93d73515d6efbcc0af6b50e8b5401bbf03e5756
kcimbalnik <kcimbalnik@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2021-04-07 10:24:40 +0000

    Match only '.', not any character
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/clang-change-filename-extension-of-i-files@37058 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/cfa/ClangPreprocessor.java
+++ src/org/sosy_lab/cpachecker/cfa/ClangPreprocessor.java
@@ -63,7 +63,7 @@ public class ClangPreprocessor extends Preprocessor {
 
   @Override
   protected String getDumpFileOfFile(String file) {
-    return file.replaceFirst("(.c|.i)$", ".ll");
+    return file.replaceFirst("(\\.c|\\.i)$", ".ll");
   }
 
   private String extractVersionNumberFromLlvmJ() {
