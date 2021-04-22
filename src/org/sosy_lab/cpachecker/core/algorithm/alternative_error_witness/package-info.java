9ca946d94382e755f08f3ffdceb61fa820e6b0ed
jhaltermann <jhaltermann@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2021-04-22 06:55:16 +0000

    added package info
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/invariantWitness@37125 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

---
+++ src/org/sosy_lab/cpachecker/core/algorithm/alternative_error_witness/package-info.java
@@ -0,0 +1,13 @@
+// This file is part of CPAchecker,
+// a tool for configurable software verification:
+// https://cpachecker.sosy-lab.org
+//
+// SPDX-FileCopyrightText: 2007-2020 Dirk Beyer <https://www.sosy-lab.org>
+//
+// SPDX-License-Identifier: Apache-2.0
+
+/** An algorithm to export a laternative error witness format using boolean formulae */
+@javax.annotation.ParametersAreNonnullByDefault
+@org.sosy_lab.common.annotations.FieldsAreNonnullByDefault
+@org.sosy_lab.common.annotations.ReturnValuesAreNonnullByDefault
+package org.sosy_lab.cpachecker.core.algorithm.alternative_error_witness;
