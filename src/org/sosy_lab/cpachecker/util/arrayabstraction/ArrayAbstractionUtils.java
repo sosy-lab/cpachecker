898042029037c5a55a50a4fc9369deb17d14132a
vport <vport@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2021-04-30 21:57:08 +0000

    Add 'ArrayAbstractionUtils'
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/array-abstraction@37204 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

---
+++ src/org/sosy_lab/cpachecker/util/arrayabstraction/ArrayAbstractionUtils.java
@@ -0,0 +1,101 @@
+// This file is part of CPAchecker,
+// a tool for configurable software verification:
+// https://cpachecker.sosy-lab.org
+//
+// SPDX-FileCopyrightText: 2007-2021 Dirk Beyer <https://www.sosy-lab.org>
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package org.sosy_lab.cpachecker.util.arrayabstraction;
+
+import org.sosy_lab.cpachecker.cfa.ast.FileLocation;
+import org.sosy_lab.cpachecker.cfa.ast.c.CExpression;
+import org.sosy_lab.cpachecker.cfa.ast.c.CExpressionAssignmentStatement;
+import org.sosy_lab.cpachecker.cfa.ast.c.CIdExpression;
+import org.sosy_lab.cpachecker.cfa.ast.c.CLeftHandSide;
+import org.sosy_lab.cpachecker.cfa.ast.c.CVariableDeclaration;
+import org.sosy_lab.cpachecker.cfa.model.CFANode;
+import org.sosy_lab.cpachecker.cfa.model.c.CAssumeEdge;
+import org.sosy_lab.cpachecker.cfa.model.c.CStatementEdge;
+import org.sosy_lab.cpachecker.cfa.types.c.CArrayType;
+import org.sosy_lab.cpachecker.cfa.types.c.CStorageClass;
+import org.sosy_lab.cpachecker.cfa.types.c.CType;
+import org.sosy_lab.cpachecker.util.states.MemoryLocation;
+
+final class ArrayAbstractionUtils {
+
+  private ArrayAbstractionUtils() {}
+
+  static CIdExpression createCIdExpression(CType pType, MemoryLocation pMemoryLocation) {
+
+    String variableName = pMemoryLocation.getIdentifier();
+    String qualifiedName = pMemoryLocation.getAsSimpleString();
+
+    CVariableDeclaration variableDeclaration =
+        new CVariableDeclaration(
+            FileLocation.DUMMY,
+            true,
+            CStorageClass.AUTO,
+            pType,
+            variableName,
+            variableName,
+            qualifiedName,
+            null);
+
+    return new CIdExpression(FileLocation.DUMMY, variableDeclaration);
+  }
+
+  static MemoryLocation getMemoryLocation(CIdExpression pCIdExpression) {
+
+    String qualifiedName = pCIdExpression.getDeclaration().getQualifiedName();
+
+    return MemoryLocation.valueOf(qualifiedName);
+  }
+
+  static CAssumeEdge createAssumeEdge(CExpression pCondition, boolean pTruthAssumption) {
+    return new CAssumeEdge(
+        "",
+        FileLocation.DUMMY,
+        CFANode.newDummyCFANode("dummy-predecessor"),
+        CFANode.newDummyCFANode("dummy-successor"),
+        pCondition,
+        pTruthAssumption);
+  }
+
+  static CVariableDeclaration createNonArrayVariableDeclaration(
+      CVariableDeclaration pArrayVariableDeclaration) {
+    CType type = pArrayVariableDeclaration.getType();
+
+    if (type instanceof CArrayType) {
+
+      CType newType = ((CArrayType) type).getType();
+
+      CVariableDeclaration variableDeclaration =
+          new CVariableDeclaration(
+              pArrayVariableDeclaration.getFileLocation(),
+              pArrayVariableDeclaration.isGlobal(),
+              pArrayVariableDeclaration.getCStorageClass(),
+              newType,
+              pArrayVariableDeclaration.getName(),
+              pArrayVariableDeclaration.getOrigName(),
+              pArrayVariableDeclaration.getQualifiedName(),
+              null);
+      return variableDeclaration;
+    }
+
+    return pArrayVariableDeclaration;
+  }
+
+  static CStatementEdge createAssignEdge(CLeftHandSide pLhs, CIdExpression pRhs) {
+
+    CExpressionAssignmentStatement assignmentStatement =
+        new CExpressionAssignmentStatement(FileLocation.DUMMY, pLhs, pRhs);
+
+    return new CStatementEdge(
+        "",
+        assignmentStatement,
+        FileLocation.DUMMY,
+        CFANode.newDummyCFANode("dummy-predecessor"),
+        CFANode.newDummyCFANode("dummy-successor"));
+  }
+}
