56f2b6e7bc0ec6914452c825188e2a08f6dc8be6
hrust <hrust@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2021-05-04 10:42:14 +0000

    fix wrong mapping from witness state names (ids) to associated ARG states
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/condition-reuse@37224 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/cpa/arg/witnessexport/WitnessFactory.java
+++ src/org/sosy_lab/cpachecker/cpa/arg/witnessexport/WitnessFactory.java
@@ -337,8 +337,16 @@ class WitnessFactory implements EdgeAppender {
       final CFAEdgeWithAdditionalInfo pAdditionalInfo) {
 
     attemptSwitchToFunctionScope(pEdge);
-    if (pFromState.isPresent()) {
-      stateToARGStates.putAll(pFrom, pFromState.orElseThrow());
+
+    Collection<ARGState> fromStates = pFromState.orElse(Collections.emptyList());
+    if (graphType == WitnessType.VIOLATION_WITNESS) {
+      stateToARGStates.putAll(pFrom, fromStates);
+    } else if (graphType == WitnessType.CORRECTNESS_WITNESS) {
+      // Correctness witnesses use GraphBuilder.CFA_FULL for graph building.
+      // GraphBuilder.CFA_FULL actually returns the ARG states corresponding to pTo in pFromState
+      stateToARGStates.putAll(pTo, fromStates);
+    } else {
+      assert false : "Unknown witness type";
     }
 
     Collection<TransitionCondition> transitions =
