ab50d4ead6a6a390dda14816199c7bde01c81149
mlingsch <mlingsch@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2021-05-09 00:49:36 +0000

    Fixed Problem because of which the logging of the aomount of strategies used was not correct
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/loopsummary@37249 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/cpa/loopsummary/AbstractLoopSummaryTransferRelation.java
+++ src/org/sosy_lab/cpachecker/cpa/loopsummary/AbstractLoopSummaryTransferRelation.java
@@ -12,7 +12,6 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
-import java.util.logging.Level;
 import org.sosy_lab.common.ShutdownNotifier;
 import org.sosy_lab.common.log.LogManager;
 import org.sosy_lab.cpachecker.cfa.CFA;
@@ -144,67 +143,6 @@ public abstract class AbstractLoopSummaryTransferRelation<EX extends CPAExceptio
       final AbstractState pState, final Precision pPrecision)
       throws InterruptedException, CPATransferException {
 
-    /*
-     *
-     * Problem when executing following command line
-     * scripts/cpa.sh -config config/loop-summary/predicateAnalysis-loopsummary.properties -setprop counterexample.export.enabled=false -timelimit 900s -stats -spec ../../sv-benchmarks/c/properties/no-overflow.prp -64 ../../sv-benchmarks/c/termination-crafted/Arrays01-EquivalentConstantIndices-1.c
-     *
-     * Problem related to precision adjustment
-     *
-     */
-
-    /*
-    * When executing
-    *
-    * scripts/cpa.sh -heap 10000M -config config/loop-summary/predicateAnalysis-loopsummary.properties -setprop counterexample.export.enabled=false -preprocess -timelimit 900s -spec test/programs/benchmarks/properties/no-overflow.prp -64 test/programs/benchmarks/uthash-2.0.2/uthash_JEN_test7-2.c
-    *
-    * Exception in thread "main" java.lang.AssertionError: Found imprecise counterexample in PredicateCPA. If this is expected for this configuration (e.g., because of UF-based heap encoding), set counterexample.export.allowImpreciseCounterexamples=true. Otherwise please report this as a bug.
-       at org.sosy_lab.cpachecker.util.predicates.PathChecker.createImpreciseCounterexample(PathChecker.java:204)
-       at org.sosy_lab.cpachecker.util.predicates.PathChecker.handleFeasibleCounterexample(PathChecker.java:126)
-       at org.sosy_lab.cpachecker.cpa.predicate.PredicateCPARefiner.performRefinementForPath(PredicateCPARefiner.java:314)
-       at org.sosy_lab.cpachecker.cpa.arg.AbstractARGBasedRefiner.performRefinementForPath(AbstractARGBasedRefiner.java:157)
-       at org.sosy_lab.cpachecker.cpa.arg.AbstractARGBasedRefiner.performRefinement(AbstractARGBasedRefiner.java:102)
-       at org.sosy_lab.cpachecker.core.algorithm.CEGARAlgorithm.refine(CEGARAlgorithm.java:294)
-       at org.sosy_lab.cpachecker.core.algorithm.CEGARAlgorithm.run(CEGARAlgorithm.java:240)
-       at org.sosy_lab.cpachecker.core.CPAchecker.runAlgorithm(CPAchecker.java:532)
-       at org.sosy_lab.cpachecker.core.CPAchecker.run(CPAchecker.java:399)
-       at org.sosy_lab.cpachecker.cmdline.CPAMain.main(CPAMain.java:170)
-    *
-    */
-
-    /*
-         * scripts/cpa.sh -heap 10000M -config config/loop-summary/predicateAnalysis-loopsummary.properties -setprop counterexample.export.enabled=false -timelimit 900s -stats -spec test/programs/benchmarks/properties/no-overflow.prp -64 test/programs/benchmarks/termination-crafted-lit/ChenFlurMukhopadhyay-SAS2012-Ex2.06.c
-         *
-         * Using summary Strategy: class org.sosy_lab.cpachecker.cpa.loopsummary.strategies.NaiveLoopAcceleration (LoopSummaryCPA:AbstractLoopSummaryTransferRelation.getAbstractSuccessors, INFO)
-
-    Exception in thread "main" java.util.NoSuchElementException
-        at java.base/java.util.ArrayList$Itr.next(ArrayList.java:1000)
-        at java.base/java.util.Collections$UnmodifiableCollection$1.next(Collections.java:1047)
-        at com.google.common.collect.Iterators.getOnlyElement(Iterators.java:301)
-        at com.google.common.collect.Iterables.getOnlyElement(Iterables.java:254)
-        at org.sosy_lab.cpachecker.cpa.arg.ARGPrecisionAdjustment.elementHasSiblings(ARGPrecisionAdjustment.java:150)
-        at org.sosy_lab.cpachecker.cpa.arg.ARGPrecisionAdjustment.prec(ARGPrecisionAdjustment.java:98)
-        at org.sosy_lab.cpachecker.cpa.arg.ARGPrecisionAdjustment.prec(ARGPrecisionAdjustment.java:54)
-        at org.sosy_lab.cpachecker.core.algorithm.CPAAlgorithm.handleState(CPAAlgorithm.java:340)
-        at org.sosy_lab.cpachecker.core.algorithm.CPAAlgorithm.run0(CPAAlgorithm.java:273)
-        at org.sosy_lab.cpachecker.core.algorithm.CPAAlgorithm.run(CPAAlgorithm.java:245)
-        at org.sosy_lab.cpachecker.core.algorithm.CEGARAlgorithm.run(CEGARAlgorithm.java:229)
-        at org.sosy_lab.cpachecker.core.CPAchecker.runAlgorithm(CPAchecker.java:532)
-        at org.sosy_lab.cpachecker.core.CPAchecker.run(CPAchecker.java:399)
-        at org.sosy_lab.cpachecker.cmdline.CPAMain.main(CPAMain.java:170)
-         *
-         */
-
-    /*
-     * For some reason busybox-1.22.0/ls-incomplete-2.c cannot be parsed when calling
-     * scripts/cpa.sh -heap 10000M -config config/loop-summary/predicateAnalysis-loopsummary.properties -setprop counterexample.export.enabled=false -preprocess -timelimit 900s -spec test/programs/benchmarks/properties/no-overflow.prp -64 test/programs/benchmarks/busybox-1.22.0/ls-incomplete-2.c
-     *
-     */
-
-    /*
-     * How do you do the refinement in this case?
-     *
-     */
     Optional<Collection<? extends AbstractState>> summarizedState =
         this.applyStrategyIfAlreadyApplied(pState, pPrecision);
     while (summarizedState.isEmpty()) {
@@ -241,13 +179,11 @@ public abstract class AbstractLoopSummaryTransferRelation<EX extends CPAExceptio
       }
     }
 
-    logger.log(Level.INFO, ((LoopSummaryPrecision) pPrecision).getStrategyCounter());
-
     stats.incrementStrategyUsageCount(
         strategies
             .get(((LoopSummaryPrecision) pPrecision).getStrategyCounter())
             .getClass()
-            .getName());
+            .getSimpleName());
 
     ((LoopSummaryPrecision) pPrecision)
         .setLoopHead(
