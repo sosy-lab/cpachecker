d3a1bba5b31e511907aa2d2a7308914e2d993e32
mlingsch <mlingsch@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2021-05-18 22:55:46 +0000

    Started Implementing Linear Extrapolation Strategy
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/loopsummary@37310 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/cpa/loopsummary/strategies/extrapolation/LinearExtrapolationStrategy.java
+++ src/org/sosy_lab/cpachecker/cpa/loopsummary/strategies/extrapolation/LinearExtrapolationStrategy.java
@@ -9,12 +9,22 @@
 package org.sosy_lab.cpachecker.cpa.loopsummary.strategies.extrapolation;
 
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import org.sosy_lab.common.ShutdownNotifier;
 import org.sosy_lab.common.log.LogManager;
+import org.sosy_lab.cpachecker.cfa.ast.c.CExpression;
+import org.sosy_lab.cpachecker.cfa.ast.c.CExpressionAssignmentStatement;
+import org.sosy_lab.cpachecker.cfa.ast.c.CIdExpression;
+import org.sosy_lab.cpachecker.cfa.model.CFAEdge;
+import org.sosy_lab.cpachecker.cfa.model.CFANode;
+import org.sosy_lab.cpachecker.cfa.model.c.CStatementEdge;
 import org.sosy_lab.cpachecker.core.interfaces.AbstractState;
 import org.sosy_lab.cpachecker.core.interfaces.Precision;
 import org.sosy_lab.cpachecker.core.interfaces.TransferRelation;
+import org.sosy_lab.cpachecker.cpa.loopsummary.strategies.GhostCFA;
 import org.sosy_lab.cpachecker.exceptions.CPATransferException;
 import org.sosy_lab.cpachecker.util.AbstractStates;
 
@@ -32,8 +42,20 @@ public class LinearExtrapolationStrategy extends AbstractExtrapolationStrategy {
   public Optional<Collection<? extends AbstractState>> summarizeLoopState(
       AbstractState pState, Precision pPrecision, TransferRelation pTransferRelation)
       throws CPATransferException, InterruptedException {
-    Optional<Integer> loopBranchIndexOptional =
-        getLoopBranchIndex(AbstractStates.extractLocation(pState));
+
+    CFANode loopStartNode = AbstractStates.extractLocation(pState);
+
+    if (loopStartNode.getNumLeavingEdges() != 1) {
+      return Optional.empty();
+    }
+
+    if (!loopStartNode.getLeavingEdge(0).getDescription().equals("while")) {
+      return Optional.empty();
+    }
+
+    loopStartNode = loopStartNode.getLeavingEdge(0).getSuccessor();
+
+    Optional<Integer> loopBranchIndexOptional = getLoopBranchIndex(loopStartNode);
     Integer loopBranchIndex;
 
     if (loopBranchIndexOptional.isEmpty()) {
@@ -42,7 +64,7 @@ public class LinearExtrapolationStrategy extends AbstractExtrapolationStrategy {
       loopBranchIndex = loopBranchIndexOptional.orElseThrow();
     }
 
-    /*Optional<CExpression> loopBoundOptional = bound(AbstractStates.extractLocation(pState));
+    Optional<CExpression> loopBoundOptional = bound(loopStartNode);
 
     CExpression loopBoundExpression;
     if (loopBoundOptional.isEmpty()) {
@@ -50,40 +72,143 @@ public class LinearExtrapolationStrategy extends AbstractExtrapolationStrategy {
     } else {
       loopBoundExpression = loopBoundOptional.get();
     }
-    */
 
-    if (!linearArithmeticExpressionsLoop(AbstractStates.extractLocation(pState), loopBranchIndex)) {
+    if (!linearArithmeticExpressionsLoop(loopStartNode, loopBranchIndex)) {
       return Optional.empty();
     }
 
-    // Get Matrix and make it Upper Diagonal
-    // See if the loop bound delta can be easily calculated, because a variable is increased a
-    // constant amount or not
-    // Get the general representation of the matrix and apply the loop unrolling, the extrapolation
-    // and the unrolling again
+    // Get the Variables for the Matrix
+    Map<String, Map<String, Integer>> loopVariableDependencies =
+        getLoopVariableDependencies(loopStartNode, loopBranchIndex);
 
-    return Optional.empty();
-
-    /*Map<String, Integer> loopVariableDelta = getLoopVariableDeltas(pState, loopBranchIndex);
-
-    int boundDelta = boundDelta(loopVariableDelta, loopBound);
-    if (boundDelta >= 0) { // TODO How do you treat non Termination?
-      return Optional.empty();
-    }
-
-    GhostCFA ghostCFA;
-    Optional<GhostCFA> ghostCFASuccess =
-        summaryCFA(pState, loopVariableDelta, loopBound, boundDelta, loopBranchIndex);
-
-    if (ghostCFASuccess.isEmpty()) {
+    Integer loopVariableDelta;
+    Optional<Integer> optionalLoopVariableDelta =
+        getLoopVariableDelta(loopVariableDependencies, loopBoundExpression);
+    if (optionalLoopVariableDelta.isEmpty()) {
       return Optional.empty();
     } else {
-      ghostCFA = ghostCFASuccess.get();
+      loopVariableDelta = optionalLoopVariableDelta.get();
     }
 
-    Collection<AbstractState> realStatesEndCollection =
-        transverseGhostCFA(ghostCFA, pState, pPrecision, pTransferRelation, loopBranchIndex);
+    if (loopVariableDelta >= 0) {
+      return Optional.empty();
+    }
 
-    return Optional.of(realStatesEndCollection);*/
+    List<String> variableOrdering;
+    Optional<List<String>> optionalVariableOrdering = getVariableOrdering(loopVariableDependencies);
+    if (optionalVariableOrdering.isEmpty()) {
+      return Optional.empty();
+    } else {
+      variableOrdering = optionalVariableOrdering.get();
+    }
+
+    // TODO refactor matrix into its own class in utils, question: Where should it go?
+    List<List<Float>> matrixRepresentation =
+        getMatrixRepresentation(loopVariableDependencies, variableOrdering);
+
+    Optional<GhostCFA> optionalGhostCFA =
+        buildGhostCFA(
+            loopVariableDelta, loopBoundExpression, matrixRepresentation, variableOrdering);
+    GhostCFA ghostCFA;
+    if (optionalGhostCFA.isEmpty()) {
+      return Optional.empty();
+    } else {
+      ghostCFA = optionalGhostCFA.get();
+    }
+
+    Collection<? extends AbstractState> realStatesEndCollection =
+        transverseGhostCFA(
+            ghostCFA, pState, pPrecision, loopStartNode, loopBranchIndex, pTransferRelation);
+
+    return Optional.of(realStatesEndCollection);
+  }
+
+  @SuppressWarnings("unused")
+  private Optional<GhostCFA> buildGhostCFA(
+      Integer pLoopVariableDelta,
+      CExpression pLoopBoundExpression,
+      List<List<Float>> pMatrixRepresentation,
+      List<String> pVariableOrdering) {
+    // TODO Auto-generated method stub
+    return Optional.empty();
+  }
+
+  @SuppressWarnings("unused")
+  private List<List<Float>> getMatrixRepresentation(
+      Map<String, Map<String, Integer>> pLoopVariableDependencies, List<String> pVariableOrdering) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @SuppressWarnings("unused")
+  private Optional<List<String>> getVariableOrdering(
+      Map<String, Map<String, Integer>> pLoopVariableDependencies) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @SuppressWarnings("unused")
+  private Optional<Integer> getLoopVariableDelta(
+      Map<String, Map<String, Integer>> pLoopVariableDependencies,
+      CExpression pLoopBoundExpression) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  private Map<String, Map<String, Integer>> getLoopVariableDependencies(
+      CFANode pLoopStartNode, Integer pLoopBranchIndex) {
+    Map<String, Map<String, Integer>> loopVariableDependencies = new HashMap<>();
+    HashMap<String, Integer> constVariableHashMap = new HashMap<>();
+    constVariableHashMap.put("1", 1);
+    loopVariableDependencies.put(
+        "1", constVariableHashMap); // Add Constant, which can only be mapped to itself
+
+    CFANode currentNode = pLoopStartNode.getLeavingEdge(pLoopBranchIndex).getSuccessor();
+
+    while (currentNode != pLoopStartNode) {
+      assert currentNode.getNumLeavingEdges() == 2;
+      CFAEdge edge = currentNode.getLeavingEdge(0);
+      CExpressionAssignmentStatement statement =
+          (CExpressionAssignmentStatement) ((CStatementEdge) edge).getStatement();
+
+      CExpression rigthSide = statement.getRightHandSide();
+      String variableToUpdate = ((CIdExpression) statement.getLeftHandSide()).getName();
+
+      Map<String, Integer> thisVariableUpdate = new HashMap<>();
+      updateVariableDependencies(thisVariableUpdate, rigthSide);
+      loopVariableDependencies.put(variableToUpdate, thisVariableUpdate);
+
+      currentNode = edge.getSuccessor();
+    }
+
+    return loopVariableDependencies;
+  }
+
+  @SuppressWarnings("unused")
+  private void updateVariableDependencies(
+      Map<String, Integer> pLoopVariableDependencies, CExpression pRigthSide) {
+
+    /*if (pRigthSide instanceof CIdExpression || pRigthSide instanceof CIntegerLiteralExpression) {
+      return true;
+    } else if (expression instanceof CBinaryExpression) {
+      String operator = ((CBinaryExpression) expression).getOperator().getOperator();
+      CExpression operand1 = ((CBinaryExpression) expression).getOperand1();
+      CExpression operand2 = ((CBinaryExpression) expression).getOperand2();
+      switch (operator) {
+        case "+":
+        case "-":
+          return linearArithemticExpression(operand1) && linearArithemticExpression(operand2);
+        case "*":
+          return (linearArithemticExpression(operand1)
+                  && operand2 instanceof CIntegerLiteralExpression)
+              || (operand1 instanceof CIntegerLiteralExpression
+                  && linearArithemticExpression(operand2));
+        default:
+          return false;
+      }
+    } else {
+      return false;
+    }*/
+
   }
 }
