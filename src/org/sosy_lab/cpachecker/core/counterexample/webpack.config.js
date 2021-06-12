f8087da1f63ab3cd40f1f92dfca9433fd10d8cf3
mlachner <mlachner@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2021-06-12 14:48:41 +0000

    Move vendor files to vendor.js
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/report-js-build@37488 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/core/counterexample/webpack.config.js
+++ src/org/sosy_lab/cpachecker/core/counterexample/webpack.config.js
@@ -87,7 +87,7 @@ module.exports = {
         vendors: {
           chunks: "all",
           name: "vendors",
-          test: /(node_modules)|(build_tmp\/dependencies\.json)/,
+          test: /(node_modules)|(build_tmp\/dependencies\.json)|(vendor)/,
         },
       },
     },
