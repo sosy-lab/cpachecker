cc1f9cbd8d3f4b30f6757da96bef53c2de1325dc
sumbricht <sumbricht@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2021-06-28 09:41:41 +0000

    Simplify handling of assertions by utilizing info from block structure.
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/acsl-bachelor@37604 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/cfa/ast/acsl/util/BlockStructure.java
+++ src/org/sosy_lab/cpachecker/cfa/ast/acsl/util/BlockStructure.java
@@ -9,10 +9,17 @@
 package org.sosy_lab.cpachecker.cfa.ast.acsl.util;
 
 import com.google.common.collect.ImmutableSet;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 import org.sosy_lab.cpachecker.cfa.ast.FileLocation;
+import org.sosy_lab.cpachecker.cfa.model.CFAEdge;
 import org.sosy_lab.cpachecker.cfa.model.CFANode;
+import org.sosy_lab.cpachecker.cfa.model.c.CFunctionCallEdge;
+import org.sosy_lab.cpachecker.util.CFAUtils;
 
 public class BlockStructure {
   private final ImmutableSet<ACSLBlock> blocks;
@@ -50,5 +57,77 @@ public class BlockStructure {
 
   public Set<CFANode> getNodesInInnermostBlockOf(CFANode node) {
     return getInnermostBlockOf(node).getContainedNodes();
+  }
+
+  private boolean ignoreEdge(CFAEdge edge) {
+    if (edge instanceof CFunctionCallEdge
+        || edge.getFileLocation().equals(FileLocation.DUMMY)
+        || edge.getDescription().contains("__CPAchecker_TMP")) {
+      return true;
+    }
+    return false;
+  }
+
+  public Set<CFAEdge> getPrevEdges(ACSLBlock pBlock, FileLocation location) {
+    Set<CFAEdge> edges = new HashSet<>();
+    for (CFANode node : pBlock.getContainedNodes()) {
+      CFAUtils.enteringEdges(node).copyInto(edges);
+      CFAUtils.leavingEdges(node).copyInto(edges);
+    }
+    edges.removeIf(this::ignoreEdge);
+    List<CFAEdge> sortedEdges = new ArrayList<>(edges);
+    sortedEdges.sort(Comparator.comparingInt(pEdge -> pEdge.getFileLocation().getNodeOffset()));
+
+    CFAEdge prev = null;
+    for (CFAEdge edge : sortedEdges) {
+      if (edge.getFileLocation().getNodeOffset() > location.getNodeOffset()) {
+        break;
+      }
+      prev = edge;
+    }
+
+    if (prev == null) {
+      // The specified location is directly at the beginning of the given block
+      return ImmutableSet.of();
+    }
+
+    ACSLBlock innermostBlockOfPrevEdge = getInnermostBlockOf(prev.getSuccessor());
+    if (innermostBlockOfPrevEdge.equals(pBlock)) {
+      return CFAUtils.enteringEdges(prev.getSuccessor()).toSet();
+    }
+
+    // There is at least one block end directly before the specified location, so return all leaving
+    // edges of the last of these blocks
+    ACSLBlock lastBlock = null;
+    for (ACSLBlock block : getBlocks()) {
+      if (!block.getContainedNodes().contains(prev.getPredecessor())
+          || block.getEndOffset() >= location.getNodeOffset()) {
+        continue;
+      }
+      if (lastBlock == null || block.contains(lastBlock)) {
+        lastBlock = block;
+      }
+    }
+    assert lastBlock != null
+        : "Previous edge is allegedly in another block, but no such block was found";
+    return lastBlock.getLeavingEdges();
+  }
+
+  public CFAEdge getNextEdge(ACSLBlock block, FileLocation location) {
+    Set<CFAEdge> edges = new HashSet<>();
+    for (CFANode node : block.getContainedNodes()) {
+      CFAUtils.enteringEdges(node).copyInto(edges);
+      CFAUtils.leavingEdges(node).copyInto(edges);
+    }
+    edges.removeIf(this::ignoreEdge);
+    List<CFAEdge> sortedEdges = new ArrayList<>(edges);
+    sortedEdges.sort(Comparator.comparingInt(pEdge -> pEdge.getFileLocation().getNodeOffset()));
+
+    for (CFAEdge edge : sortedEdges) {
+      if (edge.getFileLocation().getNodeOffset() > location.getNodeOffset()) {
+        return edge;
+      }
+    }
+    return null;
   }
 }
