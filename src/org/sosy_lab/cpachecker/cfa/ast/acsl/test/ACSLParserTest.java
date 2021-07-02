95d3672a4d216c5bf4a1ee23cb70bf791a134137
sumbricht <sumbricht@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2021-07-02 10:21:34 +0000

    Refactor and add tests.
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/acsl-bachelor@37664 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/cfa/ast/acsl/test/ACSLParserTest.java
+++ src/org/sosy_lab/cpachecker/cfa/ast/acsl/test/ACSLParserTest.java
@@ -11,10 +11,12 @@ package org.sosy_lab.cpachecker.cfa.ast.acsl.test;
 import static com.google.common.truth.Truth.assertThat;
 
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
 import java.io.IOException;
 import java.nio.file.Paths;
 import java.util.Collection;
 import java.util.List;
+import java.util.Set;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
@@ -25,6 +27,7 @@ import org.sosy_lab.common.configuration.InvalidConfigurationException;
 import org.sosy_lab.common.log.LogManager;
 import org.sosy_lab.cpachecker.cfa.CFACreator;
 import org.sosy_lab.cpachecker.cfa.CFAWithACSLAnnotations;
+import org.sosy_lab.cpachecker.cfa.ast.acsl.ACSLAnnotation;
 import org.sosy_lab.cpachecker.exceptions.ParserException;
 import org.sosy_lab.cpachecker.util.test.TestDataTools;
 
@@ -34,15 +37,13 @@ public class ACSLParserTest {
   private static final String TEST_DIR = "test/programs/acsl/";
 
   private final String programName;
-  private final boolean shouldSucceed;
-  private final int numAnnotations;
+  private final int expectedAnnotations;
   private final CFACreator cfaCreator;
 
-  public ACSLParserTest(String pProgramName, boolean pShouldSucceed, int pNumAnnotations)
+  public ACSLParserTest(String pProgramName, int pExpectedAnnotations)
       throws InvalidConfigurationException {
     programName = pProgramName;
-    shouldSucceed = pShouldSucceed;
-    numAnnotations = pNumAnnotations;
+    expectedAnnotations = pExpectedAnnotations;
     Configuration config =
         TestDataTools.configurationForTest()
             .loadFromResource(ACSLParserTest.class, "acslToWitness.properties")
@@ -54,47 +55,41 @@ public class ACSLParserTest {
   @Parameters(name = "{0}")
   public static Collection<Object[]> data() {
     ImmutableList.Builder<Object[]> b = ImmutableList.builder();
-    b.add(succeedingTask("abs.c", 1));
-    b.add(succeedingTask("abs2.c", 1));
-    b.add(succeedingTask("even.c", 1));
-    b.add(succeedingTask("even2.c", 1));
-    b.add(succeedingTask("nested.c", 2));
-    b.add(succeedingTask("simple.c", 3));
-    b.add(failingTask("badVariable.c"));
-    b.add(succeedingTask("statements.c", 1));
-
-    b.add(succeedingTask("after_if.c", 1));
-    b.add(succeedingTask("after_else.c", 1));
-    b.add(succeedingTask("after_loop.c", 1));
-    b.add(succeedingTask("after_loop2.c", 1));
-    b.add(succeedingTask("at_end.c", 1));
-    b.add(succeedingTask("end_of_do_while.c", 1));
-    b.add(succeedingTask("after_for_loop.c", 1));
-    b.add(succeedingTask("after_for_loop2.c", 1));
-    b.add(succeedingTask("in_middle.c", 1));
-    b.add(succeedingTask("traps.c", 2));
+    b.add(task("abs.c", 1));
+    b.add(task("abs2.c", 1));
+    b.add(task("even.c", 1));
+    b.add(task("even2.c", 1));
+    b.add(task("nested.c", 2));
+    b.add(task("simple.c", 3));
+    b.add(task("badVariable.c", 0));
+    b.add(task("statements.c", 4));
+    b.add(task("inv_for.c", 1));
+    b.add(task("inv_short-for.c", 1));
+    b.add(task("after_if.c", 1));
+    b.add(task("after_else.c", 1));
+    b.add(task("after_loop.c", 1));
+    b.add(task("after_loop2.c", 1));
+    b.add(task("at_end.c", 1));
+    b.add(task("end_of_do_while.c", 1));
+    b.add(task("after_for_loop.c", 1));
+    b.add(task("after_for_loop2.c", 1));
+    b.add(task("in_middle.c", 1));
+    b.add(task("traps.c", 2));
     return b.build();
   }
 
-  private static Object[] succeedingTask(String program, int numAnnotations) {
-    return new Object[] {program, true, numAnnotations};
-  }
-
-  private static Object[] failingTask(String program) {
-    return new Object[] {program, false, 0};
+  private static Object[] task(String program, int expectedAnnotations) {
+    return new Object[] {program, expectedAnnotations};
   }
 
   @Test
-  public void test()
+  public void annotationParsingProducesExpectedNumberOfAnnotations()
       throws InterruptedException, ParserException, InvalidConfigurationException, IOException {
     List<String> files = ImmutableList.of(Paths.get(TEST_DIR, programName).toString());
-    try {
-      CFAWithACSLAnnotations cfaWithLocs =
-          (CFAWithACSLAnnotations) cfaCreator.parseFileAndCreateCFA(files);
-      assertThat(shouldSucceed).isTrue();
-      assertThat(cfaWithLocs.getEdgesToAnnotations().keySet().size()).isAtLeast(numAnnotations);
-    } catch (AssertionError e) {
-      assertThat(shouldSucceed).isFalse();
-    }
+    CFAWithACSLAnnotations cfaWithLocs =
+        (CFAWithACSLAnnotations) cfaCreator.parseFileAndCreateCFA(files);
+    Set<ACSLAnnotation> annotations =
+        ImmutableSet.copyOf(cfaWithLocs.getEdgesToAnnotations().values());
+    assertThat(annotations.size()).isEqualTo(expectedAnnotations);
   }
 }
