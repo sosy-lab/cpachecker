a878c1adb07715ae8c2aad6f1018e5d1300f804f
kcimbalnik <kcimbalnik@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2021-07-16 20:43:23 +0000

    See #831: Add test for String parsing with LLVM frontend and clang
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/831-string-parsing-for-llvm-with-clang@37738 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

---
+++ src/org/sosy_lab/cpachecker/cfa/LlvmParserWithClangTest.java
@@ -0,0 +1,74 @@
+package org.sosy_lab.cpachecker.cfa;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.io.MoreFiles;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Path;
+import java.util.List;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
+import org.sosy_lab.common.configuration.Configuration;
+import org.sosy_lab.common.configuration.InvalidConfigurationException;
+import org.sosy_lab.common.log.LogManager;
+import org.sosy_lab.cpachecker.cfa.types.MachineModel;
+import org.sosy_lab.cpachecker.exceptions.ParserException;
+
+/** Unit tests for {@link LlvmParserWithClang}. */
+@RunWith(Parameterized.class)
+public class LlvmParserWithClangTest {
+
+  @Parameters(name = "{0}")
+  public static List<Object> testcases() {
+    return ImmutableList.of(
+        "test/programs/llvm/switch-case.c", "test/programs/simple/globalVariableInitialValue-1.c");
+  }
+
+  @Parameter(0)
+  public String fileName;
+
+  // two parsers needed, as otherwise there is a problem with the already running parseTimer
+  private LlvmParserWithClang fileParser;
+  private LlvmParserWithClang stringParser;
+
+  @Before
+  public void createParsers() throws InvalidConfigurationException {
+    // TODO
+    Configuration config =
+        Configuration.builder()
+            .setOption("parser.preprocessor.dumpDirectory", "output/preprocessed")
+            .build();
+    // Configuration config =
+    //     Configuration.builder().setOption("parser.clang.dumpResults", "false").build();
+    // Configuration config = Configuration.defaultConfiguration();
+    LogManager logger = LogManager.createTestLogManager();
+    ClangPreprocessor clangPreprocessor = new ClangPreprocessor(config, logger);
+    stringParser =
+        LlvmParserWithClang.Factory.getParser(clangPreprocessor, logger, MachineModel.LINUX32);
+    fileParser =
+        LlvmParserWithClang.Factory.getParser(clangPreprocessor, logger, MachineModel.LINUX32);
+  }
+
+  @Test
+  public void compareStringParsingAndFileParsing()
+      throws ParserException, InterruptedException, InvalidConfigurationException, IOException {
+    ParseResult fileResult = fileParser.parseFiles(ImmutableList.of(fileName));
+    String code = MoreFiles.asCharSource(Path.of(fileName), Charset.defaultCharset()).read();
+    ParseResult stringResult = stringParser.parseString("test.c", code);
+    assertThat(stringResult.isEmpty()).isEqualTo(fileResult.isEmpty());
+    assertThat(stringResult.getCFANodes()).hasSize(fileResult.getCFANodes().size());
+    assertThat(stringResult.getFunctions().keySet())
+        .containsExactlyElementsIn(fileResult.getFunctions().keySet());
+    for (String function : stringResult.getFunctions().keySet()) {
+      assertThat(stringResult.getCFANodes().get(function))
+          .hasSize(fileResult.getCFANodes().get(function).size());
+    }
+    assertThat(stringResult.getGlobalDeclarations()).isEqualTo(fileResult.getGlobalDeclarations());
+  }
+}
