81458615b0e2752ae154eb76f84408cbce73a448
holzners <holzners@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2021-07-20 16:35:42 +0000

    Join Values implementation FIT-TR-2013-4 Algoritm 5 (appendix C.3)
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@37747 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

---
+++ src/org/sosy_lab/cpachecker/util/smg/join/SMGJoinTargetObjects.java
@@ -0,0 +1,42 @@
+// This file is part of CPAchecker,
+// a tool for configurable software verification:
+// https://cpachecker.sosy-lab.org
+//
+// SPDX-FileCopyrightText: 2021 Dirk Beyer <https://www.sosy-lab.org>
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package org.sosy_lab.cpachecker.util.smg.join;
+
+import org.sosy_lab.cpachecker.cpa.smg.join.SMGJoinStatus;
+import org.sosy_lab.cpachecker.util.smg.SMG;
+import org.sosy_lab.cpachecker.util.smg.graph.SMGValue;
+
+/**
+ * Class implementing join algorithm from FIT-TR-2013-4 (Appendix C.3)
+ */
+public class SMGJoinTargetObjects extends SMGAbstractJoinValues {
+
+
+
+  public SMGJoinTargetObjects(
+      SMGJoinStatus pStatus,
+      SMG pInputSMG1,
+      SMG pInputSMG2,
+      SMG pDestSMG,
+      NodeMapping pMapping1,
+      NodeMapping pMapping2,
+      SMGValue pValue1,
+      SMGValue pValue2,
+      int pNestingLevelDiff) {
+    super(pStatus, pInputSMG1, pInputSMG2, pDestSMG, pMapping1, pMapping2);
+
+  }
+
+
+
+
+
+
+
+}
