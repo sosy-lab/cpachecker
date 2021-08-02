47fb6bf4020121cc3e6b6f782acf73ee4552aefb
holzners <holzners@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2021-08-02 08:35:03 +0000

    Rewrite SMGs: Map target Object  implementation
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@37820 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/util/smg/join/SMGMapTargetAddress.java
+++ src/org/sosy_lab/cpachecker/util/smg/join/SMGMapTargetAddress.java
@@ -8,8 +8,14 @@
 
 package org.sosy_lab.cpachecker.util.smg.join;
 
+import java.util.Map;
+import java.util.Optional;
 import org.sosy_lab.cpachecker.cpa.smg.join.SMGJoinStatus;
 import org.sosy_lab.cpachecker.util.smg.SMG;
+import org.sosy_lab.cpachecker.util.smg.graph.SMGObject;
+import org.sosy_lab.cpachecker.util.smg.graph.SMGPointsToEdge;
+import org.sosy_lab.cpachecker.util.smg.graph.SMGSymbolicValue;
+import org.sosy_lab.cpachecker.util.smg.graph.SMGTargetSpecifier;
 import org.sosy_lab.cpachecker.util.smg.graph.SMGValue;
 
 /**
@@ -18,7 +24,6 @@ import org.sosy_lab.cpachecker.util.smg.graph.SMGValue;
 public class SMGMapTargetAddress extends SMGAbstractJoinValues {
 
 
-  @SuppressWarnings("unused")
   public SMGMapTargetAddress(
       SMGJoinStatus pStatus,
       SMG pInputSMG1,
@@ -30,12 +35,45 @@ public class SMGMapTargetAddress extends SMGAbstractJoinValues {
       SMGValue pValue2,
       int pNestingLevelDiff) {
     super(pStatus, pInputSMG1, pInputSMG2, pDestSMG, pMapping1, pMapping2);
-
+    mapTargetAddress(pValue1, pValue2);
   }
 
+  private void mapTargetAddress(SMGValue v1, SMGValue v2) {
+    // Step 1
+    SMGPointsToEdge ptoEdge1 = inputSMG1.getPTEdge(v2);
+    SMGPointsToEdge pToEdge2 = inputSMG2.getPTEdge(v2);
+    // Step 2
+    SMGObject resSmgObject =
+        ptoEdge1.pointsTo().isZero()
+            ? SMGObject.nullInstance()
+            : mapping1.getMappedObject(ptoEdge1.pointsTo());
+    // Step 3
+    SMGTargetSpecifier tg =
+        isDLLS(ptoEdge1.pointsTo()) ? ptoEdge1.targetSpecifier() : pToEdge2.targetSpecifier();
 
+    // Step 4
+    Optional<Map.Entry<SMGValue, SMGPointsToEdge>> matchingAddressOptional =
+        destSMG.getPTEdgeMapping().entrySet().stream()
+            .filter(
+                entry ->
+                    entry.getValue().getOffset().equals(ptoEdge1.getOffset())
+                        && entry.getValue().targetSpecifier().equals(tg)
+                        && entry.getValue().pointsTo().equals(resSmgObject))
+            .findAny();
+    if (matchingAddressOptional.isPresent()) {
+      value = matchingAddressOptional.get().getKey();
+    } else {
+      // Step 5
+      value = SMGSymbolicValue.of(0); // TODO nesting level zero???
+      destSMG = destSMG.copyAndAddValue(value);
+      SMGPointsToEdge newEdge = new SMGPointsToEdge(resSmgObject, ptoEdge1.getOffset(), tg);
+      destSMG = destSMG.copyAndAddPTEdge(newEdge, value);
+      // Step 6
+      mapping1.addMapping(v1, value);
+      mapping2.addMapping(v2, value);
+    }
 
-
+  }
 
 
 
