e61e50ead2a18b8a35c7edd3382c59a2a8593a8b
pwaldinger <pwaldinger@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2021-08-16 13:55:33 +0000

    update Javadoc/reformat code
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/block-based-task-partitioning@37991 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/core/algorithm/concurrent/ShareableBooleanFormula.java
+++ src/org/sosy_lab/cpachecker/core/algorithm/concurrent/ShareableBooleanFormula.java
@@ -15,24 +15,21 @@ import org.sosy_lab.cpachecker.util.predicates.smt.Solver;
 import org.sosy_lab.java_smt.api.BooleanFormula;
 
 /**
- * {@link ShareableBooleanFormula} makes a {@link BooleanFormula} available to an arbitrary solver
- * instance.
- * <p>
- *  {@link Solver} provides no thread-safety.
- *  As such, concurrent operations must rely on separate {@link Solver} instances.
- *  Operations on {@link BooleanFormula}s are only supported among instances which belong to the
- *  same solver. As such, {@link BooleanFormula}s created under the supervision of different
- *  {@link Solver}s are not compatible at first.
- * </p>
- * <p>
- *   {@link ShareableBooleanFormula} encapsulates a {@link BooleanFormula} together with the
- *   {@link FormulaManagerView} to which it belongs.
- *   The method {@link ShareableBooleanFormula#getFor(FormulaManagerView)} provides the user with the
- *   ability to obtain an equivalent {@link BooleanFormula} usable in the context of a different (or
- *   the identical) {@link FormulaManagerView}. As such, it offers an opportunity to easily exchange
- *   a {@link BooleanFormula} across threads whose operations rely on different underlying instances
- *   of {@link Solver}.
- * </p>
+ * {@link ShareableBooleanFormula} allows sharing a {@link BooleanFormula} between code relying on
+ * separate {@link Solver} instances.
+ *
+ * <p>{@link Solver} provides no thread-safety. As such, concurrent operations must rely on separate
+ * {@link Solver} instances. Operations on {@link BooleanFormula}s are only supported among
+ * instances which belong to the same {@link Solver}. As such, {@link BooleanFormula}s created under
+ * the supervision of different {@link Solver}s are not compatible at first.
+ *
+ * <p>{@link ShareableBooleanFormula} encapsulates a {@link BooleanFormula} together with the {@link
+ * FormulaManagerView} to which it belongs. The method {@link
+ * ShareableBooleanFormula#getFor(FormulaManagerView)} provides the user with the ability to obtain
+ * an equivalent {@link BooleanFormula} usable in the context of an arbitrary {@link
+ * FormulaManagerView}. As such, it offers an opportunity to easily exchange a {@link
+ * BooleanFormula} across threads whose operations rely on different underlying instances of {@link
+ * Solver}.
  */
 public class ShareableBooleanFormula {
   private final FormulaManagerView formulaManager;
@@ -44,11 +41,11 @@ public class ShareableBooleanFormula {
     checkNotNull(pFormula);
 
     formulaManager = pFormulaManager;
-    formula =pFormula;
+    formula = pFormula;
   }
 
   public BooleanFormula getFor(FormulaManagerView targetManager) {
-    if(formulaManager == targetManager){
+    if (formulaManager == targetManager) {
       return formula;
     }
 
