dc6f17e4f2ca2c2eeec8080cbcf51680d6316e55
pwaldinger <pwaldinger@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2021-08-25 10:21:57 +0000

    support backward analysis in BlockAwareCompositeCPA
    Depending on the analysis direction configured for PredicateCPA,
    BlockAwareCompositeCPATransferRelation now appropriately changes the way
    it checks whether an edge still belongs to the target block.
    
    Probably BlockAwareCompositeCPA should acquire the configured analysis
    direction in a cleaner way than by shadowing the configuration option of
    PredicateCPA and hijacking the same prefix ...
    
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/block-based-task-partitioning@38142 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/cpa/composite/BlockAwareCompositeCPA.java
+++ src/org/sosy_lab/cpachecker/cpa/composite/BlockAwareCompositeCPA.java
@@ -6,6 +6,8 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
+// @formatter:off
+
 package org.sosy_lab.cpachecker.cpa.composite;
 
 import static com.google.common.base.Preconditions.checkState;
@@ -14,10 +16,13 @@ import org.checkerframework.checker.nullness.qual.Nullable;
 import org.sosy_lab.common.ShutdownNotifier;
 import org.sosy_lab.common.configuration.Configuration;
 import org.sosy_lab.common.configuration.InvalidConfigurationException;
+import org.sosy_lab.common.configuration.Option;
+import org.sosy_lab.common.configuration.Options;
 import org.sosy_lab.common.log.LogManager;
 import org.sosy_lab.cpachecker.cfa.CFA;
 import org.sosy_lab.cpachecker.cfa.blockgraph.Block;
 import org.sosy_lab.cpachecker.cfa.model.CFANode;
+import org.sosy_lab.cpachecker.core.AnalysisDirection;
 import org.sosy_lab.cpachecker.core.defaults.AbstractCPAFactory;
 import org.sosy_lab.cpachecker.core.interfaces.AbstractDomain;
 import org.sosy_lab.cpachecker.core.interfaces.AbstractState;
@@ -32,49 +37,20 @@ import org.sosy_lab.cpachecker.core.interfaces.TransferRelation;
 import org.sosy_lab.cpachecker.core.interfaces.WrapperCPA;
 import org.sosy_lab.cpachecker.exceptions.CPAException;
 
+@Options(prefix = "cpa.predicate")
 public class BlockAwareCompositeCPA implements ConfigurableProgramAnalysis, WrapperCPA {
-  @SuppressWarnings("FieldMayBeFinal")
-  private static class BlockAwareCompositeCPAFactory extends AbstractCPAFactory {
-    private CFA cfa = null;
-
-    private Block block = null;
-
-    private CompositeCPA wrappedCPA = null;
-
-    @Override
-    public ConfigurableProgramAnalysis createInstance()
-        throws InvalidConfigurationException, CPAException {
-      final String message = "Missing data to create BlockAwareCompositeCPA: ";
-      checkState(cfa != null, message + "CFA");
-      checkState(block != null, message + "Block");
-      checkState(wrappedCPA != null, message + "CompositeCPA");
-
-      final Configuration config = getConfiguration();
-      final LogManager logger = getLogger();
-      final ShutdownNotifier shutdownNotifier = getShutdownNotifier();
-
-      return new BlockAwareCompositeCPA(block, wrappedCPA);
-    }
-
-    @Override
-    public <T> CPAFactory set(T pObject, Class<T> pClass) throws UnsupportedOperationException {
-      if (pClass.equals(CFA.class)) {
-        cfa = (CFA) pObject;
-      } else if (pClass.equals(Block.class)) {
-        block = (Block) pObject;
-      } else if (pClass.equals(CompositeCPA.class)) {
-        wrappedCPA = (CompositeCPA) pObject;
-      }
-
-      return super.set(pObject, pClass);
-    }
-  }
-
   private final Block block;
-
   private final CompositeCPA cpa;
 
-  private BlockAwareCompositeCPA(final Block pBlock, final CompositeCPA pCPA) {
+  @SuppressWarnings("FieldMayBeFinal")
+  @Option(secure = true, description = "Direction of the analysis?")
+  private AnalysisDirection direction = AnalysisDirection.FORWARD;
+
+  private BlockAwareCompositeCPA(
+      final Block pBlock, final CompositeCPA pCPA, final Configuration pConfig)
+      throws InvalidConfigurationException {
+    pConfig.inject(this);
+
     block = pBlock;
     cpa = pCPA;
   }
@@ -91,7 +67,7 @@ public class BlockAwareCompositeCPA implements ConfigurableProgramAnalysis, Wrap
   @Override
   public TransferRelation getTransferRelation() {
     CompositeTransferRelation transferRelation = cpa.getTransferRelation();
-    return new BlockAwareCompositeTransferRelation(block, transferRelation);
+    return new BlockAwareCompositeTransferRelation(block, transferRelation, direction);
   }
 
   @Override
@@ -129,5 +105,42 @@ public class BlockAwareCompositeCPA implements ConfigurableProgramAnalysis, Wrap
   @Override
   public Iterable<ConfigurableProgramAnalysis> getWrappedCPAs() {
     return cpa.getWrappedCPAs();
+  }
+
+  @SuppressWarnings("FieldMayBeFinal")
+  private static class BlockAwareCompositeCPAFactory extends AbstractCPAFactory {
+    private CFA cfa = null;
+
+    private Block block = null;
+
+    private CompositeCPA wrappedCPA = null;
+
+    @Override
+    public ConfigurableProgramAnalysis createInstance()
+        throws InvalidConfigurationException, CPAException {
+      final String message = "Missing data to create BlockAwareCompositeCPA: ";
+      checkState(cfa != null, message + "CFA");
+      checkState(block != null, message + "Block");
+      checkState(wrappedCPA != null, message + "CompositeCPA");
+
+      final Configuration config = getConfiguration();
+      final LogManager logger = getLogger();
+      final ShutdownNotifier shutdownNotifier = getShutdownNotifier();
+
+      return new BlockAwareCompositeCPA(block, wrappedCPA, config);
+    }
+
+    @Override
+    public <T> CPAFactory set(T pObject, Class<T> pClass) throws UnsupportedOperationException {
+      if (pClass.equals(CFA.class)) {
+        cfa = (CFA) pObject;
+      } else if (pClass.equals(Block.class)) {
+        block = (Block) pObject;
+      } else if (pClass.equals(CompositeCPA.class)) {
+        wrappedCPA = (CompositeCPA) pObject;
+      }
+
+      return super.set(pObject, pClass);
+    }
   }
 }
--- src/org/sosy_lab/cpachecker/cpa/composite/BlockAwareCompositeTransferRelation.java
+++ src/org/sosy_lab/cpachecker/cpa/composite/BlockAwareCompositeTransferRelation.java
@@ -12,13 +12,11 @@ import static org.sosy_lab.cpachecker.util.AbstractStates.extractStateByType;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
 import org.checkerframework.checker.nullness.qual.Nullable;
 import org.sosy_lab.cpachecker.cfa.blockgraph.Block;
 import org.sosy_lab.cpachecker.cfa.model.CFAEdge;
+import org.sosy_lab.cpachecker.core.AnalysisDirection;
 import org.sosy_lab.cpachecker.core.interfaces.AbstractState;
-import org.sosy_lab.cpachecker.core.interfaces.AbstractStateWithLocations;
-import org.sosy_lab.cpachecker.core.interfaces.AbstractWrapperState;
 import org.sosy_lab.cpachecker.core.interfaces.Precision;
 import org.sosy_lab.cpachecker.core.interfaces.TransferRelation;
 import org.sosy_lab.cpachecker.cpa.location.LocationState;
@@ -29,39 +27,53 @@ public class BlockAwareCompositeTransferRelation implements TransferRelation {
 
   private final Block block;
 
-  BlockAwareCompositeTransferRelation(final Block pBlock, final CompositeTransferRelation pTransferRelation) {
+  private final AnalysisDirection direction;
+
+  BlockAwareCompositeTransferRelation(
+      final Block pBlock,
+      final CompositeTransferRelation pTransferRelation,
+      final AnalysisDirection pDirection) {
     block = pBlock;
     transferRelation = pTransferRelation;
+    direction = pDirection;
   }
 
   @Override
-  public Collection<CompositeState> getAbstractSuccessors(
-      AbstractState state, Precision precision) throws CPATransferException, InterruptedException {
+  public Collection<CompositeState> getAbstractSuccessors(AbstractState state, Precision precision)
+      throws CPATransferException, InterruptedException {
     Collection<CompositeState> successors;
 
     LocationState locState = extractStateByType(state, LocationState.class);
     successors = new ArrayList<>(2);
     if (locState == null) {
-      final String message
-        = "BlockAwareCompositeTransferRelation requires a composite CPA with LocationCPA";
+      final String message =
+          "BlockAwareCompositeTransferRelation requires a composite CPA with LocationCPA";
       throw new CPATransferException(message);
-    } else {
-      for (CFAEdge edge : locState.getOutgoingEdges()) {
-        if(block.contains(edge.getSuccessor())) {
-          successors.addAll(transferRelation.getAbstractSuccessorsForEdge(state, precision, edge));
-        }
+    }
+
+    for (CFAEdge edge : locState.getOutgoingEdges()) {
+      if (remainsWithinBlock(edge)) {
+        successors.addAll(transferRelation.getAbstractSuccessorsForEdge(state, precision, edge));
       }
     }
 
     return successors;
   }
 
+  private boolean remainsWithinBlock(final CFAEdge edge) {
+    if (direction == AnalysisDirection.FORWARD) {
+      return block.contains(edge.getSuccessor());
+    } else {
+      return block.contains(edge.getPredecessor());
+    }
+  }
+
   @Override
   public Collection<? extends AbstractState> getAbstractSuccessorsForEdge(
       AbstractState state, Precision precision, CFAEdge cfaEdge)
       throws CPATransferException, InterruptedException {
-    final String message
-        = "BlockAwareCompositeTransferRelation does not implement getAbstractSuccessorsForEdge";
+    final String message =
+        "BlockAwareCompositeTransferRelation does not implement getAbstractSuccessorsForEdge";
     throw new CPATransferException(message);
   }
 
@@ -70,7 +82,8 @@ public class BlockAwareCompositeTransferRelation implements TransferRelation {
       AbstractState state,
       Iterable<AbstractState> otherStates,
       @Nullable CFAEdge cfaEdge,
-      Precision precision) throws CPATransferException, InterruptedException {
+      Precision precision)
+      throws CPATransferException, InterruptedException {
     return transferRelation.strengthen(state, otherStates, cfaEdge, precision);
   }
 }
