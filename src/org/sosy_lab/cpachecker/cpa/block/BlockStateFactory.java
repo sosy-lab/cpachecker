494071291ec32c2a60ba799dc7099f2b4bdfcbee
mkettl <mkettl@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2021-11-25 22:44:01 +0000

    Remove unused Options annotation from BlockStateFactory
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/configurable-code-block-analysis@38927 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/cpa/block/BlockStateFactory.java
+++ src/org/sosy_lab/cpachecker/cpa/block/BlockStateFactory.java
@@ -15,13 +15,11 @@ import com.google.common.collect.ImmutableSortedSet;
 import java.util.Collection;
 import org.sosy_lab.common.configuration.Configuration;
 import org.sosy_lab.common.configuration.InvalidConfigurationException;
-import org.sosy_lab.common.configuration.Options;
 import org.sosy_lab.cpachecker.cfa.CFA;
 import org.sosy_lab.cpachecker.cfa.model.CFANode;
 import org.sosy_lab.cpachecker.core.AnalysisDirection;
 import org.sosy_lab.cpachecker.cpa.block.BlockState.BackwardsBlockState;
 
-@Options(prefix = "cpa.block")
 public class BlockStateFactory {
 
   private final BlockState[] states;
