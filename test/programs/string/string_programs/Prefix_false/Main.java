92c2882daa037fd798c7e6f65bf7f808c676720b
santonischki <santonischki@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2021-12-18 18:34:15 +0000

    Corrected Header for test Prefix_false
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/stringcpa@39080 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- test/programs/string/string_programs/Prefix_false/Main.java
+++ test/programs/string/string_programs/Prefix_false/Main.java
@@ -1,4 +1,10 @@
-package ownPrograms;
+// This file is part of CPAchecker,
+// a tool for configurable software verification:
+// https://cpachecker.sosy-lab.org
+//
+// SPDX-FileCopyrightText: 2021 Dirk Beyer <https://www.sosy-lab.org>
+//
+// SPDX-License-Identifier: Apache-2.0
 
 public class Main {
 
