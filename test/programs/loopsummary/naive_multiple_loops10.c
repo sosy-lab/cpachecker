403808576c12230a49fc2146c1a48c12845e1f93
mlingsch <mlingsch@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2021-12-20 01:20:53 +0000

    Added more examples with multiple loops
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/loopsummary@39086 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

---
+++ test/programs/loopsummary/naive_multiple_loops10.c
@@ -0,0 +1,37 @@
+// This file is part of CPAchecker,
+// a tool for configurable software verification:
+// https://cpachecker.sosy-lab.org
+//
+// SPDX-FileCopyrightText: 2007-2021 Dirk Beyer <https://www.sosy-lab.org>
+//
+// SPDX-License-Identifier: Apache-2.0
+
+void reach_error(){}
+void __VERIFIER_assert(int cond) {
+  if (!cond) {
+    reach_error();
+  }
+}
+
+extern int __VERIFIER_nondet_int(void);
+
+int main() {
+  int x = __VERIFIER_nondet_int();
+  if (x < 1000000) {
+    while (x<1000000) {
+      x+=1;
+    }
+    __VERIFIER_assert(x == 1000000);
+
+    while (x<100000000) {
+      x+=1;
+    }
+    __VERIFIER_assert(x == 100000000);
+
+    while (x<1000000000) {
+      x+=1;
+    }
+    __VERIFIER_assert(x == 1000000000);
+  }
+  return 0;
+}
