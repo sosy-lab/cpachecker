70051308a0ad07a2e4f4f5a6aa83237ba8bccffa
santonischki <santonischki@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2021-12-21 16:16:20 +0000

    Now checks for equality in length domain
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/stringcpa@39104 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/cpa/string/domains/LengthDomain.java
+++ src/org/sosy_lab/cpachecker/cpa/string/domains/LengthDomain.java
@@ -13,7 +13,7 @@ import org.sosy_lab.cpachecker.cpa.string.utils.Aspect;
 import org.sosy_lab.cpachecker.cpa.string.utils.Aspect.UnknownAspect;
 
 /*
- * Tracks the Length of String
+ * Tracks the Length of a string
  */
 public class LengthDomain implements AbstractStringDomain<Integer> {
 
@@ -34,36 +34,28 @@ public class LengthDomain implements AbstractStringDomain<Integer> {
 
   @Override
   public boolean isLessOrEqual(Aspect<?> p1, Aspect<?> p2) {
-
     if (p1.getDomainType().equals(TYPE) && p2.getDomainType().equals(TYPE)) {
-
       int len1 = (int) p1.getValue();
       int len2 = (int) p2.getValue();
-
-      return len1 >= len2;
+      // The length domain is defined as a flat lattice, so we check for equality here
+      return len1 == len2;
     }
-
     return false;
   }
 
   @Override
   public Aspect<?> combineAspectsForStringConcat(Aspect<?> p1, Aspect<?> p2) {
-
     if (p1 instanceof UnknownAspect) {
       return p2;
     }
     if (p2 instanceof UnknownAspect) {
       return p1;
     }
-
     if (p1.getDomainType().equals(TYPE) && p2.getDomainType().equals(TYPE)) {
-
       int len1 = (int) p1.getValue();
       int len2 = (int) p2.getValue();
-
       return new Aspect<>(this, len1 + len2);
     }
-
     return UnknownAspect.getInstance();
   }
 
