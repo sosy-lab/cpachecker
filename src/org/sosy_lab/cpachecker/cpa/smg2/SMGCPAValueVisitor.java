c15c6575e23fd4dad1a19e9f10261d9da402c453
dbaier <dbaier@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2022-02-09 11:10:24 +0000

    New SMG: Add visitor handling of float literal expressions
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/smgv2@39378 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/cpa/smg2/SMGCPAValueVisitor.java
+++ src/org/sosy_lab/cpachecker/cpa/smg2/SMGCPAValueVisitor.java
@@ -9,6 +9,7 @@
 package org.sosy_lab.cpachecker.cpa.smg2;
 
 import com.google.common.collect.ImmutableList;
+import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.util.List;
 import org.sosy_lab.cpachecker.cfa.ast.c.CAddressOfLabelExpression;
@@ -163,13 +164,15 @@ public class SMGCPAValueVisitor
   @Override
   public List<SMGValueAndSMGState> visit(CFloatLiteralExpression e) throws CPATransferException {
     // Floating point value expression
-    // BigDecimal value = e.getValue();
+    BigDecimal value = e.getValue();
 
     // If the value is == 0 we return the zero value without checking as this one always exists.
+    if (value.compareTo(BigDecimal.ZERO) == 0) {
+      return ImmutableList.of(SMGValueAndSMGState.of(state, SMGValue.zeroValue()));
+    }
     // Check if the value exists already, if it does, return that, else create a new one and return
-    // that one.
-
-    return visitDefault(e);
+    // that one. createNewValueAndMap() does both!
+    return ImmutableList.of(evaluator.createNewValueAndMap(new NumericValue(value), state));
   }
 
   @SuppressWarnings("unused")
