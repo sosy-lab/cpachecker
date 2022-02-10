ef24d1e3fb8e077c098fcaa1446d4dea74b20309
jhaltermann <jhaltermann@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2022-02-10 08:56:13 +0000

    Added package info
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/uca-generation@39389 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

---
+++ src/org/sosy_lab/cpachecker/core/algorithm/ucageneration/package-info.java
@@ -0,0 +1,12 @@
+// This file is part of CPAchecker,
+// a tool for configurable software verification:
+// https://cpachecker.sosy-lab.org
+//
+// SPDX-FileCopyrightText: 2022 Dirk Beyer <https://www.sosy-lab.org>
+//
+// SPDX-License-Identifier: Apache-2.0
+
+/**
+ * This package contains all code needed to generate UCAs
+ */
+package org.sosy_lab.cpachecker.core.algorithm.ucageneration;
