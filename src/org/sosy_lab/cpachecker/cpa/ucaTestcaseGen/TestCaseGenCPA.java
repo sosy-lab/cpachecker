bb016167ef74f84e5ededf9234811e995c045026
jhaltermann <jhaltermann@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2022-02-10 10:20:49 +0000

    Optimized imports
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/uca-generation@39408 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/cpa/ucaTestcaseGen/TestCaseGenCPA.java
+++ src/org/sosy_lab/cpachecker/cpa/ucaTestcaseGen/TestCaseGenCPA.java
@@ -11,7 +11,6 @@ package org.sosy_lab.cpachecker.cpa.ucaTestcaseGen;
 import org.sosy_lab.common.configuration.Configuration;
 import org.sosy_lab.common.configuration.InvalidConfigurationException;
 import org.sosy_lab.common.log.LogManager;
-import org.sosy_lab.cpachecker.cfa.CFA;
 import org.sosy_lab.cpachecker.cfa.model.CFANode;
 import org.sosy_lab.cpachecker.core.defaults.AbstractCPA;
 import org.sosy_lab.cpachecker.core.defaults.AutomaticCPAFactory;
