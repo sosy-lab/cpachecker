5bd175564b8fbfe2888452235fc3e7f6f81e3bba
vport <vport@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2022-02-19 13:51:50 +0000

    Add documentation to 'Slice'
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/program-slicing@39547 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/util/slicing/Slice.java
+++ src/org/sosy_lab/cpachecker/util/slicing/Slice.java
@@ -15,17 +15,71 @@ import org.sosy_lab.cpachecker.cfa.ast.ASimpleDeclaration;
 import org.sosy_lab.cpachecker.cfa.model.CFAEdge;
 import org.sosy_lab.cpachecker.util.states.MemoryLocation;
 
+/** Represents a program slice. */
 public interface Slice {
 
+  /**
+   * Returns the {@link CFA} from which this program slice was created.
+   *
+   * @return the {@link CFA} from which this program slice was created
+   */
   CFA getOriginalCfa();
 
+  /**
+   * Returns the slicing criteria that were used to create this program slice.
+   *
+   * @return the slicing criteria that were used to create this program slice
+   */
   ImmutableCollection<CFAEdge> getSlicingCriteria();
 
+  /**
+   * Returns a set of all edges contained in this program slice
+   *
+   * <p>The edges contained in a program slice are called relevant edges. Some edges are only
+   * partially relevant to a program slice (e.g., not all function parameters are relevant). These
+   * partially relevant edges are, as well as the fully relevant edges, contained in the returned
+   * set.
+   *
+   * @return a set of all edges contained in this program slice
+   */
   ImmutableSet<CFAEdge> getRelevantEdges();
 
+  /**
+   * Returns a set of all declarations in this program slice.
+   *
+   * @return a set of all declarations in this program slice
+   */
   ImmutableSet<ASimpleDeclaration> getRelevantDeclarations();
 
+  /**
+   * Returns whether the write to the specified memory location at the specified edge is relevant to
+   * this program slice.
+   *
+   * <p>If the specified memory location is not actually written to at the specified edge, either
+   * {@code true} or {@code false} is returned.
+   *
+   * @param pEdge the {@link CFAEdge} at which the memory location is written to
+   * @param pMemoryLocation the {@link MemoryLocation} that is written to
+   * @return whether the write to the specified memory location at the specified edge is relevant to
+   *     this program slice
+   * @throws IllegalArgumentException if {@code pEdge} is not a relevant edge to this program slice
+   * @throws NullPointerException if any parameter is {@code null}
+   */
   boolean isRelevantDef(CFAEdge pEdge, MemoryLocation pMemoryLocation);
 
+  /**
+   * Returns whether the read of the specified memory location at the specified edge is relevant
+   * to this program slice.
+   *
+   * <p>If the specified memory location is not actually read at the specified edge, either {@code
+   * true} or {@code false} is returned.
+   *
+   * @param pEdge the {@link CFAEdge} at which the memory location is read
+   * @param pMemoryLocation the {@link MemoryLocation} that is read
+   * @return whether the read of the specified memory location at the specified edge is relevant
+   *     to this program slice
+   * @throws IllegalArgumentException if {@code pEdge} is not a relevant edge to this program slice
+   * @throws NullPointerException if any parameter is {@code null}
+   */
   boolean isRelevantUse(CFAEdge pEdge, MemoryLocation pMemoryLocation);
 }
