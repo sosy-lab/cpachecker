7d84fd0cc3a76653c3abf46775018bf8b73cbedd
pwendler <pwendler@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2022-03-04 08:50:23 +0000

    Remove an anonymous Function instance from CParserUtils
    Putting code into proper methods instead of anonymous Function instances
    is better to read and easier to test.
    If the code is long enough that it should not be a lambda,
    then it is also long enough to have its own function.
    
    As a side effect from adjusting the callers,
    let's also remove some reassignments to local variables.
    
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@39673 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/util/CParserUtils.java
+++ src/org/sosy_lab/cpachecker/util/CParserUtils.java
@@ -8,6 +8,7 @@
 
 package org.sosy_lab.cpachecker.util;
 
+import static com.google.common.collect.FluentIterable.from;
 import static org.sosy_lab.common.collect.Collections3.transformedImmutableListCopy;
 
 import com.google.common.base.Function;
@@ -219,9 +220,9 @@ public class CParserUtils {
       for (String assumeCode : pStatements) {
         Collection<CStatement> statements =
             parseAsCStatements(assumeCode, pResultFunction, pCParser, pScope, pParserTools);
-        statements = adjustCharAssignmentSignedness(statements);
-        statements = removeDuplicates(statements);
-        result.addAll(statements);
+        result.addAll(
+            removeDuplicates(
+                from(statements).transform(CParserUtils::adjustCharAssignmentSignedness)));
       }
       return result;
     }
@@ -341,7 +342,8 @@ public class CParserUtils {
           CParserUtils.parseListOfStatements(
               tryFixACSL(assumeCode, pResultFunction, pScope), pCParser, pScope);
     }
-    statements = removeDuplicates(adjustCharAssignmentSignedness(statements));
+    statements =
+        removeDuplicates(from(statements).transform(CParserUtils::adjustCharAssignmentSignedness));
     {
       CBinaryExpressionBuilder binaryExpressionBuilder =
           new CBinaryExpressionBuilder(pParserTools.machineModel, pParserTools.logger);
@@ -614,40 +616,31 @@ public class CParserUtils {
   }
 
   /**
-   * Be nice to tools that assume that default char (when it is neither
-   * specified as signed nor as unsigned) may be unsigned.
+   * Be nice to tools that assume that default char (when it is neither specified as signed nor as
+   * unsigned) may be unsigned.
    *
-   * @param pStatements the assignment statements.
-   *
-   * @return the adjusted statements.
+   * @param pStatement the assignment statement.
+   * @return the adjusted statement.
    */
-  private static Collection<CStatement> adjustCharAssignmentSignedness(
-      Iterable<? extends CStatement> pStatements) {
-    return FluentIterable.from(pStatements).transform(new Function<CStatement, CStatement>() {
-
-      @Override
-      public CStatement apply(CStatement pStatement) {
-        if (pStatement instanceof CExpressionAssignmentStatement) {
-          CExpressionAssignmentStatement statement = (CExpressionAssignmentStatement) pStatement;
-          CLeftHandSide leftHandSide = statement.getLeftHandSide();
-          CType canonicalType = leftHandSide.getExpressionType().getCanonicalType();
-          if (canonicalType instanceof CSimpleType) {
-            CSimpleType simpleType = (CSimpleType) canonicalType;
-            CBasicType basicType = simpleType.getType();
-            if (basicType.equals(CBasicType.CHAR) && !simpleType.isSigned()
-                && !simpleType.isUnsigned()) {
-              CExpression rightHandSide = statement.getRightHandSide();
-              CExpression castedRightHandSide = new CCastExpression(rightHandSide.getFileLocation(),
-                  canonicalType, rightHandSide);
-              return new CExpressionAssignmentStatement(statement.getFileLocation(), leftHandSide,
-                  castedRightHandSide);
-            }
-          }
+  private static CStatement adjustCharAssignmentSignedness(CStatement pStatement) {
+    if (pStatement instanceof CExpressionAssignmentStatement) {
+      CExpressionAssignmentStatement statement = (CExpressionAssignmentStatement) pStatement;
+      CLeftHandSide leftHandSide = statement.getLeftHandSide();
+      CType canonicalType = leftHandSide.getExpressionType().getCanonicalType();
+      if (canonicalType instanceof CSimpleType) {
+        CSimpleType simpleType = (CSimpleType) canonicalType;
+        CBasicType basicType = simpleType.getType();
+        if (basicType.equals(CBasicType.CHAR) && !simpleType.isSigned()
+            && !simpleType.isUnsigned()) {
+          CExpression rightHandSide = statement.getRightHandSide();
+          CExpression castedRightHandSide = new CCastExpression(rightHandSide.getFileLocation(),
+              canonicalType, rightHandSide);
+          return new CExpressionAssignmentStatement(statement.getFileLocation(), leftHandSide,
+              castedRightHandSide);
         }
-        return pStatement;
       }
-
-    }).toList();
+    }
+    return pStatement;
   }
 
   /**
