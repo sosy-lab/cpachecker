626e99dd8ba4fc333b438c423bfa18d049bab01e
pchien <pchien@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2022-03-07 19:52:57 +0000

    Refactor to simplify the code and improve documentation - Extract the common methods from ARGStopSep and ARGStopJoin to reduce redundancy
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/impact-stop-join@39699 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/cpa/arg/ARGStopJoin.java
+++ src/org/sosy_lab/cpachecker/cpa/arg/ARGStopJoin.java
@@ -11,93 +11,44 @@ package org.sosy_lab.cpachecker.cpa.arg;
 import de.uni_freiburg.informatik.ultimate.util.datastructures.ImmutableSet;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
-import java.util.logging.Level;
 import org.sosy_lab.common.log.LogManager;
 import org.sosy_lab.cpachecker.core.interfaces.AbstractState;
 import org.sosy_lab.cpachecker.core.interfaces.CoveringStateSetProvider;
-import org.sosy_lab.cpachecker.core.interfaces.ForcedCoveringStopOperator;
 import org.sosy_lab.cpachecker.core.interfaces.Precision;
 import org.sosy_lab.cpachecker.core.interfaces.StopOperator;
 import org.sosy_lab.cpachecker.exceptions.CPAException;
 
-/** A stop join operator for ARGCPA, mostly copy from {@link ARGStopSep} */
-public class ARGStopJoin implements StopOperator, ForcedCoveringStopOperator {
-
-  private final boolean keepCoveredStatesInReached;
-  private final boolean inCPAEnabledAnalysis;
-  private final boolean coverTargetStates;
-  private final StopOperator wrappedStop;
-  private final LogManager logger;
-
+class ARGStopJoin extends ARGStop {
   public ARGStopJoin(
       StopOperator pWrappedStop,
       LogManager pLogger,
       boolean pInCPAEnabledAnalysis,
       boolean pKeepCoveredStatesInReached,
       boolean pCoverTargetStates) {
-    wrappedStop = pWrappedStop;
-    logger = pLogger;
-    keepCoveredStatesInReached = pKeepCoveredStatesInReached;
-    inCPAEnabledAnalysis = pInCPAEnabledAnalysis;
-    coverTargetStates = pCoverTargetStates;
+    super(
+        pWrappedStop,
+        pLogger,
+        pInCPAEnabledAnalysis,
+        pKeepCoveredStatesInReached,
+        pCoverTargetStates);
   }
 
   @Override
   public boolean stop(
       AbstractState pElement, Collection<AbstractState> pReached, Precision pPrecision)
       throws CPAException, InterruptedException {
-
     ARGState argElement = (ARGState) pElement;
     assert !argElement.isCovered()
         : "Passing element to stop which is already covered: " + argElement;
 
-    // First check if we can take a shortcut:
-    // If the new state was merged into an existing element,
-    // it is usually also covered by this existing element, so check this explicitly upfront.
-    // We do this because we want to remove the new state from the ARG completely
-    // in this case and not mark it as covered.
-
-    if (argElement.getMergedWith() != null) {
-      ARGState mergedWith = argElement.getMergedWith();
-
-      if (pReached.contains(mergedWith)) {
-        // we do this single check first as it should return true in most of the cases
-
-        if (wrappedStop.stop(
-            argElement.getWrappedState(),
-            Collections.singleton(mergedWith.getWrappedState()),
-            pPrecision)) {
-          // merged and covered
-          if (inCPAEnabledAnalysis) {
-            argElement.setCovered(mergedWith);
-          } else {
-            argElement.removeFromARG();
-          }
-          logger.log(Level.FINEST, "Element is covered by the element it was merged into");
-
-          // in this case, return true even if we should keep covered states
-          // because we should anyway not keep merged states
-          return true;
-
-        } else {
-          // unexpected case, but possible (if merge does not compute the join, but just widens e2)
-          logger.log(Level.FINEST, "Element was merged but not covered:", pElement);
-        }
-
-      } else {
-        // unexpected case, not sure if it this possible
-        logger.log(
-            Level.FINEST,
-            "Element was merged into an element that's not in the reached set, merged-with element is",
-            mergedWith);
-      }
+    if (checkCoveredByMergedWith(pElement, pReached, pPrecision)) {
+      return true;
     }
 
     // Never try to cover target states except when explicitly stated
@@ -107,40 +58,47 @@ public class ARGStopJoin implements StopOperator, ForcedCoveringStopOperator {
 
     // Now do the usual coverage checks
 
-    // TODO: should switch to normal stop-sep process instead of returning false
-    // make sure that the StopOp implements ForcedCoveringStopOperator#getCoveringStates
-    if (!(wrappedStop instanceof ForcedCoveringStopOperator)) {
-      return false;
-    }
-    CoveringStateSetProvider stopOp = (CoveringStateSetProvider) wrappedStop;
-
+    // get the list of may-cover ARG states
     Collection<ARGState> mayCoverCandidates = getMayCoverCandidates(argElement, pReached);
     if (mayCoverCandidates.isEmpty()) {
       return false;
     }
 
+    // get the list of may-cover wrapped states
     List<AbstractState> mayCoverWrappedStates = new ArrayList<>(mayCoverCandidates.size());
     for (ARGState candidate : mayCoverCandidates) {
       mayCoverWrappedStates.add(candidate.getWrappedState());
     }
 
+    if (!(wrappedStop instanceof CoveringStateSetProvider)) {
+      if (wrappedStop.stop(argElement.getWrappedState(), mayCoverWrappedStates, pPrecision)) {
+        argElement.setCovered(ImmutableSet.copyOf(mayCoverCandidates));
+        return !keepCoveredStatesInReached;
+      }
+      return false;
+    }
+
+    CoveringStateSetProvider stopOp = (CoveringStateSetProvider) wrappedStop;
     Collection<AbstractState> coveringAbsStates =
         stopOp.getCoveringStates(argElement.getWrappedState(), mayCoverWrappedStates, pPrecision);
     if (coveringAbsStates.isEmpty()) {
       return false;
     }
 
+    // mapping of wrapped state -> ARG state
     Map<AbstractState, ARGState> stateMap = new LinkedHashMap<>(mayCoverWrappedStates.size());
     for (ARGState argState : mayCoverCandidates) {
       AbstractState absState = argState.getWrappedState();
       stateMap.put(absState, argState);
     }
 
+    // map the covering wrapped states back to the corresponding ARG states and collect them
     Set<ARGState> coveringARGStates = new LinkedHashSet<>(coveringAbsStates.size());
     for (AbstractState absState : coveringAbsStates) {
       coveringARGStates.add(stateMap.get(absState));
     }
 
+    // store the coverage relation
     argElement.setCovered(ImmutableSet.copyOf(coveringARGStates));
     return !keepCoveredStatesInReached;
   }
@@ -171,29 +129,5 @@ public class ARGStopJoin implements StopOperator, ForcedCoveringStopOperator {
       candidates.add(argState);
     }
     return candidates;
-  }
-
-  @Override
-  public boolean isForcedCoveringPossible(
-      AbstractState pElement, AbstractState pReachedState, Precision pPrecision)
-      throws CPAException, InterruptedException {
-    if (!(wrappedStop instanceof ForcedCoveringStopOperator)) {
-      return false;
-    }
-
-    ARGState element = (ARGState) pElement;
-    ARGState reachedState = (ARGState) pReachedState;
-
-    if (reachedState.isCovered() || !reachedState.mayCover()) {
-      return false;
-    }
-
-    if (element.isOlderThan(reachedState)) {
-      return false;
-    }
-
-    return ((ForcedCoveringStopOperator) wrappedStop)
-        .isForcedCoveringPossible(
-            element.getWrappedState(), reachedState.getWrappedState(), pPrecision);
   }
 }
--- src/org/sosy_lab/cpachecker/cpa/arg/ARGStopSep.java
+++ src/org/sosy_lab/cpachecker/cpa/arg/ARGStopSep.java
@@ -12,21 +12,13 @@ import com.google.common.collect.Iterables;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Objects;
-import java.util.logging.Level;
 import org.sosy_lab.common.log.LogManager;
 import org.sosy_lab.cpachecker.core.interfaces.AbstractState;
-import org.sosy_lab.cpachecker.core.interfaces.ForcedCoveringStopOperator;
 import org.sosy_lab.cpachecker.core.interfaces.Precision;
 import org.sosy_lab.cpachecker.core.interfaces.StopOperator;
 import org.sosy_lab.cpachecker.exceptions.CPAException;
 
-public class ARGStopSep implements ForcedCoveringStopOperator {
-
-  private final boolean keepCoveredStatesInReached;
-  private final boolean inCPAEnabledAnalysis;
-  private final boolean coverTargetStates;
-  private final StopOperator wrappedStop;
-  private final LogManager logger;
+class ARGStopSep extends ARGStop {
 
   public ARGStopSep(
       StopOperator pWrappedStop,
@@ -34,11 +26,12 @@ public class ARGStopSep implements ForcedCoveringStopOperator {
       boolean pInCPAEnabledAnalysis,
       boolean pKeepCoveredStatesInReached,
       boolean pCoverTargetStates) {
-    wrappedStop = pWrappedStop;
-    logger = pLogger;
-    keepCoveredStatesInReached = pKeepCoveredStatesInReached;
-    inCPAEnabledAnalysis = pInCPAEnabledAnalysis;
-    coverTargetStates = pCoverTargetStates;
+    super(
+        pWrappedStop,
+        pLogger,
+        pInCPAEnabledAnalysis,
+        pKeepCoveredStatesInReached,
+        pCoverTargetStates);
   }
 
   @Override
@@ -50,46 +43,8 @@ public class ARGStopSep implements ForcedCoveringStopOperator {
     assert !argElement.isCovered()
         : "Passing element to stop which is already covered: " + argElement;
 
-    // First check if we can take a shortcut:
-    // If the new state was merged into an existing element,
-    // it is usually also covered by this existing element, so check this explicitly upfront.
-    // We do this because we want to remove the new state from the ARG completely
-    // in this case and not mark it as covered.
-
-    if (argElement.getMergedWith() != null) {
-      ARGState mergedWith = argElement.getMergedWith();
-
-      if (pReached.contains(mergedWith)) {
-        // we do this single check first as it should return true in most of the cases
-
-        if (wrappedStop.stop(
-            argElement.getWrappedState(),
-            Collections.singleton(mergedWith.getWrappedState()),
-            pPrecision)) {
-          // merged and covered
-          if (inCPAEnabledAnalysis) {
-            argElement.setCovered(mergedWith);
-          } else {
-            argElement.removeFromARG();
-          }
-          logger.log(Level.FINEST, "Element is covered by the element it was merged into");
-
-          // in this case, return true even if we should keep covered states
-          // because we should anyway not keep merged states
-          return true;
-
-        } else {
-          // unexpected case, but possible (if merge does not compute the join, but just widens e2)
-          logger.log(Level.FINEST, "Element was merged but not covered:", pElement);
-        }
-
-      } else {
-        // unexpected case, not sure if it this possible
-        logger.log(
-            Level.FINEST,
-            "Element was merged into an element that's not in the reached set, merged-with element is",
-            mergedWith);
-      }
+    if (checkCoveredByMergedWith(pElement, pReached, pPrecision)) {
+      return true;
     }
 
     // Never try to cover target states except when explicitly stated
@@ -150,29 +105,5 @@ public class ARGStopSep implements ForcedCoveringStopOperator {
       pElement.setCovered(pReachedState);
     }
     return stop;
-  }
-
-  @Override
-  public boolean isForcedCoveringPossible(
-      AbstractState pElement, AbstractState pReachedState, Precision pPrecision)
-      throws CPAException, InterruptedException {
-    if (!(wrappedStop instanceof ForcedCoveringStopOperator)) {
-      return false;
-    }
-
-    ARGState element = (ARGState) pElement;
-    ARGState reachedState = (ARGState) pReachedState;
-
-    if (reachedState.isCovered() || !reachedState.mayCover()) {
-      return false;
-    }
-
-    if (element.isOlderThan(reachedState)) {
-      return false;
-    }
-
-    return ((ForcedCoveringStopOperator) wrappedStop)
-        .isForcedCoveringPossible(
-            element.getWrappedState(), reachedState.getWrappedState(), pPrecision);
   }
 }
