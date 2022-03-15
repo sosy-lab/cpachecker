d4bb17d68dde3594d8d040658003678fd8fa18fd
mpletl <mpletl@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2022-03-15 12:44:07 +0000

    Add new example with nondeterminism, new properties-file
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/predicate-symex@39839 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

---
+++ programs/example3.c
@@ -0,0 +1,26 @@
+// This file is part of CPAchecker,
+// a tool for configurable software verification:
+// https://cpachecker.sosy-lab.org
+//
+// SPDX-FileCopyrightText: 2007-2020 Dirk Beyer <https://www.sosy-lab.org>
+//
+// SPDX-License-Identifier: Apache-2.0
+extern __nondet_int();
+
+int main() {
+  int x = __nondet_int();
+//  int x = 5;
+  if (x > 0) {
+      x++;
+  } else {
+      x--;
+  }
+  if (x==0) {
+    goto ERROR;
+  }
+  return 0;
+
+  ERROR:
+  return (-1);
+}
+
