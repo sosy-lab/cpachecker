036944584b2d8b29754b4cf4c96f6bf547a029a6
vport <vport@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2022-03-17 03:35:30 +0000

    Prepare 'CfaMutableNetwork' for merging with 'MutableCfaNetwork'
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/cfa-transformer@39856 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/cfa/CfaMutableNetwork.java
+++ src/org/sosy_lab/cpachecker/cfa/CfaMutableNetwork.java
@@ -8,67 +8,61 @@
 
 package org.sosy_lab.cpachecker.cfa;
 
-import static com.google.common.base.Preconditions.checkArgument;
 
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.TreeMultimap;
 import com.google.common.graph.EndpointPair;
-import com.google.common.graph.MutableNetwork;
-import com.google.common.graph.NetworkBuilder;
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.NavigableMap;
+import java.util.TreeMap;
 import org.sosy_lab.cpachecker.cfa.model.CFAEdge;
 import org.sosy_lab.cpachecker.cfa.model.CFANode;
 import org.sosy_lab.cpachecker.cfa.model.FunctionEntryNode;
-import org.sosy_lab.cpachecker.cfa.model.FunctionExitNode;
-import org.sosy_lab.cpachecker.util.CFAUtils;
 import org.sosy_lab.cpachecker.util.graph.ForwardingMutableNetwork;
 
+// TODO: merge CfaMutableNetwork and MutableCfaNetwork
 public class CfaMutableNetwork extends ForwardingMutableNetwork<CFANode, CFAEdge> {
 
-  private CfaMutableNetwork(MutableNetwork<CFANode, CFAEdge> pDelegate) {
+  private final CfaNetwork delegate;
+
+  private CfaMutableNetwork(MutableCfaNetwork pDelegate) {
     super(pDelegate);
+
+    delegate = pDelegate;
   }
 
-  /**
-   * Returns a new {@code CfaMutableNetwork} instance representing the specified CFA.
-   *
-   * <p>The returned {@code CfaMutableNetwork} contains all nodes, regular edges, and summary edges
-   * that occur in the specified CFA.
-   *
-   * <p>Modifying the returned mutable network does not change the original CFA, so calling methods
-   * on the returned {@code CfaMutableNetwork} instance is safe. However, mutating existing {@code
-   * CFANode} and {@code CFAEdge} objects may change (and break) the original CFA.
-   *
-   * @param pCfa the CFA to create the {@code CfaMutableNetwork} for
-   * @return the {@code CfaMutableNetwork} for the specified CFA
-   * @throws IllegalArgumentException if the specified CFA contains parallel edges (i.e., edges
-   *     connected to the same nodes in the same order)
-   * @throws NullPointerException if {@code pCfa == null}
-   */
+  /** Returns a new {@code CfaMutableNetwork} instance representing the specified CFA. */
   public static CfaMutableNetwork of(CFA pCfa) {
 
-    MutableNetwork<CFANode, CFAEdge> mutableNetwork =
-        NetworkBuilder.directed().allowsSelfLoops(true).build();
+    NavigableMap<String, FunctionEntryNode> functionEntryNodes = new TreeMap<>();
+    TreeMultimap<String, CFANode> allNodes = TreeMultimap.create();
 
-    for (CFANode cfaNode : pCfa.getAllNodes()) {
-      mutableNetwork.addNode(cfaNode);
-      if (cfaNode instanceof FunctionEntryNode) {
-        // unreachable FunctionExitNodes are not in the set returned by getAllNodes
-        FunctionExitNode functionExitNode = ((FunctionEntryNode) cfaNode).getExitNode();
-        mutableNetwork.addNode(functionExitNode);
+    for (CFANode node : pCfa.getAllNodes()) {
+
+      String functionName = node.getFunction().getQualifiedName();
+      allNodes.put(functionName, node);
+
+      if (node instanceof FunctionEntryNode) {
+        functionEntryNodes.put(functionName, (FunctionEntryNode) node);
       }
     }
 
-    for (CFANode predecessor : pCfa.getAllNodes()) {
-      for (CFAEdge cfaEdge : CFAUtils.allLeavingEdges(predecessor)) {
-        CFANode successor = cfaEdge.getSuccessor();
-        boolean edgeAdded = mutableNetwork.addEdge(predecessor, successor, cfaEdge);
-        checkArgument(edgeAdded, "CFA must not contain parallel edges");
-      }
-    }
+    MutableCFA mutableCfa =
+        new MutableCFA(
+            pCfa.getMachineModel(),
+            functionEntryNodes,
+            allNodes,
+            pCfa.getMainFunction(),
+            pCfa.getFileNames(),
+            pCfa.getLanguage());
 
-    return new CfaMutableNetwork(mutableNetwork);
+    return new CfaMutableNetwork(MutableCfaNetwork.of(mutableCfa));
+  }
+
+  public CfaNetwork getCfaNetwork() {
+    return delegate;
   }
 
   /**
@@ -136,14 +130,6 @@ public class CfaMutableNetwork extends ForwardingMutableNetwork<CFANode, CFAEdge
   /**
    * Replaces a CFA node with a different CFA node in this mutable network.
    *
-   * <p>Only the mutable network is changed by this method. The actual CFA nodes and edges are not
-   * modified. Connections between nodes and edges represented by the CFA nodes and edges themselves
-   * (i.e., defined by {@code CFAEdge#getSuccessor()}, {@code CFANode#getLeavingEdge(int)}, etc.)
-   * and connections represented by the mutable network are going to differ after invoking this
-   * method.
-   *
-   * <p>The mutable network is changed in the following way:
-   *
    * <pre>{@code
    * Before:
    * --- a ---> [pNode] --- b ---->
@@ -174,14 +160,6 @@ public class CfaMutableNetwork extends ForwardingMutableNetwork<CFANode, CFAEdge
 
   /**
    * Replaces a CFA edge with a different CFA edge in this mutable network.
-   *
-   * <p>Only the mutable network is changed by this method. The actual CFA nodes and edges are not
-   * modified. Connections between nodes and edges represented by the CFA nodes and edges themselves
-   * (i.e., defined by {@code CFAEdge#getSuccessor()}, {@code CFANode#getLeavingEdge(int)}, etc.)
-   * and connections represented by the mutable network are going to differ after invoking this
-   * method.
-   *
-   * <p>The mutable network is changed in the following way:
    *
    * <pre>{@code
    * Before:
