8ba2b07de90a82d3ad5b1a47954c99db8560284e
jhaltermann <jhaltermann@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2022-03-22 11:37:37 +0000

    Added testcases
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/uca-generation@39948 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

---
+++ test/programs/uca/jain_1-1.c
@@ -0,0 +1,36 @@
+// This file is part of the SV-Benchmarks collection of verification tasks:
+// https://gitlab.com/sosy-lab/benchmarking/sv-benchmarks
+//
+// SPDX-FileCopyrightText: 2012-2021 The SV-Benchmarks Community
+// SPDX-FileCopyrightText: 2012 FBK-ES <https://es.fbk.eu/>
+//
+// SPDX-License-Identifier: Apache-2.0
+
+extern void abort(void);
+extern void __assert_fail(const char *, const char *, unsigned int, const char *) __attribute__ ((__nothrow__ , __leaf__)) __attribute__ ((__noreturn__));
+void reach_error() { __assert_fail("0", "jain_1-1.c", 3, "reach_error"); }
+
+extern unsigned int __VERIFIER_nondet_uint(void);
+void __VERIFIER_assert(int cond) {
+  if (!(cond)) {
+    ERROR: {reach_error();abort();}
+  }
+  return;
+}
+int main()
+{
+  unsigned int y;
+
+  y = 1U;
+
+  while(1)
+    {
+      y = y +2U*__VERIFIER_nondet_uint();
+
+
+      __VERIFIER_assert (y!=0U);
+	
+    }
+    return 0;
+}
+
