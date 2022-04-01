753b2cb14a0146bfb8854359805c84358f358472
pchien <pchien@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2022-04-01 22:11:16 +0000

    Check fixed point with invariant only when external generation is running - could avoid redundant solver calls
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/imc-with-invariants@40082 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/core/algorithm/bmc/ISMCAlgorithm.java
+++ src/org/sosy_lab/cpachecker/core/algorithm/bmc/ISMCAlgorithm.java
@@ -352,7 +352,8 @@ public class ISMCAlgorithm extends AbstractBMCAlgorithm implements Algorithm {
           return true;
         }
         // Step 2: ISMC check strengthened by external invariant
-        if (solver.implies(bfmgr.and(imageAtI, loopInv), currentImage)) {
+        if (invariantGenerationRunning
+            && solver.implies(bfmgr.and(imageAtI, loopInv), currentImage)) {
           // Step 3: check if external invariant is inductive
           logger.log(Level.FINE, "Checking inductiveness of invariant ");
           BooleanFormula invariantTransition =
