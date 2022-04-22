f51c7245e45f0d773b05ba0d96e11146ea66e627
vport <vport@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2022-04-22 13:09:39 +0000

    Add documentation to `CfaNodeTransformer`
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/cfa-transformer@40218 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/cfa/transformer/CfaNodeTransformer.java
+++ src/org/sosy_lab/cpachecker/cfa/transformer/CfaNodeTransformer.java
@@ -10,9 +10,41 @@ package org.sosy_lab.cpachecker.cfa.transformer;
 
 import org.sosy_lab.cpachecker.cfa.graph.CfaNetwork;
 import org.sosy_lab.cpachecker.cfa.model.CFANode;
+import org.sosy_lab.cpachecker.cfa.model.FunctionEntryNode;
+import org.sosy_lab.cpachecker.cfa.model.FunctionExitNode;
 
+/**
+ * A {@code CfaNodeTransformer} instance returns a transformed CFA node for a specified CFA node.
+ *
+ * <p>To implement a {@code CfaNodeTransformer}, an implementation for {@link
+ * CfaNodeTransformer#transform(CFANode, CfaNetwork, CfaNodeSubstitution)} must be provided. The
+ * implementation must guarantee that every time the method is called, a new transformed node
+ * instance is created.
+ */
 @FunctionalInterface
 public interface CfaNodeTransformer {
 
-  CFANode transform(CFANode pCfaNode, CfaNetwork pCfaNetwork, CfaNodeSubstitution pSubstitution);
+  /**
+   * Returns a transformed CFA node for the specified CFA node.
+   *
+   * <p>Every time this method is called, a new transformed node instance is created.
+   *
+   * @param pNode the CFA node to get a transformed node for
+   * @param pCfa The CFA the specified node is a part of. The CFA is used to determine other nodes
+   *     that are required for node transformation (e.g., if the {@link FunctionExitNode} of a
+   *     {@link FunctionEntryNode} is required, it's determined using the specified CFA). If the
+   *     construction of a transformed node does not require other nodes, it's guaranteed that the
+   *     specified CFA is not used, so a dummy CFA can be used.
+   * @param pNodeSubstitution If the construction of a transformed node requires other transformed
+   *     nodes, the specified node substitution is used (e.g., the construction of a transformed
+   *     {@code FunctionEntryNode} requires a transformed {@code FunctionExitNode}, so the {@code
+   *     FunctionExitNode} of the {@code FunctionEntryNode} is determined using the specified CFA
+   *     and the transformed {@code FunctionExitNode} is retrieved using the specified
+   *     substitution). If the construction of a transformed node does not require other nodes, it's
+   *     guaranteed that the specified substitution is not used, so a dummy substitution can be
+   *     used.
+   * @return a transformed CFA node for the specified CFA node
+   * @throws NullPointerException if any parameter is {@code null}
+   */
+  CFANode transform(CFANode pNode, CfaNetwork pCfa, CfaNodeSubstitution pNodeSubstitution);
 }
