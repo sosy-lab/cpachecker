d0b77cb6bae12d21ab92a87ff4a235ad65d4cbda
vport <vport@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2022-04-22 13:09:08 +0000

    Add documentation to `CfaNodeSubstitution`
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/cfa-transformer@40214 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/cfa/transformer/CfaNodeSubstitution.java
+++ src/org/sosy_lab/cpachecker/cfa/transformer/CfaNodeSubstitution.java
@@ -9,9 +9,34 @@
 package org.sosy_lab.cpachecker.cfa.transformer;
 
 import org.sosy_lab.cpachecker.cfa.model.CFANode;
+import org.sosy_lab.cpachecker.cfa.model.FunctionEntryNode;
+import org.sosy_lab.cpachecker.cfa.model.FunctionExitNode;
 
+/**
+ * A {@code CfaNodeSubstitution} instance returns the substitute for a specified CFA node.
+ *
+ * <p>During CFA node and edge transformation, new transformed nodes and edges are created that
+ * require other transformed nodes and edges for their construction (e.g., to construct a new {@link
+ * FunctionEntryNode}, a {@link FunctionExitNode} is required). Instances of {@code
+ * CfaNodeSubstitution} are used to provide these transformed nodes (e.g., they provide the
+ * transformed {@code FunctionExitNode} for a given {@code FunctionExitNode}).
+ *
+ * <p>To implement a {@code CfaNodeSubstitution}, an implementation for {@link
+ * CfaNodeSubstitution#get(CFANode)} must be provided. The implementation must guarantee that a
+ * {@code CfaNodeSubstitution} instance always returns the same substitute for the same given node.
+ */
 @FunctionalInterface
 public interface CfaNodeSubstitution {
 
-  CFANode get(CFANode pOriginalNode);
+  /**
+   * Returns the substitute for the specified CFA node.
+   *
+   * <p>It's guaranteed that a {@code CfaNodeSubstitution} instance always returns the same
+   * substitute for the same given node.
+   *
+   * @param pNode the CFA node to get the substitute for
+   * @return the substitute for the specified CFA node
+   * @throws NullPointerException if {@code pNode == null}
+   */
+  CFANode get(CFANode pNode);
 }
