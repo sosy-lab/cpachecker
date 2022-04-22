bb6bce904f151795cfa1d89b0b404ff298053674
vport <vport@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2022-04-22 13:09:22 +0000

    Add documentation to `CfaEdgeSubstitution`
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/cfa-transformer@40216 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/cfa/transformer/CfaEdgeSubstitution.java
+++ src/org/sosy_lab/cpachecker/cfa/transformer/CfaEdgeSubstitution.java
@@ -9,9 +9,34 @@
 package org.sosy_lab.cpachecker.cfa.transformer;
 
 import org.sosy_lab.cpachecker.cfa.model.CFAEdge;
+import org.sosy_lab.cpachecker.cfa.model.FunctionCallEdge;
+import org.sosy_lab.cpachecker.cfa.model.FunctionSummaryEdge;
 
+/**
+ * A {@code CfaEdgeSubstitution} instance returns the substitute for a specified CFA edge.
+ *
+ * <p>During CFA edge transformation, new transformed edges are created that require other
+ * transformed edges for their construction (e.g., to construct a new {@link FunctionCallEdge}, a
+ * {@link FunctionSummaryEdge} is required). Instances of {@code CfaEdgeSubstitution} are used to
+ * provide these transformed edges (e.g., they provide the transformed {@code FunctionSummaryEdge}
+ * for a given {@code FunctionSummaryEdge}).
+ *
+ * <p>To implement a {@code CfaEdgeSubstitution}, an implementation for {@link
+ * CfaEdgeSubstitution#get(CFAEdge)} must be provided. The implementation must guarantee that a
+ * {@code CfaEdgeSubstitution} instance always returns the same substitute for the same given edge.
+ */
 @FunctionalInterface
 public interface CfaEdgeSubstitution {
 
-  CFAEdge get(CFAEdge pOriginalEdge);
+  /**
+   * Returns the substitute for the specified CFA edge.
+   *
+   * <p>It's guaranteed that a {@code CfaEdgeSubstitution} instance always returns the same
+   * substitute for the same given edge.
+   *
+   * @param pEdge the CFA edge to get the substitute for
+   * @return the substitute for the specified CFA edge
+   * @throws NullPointerException if {@code pEdge == null}
+   */
+  CFAEdge get(CFAEdge pEdge);
 }
