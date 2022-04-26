179eb87c5f6703be2ac9ac495ed744992ae1799c
vport <vport@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2022-04-26 04:41:43 +0000

    Add documentation to `CfaEdgeTransformer`
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/cfa-transformer@40243 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/cfa/transformer/CfaEdgeTransformer.java
+++ src/org/sosy_lab/cpachecker/cfa/transformer/CfaEdgeTransformer.java
@@ -11,13 +11,51 @@ package org.sosy_lab.cpachecker.cfa.transformer;
 import org.sosy_lab.cpachecker.cfa.CfaConnectedness;
 import org.sosy_lab.cpachecker.cfa.graph.CfaNetwork;
 import org.sosy_lab.cpachecker.cfa.model.CFAEdge;
+import org.sosy_lab.cpachecker.cfa.model.FunctionCallEdge;
+import org.sosy_lab.cpachecker.cfa.model.FunctionSummaryEdge;
 
+/**
+ * A {@code CfaEdgeTransformer} instance returns a transformed CFA edge for a specified CFA edge.
+ *
+ * <p>To implement a {@code CfaEdgeTransformer}, an implementation for {@link
+ * CfaEdgeTransformer#transform(CFAEdge, CfaNetwork, CfaNodeSubstitution, CfaEdgeSubstitution,
+ * CfaConnectedness)} must be provided. The implementation must guarantee that every time the method
+ * is called, a new transformed edge instance is created.
+ */
 @FunctionalInterface
 public interface CfaEdgeTransformer {
 
+  /**
+   * Returns a transformed CFA edge for the specified CFA edge.
+   *
+   * <p>Every time this method is called, a new transformed edge instance is created.
+   *
+   * @param pEdge the CFA edge to get a transformed edge for
+   * @param pCfa The CFA the specified edge is a part of. The CFA is used to determine other nodes
+   *     and edges that are required for successful edge transformation (e.g., if the {@link
+   *     FunctionSummaryEdge} of a {@link FunctionCallEdge} is required, it's determined using the
+   *     specified CFA, but also an edge's predecessor and successor are determined using the
+   *     specified CFA).
+   * @param pNodeSubstitution The construction of a transformed edge requires transformed nodes, for
+   *     which the specified node substitution is used (e.g., the construction of a transformed edge
+   *     requires transformed predecessor and successor nodes, so the predecessor and successor of
+   *     the edge are determined using the specified CFA and the transformed predecessor and
+   *     sucessor nodes are retrieved using the specified node substitution).
+   * @param pEdgeSubstitution If the construction of a transformed edge requires other transformed
+   *     edges, the specified edge substitution is used (e.g., the construction of a transformed
+   *     {@code FunctionCallEdge} requires a transformed {@code FunctionSummaryEdge}, so the {@code
+   *     FunctionSummaryEdge} of the {@code FunctionCallEdge} is determined using the specified CFA
+   *     and the transformed {@code FunctionSummaryEdge} is retrieved using the specified edge
+   *     substitution). If the construction of a transformed edge does not require other edges, it's
+   *     guaranteed that the specified edge substitution is not used, so a dummy substitution can be
+   *     used.
+   * @param pConnectedness -
+   * @return a transformed CFA edge for the specified CFA edge
+   * @throws NullPointerException if any parameter is {@code null}
+   */
   CFAEdge transform(
-      CFAEdge pCfaEdge,
-      CfaNetwork pCfaNetwork,
+      CFAEdge pEdge,
+      CfaNetwork pCfa,
       CfaNodeSubstitution pNodeSubstitution,
       CfaEdgeSubstitution pEdgeSubstitution,
       CfaConnectedness pConnectedness);
