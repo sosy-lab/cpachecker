b7bbed4480799bacaa1278d019128f6ce7137c09
dbaier <dbaier@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2022-04-29 18:17:02 +0000

    New SMG: add empty transfer relation test class and some test ideas
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/smgv2@40294 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

---
+++ src/org/sosy_lab/cpachecker/cpa/smg2/SMGCPATransferRelationTest.java
@@ -0,0 +1,31 @@
+// This file is part of CPAchecker,
+// a tool for configurable software verification:
+// https://cpachecker.sosy-lab.org
+//
+// SPDX-FileCopyrightText: 2022 Dirk Beyer <https://www.sosy-lab.org>
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package org.sosy_lab.cpachecker.cpa.smg2;
+
+public class SMGCPATransferRelationTest {
+
+  /*
+   * Declaration tests:
+   *   declare variable without value and use afterwards
+   *   declare with simple value and use
+   *   declare stack array and use
+   *   declare stack array with value
+   *   declare stack struct
+   *   declare stack struct with value
+   *   declare String with array of chars
+   *   declare String with String ("")
+   *   declare String as char * and array of chars
+   *   declare String as char * with String
+   *
+   * Function usage:
+   *   malloc
+   *   free
+   *   ...
+   */
+}
