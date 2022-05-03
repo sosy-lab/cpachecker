1bffd805526f0a1dd3aa38c5f9b005baf665b605
spiessl <spiessl@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2022-05-03 06:59:41 +0000

    Add some more test programs
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/loopsummary@40351 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

---
+++ test/programs/loopsummary/naive3_no_summary_4.c
@@ -0,0 +1,29 @@
+// This file is part of CPAchecker,
+// a tool for configurable software verification:
+// https://cpachecker.sosy-lab.org
+//
+// SPDX-FileCopyrightText: 2007-2021 Dirk Beyer <https://www.sosy-lab.org>
+//
+// SPDX-License-Identifier: Apache-2.0
+
+void reach_error(){}
+void __VERIFIER_assert(int cond) {
+  if (!cond) {
+    reach_error();
+  }
+}
+
+int main() {
+  int x = 0;
+  int i = 0;
+  while (i< 25) {
+    if (i > 25) {
+        x+=2;
+    } else {
+        x+=0;
+    }
+    i += 1;
+  }
+  __VERIFIER_assert(i!=25);
+  return 0;
+}
