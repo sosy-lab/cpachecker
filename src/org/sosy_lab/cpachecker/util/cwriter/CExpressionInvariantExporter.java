c83133ca3ae584a1ab013f2c96ecf4d4456ee47e
mcjakobs <mcjakobs@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2022-05-12 14:08:54 +0000

    export invariants for lines now either in source code copy or in external file
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@40471 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/util/cwriter/CExpressionInvariantExporter.java
+++ src/org/sosy_lab/cpachecker/util/cwriter/CExpressionInvariantExporter.java
@@ -9,6 +9,7 @@
 package org.sosy_lab.cpachecker.util.cwriter;
 
 import com.google.common.base.Splitter;
+import com.google.common.collect.FluentIterable;
 import com.google.common.collect.HashMultimap;
 import com.google.common.collect.Maps;
 import com.google.common.collect.Multimap;
@@ -18,7 +19,9 @@ import java.io.Writer;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Comparator;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Optional;
@@ -45,6 +48,7 @@ import org.sosy_lab.cpachecker.cfa.model.FunctionSummaryEdge;
 import org.sosy_lab.cpachecker.core.interfaces.AbstractState;
 import org.sosy_lab.cpachecker.core.reachedset.UnmodifiableReachedSet;
 import org.sosy_lab.cpachecker.util.AbstractStates;
+import org.sosy_lab.cpachecker.util.Pair;
 import org.sosy_lab.cpachecker.util.predicates.smt.FormulaManagerView;
 import org.sosy_lab.cpachecker.util.predicates.smt.Solver;
 import org.sosy_lab.cpachecker.util.predicates.weakening.InductiveWeakeningManager;
@@ -62,20 +66,24 @@ public class CExpressionInvariantExporter {
           "Attempt to simplify the invariant before " + "exporting [may be very expensive].")
   private boolean simplify = false;
 
+  @Option(secure = true, description = "If enabled only export invariants for specified lines.")
+  private boolean onlyForSpecifiedLines = false;
+
   @Option(
       secure = true,
-      name = "external.forLines",
+      name = "forLines",
       description =
-          "Specify lines for which an invariant should be written to external file (specified with"
-              + " option cinvariants.external.file).Lines are specified as comma separated list of"
-              + " individual lines x and line ranges x-y. Use the empty list to export all"
-              + " available invariants.")
+          "Specify lines for which an invariant should be written. "
+              + "Lines are specified as comma separated list of"
+              + " individual lines x and line ranges x-y.")
   private String exportInvariantsForLines = "";
 
   @Option(
       secure = true,
       name = "external.file",
-      description = "File name for exporting external invariants.")
+      description =
+          "File name for exporting invariants. Only supported if invariant export for specified"
+              + " lines is enabled.")
   @FileOption(FileOption.Type.OUTPUT_FILE)
   private Path externalInvariantFile = null;
 
@@ -113,19 +121,27 @@ public class CExpressionInvariantExporter {
   public void exportInvariant(CFA pCfa, UnmodifiableReachedSet pReachedSet)
       throws IOException, InterruptedException, SolverException {
 
-    for (Path program : pCfa.getFileNames()) {
-      // Grab only the last component of the program filename.
-      Path trimmedFilename = program.getFileName();
-      if (trimmedFilename != null) {
-        try (Writer output =
-            IO.openOutputFile(
-                prefix.getPath(trimmedFilename.toString()), Charset.defaultCharset())) {
-          writeProgramWithInvariants(output, program, pReachedSet);
+    if (onlyForSpecifiedLines) {
+      if (exportInvariantsForLines != null && !pReachedSet.hasWaitingState()) {
+        if (externalInvariantFile == null) {
+          exportInvariantsForRequestedLinesToSourceFileCopies(pReachedSet, pCfa.getFileNames());
+        } else {
+          exportInvariantsForRequestedLinesToFile(pReachedSet, pCfa.getFileNames().size() == 1);
+        }
+      }
+    } else {
+      for (Path program : pCfa.getFileNames()) {
+        // Grab only the last component of the program filename.
+        Path trimmedFilename = program.getFileName();
+        if (trimmedFilename != null) {
+          try (Writer output =
+              IO.openOutputFile(
+                  prefix.getPath(trimmedFilename.toString()), Charset.defaultCharset())) {
+            writeProgramWithInvariants(output, program, pReachedSet);
+          }
         }
       }
     }
-
-    exportInvariantsForRequestedLinesToFile(pReachedSet, pCfa.getFileNames().isEmpty());
   }
 
   private void writeProgramWithInvariants(
@@ -190,54 +206,92 @@ public class CExpressionInvariantExporter {
     return inductiveWeakeningManager.removeRedundancies(pInvariant);
   }
 
-  private void exportInvariantsForRequestedLinesToFile(
-      final UnmodifiableReachedSet pReachedSet, final boolean withoutPrefix)
+  private void exportInvariantsForRequestedLinesToSourceFileCopies(
+      final UnmodifiableReachedSet pReachedSet, final List<Path> sourceFiles)
       throws IOException, InterruptedException, SolverException {
-    if (externalInvariantFile != null && exportInvariantsForLines != null) {
-      try (Writer output = IO.openOutputFile(externalInvariantFile, Charset.defaultCharset())) {
-        Set<Integer> requestedLines = new HashSet<>();
-
-        // read line numbers from input
-        int posRangeSeparator, max;
-        for (String line :
-            Splitter.on(',').trimResults().omitEmptyStrings().split(exportInvariantsForLines)) {
-          try {
-            posRangeSeparator = line.indexOf('-');
-            if (0 < posRangeSeparator) {
-              max = Integer.parseInt(line.substring(posRangeSeparator + 1));
-              for (int i = Integer.parseInt(line); i <= max; i++) {
-                requestedLines.add(i);
-              }
-            } else {
-              requestedLines.add(Integer.parseInt(line));
-            }
-          } catch (NumberFormatException e) {
-            logger.log(Level.WARNING, "could not parse line number " + line + ", skipping!");
-          }
+    if (sourceFiles != null) {
+      final boolean withoutPrefix = sourceFiles.size() == 1;
+      Set<Integer> requestedLines = parseRequestedLines();
+      Map<Pair<String, Integer>, BooleanFormula> invariantsPerLine =
+          extractInvariants(pReachedSet, requestedLines, withoutPrefix);
+      String fileName = "", line;
+      int counter;
+      Pair<String, Integer> invariantKey;
+      for (Path sourceFile : sourceFiles) {
+        if (!withoutPrefix) {
+          fileName = sourceFile.getFileName().toString();
         }
-
-        // export invariants
-        for (Entry<String, BooleanFormula> mapEntry :
-            extractInvariants(pReachedSet, requestedLines, withoutPrefix).entrySet()) {
-          output.append('[');
-          output.append(mapEntry.getKey());
-          output.append(',');
-          output.append(convertInvariantToSingleLineString(mapEntry.getValue()));
-          output.append("]\n");
+        try (Writer output = IO.openOutputFile(sourceFile.getFileName(), Charset.defaultCharset());
+            BufferedReader reader = Files.newBufferedReader(sourceFile)) {
+          counter = 1;
+          while ((line = reader.readLine()) != null) {
+            invariantKey = Pair.of(fileName, counter++);
+            if (invariantsPerLine.containsKey(invariantKey)) {
+              output
+                  .append("__VERIFIER_assume(")
+                  .append(convertInvariantToSingleLineString(invariantsPerLine.get(invariantKey)))
+                  .append(");\n");
+            }
+            output.append(line).append('\n');
+          }
         }
       }
     }
   }
 
-  private Map<String, BooleanFormula> extractInvariants(
+  private void exportInvariantsForRequestedLinesToFile(
+      final UnmodifiableReachedSet pReachedSet, final boolean withoutPrefix)
+      throws IOException, InterruptedException, SolverException {
+    if (externalInvariantFile != null
+        && exportInvariantsForLines != null
+        && !pReachedSet.hasWaitingState()) {
+      Set<Integer> requestedLines = parseRequestedLines();
+      Map<Pair<String, Integer>, BooleanFormula> invariantsPerLine =
+          extractInvariants(pReachedSet, requestedLines, withoutPrefix);
+
+      try (Writer output = IO.openOutputFile(externalInvariantFile, Charset.defaultCharset())) {
+        // export invariants in order
+        for (Entry<Pair<String, Integer>, BooleanFormula> mapEntry :
+            FluentIterable.from(invariantsPerLine.entrySet())
+                .toSortedSet(
+                    (entry1, entry2) -> compareInvariantKeys(entry1.getKey(), entry2.getKey()))) {
+          writetExternalEntry(output, mapEntry);
+        }
+      }
+    }
+  }
+
+  private Set<Integer> parseRequestedLines() {
+    Set<Integer> requestedLines = new HashSet<>();
+
+    // read line numbers from input
+    int posRangeSeparator, max;
+    for (String line :
+        Splitter.on(',').trimResults().omitEmptyStrings().split(exportInvariantsForLines)) {
+      try {
+        posRangeSeparator = line.indexOf('-');
+        if (0 < posRangeSeparator) {
+          max = Integer.parseInt(line.substring(posRangeSeparator + 1));
+          for (int i = Integer.parseInt(line); i <= max; i++) {
+            requestedLines.add(i);
+          }
+        } else {
+          requestedLines.add(Integer.parseInt(line));
+        }
+      } catch (NumberFormatException e) {
+        logger.log(Level.WARNING, "could not parse line number " + line + ", skipping!");
+      }
+    }
+    return requestedLines;
+  }
+
+  private Map<Pair<String, Integer>, BooleanFormula> extractInvariants(
       final UnmodifiableReachedSet pReachedSet,
       final Set<Integer> pRequestedLines,
       final boolean withoutPrefix) {
-    final String BEFORE_TOKEN = "-";
-    // final String AFTER_TOKEN = "+";
 
     // One formula per reported state.
-    Multimap<String, BooleanFormula> byState = HashMultimap.create();
+    Multimap<Pair<String, Integer>, BooleanFormula> byState = HashMultimap.create();
     int lineNumber;
     String sourceFileName;
     CFANode loc;
@@ -260,19 +314,39 @@ public class CExpressionInvariantExporter {
 
         lineNumber = edge.getFileLocation().getStartingLineInOrigin();
         sourceFileName = edge.getFileLocation().getNiceFileName() + ":";
-        if (pRequestedLines.isEmpty() || pRequestedLines.contains(lineNumber)) {
+        if (pRequestedLines.contains(lineNumber)) {
           BooleanFormula reported = AbstractStates.extractReportedFormulas(fmgr, state);
           if (!bfmgr.isTrue(reported)) {
-            byState.put(
-                withoutPrefix
-                    ? lineNumber + BEFORE_TOKEN
-                    : sourceFileName + lineNumber + BEFORE_TOKEN,
-                reported);
+            byState.put(Pair.of(withoutPrefix ? "" : sourceFileName, lineNumber), reported);
           }
         }
       }
     }
     return Maps.transformValues(byState.asMap(), invariants -> bfmgr.or(invariants));
+  }
+
+  private int compareInvariantKeys(
+      final Pair<String, Integer> key1, final Pair<String, Integer> key2) {
+    if (key1.getFirst().equals(key2.getFirst())) {
+      return Comparator.<Pair<String, Integer>>comparingInt(key -> key.getSecond())
+          .compare(key1, key2);
+    } else {
+      return key1.getFirst().compareTo(key2.getFirst());
+    }
+  }
+
+  private void writetExternalEntry(
+      final Writer output, final Entry<Pair<String, Integer>, BooleanFormula> pEntry)
+      throws IOException, InterruptedException, SolverException {
+    final String BEFORE_TOKEN = "-";
+    // final String AFTER_TOKEN = "+";
+    output.append('[');
+    output.append(pEntry.getKey().getFirst());
+    output.append(pEntry.getKey().getSecond().toString());
+    output.append(BEFORE_TOKEN);
+    output.append(',');
+    output.append(convertInvariantToSingleLineString(pEntry.getValue()));
+    output.append("]\n");
   }
 
   private String convertInvariantToSingleLineString(BooleanFormula pInvariant)
