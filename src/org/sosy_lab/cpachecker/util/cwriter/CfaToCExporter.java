6443e962a2e7cbba74bdba93bc1b8de874df1a26
kcimbalnik <kcimbalnik@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2022-05-21 21:09:07 +0000

    See #928: Introduce new block item classes instead of using Strings
    Also, use a more functional programming style and use immutable
    data structures.
    
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/928-CFA-to-C-exporter-prototype@40572 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/util/cwriter/CfaToCExporter.java
+++ src/org/sosy_lab/cpachecker/util/cwriter/CfaToCExporter.java
@@ -10,15 +10,13 @@ package org.sosy_lab.cpachecker.util.cwriter;
 
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.ImmutableSortedMap;
 import com.google.common.collect.Iterables;
 import com.google.common.collect.MoreCollectors;
-import com.google.common.collect.MultimapBuilder;
-import com.google.common.collect.SetMultimap;
 import java.util.ArrayDeque;
 import java.util.List;
-import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Queue;
-import java.util.Set;
 import java.util.TreeMap;
 import org.sosy_lab.common.configuration.InvalidConfigurationException;
 import org.sosy_lab.cpachecker.cfa.CFA;
@@ -37,9 +35,6 @@ public class CfaToCExporter {
   // Use original, unqualified names for variables
   private static final boolean NAMES_QUALIFIED = false;
 
-  private SetMultimap<FileLocation, CFAEdge> locationToEdgesMappingForGlobalDeclarations;
-  private Map<FileLocation, String> locationToFunctionStringMapping;
-
   /**
    * Exports the given {@link CFA} to a C program.
    *
@@ -53,24 +48,33 @@ public class CfaToCExporter {
           "CFA can only be exported to C for C input programs, at the moment");
     }
 
-    locationToEdgesMappingForGlobalDeclarations =
-        MultimapBuilder.treeKeys().hashSetValues().build();
-    locationToFunctionStringMapping = new TreeMap<>();
+    final ImmutableSortedMap.Builder<FileLocation, GlobalDeclaration> globalDeclarationBuilder =
+        ImmutableSortedMap.naturalOrder();
+    final ImmutableSortedMap.Builder<FileLocation, String> functionCodeBuilder =
+        ImmutableSortedMap.naturalOrder();
 
     for (final FunctionEntryNode functionEntryNode : pCfa.getAllFunctionHeads()) {
-      locationToFunctionStringMapping.put(
-          functionEntryNode.getFileLocation(), exportFunction(functionEntryNode));
+      functionCodeBuilder.put(
+          functionEntryNode.getFileLocation(),
+          exportFunction(functionEntryNode, globalDeclarationBuilder));
     }
+    final ImmutableSortedMap<FileLocation, String> functionCodeSortedByFileLocation =
+        functionCodeBuilder.build();
 
     final StringBuilder programBuilder = new StringBuilder();
-    programBuilder.append(exportGlobalDeclarations());
-    for (final String functionString : locationToFunctionStringMapping.values()) {
-      programBuilder.append(functionString);
+    programBuilder.append(
+        exportGlobalDeclarations(
+            globalDeclarationBuilder.build(), functionCodeSortedByFileLocation.keySet()));
+    for (final String functionCode : functionCodeSortedByFileLocation.values()) {
+      programBuilder.append(functionCode);
     }
     return programBuilder.toString();
   }
 
-  private String exportFunction(final FunctionEntryNode pFunctionEntryNode) {
+  private String exportFunction(
+      final FunctionEntryNode pFunctionEntryNode,
+      final ImmutableSortedMap.Builder<FileLocation, GlobalDeclaration> globalDeclarationBuilder) {
+
     final StringBuilder functionBuilder = new StringBuilder();
     functionBuilder.append(
         pFunctionEntryNode
@@ -78,23 +82,24 @@ public class CfaToCExporter {
             .toASTString(NAMES_QUALIFIED)
             .replace(";", " {\n"));
 
-    final SetMultimap<FileLocation, CFAEdge> locationToEdgesMappingWithinFunction =
-        collectCfaEdgesByFileLocation(pFunctionEntryNode);
-    final Map<FileLocation, String> locationToCCodeMappingWithinFunction =
-        processCfaEdgesByFileLocation(locationToEdgesMappingWithinFunction);
+    final ImmutableSortedMap<FileLocation, BlockItem> blockItemsWithinFunctionSortedByFileLocation =
+        collectCfaEdgesOfFunctionIntoBlockItems(pFunctionEntryNode, globalDeclarationBuilder);
 
-    for (final String code : locationToCCodeMappingWithinFunction.values()) {
-      functionBuilder.append(code).append("\n");
+    for (final Entry<FileLocation, BlockItem> entry :
+        blockItemsWithinFunctionSortedByFileLocation.entrySet()) {
+      if (entry.getKey().isRealLocation()) {
+        functionBuilder.append(entry.getValue().toCCode()).append("\n");
+      }
     }
     functionBuilder.append("}\n");
     return functionBuilder.toString();
   }
 
-  private SetMultimap<FileLocation, CFAEdge> collectCfaEdgesByFileLocation(
-      final FunctionEntryNode pFunctionEntryNode) {
-    final SetMultimap<FileLocation, CFAEdge> locationToEdgesMappingWithinFunction =
-        MultimapBuilder.treeKeys().hashSetValues().build();
+  private ImmutableSortedMap<FileLocation, BlockItem> collectCfaEdgesOfFunctionIntoBlockItems(
+      final FunctionEntryNode pFunctionEntryNode,
+      final ImmutableSortedMap.Builder<FileLocation, GlobalDeclaration> globalDeclarationBuilder) {
 
+    final TreeMap<FileLocation, BlockItem> blockItemsSortedByFileLocation = new TreeMap<>();
     final Queue<CFANode> waitList = new ArrayDeque<>();
     waitList.offer(pFunctionEntryNode);
 
@@ -102,79 +107,130 @@ public class CfaToCExporter {
       final CFANode currentNode = waitList.poll();
 
       for (final CFAEdge cfaEdge : CFAUtils.leavingEdges(currentNode)) {
+        final FileLocation loc = cfaEdge.getFileLocation();
 
         if (cfaEdge.getEdgeType() == CFAEdgeType.DeclarationEdge
             && ((CDeclarationEdge) cfaEdge).getDeclaration().isGlobal()) {
-          locationToEdgesMappingForGlobalDeclarations.put(cfaEdge.getFileLocation(), cfaEdge);
+
+          globalDeclarationBuilder.put(loc, new GlobalDeclaration(cfaEdge));
+
         } else {
-          locationToEdgesMappingWithinFunction.put(cfaEdge.getFileLocation(), cfaEdge);
+
+          if (blockItemsSortedByFileLocation.containsKey(loc)) {
+            final BlockItem blockItem = blockItemsSortedByFileLocation.get(loc);
+            assert blockItem instanceof SimpleBlockItem
+                : "Only statements and declarations can be split into multiple edges";
+            ((SimpleBlockItem) blockItem).addEdge(cfaEdge);
+            continue;
+          }
+
+          if (currentNode instanceof CFALabelNode) {
+            blockItemsSortedByFileLocation.put(loc, new LabeledStatement(cfaEdge));
+            continue;
+          }
+
+          blockItemsSortedByFileLocation.put(loc, new SimpleBlockItem(cfaEdge));
         }
+
         waitList.offer(cfaEdge.getSuccessor());
       }
     }
-    return locationToEdgesMappingWithinFunction;
+    return ImmutableSortedMap.copyOfSorted(blockItemsSortedByFileLocation);
   }
 
-  private Map<FileLocation, String> processCfaEdgesByFileLocation(
-      final SetMultimap<FileLocation, CFAEdge> pLocationToEdgesMapping) {
-    final Map<FileLocation, String> locationToCCodeMapping = new TreeMap<>();
-
-    for (final FileLocation loc : pLocationToEdgesMapping.keySet()) {
-      if (loc.isRealLocation()) {
-        final Set<CFAEdge> edgesWithSameLocation = pLocationToEdgesMapping.get(loc);
-
-        if (edgesWithSameLocation.size() == 1) {
-          final CFAEdge cfaEdge = Iterables.getOnlyElement(edgesWithSameLocation);
-
-          if (cfaEdge.getPredecessor() instanceof CFALabelNode) {
-            locationToCCodeMapping.put(loc, processLabelEdge(cfaEdge));
-            continue;
-          }
-        }
-        final String rawStatement =
-            Iterables.getOnlyElement(
-                edgesWithSameLocation.stream()
-                    .map(CFAEdge::getRawStatement)
-                    .collect(ImmutableSet.toImmutableSet()));
-        locationToCCodeMapping.put(loc, rawStatement);
-      }
-    }
-    return locationToCCodeMapping;
-  }
-
-  private String exportGlobalDeclarations() {
+  private String exportGlobalDeclarations(
+      final ImmutableSortedMap<FileLocation, GlobalDeclaration>
+          pGlobalDeclarationsSortedByFileLocation,
+      final ImmutableSet<FileLocation> pFileLocationsOfFunctionDefinitions) {
     final StringBuilder globalDeclarationsBuilder = new StringBuilder();
 
-    for (final FileLocation loc : locationToEdgesMappingForGlobalDeclarations.keySet()) {
-      // only export original declarations
-      if (!locationToFunctionStringMapping.containsKey(loc)) {
+    for (final Entry<FileLocation, GlobalDeclaration> entry :
+        pGlobalDeclarationsSortedByFileLocation.entrySet()) {
+      final FileLocation loc = entry.getKey();
+
+      // only export original global declarations, ignore the generated ones
+      if (!pFileLocationsOfFunctionDefinitions.contains(loc)) {
         assert loc.isRealLocation();
-        final Set<CFAEdge> edgesWithSameLocation =
-            locationToEdgesMappingForGlobalDeclarations.get(loc);
-        // assert, that there is exactly one edge with this location
-        final String rawStatement =
-            edgesWithSameLocation.stream()
-                .map(CFAEdge::getRawStatement)
-                .collect(MoreCollectors.onlyElement());
-        globalDeclarationsBuilder.append(rawStatement).append("\n");
+        globalDeclarationsBuilder.append(entry.getValue().toCCode()).append("\n");
       }
     }
     return globalDeclarationsBuilder.toString();
   }
 
-  private String processLabelEdge(final CFAEdge pLabelEdge) {
-    final FileLocation labelLoc = pLabelEdge.getFileLocation();
+  private abstract static class BlockItem {
 
-    final List<FileLocation> fileLocationsOfFollowingCfaEdges =
-        CFAUtils.leavingEdges(pLabelEdge.getSuccessor()).stream()
-            .map(CFAEdge::getFileLocation)
-            .collect(ImmutableList.toImmutableList());
-    final FileLocation nextFileLocation = FileLocation.merge(fileLocationsOfFollowingCfaEdges);
+    // TODO introduce flag to prohibit exporting multiple times?
 
-    // the raw statement of a CFAEdge after a LabelNode can overlap with the following CFAEdge(s)
-    final int beginningOfDuplicatePart =
-        nextFileLocation.getNodeOffset() - labelLoc.getNodeOffset();
-    final int endOfUniquePart = Math.min(labelLoc.getNodeLength(), beginningOfDuplicatePart);
-    return pLabelEdge.getRawStatement().substring(0, endOfUniquePart);
+    protected abstract String toCCode();
+  }
+
+  private static class SimpleBlockItem extends BlockItem {
+
+    // TODO introduce flag to prohibit adding edges after export?
+
+    protected final ImmutableList.Builder<CFAEdge> edgeBuilder;
+
+    private SimpleBlockItem(final CFAEdge pEdge) {
+      edgeBuilder = ImmutableList.<CFAEdge>builder().add(pEdge);
+    }
+
+    protected void addEdge(final CFAEdge pEdge) {
+      edgeBuilder.add(pEdge);
+    }
+
+    @Override
+    protected String toCCode() {
+      // assert that all edges have the same raw statement
+      return Iterables.getOnlyElement(
+          edgeBuilder.build().stream()
+              .map(CFAEdge::getRawStatement)
+              .collect(ImmutableSet.toImmutableSet()));
+    }
+  }
+
+  private static class GlobalDeclaration extends SimpleBlockItem {
+
+    private GlobalDeclaration(final CFAEdge pEdge) {
+      super(pEdge);
+    }
+
+    @Override
+    protected String toCCode() {
+      // assert that the global declaration consists of exactly one edge
+      return edgeBuilder.build().stream()
+          .map(CFAEdge::getRawStatement)
+          .collect(MoreCollectors.onlyElement());
+    }
+  }
+
+  private static class LabeledStatement extends SimpleBlockItem {
+
+    private LabeledStatement(final CFAEdge pEdge) {
+      super(pEdge);
+    }
+
+    @Override
+    protected void addEdge(final CFAEdge pEdge) {
+      throw new AssertionError("Labeled statements consist of exactly one edge.");
+    }
+
+    @Override
+    protected String toCCode() {
+      // assert that the labeled statement consists of exactly one edge
+      final CFAEdge labelEdge = Iterables.getOnlyElement(edgeBuilder.build());
+      final FileLocation labelLoc = labelEdge.getFileLocation();
+
+      final List<FileLocation> fileLocationsOfFollowingCfaEdges =
+          CFAUtils.leavingEdges(labelEdge.getSuccessor()).stream()
+              .map(CFAEdge::getFileLocation)
+              .collect(ImmutableList.toImmutableList());
+      final FileLocation nextFileLocation = FileLocation.merge(fileLocationsOfFollowingCfaEdges);
+
+      // the raw statement of a CFAEdge after a LabelNode can overlap with the following CFAEdge(s)
+      final int beginningOfDuplicatePart =
+          nextFileLocation.getNodeOffset() - labelLoc.getNodeOffset();
+      final int endOfUniquePart = Math.min(labelLoc.getNodeLength(), beginningOfDuplicatePart);
+      return labelEdge.getRawStatement().substring(0, endOfUniquePart);
+    }
   }
 }
