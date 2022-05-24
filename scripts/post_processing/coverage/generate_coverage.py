423c93969c7f3bf7ed8f39a5ed5eba9ebbd3b6bb
mhailer <mhailer@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2022-05-24 13:10:43 +0000

    Fix code style in generate_coverage.py
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/verification-coverage@40603 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- scripts/post_processing/coverage/generate_coverage.py
+++ scripts/post_processing/coverage/generate_coverage.py
@@ -277,7 +277,9 @@ class ComputeCoverage:
         stop_after_error=False,
         cex_count=0,
     ):
-        conf = os.path.join(cpachecker_root, "config", "valueAnalysis-with-coverage.properties")
+        conf = os.path.join(
+            cpachecker_root, "config", "valueAnalysis-with-coverage.properties"
+        )
         coverage_options = [
             "-setprop",
             "counterexample.export.exportCounterexampleCoverage=true",
