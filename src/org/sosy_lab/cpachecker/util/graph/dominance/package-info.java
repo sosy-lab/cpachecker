b040d0d7801429e71a02fa2463225aba74224bff
vport <vport@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2022-05-24 10:01:51 +0000

    Improve documentation of `util.graph.dominance.package-info`
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/graph-dominance@40600 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/util/graph/dominance/package-info.java
+++ src/org/sosy_lab/cpachecker/util/graph/dominance/package-info.java
@@ -10,6 +10,6 @@
  * Contains classes related to dominator tree and dominance frontier computation and representation.
  *
  * <p>Dominance for graphs is a concept where a node {@code D} dominates a node {@code N} if every
- * path from the start node of a graph must go through {@code D}.
+ * path from the start node of a graph to {@code N} must go through {@code D}.
  */
 package org.sosy_lab.cpachecker.util.graph.dominance;
