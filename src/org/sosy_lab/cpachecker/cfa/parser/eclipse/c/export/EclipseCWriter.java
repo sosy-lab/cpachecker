e40d39af0551b7a97fcb850a8f0075388a188f81
kcimbalnik <kcimbalnik@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2022-06-27 23:32:27 +0000

    Create CfaTransformationRecords for CFAs without them
    Handle such CFAs as if they were completely transformed, so that we
    can use the same algorithm.
    
    Also see #994
    
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/994-CFA-to-C-exporter-with-CFA-transformations@40976 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/cfa/parser/eclipse/c/export/EclipseCWriter.java
+++ src/org/sosy_lab/cpachecker/cfa/parser/eclipse/c/export/EclipseCWriter.java
@@ -11,16 +11,24 @@ package org.sosy_lab.cpachecker.cfa.parser.eclipse.c.export;
 import static com.google.common.base.Preconditions.checkArgument;
 
 import com.google.common.base.Verify;
+import com.google.common.collect.FluentIterable;
+import com.google.common.collect.ImmutableBiMap;
+import com.google.common.collect.ImmutableSet;
 import java.io.IOException;
+import java.util.Set;
 import org.eclipse.cdt.core.dom.ast.IASTTranslationUnit;
 import org.eclipse.core.runtime.CoreException;
 import org.sosy_lab.common.ShutdownNotifier;
 import org.sosy_lab.cpachecker.cfa.CFA;
 import org.sosy_lab.cpachecker.cfa.CParser.ParserOptions;
+import org.sosy_lab.cpachecker.cfa.CfaTransformationRecords;
 import org.sosy_lab.cpachecker.cfa.Language;
 import org.sosy_lab.cpachecker.cfa.export.CWriter;
+import org.sosy_lab.cpachecker.cfa.model.CFAEdge;
+import org.sosy_lab.cpachecker.cfa.model.CFANode;
 import org.sosy_lab.cpachecker.cfa.parser.eclipse.c.EclipseCdtWrapper;
 import org.sosy_lab.cpachecker.exceptions.CPAException;
+import org.sosy_lab.cpachecker.util.CFAUtils;
 
 /** Writer based on Eclipse CDT. */
 class EclipseCWriter implements CWriter {
@@ -31,28 +39,78 @@ class EclipseCWriter implements CWriter {
     eclipseCdt = new EclipseCdtWrapper(pOptions, pShutdownNotifier);
   }
 
+  // TODO split into smaller methods
   @Override
   public String exportCfa(final CFA pCfa) throws IOException, CPAException, InterruptedException {
 
     checkArgument(
         pCfa.getLanguage() == Language.C,
-        "CFA can only be exported to C for C input programs, at the moment.");
-    checkArgument(
-        pCfa.getFileNames().size() == 1,
-        "CFA can only be exported for a single input program, at the moment.");
+        "CFA can only be exported to C for C programs, at the moment.");
 
-    try {
-      final IASTTranslationUnit astUnit =
-          eclipseCdt.getASTTranslationUnit(EclipseCdtWrapper.wrapFile(pCfa.getFileNames().get(0)));
+    final CfaTransformationRecords records;
+    final IASTTranslationUnit originalAst;
 
-      Verify.verify(
-          astUnit.getPreprocessorProblemsCount() == 0,
-          "Problems should have been caught during CFA generation.");
+    // When the CFA does not have CfaTransformationRecords we assume it is completely changed, i.e.,
+    // that all edges and nodes were added and that the AST node substitutions were the identity.
+    // In that case, we can not parse the original AST, but we also do not require it.
+    if (pCfa.getTransformationRecords().isEmpty()) {
 
-      return astUnit.getRawSignature();
+      final Set<CFANode> allNodes = ImmutableSet.copyOf(pCfa.getAllNodes());
+      final ImmutableSet.Builder<CFAEdge> allEdges = ImmutableSet.builder();
+      final ImmutableBiMap.Builder<CFANode, CFANode> identityBiMapOfNodes =
+          ImmutableBiMap.builder();
+      final ImmutableBiMap.Builder<CFAEdge, CFAEdge> identityBiMapOfEdges =
+          ImmutableBiMap.builder();
 
-    } catch (final CoreException pE) {
-      throw new CPAException("Failed to export CFA to C program because AST parsing failed.");
+      for (final CFANode node : allNodes) {
+        identityBiMapOfNodes.put(node, node);
+
+        final FluentIterable<CFAEdge> edges = CFAUtils.leavingEdges(node);
+        for (final CFAEdge edge : edges) {
+          allEdges.add(edge);
+          identityBiMapOfEdges.put(edge, edge);
+        }
+      }
+
+      // TODO do I need the original CFA? should it be an Optional?
+      records =
+          new CfaTransformationRecords(
+              /* pOriginalCfa = */ null,
+              /* pAddedEdges = */ allEdges.build(),
+              /* pRemovedEdges =  */ ImmutableSet.of(),
+              /* pOldEdgeToNewEdgeAfterAstNodeSubstitution = */ identityBiMapOfEdges.build(),
+              /* pAddedNodes =  */ allNodes,
+              /* pRemovedNodes = */ ImmutableSet.of(),
+              /* pOldNodeToNewNodeAfterAstNodeSubstitution = */ identityBiMapOfNodes.build());
+      originalAst = null;
+
+    } else {
+      records = pCfa.getTransformationRecords().get();
+      final CFA originalCfa = records.getOriginalCfa();
+
+      checkArgument(
+          originalCfa.getFileNames().size() == 1,
+          "CFA can only be exported for a single input file, at the moment.");
+
+      try {
+        originalAst =
+            eclipseCdt.getASTTranslationUnit(
+                EclipseCdtWrapper.wrapFile(originalCfa.getFileNames().get(0)));
+
+        Verify.verify(
+            originalAst.getPreprocessorProblemsCount() == 0,
+            "Problems should have been caught during CFA generation.");
+
+      } catch (final CoreException pE) {
+        throw new CPAException("Failed to export CFA to C program because AST parsing failed.");
+      }
     }
+
+    // TODO collect true changes (edges and nodes, in control-flow order, by traversing the CFA)
+
+    // TODO combine the original AST nodes of the unchanged parts with the newly created AST nodes
+    // for the changed parts to get the C export
+
+    return ""; // TODO
   }
 }
