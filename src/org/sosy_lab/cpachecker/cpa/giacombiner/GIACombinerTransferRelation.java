7de380bdff59319794ec57fff631947de2c94266
jhaltermann <jhaltermann@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2022-06-28 10:01:07 +0000

    Optimized imports and removed unused configs
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/uca-generation@40984 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/cpa/giacombiner/GIACombinerTransferRelation.java
+++ src/org/sosy_lab/cpachecker/cpa/giacombiner/GIACombinerTransferRelation.java
@@ -17,7 +17,6 @@ import com.google.common.collect.Maps;
 import com.google.common.collect.Sets;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
