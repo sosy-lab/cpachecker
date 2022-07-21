17f3842cd08a2e134d11e421741b1efcfe0d81df
lemberge <lemberge@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2022-07-21 09:21:12 +0000

    Fix license/copyright header in log_to_html.py
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/configurable-code-block-analysis-fixed-merges@41204 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- contrib/worker-visualization/log_to_html.py
+++ contrib/worker-visualization/log_to_html.py
@@ -1,7 +1,8 @@
-# This file is part of BenchExec, a framework for reliable benchmarking:
-# https://github.com/sosy-lab/benchexec
+# This file is part of CPAchecker,
+# a tool for configurable software verification:
+# https://cpachecker.sosy-lab.org
 #
-# SPDX-FileCopyrightText: 2007-2020 Dirk Beyer <https://www.sosy-lab.org>
+# SPDX-FileCopyrightText: 2022 Dirk Beyer <https://www.sosy-lab.org>
 #
 # SPDX-License-Identifier: Apache-2.0
 
