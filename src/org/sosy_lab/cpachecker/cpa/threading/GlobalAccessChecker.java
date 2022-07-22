1e443927b65f5b434a3905954cda97bbc5cec1e4
sumbricht <sumbricht@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2022-07-22 10:55:39 +0000

    Revert unnecessary change.
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/data-race-detection@41230 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/cpa/threading/GlobalAccessChecker.java
+++ src/org/sosy_lab/cpachecker/cpa/threading/GlobalAccessChecker.java
@@ -71,7 +71,7 @@ public class GlobalAccessChecker {
    * memory, i.e. whether the edge might influence other threads or uses only scoped variables of
    * the thread.
    */
-  public boolean hasGlobalAccess(CFAEdge edge) {
+  boolean hasGlobalAccess(CFAEdge edge) {
     switch (edge.getEdgeType()) {
       case BlankEdge:
         return false;
