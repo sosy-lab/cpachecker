2345163404e92156328741359d72dc383a6ddaab
dbaier <dbaier@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2022-07-28 18:20:29 +0000

    SMGv2: Add the Value equality wrapper class
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/smgv2@41293 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

---
+++ src/org/sosy_lab/cpachecker/cpa/smg2/util/value/ValueWrapper.java
@@ -0,0 +1,32 @@
+// This file is part of CPAchecker,
+// a tool for configurable software verification:
+// https://cpachecker.sosy-lab.org
+//
+// SPDX-FileCopyrightText: 2022 Dirk Beyer <https://www.sosy-lab.org>
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package org.sosy_lab.cpachecker.cpa.smg2.util.value;
+
+import com.google.common.base.Equivalence;
+import org.sosy_lab.cpachecker.cpa.value.type.NumericValue;
+import org.sosy_lab.cpachecker.cpa.value.type.Value;
+
+/** Wrapper for {@link Value} to make equal {@link NumericValue}s equal for the valuesMapping. */
+public final class ValueWrapper extends Equivalence<Value> {
+
+  @Override
+  protected boolean doEquivalent(Value pArg0, Value pArg1) {
+    if (pArg0 instanceof NumericValue && pArg1 instanceof NumericValue) {
+      if (pArg0.asNumericValue().longValue() == 0 && pArg1.asNumericValue().longValue() == 0) {
+        return true;
+      }
+    }
+    return pArg0.equals(pArg1);
+  }
+
+  @Override
+  protected int doHash(Value pArg0) {
+    return pArg0.hashCode();
+  }
+}
