054069b7df5ebd179bbb3a80a736c0e15706c767
mkettl <mkettl@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2022-08-01 14:32:40 +0000

    Add JavaDoc to BlockSummaryActor
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/configurable-code-block-analysis-fixed-merges@41315 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/core/algorithm/distributed_summaries/worker/BlockSummaryActor.java
+++ src/org/sosy_lab/cpachecker/core/algorithm/distributed_summaries/worker/BlockSummaryActor.java
@@ -15,29 +15,66 @@ import org.sosy_lab.cpachecker.core.algorithm.distributed_summaries.exchange.act
 import org.sosy_lab.cpachecker.exceptions.CPAException;
 import org.sosy_lab.java_smt.api.SolverException;
 
+/**
+ * A {@link BlockSummaryActor} is connected to a network of {@link BlockSummaryActor}s. All
+ * participating {@link BlockSummaryActor}s exchange messages. Every message is broadcast to every
+ * other actor. Every actor decides on its own how to handle the message.
+ */
 public interface BlockSummaryActor extends Runnable {
 
   /**
    * Answer a received message by a potentially empty set of responses.
    *
-   * @param pMessage The message to answer
-   * @return Potentially empty set of responses
-   * @throws InterruptedException thrown if program is interrupted from the outside
-   * @throws IOException thrown if loggers cannot write to the logfile
-   * @throws SolverException thrown if SMT based calculations face problems
-   * @throws CPAException thrown it the analysis should end with crashing
+   * @param pMessage The message to answer.
+   * @return Potentially empty set of responses.
+   * @throws InterruptedException thrown if program is interrupted from the outside.
+   * @throws IOException thrown if loggers cannot write to the logfile.
+   * @throws SolverException thrown if SMT based calculations face problems.
+   * @throws CPAException thrown it the analysis should end with crashing.
    */
   Collection<ActorMessage> processMessage(ActorMessage pMessage)
       throws InterruptedException, IOException, SolverException, CPAException;
 
+  /**
+   * Broadcast a collection of {@link ActorMessage}s to all other actors that implement this
+   * interface.
+   *
+   * @param pMessages A collection of messages that all actors should receive
+   * @throws InterruptedException thrown if system is interrupted unexpectedly
+   */
   void broadcast(Collection<ActorMessage> pMessages) throws InterruptedException;
 
+  /**
+   * Returns a connection to all other available actors.
+   *
+   * @return Connection to all other available actors.
+   * @see Connection
+   */
   Connection getConnection();
 
+  /**
+   * Returns false as long as this worker should run. Once it returns true it should never switch
+   * back to returning false.
+   *
+   * @return true iff actor should stop working and is never needed again, false otherwise.
+   * @see BlockSummaryWorker
+   */
   boolean shutdownRequested();
 
+  /**
+   * Get an unique ID of this actor.
+   *
+   * @return Unique ID of this worker.
+   */
   String getId();
 
+  /**
+   * Get the next received message.
+   *
+   * @return Next {@link ActorMessage} to process with {@link
+   *     BlockSummaryActor#processMessage(ActorMessage)}
+   * @throws InterruptedException thrown if system is interrupted unexpectedly
+   */
   default ActorMessage nextMessage() throws InterruptedException {
     return getConnection().read();
   }
