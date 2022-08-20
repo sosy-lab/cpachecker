deaa2fefa0fb3b7117772dbbb3ce8a28c35da734
dbaier <dbaier@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2022-08-20 17:59:47 +0000

    SMGv2: override deriveInterpolant() in the SMGEdgeInterpolator for immutable states
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/smgv2@41511 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/cpa/smg2/refiner/SMGEdgeInterpolator.java
+++ src/org/sosy_lab/cpachecker/cpa/smg2/refiner/SMGEdgeInterpolator.java
@@ -8,19 +8,29 @@
 
 package org.sosy_lab.cpachecker.cpa.smg2.refiner;
 
+import java.util.Deque;
+import java.util.Optional;
 import org.sosy_lab.common.ShutdownNotifier;
 import org.sosy_lab.common.configuration.Configuration;
 import org.sosy_lab.common.configuration.InvalidConfigurationException;
 import org.sosy_lab.common.configuration.Options;
 import org.sosy_lab.common.log.LogManager;
 import org.sosy_lab.cpachecker.cfa.CFA;
+import org.sosy_lab.cpachecker.cfa.model.CFAEdge;
+import org.sosy_lab.cpachecker.cpa.arg.path.ARGPath;
+import org.sosy_lab.cpachecker.cpa.arg.path.PathIterator;
+import org.sosy_lab.cpachecker.cpa.arg.path.PathPosition;
 import org.sosy_lab.cpachecker.cpa.smg2.SMGInformation;
 import org.sosy_lab.cpachecker.cpa.smg2.SMGOptions;
 import org.sosy_lab.cpachecker.cpa.smg2.SMGState;
 import org.sosy_lab.cpachecker.cpa.value.ValueAnalysisCPA;
+import org.sosy_lab.cpachecker.exceptions.CPAException;
 import org.sosy_lab.cpachecker.util.refinement.FeasibilityChecker;
 import org.sosy_lab.cpachecker.util.refinement.GenericEdgeInterpolator;
+import org.sosy_lab.cpachecker.util.refinement.ImmutableForgetfulState.StateAndInfo;
+import org.sosy_lab.cpachecker.util.refinement.InterpolantManager;
 import org.sosy_lab.cpachecker.util.refinement.StrongestPostOperator;
+import org.sosy_lab.cpachecker.util.states.MemoryLocation;
 
 @Options(prefix = "cpa.smg2.interpolation")
 public class SMGEdgeInterpolator
@@ -46,5 +56,105 @@ public class SMGEdgeInterpolator
         pConfig,
         pShutdownNotifier,
         pCfa);
+  }
+
+  /**
+   * This method derives an interpolant for the given error path and interpolation state.
+   *
+   * @param pErrorPath the path to check
+   * @param pCurrentEdge the current edge to interpolate
+   * @param pCallstack the current call stack
+   * @param pOffset offset of the state at where to start the current interpolation
+   * @param pInputInterpolant the input interpolant
+   */
+  @SuppressWarnings("unchecked")
+  @Override
+  public SMGInterpolant deriveInterpolant(
+      final ARGPath pErrorPath,
+      final CFAEdge pCurrentEdge,
+      final Deque<SMGState> pCallstack,
+      final PathPosition pOffset,
+      final SMGInterpolant pInputInterpolant)
+      throws CPAException, InterruptedException {
+
+    super.resetNumberOfInterpolationQueries();
+    InterpolantManager<SMGState, SMGInterpolant> interpolantMgr = getInterpolationManager();
+
+    // create initial state, based on input interpolant, and create initial successor by consuming
+    // the next edge
+    SMGState stateFromOldInterpolant = pInputInterpolant.reconstructState();
+
+    // TODO callstack-management depends on a forward-iteration on a single path.
+    // TODO Thus interpolants have to be computed from front to end. Can we assure this?
+    final Optional<SMGState> maybeSuccessor;
+    if (pCurrentEdge == null) {
+      PathIterator it = pOffset.fullPathIterator();
+      Optional<SMGState> intermediate = Optional.of(stateFromOldInterpolant);
+      do {
+        if (!intermediate.isPresent()) {
+          break;
+        }
+
+        intermediate =
+            getInitialSuccessor(intermediate.orElseThrow(), it.getOutgoingEdge(), pCallstack);
+        it.advance();
+      } while (!it.isPositionWithState());
+      maybeSuccessor = intermediate;
+    } else {
+      maybeSuccessor = getInitialSuccessor(stateFromOldInterpolant, pCurrentEdge, pCallstack);
+    }
+
+    if (!maybeSuccessor.isPresent()) {
+      return interpolantMgr.getFalseInterpolant();
+    }
+
+    SMGState initialSuccessor = maybeSuccessor.orElseThrow();
+
+    // if initial state and successor are equal, return the input interpolant
+    // in general, this returned interpolant might be stronger than needed, but only in very rare
+    // cases, the weaker interpolant would be different from the input interpolant, so we spare the
+    // effort
+    if (getApplyItpEqualityOptimization() && stateFromOldInterpolant.equals(initialSuccessor)) {
+      return pInputInterpolant;
+    }
+
+    // if the current edge just changes the names of variables
+    // (e.g. function arguments, returned variables)
+    // then return the input interpolant with those renamings
+    if (getApplyRenamingOptimization() && isOnlyVariableRenamingEdge(pCurrentEdge)) {
+      return interpolantMgr.createInterpolant(initialSuccessor);
+    }
+
+    ARGPath remainingErrorPath = pOffset.iterator().getSuffixExclusive();
+
+    // if the remaining path, i.e., the suffix, is contradicting by itself, then return the TRUE
+    // interpolant
+    if (getApplyUnsatSuffixOptimization()
+        && pInputInterpolant.isTrue()
+        && initialSuccessor.getSize() > 1
+        && isSuffixContradicting(remainingErrorPath)) {
+      return interpolantMgr.getTrueInterpolant();
+    }
+
+    for (MemoryLocation currentMemoryLocation :
+        determineMemoryLocationsToInterpolateOn(initialSuccessor)) {
+      getShutdownNotifier().shutdownIfNecessary();
+
+      // temporarily remove the value of the current memory location from the candidate
+      // interpolant
+      // Immutable copyOnOperation case
+      StateAndInfo<SMGState, SMGInformation> forgottenInformationAndNewState =
+          initialSuccessor.copyAndForget(currentMemoryLocation);
+      initialSuccessor = forgottenInformationAndNewState.getState();
+
+      // check if the remaining path now becomes feasible
+      if (isRemainingPathFeasible(remainingErrorPath, initialSuccessor)) {
+        initialSuccessor =
+            initialSuccessor.copyAndRemember(
+                currentMemoryLocation, forgottenInformationAndNewState.getInfo());
+      }
+    }
+
+    return interpolantMgr.createInterpolant(initialSuccessor);
   }
 }
