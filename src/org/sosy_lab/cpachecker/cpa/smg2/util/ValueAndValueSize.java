b9af8289eab671ffd904d31b5cbda335598e61cf
dbaier <dbaier@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2022-08-25 21:40:04 +0000

    SMGv2: add toString() for ValueAndValueSize helper class
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/smgv2@41561 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/cpa/smg2/util/ValueAndValueSize.java
+++ src/org/sosy_lab/cpachecker/cpa/smg2/util/ValueAndValueSize.java
@@ -58,4 +58,9 @@ public class ValueAndValueSize {
     }
     return value.hashCode() + sizeOfValueInBits.hashCode();
   }
+
+  @Override
+  public String toString() {
+    return value.toString() + " (" + sizeOfValueInBits + ")";
+  }
 }
