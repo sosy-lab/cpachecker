1ccf328bd07ef8c6b9724ccd653999651abda33b
dbaier <dbaier@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2022-09-05 18:11:26 +0000

    SMGv2: reorder linked list segments. doubly linked lists are not inherited from singly linked lists
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/smgv2@41617 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/util/smg/graph/SMGDoublyLinkedListSegment.java
+++ src/org/sosy_lab/cpachecker/util/smg/graph/SMGDoublyLinkedListSegment.java
@@ -10,42 +10,24 @@ package org.sosy_lab.cpachecker.util.smg.graph;
 
 import java.math.BigInteger;
 
-public class SMGDoublyLinkedListSegment extends SMGObject {
+public class SMGDoublyLinkedListSegment extends SMGSinglyLinkedListSegment {
 
-  private final int minLength;
-  private final BigInteger headOffset;
-  private final BigInteger nextOffset;
   private final BigInteger prevOffset;
 
   public SMGDoublyLinkedListSegment(
       int pNestingLevel,
       BigInteger pSize,
       BigInteger pOffset,
-      BigInteger pPrevOffset,
+      BigInteger pHeadOffset,
       BigInteger pNextOffset,
-      int pMinLength,
-      BigInteger pHeadOffset) {
-    super(pNestingLevel, pSize, pOffset);
-    minLength = pMinLength;
-    headOffset = pHeadOffset;
-    nextOffset = pNextOffset;
+      BigInteger pPrevOffset,
+      int pMinLength) {
+    super(pNestingLevel, pSize, pOffset, pHeadOffset, pNextOffset, pMinLength);
     prevOffset = pPrevOffset;
   }
 
   public BigInteger getPrevOffset() {
     return prevOffset;
-  }
-
-  public BigInteger getNextOffset() {
-    return nextOffset;
-  }
-
-  public BigInteger getHeadOffset() {
-    return headOffset;
-  }
-
-  public int getMinLength() {
-    return minLength;
   }
 
   @Override
@@ -61,12 +43,24 @@ public class SMGDoublyLinkedListSegment extends SMGObject {
   @Override
   public SMGObject copyWithNewLevel(int newLevel) {
     return new SMGDoublyLinkedListSegment(
-        newLevel, getSize(), getOffset(), prevOffset, nextOffset, minLength, headOffset);
+        newLevel,
+        getSize(),
+        getOffset(),
+        getHeadOffset(),
+        getNextOffset(),
+        prevOffset,
+        getMinLength());
   }
 
   @Override
   public SMGObject freshCopy() {
     return new SMGDoublyLinkedListSegment(
-        getNestingLevel(), getSize(), getOffset(), prevOffset, nextOffset, minLength, headOffset);
+        getNestingLevel(),
+        getSize(),
+        getOffset(),
+        getHeadOffset(),
+        getNextOffset(),
+        prevOffset,
+        getMinLength());
   }
 }
---
+++ src/org/sosy_lab/cpachecker/util/smg/graph/SMGSinglyLinkedListSegment.java
@@ -0,0 +1,65 @@
+// This file is part of CPAchecker,
+// a tool for configurable software verification:
+// https://cpachecker.sosy-lab.org
+//
+// SPDX-FileCopyrightText: 2021 Dirk Beyer <https://www.sosy-lab.org>
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package org.sosy_lab.cpachecker.util.smg.graph;
+
+import java.math.BigInteger;
+
+public class SMGSinglyLinkedListSegment extends SMGObject {
+
+  private final int minLength;
+  private final BigInteger headOffset;
+  private final BigInteger nextOffset;
+
+  public SMGSinglyLinkedListSegment(
+      int pNestingLevel,
+      BigInteger pSize,
+      BigInteger pOffset,
+      BigInteger pHeadOffset,
+      BigInteger pNextOffset,
+      int pMinLength) {
+    super(pNestingLevel, pSize, pOffset);
+    minLength = pMinLength;
+    headOffset = pHeadOffset;
+    nextOffset = pNextOffset;
+  }
+
+  public BigInteger getNextOffset() {
+    return nextOffset;
+  }
+
+  public BigInteger getHeadOffset() {
+    return headOffset;
+  }
+
+  public int getMinLength() {
+    return minLength;
+  }
+
+  @Override
+  public boolean equals(Object pOther) {
+    return super.equals(pOther);
+  }
+
+  @Override
+  public int hashCode() {
+    return super.hashCode();
+  }
+
+  @Override
+  public SMGObject copyWithNewLevel(int newLevel) {
+    return new SMGSinglyLinkedListSegment(
+        newLevel, getSize(), getOffset(), headOffset, nextOffset, minLength);
+  }
+
+  @Override
+  public SMGObject freshCopy() {
+    return new SMGSinglyLinkedListSegment(
+        getNestingLevel(), getSize(), getOffset(), headOffset, nextOffset, minLength);
+  }
+}
