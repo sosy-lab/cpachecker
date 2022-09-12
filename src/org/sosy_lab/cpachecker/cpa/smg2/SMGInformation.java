3e8c518bb9cba682a9ab17e2c5d5946e62b9ea4c
dbaier <dbaier@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2022-09-12 21:40:59 +0000

    SMGv2: add SMGInformation implementation for heap values
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/smgv2@41657 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/cpa/smg2/SMGInformation.java
+++ src/org/sosy_lab/cpachecker/cpa/smg2/SMGInformation.java
@@ -8,21 +8,29 @@
 
 package org.sosy_lab.cpachecker.cpa.smg2;
 
+import com.google.common.collect.ImmutableMap;
 import java.math.BigInteger;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
+import java.util.Set;
 import org.sosy_lab.common.collect.PathCopyingPersistentTreeMap;
 import org.sosy_lab.common.collect.PersistentMap;
 import org.sosy_lab.cpachecker.cfa.types.c.CType;
 import org.sosy_lab.cpachecker.cpa.smg.util.PersistentStack;
 import org.sosy_lab.cpachecker.cpa.smg2.util.CFunctionDeclarationAndOptionalValue;
 import org.sosy_lab.cpachecker.cpa.smg2.util.ValueAndValueSize;
+import org.sosy_lab.cpachecker.util.smg.graph.SMGHasValueEdge;
+import org.sosy_lab.cpachecker.util.smg.graph.SMGObject;
 import org.sosy_lab.cpachecker.util.states.MemoryLocation;
 
 /** Information about value assignments needed for symbolic interpolation. */
 public final class SMGInformation {
 
+  // Mappings needed to restore heap values
+  private final Map<SMGObject, Set<SMGHasValueEdge>> heapValuesRemoved;
+
+  // Mapping needed to restore program variables
   private final PersistentMap<String, CType> variableToTypeMap;
   private final Map<String, BigInteger> variableNameAndSizeInBits;
   private final PersistentMap<MemoryLocation, ValueAndValueSize> nonHeapAssignments;
@@ -37,6 +45,15 @@ public final class SMGInformation {
     variableNameAndSizeInBits = pVariableNameAndSizeInBits;
     variableToTypeMap = pVariableToTypeMap;
     stackDeclarations = pStackDeclarations;
+    heapValuesRemoved = ImmutableMap.of();
+  }
+
+  SMGInformation(Map<SMGObject, Set<SMGHasValueEdge>> pHeapValuesRemoved) {
+    nonHeapAssignments = PathCopyingPersistentTreeMap.of();
+    variableNameAndSizeInBits = new HashMap<>();
+    variableToTypeMap = PathCopyingPersistentTreeMap.of();
+    stackDeclarations = PersistentStack.of();
+    heapValuesRemoved = pHeapValuesRemoved;
   }
 
   private SMGInformation() {
@@ -44,10 +61,15 @@ public final class SMGInformation {
     variableNameAndSizeInBits = new HashMap<>();
     variableToTypeMap = PathCopyingPersistentTreeMap.of();
     stackDeclarations = PersistentStack.of();
+    heapValuesRemoved = ImmutableMap.of();
   }
 
   public static SMGInformation getEmptySMGInformation() {
     return new SMGInformation();
+  }
+
+  public Map<SMGObject, Set<SMGHasValueEdge>> getHeapValuesPerObjectMap() {
+    return heapValuesRemoved;
   }
 
   public PersistentMap<MemoryLocation, ValueAndValueSize> getAssignments() {
@@ -86,18 +108,26 @@ public final class SMGInformation {
     SMGInformation that = (SMGInformation) o;
     // Under the general assumption that variable names are unique (they aren't really) this should
     // suffice as long as heap is not tracked
-    return nonHeapAssignments.equals(that.nonHeapAssignments);
+    return nonHeapAssignments.equals(that.nonHeapAssignments)
+        && heapValuesRemoved.equals(that.heapValuesRemoved);
   }
 
   @Override
   public int hashCode() {
     return Objects.hash(nonHeapAssignments)
         + 31 * Objects.hash(variableToTypeMap)
-        + 17 * Objects.hash(variableNameAndSizeInBits);
+        + 17 * Objects.hash(variableNameAndSizeInBits)
+        + 23 * (Objects.hash(heapValuesRemoved));
   }
 
   @Override
   public String toString() {
-    return "ValueInformation[" + nonHeapAssignments + "]";
+    return "VariableInformation["
+        + nonHeapAssignments
+        + "]"
+        + "\n"
+        + "Heap Values ["
+        + heapValuesRemoved
+        + "]";
   }
 }
