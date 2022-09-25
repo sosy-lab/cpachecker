f43b77cf5a3ace6bff9c1579b26245385ff3258d
Karlheinz Friedberger <kfriedberger@gmail.com>
2022-09-25 21:16:25 +0000

    improve FormulaToCExpressionConverter and its JUnit test.
    Changes in the converter:
      The number of handled operations from SMT formulas was increased.
      For example, ITE and several FP operations are handled now.
      Additionally, brackets in the result are changed.
      Before this change, brackets for some operations were set redundant
      or too late to provide valid C expressions in the used cache.
      Now, each sub-formula is converted into a valid bracketed string representing,
      which allows to access sub-formulas, if required.
    
    Changes in the JUnit test:
      The number of excluded tests (explicitly excluded solvers per test) was reduced.
      Several tests are fixed to match the new scheme for brackets.
    
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/1007-update-JavaSMT-from-v3.10.1-to-v3.13.0@41766 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/util/cwriter/FormulaToCExpressionVisitor.java
+++ src/org/sosy_lab/cpachecker/util/cwriter/FormulaToCExpressionVisitor.java
@@ -8,8 +8,12 @@
 
 package org.sosy_lab.cpachecker.util.cwriter;
 
+import com.google.common.base.Preconditions;
 import com.google.common.base.Splitter;
+import com.google.common.collect.FluentIterable;
+import com.google.common.collect.ImmutableSet;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -17,11 +21,28 @@ import org.sosy_lab.cpachecker.util.predicates.smt.FormulaManagerView;
 import org.sosy_lab.cpachecker.util.predicates.smt.FormulaManagerView.FormulaTransformationVisitor;
 import org.sosy_lab.java_smt.api.Formula;
 import org.sosy_lab.java_smt.api.FunctionDeclaration;
+import org.sosy_lab.java_smt.api.FunctionDeclarationKind;
 
+/**
+ * This visitor transforms a formula to a plain C expression (in String representation). The visitor
+ * returns the input formula unchanged, and provides the corresponding C expression is available via
+ * a separate method {@link #getCExpressionForFormula(Formula)}.
+ *
+ * <p>Warning: The length of the computed string representation can be exponential in formula size.
+ *
+ * <p>The string-representation depends on the solver-specific representation and internal ordering
+ * of the formula. Variables (like "foo::x") that contain a scope (like function-name "foo") are
+ * replaced by their unscoped name (like "x"). Constants (numerals) are transformed into their
+ * string-representation.
+ */
 public class FormulaToCExpressionVisitor extends FormulaTransformationVisitor {
 
   // TODO: do not hardcode the function separator.
   private static final String FUNCTION_NAME_SEPARATOR = "::";
+
+  private static final ImmutableSet<FunctionDeclarationKind> COMMUTATIVE_OPERATIONS =
+      ImmutableSet.of(
+          FunctionDeclarationKind.EQ, FunctionDeclarationKind.BV_EQ, FunctionDeclarationKind.FP_EQ);
 
   private final Map<Formula, String> cache = new HashMap<>();
 
@@ -31,11 +52,9 @@ public class FormulaToCExpressionVisitor extends FormulaTransformationVisitor {
 
   @Override
   public Formula visitFreeVariable(Formula f, String name) {
-    if (name.contains(FUNCTION_NAME_SEPARATOR)) {
-      cache.put(f, Splitter.on(FUNCTION_NAME_SEPARATOR).splitToList(name).get(1));
-    } else {
-      cache.put(f, name);
-    }
+    List<String> parts = Splitter.on(FUNCTION_NAME_SEPARATOR).splitToList(name);
+    final String variableName = (parts.size() == 2) ? parts.get(1) : name;
+    cache.put(f, variableName);
     return f;
   }
 
@@ -48,23 +67,21 @@ public class FormulaToCExpressionVisitor extends FormulaTransformationVisitor {
   @Override
   public Formula visitFunction(
       Formula f, List<Formula> newArgs, FunctionDeclaration<?> functionDeclaration) {
-    String op;
-    String result;
+    String result = null;
+    // rule for visitation:
+    // - every argument already has all necessary brackets, except variables (-> plain string).
+    // - the result has surrounding brackets.
     switch (functionDeclaration.getKind()) {
       case NOT:
       case UMINUS:
       case BV_NEG:
       case FP_NEG:
       case BV_NOT:
-        op = operatorFromFunctionDeclaration(functionDeclaration);
-        result = op + "(" + cache.get(newArgs.get(0)) + ")";
-        cache.put(f, result);
+        result = operatorFromFunctionDeclaration(functionDeclaration) + cache.get(newArgs.get(0));
         break;
       case EQ_ZERO:
       case GTE_ZERO:
-        op = operatorFromFunctionDeclaration(functionDeclaration);
-        result = "(" + cache.get(newArgs.get(0)) + ")" + op;
-        cache.put(f, result);
+        result = cache.get(newArgs.get(0)) + operatorFromFunctionDeclaration(functionDeclaration);
         break;
       case FP_ROUND_EVEN:
       case FP_ROUND_AWAY:
@@ -73,16 +90,48 @@ public class FormulaToCExpressionVisitor extends FormulaTransformationVisitor {
       case FP_ROUND_ZERO:
       case FP_ROUND_TO_INTEGRAL:
         // Ignore because otherwise rounding mode is treated like an additional operand
+        // TODO These cases do not insert anything into the cache and might result in an invalid or
+        //      incomplete result. We should better abort here than continue with an invalid result.
+        break;
+      case FP_ADD:
+      case FP_SUB:
+      case FP_DIV:
+      case FP_MUL:
+        { // skip first argument, it represents the rounding-mode.
+          List<String> expressions =
+              FluentIterable.from(newArgs)
+                  .skip(1)
+                  .transform(arg -> Preconditions.checkNotNull(cache.get(arg)))
+                  .toList();
+          result = String.join(operatorFromFunctionDeclaration(functionDeclaration), expressions);
+          break;
+        }
+      case ITE:
+        result =
+            String.format(
+                "%s ? %s : %s",
+                cache.get(newArgs.get(0)), cache.get(newArgs.get(1)), cache.get(newArgs.get(2)));
         break;
       default:
-        List<String> expressions = new ArrayList<>(newArgs.size());
-        for (Formula arg : newArgs) {
-          if (cache.containsKey(arg)) {
-            expressions.add(cache.get(arg));
+        {
+          List<String> expressions = new ArrayList<>(newArgs.size());
+          for (Formula arg : newArgs) {
+            // TODO If the arg is not in the cache, we will get an invalid or incomplete result.
+            //      We should better abort here than continue with an invalid result.
+            if (cache.containsKey(arg)) {
+              expressions.add(Preconditions.checkNotNull(cache.get(arg)));
+            }
           }
+          if (COMMUTATIVE_OPERATIONS.contains(functionDeclaration.getKind())) {
+            // Some solvers (e.g., MathSAT) switch commutative arguments for operations like EQ
+            // randomly. Let's force some determinism over distinct solvers, by alphabetical order.
+            Collections.sort(expressions);
+          }
+          result = String.join(operatorFromFunctionDeclaration(functionDeclaration), expressions);
         }
-        op = operatorFromFunctionDeclaration(functionDeclaration);
-        cache.put(f, "(" + String.join(op, expressions) + ")");
+    }
+    if (result != null) {
+      cache.put(f, "(" + result + ")");
     }
     return f;
   }
@@ -158,12 +207,41 @@ public class FormulaToCExpressionVisitor extends FormulaTransformationVisitor {
         return "~";
       case BV_SHL:
         return " << ";
+      case UF:
+        // There are several CPAchecker-internal UFs that replace unsupported function in solvers.
+        // See FormulaManagerView and ReplaceBitvectorWithNumeralAndFunctionTheory for details.
+        switch (pDeclaration.getName()) {
+          case "_&_":
+            return " & ";
+          case "_!!_":
+            return " | ";
+          case "_^_":
+            return " ^ ";
+          case "_~_":
+            return "~";
+          case "_<<_":
+            return " << ";
+          case "_>>_":
+            return " >> "; // TODO arithmetic or logical shift?
+          case "_%_":
+            return " % ";
+          default:
+            // $FALL-THROUGH$
+        }
+        // $FALL-THROUGH$
       default:
         throw new UnsupportedOperationException(
             "Unexpected operand " + pDeclaration.getKind() + "(" + pDeclaration.getName() + ")");
     }
   }
 
+  /**
+   * This method provides access to the C expression (as String) of the visited formula. The method
+   * can be used to query C expressions for all sub-formulas.
+   *
+   * @return the C expression of a visited formula, or NULL, if the formula (or sub-formula) was not
+   *     yet visited.
+   */
   public String getCExpressionForFormula(Formula f) {
     return cache.get(f);
   }
