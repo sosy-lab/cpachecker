28c8ca6772b56b53198fcc2de0410c3702dbd88f
vport <vport@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2022-10-20 22:22:49 +0000

    Fix invalid links in package documentation
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/cfa-transformer@41942 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/cfa/graph/package-info.java
+++ src/org/sosy_lab/cpachecker/cfa/graph/package-info.java
@@ -7,35 +7,37 @@
 // SPDX-License-Identifier: Apache-2.0
 
 /**
- * Defines {@link Network} and {@link Graph} implementations for CFAs.
+ * Defines {@link com.google.common.graph.Network} and {@link com.google.common.graph.Graph}
+ * implementations for CFAs.
  *
- * <p>The key idea is that all connections of a CFA are fully defined by a {@link CfaNetwork}
- * instead of its individual elements (i.e., its nodes and edges). This offers a great amount of
- * flexibility as different views of a CFA can be created without changing the original CFA (e.g.,
- * by applying on-the-fly filters or transformations). Additionally, this makes modifying CFAs
- * easier, because for some implementations it isn't necessary to keep individual elements of a CFA
- * in sync (e.g., depending on the implementation, we don't need to create a CFA edge with specific
- * endpoints and also add/register the edge at those endpoints).
+ * <p>The key idea is that all connections of a CFA are fully defined by a {@link
+ * org.sosy_lab.cpachecker.cfa.graph.CfaNetwork} instead of its individual elements (i.e., its nodes
+ * and edges). This offers a great amount of flexibility as different views of a CFA can be created
+ * without changing the original CFA (e.g., by applying on-the-fly filters or transformations).
+ * Additionally, this makes modifying CFAs easier, because for some implementations it isn't
+ * necessary to keep individual elements of a CFA in sync (e.g., depending on the implementation, we
+ * don't need to create a CFA edge with specific endpoints and also add/register the edge at those
+ * endpoints).
  *
  * <p>There are three main interfaces:
  *
  * <ul>
- *   <li>{@link CfaNetwork} extends {@link Network}: All {@link Network} implementations
- *       representing CFAs implement this interface. There are methods for {@link
- *       CfaNetwork#wrap(CFA) wrapping} existing CFAs and applying on-the-fly filters and
- *       transformations.
- *   <li>{@link MutableCfaNetwork} extends {@link CfaNetwork} and {@link MutableNetwork}: The
- *       mutable version of a {@link CfaNetwork} that also provides basic methods for modifying
- *       CFAs. Existing {@link MutableCFA} instances can be {@link
- *       MutableCfaNetwork#wrap(MutableCFA) wrapped}.
- *   <li>{@link FlexCfaNetwork} extends {@link MutableCfaNetwork}: A flexible {@link
- *       MutableCfaNetwork} that allows for more advanced operations that wouldn't be possible for
- *       all {@link MutableCfaNetwork} implementations.
+ *   <li>{@link org.sosy_lab.cpachecker.cfa.graph.CfaNetwork} extends {@link
+ *       com.google.common.graph.Network}: All {@link com.google.common.graph.Network}
+ *       implementations representing CFAs implement this interface. There are methods for {@link
+ *       org.sosy_lab.cpachecker.cfa.graph.CfaNetwork#wrap(CFA) wrapping} existing CFAs and applying
+ *       on-the-fly filters and transformations.
+ *   <li>{@link org.sosy_lab.cpachecker.cfa.graph.MutableCfaNetwork} extends {@link
+ *       org.sosy_lab.cpachecker.cfa.graph.CfaNetwork} and {@link
+ *       org.sosy_lab.cpachecker.cfa.graph.MutableNetwork}: The mutable version of a {@link
+ *       org.sosy_lab.cpachecker.cfa.graph.CfaNetwork} that also provides basic methods for
+ *       modifying CFAs. Existing {@link org.sosy_lab.cpachecker.cfa.MutableCFA} instances can be
+ *       {@link org.sosy_lab.cpachecker.cfa.graph.MutableCfaNetwork#wrap(MutableCFA) wrapped}.
+ *   <li>{@link org.sosy_lab.cpachecker.cfa.graph.FlexCfaNetwork} extends {@link
+ *       org.sosy_lab.cpachecker.cfa.graph.MutableCfaNetwork}: A flexible {@link
+ *       org.sosy_lab.cpachecker.cfa.graph.MutableCfaNetwork} that allows for more advanced
+ *       operations that wouldn't be possible for all {@link
+ *       org.sosy_lab.cpachecker.cfa.graph.MutableCfaNetwork} implementations.
  * </ul>
  */
 package org.sosy_lab.cpachecker.cfa.graph;
-
-import com.google.common.graph.Graph;
-import com.google.common.graph.Network;
-import org.sosy_lab.cpachecker.cfa.CFA;
-import org.sosy_lab.cpachecker.cfa.MutableCFA;
