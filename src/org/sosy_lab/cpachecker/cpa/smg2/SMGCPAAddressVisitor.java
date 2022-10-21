0b5ab353f71fbf83a16bc7a65729ed47e125c056
dbaier <dbaier@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2022-10-21 12:02:53 +0000

    SMGv2: overapproximate subscript memory access in the address visitor such that non concrete access is always invalid
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/smgv2@41950 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/cpa/smg2/SMGCPAAddressVisitor.java
+++ src/org/sosy_lab/cpachecker/cpa/smg2/SMGCPAAddressVisitor.java
@@ -136,7 +136,7 @@ public class SMGCPAAddressVisitor
               Level.FINE,
               "A subscript value was found to be non concrete when trying to find a memory location"
                   + " of an array. No memory region could be returned.");
-          resultBuilder.add(SMGStateAndOptionalSMGObjectAndOffset.of(currentState));
+          resultBuilder.add(SMGStateAndOptionalSMGObjectAndOffset.of(currentState.withUnknownOffsetMemoryAccess()));
           continue;
         }
         // Calculate the offset out of the subscript value and the type
@@ -170,7 +170,7 @@ public class SMGCPAAddressVisitor
             Level.FINE,
             "A offset value was found to be non concrete when trying to find a memory"
                 + " location in an array. No memory region could be returned.");
-        return SMGStateAndOptionalSMGObjectAndOffset.of(currentState);
+        return SMGStateAndOptionalSMGObjectAndOffset.of(currentState.withUnknownOffsetMemoryAccess());
       }
       BigInteger baseOffset = addrOffset.asNumericValue().bigInteger();
       BigInteger finalOffset = baseOffset.add(subscriptOffset);
