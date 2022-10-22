365cd670b110c6397b5f9e359ee36e5a170bd31b
vport <vport@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2022-10-22 22:26:39 +0000

    Add `cfa.transformer.CfaFactory` interface
    To create multiple CFA instances by a single factory.
    
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/cfa-transformer@41985 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

---
+++ src/org/sosy_lab/cpachecker/cfa/transformer/CfaFactory.java
@@ -0,0 +1,59 @@
+// This file is part of CPAchecker,
+// a tool for configurable software verification:
+// https://cpachecker.sosy-lab.org
+//
+// SPDX-FileCopyrightText: 2007-2022 Dirk Beyer <https://www.sosy-lab.org>
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package org.sosy_lab.cpachecker.cfa.transformer;
+
+import org.sosy_lab.common.ShutdownNotifier;
+import org.sosy_lab.common.log.LogManager;
+import org.sosy_lab.cpachecker.cfa.CFA;
+import org.sosy_lab.cpachecker.cfa.CfaMetadata;
+import org.sosy_lab.cpachecker.cfa.CfaPostProcessor;
+import org.sosy_lab.cpachecker.cfa.graph.CfaNetwork;
+
+/**
+ * A factory that creates new {@link CFA} instances for CFAs represented by ({@link CfaNetwork},
+ * {@link CfaMetadata}) pairs.
+ *
+ * <p>The factory can create multiple CFAs. Every time the {@link CfaFactory#createCfa(CfaNetwork,
+ * CfaMetadata, LogManager, ShutdownNotifier) createCfa} method is invoked, a new {@link CFA}
+ * instance is created.
+ *
+ * <p>{@link CfaFactory} implementations may run {@link CfaPostProcessor CFA post-processors} during
+ * CFA creation.
+ */
+public interface CfaFactory {
+
+  /**
+   * Returns a new CFA for the specified {@link CfaNetwork} and {@link CfaMetadata}.
+   *
+   * <p>Every time this method is invoked, a new {@link CFA} instance is created.
+   *
+   * <p>The returned CFA resembles the CFA represented by the {@link CfaNetwork} as closely as
+   * possible. {@link CfaFactory} implementations may run {@link CfaPostProcessor CFA
+   * post-processors} during CFA creation, which may lead to differences between the specified
+   * {@link CfaNetwork} and returned {@link CFA}.
+   *
+   * @param pCfaNetwork the {@link CfaNetwork} to create a CFA for
+   * @param pCfaMetadata the {@link CfaMetadata} of the specified {@link CfaNetwork}
+   * @param pLogger the logger to use during CFA creation
+   * @param pShutdownNotifier the shutdown notifier to use during CFA creation
+   * @return a new CFA for the specified {@link CfaNetwork} and {@link CfaMetadata}
+   * @throws NullPointerException if any parameter is {@code null}
+   * @throws IllegalArgumentException if the specified {@link CfaNetwork} doesn't represent a valid
+   *     CFA
+   * @throws IllegalArgumentException if the specified {@link CfaMetadata} doesn't fit the CFA
+   *     represented by the {@link CfaNetwork} (e.g., if the {@link
+   *     CfaMetadata#getMainFunctionEntry() function entry node} doesn't exist in the {@link
+   *     CfaNetwork})
+   */
+  CFA createCfa(
+      CfaNetwork pCfaNetwork,
+      CfaMetadata pCfaMetadata,
+      LogManager pLogger,
+      ShutdownNotifier pShutdownNotifier);
+}
