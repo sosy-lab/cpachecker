6b9dd1519174945e35b818c9b9d50b5c5cd543fb
vport <vport@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2023-01-14 16:14:26 +0000

    Add static factory methods for element validators to `CfaValidator`
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/1016-cfa-network-checker@42734 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/cfa/validation/CfaValidator.java
+++ src/org/sosy_lab/cpachecker/cfa/validation/CfaValidator.java
@@ -8,10 +8,15 @@
 
 package org.sosy_lab.cpachecker.cfa.validation;
 
+import static com.google.common.base.Preconditions.checkNotNull;
+
 import com.google.common.collect.ImmutableList;
+import java.util.function.Function;
 import org.sosy_lab.cpachecker.cfa.CFA;
 import org.sosy_lab.cpachecker.cfa.CfaMetadata;
 import org.sosy_lab.cpachecker.cfa.graph.CfaNetwork;
+import org.sosy_lab.cpachecker.cfa.model.CFAEdge;
+import org.sosy_lab.cpachecker.cfa.model.CFANode;
 
 /**
  * A {@link CfaValidator} determines whether a given CFA is valid (i.e., whether a CFA is correct
@@ -43,6 +48,82 @@ public interface CfaValidator {
         }
         return validationResult;
       }
+    };
+  }
+
+  /**
+   * Returns a new {@link CfaValidator} that validates all nodes in a CFA using the specified node
+   * validator.
+   *
+   * @param pNodeValidator the node validator (CFA node -> a {@link CfaValidationResult} that
+   *     indicates whether the node is valid) that is called for every node in a CFA
+   * @return a new {@link CfaValidator} that validates all nodes in a CFA using the specified node
+   *     validator
+   * @throws NullPointerException if {@code pNodeValidator == null} or {@code pNodeValidator}
+   *     returns {@code null}
+   */
+  public static CfaValidator createNodeValidator(
+      Function<CFANode, CfaValidationResult> pNodeValidator) {
+    checkNotNull(pNodeValidator);
+    return (cfaNetwork, cfaMetadata) -> {
+      CfaValidationResult validationResult = CfaValidationResult.VALID;
+      for (CFANode node : cfaNetwork.nodes()) {
+        validationResult = validationResult.combine(pNodeValidator.apply(node));
+      }
+      return validationResult;
+    };
+  }
+
+  /**
+   * Returns a new {@link CfaValidator} that validates all edges in a CFA using the specified edge
+   * validator.
+   *
+   * @param pEdgeValidator the edge validator (CFA edge -> a {@link CfaValidationResult} that
+   *     indicates whether the edge is valid) that is called for every edge in a CFA
+   * @return a new {@link CfaValidator} that validates all edges in a CFA using the specified edge
+   *     validator
+   * @throws NullPointerException if {@code pEdgeValidator == null} or {@code pEdgeValidator}
+   *     returns {@code null}
+   */
+  public static CfaValidator createEdgeValidator(
+      Function<CFAEdge, CfaValidationResult> pEdgeValidator) {
+    checkNotNull(pEdgeValidator);
+    return (cfaNetwork, cfaMetadata) -> {
+      CfaValidationResult validationResult = CfaValidationResult.VALID;
+      for (CFAEdge edge : cfaNetwork.edges()) {
+        validationResult = validationResult.combine(pEdgeValidator.apply(edge));
+      }
+      return validationResult;
+    };
+  }
+
+  /**
+   * Returns a new {@link CfaValidator} that validates all elements in a CFA (i.e., its nodes and
+   * edges) using the specified node and edge validators.
+   *
+   * @param pNodeValidator the node validator (CFA node -> a {@link CfaValidationResult} that
+   *     indicates whether the node is valid) that is called for every node in a CFA
+   * @param pEdgeValidator the edge validator (CFA edge -> a {@link CfaValidationResult} that
+   *     indicates whether the edge is valid) that is called for every edge in a CFA
+   * @return a new {@link CfaValidator} that validates all elements in a CFA (i.e., its nodes and
+   *     edges) using the specified node and edge validators
+   * @throws NullPointerException if any parameter is {@code null} or any of the specified
+   *     validators returns {@code null}
+   */
+  public static CfaValidator createElementValidator(
+      Function<CFANode, CfaValidationResult> pNodeValidator,
+      Function<CFAEdge, CfaValidationResult> pEdgeValidator) {
+    checkNotNull(pNodeValidator);
+    checkNotNull(pEdgeValidator);
+    return (cfaNetwork, cfaMetadata) -> {
+      CfaValidationResult validationResult = CfaValidationResult.VALID;
+      for (CFANode node : cfaNetwork.nodes()) {
+        validationResult = validationResult.combine(pNodeValidator.apply(node));
+        for (CFAEdge edge : cfaNetwork.outEdges(node)) {
+          validationResult = validationResult.combine(pEdgeValidator.apply(edge));
+        }
+      }
+      return validationResult;
     };
   }
 
