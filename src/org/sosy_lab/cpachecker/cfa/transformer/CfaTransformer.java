3f67ac6433de4fe4023f709a3b7cacd82513a67f
vport <vport@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2023-01-14 15:20:34 +0000

    Make sure that at least two CFA transformers are combined
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/cfa-transformer@42731 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/cfa/transformer/CfaTransformer.java
+++ src/org/sosy_lab/cpachecker/cfa/transformer/CfaTransformer.java
@@ -36,17 +36,18 @@ public interface CfaTransformer {
    * first specified transformer and the output of the combined transformer is the output of the
    * last specified transformer.
    *
-   * @param pTransformer the first CFA transformer
-   * @param pTransformers additional CFA transformers executed after the first CFA transformer
+   * @param pFst the first CFA transformer
+   * @param pSnd the second CFA transformer
+   * @param pAdditional additional CFA transformers executed after the second CFA transformer
    * @return a new CFA transformer that is the combination of the specified transformers
-   * @throws NullPointerException if any parameter is {@code null} or {@code pTransformers} has an
+   * @throws NullPointerException if any parameter is {@code null} or {@code pAdditional} has an
    *     element that is {@code null}
    */
   public static CfaTransformer combine(
-      CfaTransformer pTransformer, CfaTransformer... pTransformers) {
-    checkNotNull(pTransformer);
-
-    ImmutableList<CfaTransformer> transformers = ImmutableList.copyOf(pTransformers);
+      CfaTransformer pFst, CfaTransformer pSnd, CfaTransformer... pAdditional) {
+    checkNotNull(pFst);
+    ImmutableList<CfaTransformer> afterFirst =
+        ImmutableList.<CfaTransformer>builder().add(pSnd).add(pAdditional).build();
 
     return new CfaTransformer() {
 
@@ -56,12 +57,10 @@ public interface CfaTransformer {
           CfaMetadata pCfaMetadata,
           LogManager pLogger,
           ShutdownNotifier pShutdownNotifier) {
-        CFA transformedCfa =
-            pTransformer.transform(pCfaNetwork, pCfaMetadata, pLogger, pShutdownNotifier);
-        for (CfaTransformer transformer : transformers) {
+        CFA transformedCfa = pFst.transform(pCfaNetwork, pCfaMetadata, pLogger, pShutdownNotifier);
+        for (CfaTransformer transformer : afterFirst) {
           transformedCfa = transformer.transform(transformedCfa, pLogger, pShutdownNotifier);
         }
-
         return transformedCfa;
       }
     };
