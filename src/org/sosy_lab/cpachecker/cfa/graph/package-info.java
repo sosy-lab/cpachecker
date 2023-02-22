8104c22d2622e675923be9fa080a8368a3947935
vport <vport@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2023-02-22 16:20:17 +0000

    Update outdated `cfa.graph.package-info.java`
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/cfa-network@43061 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/cfa/graph/package-info.java
+++ src/org/sosy_lab/cpachecker/cfa/graph/package-info.java
@@ -15,26 +15,5 @@
  * (i.e., its nodes and edges). This offers a great amount of flexibility as different views of a
  * CFA can be created without actually changing the CFA (e.g., by applying simple on-the-fly filters
  * or transformations).
- *
- * <p>There are two main interfaces:
- *
- * <ul>
- *   <li>{@link org.sosy_lab.cpachecker.cfa.graph.CfaNetwork CfaNetwork} extends {@link
- *       com.google.common.graph.Network Network}: All {@link com.google.common.graph.Network
- *       Network} implementations representing CFAs implement this interface. There are static
- *       factory methods for {@link
- *       org.sosy_lab.cpachecker.cfa.graph.CfaNetwork#wrap(org.sosy_lab.cpachecker.cfa.CFA)
- *       wrapping} existing CFAs and applying simple on-the-fly filters and transformations.
- *   <li>{@link org.sosy_lab.cpachecker.cfa.graph.MutableCfaNetwork MutableCfaNetwork} extends
- *       {@link org.sosy_lab.cpachecker.cfa.graph.CfaNetwork CfaNetwork} and {@link
- *       com.google.common.graph.MutableNetwork MutableNetwork}: The mutable version of a {@link
- *       org.sosy_lab.cpachecker.cfa.graph.CfaNetwork CfaNetwork} that also provides basic methods
- *       for modifying CFAs, like adding and removing nodes/edges. Existing {@link
- *       org.sosy_lab.cpachecker.cfa.MutableCFA MutableCFA} instances can be {@link
- *       org.sosy_lab.cpachecker.cfa.graph.MutableCfaNetwork#wrap(org.sosy_lab.cpachecker.cfa.MutableCFA)
- *       wrapped} to create a {@link org.sosy_lab.cpachecker.cfa.graph.MutableCfaNetwork
- *       MutableCfaNetwork} that modifies the underlying {@link
- *       org.sosy_lab.cpachecker.cfa.MutableCFA MutableCFA}.
- * </ul>
  */
 package org.sosy_lab.cpachecker.cfa.graph;
