bc8571dacd229738f6f37e14e2bb09d8e8e4a80c
mkettl <mkettl@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2023-04-18 13:47:11 +0000

    Make abstraction_safe.c compliant to SV-COMP standards
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/abstraction-with-block-summaries@43506 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- test/programs/block_analysis/abstraction_safe.c
+++ test/programs/block_analysis/abstraction_safe.c
@@ -6,17 +6,25 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
-extern int __VERIFIER_nondet_int();
+extern void abort(void);
+extern void __assert_fail(const char *, const char *, unsigned int, const char *) __attribute__ ((__nothrow__ , __leaf__)) __attribute__ ((__noreturn__));
+void reach_error() { __assert_fail("0", "abstraction_safe.c", 3, "reach_error"); }
+
+void __VERIFIER_assert(int cond) {
+  if (!(cond)) {
+    ERROR: {reach_error();abort();}
+  }
+  return;
+}
+int __VERIFIER_nondet_int();
 
 int main() {
-	int x = 0;
-	int y = 0;
-	while (x < 10000) {
-		x++;
-		y++;
-	}
-  if (x != y) {
-    ERROR: return 1;
+  int x = 0;
+  int y = 0;
+  while (x < 10000) {
+    x++;
+    y++;
   }
-	return 0;
+  __VERIFIER_assert(x == y);
+  return 0;
 }
