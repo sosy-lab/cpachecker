2f12f17590dd595f327195eae7efb0d80c961647
mjankola <mjankola@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2023-08-17 11:46:26 +0000

    Removing backward check
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/dual-approximate-reachability@44291 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/core/algorithm/bmc/DARAlgorithm.java
+++ src/org/sosy_lab/cpachecker/core/algorithm/bmc/DARAlgorithm.java
@@ -323,10 +323,10 @@ public class DARAlgorithm extends AbstractBMCAlgorithm implements Algorithm {
 
     for (int i = 1; i < pDualSequence.getSize(); i++){
       // SSA map went wrong and we computed wrongly backward sequence
-      if (solver.isUnsat(pDualSequence.getBackwardReachVector().get(i))){
-        isDAREnabled = false;
-        return false;
-      }
+      //if (solver.isUnsat(pDualSequence.getBackwardReachVector().get(i))){
+      //  isDAREnabled = false;
+      //  return false;
+      //}
       if (solver.implies(pDualSequence.getBackwardReachVector().get(i), backwardImage)){
         finalFixedPoint = backwardImage;
         return true;
