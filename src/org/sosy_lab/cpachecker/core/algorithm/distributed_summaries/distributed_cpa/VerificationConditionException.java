533dd0eb2e8434a9e9fb83b121be1cee01769623
mkettl <mkettl@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2023-09-14 19:43:18 +0000

    Add serial version UID to new exception
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/abstraction-with-block-summaries@44616 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/core/algorithm/distributed_summaries/distributed_cpa/VerificationConditionException.java
+++ src/org/sosy_lab/cpachecker/core/algorithm/distributed_summaries/distributed_cpa/VerificationConditionException.java
@@ -8,12 +8,17 @@
 
 package org.sosy_lab.cpachecker.core.algorithm.distributed_summaries.distributed_cpa;
 
+import java.io.Serial;
+
 /**
  * This exception is thrown iff a potential counterexample is not feasible, but it could not be
  * recognized by the analysis. Forward analysis in DSS may miss some important context that is only
  * visible when computing the verification condition.
  */
 public class VerificationConditionException extends Exception {
+
+  @Serial
+  private static final long serialVersionUID = -3488942813490840660L;
 
   public VerificationConditionException(String pMessage) {
     super(pMessage);
