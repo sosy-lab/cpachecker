73b2d6101628c32343d2242feeb01a51ddb6f4f3
jhaltermann <jhaltermann@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2023-09-30 17:00:40 +0000

    Added a ranged analysis working with sequences
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/ranged_program_analysis@44825 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

---
+++ src/org/sosy_lab/cpachecker/cpa/rangedAnalysisSequence/SequenceBoundAnalysis/SequenceCPA.java
@@ -0,0 +1,97 @@
+// This file is part of CPAchecker,
+// a tool for configurable software verification:
+// https://cpachecker.sosy-lab.org
+//
+// SPDX-FileCopyrightText: 2021 Dirk Beyer <https://www.sosy-lab.org>
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package org.sosy_lab.cpachecker.cpa.rangedAnalysisSequence.SequenceBoundAnalysis;
+
+import com.google.common.base.Splitter;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+import org.sosy_lab.common.configuration.Configuration;
+import org.sosy_lab.common.configuration.FileOption;
+import org.sosy_lab.common.configuration.InvalidConfigurationException;
+import org.sosy_lab.common.configuration.Option;
+import org.sosy_lab.common.configuration.Options;
+import org.sosy_lab.common.log.LogManager;
+import org.sosy_lab.cpachecker.cfa.CFA;
+import org.sosy_lab.cpachecker.cfa.model.CFANode;
+import org.sosy_lab.cpachecker.core.defaults.AbstractCPA;
+import org.sosy_lab.cpachecker.core.defaults.AutomaticCPAFactory;
+import org.sosy_lab.cpachecker.core.interfaces.AbstractState;
+import org.sosy_lab.cpachecker.core.interfaces.CPAFactory;
+import org.sosy_lab.cpachecker.core.interfaces.StateSpacePartition;
+import org.sosy_lab.cpachecker.core.interfaces.StopOperator;
+import org.sosy_lab.cpachecker.core.interfaces.pcc.ProofChecker.ProofCheckerCPA;
+
+@Options(prefix = "cpa.sequenceCPA")
+public class SequenceCPA extends AbstractCPA implements ProofCheckerCPA {
+
+  @Option(
+      secure = true,
+      description =
+          "The path for the input file to handle input/random values for the constant propagation"
+              + " following the lower bound. If no path is given, there is no lower bound.")
+  @FileOption(FileOption.Type.OPTIONAL_INPUT_FILE)
+  protected Path path2Bound = null;
+
+  @Option(secure = true, description = "Stop if the list of decisions taken is empty.")
+  protected boolean stopIfUnderspecifiedTestcase = false;
+
+  private List<Boolean> decisionNodes = new ArrayList<>();
+
+  public static CPAFactory factory() {
+    return AutomaticCPAFactory.forType(SequenceCPA.class);
+  }
+
+  SequenceCPA(Configuration pConfig, CFA pCFA, LogManager pLogger)
+      throws InvalidConfigurationException {
+    super("sep", "sep", new SequenceTransferRelation(pCFA, pLogger));
+    pConfig.inject(this);
+    if (path2Bound != null) {
+      decisionNodes = readDecisionNodes(path2Bound);
+    }
+  }
+
+  private List<Boolean> readDecisionNodes(Path pPath2Bound) throws InvalidConfigurationException {
+    List<Boolean> result = new ArrayList<>();
+    try {
+      for (String line : Files.readAllLines(pPath2Bound, Charset.defaultCharset())) {
+        List<String> elems = Splitter.on(',').splitToList(line);
+        if (elems.size() == 2) {
+          result.add(elems.get(1).equals("true"));
+        } else {
+          throw new InvalidConfigurationException(
+              "Excepting lines to be pair of line number and decision");
+        }
+      }
+    } catch (IOException pE) {
+      throw new InvalidConfigurationException(
+          "Could not read decision nodes from file " + pPath2Bound, pE);
+    }
+    return result;
+  }
+
+  @Override
+  public SequenceTransferRelation getTransferRelation() {
+    assert super.getTransferRelation() instanceof SequenceTransferRelation;
+    return (SequenceTransferRelation) super.getTransferRelation();
+  }
+
+  @Override
+  public StopOperator getStopOperator() {
+    return super.getStopOperator();
+  }
+
+  @Override
+  public AbstractState getInitialState(CFANode pNode, StateSpacePartition pPartition) {
+    return new SequenceState(decisionNodes, stopIfUnderspecifiedTestcase);
+  }
+}
---
+++ src/org/sosy_lab/cpachecker/cpa/rangedAnalysisSequence/SequenceBoundAnalysis/SequenceState.java
@@ -0,0 +1,127 @@
+// This file is part of CPAchecker,
+// a tool for configurable software verification:
+// https://cpachecker.sosy-lab.org
+//
+// SPDX-FileCopyrightText: 2021 Dirk Beyer <https://www.sosy-lab.org>
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package org.sosy_lab.cpachecker.cpa.rangedAnalysisSequence.SequenceBoundAnalysis;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+import org.checkerframework.checker.nullness.qual.NonNull;
+import org.sosy_lab.cpachecker.cfa.model.AssumeEdge;
+import org.sosy_lab.cpachecker.cfa.model.CFAEdge;
+import org.sosy_lab.cpachecker.core.defaults.LatticeAbstractState;
+import org.sosy_lab.cpachecker.core.interfaces.Graphable;
+import org.sosy_lab.cpachecker.core.interfaces.Targetable;
+import org.sosy_lab.cpachecker.exceptions.CPAException;
+
+public class SequenceState implements LatticeAbstractState<SequenceState>, Targetable, Graphable {
+
+  private final List<Boolean> remainingDecisionsToTake;
+  private final boolean stopIfUnderspecifiedTestcase;
+
+  public SequenceState(List<Boolean> pDecisionNodes, boolean pStopIfUnderspecifiedTestcase) {
+    this.remainingDecisionsToTake = pDecisionNodes;
+    this.stopIfUnderspecifiedTestcase = pStopIfUnderspecifiedTestcase;
+  }
+
+  SequenceState copy() {
+    return new SequenceState(
+        new ArrayList<>(remainingDecisionsToTake), stopIfUnderspecifiedTestcase);
+  }
+
+  @Override
+  public boolean isTarget() {
+    return false;
+  }
+
+  @Override
+  public @NonNull Set<TargetInformation> getTargetInformation() throws IllegalStateException {
+    return new HashSet<>();
+  }
+
+  @Override
+  public String toString() {
+    return remainingDecisionsToTake.toString();
+  }
+
+  @Override
+  public boolean equals(Object pO) {
+    if (this == pO) {
+      return true;
+    }
+    if (!(pO instanceof SequenceState)) {
+      return false;
+    }
+    SequenceState that = (SequenceState) pO;
+    return Objects.equals(remainingDecisionsToTake, that.remainingDecisionsToTake);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(remainingDecisionsToTake);
+  }
+
+  @Override
+  public String toDOTLabel() {
+    return toString();
+  }
+
+  @Override
+  public boolean shouldBeHighlighted() {
+    return false;
+  }
+
+  @Override
+  public SequenceState join(SequenceState other) throws CPAException, InterruptedException {
+    if (other.isLessOrEqual(this)) {
+      return this;
+    } else return other;
+  }
+
+  @Override
+  public boolean isLessOrEqual(SequenceState other) {
+    if (remainingDecisionsToTake.size() > other.remainingDecisionsToTake.size()) {
+      return false;
+    }
+    for (int i = 0; i < remainingDecisionsToTake.size(); i++) {
+      if (!Objects.equals(remainingDecisionsToTake.get(i), other.remainingDecisionsToTake.get(i))) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  public boolean thisEdgeShouldBeTaken(AssumeEdge pCfaEdge) {
+
+    if (remainingDecisionsToTake.size() == 0) {
+      // Do not take the edge if there are no decisions left to take and we should stop if the
+      // testcase is underspecified.
+      return !stopIfUnderspecifiedTestcase;
+    }
+    if (remainingDecisionsToTake.get(0) == pCfaEdge.getTruthAssumption()
+        || (remainingDecisionsToTake.get(0) != pCfaEdge.getTruthAssumption()
+            && pCfaEdge.isSwapped())) {
+      return true;
+    }
+    return false;
+  }
+
+  public SequenceState takeEdge(CFAEdge pCfaEdge) {
+    if (pCfaEdge instanceof AssumeEdge) {
+      AssumeEdge assumeEdge = (AssumeEdge) pCfaEdge;
+      if (thisEdgeShouldBeTaken(assumeEdge) && remainingDecisionsToTake.size() > 0) {
+        return new SequenceState(
+            remainingDecisionsToTake.subList(1, remainingDecisionsToTake.size()),
+            stopIfUnderspecifiedTestcase);
+      }
+    }
+    return copy();
+  }
+}
