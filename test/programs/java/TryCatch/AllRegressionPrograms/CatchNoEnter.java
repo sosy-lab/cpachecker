f482be0f5c0194d6f0ffb695f2ad536036c2adb5
bdamboeck <bdamboeck@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2023-10-28 08:06:04 +0000

    added workaround
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/javaexceptions@44956 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

---
+++ test/programs/java/TryCatch/AllRegressionPrograms/CatchNoEnter.java
@@ -0,0 +1,20 @@
+public class CatchNoEnter {
+    private static int i = 0;
+    private static boolean entered = false;
+
+    private static void f() {
+        i++;
+    }
+
+    public static void main(String[] args) {
+        try {
+            while (i < 10) {
+                f();
+                i++;
+            }
+        } catch (RuntimeException e) {
+            entered = true;
+        }     
+        assert !entered;
+    }
+}
---
+++ test/programs/java/TryCatch/AllRegressionPrograms/CorrectExceptionDeclaration.java
@@ -0,0 +1,18 @@
+public class CorrectExceptionDeclaration {
+    private static int i = 0;
+
+    private static void f() {
+        throw new RuntimeException(); 
+    }
+
+    public static void main(String[] args) {
+        try {
+            while (i < 10) {
+                f();
+                i++;
+            }
+        } catch (RuntimeException e) {
+            assert e instanceof RuntimeException;
+        }     
+    }
+}
