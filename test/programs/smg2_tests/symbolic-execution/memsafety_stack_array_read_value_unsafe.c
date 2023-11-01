6208a93302b4aaa7f62de950b27808574e70f9a3
dbaier <dbaier@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2023-11-01 15:16:46 +0000

    MemorySafety: More test c files for symb exec memsafety smg2
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@44985 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

---
+++ test/programs/smg2_tests/symbolic-execution/memsafety_stack_array_read_value_unsafe.c
@@ -0,0 +1,31 @@
+// This file is part of CPAchecker,
+// a tool for configurable software verification:
+// https://cpachecker.sosy-lab.org
+//
+// SPDX-FileCopyrightText: 2023 Dirk Beyer <https://www.sosy-lab.org>
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#include <stdlib.h>
+#include <assert.h>
+#include <stdio.h>
+
+extern int __VERIFIER_nondet_int(void);
+extern void abort(void);
+
+int main() {
+
+    int array[10] = {1,2,3,4,5,6,7,8,9,10};
+
+    int nondet = __VERIFIER_nondet_int();
+
+    if (nondet > 9 || nondet < 0) {
+        return 0;
+    }
+
+    int unknown_value = array[nondet]; // SAFE
+
+    assert(unknown_value != 10);  // UNSAFE as it might be 10
+    
+	return 0;	
+}
