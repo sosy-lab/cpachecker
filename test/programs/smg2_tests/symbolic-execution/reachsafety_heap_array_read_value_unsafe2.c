41e03c7e963577cadefaecee3d6cada956b526ab
dbaier <dbaier@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2023-11-02 10:15:52 +0000

    MemorySafety: Add 2 more test c files and rename 2
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@44988 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- test/programs/smg2_tests/symbolic-execution/reachsafety_heap_array_read_value_unsafe2.c
+++ test/programs/smg2_tests/symbolic-execution/reachsafety_heap_array_read_value_unsafe2.c
@@ -15,17 +15,24 @@ extern void abort(void);
 
 int main() {
 
-    int array[10] = {1,2,3,4,5,6,7,8,9,10};
+    int * array = malloc(10 * sizeof(int));
+
+    for (int i = 0; i < 10; i++) {
+        array[i] = i + 1;
+    }
 
     int nondet = __VERIFIER_nondet_int();
 
     if (nondet > 9 || nondet < 0) {
+        free(array);
         return 0;
     }
 
-    int unknown_value = array[nondet]; // SAFE
+    array[nondet] = 5; // SAFE, but makes all array values unknown
 
-    assert(unknown_value != 10);  // UNSAFE as it might be 10
-    
+    assert(array[0] != 5);  // UNSAFE as it might be 5
+    free(array);
+
+    // SAFE for MemSafety, UNSAFE for ReachSafety
 	return 0;	
 }
--- test/programs/smg2_tests/symbolic-execution/reachsafety_stack_array_read_value_unsafe1.c
+++ test/programs/smg2_tests/symbolic-execution/reachsafety_stack_array_read_value_unsafe1.c
@@ -15,22 +15,18 @@ extern void abort(void);
 
 int main() {
 
-    int * array = malloc(10 * sizeof(int));
-
-    for (int i = 0; i < 10; i++) {
-        array[i] = i + 1;
-    }
+    int array[10] = {1,2,3,4,5,6,7,8,9,10};
 
     int nondet = __VERIFIER_nondet_int();
 
     if (nondet > 9 || nondet < 0) {
-        free(array);
         return 0;
     }
 
     int unknown_value = array[nondet]; // SAFE
 
     assert(unknown_value != 10);  // UNSAFE as it might be 10
-    free(array);
+    
+    // SAFE for MemSafety, UNSAFE for ReachSafety
 	return 0;	
 }
