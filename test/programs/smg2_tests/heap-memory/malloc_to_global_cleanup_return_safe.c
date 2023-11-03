d6ce2f54909823bf0ce26644fcd6c2b5d905af2c
dbaier <dbaier@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2023-11-03 18:55:40 +0000

    MemSafety: Improve description of simple valid-memtrack tests
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@45019 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- test/programs/smg2_tests/heap-memory/malloc_to_global_cleanup_return_safe.c
+++ test/programs/smg2_tests/heap-memory/malloc_to_global_cleanup_return_safe.c
@@ -4,5 +4,5 @@ extern void *malloc (size_t __size);
 int *x = ((void *)0);
 int main() {
   x = malloc(3);
-  return 0;
+  return 0;  // VALGRIND says all memory is reachable (from outside of main), so this is safe
 }
--- test/programs/smg2_tests/heap-memory/malloc_to_local_cleanup_return_0_unsafe.c
+++ test/programs/smg2_tests/heap-memory/malloc_to_local_cleanup_return_0_unsafe.c
@@ -1,7 +1,9 @@
+#include <stdlib.h>
+
 typedef long unsigned int size_t;
 extern void *malloc (size_t __size);
 
 int main() {
   int *x = malloc(3);
-  return 0;
+  return 0;  // VALGRIND argues that the memory is lost -> valid-memtrack == false
 }
