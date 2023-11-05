2c8afbeaf66f4b168ae23557305880d1c5ad1e79
dbaier <dbaier@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2023-11-05 11:45:16 +0000

    MemSafety: Test for copying of local structs that are given to function calls via a pointer expression
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@45023 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

---
+++ test/programs/smg2_tests/const_pointer_of_struct_as_function_arg.c
@@ -0,0 +1,42 @@
+// This file is part of CPAchecker,
+// a tool for configurable software verification:
+// https://cpachecker.sosy-lab.org
+//
+// SPDX-FileCopyrightText: 2023 Dirk Beyer <https://www.sosy-lab.org>
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#include <stdlib.h>
+#include <assert.h>
+
+struct Foo {
+  int temp;
+  int *p;
+};
+
+int bar(struct Foo *pa) {
+  int value = blub(*pa);
+  return value;
+}
+
+int blub(struct Foo pap) {
+  struct Foo f = pap;
+  return *(f.p);
+}
+
+int main() {
+  int a = 0;
+  struct Foo f;
+  f.temp = 1;
+  f.p = &a;
+  struct Foo * fooPtr = &f;
+
+  int bla;
+  bla = (*fooPtr).temp;
+  assert(bla == 1);
+
+  int res = bar(fooPtr);
+
+  assert(res == a); // SAFE
+  return 0;
+}
