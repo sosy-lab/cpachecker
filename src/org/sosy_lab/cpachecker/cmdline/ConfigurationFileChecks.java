79363ae58acf93e95ae81f71a40166f291309638
dbaier <dbaier@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2023-11-20 17:59:39 +0000

    MemSafety: Update svcomp24 config to replace SMG by SMG2 and change config checks to properly check SMG2
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@45221 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/cmdline/ConfigurationFileChecks.java
+++ src/org/sosy_lab/cpachecker/cmdline/ConfigurationFileChecks.java
@@ -431,6 +431,16 @@ public class ConfigurationFileChecks {
           assertThat(spec).contains("specification/memorysafety.spc");
         }
       }
+    } else if (cpas.contains("cpa.smg2.SMGCPA")) {
+      if (isSvcompConfig) {
+        assertThat(spec).matches(".*specification/sv-comp-memory(cleanup|safety).spc$");
+      } else {
+        if (spec.contains("memorycleanup")) {
+          assertThat(spec).contains("specification/memorycleanup.spc");
+        } else if (spec.contains("memorysafety")) {
+          assertThat(spec).contains("specification/memorysafety.spc");
+        }
+      }
     } else if (basePath.toString().startsWith("ldv")) {
       assertThat(spec).endsWith("specification/sv-comp-errorlabel.spc");
     } else if (isSvcompConfig) {
