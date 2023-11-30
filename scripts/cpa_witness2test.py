db1ae91de9a4b4b21cde1d5a99fa40527a3622b4
pwendler <pwendler@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2023-11-30 10:59:06 +0000

    fix indentation in Python script
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@45382 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- scripts/cpa_witness2test.py
+++ scripts/cpa_witness2test.py
@@ -291,16 +291,16 @@ def _create_cpachecker_args(args, harness_output_dir):
 
     cpachecker_args.append("-witness2test")
     try:
-      index_of_outputpath_param = cpachecker_args.index("-outputpath")
+        index_of_outputpath_param = cpachecker_args.index("-outputpath")
     except ValueError:
         cpachecker_args += ["-outputpath", harness_output_dir]
     else:
-      assert (index_of_outputpath_param + 1) < len(cpachecker_args), (
-          "Parameter -outputpath is missing an argument: " + cpachecker_args
-      )
-      # Replace existing argument of outputpath with harness_output_dir,
-      # so that the harness is written there for compilation.
-      cpachecker_args[index_of_outputpath_param + 1] = harness_output_dir
+        assert (index_of_outputpath_param + 1) < len(cpachecker_args), (
+            "Parameter -outputpath is missing an argument: " + cpachecker_args
+        )
+        # Replace existing argument of outputpath with harness_output_dir,
+        # so that the harness is written there for compilation.
+        cpachecker_args[index_of_outputpath_param + 1] = harness_output_dir
 
     return cpachecker_args
 
