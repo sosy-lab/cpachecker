ab0bda723ca4dac30cde4b22911f2062b08ba2f9
pwendler <pwendler@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2023-12-12 17:37:39 +0000

    Remove duplicate code from ICCOperatorFactory
    For most operators the strategy is to apply the operation to each
    interval in the compound interval and then union the results.
    We only need this code once.
    
    This is basically a temporary commit that only serves to illustrate
    an intermediate step in a bigger refactoring,
    such that it is easier to see that each of the steps is correct.
    The next commit will further refactor the code from this commit.
    
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@45486 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/cpa/invariants/operators/bitvector/ICCOperatorFactory.java
+++ src/org/sosy_lab/cpachecker/cpa/invariants/operators/bitvector/ICCOperatorFactory.java
@@ -28,10 +28,10 @@ public enum ICCOperatorFactory {
         pSecondOperand.add(pFirstOperand, pAllowSignedWrapAround, pOverflowEventHandler);
   }
 
-  /** The division operator for dividing simple intervals by compound states. */
-  public Operator<BitVectorInterval, CompoundBitVectorInterval, CompoundBitVectorInterval>
-      getDivide(
-          final boolean pAllowSignedWrapAround, final OverflowEventHandler pOverflowEventHandler) {
+  private Operator<BitVectorInterval, CompoundBitVectorInterval, CompoundBitVectorInterval>
+      applyOperationToAllAndUnite(
+          final Operator<BitVectorInterval, BitVectorInterval, CompoundBitVectorInterval>
+              operator) {
     return new Operator<>() {
 
       @Override
@@ -40,10 +40,7 @@ public enum ICCOperatorFactory {
         CompoundBitVectorInterval result =
             CompoundBitVectorInterval.bottom(pFirstOperand.getTypeInfo());
         for (BitVectorInterval interval : pSecondOperand.getBitVectorIntervals()) {
-          CompoundBitVectorInterval current =
-              IICOperatorFactory.INSTANCE
-                  .getDivide(pAllowSignedWrapAround, pOverflowEventHandler)
-                  .apply(pFirstOperand, interval);
+          CompoundBitVectorInterval current = operator.apply(pFirstOperand, interval);
           if (current != null) {
             result = result.unionWith(current);
             if (result.containsAllPossibleValues()) {
@@ -56,32 +53,20 @@ public enum ICCOperatorFactory {
     };
   }
 
+  /** The division operator for dividing simple intervals by compound states. */
+  public Operator<BitVectorInterval, CompoundBitVectorInterval, CompoundBitVectorInterval>
+      getDivide(
+          final boolean pAllowSignedWrapAround, final OverflowEventHandler pOverflowEventHandler) {
+    return applyOperationToAllAndUnite(
+        IICOperatorFactory.INSTANCE.getDivide(pAllowSignedWrapAround, pOverflowEventHandler));
+  }
+
   /** The modulo operator for computing the remainders of dividing intervals by compound states. */
   public Operator<BitVectorInterval, CompoundBitVectorInterval, CompoundBitVectorInterval>
       getModulo(
           final boolean pAllowSignedWrapAround, final OverflowEventHandler pOverflowEventHandler) {
-    return new Operator<>() {
-
-      @Override
-      public CompoundBitVectorInterval apply(
-          BitVectorInterval pFirstOperand, CompoundBitVectorInterval pSecondOperand) {
-        CompoundBitVectorInterval result =
-            CompoundBitVectorInterval.bottom(pFirstOperand.getTypeInfo());
-        for (BitVectorInterval interval : pSecondOperand.getBitVectorIntervals()) {
-          CompoundBitVectorInterval current =
-              IICOperatorFactory.INSTANCE
-                  .getModulo(pAllowSignedWrapAround, pOverflowEventHandler)
-                  .apply(pFirstOperand, interval);
-          if (current != null) {
-            result = result.unionWith(current);
-            if (result.containsAllPossibleValues()) {
-              return result;
-            }
-          }
-        }
-        return result;
-      }
-    };
+    return applyOperationToAllAndUnite(
+        IICOperatorFactory.INSTANCE.getModulo(pAllowSignedWrapAround, pOverflowEventHandler));
   }
 
   /** The multiplication operator for multiplying simple intervals with compound states. */
@@ -96,55 +81,15 @@ public enum ICCOperatorFactory {
   public Operator<BitVectorInterval, CompoundBitVectorInterval, CompoundBitVectorInterval>
       getShiftLeft(
           final boolean pAllowSignedWrapAround, final OverflowEventHandler pOverflowEventHandler) {
-    return new Operator<>() {
-
-      @Override
-      public CompoundBitVectorInterval apply(
-          BitVectorInterval pFirstOperand, CompoundBitVectorInterval pSecondOperand) {
-        CompoundBitVectorInterval result =
-            CompoundBitVectorInterval.bottom(pFirstOperand.getTypeInfo());
-        for (BitVectorInterval interval : pSecondOperand.getBitVectorIntervals()) {
-          CompoundBitVectorInterval current =
-              IICOperatorFactory.INSTANCE
-                  .getShiftLeft(pAllowSignedWrapAround, pOverflowEventHandler)
-                  .apply(pFirstOperand, interval);
-          if (current != null) {
-            result = result.unionWith(current);
-            if (result.containsAllPossibleValues()) {
-              return result;
-            }
-          }
-        }
-        return result;
-      }
-    };
+    return applyOperationToAllAndUnite(
+        IICOperatorFactory.INSTANCE.getShiftLeft(pAllowSignedWrapAround, pOverflowEventHandler));
   }
 
   /** The right shift operator for right shifting simple intervals by compound states. */
   public Operator<BitVectorInterval, CompoundBitVectorInterval, CompoundBitVectorInterval>
       getShiftRight(
           final boolean pAllowSignedWrapAround, final OverflowEventHandler pOverflowEventHandler) {
-    return new Operator<>() {
-
-      @Override
-      public CompoundBitVectorInterval apply(
-          BitVectorInterval pFirstOperand, CompoundBitVectorInterval pSecondOperand) {
-        CompoundBitVectorInterval result =
-            CompoundBitVectorInterval.bottom(pFirstOperand.getTypeInfo());
-        for (BitVectorInterval interval : pSecondOperand.getBitVectorIntervals()) {
-          CompoundBitVectorInterval current =
-              IICOperatorFactory.INSTANCE
-                  .getShiftRight(pAllowSignedWrapAround, pOverflowEventHandler)
-                  .apply(pFirstOperand, interval);
-          if (current != null) {
-            result = result.unionWith(current);
-            if (result.containsAllPossibleValues()) {
-              return result;
-            }
-          }
-        }
-        return result;
-      }
-    };
+    return applyOperationToAllAndUnite(
+        IICOperatorFactory.INSTANCE.getShiftRight(pAllowSignedWrapAround, pOverflowEventHandler));
   }
 }
