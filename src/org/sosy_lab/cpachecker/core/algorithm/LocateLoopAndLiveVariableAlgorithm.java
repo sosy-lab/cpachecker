6d9fff84945e2ce1f61db146e3651cb3850ecc52
txia <txia@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2023-12-16 15:42:35 +0000

    Create Class LocateLoopAndLiveVariableAlgorithm.java
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/locate-loop-and-live-variables@45498 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

---
+++ src/org/sosy_lab/cpachecker/core/algorithm/LocateLoopAndLiveVariableAlgorithm.java
@@ -0,0 +1,13 @@
+package org.sosy_lab.cpachecker.core.algorithm;
+
+import org.sosy_lab.cpachecker.core.reachedset.ReachedSet;
+import org.sosy_lab.cpachecker.exceptions.CPAException;
+
+public class LocateLoopAndLiveVariableAlgorithm implements Algorithm {
+
+  @Override
+  public AlgorithmStatus run(ReachedSet pReachedSet) throws CPAException, InterruptedException {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'run'");
+  }
+}
