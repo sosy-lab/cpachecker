748999a4aa253ac507b346ca33ebd07e102f942e
txia <txia@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2023-12-16 15:57:46 +0000

    Add attributes
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/locate-loop-and-live-variables@45499 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/core/algorithm/LocateLoopAndLiveVariableAlgorithm.java
+++ src/org/sosy_lab/cpachecker/core/algorithm/LocateLoopAndLiveVariableAlgorithm.java
@@ -1,9 +1,19 @@
 package org.sosy_lab.cpachecker.core.algorithm;
 
+import org.sosy_lab.common.log.LogManager;
+import org.sosy_lab.cpachecker.cfa.CFA;
+import org.sosy_lab.cpachecker.cfa.CProgramScope;
 import org.sosy_lab.cpachecker.core.reachedset.ReachedSet;
 import org.sosy_lab.cpachecker.exceptions.CPAException;
 
 public class LocateLoopAndLiveVariableAlgorithm implements Algorithm {
+  private final CFA cfa;
+  private final CProgramScope cProgramScope;
+
+  public LocateLoopAndLiveVariableAlgorithm(CFA pCfa, LogManager pLogger) {
+    cfa = pCfa;
+    cProgramScope = new CProgramScope(pCfa, pLogger);
+  }
 
   @Override
   public AlgorithmStatus run(ReachedSet pReachedSet) throws CPAException, InterruptedException {
