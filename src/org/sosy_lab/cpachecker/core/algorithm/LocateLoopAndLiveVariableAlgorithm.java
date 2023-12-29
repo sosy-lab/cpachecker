9818576b4f08c0d859778f1cd1a3d3cfc0d677f2
txia <txia@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2023-12-29 08:19:09 +0000

    Add constraints to parameter of constructor To check if the program has no loop or unsupported loop structures
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/locate-loop-and-live-variables@45516 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/core/algorithm/LocateLoopAndLiveVariableAlgorithm.java
+++ src/org/sosy_lab/cpachecker/core/algorithm/LocateLoopAndLiveVariableAlgorithm.java
@@ -5,12 +5,18 @@ import org.sosy_lab.cpachecker.cfa.CFA;
 import org.sosy_lab.cpachecker.cfa.CProgramScope;
 import org.sosy_lab.cpachecker.core.reachedset.ReachedSet;
 import org.sosy_lab.cpachecker.exceptions.CPAException;
+import org.sosy_lab.cpachecker.util.LoopStructure;
 
 public class LocateLoopAndLiveVariableAlgorithm implements Algorithm {
   private final CFA cfa;
   private final CProgramScope cProgramScope;
 
   public LocateLoopAndLiveVariableAlgorithm(CFA pCfa, LogManager pLogger) {
+    if (!LoopStructure.getRecursions(pCfa).isEmpty()) {
+      throw new IllegalArgumentException("Program should not have recursion!");
+    } else if (pCfa.getLoopStructure().orElseThrow().getAllLoops().isEmpty()) {
+      throw new IllegalArgumentException("Program must have loop!");
+    }
     cfa = pCfa;
     cProgramScope = new CProgramScope(pCfa, pLogger);
   }
