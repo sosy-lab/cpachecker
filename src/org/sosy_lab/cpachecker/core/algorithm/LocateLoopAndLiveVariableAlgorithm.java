2352d8290179ed86eb04de8b3ac72421c182149e
txia <txia@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2023-12-29 08:30:36 +0000

    Add method getVariablesFromAAstNode
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/locate-loop-and-live-variables@45519 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/core/algorithm/LocateLoopAndLiveVariableAlgorithm.java
+++ src/org/sosy_lab/cpachecker/core/algorithm/LocateLoopAndLiveVariableAlgorithm.java
@@ -1,11 +1,22 @@
 package org.sosy_lab.cpachecker.core.algorithm;
 
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 import org.sosy_lab.common.log.LogManager;
 import org.sosy_lab.cpachecker.cfa.CFA;
 import org.sosy_lab.cpachecker.cfa.CProgramScope;
+import org.sosy_lab.cpachecker.cfa.ast.AAstNode;
+import org.sosy_lab.cpachecker.cfa.ast.c.CExpression;
+import org.sosy_lab.cpachecker.cfa.ast.c.CExpressionAssignmentStatement;
+import org.sosy_lab.cpachecker.cfa.ast.c.CExpressionStatement;
+import org.sosy_lab.cpachecker.cfa.ast.c.CFunctionCallAssignmentStatement;
+import org.sosy_lab.cpachecker.cfa.ast.c.CFunctionCallExpression;
+import org.sosy_lab.cpachecker.cfa.ast.c.CFunctionCallStatement;
+import org.sosy_lab.cpachecker.cfa.ast.c.CLeftHandSide;
 import org.sosy_lab.cpachecker.core.reachedset.ReachedSet;
 import org.sosy_lab.cpachecker.exceptions.CPAException;
+import org.sosy_lab.cpachecker.util.CFAUtils;
 import org.sosy_lab.cpachecker.util.LoopStructure;
 
 public class LocateLoopAndLiveVariableAlgorithm implements Algorithm {
@@ -26,6 +37,47 @@ public class LocateLoopAndLiveVariableAlgorithm implements Algorithm {
   public AlgorithmStatus run(ReachedSet pReachedSet) throws CPAException, InterruptedException {
     // TODO Auto-generated method stub
     throw new UnsupportedOperationException("Unimplemented method 'run'");
+  }
+
+  private Set<String> getVariablesFromAAstNode(AAstNode aAstNode) {
+    Set<String> variables = new HashSet<>();
+
+    if (aAstNode instanceof CExpression) {
+      CFAUtils.getVariableNamesOfExpression(((CExpression) aAstNode))
+          .forEach(e -> variables.add(e));
+
+    } else if (aAstNode instanceof CExpressionStatement) {
+      CExpression cExpression = ((CExpressionStatement) aAstNode).getExpression();
+      CFAUtils.getVariableNamesOfExpression(cExpression).forEach(e -> variables.add(e));
+
+    } else if (aAstNode instanceof CExpressionAssignmentStatement) {
+      CLeftHandSide cLeftHandSide = ((CExpressionAssignmentStatement) aAstNode).getLeftHandSide();
+      CFAUtils.getVariableNamesOfExpression(cLeftHandSide).forEach(e -> variables.add(e));
+
+      CExpression cRightHandSide = ((CExpressionAssignmentStatement) aAstNode).getRightHandSide();
+      CFAUtils.getVariableNamesOfExpression(cRightHandSide).forEach(e -> variables.add(e));
+
+    } else if (aAstNode instanceof CFunctionCallStatement) {
+      CFunctionCallStatement cFunctionCallStatement = (CFunctionCallStatement) aAstNode;
+      cFunctionCallStatement
+          .getFunctionCallExpression()
+          .getParameterExpressions()
+          .forEach(e -> CFAUtils.getVariableNamesOfExpression(e).forEach(n -> variables.add(n)));
+
+    } else if (aAstNode instanceof CFunctionCallAssignmentStatement) {
+      CFunctionCallAssignmentStatement cFunctionCallAssignmentStatement =
+          (CFunctionCallAssignmentStatement) aAstNode;
+
+      CLeftHandSide cLeftHandSide = cFunctionCallAssignmentStatement.getLeftHandSide();
+      CFAUtils.getVariableNamesOfExpression(cLeftHandSide).forEach(e -> variables.add(e));
+
+      CFunctionCallExpression cRightHandSide = cFunctionCallAssignmentStatement.getRightHandSide();
+      cRightHandSide
+          .getParameterExpressions()
+          .forEach(e -> CFAUtils.getVariableNamesOfExpression(e).forEach(n -> variables.add(n)));
+    }
+
+    return variables;
   }
 }
 
