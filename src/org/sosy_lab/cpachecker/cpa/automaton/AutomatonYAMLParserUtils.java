c149e47cfb24885e3dc363727d6901cf5b4d8393
mlingsch <mlingsch@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2024-02-01 15:29:36 +0000

    Update documentation according to styleguide
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/direct-witness-export@45642 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/cpa/automaton/AutomatonYAMLParserUtils.java
+++ src/org/sosy_lab/cpachecker/cpa/automaton/AutomatonYAMLParserUtils.java
@@ -69,10 +69,14 @@ public class AutomatonYAMLParserUtils {
   }
 
   /**
+   * Returns the offsets for the given file. If the file is not found the offsets for the file with
+   * the largest matching suffix will be returned.
+   *
    * @param pOffsetsByFile The Map containing the currently known offsets by file. This map will be
    *     updated if the file is not found with the best known match.
    * @param pFile The file for which to get the offsets.
    * @return The offsets for the given file. If the file is not found the offsets for the file with
+   *     the largest matching suffix will be returned.
    */
   public static List<Integer> getOffsetsByFileSimilarity(
       ListMultimap<String, Integer> pOffsetsByFile, String pFile) {
