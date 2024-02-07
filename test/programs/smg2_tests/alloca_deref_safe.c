5961d664a3f5636e89791f6e5b9567767263f0a7
dbaier <dbaier@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2024-02-07 14:21:17 +0000

    SMG: Update alloca examples for memsafety
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@45652 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- test/programs/smg2_tests/alloca_deref_safe.c
+++ test/programs/smg2_tests/alloca_deref_safe.c
@@ -9,17 +9,13 @@
 #include <assert.h>
 #include <alloca.h>
 
-int * fun() {
+int main() {
   int *ptr = 0;
+  {
   ptr = alloca(10*sizeof(int));
 
   *ptr = 5;
   assert(*ptr == 5);
-  return ptr;
-}
-
-int main() {
-  int *ptr = 0;
-  ptr = fun();
-  assert(*ptr == 5); // Unsafe, alloca cleans up after the function returns OR a longjmp was used that skipped over the return
+  }
+  assert(*ptr == 5); // Safe, alloca only cleans up after the function returns OR a longjmp was used that skipped over the return
 }
