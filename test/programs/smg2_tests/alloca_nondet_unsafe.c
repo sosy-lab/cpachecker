4ffc8c9f2e751d72b34c6cfc7560b0e0100fcf2c
dbaier <dbaier@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2024-02-07 13:06:27 +0000

    SMG: Add alloca example c files
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@45650 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

---
+++ test/programs/smg2_tests/alloca_nondet_unsafe.c
@@ -0,0 +1,21 @@
+// This file is part of CPAchecker,
+// a tool for configurable software verification:
+// https://cpachecker.sosy-lab.org
+//
+// SPDX-FileCopyrightText: 2023 Dirk Beyer <https://www.sosy-lab.org>
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#include <assert.h>
+#include <alloca.h>
+
+extern int __VERIFIER_nondet_int(void);
+
+int main() {
+  {
+  int *ptr = alloca(10*sizeof(int));
+
+  *ptr = __VERIFIER_nondet_int();
+  assert(*ptr == __VERIFIER_nondet_int()); // Unsafe
+  }
+}
