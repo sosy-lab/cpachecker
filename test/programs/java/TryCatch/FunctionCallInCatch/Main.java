72d71f5011a7ae699b6fab910218adc50b83c82d
pwendler <pwendler@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2024-02-22 16:26:15 +0000

    Add more test cases that highlight bugs
    Exceptions are not properly handled if there is a function call
    in a catch block or a try block.
    Empty try blocks also seem to lead to wrong finally handling.
    
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/javaexceptions@45806 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

---
+++ test/programs/java/TryCatch/FunctionCallInCatch/Main.java
@@ -0,0 +1,22 @@
+// This file is part of CPAchecker,
+// a tool for configurable software verification:
+// https://cpachecker.sosy-lab.org
+//
+// SPDX-FileCopyrightText: 2020 Dirk Beyer <https://www.sosy-lab.org>
+//
+// SPDX-License-Identifier: Apache-2.0
+
+public class Main {
+  private static void f() {
+    throw new RuntimeException();
+  }
+
+  public static void main(String[] args) {
+    try {
+      f();
+    } catch (RuntimeException e) {
+      f();
+    }
+    assert false;
+  }
+}
