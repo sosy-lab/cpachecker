7ef3c1bfa7f44a9d89f4a76a24fbcf146c44ec8f
draffler <draffler@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2024-03-07 17:17:55 +0000

    FloatingPoint: Fixed powTo in JFloat and JDouble to be in line with the IEEE standard.
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/fix_floatingpoint_calculator@46063 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/util/floatingpoint/JDouble.java
+++ src/org/sosy_lab/cpachecker/util/floatingpoint/JDouble.java
@@ -122,6 +122,9 @@ public class JDouble extends CFloat {
 
   @Override
   public CFloat powTo(CFloat exponent) {
+    if ((isOne() && !isNegative()) || isOne() && exponent.isInfinity()) {
+      return new JDouble(1.0d);
+    }
     return new JDouble(Math.pow(value, toDouble(exponent.getWrapper())));
   }
 
