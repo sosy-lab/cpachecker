375b57a63b97da670c9fc8243fca56b6d12ea4b6
dbaier <dbaier@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2024-03-19 15:27:35 +0000

    SMG: Remove preconditions that check that a write or read are valid (due to allowing symbolic sizes this is now only checked at a higher level in the state or evaluator)
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@46152 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/util/smg/SMG.java
+++ src/org/sosy_lab/cpachecker/util/smg/SMG.java
@@ -970,11 +970,6 @@ public class SMG {
    */
   public SMGAndHasValueEdges readValue(
       SMGObject object, BigInteger offset, BigInteger sizeInBits, boolean preciseRead) {
-    // Check that our field is inside the object: offset + sizeInBits <= size(object)
-    Preconditions.checkArgument(object.getSize().isNumericValue());
-    Preconditions.checkArgument(
-        offset.add(sizeInBits).compareTo(object.getSize().asNumericValue().bigIntegerValue()) <= 0);
-
     // let v := H(o, of, t)
     // TODO: Currently getHasValueEdgeByOffsetAndSize returns any edge it finds.
     // Check if multiple edges may exists for the same offset and size! -> There should never be
@@ -1089,9 +1084,6 @@ public class SMG {
       SMGObject object, BigInteger offset, BigInteger sizeInBits, SMGValue value) {
     // Check that our field is inside the object: offset + sizeInBits <= size(object)
     BigInteger offsetPlusSize = offset.add(sizeInBits);
-    Preconditions.checkArgument(object.getSize().isNumericValue());
-    Preconditions.checkArgument(
-        offsetPlusSize.compareTo(object.getSize().asNumericValue().bigIntegerValue()) <= 0);
     if (value.isZero() && isCoveredByNullifiedBlocks(object, offset, sizeInBits).isPresent()) {
       return this;
     }
