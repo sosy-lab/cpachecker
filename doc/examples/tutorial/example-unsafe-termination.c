77fd7b5113039273790bb136467e329a9c19d72d
pwendler <pwendler@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2024-04-03 04:33:14 +0000

    Add missing copyright headers
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@46247 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- doc/examples/tutorial/example-unsafe-termination.c
+++ doc/examples/tutorial/example-unsafe-termination.c
@@ -1,3 +1,11 @@
+// This file is part of CPAchecker,
+// a tool for configurable software verification:
+// https://cpachecker.sosy-lab.org
+//
+// SPDX-FileCopyrightText: 2024 Dirk Beyer <https://www.sosy-lab.org>
+//
+// SPDX-License-Identifier: Apache-2.0
+
 extern unsigned __VERIFIER_nondet_uint();
 int main() {
     int n = 1;
