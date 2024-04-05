9d4468fab3ae626b5bc2b03fddda992227688fb7
draffler <draffler@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2024-04-05 14:29:16 +0000

    FloatingPoint: Applied refaster patch.
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/fix_floatingpoint_calculator@46256 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/util/floatingpoint/test/CFloatUnitTest.java
+++ src/org/sosy_lab/cpachecker/util/floatingpoint/test/CFloatUnitTest.java
@@ -522,7 +522,7 @@ public abstract class CFloatUnitTest {
       sum += stats.get(k);
       accum.put(k, ((float) sum) / total);
     }
-    ImmutableMap<Integer, Float> accumMap = accum.build();
+    ImmutableMap<Integer, Float> accumMap = accum.buildOrThrow();
 
     ImmutableList<Float> quantiles =
         ImmutableList.of(0.1f, 0.2f, 0.3f, 0.4f, 0.5f, 0.6f, 0.7f, 0.8f, 0.9f, 1.0f);
