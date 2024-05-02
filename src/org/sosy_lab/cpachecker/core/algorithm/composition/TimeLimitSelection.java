fa4825240ddb5485541c92c62705705e1c5da763
iirkha <iirkha@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2024-05-02 19:36:35 +0000

    fix build
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/test-time-selection@46385 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/core/algorithm/composition/TimeLimitSelection.java
+++ src/org/sosy_lab/cpachecker/core/algorithm/composition/TimeLimitSelection.java
@@ -9,40 +9,26 @@
 package org.sosy_lab.cpachecker.core.algorithm.composition;
 
 
-import org.sosy_lab.cpachecker.util.Pair;
-
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-
 import static org.sosy_lab.cpachecker.core.algorithm.composition.AlgSelectionBooleanVector.init;
 
+import java.util.HashMap;
+import java.util.Map;
+import org.sosy_lab.cpachecker.util.Pair;
+
 public class TimeLimitSelection {
-    private static final Map<String, String> zeitlimitMap = new ConcurrentHashMap<>() {{
-        put("(0, 0, 1, 0, 1, 0)", "250_250");
-        put("(0, 1, 0, 0, 1, 0)", "250_250");
-        put("(0, 1, 0, 0, 1, 1)", "250_250");
 
-        put("(0, 0, 0, 0, 1, 0)", "50_50");
-        put("(0, 0, 0, 1, 1, 0)", "50_50");
-        put("(0, 0, 0, 1, 1, 1)", "50_50");
-        put("(1, 0, 1, 0, 1, 1)", "50_50");
+  private static final Map<AlgSelectionBooleanVector, Pair<Integer, Integer>> CONTEXT_TIMELIMIT =
+      new HashMap<>();
 
-        put("(0, 1, 0, 1, 0, 0)", "10_10");
-        put("(0, 1, 0, 1, 1, 1)", "10_10");
-        put("(1, 0, 0, 0, 1, 1)", "10_10");
-        put("(1, 0, 1, 0, 1, 0)", "10_10");
-    }};
+  static {
+    CONTEXT_TIMELIMIT.put(init(0, 0, 1, 0, 1, 0), Pair.of(250, 250));
+    CONTEXT_TIMELIMIT.put(init(0, 1, 0, 0, 1, 0), Pair.of(250, 250));
+    CONTEXT_TIMELIMIT.put(init(0, 1, 0, 0, 1, 1), Pair.of(250, 250));
+    //TODO
+    CONTEXT_TIMELIMIT.put(init(0, 0, 0, 0, 1, 0), Pair.of(50, 50));
+  }
 
-    private static final Map<AlgSelectionBooleanVector, Pair<Integer, Integer>> CONTEXT_TIMELIMIT = new ConcurrentHashMap<>() {{
-        put(init(0, 0, 1, 0, 1, 0), Pair.of(250, 250));
-        put(init(0, 1, 0, 0, 1, 0), Pair.of(250, 250));
-        put(init(0, 1, 0, 0, 1, 1), Pair.of(250, 250));
-        //TODO
-        put(init(0, 0, 0, 0, 1, 0), Pair.of(50, 50));
-
-    }};
-
-    public static Pair<Integer, Integer> getTimeLimits(final AlgSelectionBooleanVector selectionContext) {
-        return CONTEXT_TIMELIMIT.getOrDefault(selectionContext, Pair.of(20, 80));
-    }
+  public static Pair<Integer, Integer> getTimeLimits(final AlgSelectionBooleanVector selectionContext) {
+    return CONTEXT_TIMELIMIT.getOrDefault(selectionContext, Pair.of(20, 80));
+  }
 }
