45792f75532668d59ac436a500be3b8db1787d46
mkettl <mkettl@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2024-05-26 21:08:09 +0000

    Output variable types
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/tubes@46725 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/core/algorithm/tubes/ExportAssumeEdges.java
+++ src/org/sosy_lab/cpachecker/core/algorithm/tubes/ExportAssumeEdges.java
@@ -10,12 +10,17 @@ package org.sosy_lab.cpachecker.core.algorithm.tubes;
 
 import com.google.common.base.Joiner;
 import com.google.common.collect.FluentIterable;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
 import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.Path;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.logging.Level;
 import org.checkerframework.checker.nullness.qual.NonNull;
+import org.sosy_lab.common.JSON;
 import org.sosy_lab.common.configuration.Configuration;
 import org.sosy_lab.common.configuration.FileOption;
 import org.sosy_lab.common.configuration.FileOption.Type;
@@ -25,8 +30,10 @@ import org.sosy_lab.common.configuration.Options;
 import org.sosy_lab.common.io.IO;
 import org.sosy_lab.common.log.LogManager;
 import org.sosy_lab.cpachecker.cfa.CFA;
+import org.sosy_lab.cpachecker.cfa.ast.c.CVariableDeclaration;
 import org.sosy_lab.cpachecker.cfa.model.c.CAssumeEdge;
 import org.sosy_lab.cpachecker.core.algorithm.Algorithm;
+import org.sosy_lab.cpachecker.core.algorithm.termination.ClassVariables;
 import org.sosy_lab.cpachecker.core.reachedset.ReachedSet;
 import org.sosy_lab.cpachecker.exceptions.CPAException;
 
@@ -40,6 +47,13 @@ public class ExportAssumeEdges implements Algorithm {
   @FileOption(Type.OUTPUT_FILE)
   @Option(secure = true, name = "assumes.outputPath", description = "where to write assume edges")
   private Path outputPath = Path.of("assumes.txt");
+
+  @FileOption(Type.OUTPUT_FILE)
+  @Option(
+      secure = true,
+      name = "assumes.declarationPath",
+      description = "where to write assume edges")
+  private Path declarationPath = Path.of("declarations.json");
 
   public ExportAssumeEdges(
       Algorithm pAlgorithm, Configuration pConfiguration, CFA pCFA, LogManager pLogManager)
@@ -68,8 +82,28 @@ public class ExportAssumeEdges implements Algorithm {
       } else {
         logger.log(Level.WARNING, "Path outputPath required but is null.");
       }
+      if (declarationPath != null) {
+        ClassVariables variables = ClassVariables.collectDeclarations(cfa);
+        ImmutableList.Builder<Map<String, Object>> declaredVariables = ImmutableList.builder();
+        for (Entry<String, CVariableDeclaration> entry :
+            variables.getLocalDeclarations().entries()) {
+          declaredVariables.add(
+              ImmutableMap.<String, Object>builder()
+                  .put("name", entry.getValue().getName())
+                  .put("type", entry.getValue().getType().toString())
+                  .put("line", entry.getValue().getFileLocation().getStartingLineInOrigin())
+                  .put("function", entry.getKey())
+                  .buildOrThrow());
+        }
+        JSON.writeJSONString(declaredVariables.build(), declarationPath);
+      }
     } catch (IOException ioException) {
-      throw new CPAException("Could not write to " + outputPath, ioException);
+      logger.logUserException(
+          Level.WARNING,
+          ioException,
+          "Could not write to " + outputPath + " or " + declarationPath);
+      throw new CPAException(
+          "Could not write to " + outputPath + " or " + declarationPath, ioException);
     }
     return algorithm.run(reachedSet);
   }
