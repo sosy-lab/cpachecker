7c24092ab59520ed080e03b4e4f879a993be033b
mlingsch <mlingsch@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2024-06-04 22:08:35 +0000

    Update comment forgotten in last commmit
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@46795 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/util/expressions/ExpressionTrees.java
+++ src/org/sosy_lab/cpachecker/util/expressions/ExpressionTrees.java
@@ -515,7 +515,7 @@ public final class ExpressionTrees {
    * @param formula the formula to transform
    * @param fMgr the formula manger having the formula "in scope"
    * @param location to determine the current method for checking the scope.
-   * @param variableNameFilter a filter for variable names, which should be considered.
+   * @param pExcludeVariablesFilter a filter for variable names, which should not be considered.
    * @return the expression tree representing the formula.
    */
   public static ExpressionTree<Object> fromFormula(
