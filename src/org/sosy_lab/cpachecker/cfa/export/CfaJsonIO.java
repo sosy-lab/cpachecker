90028954b78d57eef11ae40bac034be68bf5f9fb
flindenmeier <flindenmeier@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2024-06-04 11:57:50 +0000

    Serialization of metadata and FunctionEntryNodes. Restructuring in preparation of import functionality.
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/lindenmeier-1-cfa-json-export-prototype@46785 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

---
+++ src/org/sosy_lab/cpachecker/cfa/export/CfaJsonIO.java
@@ -0,0 +1,125 @@
+// This file is part of CPAchecker,
+// a tool for configurable software verification:
+// https://cpachecker.sosy-lab.org
+//
+// SPDX-FileCopyrightText: 2024 Dirk Beyer <https://www.sosy-lab.org>
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package org.sosy_lab.cpachecker.cfa.export;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility;
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.JsonIdentityReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import java.util.NavigableMap;
+import java.util.Set;
+import org.sosy_lab.cpachecker.cfa.CfaMetadata;
+import org.sosy_lab.cpachecker.cfa.model.CFAEdge;
+import org.sosy_lab.cpachecker.cfa.model.CFANode;
+import org.sosy_lab.cpachecker.cfa.model.FunctionEntryNode;
+import org.sosy_lab.cpachecker.cfa.model.FunctionExitNode;
+import org.sosy_lab.cpachecker.util.ast.AstCfaRelation;
+import org.sosy_lab.cpachecker.util.variableclassification.Partition;
+
+/* This class provides a base for exporting and importing CFA data to and from JSON. */
+public abstract class CfaJsonIO {
+
+  /* This record represents the CFA data. */
+  protected final record CfaJsonData(
+      CfaMetadata metadata,
+      Set<CFANode> nodes,
+      NavigableMap<String, FunctionEntryNode> functions,
+      Set<CFAEdge> edges) {}
+
+  /**
+   * Configures and provides an instance of {@link ObjectMapper} for CFA serialization and
+   * deserialization.
+   *
+   * @return The configured {@link ObjectMapper} instance which only maps fields and uses
+   *     indentation and newlines. On top of that, it includes mixins to prevent redundant mappings.
+   */
+  protected static final ObjectMapper provideConfiguredCfaObjectMapper() {
+    ObjectMapper objectMapper = new ObjectMapper();
+
+    /* Only map fields of objects. */
+    objectMapper.setVisibility(PropertyAccessor.ALL, Visibility.NONE);
+    objectMapper.setVisibility(PropertyAccessor.FIELD, Visibility.ANY);
+
+    /* Enable serialization with indentation and newlines. */
+    objectMapper.configure(SerializationFeature.INDENT_OUTPUT, true);
+
+    /* Add mixins. */
+    objectMapper.addMixIn(FunctionEntryNode.class, FunctionEntryNodeMixin.class);
+    objectMapper.addMixIn(FunctionExitNode.class, FunctionExitNodeMixin.class);
+    objectMapper.addMixIn(CFANode.class, CfaNodeMixin.class);
+    objectMapper.addMixIn(CfaMetadata.class, CfaMetadataMixin.class);
+    objectMapper.addMixIn(Partition.class, PartitionMixin.class);
+
+    return objectMapper;
+  }
+
+  /**
+   * This class is a mixin for {@link FunctionEntryNode}.
+   *
+   * <p>It serializes its {@link FunctionExitNode} field as number.
+   */
+  private static final class FunctionEntryNodeMixin {
+
+    @JsonIdentityReference(alwaysAsId = true)
+    private FunctionExitNode exitNode;
+  }
+
+  /**
+   * This class is a mixin for {@link FunctionExitNode}.
+   *
+   * <p>It serializes its {@link FunctionEntryNode} field as number.
+   */
+  private static final class FunctionExitNodeMixin {
+
+    @JsonIdentityReference(alwaysAsId = true)
+    private FunctionEntryNode entryNode;
+  }
+
+  /**
+   * This class is a mixin for {@link CFANode}.
+   *
+   * <p>It prevents cyclic references by serializing the {@link CFANode} as number if the same node
+   * has already been fully serialized once.
+   */
+  @JsonIdentityInfo(
+      generator = ObjectIdGenerators.PropertyGenerator.class,
+      scope = CFANode.class,
+      property = "nodeNumber")
+  private static final class CfaNodeMixin {}
+
+  /**
+   * This class represents a mixin for {@link CfaMetadata}.
+   *
+   * <p>It serializes the main {@link FunctionEntryNode} as node number and ensures that the {@link
+   * AstCfaRelation} is not serialized.
+   */
+  private static final class CfaMetadataMixin {
+
+    @JsonIdentityReference(alwaysAsId = true)
+    private FunctionEntryNode mainFunctionEntry;
+
+    @JsonIgnore private AstCfaRelation astCFARelation;
+  }
+
+  /**
+   * This class is a mixin for {@link Partition}.
+   *
+   * <p>It prevents cyclic references by serializing the {@link Partition} as index if the same
+   * object has already been fully serialized once.
+   */
+  @JsonIdentityInfo(
+      generator = ObjectIdGenerators.PropertyGenerator.class,
+      scope = Partition.class,
+      property = "index")
+  private static final class PartitionMixin {}
+}
--- src/org/sosy_lab/cpachecker/cfa/export/CfaToJson.java
+++ src/org/sosy_lab/cpachecker/cfa/export/CfaToJson.java
@@ -10,74 +10,58 @@ package org.sosy_lab.cpachecker.cfa.export;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
-import com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility;
-import com.fasterxml.jackson.annotation.JsonIdentityInfo;
-import com.fasterxml.jackson.annotation.JsonIdentityReference;
-import com.fasterxml.jackson.annotation.ObjectIdGenerators;
-import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.core.JsonEncoding;
 import com.fasterxml.jackson.core.JsonFactory;
 import com.fasterxml.jackson.core.JsonGenerator;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
 import com.google.common.io.MoreFiles;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Path;
-import java.util.Collection;
 import org.sosy_lab.cpachecker.cfa.CFA;
+import org.sosy_lab.cpachecker.cfa.CfaMetadata;
 import org.sosy_lab.cpachecker.cfa.model.CFAEdge;
 import org.sosy_lab.cpachecker.cfa.model.CFANode;
 import org.sosy_lab.cpachecker.cfa.model.FunctionEntryNode;
-import org.sosy_lab.cpachecker.cfa.model.FunctionExitNode;
-import org.sosy_lab.cpachecker.util.CFATraversal;
-import org.sosy_lab.cpachecker.util.CFATraversal.CFAVisitor;
-import org.sosy_lab.cpachecker.util.CFATraversal.CompositeCFAVisitor;
-import org.sosy_lab.cpachecker.util.CFATraversal.EdgeCollectingCFAVisitor;
-import org.sosy_lab.cpachecker.util.CFATraversal.NodeCollectingCFAVisitor;
 
 /**
  * Enables to export all {@link CFA} {@link FunctionEntryNode}s, {@link CFANode}s, {@link CFAEdge}s
- * and relevant Metadata.
+ * and relevant {@link CfaMetadata}.
  *
  * <p>The export format is JSON.
  */
-public final class CfaToJson {
-  private final CfaJsonExport cfaJsonExport;
+public final class CfaToJson extends CfaJsonIO {
+  private final CfaJsonData cfaJsonData;
 
   public CfaToJson(CFA pCfa) {
     CFA cfa = checkNotNull(pCfa);
 
-    /* Collect all nodes and edges by traversing the CFA. */
+    //    /* Collect all nodes and edges by traversing the CFA. */
+    //
+    //    NodeCollectingCFAVisitor nodeVisitor = new NodeCollectingCFAVisitor();
+    //    EdgeCollectingCFAVisitor edgeVisitor = new EdgeCollectingCFAVisitor();
+    //
+    //    CFAVisitor visitor =
+    //        new NodeCollectingCFAVisitor(new CompositeCFAVisitor(nodeVisitor, edgeVisitor));
+    //
+    //    for (FunctionEntryNode entryNode : cfa.entryNodes()) {
+    //      CFATraversal.dfs().traverse(entryNode, visitor);
+    //    }
+    //
+    //    // TODO
+    //    for (FunctionEntryNode entryNode : cfa.entryNodes()) {
+    //     .resolve("functions").resolve(entryNode.getFunctionName() + ".json");
 
-    NodeCollectingCFAVisitor nodeVisitor = new NodeCollectingCFAVisitor();
-    EdgeCollectingCFAVisitor edgeVisitor = new EdgeCollectingCFAVisitor();
-
-    CFAVisitor visitor =
-        new NodeCollectingCFAVisitor(new CompositeCFAVisitor(nodeVisitor, edgeVisitor));
-
-    for (FunctionEntryNode entryNode : cfa.entryNodes()) {
-      CFATraversal.dfs().traverse(entryNode, visitor);
-    }
-
-    this.cfaJsonExport =
-        new CfaJsonExport(nodeVisitor.getVisitedNodes(), edgeVisitor.getVisitedEdges());
+    this.cfaJsonData =
+        new CfaJsonData(cfa.getMetadata(), cfa.nodes(), cfa.getAllFunctions(), cfa.edges());
   }
 
   /**
-   * This record represents the {@link CFA} data to be exported to JSON.
+   * Writes the {@link CFA} data.
    *
-   * <p>It contains all {@link CFANode}s and {@link CFAEdge}s.
-   */
-  private record CfaJsonExport(Collection<CFANode> nodes, Collection<CFAEdge> edges) {}
-
-  /**
-   * Writes the {@link CfaJsonExport} data.
-   *
-   * @param pOutdir Directory to which the JSON files are to be written.
+   * @param pOutdir Directory to which JSON files are to be written.
    * @throws IOException If an error with {@link FileOutputStream} or {@link JsonGenerator} occurs.
    */
-  public void write(Path pOutdir) throws IOException {
+  public final void write(Path pOutdir) throws IOException {
     /* Define the file path and create any required directories. */
     Path jsonFilePath = pOutdir.resolve("cfa.json");
     MoreFiles.createParentDirectories(jsonFilePath);
@@ -87,67 +71,51 @@ public final class CfaToJson {
         JsonGenerator jsonGenerator =
             new JsonFactory().createGenerator(fileOutputStream, JsonEncoding.UTF8); ) {
 
-      provideConfiguredCfaObjectMapper().writeValue(jsonGenerator, this.cfaJsonExport);
+      provideConfiguredCfaObjectMapper().writeValue(jsonGenerator, this.cfaJsonData);
+
+      // TODO
+      // provideConfiguredCfaObjectMapper().readValue(jsonFilePath.toFile(), CfaJsonExport.class);
     }
   }
 
-  /**
-   * Configures and provides an instance of {@link ObjectMapper} for CFA serialization.
-   *
-   * @return The configured {@link ObjectMapper} instance which only maps fields and uses
-   *     indentation and newlines.
-   */
-  private static ObjectMapper provideConfiguredCfaObjectMapper() {
-    ObjectMapper objectMapper = new ObjectMapper();
-
-    /* Only map fields of objects. */
-    objectMapper.setVisibility(PropertyAccessor.ALL, Visibility.NONE);
-    objectMapper.setVisibility(PropertyAccessor.FIELD, Visibility.ANY);
-
-    /* Enable serialization with indentation and newlines. */
-    objectMapper.configure(SerializationFeature.INDENT_OUTPUT, true);
-
-    /* Add mixins. */
-    objectMapper.addMixIn(FunctionEntryNode.class, FunctionEntryNodeMixin.class);
-    objectMapper.addMixIn(FunctionExitNode.class, FunctionExitNodeMixin.class);
-    objectMapper.addMixIn(CFANode.class, CfaNodeMixin.class);
-
-    return objectMapper;
-  }
-
-  /**
-   * This class is a mixin for {@link FunctionEntryNode}.
-   *
-   * <p>It serializes its {@link FunctionExitNode} field as number.
-   */
-  private abstract static class FunctionEntryNodeMixin {
-
-    @JsonIdentityReference(alwaysAsId = true)
-    private FunctionExitNode exitNode;
-  }
-
-  /**
-   * This class is a mixin for {@link FunctionExitNode}.
-   *
-   * <p>It serializes its {@link FunctionEntryNode} field as number.
-   */
-  private abstract static class FunctionExitNodeMixin {
-
-    @JsonIdentityReference(alwaysAsId = true)
-    private FunctionEntryNode entryNode;
-  }
-
-  /**
-   * This class is a mixin for {@link CFANode}.
-   *
-   * <p>It prevents cyclic references by serializing the {@link CFANode} as number if the same node
-   * has already been fully serialized once.
-   */
-  @JsonIdentityInfo(
-      generator = ObjectIdGenerators.PropertyGenerator.class,
-      scope = CFANode.class,
-      property = "nodeNumber")
-  private abstract static class CfaNodeMixin {}
+  //  private final static class CfaJsonExportDeserializer extends JsonDeserializer<CfaJsonExport> {
+  //
+  //    @Override
+  //    public CfaJsonExport deserialize(JsonParser pJsonParser, DeserializationContext pContext)
+  //        throws IOException {
+  //      JsonNode rootNode = pJsonParser.getCodec().readTree(pJsonParser);
+  //
+  //      CfaMetadata metadata = deserializeMetadata(rootNode.get("metadata"), pJsonParser);
+  //      //Set<CFANode> nodes = deserializeNodes(rootNode.get("nodes"), pJsonParser);
+  //      //NavigableMap<String, FunctionEntryNode> functions =
+  //      //    deserializeFunctions(rootNode.get("functions"));
+  //      //Set<CFAEdge> edges = deserializeEdges(rootNode.get("edges"));
+  //
+  //      return new CfaJsonExport(null, null, null, null);
+  //    }
+  //
+  //    private CfaMetadata deserializeMetadata(JsonNode pNode, JsonParser pJsonParser) throws
+  // IOException {
+  //      return pJsonParser.getCodec().readValue(pJsonParser, CfaMetadata.class);
+  //    }
+  //
+  //    private Set<CFANode> deserializeNodes(JsonNode pNode, JsonParser pJsonParser) throws
+  // IOException {
+  //      HashSet<CFANode> nodes = new HashSet<>();
+  //      for (JsonNode node : pNode) {
+  //        nodes.add(pJsonParser.getCodec().readValue(pJsonParser, CFANode.class));
+  //      }
+  //      return nodes;
+  //    }
+  //
+  //    private NavigableMap<String, FunctionEntryNode> deserializeFunctions(JsonNode pNode) {
+  //      // TODO: Implement deserialization logic for nodes
+  //      return null;
+  //    }
+  //
+  //    private Set<CFAEdge> deserializeEdges(JsonNode pNode) {
+  //      // TODO: Implement deserialization logic for edges
+  //      return null;
+  //    }
+  //  }
 }
-    // TODO for (FunctionEntryNode entryNode : cfa.entryNodes()) {
-    // .resolve("functions").resolve(entryNode.getFunctionName() + ".json");
