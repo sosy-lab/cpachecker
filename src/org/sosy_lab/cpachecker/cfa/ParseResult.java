sosy-lab/cpachecker

commit b1f0f3edf12cbddb62ae6a93898274b3f1cc5bc7

Track information about which variables and parameters are in scope in the AstCfaRelation
Currently there are some caveats with this approach
- Static variables are not considered
- The mapping is between the AST and the CFA meaning that if the CFA is modified these changes would not be reflected

Since the current use case are exporting better witnesses, this should suffice for now.

git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@46804 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

src/org/sosy_lab/cpachecker/cfa/ParseResult.java
@@ -10,6 +10,7 @@ package org.sosy_lab.cpachecker.cfa;
 
 import com.google.common.base.Verify;
 import com.google.common.collect.FluentIterable;
+import com.google.common.collect.ImmutableMultimap;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.TreeMultimap;
 import java.nio.file.Path;
@@ -17,6 +18,8 @@ import java.util.List;
 import java.util.NavigableMap;
 import java.util.Optional;
 import org.sosy_lab.cpachecker.cfa.ast.ADeclaration;
+import org.sosy_lab.cpachecker.cfa.ast.AParameterDeclaration;
+import org.sosy_lab.cpachecker.cfa.ast.AVariableDeclaration;
 import org.sosy_lab.cpachecker.cfa.ast.FileLocation;
 import org.sosy_lab.cpachecker.cfa.ast.acsl.util.SyntacticBlock;
 import org.sosy_lab.cpachecker.cfa.model.CFAEdge;
@@ -43,7 +46,9 @@ public record ParseResult(
     List<Path> fileNames,
     Optional<AstCfaRelation> astStructure,
     Optional<List<FileLocation>> commentLocations,
-    Optional<List<SyntacticBlock>> blocks) {
+    Optional<List<SyntacticBlock>> blocks,
+    Optional<ImmutableMultimap<CFANode, AVariableDeclaration>> cfaNodeToAstLocalVariablesInScope,
+    Optional<ImmutableMultimap<CFANode, AParameterDeclaration>> cfaNodeToAstParametersInScope) {
 
   public ParseResult(
       NavigableMap<String, FunctionEntryNode> pFunctions,
@@ -55,6 +60,8 @@ public record ParseResult(
         pCfaNodes,
         pGlobalDeclarations,
         pFileNames,
+        Optional.empty(),
+        Optional.empty(),
         Optional.empty(),
         Optional.empty(),
         Optional.empty());
@@ -74,7 +81,9 @@ public record ParseResult(
         pFileNames,
         Optional.empty(),
         Optional.of(pCommentLocations),
-        Optional.of(pBlocks));
+        Optional.of(pBlocks),
+        Optional.empty(),
+        Optional.empty());
   }
 
   public boolean isEmpty() {
@@ -96,6 +105,25 @@ public record ParseResult(
         fileNames,
         Optional.of(pAstCfaRelation),
         commentLocations,
-        blocks);
+        blocks,
+        cfaNodeToAstLocalVariablesInScope,
+        cfaNodeToAstParametersInScope);
+  }
+
+  public ParseResult withInScopeInformation(
+      ImmutableMultimap<CFANode, AVariableDeclaration> pCfaNodeToAstLocalVariablesInScope,
+      ImmutableMultimap<CFANode, AParameterDeclaration> pCfaNodeToAstParametersInScope) {
+    Verify.verify(cfaNodeToAstLocalVariablesInScope.isEmpty());
+    Verify.verify(cfaNodeToAstParametersInScope.isEmpty());
+    return new ParseResult(
+        functions,
+        cfaNodes,
+        globalDeclarations,
+        fileNames,
+        astStructure,
+        commentLocations,
+        blocks,
+        Optional.of(pCfaNodeToAstLocalVariablesInScope),
+        Optional.of(pCfaNodeToAstParametersInScope));
   }
 }

src/org/sosy_lab/cpachecker/cfa/parser/eclipse/c/AstCfaRelationBuilder.java
@@ -9,14 +9,18 @@
 package org.sosy_lab.cpachecker.cfa.parser.eclipse.c;
 
 import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableMultimap;
 import com.google.common.collect.ImmutableSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import org.eclipse.cdt.core.dom.ast.IASTTranslationUnit;
 import org.sosy_lab.cpachecker.cfa.CSourceOriginMapping;
+import org.sosy_lab.cpachecker.cfa.ast.AParameterDeclaration;
+import org.sosy_lab.cpachecker.cfa.ast.AVariableDeclaration;
 import org.sosy_lab.cpachecker.cfa.ast.FileLocation;
 import org.sosy_lab.cpachecker.cfa.model.CFAEdge;
+import org.sosy_lab.cpachecker.cfa.model.CFANode;
 import org.sosy_lab.cpachecker.util.ast.AstCfaRelation;
 import org.sosy_lab.cpachecker.util.ast.DeclarationElement;
 import org.sosy_lab.cpachecker.util.ast.IfElement;
@@ -28,7 +32,10 @@ class AstCfaRelationBuilder {
   public static AstCfaRelation getASTCFARelation(
       CSourceOriginMapping pSourceOriginMapping,
       ImmutableSet<CFAEdge> pEdges,
-      List<IASTTranslationUnit> pAsts) {
+      List<IASTTranslationUnit> pAsts,
+      ImmutableMultimap<CFANode, AVariableDeclaration> pCfaNodeToAstLocalVariablesInScope,
+      ImmutableMultimap<CFANode, AParameterDeclaration> pCfaNodeToAstParametersVariablesInScope,
+      ImmutableSet<AVariableDeclaration> pGlobalVariables) {
     AstLocationClassifier classifier = new AstLocationClassifier(pSourceOriginMapping);
     for (IASTTranslationUnit ast : pAsts) {
       ast.accept(classifier);
@@ -37,7 +44,10 @@ class AstCfaRelationBuilder {
         getIfStructures(pEdges, classifier),
         getIterationStructures(pEdges, classifier),
         classifier.getStatementOffsetsToLocations(),
-        getStatementStructures(pEdges, classifier));
+        getStatementStructures(pEdges, classifier),
+        pCfaNodeToAstLocalVariablesInScope,
+        pCfaNodeToAstParametersVariablesInScope,
+        pGlobalVariables);
   }
 
   private static ImmutableSet<IfElement> getIfStructures(

src/org/sosy_lab/cpachecker/cfa/parser/eclipse/c/FunctionScope.java
@@ -15,6 +15,7 @@ import static com.google.common.base.Preconditions.checkState;
 import com.google.common.base.Joiner;
 import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Iterables;
 import java.util.ArrayDeque;
 import java.util.Collection;
@@ -58,6 +59,10 @@ class FunctionScope extends AbstractScope {
   private final Deque<Map<String, CSimpleDeclaration>> varsStackWitNewNames = new ArrayDeque<>();
   private final Deque<Map<String, CSimpleDeclaration>> varsList = new ArrayDeque<>();
   private final Deque<Map<String, CSimpleDeclaration>> varsListWithNewNames = new ArrayDeque<>();
+
+  // Cache for all local variables in the current scope.
+  private ImmutableSet<CSimpleDeclaration> localVarsDeclarations = ImmutableSet.of();
+  private boolean modifiedLocalVars = true;
 
   private CFunctionDeclaration currentFunction = null;
   private Optional<CVariableDeclaration> returnVariable = null;
@@ -148,6 +153,7 @@ class FunctionScope extends AbstractScope {
     typesStack.removeLast();
     labelsStack.removeLast();
     labelsNodeStack.removeLast();
+    modifiedLocalVars = true;
   }
 
   /** returns only the most local scope, i.e., the scope between the nearest curly brackets. */
@@ -177,6 +183,16 @@ class FunctionScope extends AbstractScope {
       }
     }
     return artificialScope.variableNameInUse(name);
+  }
+
+  /** returns all variables in the current scopes and caches them for further reuse. */
+  public ImmutableSet<CSimpleDeclaration> getVariablesInScope() {
+    if (modifiedLocalVars) {
+      localVarsDeclarations =
+          ImmutableSet.copyOf(Iterables.concat(getVariablesOfMostLocalScopes()));
+      modifiedLocalVars = false;
+    }
+    return localVarsDeclarations;
   }
 
   @Override
@@ -283,6 +299,7 @@ class FunctionScope extends AbstractScope {
 
     vars.put(name, declaration);
     varsWithNewNames.put(declaration.getName(), declaration);
+    modifiedLocalVars = true;
   }
 
   @Override

src/org/sosy_lab/cpachecker/cfa/parser/eclipse/c/CFABuilder.java
@@ -12,6 +12,7 @@ import static com.google.common.base.Strings.isNullOrEmpty;
 
 import com.google.common.collect.Collections2;
 import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableMultimap;
 import com.google.common.collect.TreeMultimap;
 import java.nio.file.Path;
 import java.util.ArrayList;
@@ -39,6 +40,8 @@ import org.sosy_lab.common.log.LogManager;
 import org.sosy_lab.common.log.LogManagerWithoutDuplicates;
 import org.sosy_lab.cpachecker.cfa.ParseResult;
 import org.sosy_lab.cpachecker.cfa.ast.ADeclaration;
+import org.sosy_lab.cpachecker.cfa.ast.AParameterDeclaration;
+import org.sosy_lab.cpachecker.cfa.ast.AVariableDeclaration;
 import org.sosy_lab.cpachecker.cfa.ast.FileLocation;
 import org.sosy_lab.cpachecker.cfa.ast.acsl.util.SyntacticBlock;
 import org.sosy_lab.cpachecker.cfa.ast.c.CAstNode;
@@ -340,6 +343,10 @@ class CFABuilder extends ASTVisitor {
       ((CDeclaration) decl.declaration()).getType().accept(fillInAllBindingsVisitor);
     }
 
+    ImmutableMultimap.Builder<CFANode, AVariableDeclaration> cfaNodeToAstLocalVariablesInScope =
+        ImmutableMultimap.builder();
+    ImmutableMultimap.Builder<CFANode, AParameterDeclaration> cfaNodeToAstParametersInScope =
+        ImmutableMultimap.builder();
     for (FunctionsOfTranslationUnit functionDeclaration : functionDeclarations) {
       GlobalScope actScope = functionDeclaration.scope();
 
@@ -358,7 +365,9 @@ class CFABuilder extends ASTVisitor {
             actFunctions,
             actTypes,
             actTypeDefs,
-            actVars);
+            actVars,
+            cfaNodeToAstLocalVariablesInScope,
+            cfaNodeToAstParametersInScope);
       }
     }
 
@@ -380,6 +389,10 @@ class CFABuilder extends ASTVisitor {
       result = new ParseResult(cfas, cfaNodes, globalDecls, parsedFiles);
     }
 
+    result =
+        result.withInScopeInformation(
+            cfaNodeToAstLocalVariablesInScope.build(), cfaNodeToAstParametersInScope.build());
+
     return result;
   }
 
@@ -390,7 +403,9 @@ class CFABuilder extends ASTVisitor {
       ImmutableMap<String, CFunctionDeclaration> functions,
       ImmutableMap<String, CComplexTypeDeclaration> types,
       ImmutableMap<String, CTypeDefDeclaration> typedefs,
-      ImmutableMap<String, CSimpleDeclaration> globalVars)
+      ImmutableMap<String, CSimpleDeclaration> globalVars,
+      ImmutableMultimap.Builder<CFANode, AVariableDeclaration> cfaNodeToAstLocalVariablesInScope,
+      ImmutableMultimap.Builder<CFANode, AParameterDeclaration> cfaNodeToAstParametersInScope)
       throws InterruptedException {
 
     FunctionScope localScope =
@@ -405,7 +420,9 @@ class CFABuilder extends ASTVisitor {
             machine,
             fileName,
             sideAssignmentStack,
-            checkBinding);
+            checkBinding,
+            cfaNodeToAstLocalVariablesInScope,
+            cfaNodeToAstParametersInScope);
 
     declaration.accept(functionBuilder);
 

src/org/sosy_lab/cpachecker/cfa/parser/eclipse/c/CFAFunctionBuilder.java
@@ -15,6 +15,7 @@ import static org.sosy_lab.cpachecker.cfa.CFACreationUtils.isReachableNode;
 import com.google.common.base.Verify;
 import com.google.common.collect.ArrayListMultimap;
 import com.google.common.collect.FluentIterable;
+import com.google.common.collect.ImmutableMultimap;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Multimap;
 import java.math.BigInteger;
@@ -73,6 +74,9 @@ import org.sosy_lab.common.log.LogManager;
 import org.sosy_lab.common.log.LogManagerWithoutDuplicates;
 import org.sosy_lab.cpachecker.cfa.CFACreationUtils;
 import org.sosy_lab.cpachecker.cfa.ast.ADeclaration;
+import org.sosy_lab.cpachecker.cfa.ast.AParameterDeclaration;
+import org.sosy_lab.cpachecker.cfa.ast.ASimpleDeclaration;
+import org.sosy_lab.cpachecker.cfa.ast.AVariableDeclaration;
 import org.sosy_lab.cpachecker.cfa.ast.FileLocation;
 import org.sosy_lab.cpachecker.cfa.ast.acsl.util.FunctionBlock;
 import org.sosy_lab.cpachecker.cfa.ast.acsl.util.StatementBlock;
@@ -179,6 +183,8 @@ class CFAFunctionBuilder extends ASTVisitor {
   private final ShutdownNotifier shutdownNotifier;
   private final CheckBindingVisitor checkBinding;
   private final Sideassignments sideAssignmentStack;
+  ImmutableMultimap.Builder<CFANode, AVariableDeclaration> cfaNodeToAstLocalVariablesInScope;
+  ImmutableMultimap.Builder<CFANode, AParameterDeclaration> cfaNodeToAstParametersInScope;
 
   private boolean encounteredAsm = false;
 
@@ -191,7 +197,9 @@ class CFAFunctionBuilder extends ASTVisitor {
       MachineModel pMachine,
       String staticVariablePrefix,
       Sideassignments pSideAssignmentStack,
-      CheckBindingVisitor pCheckBinding) {
+      CheckBindingVisitor pCheckBinding,
+      ImmutableMultimap.Builder<CFANode, AVariableDeclaration> pCfaNodeToAstLocalVariablesInScope,
+      ImmutableMultimap.Builder<CFANode, AParameterDeclaration> pCfaNodeToAstParametersInScope) {
     options = pOptions;
     logger = pLogger;
     shutdownNotifier = pShutdownNotifier;
@@ -216,6 +224,8 @@ class CFAFunctionBuilder extends ASTVisitor {
     shouldVisitStatements = true;
     shouldVisitExpressions = true;
     sideAssignmentStack = pSideAssignmentStack;
+    cfaNodeToAstLocalVariablesInScope = pCfaNodeToAstLocalVariablesInScope;
+    cfaNodeToAstParametersInScope = pCfaNodeToAstParametersInScope;
   }
 
   FunctionEntryNode getStartNode() {
@@ -1032,6 +1042,14 @@ class CFAFunctionBuilder extends ASTVisitor {
   private CFANode newCFANode() {
     assert cfa != null;
     CFANode nextNode = new CFANode(cfa.getFunction());
+
+    for (ASimpleDeclaration elem : scope.getVariablesInScope()) {
+      if (elem instanceof AVariableDeclaration variable) {
+        cfaNodeToAstLocalVariablesInScope.put(nextNode, variable);
+      } else if (elem instanceof AParameterDeclaration parameter) {
+        cfaNodeToAstParametersInScope.put(nextNode, parameter);
+      }
+    }
     return nextNode;
   }
 

src/org/sosy_lab/cpachecker/cfa/parser/eclipse/c/EclipseCParser.java
@@ -16,6 +16,7 @@ import com.google.common.base.CharMatcher;
 import com.google.common.base.Function;
 import com.google.common.base.Preconditions;
 import com.google.common.base.Strings;
+import com.google.common.collect.FluentIterable;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Iterators;
@@ -46,11 +47,13 @@ import org.sosy_lab.cpachecker.cfa.CParser;
 import org.sosy_lab.cpachecker.cfa.CProgramScope;
 import org.sosy_lab.cpachecker.cfa.CSourceOriginMapping;
 import org.sosy_lab.cpachecker.cfa.ParseResult;
+import org.sosy_lab.cpachecker.cfa.ast.AVariableDeclaration;
 import org.sosy_lab.cpachecker.cfa.ast.c.CAstNode;
 import org.sosy_lab.cpachecker.cfa.parser.Parsers.EclipseCParserOptions;
 import org.sosy_lab.cpachecker.cfa.parser.Scope;
 import org.sosy_lab.cpachecker.cfa.types.MachineModel;
 import org.sosy_lab.cpachecker.exceptions.CParserException;
+import org.sosy_lab.cpachecker.util.Pair;
 
 /** Parser based on Eclipse CDT */
 class EclipseCParser implements CParser {
@@ -343,8 +346,15 @@ class EclipseCParser implements CParser {
       result =
           result.withASTStructure(
               AstCfaRelationBuilder.getASTCFARelation(
-                  pSourceOriginMapping, result.getCFAEdges(), asts));
-
+                  pSourceOriginMapping,
+                  result.getCFAEdges(),
+                  asts,
+                  result.cfaNodeToAstLocalVariablesInScope().orElseThrow(),
+                  result.cfaNodeToAstParametersInScope().orElseThrow(),
+                  FluentIterable.from(result.globalDeclarations())
+                      .transform(Pair::getFirst)
+                      .filter(AVariableDeclaration.class)
+                      .toSet()));
       return result;
     } catch (CFAGenerationRuntimeException e) {
       throw new CParserException(e);

src/org/sosy_lab/cpachecker/util/ast/AstCfaRelation.java
@@ -8,13 +8,19 @@
 
 package org.sosy_lab.cpachecker.util.ast;
 
+import com.google.common.base.Verify;
 import com.google.common.collect.ComparisonChain;
+import com.google.common.collect.FluentIterable;
 import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableMultimap;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.ImmutableSortedMap;
 import com.google.errorprone.annotations.concurrent.LazyInit;
 import java.util.Objects;
 import java.util.Optional;
+import org.sosy_lab.cpachecker.cfa.ast.AParameterDeclaration;
+import org.sosy_lab.cpachecker.cfa.ast.AVariableDeclaration;
+import org.sosy_lab.cpachecker.cfa.ast.AbstractSimpleDeclaration;
 import org.sosy_lab.cpachecker.cfa.ast.FileLocation;
 import org.sosy_lab.cpachecker.cfa.model.CFAEdge;
 import org.sosy_lab.cpachecker.cfa.model.CFANode;
@@ -50,15 +56,27 @@ public final class AstCfaRelation {
   @LazyInit
   private ImmutableMap<Pair<Integer, Integer>, IfElement> lineAndStartColumnToIfStructure = null;
 
+  // Static variables are currently not being considered, since it is somewhat unclear how to handle
+  // them.
+  private final ImmutableMultimap<CFANode, AVariableDeclaration> cfaNodeToAstLocalVariablesInScope;
+  private final ImmutableMultimap<CFANode, AParameterDeclaration> cfaNodeToAstParametersInScope;
+  private final ImmutableSet<AVariableDeclaration> globalVariables;
+
   public AstCfaRelation(
       ImmutableSet<IfElement> pIfElements,
       ImmutableSet<IterationElement> pIterationStructures,
       ImmutableSortedMap<Integer, FileLocation> pStatementOffsetsToLocations,
-      ImmutableSet<StatementElement> pStatementElements) {
+      ImmutableSet<StatementElement> pStatementElements,
+      ImmutableMultimap<CFANode, AVariableDeclaration> pCfaNodeToAstLocalVariablesInScope,
+      ImmutableMultimap<CFANode, AParameterDeclaration> pCfaNodeToAstParametersVariablesInScope,
+      ImmutableSet<AVariableDeclaration> pGlobalVariables) {
     ifElements = pIfElements;
     iterationStructures = pIterationStructures;
     statementOffsetsToLocations = pStatementOffsetsToLocations;
     statementElements = pStatementElements;
+    cfaNodeToAstLocalVariablesInScope = pCfaNodeToAstLocalVariablesInScope;
+    cfaNodeToAstParametersInScope = pCfaNodeToAstParametersVariablesInScope;
+    globalVariables = pGlobalVariables;
   }
 
   /**
@@ -187,5 +205,20 @@ public final class AstCfaRelation {
     return Objects.requireNonNull(
             startingLocationToTightestStatement.floorEntry(new StartingLocation(pColumn, pLine)))
         .getValue();
+  }
+
+  public FluentIterable<AbstractSimpleDeclaration> getVariablesAndParametersInScope(CFANode pNode) {
+    Verify.verify(
+        cfaNodeToAstLocalVariablesInScope.containsKey(pNode),
+        "No local variables in scope for node %s",
+        pNode);
+    Verify.verify(
+        cfaNodeToAstParametersInScope.containsKey(pNode),
+        "No parameters in scope for node %s",
+        pNode);
+    return FluentIterable.concat(
+        cfaNodeToAstLocalVariablesInScope.get(pNode),
+        cfaNodeToAstParametersInScope.get(pNode),
+        globalVariables);
   }
 }

commit a19a17375920f1db89a93bdc54b887d84419e66a

Finish optimizing the comptutation of what local variables are in scope
This solves the regressions found in: https://buildbot.sosy-lab.org/cpachecker/results/nightly-frontend/00610.nightly-frontend.diff.html#/

Fixes: b1f0f3edf12cbddb62ae6a93898274b3f1cc5bc7

git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@46816 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

src/org/sosy_lab/cpachecker/cfa/ParseResult.java
@@ -47,8 +47,10 @@ public record ParseResult(
     Optional<AstCfaRelation> astStructure,
     Optional<List<FileLocation>> commentLocations,
     Optional<List<SyntacticBlock>> blocks,
-    Optional<ImmutableMap<CFANode,  ImmutableSet<AVariableDeclaration>>> cfaNodeToAstLocalVariablesInScope,
-    Optional<ImmutableMap<CFANode,  ImmutableSet<AParameterDeclaration>>> cfaNodeToAstParametersInScope) {
+    Optional<ImmutableMap<CFANode, ImmutableSet<AVariableDeclaration>>>
+        cfaNodeToAstLocalVariablesInScope,
+    Optional<ImmutableMap<CFANode, ImmutableSet<AParameterDeclaration>>>
+        cfaNodeToAstParametersInScope) {
 
   public ParseResult(
       NavigableMap<String, FunctionEntryNode> pFunctions,

src/org/sosy_lab/cpachecker/cfa/parser/eclipse/c/AstCfaRelationBuilder.java
@@ -9,7 +9,6 @@
 package org.sosy_lab.cpachecker.cfa.parser.eclipse.c;
 
 import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.ImmutableMultimap;
 import com.google.common.collect.ImmutableSet;
 import java.util.List;
 import java.util.Map;

src/org/sosy_lab/cpachecker/cfa/parser/eclipse/c/FunctionScope.java
@@ -25,6 +25,7 @@ import java.util.Deque;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Optional;
 import org.checkerframework.checker.nullness.qual.Nullable;
 import org.sosy_lab.cpachecker.cfa.CProgramScope;
@@ -66,6 +67,8 @@ class FunctionScope extends AbstractScope {
   // Cache for all local variables in the current scope.
   private ImmutableSet<AVariableDeclaration> localVarsDeclarations = ImmutableSet.of();
   private ImmutableSet<AParameterDeclaration> parameterDeclarations = ImmutableSet.of();
+  private final Deque<ImmutableSet.Builder<AVariableDeclaration>> localVarsStackWitNewNames =
+      new ArrayDeque<>();
   private boolean modifiedLocalVars = true;
   private boolean modifiedParameters = true;
 
@@ -149,6 +152,12 @@ class FunctionScope extends AbstractScope {
     varsStackWitNewNames.addLast(new HashMap<>());
     varsList.addLast(varsStack.getLast());
     varsListWithNewNames.addLast(varsStackWitNewNames.getLast());
+    // Optimizations to keep track of all variables which are in scope
+    ImmutableSet.Builder<AVariableDeclaration> newScopeBuilder = new ImmutableSet.Builder<>();
+    if (localVarsStackWitNewNames.peekLast() != null) {
+      newScopeBuilder.addAll(Objects.requireNonNull(localVarsStackWitNewNames.peekLast()).build());
+    }
+    localVarsStackWitNewNames.addLast(newScopeBuilder);
   }
 
   public void leaveBlock() {
@@ -158,6 +167,8 @@ class FunctionScope extends AbstractScope {
     typesStack.removeLast();
     labelsStack.removeLast();
     labelsNodeStack.removeLast();
+    // Optimizations to keep track of all variables which are in scope
+    localVarsStackWitNewNames.removeLast();
     modifiedLocalVars = true;
   }
 
@@ -192,11 +203,8 @@ class FunctionScope extends AbstractScope {
 
   /** returns all variables in the current scopes and caches them for further reuse. */
   public ImmutableSet<AVariableDeclaration> getVariablesInScope() {
-    if (modifiedLocalVars) {
-      localVarsDeclarations =
-          FluentIterable.concat(getVariablesOfMostLocalScopes())
-              .filter(AVariableDeclaration.class)
-              .toSet();
+    if (modifiedLocalVars && localVarsStackWitNewNames.peekLast() != null) {
+      localVarsDeclarations = Objects.requireNonNull(localVarsStackWitNewNames.peekLast()).build();
       modifiedLocalVars = false;
     }
     return localVarsDeclarations;
@@ -318,8 +326,14 @@ class FunctionScope extends AbstractScope {
 
     vars.put(name, declaration);
     varsWithNewNames.put(declaration.getName(), declaration);
-    modifiedLocalVars = true;
-    modifiedParameters = true;
+
+    // Optimizations to keep track of all variables which are in scope
+    if (declaration instanceof AVariableDeclaration pAVariableDeclaration) {
+      localVarsStackWitNewNames.getLast().add(pAVariableDeclaration);
+      modifiedLocalVars = true;
+    } else if (declaration instanceof AParameterDeclaration) {
+      modifiedParameters = true;
+    }
   }
 
   @Override