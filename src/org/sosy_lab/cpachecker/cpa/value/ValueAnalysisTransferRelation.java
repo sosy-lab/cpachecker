sosy-lab/cpachecker

commit 044b1ea31b25b19011e7050da558d67f4c158fc3

FloatingPoint: Store floats as FloatValues throughout CPAchecker and remove the BigDecimal workaround.
git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/fix_floatingpoint_calculator@46831 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

src/org/sosy_lab/cpachecker/cpa/value/ValueAnalysisTransferRelation.java
@@ -12,7 +12,6 @@ import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
 import java.io.IOException;
-import java.math.BigDecimal;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Collection;
@@ -127,6 +126,7 @@ import org.sosy_lab.cpachecker.util.BuiltinFloatFunctions;
 import org.sosy_lab.cpachecker.util.BuiltinOverflowFunctions;
 import org.sosy_lab.cpachecker.util.CFAEdgeUtils;
 import org.sosy_lab.cpachecker.util.Pair;
+import org.sosy_lab.cpachecker.util.floatingpoint.FloatValue;
 import org.sosy_lab.cpachecker.util.states.MemoryLocation;
 import org.sosy_lab.cpachecker.util.states.MemoryLocationValueHandler;
 import org.xml.sax.SAXException;
@@ -1451,13 +1451,7 @@ public class ValueAnalysisTransferRelation
                   BuiltinFloatFunctions.getTypeOfBuiltinFloatFunction(nameOfCalledFunc);
               if (ImmutableList.of(CBasicType.FLOAT, CBasicType.DOUBLE)
                   .contains(paramType.getType())) {
-                final BigDecimal integralPartValue =
-                    switch (paramType.getType()) {
-                      case FLOAT -> BigDecimal.valueOf((float) ((long) numericValue.floatValue()));
-                      case DOUBLE ->
-                          BigDecimal.valueOf((double) ((long) numericValue.doubleValue()));
-                      default -> throw new AssertionError("Unsupported float type: " + paramType);
-                    };
+                FloatValue integralPartValue = numericValue.floatingPointValue();
                 CFloatLiteralExpression integralPart =
                     new CFloatLiteralExpression(
                         functionCallExpression.getFileLocation(), paramType, integralPartValue);

src/org/sosy_lab/cpachecker/cfa/ast/AFloatLiteralExpression.java
@@ -8,31 +8,27 @@
 
 package org.sosy_lab.cpachecker.cfa.ast;
 
-import java.math.BigDecimal;
 import java.util.Objects;
 import org.sosy_lab.cpachecker.cfa.types.Type;
+import org.sosy_lab.cpachecker.util.floatingpoint.FloatValue;
 
 public abstract class AFloatLiteralExpression extends ALiteralExpression {
 
   private static final long serialVersionUID = 8161363025296340648L;
-  private final BigDecimal value;
+  private final FloatValue value;
 
-  protected AFloatLiteralExpression(FileLocation pFileLocation, Type pType, BigDecimal pValue) {
+  protected AFloatLiteralExpression(FileLocation pFileLocation, Type pType, FloatValue pValue) {
     super(pFileLocation, pType);
     value = pValue;
   }
 
   @Override
-  public BigDecimal getValue() {
+  public FloatValue getValue() {
     return value;
   }
 
   @Override
   public String toASTString() {
-    // If the value is integral and has no zeroes after the decimal point yet, add one
-    if (value.scale() <= 0) {
-      return String.format("%.1f", value);
-    }
     return value.toString();
   }
 

src/org/sosy_lab/cpachecker/cfa/ast/c/CFloatLiteralExpression.java
@@ -8,64 +8,33 @@
 
 package org.sosy_lab.cpachecker.cfa.ast.c;
 
-import java.math.BigDecimal;
 import org.sosy_lab.cpachecker.cfa.ast.AFloatLiteralExpression;
 import org.sosy_lab.cpachecker.cfa.ast.FileLocation;
-import org.sosy_lab.cpachecker.cfa.types.c.CBasicType;
-import org.sosy_lab.cpachecker.cfa.types.c.CSimpleType;
 import org.sosy_lab.cpachecker.cfa.types.c.CType;
+import org.sosy_lab.cpachecker.util.floatingpoint.FloatValue;
+import org.sosy_lab.cpachecker.util.floatingpoint.FloatValue.Format;
 
 public final class CFloatLiteralExpression extends AFloatLiteralExpression
     implements CLiteralExpression {
 
   private static final long serialVersionUID = 5021145411123854111L;
 
-  public CFloatLiteralExpression(FileLocation pFileLocation, CType pType, BigDecimal pValue) {
+  public CFloatLiteralExpression(FileLocation pFileLocation, CType pType, FloatValue pValue) {
     super(pFileLocation, pType, pValue);
   }
 
   /** Returns a <code>CFloatLiteralExpression</code> for positive infinity. */
   public static CFloatLiteralExpression forPositiveInfinity(FileLocation pFileLocation, CType pType)
       throws NumberFormatException {
-    // TODO: This method is a temporary hack until 'BigDecimal's are fully replaced by 'CFloat's in
-    // AFloatLiteralExpression class.
-
-    return new CFloatLiteralExpression(pFileLocation, pType, getInfinityApprox(false, pType));
+    return new CFloatLiteralExpression(
+        pFileLocation, pType, FloatValue.infinity(Format.fromCType(pType)));
   }
 
   /** Returns a <code>CFloatLiteralExpression</code> for negative infinity. */
   public static CFloatLiteralExpression forNegativeInfinity(FileLocation pFileLocation, CType pType)
       throws NumberFormatException {
-    // TODO: This method is a temporary hack until 'BigDecimal's are fully replaced by 'CFloat's in
-    // AFloatLiteralExpression class.
-
-    return new CFloatLiteralExpression(pFileLocation, pType, getInfinityApprox(true, pType));
-  }
-
-  private static BigDecimal getInfinityApprox(boolean pIsNegative, CType pType) {
-    // TODO: This method is a temporary hack until 'BigDecimal's are replaced by 'CFloat's in
-    // AFloatLiteralExpression class.
-    // This is necessary because CFloats are already able to return "inf" as a value, however, the
-    // BigDecimal object requires a concrete numerical value instead. The code below thus returns a
-    // placeholder value in the meantime.
-    BigDecimal APPROX_INFINITY =
-        BigDecimal.valueOf(Double.MAX_VALUE).add(BigDecimal.valueOf(Double.MAX_VALUE));
-
-    CBasicType basicType = ((CSimpleType) pType).getType();
-    switch (basicType) {
-      case FLOAT:
-      case DOUBLE:
-        if (pIsNegative) {
-          return APPROX_INFINITY.negate();
-        } else {
-          return APPROX_INFINITY;
-        }
-      default:
-        // unsupported operation
-        break;
-    }
-
-    throw new NumberFormatException(String.format("Invalid type for float infinity: %s", pType));
+    return new CFloatLiteralExpression(
+        pFileLocation, pType, FloatValue.negativeInfinity(Format.fromCType(pType)));
   }
 
   @Override

src/org/sosy_lab/cpachecker/cfa/ast/java/JFloatLiteralExpression.java
@@ -8,11 +8,11 @@
 
 package org.sosy_lab.cpachecker.cfa.ast.java;
 
-import java.math.BigDecimal;
 import org.sosy_lab.cpachecker.cfa.ast.AFloatLiteralExpression;
 import org.sosy_lab.cpachecker.cfa.ast.FileLocation;
 import org.sosy_lab.cpachecker.cfa.types.java.JSimpleType;
 import org.sosy_lab.cpachecker.cfa.types.java.JType;
+import org.sosy_lab.cpachecker.util.floatingpoint.FloatValue;
 
 /** This class represents the float number literal AST node type. */
 public final class JFloatLiteralExpression extends AFloatLiteralExpression
@@ -20,7 +20,7 @@ public final class JFloatLiteralExpression extends AFloatLiteralExpression
 
   private static final long serialVersionUID = -8344145326316408368L;
 
-  public JFloatLiteralExpression(FileLocation pFileLocation, BigDecimal pValue) {
+  public JFloatLiteralExpression(FileLocation pFileLocation, FloatValue pValue) {
     super(pFileLocation, JSimpleType.getFloat(), pValue);
   }
 

src/org/sosy_lab/cpachecker/cfa/parser/eclipse/c/ASTConverterTest.java
@@ -197,20 +197,19 @@ public class ASTConverterTest {
     record TestCase(String input, String expected, CType type) {}
     ImmutableList<TestCase> input_output =
         ImmutableList.of(
-            new TestCase("0", "0.0000000000000000", CNumericTypes.DOUBLE),
-            new TestCase("-0", "0.0000000000000000", CNumericTypes.DOUBLE),
-            new TestCase("0xf", "15.0000000", CNumericTypes.DOUBLE),
-            new TestCase("5e2", "500.00000000000000", CNumericTypes.FLOAT),
-            new TestCase("5e+2", "500.00000000000000", CNumericTypes.FLOAT),
-            new TestCase("0x5e2", "1506.0000000000000", CNumericTypes.FLOAT),
-            new TestCase("0x5ep2", "376.00000000000000", CNumericTypes.FLOAT),
-            new TestCase("0x5ep-2", "23.500000000000000", CNumericTypes.FLOAT),
-            new TestCase(
-                "3.41E+38", "341000000000000000000000000000000000000", CNumericTypes.DOUBLE),
-            new TestCase("-308e-2f", "-3.0800000000000001", CNumericTypes.FLOAT),
-            new TestCase("-308L", "-308.00000000000000", CNumericTypes.LONG_DOUBLE),
-            new TestCase("-0x308p-2F", "-194.00000000000000", CNumericTypes.FLOAT),
-            new TestCase("-0x30al", "-778.00000000000000", CNumericTypes.LONG_DOUBLE));
+            new TestCase("0", "0.0000000000000000e+00", CNumericTypes.DOUBLE),
+            new TestCase("-0", "-0.0000000000000000e+00", CNumericTypes.DOUBLE),
+            new TestCase("0xfp00", "1.5000000000000000e+01", CNumericTypes.DOUBLE),
+            new TestCase("5e2", "5.00000000e+02", CNumericTypes.FLOAT),
+            new TestCase("5e+2", "5.00000000e+02", CNumericTypes.FLOAT),
+            new TestCase("0x5e2", "1.50600000e+03", CNumericTypes.FLOAT),
+            new TestCase("0x5ep2", "3.76000000e+02", CNumericTypes.FLOAT),
+            new TestCase("0x5ep-2", "2.35000000e+01", CNumericTypes.FLOAT),
+            new TestCase("3.41E+38", "3.4100000000000000e+38", CNumericTypes.DOUBLE),
+            new TestCase("-308e-2f", "-3.07999992e+00", CNumericTypes.FLOAT),
+            new TestCase("-308L", "-3.0800000000000000e+02", CNumericTypes.LONG_DOUBLE),
+            new TestCase("-0x308p-2F", "-1.94000000e+02", CNumericTypes.FLOAT),
+            new TestCase("-0x30al", "-7.7800000000000000e+02", CNumericTypes.LONG_DOUBLE));
 
     for (ASTLiteralConverter converter : converters) {
       for (TestCase test : input_output) {
@@ -218,7 +217,7 @@ public class ASTConverterTest {
             (CFloatLiteralExpression)
                 converter.parseFloatLiteral(FileLocation.DUMMY, test.type(), test.input(), null);
 
-        assertThat(literal.getValue().toPlainString()).isEqualTo(test.expected());
+        assertThat(literal.getValue().toString()).isEqualTo(test.expected());
         assertThat(test.type()).isSameInstanceAs(literal.getExpressionType());
       }
     }

src/org/sosy_lab/cpachecker/cfa/parser/eclipse/c/ASTLiteralConverter.java
@@ -13,9 +13,9 @@ import static java.lang.Character.isDigit;
 import com.google.common.annotations.VisibleForTesting;
 import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableList;
-import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.util.Arrays;
+import java.util.Locale;
 import java.util.stream.Stream;
 import org.eclipse.cdt.core.dom.ast.IASTLiteralExpression;
 import org.eclipse.cdt.core.dom.ast.IASTNode;
@@ -150,37 +150,34 @@ class ASTLiteralConverter {
   @VisibleForTesting
   CLiteralExpression parseFloatLiteral(
       FileLocation pFileLoc, CType pType, String pValueStr, IASTLiteralExpression pExp) {
+    String input = pValueStr.toLowerCase(Locale.ROOT);
 
     // According to section 6.4.4.2 "Floating constants" of the C standard,
     // an unsuffixed floating constant has type double. If suffixed by the letter f or F, it has
     // type float. If suffixed by the letter l or L, it has type long double.
-
-    // FIXME: Remove the suffix from the input to avoid parsing it twice
-    FloatValue cFloat;
-    if (pValueStr.endsWith("L") || pValueStr.endsWith("l")) {
-      cFloat = FloatValue.fromString(Format.Extended, pValueStr);
-    } else if (pValueStr.endsWith("F") || pValueStr.endsWith("f")) {
-      cFloat = FloatValue.fromString(Format.Float32, pValueStr);
+    Format format;
+    if (input.endsWith("l")) {
+      input = input.substring(0, input.length() - 1);
+      format = Format.Extended;
+    } else if (input.endsWith("f")) {
+      input = input.substring(0, input.length() - 1);
+      format = Format.Float32;
     } else {
-      // literal has no suffix declared
-      cFloat = FloatValue.fromString(Format.Float64, pValueStr);
+      format = Format.Float64;
     }
 
-    if (cFloat.isInfinite()) {
-      if (cFloat.isNegative()) {
-        return CFloatLiteralExpression.forNegativeInfinity(pFileLoc, pType);
-      } else {
-        return CFloatLiteralExpression.forPositiveInfinity(pFileLoc, pType);
-      }
-    }
-
+    FloatValue value;
     try {
-      BigDecimal value = new BigDecimal(cFloat.toString());
-      return new CFloatLiteralExpression(pFileLoc, pType, value);
+      value = FloatValue.fromString(format, input);
     } catch (NumberFormatException e) {
       throw parseContext.parseError(
-          String.format("unable to parse floating point literal (%s)", cFloat.toString()), pExp);
+          String.format("unable to parse floating point literal (%s)", pValueStr), pExp);
     }
+
+    // Round the parsed value to the target type
+    value = value.withPrecision(Format.fromCType(pType));
+
+    return new CFloatLiteralExpression(pFileLoc, pType, value);
   }
 
   @VisibleForTesting

src/org/sosy_lab/cpachecker/cfa/parser/eclipse/java/ASTConverter.java
@@ -22,7 +22,6 @@ import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Sets;
 import java.lang.reflect.Array;
 import java.lang.reflect.Constructor;
-import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
@@ -144,6 +143,8 @@ import org.sosy_lab.cpachecker.cfa.types.java.JMethodType;
 import org.sosy_lab.cpachecker.cfa.types.java.JReferenceType;
 import org.sosy_lab.cpachecker.cfa.types.java.JSimpleType;
 import org.sosy_lab.cpachecker.cfa.types.java.JType;
+import org.sosy_lab.cpachecker.util.floatingpoint.FloatValue;
+import org.sosy_lab.cpachecker.util.floatingpoint.FloatValue.Format;
 
 class ASTConverter {
 
@@ -2773,22 +2774,9 @@ class ASTConverter {
     }
   }
 
-  private BigDecimal parseFloatLiteral(String valueStr) {
-
-    BigDecimal value;
-    try {
-      value = new BigDecimal(valueStr);
-    } catch (NumberFormatException nfe1) {
-      try {
-        // this might be a hex floating point literal
-        // BigDecimal doesn't support this, but Double does
-        // TODO handle hex floating point literals that are too large for Double
-        value = BigDecimal.valueOf(Double.parseDouble(valueStr));
-      } catch (NumberFormatException nfe2) {
-        throw new CFAGenerationRuntimeException("Illegal floating point literal", nfe2);
-      }
-    }
-    return value;
+  private FloatValue parseFloatLiteral(String valueStr) {
+    // TODO: Add support for "f" and "l" suffix and use a parameter for the target type
+    return FloatValue.fromString(Format.Float64, valueStr);
   }
 
   private BigInteger parseIntegerLiteral(String s, ASTNode e) {

src/org/sosy_lab/cpachecker/cfa/parser/llvm/CFABuilder.java
@@ -14,7 +14,6 @@ import com.google.common.base.CharMatcher;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.TreeMultimap;
-import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.nio.file.Path;
 import java.util.ArrayList;
@@ -94,6 +93,8 @@ import org.sosy_lab.cpachecker.exceptions.UnrecognizedCodeException;
 import org.sosy_lab.cpachecker.util.CFATraversal;
 import org.sosy_lab.cpachecker.util.CFAUtils;
 import org.sosy_lab.cpachecker.util.Pair;
+import org.sosy_lab.cpachecker.util.floatingpoint.FloatValue;
+import org.sosy_lab.cpachecker.util.floatingpoint.FloatValue.Format;
 import org.sosy_lab.llvm_j.BasicBlock;
 import org.sosy_lab.llvm_j.Function;
 import org.sosy_lab.llvm_j.LLVMException;
@@ -1341,7 +1342,8 @@ public class CFABuilder {
         CBasicType basicType = ((CSimpleType) canonicalType).getType();
         if (basicType == CBasicType.FLOAT || basicType == CBasicType.DOUBLE) {
           // use expected type for float, not canonical
-          zeroExpression = new CFloatLiteralExpression(loc, pExpectedType, BigDecimal.ZERO);
+          Format format = basicType == CBasicType.FLOAT ? Format.Float32 : Format.Float64;
+          zeroExpression = new CFloatLiteralExpression(loc, pExpectedType, FloatValue.zero(format));
         } else {
           zeroExpression = CIntegerLiteralExpression.ZERO;
         }

commit dd55b03351b5b6b66cb7908108fddb5a31f2747d

FloatingPoint: Fixed a bug in the handling of `modf` that was accidentially introduced by 044b1ea31b25b19011e7050da558d67f4c158fc3
git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/fix_floatingpoint_calculator@46877 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

src/org/sosy_lab/cpachecker/cpa/value/ValueAnalysisTransferRelation.java
@@ -127,6 +127,7 @@ import org.sosy_lab.cpachecker.util.BuiltinOverflowFunctions;
 import org.sosy_lab.cpachecker.util.CFAEdgeUtils;
 import org.sosy_lab.cpachecker.util.Pair;
 import org.sosy_lab.cpachecker.util.floatingpoint.FloatValue;
+import org.sosy_lab.cpachecker.util.floatingpoint.FloatValue.RoundingMode;
 import org.sosy_lab.cpachecker.util.states.MemoryLocation;
 import org.sosy_lab.cpachecker.util.states.MemoryLocationValueHandler;
 import org.xml.sax.SAXException;
@@ -1451,7 +1452,8 @@ public class ValueAnalysisTransferRelation
                   BuiltinFloatFunctions.getTypeOfBuiltinFloatFunction(nameOfCalledFunc);
               if (ImmutableList.of(CBasicType.FLOAT, CBasicType.DOUBLE)
                   .contains(paramType.getType())) {
-                FloatValue integralPartValue = numericValue.floatingPointValue();
+                FloatValue integralPartValue =
+                    numericValue.floatingPointValue().roundToInteger(RoundingMode.TRUNCATE);
                 CFloatLiteralExpression integralPart =
                     new CFloatLiteralExpression(
                         functionCallExpression.getFileLocation(), paramType, integralPartValue);