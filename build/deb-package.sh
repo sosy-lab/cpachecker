e0520402b4cb92999a0d39e9e2b23e520e2113eb
pwendler <pwendler@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2024-06-21 14:03:17 +0000

    Script and definitions for creating a .deb package for CPAchecker
    This is the base for creating a usable Debian/Ubuntu package,
    with the minimal amount of work.
    
    The package is created from a release ZIP of CPAchecker,
    so both the source and the binary package
    contain a fully built CPAchecker.
    
    CPAchecker is installed into /opt/cpachecker
    because this is significantly easier than a proper package layout.
    
    Part of #1220.
    
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/1220-deb-package@46900 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

---
+++ build/deb-package.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+# This file is part of CPAchecker,
+# a tool for configurable software verification:
+# https://cpachecker.sosy-lab.org
+#
+# SPDX-FileCopyrightText: 2024 Dirk Beyer <https://www.sosy-lab.org>
+#
+# SPDX-License-Identifier: Apache-2.0
+
+set -euo pipefail
+IFS=$'\n\t'
+
+VERSION="${1:-}"
+if [[ -z $VERSION ]]; then
+  echo "Please provide the version of CPAchecker as first parameter."
+  exit 1
+fi
+CPACHECKER="CPAchecker-$VERSION-unix"
+
+ARCHIVE="${2:-}"
+if [[ -z $ARCHIVE ]]; then
+  echo "Please provide the CPAchecker-$VERSION-unix.zip file as second parameter."
+  exit 1
+fi
+if [[ $(basename "$ARCHIVE") != $CPACHECKER.zip ]]; then
+  echo "The given archive '$ARCHIVE' does not seem to be CPAchecker for version '$VERSION' on Linux."
+  exit 1
+fi
+if [[ ! -f $ARCHIVE ]]; then
+  echo "Archive '$ARCHIVE' does not exist."
+  exit 1
+fi
+
+export CPACHECKER VERSION
+
+if [[ -z $DEBFULLNAME ]]; then
+  echo "Please define environment variable DEBFULLNAME with your name you want to use for the Debian package."
+  exit 1
+fi
+if [[ -z $DEBEMAIL ]]; then
+  echo "Please define environment variable DEBEMAIL with your name you want to use for the Debian package."
+  exit 1
+fi
+
+TEMP_DEB="$(mktemp -d)"
+BUILD_DIR="$TEMP_DEB/build"
+mkdir "$BUILD_DIR"
+cp -a "$(dirname "$0")/debian" "$BUILD_DIR"
+
+podman run --rm -w "$BUILD_DIR" -v "$TEMP_DEB:$TEMP_DEB:rw" -v "$ARCHIVE:/$CPACHECKER.zip:ro" -e CPACHECKER -e "DEB*" -e VERSION ubuntu:20.04 bash -c '
+  set -euo pipefail
+
+  apt-get update
+  apt-get install -y --no-install-recommends dpkg-dev devscripts unzip
+  TZ=UTC DEBIAN_FRONTEND=noninteractive apt-get install -y $(dpkg-checkbuilddeps 2>&1 | grep -o "Unmet build dependencies:.*" | cut -d: -f2- | sed "s/([^)]*)//g")
+
+  unzip "/$CPACHECKER.zip"
+  # clean up irrelevant stuff
+  rm -rf "$CPACHECKER/lib/native/"{x86-*,*macosx,*windows}
+  # Only necessary before CPAchecker 2.4
+  rm -f "$CPACHECKER/lib/jpl"* "$CPACHECKER/lib/native/x86_64-linux/chc_lib"* "$CPACHECKER/lib/native/x86_64-linux/libjpl.so"*
+
+  dch -v "$VERSION-1" "New upstream version."
+  dch -r ""
+
+  dpkg-buildpackage --build=source,binary --no-sign
+'
+
+cp -a "$BUILD_DIR"/debian/changelog "$(dirname "$0")/debian"
+cp -a "$TEMP_DEB"/*.* .
+rm -rf "$TEMP_DEB"
+
+debsign -k "$DEBKEY" cpachecker_$VERSION-*.changes
