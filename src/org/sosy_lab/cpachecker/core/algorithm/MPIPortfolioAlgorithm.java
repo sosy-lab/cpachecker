sosy-lab/cpachecker

commit 09ada19f90fc73839a293728876f8356a292af31

Use new command-line arguments in MPIPortfolioAlgorithm
git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/1218-new-command-line-arguments@46930 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

src/org/sosy_lab/cpachecker/core/algorithm/MPIPortfolioAlgorithm.java
@@ -549,9 +549,9 @@ public class MPIPortfolioAlgorithm implements Algorithm, StatisticsProvider {
 
       // Bring the command-line into a format which is executable by a python-script
       ImmutableList.Builder<String> cmdLineBuilder = ImmutableList.builder();
-      cmdLineBuilder.add("bin/cpachecker").add("-config").add(configPath.toString());
+      cmdLineBuilder.add("bin/cpachecker").add("--config").add(configPath.toString());
       for (String opt : Splitter.on('\n').omitEmptyStrings().split(config.asPropertiesString())) {
-        cmdLineBuilder.add("-setprop").add(opt);
+        cmdLineBuilder.add("--setprop").add(opt);
       }
       cmdLine = cmdLineBuilder.build();
     }

commit a8d162c38668914116395b946a2d8442c986c612

Fix MPIPortfolioAlgorithm
r46930 / 09ada19f introduced a wrong argument.

git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/1218-new-command-line-arguments@46960 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

src/org/sosy_lab/cpachecker/core/algorithm/MPIPortfolioAlgorithm.java
@@ -551,7 +551,7 @@ public class MPIPortfolioAlgorithm implements Algorithm, StatisticsProvider {
       ImmutableList.Builder<String> cmdLineBuilder = ImmutableList.builder();
       cmdLineBuilder.add("bin/cpachecker").add("--config").add(configPath.toString());
       for (String opt : Splitter.on('\n').omitEmptyStrings().split(config.asPropertiesString())) {
-        cmdLineBuilder.add("--setprop").add(opt);
+        cmdLineBuilder.add("--option").add(opt);
       }
       cmdLine = cmdLineBuilder.build();
     }