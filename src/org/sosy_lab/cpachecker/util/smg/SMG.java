7e64169800754b36b7e92002b65035fe7483828b
dbaier <dbaier@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2024-07-10 12:53:58 +0000

    SMG: add methods to get all SMGObjects that have a certain SMGValue saved in them to the SMG
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@47126 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/util/smg/SMG.java
+++ src/org/sosy_lab/cpachecker/util/smg/SMG.java
@@ -446,6 +446,12 @@ public class SMG {
     return sourceObjectsMap.values().stream().reduce(0, Integer::sum);
   }
 
+  public Set<SMGObject> getAllObjectsWithValueInThem(SMGValue value) {
+    PersistentMap<SMGObject, Integer> sourceObjectsMap =
+        valuesToRegionsTheyAreSavedIn.getOrDefault(value, PathCopyingPersistentTreeMap.of());
+    return sourceObjectsMap.keySet();
+  }
+
   public SMG copyAndAddHVEdges(Iterable<SMGHasValueEdge> edges, SMGObject source) {
     PersistentSet<SMGHasValueEdge> smgEdges = hasValueEdges.get(source);
     PersistentMap<SMGValue, PersistentMap<SMGObject, Integer>> newValuesToRegionsTheyAreSavedIn =
