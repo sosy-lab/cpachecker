af321f2ee128d62e7b3dcb922e50aee856e7c186
Karlheinz Friedberger <kfriedberger@gmail.com>
2024-07-15 18:51:19 +0000

    Improve method signature to get more information for an error.
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/chore-cleanup@47166 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/util/cwriter/FormulaToCExpressionVisitor.java
+++ src/org/sosy_lab/cpachecker/util/cwriter/FormulaToCExpressionVisitor.java
@@ -77,11 +77,13 @@ public class FormulaToCExpressionVisitor extends FormulaTransformationVisitor {
       case BV_NEG:
       case FP_NEG:
       case BV_NOT:
-        result = operatorFromFunctionDeclaration(functionDeclaration) + cache.get(newArgs.get(0));
+        result =
+            operatorFromFunctionDeclaration(functionDeclaration, f) + cache.get(newArgs.get(0));
         break;
       case EQ_ZERO:
       case GTE_ZERO:
-        result = cache.get(newArgs.get(0)) + operatorFromFunctionDeclaration(functionDeclaration);
+        result =
+            cache.get(newArgs.get(0)) + operatorFromFunctionDeclaration(functionDeclaration, f);
         break;
       case FP_ROUND_EVEN:
       case FP_ROUND_AWAY:
@@ -103,7 +105,8 @@ public class FormulaToCExpressionVisitor extends FormulaTransformationVisitor {
                   .skip(1)
                   .transform(arg -> Preconditions.checkNotNull(cache.get(arg)))
                   .toList();
-          result = String.join(operatorFromFunctionDeclaration(functionDeclaration), expressions);
+          result =
+              String.join(operatorFromFunctionDeclaration(functionDeclaration, f), expressions);
           break;
         }
       case ITE:
@@ -127,7 +130,8 @@ public class FormulaToCExpressionVisitor extends FormulaTransformationVisitor {
             // randomly. Let's force some determinism over distinct solvers, by alphabetical order.
             Collections.sort(expressions);
           }
-          result = String.join(operatorFromFunctionDeclaration(functionDeclaration), expressions);
+          result =
+              String.join(operatorFromFunctionDeclaration(functionDeclaration, f), expressions);
         }
     }
     if (result != null) {
@@ -136,7 +140,7 @@ public class FormulaToCExpressionVisitor extends FormulaTransformationVisitor {
     return f;
   }
 
-  private String operatorFromFunctionDeclaration(FunctionDeclaration<?> pDeclaration) {
+  private String operatorFromFunctionDeclaration(FunctionDeclaration<?> pDeclaration, Formula f) {
     switch (pDeclaration.getKind()) {
       case NOT:
         return "!";
@@ -231,7 +235,9 @@ public class FormulaToCExpressionVisitor extends FormulaTransformationVisitor {
         // $FALL-THROUGH$
       default:
         throw new UnsupportedOperationException(
-            "Unexpected operand " + pDeclaration.getKind() + "(" + pDeclaration.getName() + ")");
+            String.format(
+                "Unexpected operand %s (%s) in formula '%s'",
+                pDeclaration.getKind(), pDeclaration.getName(), f));
     }
   }
 
