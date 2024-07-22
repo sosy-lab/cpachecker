27c46885074b88a6a0d810e3fab151481a267f3b
txia <txia@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2024-07-22 20:35:54 +0000

    Format
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/locate-loop-and-live-variables@47260 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/core/algorithm/instrumentation/InstrumentationAutomaton.java
+++ src/org/sosy_lab/cpachecker/core/algorithm/instrumentation/InstrumentationAutomaton.java
@@ -14,7 +14,7 @@ import com.google.common.collect.ImmutableMap;
  * Data structure that defines the required transformation of CFA. It is used in
  * InstrumentationOperatorAlgorithm and injects new transitions into an original CFA.
  */
-public class InstrumentationAutomaton{
+public class InstrumentationAutomaton {
   private InstrumentationProperty instrumentationProperty;
   private ImmutableMap<String, String> liveVariablesAndTypes;
 
@@ -25,12 +25,13 @@ public class InstrumentationAutomaton{
 
   /**
    * @param pInstrumentationProperty temporary indication of which property is used in the
-   *                                 transformation
+   *     transformation
    * @param pLiveVariablesAndTypes the mapping from variable names used, but not declared, in a loop
-   *                              to their types
+   *     to their types
    */
-  InstrumentationAutomaton(InstrumentationProperty pInstrumentationProperty,
-                           ImmutableMap<String, String> pLiveVariablesAndTypes) {
+  InstrumentationAutomaton(
+      InstrumentationProperty pInstrumentationProperty,
+      ImmutableMap<String, String> pLiveVariablesAndTypes) {
     this.liveVariablesAndTypes = pLiveVariablesAndTypes;
   }
 }
