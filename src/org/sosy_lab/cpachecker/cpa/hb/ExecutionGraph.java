ff30bd94ac3173a2a7f7869d98b33ccbe9e4077b
lbajczi <lbajczi@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2024-07-30 20:55:05 +0000

    Removed unused imports
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@47360 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/cpa/hb/ExecutionGraph.java
+++ src/org/sosy_lab/cpachecker/cpa/hb/ExecutionGraph.java
@@ -22,7 +22,6 @@ import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 public record ExecutionGraph(
     Map<String, Set<MemoryEvent>> revisitableReads,
