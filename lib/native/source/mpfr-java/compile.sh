9506f62bc6db4af50c2045a61cb3e892c5c569ee
draffler <draffler@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2024-08-02 19:57:09 +0000

    Floatingpoint: Add a build script for mpfr-java
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/fix_floatingpoint_calculator@47412 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

---
+++ lib/native/source/mpfr-java/compile.sh
@@ -0,0 +1,72 @@
+#!/usr/bin/env bash
+
+# This file is part of CPAchecker,
+# a tool for configurable software verification:
+# https://cpachecker.sosy-lab.org
+#
+# SPDX-FileCopyrightText: 2007-2024 Dirk Beyer <https://www.sosy-lab.org>
+#
+# SPDX-License-Identifier: Apache-2.0
+
+# Build mpfr-java with its dependencies GMP and MPFR and copy the libraries to the CPAchecker /lib directory
+# Usage: ./compile.sh PATH_TO_GMP PATH_TO_MPFR PATH_TO_MPFRJAVA
+
+# Get the directory of the script
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+# Get the path to GMP
+PATH_GMP=$1
+if [ ! -f "${PATH_GMP}/gmp.h" ]; then
+    echo -e "Could not find GMP.\nYou need to specify the path to GMP as the first argument on the command line"
+    exit 1
+fi
+
+# Get the path to MPFR
+PATH_MPFR=$2
+if [ ! -f "${PATH_MPFR}/src/mpfr.h" ]; then
+    echo -e "Could not find MPFR.\nYou need to specify the path to MPFR as the second argument on the command line"
+    exit 1
+fi
+
+# Get the path to mpfr-java
+PATH_MPFRJAVA=$3
+if [ ! -f "${PATH_MPFRJAVA}/src/main/native-package/src/mpfr_java.h" ]; then
+    echo -e "Could not find mpfr-java.\nYou need to specify the path to mpfr-java as the third argument on the command line"
+    exit 1
+fi
+
+# Get the installation path for the libraries
+PATH_INSTALL=$( cd $SCRIPT_DIR && cd ../../x86_64-linux && pwd )
+
+echo "Compiling GMP..."
+cd $PATH_GMP \
+    && ./configure --with-pic \
+    && make \
+    && cp .libs/libgmp.so.10.5.0 $PATH_INSTALL/libgmp.so
+
+echo -e "\n\n"
+
+echo "Compiling MPFR..."
+cd $PATH_MPFR \
+    && ./configure --with-pic --with-gmp=$PATH_GMP/install \
+    && make \
+    && cp src/.libs/libmpfr.so.6.2.1 $PATH_INSTALL/libmpfr.so
+
+echo -e "\n\n"
+
+echo "Building mpfr-java..."
+cd $PATH_MPFRJAVA \
+   && git apply $SCRIPT_DIR/mpfr-java.patch \
+   && mvn install -Dmpfr.cppflags="-I${PATH_GMP}/install/include -I${PATH_MPFR}/install/include" -Dmpfr.ldflags="-L${PATH_GMP}/.libs -L${PATH_MPFR}/src/.libs" -DskipTests \
+   && cp target/native-build/.libs/libmpfr_java-1.4.so $PATH_INSTALL/libmpfr_java.so \
+   && cp target/mpfr_java-1.4.jar $PATH_INSTALL/mpfr_java.jar
+
+echo -e "\n\n"
+
+echo "Postprocessing..."
+cd $PATH_INSTALL \
+   && strip libgmp.so libmpfr.so libmpfr_java.so \
+   && patchelf --set-rpath '$ORIGIN' --replace-needed libgmp.so.10 libgmp.so libmpfr.so \
+   && patchelf --set-rpath '$ORIGIN' --replace-needed libmpfr.so.6 libmpfr.so --replace-needed libgmp.so.10 libgmp.so libmpfr_java.so
+
+echo "Done!"
