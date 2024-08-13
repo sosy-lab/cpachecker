e051ccebd3a7410e158421465cc840d4089548a8
mjankola <mjankola@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2024-08-13 14:05:44 +0000

    introducing pattern matching for addition and substraction
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/locate-loop-and-live-variables@47570 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/core/algorithm/instrumentation/InstrumentationPattern.java
+++ src/org/sosy_lab/cpachecker/core/algorithm/instrumentation/InstrumentationPattern.java
@@ -14,7 +14,22 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 import javax.annotation.Nullable;
+import org.eclipse.jdt.core.dom.ASTNode;
 import org.mockito.internal.matchers.Null;
+import org.sosy_lab.cpachecker.cfa.ast.AAstNode;
+import org.sosy_lab.cpachecker.cfa.ast.ALeftHandSide;
+import org.sosy_lab.cpachecker.cfa.ast.ARightHandSide;
+import org.sosy_lab.cpachecker.cfa.ast.c.CAssignment;
+import org.sosy_lab.cpachecker.cfa.ast.c.CBinaryExpression;
+import org.sosy_lab.cpachecker.cfa.ast.c.CBinaryExpression.BinaryOperator;
+import org.sosy_lab.cpachecker.cfa.ast.c.CExpression;
+import org.sosy_lab.cpachecker.cfa.ast.c.CFunctionCall;
+import org.sosy_lab.cpachecker.cfa.ast.c.CFunctionCallExpression;
+import org.sosy_lab.cpachecker.cfa.ast.c.CInitializer;
+import org.sosy_lab.cpachecker.cfa.ast.c.CInitializerExpression;
+import org.sosy_lab.cpachecker.cfa.ast.c.CReturnStatement;
+import org.sosy_lab.cpachecker.cfa.ast.c.CUnaryExpression;
+import org.sosy_lab.cpachecker.cfa.ast.c.CVariableDeclaration;
 import org.sosy_lab.cpachecker.cfa.model.CFAEdge;
 import org.sosy_lab.cpachecker.cfa.model.c.CAssumeEdge;
 
@@ -70,8 +85,99 @@ public class InstrumentationPattern {
       case TRUE -> ImmutableList.of();
       case COND -> isOriginalCond(pCFAEdge) ? ImmutableList.of() : null;
       case NOT_COND -> isNegatedCond(pCFAEdge) ? ImmutableList.of() : null;
+      case ADD -> getTheOperandsFromAdd(pCFAEdge);
+      case SUB -> getTheOperandsFromSub(pCFAEdge);
       default -> null;
     };
+  }
+
+  @Nullable
+  private ImmutableList<String> getTheOperandsFromAdd(CFAEdge pCFAEdge) {
+    if (pCFAEdge.getRawAST().isPresent()) {
+      AAstNode astNode = pCFAEdge.getRawAST().get();
+      CExpression expression = extractExpression(astNode);
+      if (expression instanceof CBinaryExpression
+          && ((CBinaryExpression) expression).getOperator().equals(BinaryOperator.PLUS)) {
+          return ImmutableList.of(((CBinaryExpression) expression).getOperand1().toASTString(),
+              ((CBinaryExpression) expression).getOperand2().toASTString());
+      }
+    }
+    return null;
+  }
+
+  @Nullable
+  private ImmutableList<String> getTheOperandsFromSub(CFAEdge pCFAEdge) {
+    if (pCFAEdge.getRawAST().isPresent()) {
+      AAstNode astNode = pCFAEdge.getRawAST().get();
+      CExpression expression = extractExpression(astNode);
+      if (expression instanceof CBinaryExpression
+          && ((CBinaryExpression) expression).getOperator().equals(BinaryOperator.MINUS)) {
+        return ImmutableList.of(((CBinaryExpression) expression).getOperand1().toASTString(),
+            ((CBinaryExpression) expression).getOperand2().toASTString());
+      }
+    }
+    return null;
+  }
+
+  @Nullable
+  private CExpression extractExpression(AAstNode pAAstNode) {
+    if (pAAstNode instanceof CReturnStatement) {
+      // return statement
+      Optional<CExpression> optionalCExpression = ((CReturnStatement) pAAstNode).getReturnValue();
+      if (optionalCExpression.isPresent()) {
+        CExpression cExpression = optionalCExpression.get();
+        if (cExpression instanceof CBinaryExpression) {
+          return (CBinaryExpression) cExpression;
+        } else if (cExpression instanceof CUnaryExpression){
+          return (CUnaryExpression) cExpression;
+        }
+      }
+    } else if (pAAstNode instanceof CAssignment) {
+      // assignment
+      ALeftHandSide leftHandSide = ((CAssignment) pAAstNode).getLeftHandSide();
+      ARightHandSide rightHandSide = ((CAssignment) pAAstNode).getRightHandSide();
+      if (rightHandSide instanceof CFunctionCallExpression) {
+        // function call expression
+        List<CExpression> parameterExpressions = ((CFunctionCallExpression) rightHandSide).getParameterExpressions();
+        for (CExpression expression : parameterExpressions) {
+          if (expression instanceof CBinaryExpression) {
+            return (CBinaryExpression) expression;
+          }
+          if (expression instanceof CUnaryExpression) {
+            return (CUnaryExpression) expression;
+          }
+        }
+      } else if (rightHandSide instanceof CBinaryExpression) {
+        return (CBinaryExpression) rightHandSide;
+      } else if (rightHandSide instanceof CUnaryExpression){
+        return (CUnaryExpression) rightHandSide;
+      }
+    } else if (pAAstNode instanceof CBinaryExpression) {
+      // binary expression
+      return (CBinaryExpression) pAAstNode;
+    } else if (pAAstNode instanceof CFunctionCall) {
+      // function call
+      CFunctionCallExpression cFunctionCallExpression = ((CFunctionCall) pAAstNode).getFunctionCallExpression();
+      List<CExpression> parameterExpressions = cFunctionCallExpression.getParameterExpressions();
+      for (CExpression expression : parameterExpressions) {
+        if (expression instanceof CBinaryExpression) {
+          return (CBinaryExpression) expression;
+        }
+        if (expression instanceof CUnaryExpression) {
+          return (CUnaryExpression) expression;
+        }
+      }
+    } else if (pAAstNode instanceof CVariableDeclaration) {
+      // variable declaration
+      CInitializer cInitializer = ((CVariableDeclaration) pAAstNode).getInitializer();
+      if (cInitializer instanceof CInitializerExpression) {
+        CExpression cExpression = ((CInitializerExpression) cInitializer).getExpression();
+        if (cExpression instanceof CBinaryExpression) {
+          return (CBinaryExpression) cExpression;
+        }
+      }
+    }
+    return null;
   }
 
   private boolean isNegatedCond(CFAEdge pCFAEdge) {
