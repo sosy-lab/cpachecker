2e5180f453cab08f36b8100b89407c15d676b37f
dbaier <dbaier@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2024-08-28 10:26:27 +0000

    Format
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/update-javasmt-to-v5.0.0@47726 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/util/predicates/smt/BitvectorFormulaManagerView.java
+++ src/org/sosy_lab/cpachecker/util/predicates/smt/BitvectorFormulaManagerView.java
@@ -81,8 +81,8 @@ public class BitvectorFormulaManagerView extends BaseManagerView
   /**
    * Deprecated and unsupported operation.
    *
-   * This method returns the formula for the C99-conform MODULO-operator, which is rounded towards
-   * zero. SMTlib2 rounds towards positive or negative infinity, depending on both operands.
+   * <p>This method returns the formula for the C99-conform MODULO-operator, which is rounded
+   * towards zero. SMTlib2 rounds towards positive or negative infinity, depending on both operands.
    *
    * <p>Example: SMTlib2: 10%3==1, 10%(-3)==1, (-10)%3==2, (-10)%(-3)==2 C99: 10%3==1, 10%(-3)==1,
    * (-10)%3==(-1), (-10)%(-3)==(-1)
