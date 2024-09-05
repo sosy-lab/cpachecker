6e751e682b5da49c583f41ba9109417456ce9b52
draffler <draffler@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2024-09-05 22:40:55 +0000

    Added two test classes to find out how to best map the C operators % and / to SMT-LIB2 formulas for integer and bitvector logic
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/improve_smt_modulo_and_remainder_behavior_for_c@47806 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

---
+++ src/org/sosy_lab/cpachecker/util/predicates/smt/RemainderBitvectorTest.java
@@ -0,0 +1,129 @@
+// This file is part of CPAchecker,
+// a tool for configurable software verification:
+// https://cpachecker.sosy-lab.org
+//
+// SPDX-FileCopyrightText: 2024 Dirk Beyer <https://www.sosy-lab.org>
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package org.sosy_lab.cpachecker.util.predicates.smt;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import com.google.common.base.Preconditions;
+import com.google.common.collect.ImmutableList;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+import org.sosy_lab.common.ShutdownNotifier;
+import org.sosy_lab.common.configuration.Configuration;
+import org.sosy_lab.common.configuration.InvalidConfigurationException;
+import org.sosy_lab.common.log.LogManager;
+import org.sosy_lab.cpachecker.util.Pair;
+import org.sosy_lab.cpachecker.util.predicates.smt.FormulaManagerView.Theory;
+import org.sosy_lab.java_smt.SolverContextFactory.Solvers;
+import org.sosy_lab.java_smt.api.BitvectorFormula;
+import org.sosy_lab.java_smt.api.ProverEnvironment;
+import org.sosy_lab.java_smt.api.SolverContext.ProverOptions;
+import org.sosy_lab.java_smt.api.SolverException;
+
+@RunWith(Parameterized.class)
+public class RemainderBitvectorTest {
+  private Solver solver;
+  private FormulaManagerView fmgr;
+  private BooleanFormulaManagerView bfmgr;
+  private BitvectorFormulaManagerView bvfmgr;
+
+  private final Solvers solverName;
+
+  @Parameters(name = "{0}")
+  public static Solvers[] data() {
+    return new Solvers[] {
+      // Solvers.MATHSAT5,
+      Solvers.Z3, Solvers.PRINCESS, Solvers.BOOLECTOR, Solvers.CVC4, Solvers.CVC5, Solvers.BITWUZLA
+    };
+  }
+
+  public RemainderBitvectorTest(Solvers pSolvers) {
+    try {
+      Configuration config =
+          Configuration.builder()
+              .setOption("solver.solver", pSolvers.toString())
+              .setOption("cpa.predicate.encodeFloatAs", Theory.UNSUPPORTED.toString())
+              .setOption("cpa.predicate.encodeIntegerAs", Theory.UNSUPPORTED.toString())
+              .build();
+      ShutdownNotifier notifier = ShutdownNotifier.createDummy();
+      LogManager logger = LogManager.createTestLogManager();
+
+      solver = Solver.create(config, logger, notifier);
+      solverName = pSolvers;
+      fmgr = solver.getFormulaManager();
+      bfmgr = fmgr.getBooleanFormulaManager();
+      bvfmgr = fmgr.getBitvectorFormulaManager();
+    } catch (InvalidConfigurationException pE) {
+      throw new RuntimeException(pE);
+    }
+  }
+
+  ImmutableList<Pair<Integer, Integer>> testValues =
+      ImmutableList.of(
+          Pair.of(5, 3),
+          Pair.of(5, -3),
+          Pair.of(-5, 3),
+          Pair.of(-5, -3),
+          Pair.of(3, 2),
+          Pair.of(3, -2),
+          Pair.of(-3, 2),
+          Pair.of(-3, -2),
+          Pair.of(1, 2),
+          Pair.of(1, -2),
+          Pair.of(-1, 2),
+          Pair.of(-1, -2));
+
+  @Test
+  public void BitvectorDivisionTest() {
+    for (Pair<Integer, Integer> value : testValues) {
+      int x = value.getFirstNotNull();
+      int y = value.getSecondNotNull();
+      assertThat(
+              eval(bvfmgr.divide(bvfmgr.makeBitvector(32, x), bvfmgr.makeBitvector(32, y), true)))
+          .isEqualTo(x / y);
+    }
+  }
+
+  @Test
+  public void BitvectorRemainderTest() {
+    for (Pair<Integer, Integer> value : testValues) {
+      int x = value.getFirstNotNull();
+      int y = value.getSecondNotNull();
+      assertThat(
+              eval(
+                  bvfmgr.remainder(bvfmgr.makeBitvector(32, x), bvfmgr.makeBitvector(32, y), true)))
+          .isEqualTo(x % y);
+    }
+  }
+
+  @Test
+  public void BitvectorModuloTest() {
+    for (Pair<Integer, Integer> value : testValues) {
+      int x = value.getFirstNotNull();
+      int y = value.getSecondNotNull();
+      assertThat(eval(bvfmgr.smodulo(bvfmgr.makeBitvector(32, x), bvfmgr.makeBitvector(32, y))))
+          .isEqualTo(Math.floorMod(x, y));
+    }
+  }
+
+  public int eval(BitvectorFormula pFormula) {
+    BitvectorFormula var = bvfmgr.makeVariable(32, "v");
+    try (ProverEnvironment prover = solver.newProverEnvironment(ProverOptions.GENERATE_MODELS)) {
+      prover.push(bvfmgr.equal(var, pFormula));
+      Preconditions.checkArgument(!prover.isUnsat());
+      return prover.getModel().evaluate(var).intValue();
+    } catch (InterruptedException pE) {
+      return 0;
+    } catch (SolverException pE) {
+      throw new RuntimeException(pE);
+    }
+  }
+}
---
+++ src/org/sosy_lab/cpachecker/util/predicates/smt/RemainderIntegerTest.java
@@ -0,0 +1,125 @@
+// This file is part of CPAchecker,
+// a tool for configurable software verification:
+// https://cpachecker.sosy-lab.org
+//
+// SPDX-FileCopyrightText: 2024 Dirk Beyer <https://www.sosy-lab.org>
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package org.sosy_lab.cpachecker.util.predicates.smt;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import com.google.common.base.Preconditions;
+import com.google.common.collect.ImmutableList;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+import org.sosy_lab.common.ShutdownNotifier;
+import org.sosy_lab.common.configuration.Configuration;
+import org.sosy_lab.common.configuration.InvalidConfigurationException;
+import org.sosy_lab.common.log.LogManager;
+import org.sosy_lab.cpachecker.util.Pair;
+import org.sosy_lab.cpachecker.util.predicates.smt.FormulaManagerView.Theory;
+import org.sosy_lab.java_smt.SolverContextFactory.Solvers;
+import org.sosy_lab.java_smt.api.NumeralFormula.IntegerFormula;
+import org.sosy_lab.java_smt.api.ProverEnvironment;
+import org.sosy_lab.java_smt.api.SolverContext.ProverOptions;
+import org.sosy_lab.java_smt.api.SolverException;
+
+@RunWith(Parameterized.class)
+public class RemainderIntegerTest {
+  private Solver solver;
+  private FormulaManagerView fmgr;
+  private BooleanFormulaManagerView bfmgr;
+  private IntegerFormulaManagerView ifmgr;
+
+  private final Solvers solverName;
+
+  @Parameters(name = "{0}")
+  public static Solvers[] data() {
+    return new Solvers[] {
+      Solvers.OPENSMT, Solvers.SMTINTERPOL, Solvers.Z3, Solvers.PRINCESS, Solvers.CVC4, Solvers.CVC5
+    };
+  }
+
+  public RemainderIntegerTest(Solvers pSolvers) {
+    try {
+      Configuration config =
+          Configuration.builder()
+              .setOption("solver.solver", pSolvers.toString())
+              .setOption("solver.nonLinearArithmetic", "USE")
+              .setOption("cpa.predicate.encodeFloatAs", Theory.UNSUPPORTED.toString())
+              .setOption("cpa.predicate.encodeBitvectorAs", Theory.UNSUPPORTED.toString())
+              .build();
+      ShutdownNotifier notifier = ShutdownNotifier.createDummy();
+      LogManager logger = LogManager.createTestLogManager();
+
+      solver = Solver.create(config, logger, notifier);
+      solverName = pSolvers;
+      fmgr = solver.getFormulaManager();
+      bfmgr = fmgr.getBooleanFormulaManager();
+      ifmgr = fmgr.getIntegerFormulaManager();
+    } catch (InvalidConfigurationException pE) {
+      throw new RuntimeException(pE);
+    }
+  }
+
+  ImmutableList<Pair<Integer, Integer>> testValues =
+      ImmutableList.of(
+          Pair.of(5, 3),
+          Pair.of(5, -3),
+          Pair.of(-5, 3),
+          Pair.of(-5, -3),
+          Pair.of(3, 2),
+          Pair.of(3, -2),
+          Pair.of(-3, 2),
+          Pair.of(-3, -2),
+          Pair.of(1, 2),
+          Pair.of(1, -2),
+          Pair.of(-1, 2),
+          Pair.of(-1, -2));
+
+  @Test
+  public void IntegerDivisionTest() {
+    for (Pair<Integer, Integer> value : testValues) {
+      int x = value.getFirstNotNull();
+      int y = value.getSecondNotNull();
+      assertThat(eval(ifmgr.divide(ifmgr.makeNumber(x), ifmgr.makeNumber(y)))).isEqualTo(x / y);
+    }
+  }
+
+  @Test
+  public void IntegerRemainderTest() {
+    for (Pair<Integer, Integer> value : testValues) {
+      int x = value.getFirstNotNull();
+      int y = value.getSecondNotNull();
+      assertThat(eval(ifmgr.remainder(ifmgr.makeNumber(x), ifmgr.makeNumber(y), bfmgr)))
+          .isEqualTo(x % y);
+    }
+  }
+
+  @Test
+  public void IntegerModuloTest() {
+    for (Pair<Integer, Integer> value : testValues) {
+      int x = value.getFirstNotNull();
+      int y = value.getSecondNotNull();
+      assertThat(eval(ifmgr.modulo(ifmgr.makeNumber(x), ifmgr.makeNumber(y))))
+          .isEqualTo(Math.floorMod(x, y));
+    }
+  }
+
+  public int eval(IntegerFormula pFormula) {
+    IntegerFormula var = ifmgr.makeVariable("v");
+    try (ProverEnvironment prover = solver.newProverEnvironment(ProverOptions.GENERATE_MODELS)) {
+      prover.push(ifmgr.equal(var, pFormula));
+      Preconditions.checkArgument(!prover.isUnsat());
+      return prover.getModel().evaluate(var).intValueExact();
+    } catch (InterruptedException pE) {
+      return 0;
+    } catch (SolverException pE) {
+      throw new RuntimeException(pE);
+    }
+  }
+}
