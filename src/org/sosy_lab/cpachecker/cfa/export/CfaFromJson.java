b98fc663052d3ed66c512cba073e66e4f4e491eb
flindenmeier <flindenmeier@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2024-09-27 06:44:04 +0000

    Refactored CfaFromJson.
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/lindenmeier-1-cfa-json-export-prototype@48014 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/cfa/export/CfaFromJson.java
+++ src/org/sosy_lab/cpachecker/cfa/export/CfaFromJson.java
@@ -50,6 +50,50 @@ public final class CfaFromJson {
       "org.sosy_lab.cpachecker.util.ast.AstCfaRelation$StartingLocation";
 
   /**
+   * Reads a {@link MutableCFA} from a JSON file.
+   *
+   * @param pCfaJsonFile The path to the JSON file containing the {@link CfaJsonModule.CfaJsonData}
+   *     record.
+   * @return The {@link MutableCFA} object read from the JSON file.
+   * @throws JsonParseException If there is an error parsing the JSON file.
+   * @throws JsonMappingException If there is an error mapping the JSON data to the {@link
+   *     CfaJsonModule.CfaJsonData} record.
+   * @throws IOException If there is an error reading the JSON file.
+   */
+  public static MutableCFA read(@Nullable Path pCfaJsonFile)
+      throws JsonParseException, JsonMappingException, IOException {
+
+    checkState(pCfaJsonFile != null, "No JSON file specified");
+
+    /* Read CfaJsonData from file. */
+    CfaJsonData cfaJsonData =
+        getImportingObjectMapper()
+            .readValue(pCfaJsonFile.toFile(), CfaJsonModule.CfaJsonData.class);
+
+    return new MutableCFA(cfaJsonData.functions(), cfaJsonData.nodes(), cfaJsonData.metadata());
+  }
+
+  /**
+   * Reads a {@link JsonNode} from the specified file path.
+   *
+   * <p>The node represents the whole JSON content of the file.
+   *
+   * @param pCfaJsonFile The path to the JSON file, can be null.
+   * @return the JSON node read from the file.
+   * @throws JsonParseException if the JSON content is not valid.
+   * @throws JsonMappingException if the JSON content cannot be mapped to a JSON node.
+   * @throws IOException if an I/O error occurs while reading the file.
+   */
+  public static JsonNode readJsonNode(@Nullable Path pCfaJsonFile)
+      throws JsonParseException, JsonMappingException, IOException {
+
+    checkState(pCfaJsonFile != null, "No JSON file specified");
+
+    /* Read JsonNode from file. */
+    return getImportingObjectMapper().readTree(pCfaJsonFile.toFile());
+  }
+
+  /**
    * Retrieves the StartingLocation record.
    *
    * <p>This workaround is necessary because the record is not directly accessible.
@@ -128,49 +172,5 @@ public final class CfaFromJson {
         throws IOException, JsonProcessingException {
       throw new UnsupportedOperationException("Not implemented");
     }
-  }
-
-  /**
-   * Reads a {@link MutableCFA} from a JSON file.
-   *
-   * @param pCfaJsonFile The path to the JSON file containing the {@link CfaJsonModule.CfaJsonData}
-   *     record.
-   * @return The {@link MutableCFA} object read from the JSON file.
-   * @throws JsonParseException If there is an error parsing the JSON file.
-   * @throws JsonMappingException If there is an error mapping the JSON data to the {@link
-   *     CfaJsonModule.CfaJsonData} record.
-   * @throws IOException If there is an error reading the JSON file.
-   */
-  public static MutableCFA read(@Nullable Path pCfaJsonFile)
-      throws JsonParseException, JsonMappingException, IOException {
-
-    checkState(pCfaJsonFile != null, "No JSON file specified.");
-
-    /* Read CfaJsonData from file. */
-    CfaJsonData cfaJsonData =
-        getImportingObjectMapper()
-            .readValue(pCfaJsonFile.toFile(), CfaJsonModule.CfaJsonData.class);
-
-    return new MutableCFA(cfaJsonData.functions(), cfaJsonData.nodes(), cfaJsonData.metadata());
-  }
-
-  /**
-   * Reads a {@link JsonNode} from the specified file path.
-   *
-   * <p>The node represents the whole JSON content of the file.
-   *
-   * @param pCfaJsonFile The path to the JSON file, can be null.
-   * @return the JSON node read from the file.
-   * @throws JsonParseException if the JSON content is not valid.
-   * @throws JsonMappingException if the JSON content cannot be mapped to a JSON node.
-   * @throws IOException if an I/O error occurs while reading the file.
-   */
-  public static JsonNode readJsonNode(@Nullable Path pCfaJsonFile)
-      throws JsonParseException, JsonMappingException, IOException {
-
-    checkState(pCfaJsonFile != null, "No JSON file specified.");
-
-    /* Read JsonNode from file. */
-    return getImportingObjectMapper().readTree(pCfaJsonFile.toFile());
   }
 }
