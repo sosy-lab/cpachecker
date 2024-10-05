46353b673be02ae71dab8467d72538abe3993eb7
flindenmeier <flindenmeier@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2024-10-05 10:54:59 +0000

    Added overview for the serialization and deserialization of partitions.
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/lindenmeier-1-cfa-json-export-prototype@48116 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/cfa/export/json/CfaJsonData.java
+++ src/org/sosy_lab/cpachecker/cfa/export/json/CfaJsonData.java
@@ -14,18 +14,53 @@ import com.google.common.collect.TreeMultimap;
 import java.util.NavigableMap;
 import java.util.Set;
 import org.sosy_lab.cpachecker.cfa.CfaMetadata;
+import org.sosy_lab.cpachecker.cfa.export.json.deserialization.CfaEdgeIdResolver;
+import org.sosy_lab.cpachecker.cfa.export.json.deserialization.EdgeToPartitionsDeserializer;
+import org.sosy_lab.cpachecker.cfa.export.json.deserialization.PartitionIdResolver;
 import org.sosy_lab.cpachecker.cfa.export.json.deserialization.PartitionsDeserializer;
+import org.sosy_lab.cpachecker.cfa.export.json.serialization.CfaEdgeIdGenerator;
+import org.sosy_lab.cpachecker.cfa.export.json.serialization.EdgeToPartitionsTableToListConverter;
+import org.sosy_lab.cpachecker.cfa.export.json.serialization.PartitionSetToSortedListConverter;
 import org.sosy_lab.cpachecker.cfa.export.json.serialization.PartitionsSerializer;
 import org.sosy_lab.cpachecker.cfa.model.CFAEdge;
 import org.sosy_lab.cpachecker.cfa.model.CFANode;
 import org.sosy_lab.cpachecker.cfa.model.FunctionEntryNode;
 import org.sosy_lab.cpachecker.util.variableclassification.Partition;
+import org.sosy_lab.cpachecker.util.variableclassification.VariableClassification;
 
 /**
  * This record represents the JSON data structure that a CFA is exported to and imported from.
  *
  * <p>The order of the fields is important for the deserialization process! Have a look at {@link
  * PartitionsDeserializer} for more information.
+ *
+ * <p>The serialization and deserialization of {@link Partition}s is only necessary if we want to
+ * serialize and deserialize the {@link VariableClassification} field of {@link CfaMetadata}.
+ *
+ * <p>Involved in this serialization process are:
+ *
+ * <ul>
+ *   <li>{@link PartitionsSerializer}
+ *   <li>{@link CfaEdgeIdGenerator}
+ *   <li>{@link EdgeToPartitionsTableToListConverter}
+ *   <li>{@link PartitionSetToSortedListConverter}
+ * </ul>
+ *
+ * <p>Involved in this deserialization process are:
+ *
+ * <ul>
+ *   <li>{@link PartitionsDeserializer}
+ *   <li>{@link CfaEdgeIdResolver}
+ *   <li>{@link EdgeToPartitionsDeserializer}
+ *   <li>{@link PartitionIdResolver}
+ * </ul>
+ *
+ * <p>Involved in both serialization and deserialization are:
+ *
+ * <ul>
+ *   <li>{@link PartitionHandler}
+ *   <li>{@link EdgeToPartitionEntry}
+ * </ul>
  *
  * @see CfaJsonImport
  * @see CfaJsonExport
