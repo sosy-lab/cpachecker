771b067490bf51a4ccf3a2ea4e0a46a637cf2e48
flindenmeier <flindenmeier@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2024-10-06 15:57:43 +0000

    More documentation for BracketRemoverConverter.
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/lindenmeier-1-cfa-json-export-prototype@48121 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/cfa/export/json/deserialization/BracketRemoverConverter.java
+++ src/org/sosy_lab/cpachecker/cfa/export/json/deserialization/BracketRemoverConverter.java
@@ -20,6 +20,27 @@ import org.sosy_lab.cpachecker.cfa.export.json.mixins.CAssumeEdgeMixin;
  *
  * <p>If the input string is null, null is returned.
  *
+ * <p>For example, if this converter is applied to a field "rawStatement" like this:
+ *
+ * <pre>
+ * *AT*JsonDeserialize(converter=BracketRemoverConverter.class)
+ * private String rawStatement;
+ * </pre>
+ *
+ * <p>It acts like a filter that removes brackets from the field before it is passed to the
+ * constructor of the class that contains the field.
+ *
+ * <p>Examples:
+ *
+ * <pre>
+ * <table border="1">
+ *   <tr><th>JSON</th>                             <th>Converter</th>        <th>Constructor</th></tr>
+ *   <tr><td>"rawStatement": "[i == 20]"</td><td>-> "[i == 20]"</td>   <td>-> "i == 20"</td></tr>
+ *   <tr><td>"rawStatement": ""</td>         <td>-> ""</td>            <td>-> ""</td></tr>
+ *   <tr><td>"rawStatement": null</td>       <td>-> null</td>          <td>-> null</td></tr>
+ * </table>
+ * </pre>
+ *
  * @see CfaJsonImport
  * @see CAssumeEdgeMixin
  */
