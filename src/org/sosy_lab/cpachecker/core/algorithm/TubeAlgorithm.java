1af43ae5750d9cd3b6ac785f8a097fc628f877f5
okatz <okatz@4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c>
2024-10-13 02:31:15 +0000

    new functionality to open a json with options and solved issue in approx functionality in TubeAlgorithm
    git-svn-id: https://svn.sosy-lab.org/software/cpachecker/branches/error-condition-cpa@48217 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

--- src/org/sosy_lab/cpachecker/core/algorithm/TubeAlgorithm.java
+++ src/org/sosy_lab/cpachecker/core/algorithm/TubeAlgorithm.java
@@ -8,6 +8,11 @@
 
 package org.sosy_lab.cpachecker.core.algorithm;
 
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 import org.sosy_lab.common.log.LogManager;
 import org.sosy_lab.cpachecker.core.interfaces.AbstractState;
 import org.sosy_lab.cpachecker.core.reachedset.ReachedSet;
@@ -21,13 +26,13 @@ import java.util.logging.Level;
 /**
  * This class represents a TubeAlgorithm that performs additional operations on the reached set after running another algorithm.
  */
-public class TubeAlgorithm implements Algorithm{
-  /**
-   *
-   */
-  private final Algorithm algorithm;
-  private final LogManager logger;
 
+
+public class TubeAlgorithm implements Algorithm{
+  private Map<ARGState,String> stateClassification;
+  private final Algorithm algorithm;
+
+  private final LogManager logger;
   /**
    * This class represents a TubeAlgorithm that performs additional operations on the reached set after running another algorithm.
    */
@@ -35,6 +40,7 @@ public class TubeAlgorithm implements Algorithm{
     algorithm = pAlgorithm;
 
     this.logger = pLogger;
+    this.stateClassification = new HashMap<>();
   }
   /**
    * Runs the algorithm on the given reached set and performs additional operations.
@@ -47,53 +53,49 @@ public class TubeAlgorithm implements Algorithm{
   @Override
   public AlgorithmStatus run(ReachedSet reachedSet) throws CPAException, InterruptedException {
     AlgorithmStatus status = algorithm.run(reachedSet);
+
     for (AbstractState abstractState : reachedSet) {
       ARGState state = (ARGState) abstractState;
-      if (state.getChildren().isEmpty()) { //instead of just this state, check all the states and based on their answers i can then use the logic from the next TODO
 
+      if (state.getChildren().isEmpty()) { // This state is a leaf-node
         TubeState tubeState = AbstractStates.extractStateByType(state, TubeState.class);
-        //TODO 1. find all leafs that has been negated at least once --> safe (no error) print: over to check if an error: errorCount >0
-        //2. if there is no negation in the leafs from 1. && all leafs are error print under
-        //3. if its noth -- precise
-        //4. else: unclassified
-        //in my json: x<8 will be precise
-        // if x<6 its under
-        //if less then 50 its over
-        // if its 10 unclassified
-        // go to sv benchmarks and identify types of programs. pick 20 and create under/over/precise jsons for them. if ill have extra time to automate a json creator script with under over precice
+
         if (tubeState != null) {
+          String classification;
           if (isTubeOverApprox(tubeState) && isTubeUnderApprox(tubeState)){
-            logger.log(Level.INFO,"precise");
-          }else if (isTubeUnderApprox(tubeState)){
-            logger.log(Level.INFO,"under");
-          }else if (isTubeOverApprox(tubeState)){
-            logger.log(Level.INFO,"over");
+            classification = "precise";
+          } else if (isTubeUnderApprox(tubeState)){
+            classification = "under";
+          } else if (isTubeOverApprox(tubeState)) {
+            classification = "over";
+          } else {
+            classification = "unclassified";
           }
+          stateClassification.put(state, classification); // Save the classification
         }
       }
+    }
+    Set<String> uniqueValues = new HashSet<>(stateClassification.values());
+
+    if (uniqueValues.size() == 1 && uniqueValues.contains("over")) {
+      logger.log(Level.INFO, "over");
+    } else if (uniqueValues.size() == 1 && uniqueValues.contains("under")) {
+      logger.log(Level.INFO, "under");
+    } else if (uniqueValues.contains("over") && uniqueValues.contains("under")) {
+      logger.log(Level.INFO, "precise");
+    } else {
+      logger.log(Level.INFO, "unclassified");
     }
     return status;
   }
 
-  /**
-   * Checks if the given TubeState represents a tube that is under approximation.
-   *
-   * @param tubeState The TubeState to check.
-   * @return True if the tube is under approximation, false otherwise.
-   */
   private boolean isTubeUnderApprox(TubeState tubeState) {
     // first rule: if there is no negation on the TubeState,
-    // and all leafs have an error count greater then 0,
+    // and all leafs have an error count greater than 0,
     // then the tube is under approx.
     return !tubeState.isNegated() && tubeState.getErrorCounter() >= 1;
   }
 
-  /**
-   * Checks if the given TubeState represents a tube that is over approximation.
-   *
-   * @param tubeState The TubeState to check.
-   * @return True if the tube is over approximation, false otherwise.
-   */
   private boolean isTubeOverApprox(TubeState tubeState) {
     // second rule: if there is at least one negation in the TubeState
     // and all leafs have error count of 0, then the tube is over approx.
--- src/org/sosy_lab/cpachecker/cpa/tube/TubeCPA.java
+++ src/org/sosy_lab/cpachecker/cpa/tube/TubeCPA.java
@@ -8,97 +8,57 @@
 
 package org.sosy_lab.cpachecker.cpa.tube;
 
-import static com.google.common.base.Preconditions.checkState;
-
-import ap.terfor.Formula;
 import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.base.Function;
 import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.ImmutableMap.Builder;
-import com.google.common.collect.ImmutableSet;
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Deque;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
-import java.util.Optional;
 import java.util.logging.Level;
 import org.sosy_lab.common.ShutdownNotifier;
 import org.sosy_lab.common.configuration.Configuration;
+import org.sosy_lab.common.configuration.FileOption;
+import org.sosy_lab.common.configuration.FileOption.Type;
 import org.sosy_lab.common.configuration.InvalidConfigurationException;
+import org.sosy_lab.common.configuration.Option;
+import org.sosy_lab.common.configuration.Options;
 import org.sosy_lab.common.log.LogManager;
 import org.sosy_lab.cpachecker.cfa.CFA;
-import org.sosy_lab.cpachecker.cfa.CParser;
-import org.sosy_lab.cpachecker.cfa.CParser.Factory;
-import org.sosy_lab.cpachecker.cfa.CProgramScope;
-import org.sosy_lab.cpachecker.cfa.ast.AExpression;
-import org.sosy_lab.cpachecker.cfa.ast.FileLocation;
-import org.sosy_lab.cpachecker.cfa.ast.c.*;
 import org.sosy_lab.cpachecker.cfa.model.CFAEdge;
 import org.sosy_lab.cpachecker.cfa.model.CFANode;
-import org.sosy_lab.cpachecker.cfa.types.c.CType;
 import org.sosy_lab.cpachecker.core.AnalysisDirection;
 import org.sosy_lab.cpachecker.core.defaults.AbstractCPA;
 import org.sosy_lab.cpachecker.core.defaults.FlatLatticeDomain;
 import org.sosy_lab.cpachecker.core.interfaces.AbstractState;
 import org.sosy_lab.cpachecker.core.interfaces.CPAFactory;
 import org.sosy_lab.cpachecker.core.interfaces.StateSpacePartition;
-import org.sosy_lab.cpachecker.cpa.automaton.InvalidAutomatonException;
-import org.sosy_lab.cpachecker.exceptions.UnrecognizedCodeException;
-import org.sosy_lab.cpachecker.util.CParserUtils;
-import org.sosy_lab.cpachecker.util.CParserUtils.ParserTools;
-import org.sosy_lab.cpachecker.util.expressions.ExpressionTree;
-import org.sosy_lab.cpachecker.util.expressions.ExpressionTrees;
-import org.sosy_lab.cpachecker.util.predicates.pathformula.ErrorConditions;
-import org.sosy_lab.cpachecker.util.predicates.pathformula.SSAMap;
-import org.sosy_lab.cpachecker.util.predicates.pathformula.SSAMap.SSAMapBuilder;
-import org.sosy_lab.cpachecker.util.predicates.pathformula.ctoformula.Constraints;
 import org.sosy_lab.cpachecker.util.predicates.pathformula.ctoformula.CtoFormulaConverter;
 import org.sosy_lab.cpachecker.util.predicates.pathformula.ctoformula.CtoFormulaTypeHandler;
-import org.sosy_lab.cpachecker.util.predicates.pathformula.ctoformula.ExpressionToFormulaVisitor;
 import org.sosy_lab.cpachecker.util.predicates.pathformula.ctoformula.FormulaEncodingOptions;
-import org.sosy_lab.cpachecker.util.predicates.pathformula.pointeraliasing.PointerTargetSet;
-import org.sosy_lab.cpachecker.util.predicates.pathformula.pointeraliasing.PointerTargetSetBuilder;
-import org.sosy_lab.cpachecker.util.predicates.pathformula.pointeraliasing.PointerTargetSetBuilder.DummyPointerTargetSetBuilder;
 import org.sosy_lab.cpachecker.util.predicates.smt.FormulaManagerView;
-import org.sosy_lab.cpachecker.util.predicates.smt.Solver;
-import org.sosy_lab.java_smt.api.BitvectorFormula;
-import org.sosy_lab.java_smt.api.BooleanFormula;
-import org.sosy_lab.java_smt.api.FormulaType;
-
-
+@Options(prefix = "tubeCPA")
 public class TubeCPA extends AbstractCPA {
-
-  ObjectMapper om = new ObjectMapper();
+  @Option(
+      secure = true,
+      required = true,
+      description =
+          "List of files with tubes")
+  @FileOption(Type.OUTPUT_FILE)
+  private Path initialFile;
+  private final ObjectMapper om = new ObjectMapper();
   private final LogManager logger;
-
-  String path = "C:\\Users\\ozkat\\Desktop/jsonT.json";
-
   private final CFA cfa;
-
 
   CtoFormulaTypeHandler typeHandler;
   private  final Function<FormulaManagerView,CtoFormulaConverter> supplier;
 
 
-    /**
-     * Initialize a new instance of TubeCPA.
-     *
-     * @param config              the configuration object
-     * @param pLogger              the pLogger object
-     * @param pShutdownNotifier   the shutdown notifier object
-     * @param pCfa                the CFA object
-     */
     public TubeCPA(Configuration config, LogManager pLogger,ShutdownNotifier pShutdownNotifier, CFA pCfa) {
         super("sep", "sep", new FlatLatticeDomain(), new TubeTransferRelation());
         try {
+          config.inject(this);
             this.logger = pLogger;
             FormulaEncodingOptions options = new FormulaEncodingOptions(config);
             this.typeHandler = new CtoFormulaTypeHandler(pLogger, pCfa.getMachineModel());
@@ -119,20 +79,12 @@ public class TubeCPA extends AbstractCPA {
         }
     }
 
-    /**
-     * Parses the given JSON file containing formulas and converts them to a map of line numbers to BooleanFormulas.
-     *
-     * @param jsonFilePath The path to the JSON file containing the formulas.
-     * @return An ImmutableMap containing the parsed formulas, with line numbers as keys and BooleanFormulas as values.
-     * @throws IOException If an I/O error occurs while reading the JSON file.
-     */
-    public ImmutableMap<Integer, String> parseJson(String jsonFilePath) throws IOException {
+    public ImmutableMap<Integer, String> parseJson(Path jsonFilePath) throws IOException {
       TypeReference<Map<Integer, String>> typeRef = new TypeReference<Map<Integer, String>>() {};
-      Map<Integer, String> map = om.readValue(Paths.get(jsonFilePath).toFile(), typeRef);
+      Map<Integer, String> map = om.readValue(jsonFilePath.toFile(), typeRef);
 
       return ImmutableMap.copyOf(map);
     }
-
 
   public static CPAFactory factory() {
     return new TubeCPAFactory(AnalysisDirection.FORWARD);
@@ -146,9 +98,9 @@ public class TubeCPA extends AbstractCPA {
   public AbstractState getInitialState(CFANode node, StateSpacePartition partition)
           throws InterruptedException {
       CFAEdge edge = node.getLeavingEdge(0);
-    ImmutableMap<Integer, String> asserts;
-    try {
-          asserts = parseJson(path);
+      ImmutableMap<Integer, String> asserts;
+      try {
+          asserts = parseJson(initialFile);
       } catch (IOException e) {
           asserts = ImmutableMap.of();
           logger.log(Level.SEVERE, "An error occurred while parsing the JSON file", e);
