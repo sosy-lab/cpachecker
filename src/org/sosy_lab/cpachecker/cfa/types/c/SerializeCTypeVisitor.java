5cf7bd31784323637d605a820856a323828cde1d
Thomas Lemberger <lembergerth@gmail.com>
2024-10-23 17:24:15 +0200

    Add missing SPDX headers

--- src/org/sosy_lab/cpachecker/cfa/types/c/SerializeCTypeVisitor.java
+++ src/org/sosy_lab/cpachecker/cfa/types/c/SerializeCTypeVisitor.java
@@ -1,3 +1,11 @@
+// This file is part of CPAchecker,
+// a tool for configurable software verification:
+// https://cpachecker.sosy-lab.org
+//
+// SPDX-FileCopyrightText: 2007-2024 Dirk Beyer <https://www.sosy-lab.org>
+//
+// SPDX-License-Identifier: Apache-2.0
+
 package org.sosy_lab.cpachecker.cfa.types.c;
 
 import org.sosy_lab.cpachecker.cfa.ast.c.CEnumerator;
