c11813a9ba82fe2662aa5799987548c50c747286
BaierD <daniel.baier@sosy.ifi.lmu.de>
2024-10-28 17:11:54 +0100

    SMT Solvers: add more base tests for SMTLib2 division to C

--- src/org/sosy_lab/cpachecker/util/predicates/smt/SolverOperationComparisonToProgrammingLanguageTest.java
+++ src/org/sosy_lab/cpachecker/util/predicates/smt/SolverOperationComparisonToProgrammingLanguageTest.java
@@ -8,7 +8,10 @@
 
 package org.sosy_lab.cpachecker.util.predicates.smt;
 
+import static com.google.common.truth.Truth.assertThat;
+
 import com.google.common.truth.TruthJUnit;
+import java.math.BigInteger;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -19,10 +22,12 @@ import org.sosy_lab.java_smt.SolverContextFactory.Solvers;
 import org.sosy_lab.java_smt.api.BitvectorFormula;
 import org.sosy_lab.java_smt.api.BooleanFormula;
 import org.sosy_lab.java_smt.api.NumeralFormula.IntegerFormula;
+import org.sosy_lab.java_smt.api.ProverEnvironment;
+import org.sosy_lab.java_smt.api.SolverContext.ProverOptions;
 import org.sosy_lab.java_smt.api.SolverException;
 
 /*
- * Note: since we are using the ManagerViews here, we might end up using different theories
+ * Note: when using the ManagerViews here, we might end up using different theories
  * internally compared to what one would expect in the test if a solver does not support a method.
  * This is fine however, as if the solver does not support something, and we have a backup that
  * works as expected, we are good.
@@ -43,10 +48,106 @@ public class SolverOperationComparisonToProgrammingLanguageTest extends SolverVi
     return solverToUse;
   }
 
+  // TODO: find mismatching/matching examples and test only those compared to the -11 - +11 range,
+  //  as this is expensive in some solvers.
+
   @Before
   public void setUp() {
-    imgr = imgrv;
+    // TODO: by testing Views instead of the raw managers, we can test the automatic (CPAchecker
+    // internal) translation of formulas from one theory into another. Test by raw managers and by
+    // views.
     bvmgr = mgrv.getBitvectorFormulaManager();
+  }
+
+  /**
+   * SMTLib2 Integer divide() dividend 0 test for C/Java division. C/Java does not allow div 0,
+   * SMTLib2 allows arbitrary results for div zero. This tests that an unexpected result based on
+   * divisor 0 is possible for otherwise impossible operations. This is not in line how C/Java
+   * handle div by 0.
+   */
+  @Test
+  public void testSmtIntegerDivisionZeroOperationToC()
+      throws SolverException, InterruptedException {
+    requireIntegers();
+    requireNonLinearIntegerDivision();
+    IntegerFormula one = imgr.makeNumber(1);
+    IntegerFormula dividend = imgr.makeNumber(5);
+    IntegerFormula divisor = imgr.makeVariable("divisor");
+    IntegerFormula div = imgr.divide(dividend, divisor);
+    BooleanFormula divEq = imgr.equal(div, one);
+    // Limits the divisor such that all valid models not based on 0 are blocked (divisor is not
+    // 1,2,3,4,5)
+    BooleanFormula limitDivisor =
+        bmgr.or(
+            imgr.lessOrEquals(divisor, imgr.makeNumber(0)),
+            imgr.greaterThan(divisor, imgr.makeNumber(5)));
+    BooleanFormula assertion = bmgr.and(divEq, limitDivisor);
+    // SMT returns only models based on divisor == 0
+    try (ProverEnvironment prover = context.newProverEnvironment(ProverOptions.GENERATE_MODELS)) {
+      prover.push(assertion);
+      // SAT
+      assertThat(!prover.isUnsat()).isTrue();
+      assertThat(prover.getModel().evaluate(divisor)).isEqualTo(BigInteger.ZERO);
+    }
+  }
+
+  /**
+   * SMTLib2 Integer divide() base test for C/Java division. C/Java simply truncate integers,
+   * SMTLib2 rounds towards TODO TODO: test edge-cases
+   */
+  @Test
+  public void testSmtIntegerDivisionOperationToC() throws SolverException, InterruptedException {
+    requireIntegers();
+    int dividend = -11;
+    int divisor = -10;
+    int expectedRes = dividend / divisor;
+    IntegerFormula expectedResFormula = imgr.makeNumber(expectedRes);
+    IntegerFormula div = imgr.divide(imgr.makeNumber(dividend), imgr.makeNumber(divisor));
+    BooleanFormula divEq = imgr.equal(div, expectedResFormula);
+    // SMT returns 2, expected 1
+    assertThatFormula(divEq).isUnsatisfiable();
+  }
+
+  /**
+   * Our SMTLib2 Integer division implementation base test for C/Java division. C/Java simply
+   * truncate integers, ... TODO TODO: test edge-cases
+   */
+  @Test
+  public void testOurSmtIntegerDivisionOperationToC() throws SolverException, InterruptedException {
+    requireIntegers();
+    for (int dividend = -11; dividend <= 11; dividend++) {
+      for (int divisor = -11; divisor <= 11; divisor++) {
+        if (divisor == 0) {
+          continue;
+        }
+        int expectedRes = dividend / divisor;
+        IntegerFormula expectedResFormula = imgr.makeNumber(expectedRes);
+        IntegerFormula div = imgrv.cDivide(imgr.makeNumber(dividend), imgr.makeNumber(divisor));
+        BooleanFormula divEq = imgr.equal(div, expectedResFormula);
+        assertThatFormula(divEq).isSatisfiable();
+      }
+    }
+  }
+
+  /** BV division() comparison to C/Java division (/) operator. TODO: text TODO: test edge-cases */
+  @Test
+  public void testSmtBitvectorDivisionOperationToC() throws SolverException, InterruptedException {
+    requireBitvectors();
+    for (int dividend = -11; dividend <= 11; dividend++) {
+      for (int divisor = -11; divisor <= 11; divisor++) {
+        if (divisor == 0) {
+          continue;
+        }
+        int expectedRes = dividend / divisor;
+        BitvectorFormula expectedResFormula = bvmgr.makeBitvector(16, expectedRes);
+        BooleanFormula divEq =
+            bvmgr.equal(
+                bvmgr.divide(
+                    bvmgr.makeBitvector(16, dividend), bvmgr.makeBitvector(16, divisor), true),
+                expectedResFormula);
+        assertThatFormula(divEq).isSatisfiable();
+      }
+    }
   }
 
   /**
@@ -79,6 +180,9 @@ public class SolverOperationComparisonToProgrammingLanguageTest extends SolverVi
     }
   }
 
+  // TODO: ReplaceIntegerWithBitvectorTheory.modulo/div/remainder
+  // TODO: ReplaceBitvectorWithNumeralAndFunctionTheory.modulo/div/remainder
+
   /** Int modulo does not equal the % operator in C/Java! */
   @Test
   public void testSmtIntegerModuloOperationToC() throws SolverException, InterruptedException {
@@ -94,10 +198,8 @@ public class SolverOperationComparisonToProgrammingLanguageTest extends SolverVi
     int divisor = 2;
     int expectedRes = dividend % divisor;
     IntegerFormula expectedResFormula = imgr.makeNumber(expectedRes);
-    // Some of those (e.g. for positive dividend and divisor) are correct, but not all
-    BooleanFormula modEq =
-        imgr.equal(
-            imgrv.modulo(imgr.makeNumber(dividend), imgr.makeNumber(divisor)), expectedResFormula);
+    IntegerFormula mod = imgrv.modulo(imgr.makeNumber(dividend), imgr.makeNumber(divisor));
+    BooleanFormula modEq = imgr.equal(mod, expectedResFormula);
     assertThatFormula(modEq).isUnsatisfiable();
   }
 
