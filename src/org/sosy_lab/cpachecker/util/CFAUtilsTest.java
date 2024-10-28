57f67800283fedd3aade641e886fe6daf7ea7a87
Marian Lingsch-Rosenfeld <Marian.Lingsch@sosy.ifi.lmu.de>
2024-10-28 20:32:19 +0100

    [CFAUtils] Add tests for the function getClosestFullExpression in CFAUtils
    The tests should capture all possible edge cases which should be handled by the function

---
+++ src/org/sosy_lab/cpachecker/util/CFAUtilsTest.java
@@ -0,0 +1,110 @@
+// This file is part of CPAchecker,
+// a tool for configurable software verification:
+// https://cpachecker.sosy-lab.org
+//
+// SPDX-FileCopyrightText: 2024 Dirk Beyer <https://www.sosy-lab.org>
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package org.sosy_lab.cpachecker.util;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import com.google.common.collect.FluentIterable;
+import java.nio.file.Path;
+import java.util.Optional;
+import java.util.Set;
+import java.util.logging.Level;
+import org.junit.Test;
+import org.sosy_lab.common.configuration.Configuration;
+import org.sosy_lab.cpachecker.cfa.CFA;
+import org.sosy_lab.cpachecker.cfa.ast.FileLocation;
+import org.sosy_lab.cpachecker.cfa.model.CFAEdge;
+import org.sosy_lab.cpachecker.util.ast.AstCfaRelation;
+import org.sosy_lab.cpachecker.util.test.CPATestRunner;
+import org.sosy_lab.cpachecker.util.test.TestDataTools;
+import org.sosy_lab.cpachecker.util.test.TestResults;
+
+public class CFAUtilsTest {
+
+  private final Level logLevel = Level.FINEST;
+
+  private TestResults parseProgram(String pProgramName) throws Exception {
+    final Configuration config =
+        TestDataTools.configurationForTest()
+            .setOption("--config", "config/generateCFA.properties")
+            .build();
+
+    String test_dir = "test/programs/cfa-ast-relation/";
+    Path program = Path.of(test_dir, pProgramName);
+
+    return CPATestRunner.run(config, program.toString(), logLevel);
+  }
+
+  /**
+   * Small helper function to get the edge from the CFA that contains the given strings.
+   *
+   * <p>DO NOT USE THIS IF YOU DO NOT KNOW EXACTLY WHAT CFA YOU ARE WORKING WITH!!!!!!!!!
+   */
+  private static CFAEdge getEdge(Set<String> pStringsInEdge, CFA pCFA) {
+    for (CFAEdge edge : CFAUtils.allEdges(pCFA)) {
+      if (FluentIterable.from(pStringsInEdge)
+          .allMatch(string -> edge.toString().contains(string))) {
+        return edge;
+      }
+    }
+    assertThat(false).isTrue();
+    return null;
+  }
+
+  private void testFullExpression(
+      String pProgramName,
+      Set<String> pStringsToIdentifyEdge,
+      int pExpectedStartingLineInOrigin,
+      int pExpectedStartColumnInLine)
+      throws Exception {
+    TestResults results = parseProgram(pProgramName);
+    CFA cfa = results.getCheckerResult().getCfa();
+    assertThat(cfa).isNotNull();
+
+    AstCfaRelation astCfaRelation = cfa.getAstCfaRelation();
+    CFAEdge edge = getEdge(pStringsToIdentifyEdge, cfa);
+    Optional<FileLocation> optionalExpressionLocation =
+        CFAUtils.getClosestFullExpression(edge, astCfaRelation);
+    assertThat(optionalExpressionLocation).isPresent();
+    FileLocation expressionLocation = optionalExpressionLocation.orElseThrow();
+    assertThat(expressionLocation.getStartingLineInOrigin())
+        .isEqualTo(pExpectedStartingLineInOrigin);
+    assertThat(expressionLocation.getStartColumnInLine()).isEqualTo(pExpectedStartColumnInLine);
+  }
+
+  /**
+   * Test that the results from the function {@link CFAUtils#getClosestFullExpression} match those
+   * of a full expression as defined in §6.8 (4) of the C11 standard.
+   *
+   * @throws Exception in case the parsing of the program fails
+   */
+  @Test
+  public void testFullExpressionStartPosition() throws Exception {
+    testFullExpression("full-expression.c", Set.of("x + y"), 10, 10);
+    testFullExpression("full-expression.c", Set.of("x = 1"), 2, 11);
+    testFullExpression("full-expression.c", Set.of("y = 1"), 3, 11);
+    testFullExpression("full-expression.c", Set.of("x = 2"), 4, 3);
+    testFullExpression("full-expression.c", Set.of("y = 2"), 5, 3);
+    testFullExpression("full-expression.c", Set.of("x != 0"), 6, 10);
+    testFullExpression("full-expression.c", Set.of("y != 0"), 6, 10);
+    testFullExpression("full-expression.c", Set.of("z + w"), 13, 30);
+    testFullExpression("full-expression.c", Set.of("f()"), 16, 3);
+    testFullExpression("full-expression.c", Set.of("g(1, 2)"), 17, 3);
+    testFullExpression("full-expression.c", Set.of("j < 0"), 21, 8);
+    testFullExpression("full-expression.c", Set.of("i == 0"), 21, 8);
+    testFullExpression("full-expression.c", Set.of("i < 10"), 21, 25);
+    testFullExpression("full-expression.c", Set.of("j == 0"), 21, 25);
+    testFullExpression("full-expression.c", Set.of("i < 5"), 21, 43);
+    testFullExpression("full-expression.c", Set.of("i != 0"), 21, 43);
+    testFullExpression("full-expression.c", Set.of("s != q"), 27, 11);
+    testFullExpression("full-expression.c", Set.of("s == 1"), 27, 11);
+    testFullExpression("full-expression.c", Set.of("q == 2"), 27, 11);
+    testFullExpression("full-expression.c", Set.of("l = 0"), 28, 11);
+  }
+}
---
+++ test/programs/cfa-ast-relation/full-expression.c
@@ -0,0 +1,29 @@
+int f() {
+  int x = 1;
+  int y = 1;
+  x = 2;
+  y = 2;
+  while (x != 0 && y != 0) {
+    x = x - 1;
+    y = y - 1;
+  }
+  return x + y;
+}
+
+int g(int z, int w) { return z + w; }
+
+int main() {
+  f();
+  g(1, 2);
+
+  int i = 0;
+  int j = 0;
+  for (i == 0 || j < 0; j == 0 && i < 10; i < 5 && i != 0) {
+    i++;
+  }
+
+  int q = 0;
+  int s = 0;
+  int p = s != q ? s == 1 : q == 2;
+  int l = (0);
+}
