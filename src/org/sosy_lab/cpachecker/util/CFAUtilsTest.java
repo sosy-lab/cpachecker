9601cd7fe31c07cc8b28df00ec9b85b4f5bddc13
Marian Lingsch-Rosenfeld <Marian.Lingsch@sosy.ifi.lmu.de>
2024-10-30 17:56:58 +0100

    [CFAUtils] Refactor tests such that the parsing is only done once

--- src/org/sosy_lab/cpachecker/util/CFAUtilsTest.java
+++ src/org/sosy_lab/cpachecker/util/CFAUtilsTest.java
@@ -15,7 +15,6 @@ import com.google.common.collect.FluentIterable;
 import com.google.common.collect.ImmutableSet;
 import java.nio.file.Path;
 import java.util.Optional;
-import java.util.Set;
 import java.util.logging.Level;
 import org.junit.Test;
 import org.sosy_lab.common.configuration.Configuration;
@@ -48,9 +47,9 @@ public class CFAUtilsTest {
    *
    * <p>DO NOT USE THIS IF YOU DO NOT KNOW EXACTLY WHAT CFA YOU ARE WORKING WITH!!!!!!!!!
    */
-  private static CFAEdge getEdge(Set<String> pStringsInEdge, CFA pCFA) {
+  private static CFAEdge getEdge(String pStringsInEdge, CFA pCFA) {
     for (CFAEdge edge : CFAUtils.allEdges(pCFA)) {
-      if (FluentIterable.from(pStringsInEdge)
+      if (FluentIterable.from(ImmutableSet.of(pStringsInEdge))
           .allMatch(string -> edge.toString().contains(string))) {
         return edge;
       }
@@ -60,17 +59,12 @@ public class CFAUtilsTest {
   }
 
   private void testFullExpression(
-      String pProgramName,
-      Set<String> pStringsToIdentifyEdge,
+      CFA pCFA,
+      String pStringsToIdentifyEdge,
       int pExpectedStartingLineInOrigin,
-      int pExpectedStartColumnInLine)
-      throws Exception {
-    TestResults results = parseProgram(pProgramName);
-    CFA cfa = results.getCheckerResult().getCfa();
-    assertThat(cfa).isNotNull();
-
-    AstCfaRelation astCfaRelation = cfa.getAstCfaRelation();
-    CFAEdge edge = getEdge(pStringsToIdentifyEdge, cfa);
+      int pExpectedStartColumnInLine) {
+    AstCfaRelation astCfaRelation = pCFA.getAstCfaRelation();
+    CFAEdge edge = getEdge(pStringsToIdentifyEdge, pCFA);
     Optional<FileLocation> optionalExpressionLocation =
         CFAUtils.getClosestFullExpression(edge, astCfaRelation);
     assertThat(optionalExpressionLocation).isPresent();
@@ -88,25 +82,30 @@ public class CFAUtilsTest {
    */
   @Test
   public void testFullExpressionStartPosition() throws Exception {
-    testFullExpression("full-expression.c", ImmutableSet.of("x + y"), 18, 10);
-    testFullExpression("full-expression.c", ImmutableSet.of("x = 1"), 10, 11);
-    testFullExpression("full-expression.c", ImmutableSet.of("y = 1"), 11, 11);
-    testFullExpression("full-expression.c", ImmutableSet.of("x = 2"), 12, 3);
-    testFullExpression("full-expression.c", ImmutableSet.of("y = 2"), 13, 3);
-    testFullExpression("full-expression.c", ImmutableSet.of("x != 0"), 14, 10);
-    testFullExpression("full-expression.c", ImmutableSet.of("y != 0"), 14, 10);
-    testFullExpression("full-expression.c", ImmutableSet.of("z + w"), 21, 30);
-    testFullExpression("full-expression.c", ImmutableSet.of("f()"), 24, 3);
-    testFullExpression("full-expression.c", ImmutableSet.of("g(1, 2)"), 25, 3);
-    testFullExpression("full-expression.c", ImmutableSet.of("j < 0"), 29, 8);
-    testFullExpression("full-expression.c", ImmutableSet.of("i == 0"), 29, 8);
-    testFullExpression("full-expression.c", ImmutableSet.of("i < 10"), 29, 25);
-    testFullExpression("full-expression.c", ImmutableSet.of("j == 0"), 29, 25);
-    testFullExpression("full-expression.c", ImmutableSet.of("i < 5"), 29, 43);
-    testFullExpression("full-expression.c", ImmutableSet.of("i != 0"), 29, 43);
-    testFullExpression("full-expression.c", ImmutableSet.of("s != q"), 35, 11);
-    testFullExpression("full-expression.c", ImmutableSet.of("s == 1"), 35, 11);
-    testFullExpression("full-expression.c", ImmutableSet.of("q == 2"), 35, 11);
-    testFullExpression("full-expression.c", ImmutableSet.of("l = 0"), 36, 11);
+    String programName = "full-expression.c";
+    TestResults results = parseProgram(programName);
+    CFA cfa = results.getCheckerResult().getCfa();
+    assertThat(cfa).isNotNull();
+
+    testFullExpression(cfa, "x + y", 18, 10);
+    testFullExpression(cfa, "x = 1", 10, 11);
+    testFullExpression(cfa, "y = 1", 11, 11);
+    testFullExpression(cfa, "x = 2", 12, 3);
+    testFullExpression(cfa, "y = 2", 13, 3);
+    testFullExpression(cfa, "x != 0", 14, 10);
+    testFullExpression(cfa, "y != 0", 14, 10);
+    testFullExpression(cfa, "z + w", 21, 30);
+    testFullExpression(cfa, "f()", 24, 3);
+    testFullExpression(cfa, "g(1, 2)", 25, 3);
+    testFullExpression(cfa, "j < 0", 29, 8);
+    testFullExpression(cfa, "i == 0", 29, 8);
+    testFullExpression(cfa, "i < 10", 29, 25);
+    testFullExpression(cfa, "j == 0", 29, 25);
+    testFullExpression(cfa, "i < 5", 29, 43);
+    testFullExpression(cfa, "i != 0", 29, 43);
+    testFullExpression(cfa, "s != q", 35, 11);
+    testFullExpression(cfa, "s == 1", 35, 11);
+    testFullExpression(cfa, "q == 2", 35, 11);
+    testFullExpression(cfa, "l = 0", 36, 11);
   }
 }
