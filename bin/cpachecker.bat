192466c8f74f602b19b280789f939eb3f01d981f
Philipp Wendler <uni@philippwendler.de>
2024-11-07 16:06:30 +0100

    Apply refactoring from 7126d56b to cpachecker.bat as well

--- bin/cpachecker.bat
+++ bin/cpachecker.bat
@@ -1,4 +1,5 @@
 @echo off
+setlocal enableDelayedExpansion
 
 REM This file is part of CPAchecker,
 REM a tool for configurable software verification:
@@ -17,10 +18,6 @@ IF "%JAVA%"=="" (
     SET JAVA=java
   )
 )
-
-REM the default heap and stack sizes of the Java VM
-SET DEFAULT_HEAP_SIZE=1200M
-SET DEFAULT_STACK_SIZE=1024k
 
 REM ------------------------------------------------------------------------------
 REM From here on you should not need to change anything
@@ -43,7 +40,7 @@ SET "CLASSPATH=%CLASSPATH%;%PATH_TO_CPACHECKER%\classes;%PATH_TO_CPACHECKER%\cpa
 
 REM loop over all input parameters and parse them
 SET OPTIONS=
-SET JAVA_ASSERTIONS=-ea
+SET BENCHMARK_MODE=
 
 :loop
 IF NOT [%1]==[] (
@@ -52,12 +49,10 @@ IF NOT [%1]==[] (
     REM params starting with "-X" are used for JVM
     SET "JAVA_VM_ARGUMENTS=%JAVA_VM_ARGUMENTS% %1"
   ) ELSE IF [%1]==[--benchmark] (
-    SET JAVA_ASSERTIONS=-da
-    SET DEFAULT_HEAP_SIZE=xxxx
+    SET BENCHMARK_MODE=true
     SET "OPTIONS=%OPTIONS% %1"
   ) ELSE IF [%1]==[-benchmark] (
-    SET JAVA_ASSERTIONS=-da
-    SET DEFAULT_HEAP_SIZE=xxxx
+    SET BENCHMARK_MODE=true
     SET "OPTIONS=%OPTIONS% %1"
   ) ELSE IF [%1]==[--heap] (
     SET JAVA_HEAP_SIZE=%2
@@ -110,6 +105,7 @@ IF NOT [%1]==[] (
   GOTO :loop
 )
 
+REM Determine temp dir to use for JVM
 IF NOT "%TMPDIR%"=="" (
   SET "JAVA_VM_ARGUMENTS=%JAVA_VM_ARGUMENTS% -Djava.io.tmpdir^=%TMPDIR%"
 ) ELSE IF NOT "%TEMP%"=="" (
@@ -118,25 +114,34 @@ IF NOT "%TMPDIR%"=="" (
   SET "JAVA_VM_ARGUMENTS=%JAVA_VM_ARGUMENTS% -Djava.io.tmpdir^=%TMP%"
 )
 
+REM Determine whether to enable Java assertions
+IF defined BENCHMARK_MODE (
+  SET JAVA_ASSERTIONS=-da
+) ELSE (
+  SET JAVA_ASSERTIONS=-ea
+)
+
+REM Determine heap size
 IF NOT "%JAVA_HEAP_SIZE%"=="" (
   ECHO Running CPAchecker with Java heap of size %JAVA_HEAP_SIZE%.
   SET USE_JAVA_HEAP_SIZE=%JAVA_HEAP_SIZE%
+) ELSE IF defined BENCHMARK_MODE (
+  ECHO A heap size needs to be specified with --heap if --benchmark is given.
+  ECHO Please see doc/Benchmark.md for further information.
+  EXIT /B 1
 ) ELSE (
-  SET JAVA_HEAP_SIZE=%DEFAULT_HEAP_SIZE%
-  IF "%DEFAULT_HEAP_SIZE%"=="xxxx" (
-    ECHO A heap size needs to be specified with --heap if --benchmark is given.
-    ECHO Please see doc/Benchmark.md for further information.
-    EXIT /B 1
-  )
-  ECHO Running CPAchecker with default heap size %DEFAULT_HEAP_SIZE%. Specify a larger value with --heap if you have more RAM.
+  SET JAVA_HEAP_SIZE=1200M
+  ECHO Running CPAchecker with default heap size !JAVA_HEAP_SIZE!. Specify a larger value with --heap if you have more RAM.
 )
 
+REM Determine stack size
 IF NOT "%JAVA_STACK_SIZE%"=="" (
   ECHO Running CPAchecker with Java stack of size %JAVA_STACK_SIZE%.
 ) ELSE (
-  SET JAVA_STACK_SIZE=%DEFAULT_STACK_SIZE%
-  ECHO Running CPAchecker with default stack size %DEFAULT_STACK_SIZE%. Specify a larger value with --stack if needed.
+  SET JAVA_STACK_SIZE=1024k
+  ECHO Running CPAchecker with default stack size !JAVA_STACK_SIZE!. Specify a larger value with --stack if needed.
 )
+
 
 IF NOT "%JAVA_VM_ARGUMENTS%"=="" (
   ECHO Running CPAchecker with the following extra VM options:%JAVA_VM_ARGUMENTS%.
