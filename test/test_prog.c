9b4ad9d1e24430bbd81bd03f943ad2f5bd2cabeb
Felix <felixhajuj@gmail.com>
2024-11-11 00:02:16 +0100

    differnet test

--- test/test_prog.c
+++ test/test_prog.c
@@ -2,7 +2,7 @@ void reach_error(){}
 extern int __VERIFIER_nondet_int();
 int main() {
   int x = __VERIFIER_nondet_int();
-  if (x < 0 || x % 2 == 0) { // Error Condition
+  if (x < 0 || (x & 1) == 0) { // Error Condition
           reach_error(); // Error
           return 1;
       }
