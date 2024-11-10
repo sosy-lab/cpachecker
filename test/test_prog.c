20ffce079a787de70400f965a5165c7ca4509a81
Felix <felixhajuj@gmail.com>
2024-11-11 00:01:45 +0100

    differnet test

--- test/test_prog.c
+++ test/test_prog.c
@@ -2,21 +2,10 @@ void reach_error(){}
 extern int __VERIFIER_nondet_int();
 int main() {
   int x = __VERIFIER_nondet_int();
-  if ((x & 1) == 0) {x = x/2;}
-  int i = 3;
-  while (i <= x) {
-    while (is_divisible(x,i)) {
-            x = x / i;
-    }
-    i += 2;
-  }
-  if (x != 1) {
-    reach_error();
-    return 1; // error condition
-  }
+  if (x < 0 || x % 2 == 0) { // Error Condition
+          reach_error(); // Error
+          return 1;
+      }
   else return 0;
 }
 
-inline int is_divisible(int a, int b) {
-    return a % b == 0;
-}
