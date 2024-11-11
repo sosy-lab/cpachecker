88d24c32cfbe570a54c6372a17215de9d9a49fd7
Felix <felixhajuj@gmail.com>
2024-11-11 17:29:44 +0100

    testing minor modifications

--- test/test_prog.c
+++ test/test_prog.c
@@ -2,10 +2,14 @@ void reach_error(){}
 extern int __VERIFIER_nondet_int();
 int main() {
   int x = __VERIFIER_nondet_int();
-  if (x < 0) { // Error Condition -> x is negative
-    reach_error(); // Error State 1
-    return 1;
-  }
+  // if (x < 0) { // Error Condition -> x is negative
+  //  reach_error(); // Error State 1
+  //  return 1;
+  // }
+  if (0 < x < 10) { // Error Condition -> x is negative
+      reach_error(); // Error State 1
+      return 1;
+    }
   if (x == 2) { // Error Condition -> x is even
     reach_error(); // Error State 2
     return 1;
