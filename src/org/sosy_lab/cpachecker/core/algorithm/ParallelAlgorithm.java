0730b4be405a322b402a53dbb3d3d830f2b9da26
Marek Jankola <marek.jankola@sosy.ifi.lmu.de>
2024-11-12 15:57:12 +0100

    Adding an option to not write output files for the unsuccessful analysis

--- src/org/sosy_lab/cpachecker/core/algorithm/ParallelAlgorithm.java
+++ src/org/sosy_lab/cpachecker/core/algorithm/ParallelAlgorithm.java
@@ -92,6 +92,11 @@ public class ParallelAlgorithm implements Algorithm, StatisticsProvider {
   @FileOption(FileOption.Type.OPTIONAL_INPUT_FILE)
   private List<AnnotatedValue<Path>> configFiles;
 
+  @Option(
+      secure = true,
+      description = "toggle to write all the files also for the unsuccessful analyses")
+  private boolean writeUnsuccessfulAnalysisFiles = false;
+
   private static final String SUCCESS_MESSAGE =
       "One of the parallel analyses has finished successfully, cancelling all other runs.";
 
@@ -121,7 +126,7 @@ public class ParallelAlgorithm implements Algorithm, StatisticsProvider {
       throws InvalidConfigurationException, CPAException, InterruptedException {
     config.inject(this);
 
-    stats = new ParallelAlgorithmStatistics(pLogger);
+    stats = new ParallelAlgorithmStatistics(pLogger, writeUnsuccessfulAnalysisFiles);
     globalConfig = config;
     logger = checkNotNull(pLogger);
     shutdownManager = ShutdownManager.createWithParent(checkNotNull(pShutdownNotifier));
@@ -543,9 +548,11 @@ public class ParallelAlgorithm implements Algorithm, StatisticsProvider {
     private final List<StatisticsEntry> allAnalysesStats = new CopyOnWriteArrayList<>();
     private int noOfAlgorithmsUsed = 0;
     private String successfulAnalysisName = null;
+    private boolean writeUnsuccessfulAnalysisFiles;
 
-    ParallelAlgorithmStatistics(LogManager pLogger) {
+    ParallelAlgorithmStatistics(LogManager pLogger, boolean pWriteUnsuccessfulAnalysisFiles) {
       logger = checkNotNull(pLogger);
+      writeUnsuccessfulAnalysisFiles = pWriteUnsuccessfulAnalysisFiles;
     }
 
     public synchronized StatisticsEntry getNewSubStatistics(
@@ -612,7 +619,7 @@ public class ParallelAlgorithm implements Algorithm, StatisticsProvider {
       for (StatisticsEntry subStats : allAnalysesStats) {
         if (isSuccessfulAnalysis(subStats)) {
           successfullAnalysisStats = subStats;
-        } else {
+        } else if (writeUnsuccessfulAnalysisFiles) {
           writeSubOutputFiles(pResult, subStats);
         }
       }
