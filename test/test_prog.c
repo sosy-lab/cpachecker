25d1841b3720f5797b3a1ee6c41d277b683f7973
Felix <felixhajuj@gmail.com>
2024-11-14 12:12:09 +0100

    testing elimination

--- test/test_prog.c
+++ test/test_prog.c
@@ -6,10 +6,10 @@ int main() {
   //  reach_error(); // Error State 1
   //  return 1;
   // }
-  // if (0 < x < 10) { // Error Condition -> x is negative
-  //     reach_error(); // Error State 1
-  //    return 1;
-  // }
+  if (0 < x < 10) { // Error Condition -> x is negative
+    reach_error(); // Error State 1
+    return 1;
+  }
   if (x == 2) { // Error Condition -> x is even
     reach_error(); // Error State 2
     return 1;
