7bc9dac1b74714bf5033af482e294ae49d438d90
Thomas Lemberger <lembergerth@gmail.com>
2024-11-17 16:23:41 +0100

    Remove Distributed Value-Analysis CPA, not functional yet

--- src/org/sosy_lab/cpachecker/core/algorithm/distributed_summaries/distributed_cpa/DCPAFactory.java
+++ src/org/sosy_lab/cpachecker/core/algorithm/distributed_summaries/distributed_cpa/DCPAFactory.java
@@ -24,7 +24,6 @@ import org.sosy_lab.cpachecker.core.algorithm.distributed_summaries.distributed_
 import org.sosy_lab.cpachecker.core.algorithm.distributed_summaries.distributed_cpa.distributed_block_cpa.DistributedBlockCPA;
 import org.sosy_lab.cpachecker.core.algorithm.distributed_summaries.distributed_cpa.function_pointer.DistributedFunctionPointerCPA;
 import org.sosy_lab.cpachecker.core.algorithm.distributed_summaries.distributed_cpa.predicate.DistributedPredicateCPA;
-import org.sosy_lab.cpachecker.core.algorithm.distributed_summaries.distributed_cpa.value.DistributedValueAnalysisCPA;
 import org.sosy_lab.cpachecker.core.algorithm.distributed_summaries.exchange.actor_messages.BlockSummaryMessageFactory;
 import org.sosy_lab.cpachecker.core.algorithm.distributed_summaries.worker.BlockSummaryAnalysisOptions;
 import org.sosy_lab.cpachecker.core.interfaces.ConfigurableProgramAnalysis;
@@ -34,7 +33,6 @@ import org.sosy_lab.cpachecker.cpa.callstack.CallstackCPA;
 import org.sosy_lab.cpachecker.cpa.composite.CompositeCPA;
 import org.sosy_lab.cpachecker.cpa.functionpointer.FunctionPointerCPA;
 import org.sosy_lab.cpachecker.cpa.predicate.PredicateCPA;
-import org.sosy_lab.cpachecker.cpa.value.ValueAnalysisCPA;
 import org.sosy_lab.cpachecker.util.CFAUtils;
 
 public class DCPAFactory {
@@ -104,18 +102,10 @@ public class DCPAFactory {
           pShutdownNotifier,
           integerToNodeMap);
     }
-    if (pCPA instanceof ValueAnalysisCPA valueCPA) {
-      return distribute(valueCPA);
-    }
     /* TODO: implement support for LocationCPA and LocationBackwardCPA
     as soon as targetCFANode is not required anymore */
     // creates CPA for every thread without communication
     return null;
-  }
-
-  private static DistributedConfigurableProgramAnalysis distribute(
-      ValueAnalysisCPA pValueAnalysisCPA) {
-    return new DistributedValueAnalysisCPA(pValueAnalysisCPA);
   }
 
   private static DistributedConfigurableProgramAnalysis distribute(
