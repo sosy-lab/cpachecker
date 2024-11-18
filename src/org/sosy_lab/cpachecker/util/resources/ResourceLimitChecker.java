3057b64596d943a8c14c2dddc415199f497f8ac3
BaierD <daniel.baier@sosy.ifi.lmu.de>
2024-11-18 09:24:25 +0100

    ResourceLimitChecker: improve documentation for changed behavior/methods and switch set thread method called to new name

--- src/org/sosy_lab/cpachecker/util/resources/ResourceLimitChecker.java
+++ src/org/sosy_lab/cpachecker/util/resources/ResourceLimitChecker.java
@@ -68,12 +68,17 @@ public final class ResourceLimitChecker {
     }
   }
 
-  /** Actually start enforcing the limits. May be called only once. */
+  /**
+   * Actually start enforcing the limits. May be called only once. Only a {@link ThreadCpuTimeLimit}
+   * started with this method associates the current {@link Thread} with the {@link
+   * ThreadCpuTimeLimit} and starts its tracking from the point of calling this method. All other
+   * time-limits start from the moment of their creation.
+   */
   public void start() {
     if (thread != null) {
       for (ResourceLimit limit : limits) {
         if (limit instanceof ThreadCpuTimeLimit pThreadCpuTimeLimit) {
-          pThreadCpuTimeLimit.withThread(Thread.currentThread());
+          pThreadCpuTimeLimit.setThread(Thread.currentThread());
         }
       }
       thread.start();
