1188b4f214d7c157d440fea071f45de340f3d596
Rabea Lühmann <r.luehmann@campus.lmu.de>
2024-12-09 13:56:38 +0100

    Implement JSON Deserializer for LemmaEntry
    The deserializer parses the fields "value" and "format" from a
    "witness.yml" file into an instance of LemmaEntry.
    The test class LemmaEntryTest passes without errors or failures.

--- src/org/sosy_lab/cpachecker/util/yamlwitnessexport/model/LemmaEntry.java
+++ src/org/sosy_lab/cpachecker/util/yamlwitnessexport/model/LemmaEntry.java
@@ -9,8 +9,17 @@
 package org.sosy_lab.cpachecker.util.yamlwitnessexport.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import java.io.IOException;
 import org.sosy_lab.cpachecker.util.yamlwitnessexport.YAMLWitnessExpressionType;
+import org.sosy_lab.cpachecker.util.yamlwitnessexport.model.LemmaEntry.LemmaRecordDeserializer;
 
+@JsonDeserialize(using = LemmaRecordDeserializer.class)
 public class LemmaEntry extends AbstractInformationRecord {
 
   @JsonProperty("value")
@@ -25,5 +34,27 @@ public class LemmaEntry extends AbstractInformationRecord {
 
   public String getValue() {
     return value;
+  }
+
+  public static class LemmaRecordDeserializer extends JsonDeserializer<LemmaEntry> {
+    @Override
+    public LemmaEntry deserialize(JsonParser jp, DeserializationContext ctxt) throws IOException {
+      ObjectMapper mapper = (ObjectMapper) jp.getCodec();
+      JsonNode node = mapper.readTree(jp);
+
+      // The node should now be the 'lemma' node. Move one level deeper to its children.
+      JsonNode lemmaNode = node.get("lemma");
+      assert lemmaNode != null;
+
+      // Delegate the actual object mapping back to Jackson:
+      // WaypointRecord result = mapper.treeToValue(waypointNode, WaypointRecord.class);
+      // CAVEAT: does not work, since this would use the custom deserializer.
+      // Using the original deserializer is apparently very hard.
+      // For now just manually construct this
+      // (less elegant, but we probably never touch that code again, so it is fine):
+      return new LemmaEntry(
+          mapper.treeToValue(lemmaNode.get("value"), String.class),
+          mapper.treeToValue(lemmaNode.get("format"), YAMLWitnessExpressionType.class));
+    }
   }
 }
