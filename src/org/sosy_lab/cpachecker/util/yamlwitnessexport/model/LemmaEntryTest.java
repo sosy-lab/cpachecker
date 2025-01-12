ba29e08f127473d39427ae73d3a85f62f1187545
Rabea Lühmann <r.luehmann@campus.lmu.de>
2025-01-12 17:17:20 +0100

    Fix Pipeline
    Removes IOException that is never thrown.
    Changes List into ImmutableList.

--- src/org/sosy_lab/cpachecker/util/yamlwitnessexport/model/LemmaEntryTest.java
+++ src/org/sosy_lab/cpachecker/util/yamlwitnessexport/model/LemmaEntryTest.java
@@ -12,6 +12,7 @@ import static com.google.common.truth.Truth.assertThat;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
 import java.io.File;
 import java.io.IOException;
@@ -38,9 +39,9 @@ public class LemmaEntryTest {
   }
 
   @Test
-  public void testParseLemmas() throws IOException {
+  public void testParseLemmas() {
     Path lemmaFile = Path.of(TEST_DIR_PATH, "witness.yml");
-    List<Path> lemmaFiles = List.of(lemmaFile);
+    ImmutableList<Path> lemmaFiles = ImmutableList.of(lemmaFile);
     ImmutableSet<LemmaEntry> lemmaSet = LemmaUtils.parseLemmas(lemmaFiles, null);
     assertThat(lemmaSet).hasSize(2);
     assertThat(lemmaSet.asList().get(0).getValue()).isEqualTo("MaxArray(A,0) = A[0]");
