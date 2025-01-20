6636a293b9df687a6e7b38f4dd2d0da82fb42cd7
Diego Salgado <diiego.ise@gmail.com>
2025-01-20 22:31:29 +0100

    Add unit tests for TaintAnalysis cases
    Introduced new test methods to verify the first set of taint analysis scenarios, covering both tainted and non-tainted programs in safe and unsafe cases.

--- src/org/sosy_lab/cpachecker/cpa/taintanalysis/TaintAnalysisTest.java
+++ src/org/sosy_lab/cpachecker/cpa/taintanalysis/TaintAnalysisTest.java
@@ -1,30 +1,49 @@
-// This file is part of CPAchecker,
-// a tool for configurable software verification:
-// https://cpachecker.sosy-lab.org
-//
-// SPDX-FileCopyrightText: 2025 Dirk Beyer <https://www.sosy-lab.org>
-//
-// SPDX-License-Identifier: Apache-2.0
-
 package org.sosy_lab.cpachecker.cpa.taintanalysis;
 
-import java.nio.file.Path;
-import java.util.logging.Level;
+import org.junit.Test;
 import org.sosy_lab.common.configuration.Configuration;
 import org.sosy_lab.cpachecker.util.test.CPATestRunner;
 import org.sosy_lab.cpachecker.util.test.TestDataTools;
 import org.sosy_lab.cpachecker.util.test.TestResults;
 
+import java.nio.file.Path;
+import java.util.logging.Level;
+
 public class TaintAnalysisTest {
+
   private TestResults parseProgram(String pProgramName) throws Exception {
     final Configuration config =
         TestDataTools.configurationForTest()
             .setOption("--config", "config/predicateAnalysis--taintAnalysis.properties")
             .build();
 
-    String test_dir = "test/programs/taint_analysis/";
-    Path program = Path.of(test_dir, pProgramName);
+    String testDir = "test/programs/taint_analysis/";
+    Path programPath = Path.of(testDir, pProgramName);
 
-    return CPATestRunner.run(config, program.toString(), Level.FINEST);
+    return CPATestRunner.run(config, programPath.toString(), Level.FINEST);
+  }
+
+  @Test
+  public void testExampleNotTaintedSafe() throws Exception {
+    TestResults results = parseProgram("exampleNotTaintedSafe.c");
+    results.assertIsSafe();
+  }
+
+  @Test
+  public void testExampleNotTaintedUnsafe() throws Exception {
+    TestResults results = parseProgram("exampleNotTaintedUnsafe.c");
+    results.assertIsUnsafe();
+  }
+
+  @Test
+  public void testExampleTaintedSafe() throws Exception {
+    TestResults results = parseProgram("exampleTaintedSafe.c");
+    results.assertIsSafe();
+  }
+
+  @Test
+  public void testExampleTaintedUnsafe() throws Exception {
+    TestResults results = parseProgram("exampleTaintedUnsafe.c");
+    results.assertIsUnsafe();
   }
 }
