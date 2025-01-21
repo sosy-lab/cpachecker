e255743d6f869cb2b2690dddefe0f8b1333ace1c
mjakobs <jakobs@sosy.ifi.lmu.de>
2025-01-21 08:35:39 +0100

    Implemented suggestion for nicer solution of problem solved in previous commit 62baeb3f

--- src/org/sosy_lab/cpachecker/cpa/value/ValueAnalysisResultToLoopInvariants.java
+++ src/org/sosy_lab/cpachecker/cpa/value/ValueAnalysisResultToLoopInvariants.java
@@ -25,7 +25,6 @@ import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
@@ -195,8 +194,6 @@ public class ValueAnalysisResultToLoopInvariants implements AutoCloseable {
 
   private ImmutableMap<MemoryLocation, Type> extractVarsWithType(final CFA pCfa) {
     ImmutableMap.Builder<MemoryLocation, Type> builder = ImmutableMap.builder();
-    Set<MemoryLocation> seenVars = new HashSet<>();
-    MemoryLocation var;
 
     for (AbstractSimpleDeclaration decl :
         FluentIterable.from(pCfa.edges())
@@ -207,10 +204,8 @@ public class ValueAnalysisResultToLoopInvariants implements AutoCloseable {
                 Predicates.or(
                     Predicates.instanceOf(AVariableDeclaration.class),
                     Predicates.instanceOf(AParameterDeclaration.class)))) {
-      var = MemoryLocation.forDeclaration(decl);
-      if (seenVars.add(var)) {
-        builder.put(var, decl.getType());
-      }
+
+      builder.put(MemoryLocation.forDeclaration(decl), decl.getType());
     }
 
     for (AParameterDeclaration decl :
@@ -219,13 +214,11 @@ public class ValueAnalysisResultToLoopInvariants implements AutoCloseable {
             .transform(FunctionCallEdge::getFunctionCallExpression)
             .transform(AFunctionCallExpression::getDeclaration)
             .transformAndConcat(AFunctionDeclaration::getParameters)) {
-      var = MemoryLocation.forDeclaration(decl);
-      if (seenVars.add(var)) {
-        builder.put(var, decl.getType());
-      }
+
+      builder.put(MemoryLocation.forDeclaration(decl), decl.getType());
     }
 
-    return builder.buildOrThrow();
+    return builder.buildKeepingLast();
   }
 
   @Override
