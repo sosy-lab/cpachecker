31f041327da4943dcf952fbd40fabb042d747b9e
Diego Salgado <diiego.ise@gmail.com>
2025-01-23 11:54:02 +0100

    Add SPDX headers and update copyright years for taint analysis
    Included SPDX license headers and copyright information in relevant files related to taint analysis. Updated the copyright year to 2024 to reflect the current state.

--- test/programs/taint_analysis/exampleTaintedSafe.c
+++ test/programs/taint_analysis/exampleTaintedSafe.c
@@ -1,4 +1,12 @@
-// Variable x is tainted and the assertion agrees 
+// This file is part of CPAchecker,
+// a tool for configurable software verification:
+// https://cpachecker.sosy-lab.org
+//
+// SPDX-FileCopyrightText: 2024 Dirk Beyer <https://www.sosy-lab.org>
+//
+// SPDX-License-Identifier: Apache-2.0
+
+// Variable x is tainted and the assertion agrees
 int main() {
     int x = __VERIFIER_nondet_int();
     __VERIFIER_assert_taint(x,1);
