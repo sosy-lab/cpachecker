ce491c3376d94966e58f0bccb5478f4ceac0cd76
noahkoenig <email@noahkoenig.de>
2025-01-29 14:15:46 +0100

    add yml witness parsing and validation test

--- src/org/sosy_lab/cpachecker/util/yamlwitnessexport/YAMLWitnessTest.java
+++ src/org/sosy_lab/cpachecker/util/yamlwitnessexport/YAMLWitnessTest.java
@@ -8,39 +8,79 @@
 
 package org.sosy_lab.cpachecker.util.yamlwitnessexport;
 
+import static com.google.common.truth.Truth.assertThat;
 import static com.google.common.truth.Truth.assertWithMessage;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
 import org.junit.Test;
 import org.sosy_lab.common.configuration.Configuration;
+import org.sosy_lab.common.configuration.InvalidConfigurationException;
+import org.sosy_lab.cpachecker.cpa.automaton.AutomatonWitnessV2ParserUtils;
 import org.sosy_lab.cpachecker.util.test.CPATestRunner;
 import org.sosy_lab.cpachecker.util.test.CPATestRunner.ExpectedVerdict;
 import org.sosy_lab.cpachecker.util.test.TestDataTools;
 import org.sosy_lab.cpachecker.util.test.TestResults;
+import org.sosy_lab.cpachecker.util.yamlwitnessexport.model.AbstractEntry;
 
 public class YAMLWitnessTest {
 
-  private static final String TEST_DIR_PATH = "test/programs/witnessValidation/";
+  private static final String TEST_DIR = "./test/programs/witnessValidation/";
+
+  private static final String DEFAULT_CONFIG_PATH = "./config/default.properties";
+
+  private static final String OUTPUT_DIR = "./output/";
+
+  // TODO also parametrize version
+  private static final String YML_WITNESS_FILE = "witness-2.0.yml";
+
+  private static final String CORRECTNESS_WITNESS_PATH = OUTPUT_DIR + YML_WITNESS_FILE;
+
+  private static final String VIOLATION_WITNESS_PATH =
+      OUTPUT_DIR + "Counterexample.1." + YML_WITNESS_FILE;
+
+  private static final String WITNESS_OPTION = "--witness";
 
   @Test(timeout = 90000)
   public void max_true() throws Exception {
-    verifyProgram(TEST_DIR_PATH + "max.c", ExpectedVerdict.TRUE);
+    String path = TEST_DIR + "max.c";
+    ExpectedVerdict expectedVerdict = ExpectedVerdict.TRUE;
+    // generate, parse and validate witness
+    generateWitness(path, expectedVerdict);
+    try (InputStream inputStream = createInputStream(expectedVerdict)) {
+      List<AbstractEntry> entries = AutomatonWitnessV2ParserUtils.parseYAML(inputStream);
+      assertThat(entries.isEmpty()).isFalse();
+    }
+    validateWitness(path, expectedVerdict);
   }
 
   @Test(timeout = 90000)
   public void minepump_spec1_product33_false() throws Exception {
-    verifyProgram(TEST_DIR_PATH + "minepump_spec1_product33.cil.c", ExpectedVerdict.FALSE);
+    String path = TEST_DIR + "minepump_spec1_product33.cil.c";
+    ExpectedVerdict expectedVerdict = ExpectedVerdict.FALSE;
+    // generate, parse and validate yaml witness
+    generateWitness(path, expectedVerdict);
+    try (InputStream inputStream = createInputStream(expectedVerdict)) {
+      List<AbstractEntry> entries = AutomatonWitnessV2ParserUtils.parseYAML(inputStream);
+      assertThat(entries.isEmpty()).isFalse();
+    }
+    validateWitness(path, expectedVerdict);
   }
 
-  private static void verifyProgram(String pFilePath, ExpectedVerdict pExpected) throws Exception {
+  private static void generateWitness(String pFilePath, ExpectedVerdict pExpectedVerdict)
+      throws Exception {
     // just use default config
-    Configuration generationConfig =
-      TestDataTools.configurationForTest().loadFromFile("./config/default.properties").build();
+    Configuration generationConfig = createVerificationConfig();
 
     TestResults results = CPATestRunner.run(generationConfig, pFilePath);
     // trigger statistics so that the witness is written to the file
     results.getCheckerResult().writeOutputFiles();
 
-    switch (pExpected) {
+    switch (pExpectedVerdict) {
       case TRUE:
         results.assertIsSafe();
         break;
@@ -50,6 +90,67 @@ public class YAMLWitnessTest {
       default:
         assertWithMessage("Cannot determine expected result.").fail();
         throw new AssertionError("Unreachable code.");
+    }
+  }
+
+  private static InputStream createInputStream(ExpectedVerdict pExpectedVerdict)
+      throws FileNotFoundException {
+    switch (pExpectedVerdict) {
+      case TRUE:
+        return new FileInputStream(new File(CORRECTNESS_WITNESS_PATH));
+      case FALSE:
+        return new FileInputStream(new File(VIOLATION_WITNESS_PATH));
+      default:
+        throw new AssertionError("Unsupported verdict " + pExpectedVerdict);
+    }
+  }
+
+  private static void validateWitness(String pFilePath, ExpectedVerdict pExpectedVerdict)
+      throws Exception {
+    Configuration config = createValidationConfig(pExpectedVerdict);
+
+    TestResults results = CPATestRunner.run(config, pFilePath);
+    // trigger statistics so that the witness is written to the file
+    results.getCheckerResult().writeOutputFiles();
+
+    switch (pExpectedVerdict) {
+      case TRUE:
+        results.assertIsSafe();
+        break;
+      case FALSE:
+        results.assertIsUnsafe();
+        break;
+      default:
+        assertWithMessage("Cannot determine expected result.").fail();
+        throw new AssertionError("Unreachable code.");
+    }
+  }
+
+  private static Configuration createVerificationConfig()
+      throws InvalidConfigurationException, IOException {
+    return TestDataTools.configurationForTest()
+        .loadFromFile(DEFAULT_CONFIG_PATH)
+        // configurationForTest() disables output, we override it to obtain witnesses
+        .setOption("output.disable", "false")
+        .build();
+  }
+
+  private static Configuration createValidationConfig(ExpectedVerdict pExpectedVerdict)
+      throws InvalidConfigurationException, IOException {
+    return TestDataTools.configurationForTest()
+        .loadFromFile(DEFAULT_CONFIG_PATH)
+        .setOption(WITNESS_OPTION, getWitnessFileName(pExpectedVerdict))
+        .build();
+  }
+
+  private static String getWitnessFileName(ExpectedVerdict pExpectedVerdict) {
+    switch (pExpectedVerdict) {
+      case TRUE:
+        return CORRECTNESS_WITNESS_PATH;
+      case FALSE:
+        return VIOLATION_WITNESS_PATH;
+      default:
+        throw new AssertionError("Unsupported verdict " + pExpectedVerdict);
     }
   }
 }
