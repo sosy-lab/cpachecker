3b2a94bede3dd2fa56576e9457bafb4dc637f1a7
mjakobs <jakobs@sosy.ifi.lmu.de>
2025-02-26 08:34:19 +0100

    also delete unused parameter in cast in assumption to edge allocator

--- src/org/sosy_lab/cpachecker/core/counterexample/AssumptionToEdgeAllocator.java
+++ src/org/sosy_lab/cpachecker/core/counterexample/AssumptionToEdgeAllocator.java
@@ -1530,11 +1530,7 @@ public class AssumptionToEdgeAllocator {
                 : new LogManagerWithoutDuplicates(logger);
         Value castValue =
             AbstractExpressionValueVisitor.castCValue(
-                new NumericValue(pIntegerValue),
-                pType,
-                machineModel,
-                logManager,
-                FileLocation.DUMMY);
+                new NumericValue(pIntegerValue), pType, machineModel, logManager);
         if (castValue.isUnknown()) {
           return UnknownValueLiteral.getInstance();
         }
