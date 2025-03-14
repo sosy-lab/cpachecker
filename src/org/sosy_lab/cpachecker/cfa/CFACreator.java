9709c6c7085c457e7bbe5bbd4959fd3678ffe106
Thomas Lemberger <lembergerth@gmail.com>
2025-03-14 08:09:05 +0100

    Remove incomplete CfaToCExporter
    Meant as an alternative to CfaToCTranslator, with the goal
    to stay closer to the original C program, CfaToCExporter was not finished
    and is not actively pursued anymore. So remove this leftover implementation.

--- src/org/sosy_lab/cpachecker/cfa/CFACreator.java
+++ src/org/sosy_lab/cpachecker/cfa/CFACreator.java
@@ -94,7 +94,6 @@ import org.sosy_lab.cpachecker.util.LiveVariables;
 import org.sosy_lab.cpachecker.util.LoopStructure;
 import org.sosy_lab.cpachecker.util.Pair;
 import org.sosy_lab.cpachecker.util.cwriter.CFAToCTranslator;
-import org.sosy_lab.cpachecker.util.cwriter.CfaToCExporter;
 import org.sosy_lab.cpachecker.util.statistics.StatisticsUtils;
 import org.sosy_lab.cpachecker.util.variableclassification.VariableClassificationBuilder;
 
@@ -194,14 +193,6 @@ public class CFACreator {
   @Option(secure = true, name = "cfa.exportToC.file", description = "export CFA as C file")
   @FileOption(FileOption.Type.OUTPUT_FILE)
   private Path exportCfaToCFile = Path.of("cfa.c");
-
-  @Option(
-      secure = true,
-      name = "cfa.exportToC.stayCloserToInput",
-      description =
-          "produce C programs more similar to the input program"
-              + "\n(only possible for a single input file)")
-  private boolean exportCfaToCStayingCloserToInput = false;
 
   @Option(secure = true, name = "cfa.callgraph.export", description = "dump a simple call graph")
   private boolean exportFunctionCalls = true;
@@ -1194,25 +1185,11 @@ public class CFACreator {
 
     if (exportCfaToC && exportCfaToCFile != null) {
       try {
-        String code;
-        if (exportCfaToCStayingCloserToInput && cfa.getFileNames().size() == 1) {
-          code = new CfaToCExporter(logger, config, shutdownNotifier).exportCfa(cfa);
-        } else {
-          if (exportCfaToCStayingCloserToInput) {
-            logger.log(
-                Level.INFO,
-                "Using the regular CFA-to-C exporter (staying closer to the input program is only"
-                    + " possible for a single input file)");
-          }
-          code = new CFAToCTranslator(config).translateCfa(cfa);
-        }
+        String code = new CFAToCTranslator(config).translateCfa(cfa);
         try (Writer writer = IO.openOutputFile(exportCfaToCFile, Charset.defaultCharset())) {
           writer.write(code);
         }
-      } catch (CPAException
-          | IOException
-          | InterruptedException
-          | InvalidConfigurationException e) {
+      } catch (CPAException | IOException | InvalidConfigurationException e) {
         logger.logUserException(Level.WARNING, e, "Could not write CFA to C file.");
       }
     }
