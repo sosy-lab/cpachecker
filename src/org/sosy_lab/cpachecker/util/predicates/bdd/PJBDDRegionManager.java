c2859b73d8c1afa6a07d8907194fcd807b76fa47
Simon Roßmair <simon@rossmair.email>
2025-03-24 18:09:56 +0100

    PJBDD: Add UnsupportedOperationException for CBDD and TBDD as some operations are not supported in PJBDD CBDD/TBDD (makeVariableBefore, setVarOrder, makeExists)

--- src/org/sosy_lab/cpachecker/util/predicates/bdd/PJBDDRegionManager.java
+++ src/org/sosy_lab/cpachecker/util/predicates/bdd/PJBDDRegionManager.java
@@ -282,16 +282,23 @@ public class PJBDDRegionManager implements RegionManager {
       switch (useDDType) {
         case "BDD":
           builder = Builders.bddBuilder();
-          break;
         case "CBDD":
+          /*
+          makeVariableBefore, setVarOrder, makeExists are not supported operations in CBDD PJBDD
           builder = Builders.cbddBuilder();
           break;
+           */
+          throw new UnsupportedOperationException();
         case "TaggedDD":
           builder = Builders.taggedDDBuilder();
           break;
         case "TBDD":
+          /*
+          makeVariableBefore, setVarOrder are not supported operations in TBDD PJBDD
           builder = Builders.tbddBuilder();
           break;
+           */
+          throw new UnsupportedOperationException();
         case "IntDD":
           builder = Builders.intBuilder();
           if (synchronizeReordering) {
