18556091ebb9a7619b73ad649479894e692a59c3
Florian Weiss <weiss.florian@campus.lmu.de>
2025-03-28 11:12:18 +0100

    Removed ACSL from C-Testcase simple-valid.c

--- test/programs/witnessValidation/simple-valid.c
+++ test/programs/witnessValidation/simple-valid.c
@@ -11,15 +11,12 @@
 int main() {
 
   int* x = (int*) malloc(6*sizeof(int));
-  //@assert \valid(x)
 
   for (int i = 0; i < 6; i++) {
     x[i] = i;
   }
-  //@assert \valid(x)
 
   free(x);
-  //@assert \valid(x)
 
   free(x); //To export violation witnesses
   return 0;
