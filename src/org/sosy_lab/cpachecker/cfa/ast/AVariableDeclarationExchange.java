dc2b185f25184d9e453214d871311517cde0ea2a
Marian Lingsch-Rosenfeld <Marian.Lingsch@sosy.ifi.lmu.de>
2025-04-16 11:32:16 +0200

    Add not null checks to data class

--- src/org/sosy_lab/cpachecker/cfa/ast/AVariableDeclarationExchange.java
+++ src/org/sosy_lab/cpachecker/cfa/ast/AVariableDeclarationExchange.java
@@ -8,10 +8,22 @@
 
 package org.sosy_lab.cpachecker.cfa.ast;
 
+import static com.google.common.base.Preconditions.checkNotNull;
+
 import com.fasterxml.jackson.annotation.JsonProperty;
 import org.checkerframework.checker.nullness.qual.NonNull;
 import org.sosy_lab.cpachecker.cfa.types.c.CBasicType;
 
 public record AVariableDeclarationExchange(
-    @JsonProperty("name") String name,
-    @JsonProperty("simpleType") @NonNull CBasicType simpleType) {}
+    @JsonProperty("name") @NonNull String name,
+    @JsonProperty("simpleType") @NonNull CBasicType simpleType) {
+
+  public AVariableDeclarationExchange(
+      @JsonProperty("name") String name,
+      @JsonProperty("simpleType") @NonNull CBasicType simpleType) {
+    checkNotNull(name);
+    checkNotNull(simpleType);
+    this.name = name;
+    this.simpleType = simpleType;
+  }
+}
