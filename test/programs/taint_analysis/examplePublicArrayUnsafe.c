67aac2ab9cb90a975e02c2e706d64ed3fe52f2f8
Diego Salgado <diiego.ise@gmail.com>
2025-04-24 17:25:19 +0200

    Add taint analysis test cases for public and secret arrays
    This commit introduces four new test cases for taint analysis: `exampleSecretArraySafe.c`, `exampleSecretArrayUnsafe.c`, `examplePublicArraySafe.c`, and `examplePublicArrayUnsafe.c`. These cases verify proper handling of public and secret data classification in arrays.

---
+++ test/programs/taint_analysis/examplePublicArrayUnsafe.c
@@ -0,0 +1,17 @@
+// This file is part of CPAchecker,
+// a tool for configurable software verification:
+// https://cpachecker.sosy-lab.org
+//
+// SPDX-FileCopyrightText: 2024 Dirk Beyer <https://www.sosy-lab.org>
+//
+// SPDX-License-Identifier: Apache-2.0
+
+int main() {
+
+    int x = 5;
+
+    int d[1];
+    d[0] = x;
+
+    __VERIFIER_is_public(d[0], 0);
+}
---
+++ test/programs/taint_analysis/exampleSecretArrayUnsafe.c
@@ -0,0 +1,17 @@
+// This file is part of CPAchecker,
+// a tool for configurable software verification:
+// https://cpachecker.sosy-lab.org
+//
+// SPDX-FileCopyrightText: 2024 Dirk Beyer <https://www.sosy-lab.org>
+//
+// SPDX-License-Identifier: Apache-2.0
+
+int main() {
+
+    int x = __VERIFIER_nondet_int();
+
+    int d[1];
+    d[0] = x;
+
+    __VERIFIER_is_public(d, 1);
+}
