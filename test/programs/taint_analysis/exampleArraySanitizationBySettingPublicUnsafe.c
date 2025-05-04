c5f0dbe21e399914dc500e6566ada2e190d7ba91
Diego Salgado <diiego.ise@gmail.com>
2025-05-04 18:11:29 +0200

    Add tests for array sanitization in taint analysis
    Introduce two benchmark programs to test array sanitization in CPAchecker. One example verifies unsafe behavior when sanitization is incomplete, and the other confirms correct sanitization handling.

---
+++ test/programs/taint_analysis/exampleArraySanitizationBySettingPublicUnsafe.c
@@ -0,0 +1,21 @@
+// This file is part of CPAchecker,
+// a tool for configurable software verification:
+// https://cpachecker.sosy-lab.org
+//
+// SPDX-FileCopyrightText: 2024 Dirk Beyer <https://www.sosy-lab.org>
+//
+// SPDX-License-Identifier: Apache-2.0
+
+int main() {
+    int a = __VERIFIER_nondet_int();
+
+    int d[2];
+    d[0] = a; // d is now tainted
+
+    // Sanitize the array by making it public
+    __VERIFIER_set_public(d, 1);
+
+    // Should be public now, but we assert that it is not,
+    // so this benchmark program must return a property violation.
+    __VERIFIER_is_public(d, 0);
+}
