97dafc4196cb20bc060f104de63b853ca8e7e397
Diego Salgado <diiego.ise@gmail.com>
2025-05-07 17:28:27 +0200

    Add test for multiple sanitization methods in taint analysis
    Introduce a new test case to verify handling of different sanitization methods in taint analysis. The test covers scenarios where variables are sanitized via redefinition and a sanitization function.

---
+++ test/programs/taint_analysis/exampleMultipleSanitizeMethods.c
@@ -0,0 +1,36 @@
+// This file is part of CPAchecker,
+// a tool for configurable software verification:
+// https://cpachecker.sosy-lab.org
+//
+// SPDX-FileCopyrightText: 2024 Dirk Beyer <https://www.sosy-lab.org>
+//
+// SPDX-License-Identifier: Apache-2.0
+
+extern int __VERIFIER_nondet_int();
+extern void __VERIFIER_set_public(int variable, int booleanFlag);
+extern void __VERIFIER_is_public(int variable, int booleanFlag);
+
+int main() {
+    int x = __VERIFIER_nondet_int();
+    int y = __VERIFIER_nondet_int();
+
+    // z is tainted
+    int z = x + y;
+    __VERIFIER_is_public(z, 0);
+
+    // sanitize x by redefinition
+    x = 1;
+    __VERIFIER_is_public(x, 1);
+
+    // z is still tainted
+    z = x + y;
+    __VERIFIER_is_public(z, 0);
+
+    // sanitize y by the sanitization function
+    __VERIFIER_set_public(y, 1);
+    __VERIFIER_is_public(y, 1);
+
+    // z is expected not tainted
+    z = x + y;
+    __VERIFIER_is_public(z, 1);
+}
