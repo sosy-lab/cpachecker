7863dd6457c3599ae497fe49642cbe0c85e24259
Diego Salgado <diiego.ise@gmail.com>
2025-05-08 18:40:49 +0200

    Add taint analysis tests for sqrt handling (safe and unsafe)
    Introduce two test cases for taint analysis focusing on function notation calls involving square root operations. The tests verify whether taint propagation is correctly handled in both safe and unsafe scenarios. Which currently is not. Therefore, these tests are currently being ignored in the test class, and the implementation must be fixed.

---
+++ test/programs/taint_analysis/c_function_notation_calls/taintBySqrtSafe.c
@@ -0,0 +1,24 @@
+// This file is part of CPAchecker,
+// a tool for configurable software verification:
+// https://cpachecker.sosy-lab.org
+//
+// SPDX-FileCopyrightText: 2024 Dirk Beyer <https://www.sosy-lab.org>
+//
+// SPDX-License-Identifier: Apache-2.0
+
+extern double SQRT(double number1);
+extern void __VERIFIER_is_public(int variable, int booleanFlag);
+extern void __VERIFIER_set_public(int variable, int booleanFlag);
+
+int main() {
+    double x = 2;
+    double y = 2;
+
+    // asume some operation tainted y
+    __VERIFIER_set_public(y, 0);
+
+    double z = x * y;
+    double w = SQRT(z);
+
+    __VERIFIER_is_public(w, 0);
+}
