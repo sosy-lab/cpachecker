51ce82b70c601ecf912946a4a0837b690be5de15
Diego Salgado <diiego.ise@gmail.com>
2025-05-10 21:07:48 +0200

    Update function signatures in taintBySqrtUnsafe.c
    Changed the return type of __VERIFIER_is_public to int for consistency and added a declaration for __VERIFIER_set_public.

--- test/programs/taint_analysis/c_function_notation_calls/taintBySqrtUnsafe.c
+++ test/programs/taint_analysis/c_function_notation_calls/taintBySqrtUnsafe.c
@@ -5,9 +5,9 @@
 // SPDX-FileCopyrightText: 2024 Dirk Beyer <https://www.sosy-lab.org>
 //
 // SPDX-License-Identifier: Apache-2.0
-
 extern double SQRT(double number1);
-extern void __VERIFIER_is_public(int variable, int booleanFlag);
+extern int __VERIFIER_is_public(int variable, int booleanFlag);
+extern void __VERIFIER_set_public(int variable, int booleanFlag);
 
 int main() {
     double x = 2;
