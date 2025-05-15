4ecf3bd2bce37826825d152580268b09ec6431f2
Diego Salgado <diiego.ise@gmail.com>
2025-05-15 13:27:01 +0200

    Refactor taintBySqrtSafe.c
    Updated variable declarations and initialization. This is not directly related to the SQRT function but shows that the taint analysis supports different kinds of declarations and initializations.
    
    Adjusted extern function signatures; renamed the parameter of SQRT, because unnecessary1 at the end, and updated the return value of the public-state check to minimize the terminal analysis logs.
    
    Regarding the test logic: now the expression SQRT(x*y) is directly passed to the variable z, since with the last updates of the analysis code, now it is not necessary to save the inner multiplication in another variable for the taint analysis to be able to handle the information flow of the statement correctly.
    
    Added one line at the end to show that similarly to the last explanation, the public-state check supports now more kind of expressions, like e.g., in this case extern function calls as first argument. (Previously only variables like `x` were supported there).

--- test/programs/taint_analysis/c_function_notation_calls/taintBySqrtSafe.c
+++ test/programs/taint_analysis/c_function_notation_calls/taintBySqrtSafe.c
@@ -6,19 +6,25 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
-extern double SQRT(double number1);
-extern void __VERIFIER_is_public(int variable, int booleanFlag);
+extern double SQRT(double number);
+extern int __VERIFIER_is_public(int variable, int booleanFlag);
 extern void __VERIFIER_set_public(int variable, int booleanFlag);
 
 int main() {
-    double x = 2;
-    double y = 2;
+    double x, y, z;
+
+    x = 2;
+    y = 2;
 
     // asume some operation tainted y
     __VERIFIER_set_public(y, 0);
 
-    double z = x * y;
-    double w = SQRT(z);
+    z = SQRT(x * y);
 
-    __VERIFIER_is_public(w, 0);
+    // No property violation expected
+    __VERIFIER_is_public(z, 0);
+
+    // This line is just to show that the publicity check also supports
+    // direct extern function calls.
+    __VERIFIER_is_public(SQRT(x * y), 0);
 }
