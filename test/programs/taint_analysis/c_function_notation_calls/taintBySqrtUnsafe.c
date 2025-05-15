87f80496983f825d89f7632f50e7eab038ab7903
Diego Salgado <diiego.ise@gmail.com>
2025-05-15 13:28:14 +0200

    Refactor taintBySqrtUnsafe to simplify SQRT usage
    Combined multiplication and SQRT calls into a single expression for clarity and reduced intermediate variables. Added a comment explicitly marking the expected property violation for better code readability.

--- test/programs/taint_analysis/c_function_notation_calls/taintBySqrtUnsafe.c
+++ test/programs/taint_analysis/c_function_notation_calls/taintBySqrtUnsafe.c
@@ -5,6 +5,7 @@
 // SPDX-FileCopyrightText: 2024 Dirk Beyer <https://www.sosy-lab.org>
 //
 // SPDX-License-Identifier: Apache-2.0
+
 extern double SQRT(double number1);
 extern int __VERIFIER_is_public(int variable, int booleanFlag);
 extern void __VERIFIER_set_public(int variable, int booleanFlag);
@@ -16,8 +17,8 @@ int main() {
     // asume some operation tainted y
     __VERIFIER_set_public(y, 0);
 
-    double z = x * y;
-    double w = SQRT(z);
+    double w = SQRT(x * y);
 
+    // Property violation expected
     __VERIFIER_is_public(w, 1);
 }
