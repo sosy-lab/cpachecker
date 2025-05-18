af2dc1aa165d0895e009bbb2a77fad5bfd494ea2
Diego Salgado <diiego.ise@gmail.com>
2025-05-18 20:25:50 +0200

    Add test case for unreachable taint analysis scenario
    This test case checks the handling of unreachable code paths in taint analysis.
    The analysis is not working properly on this.

---
+++ test/programs/taint_analysis/unreachableTaintSafe.c
@@ -0,0 +1,28 @@
+// This file is part of CPAchecker,
+// a tool for configurable software verification:
+// https://cpachecker.sosy-lab.org
+//
+// SPDX-FileCopyrightText: 2025 Dirk Beyer <https://www.sosy-lab.org>
+//
+// SPDX-License-Identifier: Apache-2.0
+
+extern int __VERIFIER_nondet_int();
+extern int __VERIFIER_is_public(int variable, int booleanFlag);
+
+int main() {
+    int x = 2;
+    int y = __VERIFIER_nondet_int(); // b is initialized tainted
+
+    int z;
+
+    if (x > 0) {
+        z = y; // tainted - reachable
+    } else {
+        z = x; // not tainted - unreachable
+    }
+
+    // No property violation expected
+    __VERIFIER_is_public(z, 0);
+
+    // TODO: the analysis is exploring the unreachable branch z = x, leading to wrong results - FIX
+}
