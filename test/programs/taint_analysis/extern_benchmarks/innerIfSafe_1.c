f1ba72de21020e97bdb5e0f666f04afbcad1801a
Diego Salgado <diiego.ise@gmail.com>
2025-05-24 03:58:02 +0200

    Add detailed taint annotations and expectations in innerIfSafe_1.c

--- test/programs/taint_analysis/extern_benchmarks/innerIfSafe_1.c
+++ test/programs/taint_analysis/extern_benchmarks/innerIfSafe_1.c
@@ -10,7 +10,7 @@
 
 extern int __VERIFIER_nondet_int();
 extern int __VERIFIER_is_public(int variable, int booleanFlag);
-
+//TODO: Unsafe case
 int main() {
 
     int a = __VERIFIER_nondet_int();
@@ -21,16 +21,34 @@ int main() {
     if (x) {
         if (2) {
             b = 2;
-            c = 3; // t(c) = U
+            c = 3;
+            // t(a) = T, t(b) = U, t(c) = U, t(x) = T
         } else {
-            c = 2; // t(c) = U
+            c = 2;
+            // t(a) = T, t(b) = T, t(c) = U, t(x) = T
         }
+        // t(a) = T + T = T
+        // t(b) = U + T = T
+        // t(c) = U + U = U
+        // t(x) = T + T = T
     } else {
-        c = x; // t(c) = t(x) = T
+        c = x;
+        // t(a) = T, t(b) = T, t(c) = T, t(x) = T
     }
+    // t(a) = T + T = T
+    // t(b) = T + T = T
+    // t(c) = U + T = T
+    // t(x) = T + T = T
 
-    // t(c) = U + U + T = T
+    // a is expected to be tainted
+    __VERIFIER_is_public(a, 0);
+
+    // b is expected to be tainted
+    __VERIFIER_is_public(b, 0);
 
     // c is expected to be tainted
     __VERIFIER_is_public(c, 0);
+
+    // x is expected to be tainted
+    __VERIFIER_is_public(x, 0);
 }
