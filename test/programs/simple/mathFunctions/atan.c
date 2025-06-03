adc78a380fe19f2da3b7b84bac5e3fb59df2e28e
Daniel Raffler <daniel-raffler@gmx.de>
2025-06-03 11:38:28 +0200

    Add test programs for functions from <math.h> to simple.set

---
+++ test/programs/simple/mathFunctions/atan.c
@@ -0,0 +1,21 @@
+// This file is part of CPAchecker,
+// a tool for configurable software verification:
+// https://cpachecker.sosy-lab.org
+//
+// SPDX-FileCopyrightText: 2007-2025 Dirk Beyer <https://www.sosy-lab.org>
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#include <math.h>
+#include <stdlib.h>
+
+void reach_error() {
+  abort();
+}
+
+int main() {
+  if (atan(0) == 13.0) {
+    reach_error();
+  }
+  return 0;
+}
