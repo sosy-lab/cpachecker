7cd8efd6ad681b2621b9f385725e05c92d7b1bc8
Levente Bajczi <bajczilevi@gmail.com>
2025-06-11 15:57:38 +0200

    Added range constraint

--- src/org/sosy_lab/cpachecker/util/predicates/smt/BitvectorFormulaManagerView.java
+++ src/org/sosy_lab/cpachecker/util/predicates/smt/BitvectorFormulaManagerView.java
@@ -163,6 +163,16 @@ public class BitvectorFormulaManagerView extends BaseManagerView
     return manager.lessOrEquals(pNumber1, pNumbe2, signed);
   }
 
+  public BooleanFormula addRangeConstraint(
+      BitvectorFormula term, BigInteger start, BigInteger end) {
+    if (manager instanceof ReplaceBitvectorWithNonlinIntegerAndFunctionTheory nonlinIntManager) {
+      return nonlinIntManager
+          .addRangeConstraint(term, start, end);
+    } else {
+      throw new UnsupportedOperationException("Cannot add range constraint to manager %s".formatted(manager));
+    }
+  }
+
   @Override
   public BitvectorFormula not(BitvectorFormula pBits) {
     return manager.not(pBits);
