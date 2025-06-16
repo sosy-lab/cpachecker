453cb0279ea570df4e68dcfa55645f9217017f09
Levente Bajczi <bajczilevi@gmail.com>
2025-06-16 13:19:29 +0200

    Moved pointer range constraint to proper place

--- src/org/sosy_lab/cpachecker/util/predicates/pathformula/pointeraliasing/PointerTargetSetManager.java
+++ src/org/sosy_lab/cpachecker/util/predicates/pathformula/pointeraliasing/PointerTargetSetManager.java
@@ -638,14 +638,14 @@ class PointerTargetSetManager {
       for (Formula oldAddress : pHighestAllocatedAddresses) {
         pConstraints.addConstraint(
             formulaManager.makeGreaterThan(newBaseFormula, oldAddress, true));
-        // this should probably only be given if
-        //  cpa.predicate.addRangeConstraintsForNondet
-        // is 'true'. But this is no-op for most cases anyway.
-        pConstraints.addConstraint(
-            formulaManager.makeRangeConstraint(newBaseFormula, false)
-        );
       }
     }
+    // this should probably only be given if
+    //  cpa.predicate.addRangeConstraintsForNondet
+    // is 'true'. But this is no-op for most cases anyway.
+    pConstraints.addConstraint(
+        formulaManager.makeRangeConstraint(newBaseFormula, false)
+    );
 
     // Add alignment constraint
     // For incomplete types, better not add constraints (imprecise) than a wrong one (unsound).
