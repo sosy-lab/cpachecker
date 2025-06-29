3f71de905dcc141025fc76b6ad3b8d6f290c6954
Levente Bajczi <bajczilevi@gmail.com>
2025-06-29 12:01:19 +0200

    Moved test to be based on SolverViewBasedTest0

--- src/org/sosy_lab/cpachecker/util/predicates/smt/ReplaceBitvectorWithNLAIntegerTheoryTest.java
+++ src/org/sosy_lab/cpachecker/util/predicates/smt/ReplaceBitvectorWithNLAIntegerTheoryTest.java
@@ -9,74 +9,84 @@
 package org.sosy_lab.cpachecker.util.predicates.smt;
 
 import static com.google.common.truth.Truth.assertWithMessage;
+import static com.google.common.truth.TruthJUnit.assume;
 
-import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
 import java.math.BigInteger;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
+import org.junit.Before;
 import org.junit.Test;
-import org.sosy_lab.common.ShutdownManager;
-import org.sosy_lab.common.configuration.Configuration;
-import org.sosy_lab.common.log.LogManager;
-import org.sosy_lab.cpachecker.util.Pair;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
+import org.sosy_lab.common.configuration.ConfigurationBuilder;
+import org.sosy_lab.common.configuration.InvalidConfigurationException;
 import org.sosy_lab.cpachecker.util.predicates.smt.FormulaManagerView.Theory;
 import org.sosy_lab.cpachecker.util.predicates.smt.ReplaceIntegerWithBitvectorTheory.ReplaceIntegerEncodingOptions;
-import org.sosy_lab.java_smt.SolverContextFactory;
 import org.sosy_lab.java_smt.SolverContextFactory.Solvers;
 import org.sosy_lab.java_smt.api.BitvectorFormula;
 import org.sosy_lab.java_smt.api.BooleanFormula;
-import org.sosy_lab.java_smt.api.BooleanFormulaManager;
 import org.sosy_lab.java_smt.api.FormulaType;
-import org.sosy_lab.java_smt.api.IntegerFormulaManager;
 import org.sosy_lab.java_smt.api.NumeralFormula.IntegerFormula;
 import org.sosy_lab.java_smt.api.ProverEnvironment;
-import org.sosy_lab.java_smt.api.SolverContext;
 import org.sosy_lab.java_smt.api.SolverContext.ProverOptions;
 
-public class ReplaceBitvectorWithNLAIntegerTheoryTest {
+@RunWith(Parameterized.class)
+public class ReplaceBitvectorWithNLAIntegerTheoryTest extends SolverViewBasedTest0 {
+  @Parameters(name = "{0}")
+  public static Solvers[] getAllSolvers() {
+    return Solvers.values();
+  }
 
-  private static SolverContext context;
-  private static IntegerFormulaManager ifm;
-  private static BooleanFormulaManager bfm;
-  private static FormulaWrappingHandler wrappingHandler;
-  private static ReplaceBitvectorWithNLAIntegerTheory replacer;
+  @Parameter(0)
+  public Solvers solverToUse;
 
-  @BeforeClass
-  public static void initSolver() throws Exception {
-    context =
-        SolverContextFactory.createSolverContext(
-            Configuration.defaultConfiguration(),
-            LogManager.createNullLogManager(),
-            ShutdownManager.create().getNotifier(),
-            Solvers.Z3);
+  @Override
+  protected Solvers solverToUse() {
+    return solverToUse;
+  }
+
+  @Override
+  protected ConfigurationBuilder createTestConfigBuilder() {
+    ConfigurationBuilder testConfigBuilder = super.createTestConfigBuilder();
+    testConfigBuilder.setOption("solver.nonLinearArithmetic", "USE");
+    return testConfigBuilder;
+  }
+
+  private FormulaWrappingHandler wrappingHandler;
+  private ReplaceBitvectorWithNLAIntegerTheory replacer;
+
+  private void requireModuloDivMultiply() {
+    assume()
+        .withMessage("Solver %s does not support modulo/div/multiply", solverToUse())
+        .that(solverToUse())
+        .isNotIn(
+            ImmutableSet.of(Solvers.MATHSAT5, Solvers.CVC4, Solvers.SMTINTERPOL, Solvers.OPENSMT));
+  }
+
+  @Before
+  public void setupParams() throws InvalidConfigurationException {
+    requireIntegers();
+    requireModuloDivMultiply();
     wrappingHandler =
         new FormulaWrappingHandler(
             context.getFormulaManager(),
             Theory.INTEGER,
             Theory.FLOAT,
             Theory.INTEGER,
-            new ReplaceIntegerEncodingOptions(Configuration.defaultConfiguration()));
-    ifm = context.getFormulaManager().getIntegerFormulaManager();
-    bfm = context.getFormulaManager().getBooleanFormulaManager();
-    replacer =
-        new ReplaceBitvectorWithNLAIntegerTheory(
-            wrappingHandler, bfm, ifm, Configuration.defaultConfiguration());
-  }
-
-  @AfterClass
-  public static void shutdownSolver() {
-    context.close();
+            new ReplaceIntegerEncodingOptions(config));
+    replacer = new ReplaceBitvectorWithNLAIntegerTheory(wrappingHandler, bmgr, imgr, config);
   }
 
   @Test
   public void testUnsignedWrapAround() throws Exception {
-    final Integer[] values = new Integer[] {1, -1, 15, -15, 16, -16, 25, -25};
+    final int[] values = new int[] {1, -1, 15, -15, 16, -16, 25, -25};
     for (Integer value : values) {
-      IntegerFormula formula = ifm.makeNumber(BigInteger.valueOf(value));
+      IntegerFormula formula = imgr.makeNumber(BigInteger.valueOf(value));
       IntegerFormula expected =
-          ifm.makeNumber(BigInteger.valueOf(value).mod(BigInteger.valueOf(16)));
+          imgr.makeNumber(BigInteger.valueOf(value).mod(BigInteger.valueOf(16)));
       try (ProverEnvironment prover = context.newProverEnvironment()) {
-        BooleanFormula constraint = ifm.equal(replacer.wrapAround(formula, 4), expected);
+        BooleanFormula constraint = imgr.equal(replacer.wrapAround(formula, 4), expected);
         prover.addConstraint(constraint);
         assertWithMessage("Formula %s should be trivially satisfiable".formatted(constraint))
             .that(prover.isUnsat())
@@ -91,10 +101,10 @@ public class ReplaceBitvectorWithNLAIntegerTheoryTest {
     for (Integer value : values) {
       BigInteger expectedNumber =
           value >= 8 ? BigInteger.valueOf(value - 16) : BigInteger.valueOf(value);
-      IntegerFormula formula = ifm.makeNumber(BigInteger.valueOf(value));
-      IntegerFormula expected = ifm.makeNumber(expectedNumber);
+      IntegerFormula formula = imgr.makeNumber(BigInteger.valueOf(value));
+      IntegerFormula expected = imgr.makeNumber(expectedNumber);
       try (ProverEnvironment prover = context.newProverEnvironment()) {
-        BooleanFormula constraint = ifm.equal(replacer.wrapAroundSigned(formula, 4), expected);
+        BooleanFormula constraint = imgr.equal(replacer.wrapAroundSigned(formula, 4), expected);
         prover.addConstraint(constraint);
         assertWithMessage("Formula %s should be trivially satisfiable".formatted(constraint))
             .that(prover.isUnsat())
@@ -117,7 +127,7 @@ public class ReplaceBitvectorWithNLAIntegerTheoryTest {
       prover.addConstraint(range);
       BooleanFormula extend = replacer.equal(replacer.extend(base, 8, false), larger);
       prover.addConstraint(extend);
-      BooleanFormula extract = bfm.not(replacer.equal(replacer.extract(larger, 7, 0), base));
+      BooleanFormula extract = bmgr.not(replacer.equal(replacer.extract(larger, 7, 0), base));
       prover.addConstraint(extract);
       boolean unsat = prover.isUnsat();
 
@@ -143,7 +153,7 @@ public class ReplaceBitvectorWithNLAIntegerTheoryTest {
       prover.addConstraint(range);
       BooleanFormula extend = replacer.equal(replacer.extend(base, 8, true), larger);
       prover.addConstraint(extend);
-      BooleanFormula extract = bfm.not(replacer.equal(replacer.extract(larger, 7, 0), base));
+      BooleanFormula extract = bmgr.not(replacer.equal(replacer.extract(larger, 7, 0), base));
       prover.addConstraint(extract);
       boolean unsat = prover.isUnsat();
       assertWithMessage(
@@ -156,65 +166,63 @@ public class ReplaceBitvectorWithNLAIntegerTheoryTest {
 
   @Test
   public void testModulo() throws Exception {
-    final ImmutableList<Pair<Pair<Integer, Integer>, Integer>> testData =
-        ImmutableList.of(
-            Pair.of(Pair.of(0, 5), 0),
-            Pair.of(Pair.of(-1, 5), -1),
-            Pair.of(Pair.of(-5, 5), 0),
-            Pair.of(Pair.of(-6, 5), -1),
-            Pair.of(Pair.of(6, 5), 1),
-            Pair.of(Pair.of(0, -5), 0),
-            Pair.of(Pair.of(-1, -5), -1),
-            Pair.of(Pair.of(-5, -5), 0),
-            Pair.of(Pair.of(-6, -5), -1),
-            Pair.of(Pair.of(6, -5), 1));
-    for (Pair<Pair<Integer, Integer>, Integer> testDatum : testData) {
-      BitvectorFormula a = replacer.makeBitvector(4, testDatum.getFirst().getFirst());
-      BitvectorFormula b = replacer.makeBitvector(4, testDatum.getFirst().getSecond());
-      BitvectorFormula c = replacer.makeBitvector(4, testDatum.getSecond());
+    checkModulo(0, 5, 0);
+    checkModulo(-1, 5, -1);
+    checkModulo(-5, 5, 0);
+    checkModulo(-6, 5, -1);
+    checkModulo(6, 5, 1);
+    checkModulo(0, -5, 0);
+    checkModulo(-1, -5, -1);
+    checkModulo(-5, -5, 0);
+    checkModulo(-6, -5, -1);
+    checkModulo(6, -5, 1);
+  }
 
-      BitvectorFormula input = replacer.remainder(a, b, true);
-      try (ProverEnvironment prover = context.newProverEnvironment()) {
-        BooleanFormula constraint = replacer.equal(input, c);
-        prover.addConstraint(constraint);
-        assertWithMessage(
-                "Formula %n%s for inputs%n%s%nshould be trivially satisfiable"
-                    .formatted(constraint, testDatum))
-            .that(prover.isUnsat())
-            .isFalse();
-      }
+  private void checkModulo(int aVal, int bVal, int expected) throws Exception {
+    BitvectorFormula a = replacer.makeBitvector(4, aVal);
+    BitvectorFormula b = replacer.makeBitvector(4, bVal);
+    BitvectorFormula c = replacer.makeBitvector(4, expected);
+
+    BitvectorFormula input = replacer.remainder(a, b, true);
+    try (ProverEnvironment prover = context.newProverEnvironment()) {
+      BooleanFormula constraint = replacer.equal(input, c);
+      prover.addConstraint(constraint);
+      assertWithMessage(
+              "Formula %n%s for inputs%n(%d %% %d == %d)%nshould be trivially satisfiable"
+                  .formatted(constraint, aVal, bVal, expected))
+          .that(prover.isUnsat())
+          .isFalse();
     }
   }
 
   @Test
   public void testDiv() throws Exception {
-    final ImmutableList<Pair<Pair<Integer, Integer>, Integer>> testData =
-        ImmutableList.of(
-            Pair.of(Pair.of(0, 5), 0),
-            Pair.of(Pair.of(-1, 5), 0),
-            Pair.of(Pair.of(-5, 5), -1),
-            Pair.of(Pair.of(-6, 5), -1),
-            Pair.of(Pair.of(6, 5), 1),
-            Pair.of(Pair.of(0, -5), 0),
-            Pair.of(Pair.of(-1, -5), 0),
-            Pair.of(Pair.of(-5, -5), 1),
-            Pair.of(Pair.of(-6, -5), 1),
-            Pair.of(Pair.of(6, -5), -1));
-    for (Pair<Pair<Integer, Integer>, Integer> testDatum : testData) {
-      BitvectorFormula a = replacer.makeBitvector(4, testDatum.getFirst().getFirst());
-      BitvectorFormula b = replacer.makeBitvector(4, testDatum.getFirst().getSecond());
-      BitvectorFormula c = replacer.makeBitvector(4, testDatum.getSecond());
+    checkDiv(0, 5, 0);
+    checkDiv(-1, 5, 0);
+    checkDiv(-5, 5, -1);
+    checkDiv(-6, 5, -1);
+    checkDiv(6, 5, 1);
+    checkDiv(0, -5, 0);
+    checkDiv(-1, -5, 0);
+    checkDiv(-5, -5, 1);
+    checkDiv(-6, -5, 1);
+    checkDiv(6, -5, -1);
+  }
 
-      BitvectorFormula input = replacer.divide(a, b, true);
-      try (ProverEnvironment prover = context.newProverEnvironment()) {
-        BooleanFormula constraint = replacer.equal(input, c);
-        prover.addConstraint(constraint);
-        assertWithMessage(
-                "Formula %n%s for inputs%n%s%nshould be trivially satisfiable"
-                    .formatted(constraint, testDatum))
-            .that(prover.isUnsat())
-            .isFalse();
-      }
+  private void checkDiv(int aVal, int bVal, int expected) throws Exception {
+    BitvectorFormula a = replacer.makeBitvector(4, aVal);
+    BitvectorFormula b = replacer.makeBitvector(4, bVal);
+    BitvectorFormula c = replacer.makeBitvector(4, expected);
+
+    BitvectorFormula input = replacer.divide(a, b, true);
+    try (ProverEnvironment prover = context.newProverEnvironment()) {
+      BooleanFormula constraint = replacer.equal(input, c);
+      prover.addConstraint(constraint);
+      assertWithMessage(
+              "Formula %n%s for inputs%n(%d / %d == %d)%nshould be trivially satisfiable"
+                  .formatted(constraint, aVal, bVal, expected))
+          .that(prover.isUnsat())
+          .isFalse();
     }
   }
 
@@ -244,7 +252,7 @@ public class ReplaceBitvectorWithNLAIntegerTheoryTest {
       prover.addConstraint(rangeB);
       BooleanFormula multiply =
           replacer.equal(replacer.multiply(a, b), replacer.multiply(aWrapped, bWrapped));
-      prover.addConstraint(bfm.not(multiply));
+      prover.addConstraint(bmgr.not(multiply));
       boolean unsat = prover.isUnsat();
       assertWithMessage(
               "Formulas {%n%s, %n%s, %n%s%n} should be trivially unsatisfiable, but found model: %n%s"
