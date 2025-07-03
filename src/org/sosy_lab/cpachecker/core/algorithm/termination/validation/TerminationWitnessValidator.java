2d5a34bfcacd1c2261e15496fcad3fbd03298cd9
Marek Jankola <marek.jankola@sosy.ifi.lmu.de>
2025-07-03 17:15:21 +0200

    Adding the skeleton of the algorithm with gaps that need to be filleD

--- src/org/sosy_lab/cpachecker/core/algorithm/termination/validation/TerminationWitnessValidator.java
+++ src/org/sosy_lab/cpachecker/core/algorithm/termination/validation/TerminationWitnessValidator.java
@@ -12,7 +12,9 @@ import com.google.common.collect.ImmutableCollection;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableMap.Builder;
 import com.google.common.collect.ImmutableSet;
+import de.uni_freiburg.informatik.ultimate.util.datastructures.HashDeque;
 import java.nio.file.Path;
+import java.util.Deque;
 import java.util.Set;
 import org.sosy_lab.common.Classes;
 import org.sosy_lab.common.ShutdownNotifier;
@@ -27,8 +29,11 @@ import org.sosy_lab.cpachecker.cfa.model.CFANode;
 import org.sosy_lab.cpachecker.core.algorithm.Algorithm;
 import org.sosy_lab.cpachecker.core.algorithm.bmc.IMCAlgorithm;
 import org.sosy_lab.cpachecker.core.algorithm.bmc.candidateinvariants.ExpressionTreeLocationInvariant;
+import org.sosy_lab.cpachecker.core.defaults.DummyTargetState;
+import org.sosy_lab.cpachecker.core.defaults.SingletonPrecision;
 import org.sosy_lab.cpachecker.core.interfaces.ConfigurableProgramAnalysis;
 import org.sosy_lab.cpachecker.core.reachedset.ReachedSet;
+import org.sosy_lab.cpachecker.cpa.arg.ARGState;
 import org.sosy_lab.cpachecker.cpa.predicate.PredicateCPA;
 import org.sosy_lab.cpachecker.exceptions.CPAException;
 import org.sosy_lab.cpachecker.exceptions.CPATransferException;
@@ -36,7 +41,6 @@ import org.sosy_lab.cpachecker.util.CPAs;
 import org.sosy_lab.cpachecker.util.LoopStructure;
 import org.sosy_lab.cpachecker.util.WitnessInvariantsExtractor;
 import org.sosy_lab.cpachecker.util.WitnessInvariantsExtractor.InvalidWitnessException;
-import org.sosy_lab.cpachecker.util.predicates.pathformula.PathFormula;
 import org.sosy_lab.cpachecker.util.predicates.pathformula.PathFormulaManager;
 import org.sosy_lab.cpachecker.util.predicates.smt.BooleanFormulaManagerView;
 import org.sosy_lab.cpachecker.util.predicates.smt.FormulaManagerView;
@@ -57,6 +61,9 @@ public class TerminationWitnessValidator implements Algorithm {
   private Path terminatingStatementsAutomaton =
       Classes.getCodeLocation(NonTerminationWitnessValidator.class)
           .resolveSibling("config/specification/TerminatingStatements.spc");
+
+  private static final DummyTargetState DUMMY_TARGET_STATE =
+      DummyTargetState.withSimpleTargetInformation("termination");
 
   private final Path witnessPath;
   private final CFA cfa;
@@ -103,6 +110,8 @@ public class TerminationWitnessValidator implements Algorithm {
   @Override
   public AlgorithmStatus run(ReachedSet pReachedSet) throws CPAException, InterruptedException {
     Set<ExpressionTreeLocationInvariant> invariants;
+    ImmutableCollection<LoopStructure.Loop> loops =
+        cfa.getLoopStructure().orElseThrow().getAllLoops();
     try {
       WitnessInvariantsExtractor invariantsExtractor =
           new WitnessInvariantsExtractor(config, logger, cfa, shutdownNotifier, witnessPath);
@@ -115,9 +124,36 @@ public class TerminationWitnessValidator implements Algorithm {
     }
 
     ImmutableMap<LoopStructure.Loop, Formula> loopsToTransitionInvariants =
-        mapTransitionInvariantsToLoops(
-            cfa.getLoopStructure().orElseThrow().getAllLoops(), invariants);
-    return AlgorithmStatus.NO_PROPERTY_CHECKED;
+        mapTransitionInvariantsToLoops(loops, invariants);
+
+    // Check that every transition invariant is disjunctively well-founded
+    for (LoopStructure.Loop loop : loops) {
+      Deque<Formula> waitlist = new HashDeque<>();
+      waitlist.add(loopsToTransitionInvariants.get(loop));
+
+      while (!waitlist.isEmpty()) {
+        Formula invariant = waitlist.remove();
+        // Check the well-foundedness of the subformula
+        if (!isTheFormulaWellFounded(loop, loopsToTransitionInvariants.get(loop))
+            && !canBeDecomposedByDNFRules(invariant, waitlist)) {
+          // The invariant is not disjunctively well-founded
+          pReachedSet.add(new ARGState(DUMMY_TARGET_STATE, null), SingletonPrecision.getInstance());
+          return AlgorithmStatus.SOUND_AND_PRECISE;
+        }
+      }
+    }
+
+    // Check that every invariant is actually a transition invariant, i.e. R^+ => T
+    // TODO: Consider, implementing these checks in two different configurations and run them in
+    // parallel.
+    for (LoopStructure.Loop loop : loops) {
+      if (!isTheInvariantTransitionInvariant(loop, loopsToTransitionInvariants.get(loop))) {
+        pReachedSet.add(new ARGState(DUMMY_TARGET_STATE, null), SingletonPrecision.getInstance());
+        return AlgorithmStatus.SOUND_AND_PRECISE;
+      }
+    }
+    pReachedSet.clear();
+    return AlgorithmStatus.SOUND_AND_PRECISE;
   }
 
   private ImmutableMap<LoopStructure.Loop, Formula> mapTransitionInvariantsToLoops(
@@ -158,5 +194,21 @@ public class TerminationWitnessValidator implements Algorithm {
       }
     }
     return false;
+  }
+
+  // TODO: Write this method
+  private boolean isTheFormulaWellFounded(LoopStructure.Loop pLoop, Formula pFormula) {
+    return true;
+  }
+
+  // TODO: Write the method that decomposes the formula with rewrite rules and puts the formulas in
+  // the waitlist.
+  private boolean canBeDecomposedByDNFRules(Formula pFormula, Deque<Formula> pWaitlist) {
+    return false;
+  }
+
+  // TODO: Write this method
+  private boolean isTheInvariantTransitionInvariant(LoopStructure.Loop pLoop, Formula pInvariant) {
+    return true;
   }
 }
