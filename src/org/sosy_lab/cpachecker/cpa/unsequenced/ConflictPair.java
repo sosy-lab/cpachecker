170a6bd9ec094bb85511a95b72d2dc47ac822e62
erya_wang <eryawang620@gmail.com>
2025-07-13 22:22:27 +0200

    add javadoc

--- src/org/sosy_lab/cpachecker/cpa/unsequenced/ConflictPair.java
+++ src/org/sosy_lab/cpachecker/cpa/unsequenced/ConflictPair.java
@@ -10,6 +10,18 @@ package org.sosy_lab.cpachecker.cpa.unsequenced;
 import org.sosy_lab.cpachecker.cfa.ast.c.CRightHandSide;
 import org.sosy_lab.cpachecker.cfa.model.CFAEdge;
 
+
+/**
+ * A pair of side‐effect accesses that conflict due to unsequenced memory operations.
+ * <p>
+ * Use {@link #of} to create an ordered pair so that (a,b) and (b,a) produce the same object.
+ *
+ * @param accessA first side‐effect (lexicographically ≤ accessB)
+ * @param accessB second side‐effect (lexicographically ≥ accessA)
+ * @param location CFA edge where the conflict was found
+ * @param exprA AST node for accessA
+ * @param exprB AST node for accessB
+ */
 public record ConflictPair(
     SideEffectInfo accessA,
     SideEffectInfo accessB,
