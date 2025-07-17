b19dede4861fa40f59431ec16adb74175f6517be
Daniel Raffler <daniel-raffler@gmx.de>
2025-07-17 13:50:50 +0200

    Add test programs for fmax/fmin when both arguments are (negative) zero

---
+++ test/programs/simple/mathFunctions/minZero1Negative.c
@@ -0,0 +1,21 @@
+// This file is part of CPAchecker,
+// a tool for configurable software verification:
+// https://cpachecker.sosy-lab.org
+//
+// SPDX-FileCopyrightText: 2025 Dirk Beyer <https://www.sosy-lab.org>
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#include <math.h>
+#include <stdlib.h>
+
+void reach_error() {
+  abort();
+}
+
+int main() {
+  // fmin(-0,+0) = -0
+  if (signbit(fmin(0.0, -0.0))) {
+    reach_error();
+  }
+}
