004a161677c330ca1d78ac9739387b6ff7d0fcb1
Jens Hartmann <hartmannje@cip.ifi.lmu.de>
2025-07-17 11:07:00 +0200

    removed unneccessary import

--- src/org/sosy_lab/cpachecker/core/algorithm/TestCaseGeneratorAlgorithm.java
+++ src/org/sosy_lab/cpachecker/core/algorithm/TestCaseGeneratorAlgorithm.java
@@ -21,7 +21,6 @@ import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 import java.util.logging.Level;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import org.checkerframework.checker.nullness.qual.Nullable;
 import org.sosy_lab.common.ShutdownNotifier;
