62fcedaa0a8a355def1d5d6d1335a1a2578d8309
Rabea Lühmann <r.luehmann@campus.lmu.de>
2025-07-30 08:08:01 -0400

    Fix Pipeline

--- test/programs/lemma/fibonacci/fibonacci.c
+++ test/programs/lemma/fibonacci/fibonacci.c
@@ -1,3 +1,12 @@
+/*
+ This file is part of CPAchecker,
+ a tool for configurable software verification:
+ https://cpachecker.sosy-lab.org
+
+ SPDX-FileCopyrightText: 2007-2023 Dirk Beyer <https://www.sosy-lab.org>
+
+ SPDX-License-Identifier: Apache-2.0
+*/
 #include <stdio.h>
 unsigned int fibonacci(unsigned char n){
   if (n <= 1) {
