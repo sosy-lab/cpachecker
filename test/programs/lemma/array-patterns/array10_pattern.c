d0db94c09a1608c4617505bb25259708333dc546
Rabea Lühmann <r.luehmann@campus.lmu.de>
2025-07-31 15:37:47 +0200

    Fix Pipeline

--- test/programs/lemma/array-patterns/array10_pattern.c
+++ test/programs/lemma/array-patterns/array10_pattern.c
@@ -1,4 +1,13 @@
 /*
+ This file is part of CPAchecker,
+ a tool for configurable software verification:
+ https://cpachecker.sosy-lab.org
+
+ SPDX-FileCopyrightText: 2007-2023 Dirk Beyer <https://www.sosy-lab.org>
+
+ SPDX-License-Identifier: Apache-2.0
+ */
+/*
 
 
    Licensed under the Apache License, Version 2.0 (the "License");
