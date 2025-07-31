10e3ed3cd0b51ec6abc9f0075834baf5fbc6533e
Matthias Kettl <matthias.kettl@sosy.ifi.lmu.de>
2025-07-31 17:19:33 +0200

    Add missing package info

--- src/org/sosy_lab/cpachecker/core/algorithm/distributed_summaries/distributed_cpa/operators/coverage/package-info.java
+++ src/org/sosy_lab/cpachecker/core/algorithm/distributed_summaries/distributed_cpa/operators/coverage/package-info.java
@@ -6,4 +6,9 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
+/**
+ * Provides operators for coverage checks in the distributed CPA framework.
+ * These operators determine whether one abstract state covers another,
+ * to decide which preconditions are necessary for the analysis.
+ */
 package org.sosy_lab.cpachecker.core.algorithm.distributed_summaries.distributed_cpa.operators.coverage;
