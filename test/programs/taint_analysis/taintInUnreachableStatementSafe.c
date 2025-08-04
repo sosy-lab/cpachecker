ce2d7bbb6171a24ec9ac5d65d4c865491127b5c1
Diego Salgado <diiego.ise@gmail.com>
2025-08-04 19:32:51 +0200

    Add license and copyright headers to taint analysis test files
    - Updated `taintInUnreachableStatementSafe.c` and `taintInUnreachableStatementUnsafe.c` with license and copyright information.
    - Corrected function signature from `char __VERIFIER_nondet_int()` to `int __VERIFIER_nondet_int()`.

--- test/programs/taint_analysis/taintInUnreachableStatementSafe.c
+++ test/programs/taint_analysis/taintInUnreachableStatementSafe.c
@@ -1,4 +1,12 @@
-extern char __VERIFIER_nondet_int();
+// This file is part of CPAchecker,
+// a tool for configurable software verification:
+// https://cpachecker.sosy-lab.org
+//
+// SPDX-FileCopyrightText: 2025 Dirk Beyer <https://www.sosy-lab.org>
+//
+// SPDX-License-Identifier: Apache-2.0
+
+extern int __VERIFIER_nondet_int();
 
 // This program is unsafe (false-positive) for a path-insensitive taint analysis
 int main() {
