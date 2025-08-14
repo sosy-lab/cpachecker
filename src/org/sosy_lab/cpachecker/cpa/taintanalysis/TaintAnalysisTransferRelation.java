1888c685071afef9ae61e342fc66049413e1f064
Diego Salgado <diiego.ise@gmail.com>
2025-08-14 18:48:12 +0200

    Import `Comparator` in `TaintAnalysisTransferRelation` (used in handling of blank edges)

--- src/org/sosy_lab/cpachecker/cpa/taintanalysis/TaintAnalysisTransferRelation.java
+++ src/org/sosy_lab/cpachecker/cpa/taintanalysis/TaintAnalysisTransferRelation.java
@@ -18,6 +18,7 @@ import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
