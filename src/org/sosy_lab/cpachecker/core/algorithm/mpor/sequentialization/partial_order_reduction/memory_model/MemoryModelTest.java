73ca9cedb77e1a18ce24217984dcffd411f7065d
noahkoenig <email@noahkoenig.de>
2025-08-26 01:03:39 +0200

    add pointer dereference unit test

--- src/org/sosy_lab/cpachecker/core/algorithm/mpor/sequentialization/partial_order_reduction/memory_model/MemoryModelTest.java
+++ src/org/sosy_lab/cpachecker/core/algorithm/mpor/sequentialization/partial_order_reduction/memory_model/MemoryModelTest.java
@@ -15,9 +15,9 @@ import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.ImmutableSetMultimap;
 import com.google.common.collect.ImmutableTable;
 import java.math.BigInteger;
+import java.util.Optional;
 import org.junit.Test;
 import org.sosy_lab.cpachecker.cfa.ast.FileLocation;
-import org.sosy_lab.cpachecker.cfa.ast.c.CIdExpression;
 import org.sosy_lab.cpachecker.cfa.ast.c.CInitializer;
 import org.sosy_lab.cpachecker.cfa.ast.c.CInitializerExpression;
 import org.sosy_lab.cpachecker.cfa.ast.c.CIntegerLiteralExpression;
@@ -26,73 +26,87 @@ import org.sosy_lab.cpachecker.cfa.types.c.CBasicType;
 import org.sosy_lab.cpachecker.cfa.types.c.CPointerType;
 import org.sosy_lab.cpachecker.cfa.types.c.CSimpleType;
 import org.sosy_lab.cpachecker.cfa.types.c.CStorageClass;
-import org.sosy_lab.cpachecker.core.algorithm.mpor.MPOROptions;
-import org.sosy_lab.cpachecker.core.algorithm.mpor.nondeterminism.NondeterminismSource;
-import org.sosy_lab.cpachecker.core.algorithm.mpor.sequentialization.ast.seq_custom.statement.multi_control.MultiControlStatementEncoding;
-import org.sosy_lab.cpachecker.core.algorithm.mpor.sequentialization.ghost_elements.bit_vector.BitVectorEncoding;
-import org.sosy_lab.cpachecker.core.algorithm.mpor.sequentialization.partial_order_reduction.ReductionMode;
 
 public class MemoryModelTest {
 
-  private final MPOROptions testOptions =
-      MPOROptions.testInstance(
-          true,
-          BitVectorEncoding.NONE,
-          false,
-          false,
-          false,
-          false,
-          MultiControlStatementEncoding.NONE,
-          MultiControlStatementEncoding.SWITCH_CASE,
-          false,
-          false,
-          false,
-          false,
-          false,
-          // enable link reduction, otherwise MemoryModel is not created
-          true,
-          false,
-          0,
-          false,
-          NondeterminismSource.NUM_STATEMENTS,
-          false,
-          false,
-          ReductionMode.NONE,
-          true,
-          false,
-          false);
+  // Types
 
-  private final CSimpleType intType =
+  private final CSimpleType int_type =
       new CSimpleType(false, false, CBasicType.INT, false, false, true, false, false, false, false);
 
+  private final CPointerType int_pointer_type = new CPointerType(false, false, int_type);
+
+  // Expressions
+
   private final CIntegerLiteralExpression int_0 =
-      new CIntegerLiteralExpression(FileLocation.DUMMY, intType, BigInteger.valueOf(0));
+      new CIntegerLiteralExpression(FileLocation.DUMMY, int_type, BigInteger.valueOf(0));
 
-  private final CPointerType intPointerType = new CPointerType(false, false, intType);
+  // Initializers
 
-  private final CInitializer pointerInitializer =
+  private final CInitializer int_0_initializer =
       new CInitializerExpression(FileLocation.DUMMY, int_0);
 
-  private final CVariableDeclaration pointerDeclaration =
+  // Declarations
+
+  private final CVariableDeclaration int_pointer_declaration =
       new CVariableDeclaration(
           FileLocation.DUMMY,
           true,
           CStorageClass.AUTO,
-          intPointerType,
+          int_pointer_type,
           "int_ptr",
           "int_ptr",
           "int_ptr",
-          pointerInitializer);
+          int_0_initializer);
 
-  private final CIdExpression pointer = new CIdExpression(FileLocation.DUMMY, pointerDeclaration);
+  private final CVariableDeclaration int_a_declaration =
+      new CVariableDeclaration(
+          FileLocation.DUMMY,
+          true,
+          CStorageClass.AUTO,
+          int_type,
+          "int_A",
+          "int_A",
+          "int_A",
+          int_0_initializer);
 
   // Memory Locations
 
+  private final MemoryLocation int_pointer_memory_location =
+      MemoryLocation.of(Optional.empty(), int_pointer_declaration);
+
+  private final MemoryLocation int_a_memory_location =
+      MemoryLocation.of(Optional.empty(), int_a_declaration);
+
   @Test
-  public void testPointerAssignment() {
+  public void test_single_pointer_assignment() {
+    // first create necessary collections
+    ImmutableMap<MemoryLocation, Integer> memoryLocationIds =
+        ImmutableMap.<MemoryLocation, Integer>builder()
+            .put(int_pointer_memory_location, 0)
+            .put(int_a_memory_location, 1)
+            .buildOrThrow();
+    // int_ptr = &int_A; i.e. pointer assignment
+    ImmutableSetMultimap<CVariableDeclaration, MemoryLocation> pointerAssignments =
+        ImmutableSetMultimap.<CVariableDeclaration, MemoryLocation>builder()
+            .put(int_pointer_declaration, int_a_memory_location)
+            .build();
+    // *int_ptr i.e. pointer dereference
+    ImmutableSet<MemoryLocation> pointerDereferences =
+        ImmutableSet.<MemoryLocation>builder().add(int_pointer_memory_location).build();
+
+    // create memory model
     MemoryModel testMemoryModel =
         new MemoryModel(
-            ImmutableMap.of(), ImmutableSetMultimap.of(), ImmutableTable.of(), ImmutableSet.of());
-    assertThat(testMemoryModel).isNotNull();
+            memoryLocationIds, pointerAssignments, ImmutableTable.of(), pointerDereferences);
+
+    // find the memory locations associated with dereference of 'int_ptr'
+    ImmutableSet<MemoryLocation> memoryLocations =
+        MemoryLocationFinder.findMemoryLocationsByPointerDereference(
+            int_pointer_memory_location, Optional.empty(), testMemoryModel);
+
+    // memory location of 'int_A' should be only one associated with dereference of 'int_ptr'
+    assertThat(memoryLocations.size() == 1).isTrue();
+    assertThat(memoryLocations.contains(int_a_memory_location)).isTrue();
   }
 }
