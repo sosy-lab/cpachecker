c27edbb036ee38bcef47975c3d2ef460fc4ebeb6
Paulína Ayaziová <paulina.ayazi@gmail.com>
2025-09-12 12:08:25 +0200

    Rename symbolic to constraints analysis, add operators

---
+++ src/org/sosy_lab/cpachecker/core/algorithm/distributed_summaries/distributed_cpa/constraints/SerializeConstraintsStateOperator.java
@@ -0,0 +1,46 @@
+// This file is part of CPAchecker,
+// a tool for configurable software verification:
+// https://cpachecker.sosy-lab.org
+//
+// SPDX-FileCopyrightText: 2025 Dirk Beyer <https://www.sosy-lab.org>
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package org.sosy_lab.cpachecker.core.algorithm.distributed_summaries.distributed_cpa.constraints;
+
+import com.google.common.collect.ImmutableMap;
+import java.io.IOException;
+import java.util.HashSet;
+import org.sosy_lab.cpachecker.core.algorithm.distributed_summaries.communication.DssSerializeObjectUtil;
+import org.sosy_lab.cpachecker.core.algorithm.distributed_summaries.communication.messages.ContentBuilder;
+import org.sosy_lab.cpachecker.core.algorithm.distributed_summaries.distributed_cpa.operators.serialize.SerializeOperator;
+import org.sosy_lab.cpachecker.core.interfaces.AbstractState;
+import org.sosy_lab.cpachecker.cpa.constraints.constraint.Constraint;
+import org.sosy_lab.cpachecker.cpa.constraints.domain.ConstraintsState;
+import org.sosy_lab.cpachecker.util.globalinfo.SerializationInfoStorage;
+
+public class SerializeConstraintsStateOperator implements SerializeOperator {
+
+  public static final String CONSTRAINTS_KEY = "constraints";
+
+  @Override
+  public ImmutableMap<String, String> serialize(AbstractState pState) {
+    ConstraintsState state = (ConstraintsState) pState;
+    HashSet<Constraint> constraints = new HashSet<Constraint>(state.getConstraints());
+    String serializedConstraints;
+
+    try {
+      serializedConstraints = DssSerializeObjectUtil.serialize(constraints);
+    } catch (IOException e) {
+      throw new AssertionError("Unable to serialize constraints "
+          + state.getConstraints().toString());
+    }
+
+    return ContentBuilder.builder()
+        .pushLevel(ConstraintsState.class.getName())
+        .put(CONSTRAINTS_KEY, serializedConstraints)
+        .build();
+  }
+
+}
+
