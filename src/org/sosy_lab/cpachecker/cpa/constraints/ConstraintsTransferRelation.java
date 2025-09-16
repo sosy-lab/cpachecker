3e1abdd459051766fb1ad616c7f266fd5f02928f
Paulína Ayaziová <paulina.ayazi@gmail.com>
2025-09-16 18:22:38 +0200

    Fix strengthening with violation condition

--- src/org/sosy_lab/cpachecker/cpa/constraints/ConstraintsTransferRelation.java
+++ src/org/sosy_lab/cpachecker/cpa/constraints/ConstraintsTransferRelation.java
@@ -299,6 +299,9 @@ public class ConstraintsTransferRelation
       } else if (currStrengtheningState instanceof BlockState) {
         strengthenOperator = new BlockAnalysisStrengthenOperator();
       }
+      if (currStrengtheningState instanceof ConstraintsState) {
+        strengthenOperator = new ConstraintsAnalysisStrengthenOperator();
+      }
 
       if (strengthenOperator != null) {
         Optional<Collection<ConstraintsState>> oNewStrengthenedStates =
