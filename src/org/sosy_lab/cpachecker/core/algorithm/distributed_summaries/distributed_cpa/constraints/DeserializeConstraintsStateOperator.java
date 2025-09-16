2824311492c83b75d37324246f07c23fbc1baf9b
Paulína Ayaziová <paulina.ayazi@gmail.com>
2025-09-16 18:18:04 +0200

    Fix unchecked cast

--- src/org/sosy_lab/cpachecker/core/algorithm/distributed_summaries/distributed_cpa/constraints/DeserializeConstraintsStateOperator.java
+++ src/org/sosy_lab/cpachecker/core/algorithm/distributed_summaries/distributed_cpa/constraints/DeserializeConstraintsStateOperator.java
@@ -9,40 +9,32 @@
 package org.sosy_lab.cpachecker.core.algorithm.distributed_summaries.distributed_cpa.constraints;
 
 import com.google.common.base.Preconditions;
-import com.google.common.collect.ImmutableList;
-import com.google.common.collect.ImmutableSet;
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.util.ArrayList;
-import java.util.Base64;
 import java.util.HashSet;
 import org.sosy_lab.cpachecker.core.algorithm.distributed_summaries.communication.DssSerializeObjectUtil;
 import org.sosy_lab.cpachecker.core.algorithm.distributed_summaries.communication.messages.ContentReader;
 import org.sosy_lab.cpachecker.core.algorithm.distributed_summaries.communication.messages.DssMessage;
 import org.sosy_lab.cpachecker.core.algorithm.distributed_summaries.distributed_cpa.operators.deserialize.DeserializeOperator;
-import org.sosy_lab.cpachecker.core.algorithm.distributed_summaries.distributed_cpa.predicate.PredicateOperatorUtil;
-import org.sosy_lab.cpachecker.core.algorithm.distributed_summaries.distributed_cpa.predicate.SerializePredicateStateOperator;
 import org.sosy_lab.cpachecker.core.interfaces.AbstractState;
 import org.sosy_lab.cpachecker.cpa.constraints.constraint.Constraint;
 import org.sosy_lab.cpachecker.cpa.constraints.domain.ConstraintsState;
-import org.sosy_lab.cpachecker.cpa.predicate.PredicateAbstractState;
-import org.sosy_lab.cpachecker.util.globalinfo.SerializationInfoStorage;
-import org.sosy_lab.cpachecker.util.predicates.pathformula.PathFormula;
-import org.sosy_lab.cpachecker.util.predicates.pathformula.SSAMap;
-import org.sosy_lab.cpachecker.util.predicates.pathformula.pointeraliasing.PointerTargetSet;
 
 public class DeserializeConstraintsStateOperator implements DeserializeOperator {
   @Override
   public AbstractState deserialize(DssMessage pMessage) throws InterruptedException {
     // SerializationInfoStorage.storeSerializationInformation(predicateCPA, cfa);
     ContentReader constraintContent = pMessage.getAbstractStateContent(ConstraintsState.class);
-    String serializedConstraints = constraintContent.get(SerializeConstraintsStateOperator.CONSTRAINTS_KEY);
+    String serializedConstraints =
+        constraintContent.get(SerializeConstraintsStateOperator.CONSTRAINTS_KEY);
     Preconditions.checkNotNull(serializedConstraints, "Constraints List must be provided");
 
-    HashSet<Constraint> constraints;
+    HashSet<?> deserializedObjects;
+    HashSet<Constraint> constraints = new HashSet<>();
     try {
-      constraints = DssSerializeObjectUtil.deserialize(serializedConstraints, HashSet.class);
+      deserializedObjects =
+          DssSerializeObjectUtil.deserialize(serializedConstraints, HashSet.class);
+      for (Object o : deserializedObjects) {
+        constraints.add((Constraint) o);
+      }
       return new ConstraintsState(constraints);
 
     } catch (ClassCastException e) {
