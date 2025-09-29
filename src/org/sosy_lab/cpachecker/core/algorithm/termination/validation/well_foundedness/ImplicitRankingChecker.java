e9fe0b6f00a20586fb2ed2882de1d3d4f0d055ed
Marek Jankola <marek.jankola@sosy.ifi.lmu.de>
2025-09-29 17:37:57 +0200

    Removing forgotten print

--- src/org/sosy_lab/cpachecker/core/algorithm/termination/validation/well_foundedness/ImplicitRankingChecker.java
+++ src/org/sosy_lab/cpachecker/core/algorithm/termination/validation/well_foundedness/ImplicitRankingChecker.java
@@ -160,7 +160,6 @@ public class ImplicitRankingChecker implements WellFoundednessChecker {
     }
     builder.append("}}");
     String overapproximatingProgam = builder.toString();
-    System.out.println(overapproximatingProgam);
 
     try {
       // Initialization:
