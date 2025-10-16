fe546f85c29aacf69ce80a742b6b8eb52b07ca5b
Philipp Wendler <uni@philippwendler.de>
2025-10-16 08:04:01 +0200

    Avoid new Python syntax
    The last commit accidentally introduced syntax that is only valid in
    some newer Python version, which I did not know.
    Our flake8 version is old and does not support.
    So temporarily revert this until we update flake8 or get rid of it.

--- scripts/post_processing/coverage/tests/test_generate_executions.py
+++ scripts/post_processing/coverage/tests/test_generate_executions.py
@@ -80,10 +80,9 @@ class TestGenerateExceptionFoundBug(TestGenerateExecutions):
         cex_count = 2  # Will only produce one, checking the output though.
         aa_file = os.path.join(self.aux_root, "dummy_aa.spc")
 
-        with (
-            patch.object(self.logger, "error") as mock_logger,
-            patch.object(self.logger, "info") as mock_info,
-        ):
+        with patch.object(self.logger, "error") as mock_logger, patch.object(
+            self.logger, "info"
+        ) as mock_info:
             try:
                 g = generate_coverage.GenerateFirstThenCollect(
                     instance=instance,
@@ -683,10 +682,9 @@ class TestCoverageIntegrationTimeout(TestCoverage):
         # Will allow only one terminating execution to be generated,
         # since the timer adds 10 second for each call to method 'time'.
         timer = MockAdd10SecTimer(start_time)
-        with (
-            patch.object(self.logger, "info") as mock_info,
-            patch.object(self.logger, "error") as mock_error,
-        ):
+        with patch.object(self.logger, "info") as mock_info, patch.object(
+            self.logger, "error"
+        ) as mock_error:
             mock_info.side_effect = side_effect
             generate_coverage.main(argv, self.logger, timer=timer)
             self.assertEqual(mock_error.mock_calls, [])
