bffd3166740b15c1fb7244848475d980cfa2a3fb
BaierD <daniel.baier@sosy.ifi.lmu.de>
2025-10-26 19:48:48 +0100

    Remove (unused and not implemented) legacy option from SMG2 and delete old SMG code with it

--- src/org/sosy_lab/cpachecker/cpa/smg2/SMGOptions.java
+++ src/org/sosy_lab/cpachecker/cpa/smg2/SMGOptions.java
@@ -18,7 +18,6 @@ import org.sosy_lab.common.configuration.InvalidConfigurationException;
 import org.sosy_lab.common.configuration.Option;
 import org.sosy_lab.common.configuration.Options;
 import org.sosy_lab.common.io.PathTemplate;
-import org.sosy_lab.cpachecker.cpa.smg.SMGRuntimeCheck;
 import org.sosy_lab.cpachecker.cpa.smg2.util.SMGException;
 
 @Options(prefix = "cpa.smg2")
@@ -345,12 +344,6 @@ public class SMGOptions {
 
   @Option(
       secure = true,
-      name = "runtimeCheck",
-      description = "Sets the level of runtime checking: NONE, HALF, FULL")
-  private SMGRuntimeCheck runtimeCheck = SMGRuntimeCheck.NONE;
-
-  @Option(
-      secure = true,
       name = "exportSMG.file",
       description = "Filename format for SMG graph dumps")
   @FileOption(Type.OUTPUT_FILE)
@@ -589,10 +582,6 @@ public class SMGOptions {
 
   public boolean unknownOnUndefined() {
     return unknownOnUndefined;
-  }
-
-  public SMGRuntimeCheck getRuntimeCheck() {
-    return runtimeCheck;
   }
 
   public PathTemplate getExportSMGFilePattern() {
