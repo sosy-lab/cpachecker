661ba9a23f511fce68bd63efb561efdaf83c7096
BaierD <daniel.baier@sosy.ifi.lmu.de>
2025-10-30 16:03:31 +0100

    Only ever return logical expressions from OverflowCPA

--- src/org/sosy_lab/cpachecker/cpa/overflow/OverflowTransferRelation.java
+++ src/org/sosy_lab/cpachecker/cpa/overflow/OverflowTransferRelation.java
@@ -12,8 +12,11 @@ import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
 import java.util.Collection;
 import java.util.Set;
+import org.sosy_lab.cpachecker.cfa.ast.c.CBinaryExpression;
+import org.sosy_lab.cpachecker.cfa.ast.c.CBinaryExpression.BinaryOperator;
 import org.sosy_lab.cpachecker.cfa.ast.c.CBinaryExpressionBuilder;
 import org.sosy_lab.cpachecker.cfa.ast.c.CExpression;
+import org.sosy_lab.cpachecker.cfa.ast.c.CIntegerLiteralExpression;
 import org.sosy_lab.cpachecker.cfa.model.CFAEdge;
 import org.sosy_lab.cpachecker.core.defaults.SingleEdgeTransferRelation;
 import org.sosy_lab.cpachecker.core.interfaces.AbstractState;
@@ -51,6 +54,22 @@ public class OverflowTransferRelation extends SingleEdgeTransferRelation {
     for (CFAEdge nextEdge : cfaEdge.getSuccessor().getLeavingEdges()) {
       Set<CExpression> assumptions = noOverflowAssumptionBuilder.assumptionsForEdge(nextEdge);
 
+      ImmutableSet.Builder<CExpression> logicalAssumptions = ImmutableSet.builder();
+      for (CExpression assumption : assumptions) {
+        if (assumption instanceof CBinaryExpression binExpr && !binExpr.getOperator().isLogicalOperator()) {
+          // Transform into logical expr
+          CExpression logicalAssumption = mkLogical(binExpr);
+          // if (cfaEdge instanceof AssumeEdge assumeEdge &&  !assumeEdge.getTruthAssumption()) {
+            // TODO: Does the analysis apply this?
+            // logicalAssumption = mkNot(logicalAssumption);
+          //}
+          logicalAssumptions.add(logicalAssumption);
+        } else {
+          logicalAssumptions.add(assumption);
+        }
+      }
+      assumptions = logicalAssumptions.build();
+
       for (CExpression assumption : assumptions) {
         outStates.add(new OverflowState(ImmutableSet.of(mkNot(assumption)), true, prev));
       }
@@ -60,6 +79,10 @@ public class OverflowTransferRelation extends SingleEdgeTransferRelation {
     }
 
     return outStates.build();
+  }
+
+  private CExpression mkLogical(CBinaryExpression expr) throws UnrecognizedCodeException {
+    return mkNot(expressionBuilder.buildBinaryExpression(CIntegerLiteralExpression.ZERO, expr, BinaryOperator.EQUALS));
   }
 
   private CExpression mkNot(CExpression arg) {
