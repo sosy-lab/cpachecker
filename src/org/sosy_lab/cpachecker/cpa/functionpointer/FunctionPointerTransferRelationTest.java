2ff2ffc0deb0a862d7949853830e602790fa01ff
Matthias Kettl <matthias.kettl@sosy.ifi.lmu.de>
2025-11-14 11:06:54 +0100

    Add MWE to unit tests for function pointer transfer relation
    Additionally adds the full program where CPAchecker used to produce a wrong result

--- src/org/sosy_lab/cpachecker/cpa/functionpointer/FunctionPointerTransferRelationTest.java
+++ src/org/sosy_lab/cpachecker/cpa/functionpointer/FunctionPointerTransferRelationTest.java
@@ -46,6 +46,9 @@ import org.sosy_lab.cpachecker.core.defaults.SingletonPrecision;
 import org.sosy_lab.cpachecker.core.interfaces.AbstractState;
 import org.sosy_lab.cpachecker.core.interfaces.Precision;
 import org.sosy_lab.cpachecker.exceptions.CPATransferException;
+import org.sosy_lab.cpachecker.util.test.CPATestRunner;
+import org.sosy_lab.cpachecker.util.test.TestDataTools;
+import org.sosy_lab.cpachecker.util.test.TestResults;
 
 public class FunctionPointerTransferRelationTest {
 
@@ -181,5 +184,20 @@ public class FunctionPointerTransferRelationTest {
         relation.getAbstractSuccessorsForEdge(
             initialState, precision, createAssumeEdge(BinaryOperator.LESS_EQUAL, false));
     assertThat(successors).hasSize(1);
+  }
+
+  @Test
+  public void testExampleWithUnknownFunctionPointerTarget() throws Exception {
+    Configuration config =
+        TestDataTools.configurationForTest()
+            .loadFromFile("config/kInduction.properties")
+            .setOption("analysis.entryFunction", "main")
+            .setOption("specification", "test/config/properties/unreach-call.prp")
+            .build();
+
+    TestResults result =
+        CPATestRunner.run(
+            config, "test/programs/function_pointer/unknown_function_pointer_minimal.c");
+    result.assertIsUnsafe();
   }
 }
