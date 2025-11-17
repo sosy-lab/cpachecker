cc4b9ded474861f9e5831c573c3507b6d24b2c8e
Marek Jankola <marek.jankola@sosy.ifi.lmu.de>
2025-11-17 05:58:59 +0100

    Adding a transition_location_invariant

--- src/org/sosy_lab/cpachecker/util/yamlwitnessexport/model/AbstractInvariantEntry.java
+++ src/org/sosy_lab/cpachecker/util/yamlwitnessexport/model/AbstractInvariantEntry.java
@@ -59,7 +59,10 @@ public abstract class AbstractInvariantEntry extends AbstractInformationRecord {
     private Class<? extends AbstractInvariantEntry> getClassForType(String type) {
       return switch (type) {
         case "function_contract" -> FunctionContractEntry.class;
-        case "loop_invariant", "location_invariant", "transition_loop_invariant" ->
+        case "loop_invariant",
+            "location_invariant",
+            "transition_loop_invariant",
+            "transition_location_invariant" ->
             InvariantEntry.class;
         default -> throw new IllegalArgumentException("Unknown invariant type: " + type);
       };
