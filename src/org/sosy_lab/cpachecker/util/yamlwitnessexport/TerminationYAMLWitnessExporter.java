2b080b54e579b5bb5817a51c588c176730d8fe8d
Marek Jankola <marek.jankola@sosy.ifi.lmu.de>
2025-11-19 12:01:11 +0100

    Replacing 1 with 0 for trivial case

--- src/org/sosy_lab/cpachecker/util/yamlwitnessexport/TerminationYAMLWitnessExporter.java
+++ src/org/sosy_lab/cpachecker/util/yamlwitnessexport/TerminationYAMLWitnessExporter.java
@@ -145,7 +145,7 @@ public class TerminationYAMLWitnessExporter extends AbstractYAMLWitnessExporter 
     String currentRank =
         rightSideOfRankingFunction(wrapTheVariablesWithCastToLongLong(rankingFunction, variables));
     if (prevRank.contains(TMP_KEYWORD)) {
-      transitionInvariants.add("1");
+      transitionInvariants.add("0");
     } else {
       transitionInvariants.add(prevRank + " > " + currentRank);
     }
