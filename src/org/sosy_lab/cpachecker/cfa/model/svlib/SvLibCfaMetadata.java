5fb79fe1b391aeaa09db0b1871dea8cc3f54c208
Marian Lingsch-Rosenfeld <Marian.Lingsch@sosy.ifi.lmu.de>
2025-11-22 21:53:46 +0100

    Document what SvLibCfaMetadata does
    Addresses: https://gitlab.com/sosy-lab/software/cpachecker/-/merge_requests/323#note_2907096708

--- src/org/sosy_lab/cpachecker/cfa/model/svlib/SvLibCfaMetadata.java
+++ src/org/sosy_lab/cpachecker/cfa/model/svlib/SvLibCfaMetadata.java
@@ -22,6 +22,37 @@ import org.sosy_lab.cpachecker.cfa.parser.svlib.ast.SvLibProcedureDeclaration;
 import org.sosy_lab.cpachecker.cfa.parser.svlib.ast.commands.SmtLibCommand;
 import org.sosy_lab.cpachecker.cfa.parser.svlib.ast.statements.SvLibHavocStatement;
 
+/**
+ * Metadata for SV-Lib CFAs.
+ *
+ * <p>The metadata contains all the information which is known during parsing, but forgotten during
+ * the CFA building which is required to make use of the specifications inside the program or export
+ * the witnesses.
+ *
+ * <p>Analysis developers should never have a need to access this class directly (except for maybe
+ * the smtLibCommands in particular cases). This information is only intended for people working on
+ * the frontend/changing the specifications being checked or witness export.
+ *
+ * @param smtLibCommands the SMT-Lib commands parsed from the SV-Lib input file
+ * @param tagReferenceToScope mapping from tag references to their scopes, required to resolve
+ *     variable declarations from their name in the SMT-LIB encoding during witness export
+ * @param functionToProcedureDeclaration mapping from function declarations in the CFA to their
+ *     corresponding procedure declarations from the parsing phase. Necessary to get input and local
+ *     variables of procedures during witness export.
+ * @param nodesToActualProcedureDefinitionEnd mapping from CFA nodes to the procedure declarations
+ *     whose definition ends at the respective node. Currently, we split procedure definitions into
+ *     function definitions which start with initializing the local and output variables. This map
+ *     points the last node to the corresponding procedure declaration. This is necessary to
+ *     determine which concrete state contains all the information in the violation witness export.
+ * @param nodesToActualHavocStatementEnd mapping from CFA nodes to the havoc statements whose
+ *     definition ends at the respective node. Similar to procedure definitions, we split parallel
+ *     havoc statements into single havoc function calls which each initialize one variable. In the
+ *     same manner as for procedure definitions, we need this to determine the correct concrete
+ *     state during violation witness export.
+ * @param tagAnnotations mapping from CFA nodes to the tag properties (annotations) associated with
+ *     them
+ * @param tagReferences mapping from CFA nodes to the tag references associated with them
+ */
 public record SvLibCfaMetadata(
     ImmutableList<SmtLibCommand> smtLibCommands,
     ImmutableMap<SvLibTagReference, SvLibScope> tagReferenceToScope,
