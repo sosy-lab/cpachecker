5e8d60f5f702aa95680efe65cc3aa2b8bb844d92
Maximilian Hofstetter <maximilian.hofstetter@gmx.net>
2025-11-24 13:18:15 +0100

    FunArray: Add licensing to test programs

--- test/programs/fun_array/funarray_uniform_init_undet_length.c
+++ test/programs/fun_array/funarray_uniform_init_undet_length.c
@@ -1,3 +1,13 @@
+/*
+ * This file is part of CPAchecker,
+ * a tool for configurable software verification:
+ * https://cpachecker.sosy-lab.org
+ *
+ * SPDX-FileCopyrightText: 2025 Dirk Beyer <https://www.sosy-lab.org>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 extern void __VERIFIER_error(void);
 extern int __VERIFIER_nondet_int(void);
 
