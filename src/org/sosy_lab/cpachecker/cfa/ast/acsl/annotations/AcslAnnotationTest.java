86862d401a5bd30d5394f53f0906c2fbe8305cad
Rabea Lühmann <r.luehmann@campus.lmu.de>
2025-11-25 13:40:49 +0100

    Add method to AcslParser
    Adds a method to parse an Acsl Annotation to the Acsl Parser. The method
    is not yet implemented and the corresponding unit test fails.

---
+++ src/org/sosy_lab/cpachecker/cfa/ast/acsl/annotations/AcslAnnotationTest.java
@@ -0,0 +1,91 @@
+// This file is part of CPAchecker,
+// a tool for configurable software verification:
+// https://cpachecker.sosy-lab.org
+//
+// SPDX-FileCopyrightText: 2025 Dirk Beyer <https://www.sosy-lab.org>
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package org.sosy_lab.cpachecker.cfa.ast.acsl.annotations;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
+import java.math.BigInteger;
+import java.util.Objects;
+import org.junit.Test;
+import org.sosy_lab.cpachecker.cfa.CProgramScope;
+import org.sosy_lab.cpachecker.cfa.ast.FileLocation;
+import org.sosy_lab.cpachecker.cfa.ast.acsl.AcslBinaryTermPredicate;
+import org.sosy_lab.cpachecker.cfa.ast.acsl.AcslBinaryTermPredicate.AcslBinaryTermExpressionOperator;
+import org.sosy_lab.cpachecker.cfa.ast.acsl.AcslBuiltinLogicType;
+import org.sosy_lab.cpachecker.cfa.ast.acsl.AcslCVariableDeclaration;
+import org.sosy_lab.cpachecker.cfa.ast.acsl.AcslIdTerm;
+import org.sosy_lab.cpachecker.cfa.ast.acsl.AcslIntegerLiteralTerm;
+import org.sosy_lab.cpachecker.cfa.ast.acsl.annotations.AcslAssertion.AcslAssertionKind;
+import org.sosy_lab.cpachecker.cfa.ast.acsl.parser.AcslParser;
+import org.sosy_lab.cpachecker.cfa.ast.c.CFunctionDeclaration;
+import org.sosy_lab.cpachecker.cfa.ast.c.CVariableDeclaration;
+import org.sosy_lab.cpachecker.cfa.types.c.CBasicType;
+import org.sosy_lab.cpachecker.cfa.types.c.CFunctionType;
+import org.sosy_lab.cpachecker.cfa.types.c.CSimpleType;
+import org.sosy_lab.cpachecker.cfa.types.c.CStorageClass;
+import org.sosy_lab.cpachecker.cfa.types.c.CTypeQualifiers;
+
+public class AcslAnnotationTest {
+
+  private CSimpleType basicInt() {
+    return new CSimpleType(
+        CTypeQualifiers.NONE, CBasicType.INT, false, false, true, false, false, false, false);
+  }
+
+  private CProgramScope getCProgramScope() {
+    String currentFunctionName = "f";
+
+    CProgramScope scope =
+        CProgramScope.mutableCoy(CProgramScope.empty().withFunctionScope(currentFunctionName));
+    for (String var : ImmutableList.of("x", "y", "z")) {
+      scope.registerDeclaration(
+          new CVariableDeclaration(
+              FileLocation.DUMMY,
+              true,
+              CStorageClass.AUTO,
+              basicInt(),
+              var,
+              var,
+              var,
+              null /* No initializer, we only want it for testing */));
+    }
+    scope.registerDeclaration(
+        new CFunctionDeclaration(
+            FileLocation.DUMMY,
+            new CFunctionType(basicInt(), ImmutableList.of(), false),
+            currentFunctionName,
+            ImmutableList.of(),
+            ImmutableSet.of()));
+
+    return scope;
+  }
+
+  @Test
+  public void parseAssertion() {
+    CProgramScope cProgramScope = getCProgramScope();
+    String input = "assert x == 10;";
+
+    AcslAssertion expected =
+        new AcslAssertion(
+            FileLocation.DUMMY,
+            AcslAssertionKind.ASSERTION,
+            new AcslBinaryTermPredicate(
+                FileLocation.DUMMY,
+                new AcslIdTerm(
+                    FileLocation.DUMMY,
+                    new AcslCVariableDeclaration(
+                        (CVariableDeclaration)
+                            Objects.requireNonNull(cProgramScope.lookupVariable("x")))),
+                new AcslIntegerLiteralTerm(
+                    FileLocation.DUMMY, AcslBuiltinLogicType.INTEGER, BigInteger.TEN),
+                AcslBinaryTermExpressionOperator.EQUALS));
+    AcslAssertion parsed = (AcslAssertion) AcslParser.parseAcslAnnotation(input);
+    assert parsed.equals(expected);
+  }
+}
