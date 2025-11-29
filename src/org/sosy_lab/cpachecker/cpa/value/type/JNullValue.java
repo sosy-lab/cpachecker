10a7bcfd772f4aee8d33fd2060effbe0e2f99079
BaierD <daniel.baier@sosy.ifi.lmu.de>
2025-11-29 19:17:44 +0100

    Remove TODO about C from JNullValue.java

--- src/org/sosy_lab/cpachecker/cpa/value/type/JNullValue.java
+++ src/org/sosy_lab/cpachecker/cpa/value/type/JNullValue.java
@@ -47,7 +47,6 @@ public enum JNullValue implements Value {
    */
   @Override
   public OptionalLong asLong(CType pType) {
-    // TODO: wrong for C....
     throw new AssertionError("Null cannot be represented as Long");
   }
 
