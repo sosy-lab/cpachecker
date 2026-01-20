aff5d815613edc0aa19413994813a101e2539fed
BaierD <daniel.baier@sosy.ifi.lmu.de>
2026-01-20 19:37:29 +0100

    Add more tests for SMG2 comparing 2 pointers that exceed their memory for equality

--- src/org/sosy_lab/cpachecker/util/smg/SMGProveNequalityTest.java
+++ src/org/sosy_lab/cpachecker/util/smg/SMGProveNequalityTest.java
@@ -138,28 +138,28 @@ public class SMGProveNequalityTest extends SMGTest0 {
 
     // State is irrelevant for this check (as we only use concrete offsets)
     SMGProveNequality nequality = new SMGProveNequality(SMGCPATest0.stateFromSMG(smg), options);
-    assertThat(nequality.checkIfEdgePointsOutOfBoundsAndMayOverlap(pt1, pt2, false)).isFalse();
-    assertThat(nequality.checkIfEdgePointsOutOfBoundsAndMayOverlap(pt2, pt1, false)).isFalse();
+    assertThat(nequality.checkPointsToEdgesOutOfBoundsEquality(pt1, pt2)).isFalse();
+    assertThat(nequality.checkPointsToEdgesOutOfBoundsEquality(pt2, pt1)).isFalse();
 
-    assertThat(nequality.checkIfEdgePointsOutOfBoundsAndMayOverlap(pt1, pt2, true)).isFalse();
-    assertThat(nequality.checkIfEdgePointsOutOfBoundsAndMayOverlap(pt2, pt1, true)).isFalse();
+    assertThat(nequality.checkPointsToEdgesOutOfBoundsEquality(pt1, pt2)).isFalse();
+    assertThat(nequality.checkPointsToEdgesOutOfBoundsEquality(pt2, pt1)).isFalse();
 
     SMGPointsToEdge pt22 = createPTEdge(32, SMGTargetSpecifier.IS_REGION, reg2);
 
-    assertThat(nequality.checkIfEdgePointsOutOfBoundsAndMayOverlap(pt1, pt22, true)).isFalse();
-    assertThat(nequality.checkIfEdgePointsOutOfBoundsAndMayOverlap(pt22, pt1, true)).isFalse();
+    assertThat(nequality.checkPointsToEdgesOutOfBoundsEquality(pt1, pt22)).isFalse();
+    assertThat(nequality.checkPointsToEdgesOutOfBoundsEquality(pt22, pt1)).isFalse();
 
-    assertThat(nequality.checkIfEdgePointsOutOfBoundsAndMayOverlap(pt1, pt22, false)).isFalse();
-    assertThat(nequality.checkIfEdgePointsOutOfBoundsAndMayOverlap(pt22, pt1, false)).isFalse();
+    assertThat(nequality.checkPointsToEdgesOutOfBoundsEquality(pt1, pt22)).isFalse();
+    assertThat(nequality.checkPointsToEdgesOutOfBoundsEquality(pt22, pt1)).isFalse();
 
     SMGPointsToEdge pt12 = createPTEdge(31, SMGTargetSpecifier.IS_REGION, reg1);
     SMGPointsToEdge pt23 = createPTEdge(63, SMGTargetSpecifier.IS_REGION, reg2);
 
-    assertThat(nequality.checkIfEdgePointsOutOfBoundsAndMayOverlap(pt12, pt23, true)).isFalse();
-    assertThat(nequality.checkIfEdgePointsOutOfBoundsAndMayOverlap(pt23, pt12, true)).isFalse();
+    assertThat(nequality.checkPointsToEdgesOutOfBoundsEquality(pt12, pt23)).isFalse();
+    assertThat(nequality.checkPointsToEdgesOutOfBoundsEquality(pt23, pt12)).isFalse();
 
-    assertThat(nequality.checkIfEdgePointsOutOfBoundsAndMayOverlap(pt12, pt23, false)).isFalse();
-    assertThat(nequality.checkIfEdgePointsOutOfBoundsAndMayOverlap(pt23, pt12, false)).isFalse();
+    assertThat(nequality.checkPointsToEdgesOutOfBoundsEquality(pt12, pt23)).isFalse();
+    assertThat(nequality.checkPointsToEdgesOutOfBoundsEquality(pt23, pt12)).isFalse();
   }
 
   @Test
@@ -170,8 +170,8 @@ public class SMGProveNequalityTest extends SMGTest0 {
     SMGPointsToEdge pt1Zero = createPTEdge(0, SMGTargetSpecifier.IS_REGION, reg1);
     SMGPointsToEdge pt2Zero = createPTEdge(0, SMGTargetSpecifier.IS_REGION, reg2);
 
-    SMGPointsToEdge pt1Max = createPTEdge(0, SMGTargetSpecifier.IS_REGION, reg1);
-    SMGPointsToEdge pt2Max = createPTEdge(0, SMGTargetSpecifier.IS_REGION, reg2);
+    SMGPointsToEdge pt1Max = createPTEdge(31, SMGTargetSpecifier.IS_REGION, reg1);
+    SMGPointsToEdge pt2Max = createPTEdge(63, SMGTargetSpecifier.IS_REGION, reg2);
 
     SMGPointsToEdge pt1Middle = createPTEdge(16, SMGTargetSpecifier.IS_REGION, reg1);
     SMGPointsToEdge pt2Middle = createPTEdge(32, SMGTargetSpecifier.IS_REGION, reg2);
@@ -206,28 +206,252 @@ public class SMGProveNequalityTest extends SMGTest0 {
 
     for (SMGPointsToEdge exceedingEdge : allExceedingEdges) {
       for (SMGPointsToEdge notExceedingEdge : allNotExceedingEdges) {
-        assertThat(
-                nequality.checkIfEdgePointsOutOfBoundsAndMayOverlap(
-                    exceedingEdge, notExceedingEdge, true))
-            .isTrue();
-        assertThat(
-                nequality.checkIfEdgePointsOutOfBoundsAndMayOverlap(
-                    notExceedingEdge, exceedingEdge, true))
-            .isFalse();
+        assertThat(nequality.checkPointsToEdgeOutOfBounds(exceedingEdge)).isTrue();
+        assertThat(nequality.checkPointsToEdgeOutOfBounds(notExceedingEdge)).isFalse();
       }
     }
 
     for (SMGPointsToEdge exceedingEdge : allExceedingEdges) {
       for (SMGPointsToEdge otherExceedingEdge : allExceedingEdges) {
-        assertThat(
-                nequality.checkIfEdgePointsOutOfBoundsAndMayOverlap(
-                    exceedingEdge, otherExceedingEdge, true))
-            .isTrue();
-        assertThat(
-                nequality.checkIfEdgePointsOutOfBoundsAndMayOverlap(
-                    otherExceedingEdge, exceedingEdge, true))
-            .isTrue();
+        assertThat(nequality.checkPointsToEdgeOutOfBounds(exceedingEdge)).isTrue();
+        assertThat(nequality.checkPointsToEdgeOutOfBounds(otherExceedingEdge)).isTrue();
       }
     }
+  }
+
+  @Test
+  public void pointsToEdgeOutOfBoundsWithMemoryLayoutCheckTest()
+      throws InvalidConfigurationException, SMGSolverException {
+    SMGObject reg1 = createRegion(32);
+    SMGObject reg2 = createRegion(64);
+    SMGObject reg3 = createRegion(128);
+
+    SMGPointsToEdge pt1OffsetZero = createPTEdge(0, SMGTargetSpecifier.IS_REGION, reg1);
+    SMGPointsToEdge pt2OffsetZero = createPTEdge(0, SMGTargetSpecifier.IS_REGION, reg2);
+    SMGPointsToEdge pt3OffsetZero = createPTEdge(0, SMGTargetSpecifier.IS_REGION, reg3);
+    SMGPointsToEdge pt3Offset32 = createPTEdge(32, SMGTargetSpecifier.IS_REGION, reg3);
+    SMGPointsToEdge pt3Offset64 = createPTEdge(64, SMGTargetSpecifier.IS_REGION, reg3);
+
+    SMGPointsToEdge pt1OffsetMax = createPTEdge(31, SMGTargetSpecifier.IS_REGION, reg1);
+    SMGPointsToEdge pt2OffsetMax = createPTEdge(63, SMGTargetSpecifier.IS_REGION, reg2);
+
+    SMGPointsToEdge pt2Offset32 = createPTEdge(32, SMGTargetSpecifier.IS_REGION, reg2);
+
+    SMGPointsToEdge pt1NegativeExceededBy1 = createPTEdge(-1, SMGTargetSpecifier.IS_REGION, reg1);
+
+    SMGPointsToEdge pt1NegativeExceededBy16 = createPTEdge(-16, SMGTargetSpecifier.IS_REGION, reg1);
+
+    SMGPointsToEdge pt1ExceededBy1 = createPTEdge(32, SMGTargetSpecifier.IS_REGION, reg1);
+
+    SMGPointsToEdge pt1ExceededBy16 = createPTEdge(48, SMGTargetSpecifier.IS_REGION, reg1);
+    SMGPointsToEdge pt2ExceededBy64 = createPTEdge(128, SMGTargetSpecifier.IS_REGION, reg2);
+
+    SMGPointsToEdge pt1ExceededBy32 = createPTEdge(64, SMGTargetSpecifier.IS_REGION, reg1);
+    SMGPointsToEdge pt2ExceededBy128 = createPTEdge(192, SMGTargetSpecifier.IS_REGION, reg2);
+
+    // State is irrelevant for this check (as we only use concrete offsets)
+    SMGProveNequality nequality = new SMGProveNequality(SMGCPATest0.stateFromSMG(smg), options);
+
+    assertThat(nequality.checkPointsToEdgesOutOfBoundsEquality(pt1OffsetZero, pt3OffsetZero))
+        .isFalse();
+    assertThat(nequality.checkPointsToEdgesOutOfBoundsEquality(pt1OffsetMax, pt3OffsetZero))
+        .isFalse();
+    assertThat(nequality.checkPointsToEdgesOutOfBoundsEquality(pt1OffsetMax, pt3Offset32))
+        .isFalse();
+    assertThat(nequality.checkPointsToEdgesOutOfBoundsEquality(pt1OffsetMax, pt3Offset64))
+        .isFalse();
+
+    assertThat(nequality.checkPointsToEdgesOutOfBoundsEquality(pt1ExceededBy1, pt3OffsetZero))
+        .isTrue();
+    assertThat(nequality.checkPointsToEdgesOutOfBoundsEquality(pt1ExceededBy1, pt3Offset32))
+        .isFalse();
+
+    assertThat(nequality.checkPointsToEdgesOutOfBoundsEquality(pt1ExceededBy16, pt3OffsetZero))
+        .isTrue();
+    assertThat(nequality.checkPointsToEdgesOutOfBoundsEquality(pt1ExceededBy16, pt3Offset32))
+        .isFalse();
+    assertThat(nequality.checkPointsToEdgesOutOfBoundsEquality(pt1ExceededBy16, pt2OffsetZero))
+        .isTrue();
+    assertThat(nequality.checkPointsToEdgesOutOfBoundsEquality(pt1ExceededBy16, pt2Offset32))
+        .isFalse();
+
+    assertThat(nequality.checkPointsToEdgesOutOfBoundsEquality(pt1ExceededBy32, pt2OffsetZero))
+        .isTrue();
+    assertThat(nequality.checkPointsToEdgesOutOfBoundsEquality(pt1ExceededBy32, pt2Offset32))
+        .isTrue();
+    assertThat(nequality.checkPointsToEdgesOutOfBoundsEquality(pt1ExceededBy32, pt2OffsetMax))
+        .isFalse();
+
+    assertThat(nequality.checkPointsToEdgesOutOfBoundsEquality(pt1ExceededBy32, pt3OffsetZero))
+        .isTrue();
+    assertThat(nequality.checkPointsToEdgesOutOfBoundsEquality(pt1ExceededBy32, pt3Offset32))
+        .isTrue();
+    // The memory has 64 bits space before the pointer, so this can not match!
+    assertThat(nequality.checkPointsToEdgesOutOfBoundsEquality(pt1ExceededBy32, pt3Offset64))
+        .isFalse();
+
+    // None can succeed for exceeding negatively by 1, as the rest of the memory can not fit
+    assertThat(
+            nequality.checkPointsToEdgesOutOfBoundsEquality(pt1NegativeExceededBy1, pt3OffsetZero))
+        .isFalse();
+
+    // region 2 is too large for this to match
+    assertThat(
+            nequality.checkPointsToEdgesOutOfBoundsEquality(pt1NegativeExceededBy16, pt2OffsetZero))
+        .isFalse();
+    // region 3 is too large for this to match
+    assertThat(
+            nequality.checkPointsToEdgesOutOfBoundsEquality(pt1NegativeExceededBy16, pt3OffsetZero))
+        .isFalse();
+
+    // region 2 is too large for this to match
+    assertThat(
+            nequality.checkPointsToEdgesOutOfBoundsEquality(pt1NegativeExceededBy16, pt2Offset32))
+        .isFalse();
+    // region 3 is too large for this to match
+    assertThat(
+            nequality.checkPointsToEdgesOutOfBoundsEquality(pt1NegativeExceededBy16, pt3Offset32))
+        .isFalse();
+
+    assertThat(
+            nequality.checkPointsToEdgesOutOfBoundsEquality(pt1NegativeExceededBy16, pt2OffsetMax))
+        .isTrue();
+    // region 3 is too large for this to match
+    assertThat(
+            nequality.checkPointsToEdgesOutOfBoundsEquality(pt1NegativeExceededBy16, pt3Offset64))
+        .isFalse();
+
+    assertThat(nequality.checkPointsToEdgesOutOfBoundsEquality(pt2ExceededBy64, pt3Offset32))
+        .isTrue();
+    assertThat(nequality.checkPointsToEdgesOutOfBoundsEquality(pt2ExceededBy64, pt3Offset64))
+        .isTrue();
+
+    assertThat(nequality.checkPointsToEdgesOutOfBoundsEquality(pt2ExceededBy128, pt3Offset32))
+        .isTrue();
+    assertThat(nequality.checkPointsToEdgesOutOfBoundsEquality(pt2ExceededBy128, pt3OffsetZero))
+        .isTrue();
+    assertThat(nequality.checkPointsToEdgesOutOfBoundsEquality(pt2ExceededBy128, pt3Offset64))
+        .isTrue();
+  }
+
+  /*
+   * Test for both pointers exceeding (both positive or both negative),
+   * as then we might need to take the other memory structure into account.
+   * E.g. 2 memory structures 32 bit, both pointer have offset 32 bit, they can't be equal,
+   * as both are at the end of their memory. If one is offset 64 bit, it may be equal to the 32
+   * bit one, as the memory of the 32 bit fits in between the end of the 64 bit
+   * offset memory and the pointers locations.
+   */
+  @Test
+  public void twoPointsToEdgesOutOfBoundsWithEqualSignumWithMemoryLayoutCheckTest()
+      throws InvalidConfigurationException, SMGSolverException {
+    SMGObject reg1 = createRegion(32);
+    SMGObject reg2 = createRegion(64);
+
+    SMGPointsToEdge pt1ExceededBy32 = createPTEdge(64, SMGTargetSpecifier.IS_REGION, reg1);
+    SMGPointsToEdge pt1ExceededBy63 = createPTEdge(95, SMGTargetSpecifier.IS_REGION, reg1);
+    SMGPointsToEdge pt1ExceededBy64 = createPTEdge(96, SMGTargetSpecifier.IS_REGION, reg1);
+    SMGPointsToEdge pt1ExceededBy65 = createPTEdge(97, SMGTargetSpecifier.IS_REGION, reg1);
+    SMGPointsToEdge pt1ExceededBy95 = createPTEdge(127, SMGTargetSpecifier.IS_REGION, reg1);
+    SMGPointsToEdge pt1ExceededBy96 = createPTEdge(128, SMGTargetSpecifier.IS_REGION, reg1);
+    SMGPointsToEdge pt1ExceededBy1 = createPTEdge(32, SMGTargetSpecifier.IS_REGION, reg1);
+    SMGPointsToEdge pt1NegativeExceededBy16 = createPTEdge(-16, SMGTargetSpecifier.IS_REGION, reg1);
+    SMGPointsToEdge pt1NegativeExceededBy1 = createPTEdge(-1, SMGTargetSpecifier.IS_REGION, reg1);
+    SMGPointsToEdge pt1NegativeExceededBy32 = createPTEdge(-32, SMGTargetSpecifier.IS_REGION, reg1);
+    SMGPointsToEdge pt1NegativeExceededBy64 = createPTEdge(-64, SMGTargetSpecifier.IS_REGION, reg1);
+    SMGPointsToEdge pt1NegativeExceededBy65 = createPTEdge(-65, SMGTargetSpecifier.IS_REGION, reg1);
+    SMGPointsToEdge pt1NegativeExceededBy96 = createPTEdge(-96, SMGTargetSpecifier.IS_REGION, reg1);
+
+    SMGPointsToEdge pt2ExceededBy32 = createPTEdge(96, SMGTargetSpecifier.IS_REGION, reg2);
+    SMGPointsToEdge pt2ExceededBy33 = createPTEdge(97, SMGTargetSpecifier.IS_REGION, reg2);
+    SMGPointsToEdge pt2ExceededBy64 = createPTEdge(128, SMGTargetSpecifier.IS_REGION, reg2);
+    SMGPointsToEdge pt2ExceededBy128 = createPTEdge(192, SMGTargetSpecifier.IS_REGION, reg2);
+    SMGPointsToEdge pt2NegativeExceededBy1 = createPTEdge(-1, SMGTargetSpecifier.IS_REGION, reg2);
+    SMGPointsToEdge pt2NegativeExceededBy32 = createPTEdge(-32, SMGTargetSpecifier.IS_REGION, reg2);
+    SMGPointsToEdge pt2NegativeExceededBy33 = createPTEdge(-33, SMGTargetSpecifier.IS_REGION, reg2);
+    SMGPointsToEdge pt2NegativeExceededBy48 = createPTEdge(-48, SMGTargetSpecifier.IS_REGION, reg2);
+    SMGPointsToEdge pt2ExceededBy1 = createPTEdge(64, SMGTargetSpecifier.IS_REGION, reg2);
+
+    // State is irrelevant for this check (as we only use concrete offsets)
+    SMGProveNequality nequality = new SMGProveNequality(SMGCPATest0.stateFromSMG(smg), options);
+
+    assertThat(nequality.checkPointsToEdgesOutOfBoundsEquality(pt1ExceededBy1, pt2ExceededBy1))
+        .isFalse();
+    assertThat(nequality.checkPointsToEdgesOutOfBoundsEquality(pt1ExceededBy32, pt2ExceededBy1))
+        .isFalse();
+    assertThat(nequality.checkPointsToEdgesOutOfBoundsEquality(pt1ExceededBy63, pt2ExceededBy1))
+        .isFalse();
+    // region 2 fits in between region 1 and its pointer + offsets (exactly) here, with buffer below
+    assertThat(nequality.checkPointsToEdgesOutOfBoundsEquality(pt1ExceededBy64, pt2ExceededBy1))
+        .isTrue();
+    assertThat(nequality.checkPointsToEdgesOutOfBoundsEquality(pt1ExceededBy65, pt2ExceededBy1))
+        .isTrue();
+    assertThat(nequality.checkPointsToEdgesOutOfBoundsEquality(pt1ExceededBy95, pt2ExceededBy1))
+        .isTrue();
+    assertThat(nequality.checkPointsToEdgesOutOfBoundsEquality(pt1ExceededBy96, pt2ExceededBy1))
+        .isTrue();
+
+    // With the layout: reg2 follow by reg1 directly, the offsets can match exactly here
+    assertThat(nequality.checkPointsToEdgesOutOfBoundsEquality(pt1ExceededBy1, pt2ExceededBy32))
+        .isTrue();
+    // Now the offset of region1s pointer exceeds the reach of region2s pointer
+    assertThat(nequality.checkPointsToEdgesOutOfBoundsEquality(pt1ExceededBy32, pt2ExceededBy32))
+        .isFalse();
+    assertThat(nequality.checkPointsToEdgesOutOfBoundsEquality(pt1ExceededBy63, pt2ExceededBy32))
+        .isFalse();
+    assertThat(nequality.checkPointsToEdgesOutOfBoundsEquality(pt1ExceededBy64, pt2ExceededBy32))
+        .isFalse();
+    assertThat(nequality.checkPointsToEdgesOutOfBoundsEquality(pt1ExceededBy65, pt2ExceededBy32))
+        .isFalse();
+    assertThat(nequality.checkPointsToEdgesOutOfBoundsEquality(pt1ExceededBy95, pt2ExceededBy32))
+        .isFalse();
+    // region 2 fits in between region 1 and its pointer + offsets
+    assertThat(nequality.checkPointsToEdgesOutOfBoundsEquality(pt1ExceededBy96, pt2ExceededBy32))
+        .isTrue();
+
+    // Reg1 fits in between reg2 and its pointer
+    assertThat(nequality.checkPointsToEdgesOutOfBoundsEquality(pt1ExceededBy1, pt2ExceededBy33))
+        .isTrue();
+    assertThat(nequality.checkPointsToEdgesOutOfBoundsEquality(pt1ExceededBy1, pt2ExceededBy64))
+        .isTrue();
+    assertThat(nequality.checkPointsToEdgesOutOfBoundsEquality(pt1ExceededBy1, pt2ExceededBy128))
+        .isTrue();
+
+    // reg 2 fits not in between reg1 and its pointer and vice versa
+    assertThat(
+            nequality.checkPointsToEdgesOutOfBoundsEquality(
+                pt1NegativeExceededBy64, pt2NegativeExceededBy1))
+        .isFalse();
+    assertThat(
+            nequality.checkPointsToEdgesOutOfBoundsEquality(
+                pt1NegativeExceededBy32, pt2NegativeExceededBy1))
+        .isFalse();
+    assertThat(
+            nequality.checkPointsToEdgesOutOfBoundsEquality(
+                pt1NegativeExceededBy1, pt2NegativeExceededBy1))
+        .isFalse();
+    assertThat(
+            nequality.checkPointsToEdgesOutOfBoundsEquality(
+                pt2NegativeExceededBy32, pt1NegativeExceededBy1))
+        .isFalse();
+
+    // reg 1 fits in between reg2 and its pointer and vice versa
+    assertThat(
+            nequality.checkPointsToEdgesOutOfBoundsEquality(
+                pt1NegativeExceededBy65, pt2NegativeExceededBy1))
+        .isTrue();
+    assertThat(
+            nequality.checkPointsToEdgesOutOfBoundsEquality(
+                pt1NegativeExceededBy96, pt2NegativeExceededBy32))
+        .isTrue();
+
+    assertThat(
+            nequality.checkPointsToEdgesOutOfBoundsEquality(
+                pt2NegativeExceededBy33, pt1NegativeExceededBy1))
+        .isTrue();
+    assertThat(
+            nequality.checkPointsToEdgesOutOfBoundsEquality(
+                pt2NegativeExceededBy48, pt1NegativeExceededBy16))
+        .isTrue();
   }
 }
