f98aaf2a39a3da75bf98ea075fa34c534a3c001f
Paulína Ayaziová <paulina.ayazi@gmail.com>
2026-01-24 21:45:36 +0100

    Remoove unnecessary code

--- src/org/sosy_lab/cpachecker/core/algorithm/distributed_summaries/distributed_cpa/composite/DeserializeCompositeStateOperator.java
+++ src/org/sosy_lab/cpachecker/core/algorithm/distributed_summaries/distributed_cpa/composite/DeserializeCompositeStateOperator.java
@@ -9,22 +9,16 @@
 package org.sosy_lab.cpachecker.core.algorithm.distributed_summaries.distributed_cpa.composite;
 
 import com.google.common.collect.ImmutableList;
-import java.util.HashMap;
 import java.util.List;
 import org.sosy_lab.cpachecker.core.algorithm.distributed_summaries.communication.messages.DssMessage;
 import org.sosy_lab.cpachecker.core.algorithm.distributed_summaries.decomposition.graph.BlockNode;
 import org.sosy_lab.cpachecker.core.algorithm.distributed_summaries.distributed_cpa.DistributedConfigurableProgramAnalysis;
 import org.sosy_lab.cpachecker.core.algorithm.distributed_summaries.distributed_cpa.DssBlockAnalysisStatistics;
-import org.sosy_lab.cpachecker.core.algorithm.distributed_summaries.distributed_cpa.constraints.DeserializeConstraintsStateOperator;
-import org.sosy_lab.cpachecker.core.algorithm.distributed_summaries.distributed_cpa.constraints.util.SymbolicIdentifierRenamer;
 import org.sosy_lab.cpachecker.core.algorithm.distributed_summaries.distributed_cpa.operators.deserialize.DeserializeOperator;
-import org.sosy_lab.cpachecker.core.algorithm.distributed_summaries.distributed_cpa.value.DeserializeValueAnalysisStateOperator;
 import org.sosy_lab.cpachecker.core.interfaces.AbstractState;
 import org.sosy_lab.cpachecker.core.interfaces.ConfigurableProgramAnalysis;
 import org.sosy_lab.cpachecker.core.interfaces.StateSpacePartition;
 import org.sosy_lab.cpachecker.cpa.composite.CompositeState;
-import org.sosy_lab.cpachecker.cpa.constraints.domain.ConstraintsState;
-import org.sosy_lab.cpachecker.cpa.value.ValueAnalysisState;
 
 public class DeserializeCompositeStateOperator implements DeserializeOperator {
 
@@ -63,22 +57,4 @@ public class DeserializeCompositeStateOperator implements DeserializeOperator {
       stats.getDeserializationTime().stop();
     }
   }
-
-  /*public CompositeState renameIdentifiersState(CompositeState pState) {
-
-    ImmutableList.Builder<AbstractState> states = ImmutableList.builder();
-    SymbolicIdentifierRenamer visitor = new SymbolicIdentifierRenamer(new HashMap<>());
-
-    for (AbstractState state : pState.getWrappedStates()) {
-      if (state instanceof ValueAnalysisState valueState) {
-        states.add(DeserializeValueAnalysisStateOperator.renameIds(valueState, visitor, blockNode.getId()));
-      } else if (state instanceof ConstraintsState constraintsState) {
-        states.add(DeserializeConstraintsStateOperator.renameIds(constraintsState, visitor));
-      } else {
-        states.add(state);
-      }
-    }
-
-    return new CompositeState(states.build());
-  }*/
 }
