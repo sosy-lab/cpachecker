7d3b97172b2c1ad73072b0d015b9e7ec572f4648
noahkoenig <email@noahkoenig.de>
2026-01-28 16:34:10 +0100

    update CAstStatement JavaDoc

--- src/org/sosy_lab/cpachecker/cfa/ast/c/CAstStatement.java
+++ src/org/sosy_lab/cpachecker/cfa/ast/c/CAstStatement.java
@@ -10,12 +10,15 @@ package org.sosy_lab.cpachecker.cfa.ast.c;
 
 import org.sosy_lab.cpachecker.cfa.CFA;
 import org.sosy_lab.cpachecker.cfa.ast.AAstNode.AAstNodeRepresentation;
-import org.sosy_lab.cpachecker.cfa.model.CFAEdge;
 
 /**
- * Provides a common interface for {@link CStatement} (via {@link CStatementWrapper}) and all other
- * statements that do not appear in the {@link CFAEdge}s of a {@link CFA}, but may e.g. be exported
- * in actual C programs like a {@link CIfStatement}.
+ * Provides a common interface for {@link CStatement} (via {@link CStatementWrapper}) and statements
+ * that are exported in actual C programs like a {@link CIfStatement}.
+ *
+ * <p>This an extra interface is added because using {@link CStatement} as the common base would
+ * require adjustments to {@link CFA} handling (where a {@link CStatement} is linked to a single
+ * edge whereas a {@link CIfStatement} represents multiple edges) and all visitors that handle
+ * {@link CStatement}.
  */
 public sealed interface CAstStatement
     permits CGotoStatement, CIfStatement, CLabelStatement, CStatementWrapper {
