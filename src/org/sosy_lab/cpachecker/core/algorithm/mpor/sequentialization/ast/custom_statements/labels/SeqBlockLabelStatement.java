108d2990ffd5b3210bdc203deb29c4a7803d7507
noahkoenig <email@noahkoenig.de>
2026-01-28 16:59:30 +0100

    remove SeqLabelStatement interface

--- src/org/sosy_lab/cpachecker/core/algorithm/mpor/sequentialization/ast/custom_statements/labels/SeqBlockLabelStatement.java
+++ src/org/sosy_lab/cpachecker/core/algorithm/mpor/sequentialization/ast/custom_statements/labels/SeqBlockLabelStatement.java
@@ -9,29 +9,28 @@
 package org.sosy_lab.cpachecker.core.algorithm.mpor.sequentialization.ast.custom_statements.labels;
 
 import org.sosy_lab.cpachecker.cfa.ast.c.CLabelStatement;
+import org.sosy_lab.cpachecker.core.algorithm.mpor.sequentialization.ast.custom_statements.SeqStatement;
 import org.sosy_lab.cpachecker.core.algorithm.mpor.sequentialization.ast.custom_statements.thread_statements.CSeqThreadStatement;
 import org.sosy_lab.cpachecker.core.algorithm.mpor.sequentialization.strings.hard_coded.SeqSyntax;
 
-/** The label of a block of {@link CSeqThreadStatement}s. */
+/**
+ * The label before a block of {@link CSeqThreadStatement}s, e.g. {@code T0_0:}. This cannot be
+ * replaced with a {@link CLabelStatement} directly because the name of the label includes a number
+ * which is adjusted during the sequentialization process.
+ */
 // we store the thread prefix so that cloning does not require the options (shortVariables)
-public record SeqBlockLabelStatement(String threadPrefix, int number) implements SeqLabelStatement {
-
-  @Override
-  public String toASTString() {
-    return toASTStringWithoutColon() + SeqSyntax.COLON;
-  }
-
-  @Override
-  public String toASTStringWithoutColon() {
-    return threadPrefix + SeqSyntax.UNDERSCORE + number;
-  }
+public record SeqBlockLabelStatement(String threadPrefix, int number) implements SeqStatement {
 
   public SeqBlockLabelStatement withLabelNumber(int pLabelNumber) {
     return new SeqBlockLabelStatement(threadPrefix, pLabelNumber);
   }
 
-  @Override
   public CLabelStatement toCLabelStatement() {
-    return new CLabelStatement(toASTStringWithoutColon());
+    return new CLabelStatement(threadPrefix + SeqSyntax.UNDERSCORE + number);
+  }
+
+  @Override
+  public String toASTString() {
+    return threadPrefix + SeqSyntax.UNDERSCORE + number + SeqSyntax.COLON;
   }
 }
