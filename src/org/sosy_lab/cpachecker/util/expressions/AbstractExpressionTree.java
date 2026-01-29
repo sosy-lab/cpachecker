0e4b4d698d6a78a6f0d0d5d758cd798ca9a21b5f
noahkoenig <email@noahkoenig.de>
2026-01-29 21:33:56 +0100

    revert changes to AbstractExpressionTree

--- src/org/sosy_lab/cpachecker/util/expressions/AbstractExpressionTree.java
+++ src/org/sosy_lab/cpachecker/util/expressions/AbstractExpressionTree.java
@@ -10,9 +10,6 @@ package org.sosy_lab.cpachecker.util.expressions;
 
 import org.sosy_lab.cpachecker.cfa.ast.c.CExpression;
 import org.sosy_lab.cpachecker.core.counterexample.CExpressionToOriginalCodeVisitor;
-import org.sosy_lab.cpachecker.exceptions.UnrecognizedCodeException;
-import org.sosy_lab.cpachecker.util.cwriter.export.CExportExpression;
-import org.sosy_lab.cpachecker.util.cwriter.export.CExpressionWrapper;
 
 abstract class AbstractExpressionTree<LeafType> implements ExpressionTree<LeafType> {
 
@@ -24,21 +21,6 @@ abstract class AbstractExpressionTree<LeafType> implements ExpressionTree<LeafTy
   private String formatLeafExpression(LeafType pLeafExpression) {
     if (pLeafExpression instanceof CExpression cExpression) {
       return cExpression.accept(CExpressionToOriginalCodeVisitor.BASIC_TRANSFORMER);
-    }
-    if (pLeafExpression instanceof CExportExpression cExportExpression) {
-      // for wrapped CExpression, handle them like CExpression
-      if (pLeafExpression instanceof CExpressionWrapper(CExpression cExpression)) {
-        return cExpression.accept(CExpressionToOriginalCodeVisitor.BASIC_TRANSFORMER);
-      } else {
-        // for all other CExportExpression, just call toASTString
-        try {
-          return cExportExpression.toASTString();
-        } catch (UnrecognizedCodeException e) {
-          // TODO this is not ideal, likely we can get rid of UnrecognizedCodeException later in
-          //  toASTString
-          throw new AssertionError(e);
-        }
-      }
     }
     if (pLeafExpression == null) {
       return "null";
