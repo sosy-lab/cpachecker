dbe88935d8cd10b5e9b9506bc7f19c849caf8547
BaierD <daniel.baier@sosy.ifi.lmu.de>
2026-01-29 16:25:27 +0100

    Add test programs to SMG2 tests for testing nondet functions for integer and float types for 64 bits

--- src/org/sosy_lab/cpachecker/cpa/smg2/SMGCPA64Test.java
+++ src/org/sosy_lab/cpachecker/cpa/smg2/SMGCPA64Test.java
@@ -8,6 +8,7 @@
 
 package org.sosy_lab.cpachecker.cpa.smg2;
 
+import org.junit.Ignore;
 import org.junit.Test;
 import org.sosy_lab.cpachecker.cfa.types.MachineModel;
 
@@ -25,6 +26,23 @@ public class SMGCPA64Test extends SMGBaseCPATest {
   public void pointerArithmeticsAndComparisonsIntPtrCastNumericViaMallocProof() throws Exception {
     String testProgram =
         "test/programs/pointer_arithmetics/pointer_arithmetics_numeric_cast_int_malloc_64_safe.c";
+    runAndAssertSafe(testProgram);
+  }
+
+  // Tests that (integer) types are not comparable to values from larger types,
+  //  e.g. nondet_bool() != 2;
+  @Ignore // Ignore as we currently fail this in SMG2
+  @Test
+  public void nondetIntegerTypeBoundsProof() throws Exception {
+    String testProgram = "test/programs/basics/type_tests/nondet_generator_integer_types_64_true.c";
+    runAndAssertSafe(testProgram);
+  }
+
+  // Tests that (float) types are not comparable to values from larger types
+  @Ignore // Ignore as we currently fail this in SMG2
+  @Test
+  public void nondetFloatingPointTypeBoundsProof() throws Exception {
+    String testProgram = "test/programs/basics/type_tests/nondet_generator_float_types_64_true.c";
     runAndAssertSafe(testProgram);
   }
 
