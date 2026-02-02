0c4fe3f90a4b59d25959c51c759dcccaaabd10f9
Michael Brocke <brocke.michael@gmail.com>
2026-02-02 23:23:30 +0100

    Modify condition to check for Scalar Type instead of SimpleType

--- src/org/sosy_lab/cpachecker/cfa/parser/eclipse/c/ASTConverter.java
+++ src/org/sosy_lab/cpachecker/cfa/parser/eclipse/c/ASTConverter.java
@@ -997,7 +997,7 @@ class ASTConverter {
     if (castType.getCanonicalType() instanceof CCompositeType compositeType
         && compositeType.getKind() == ComplexTypeKind.UNION
         && operand != null
-        && operand.getExpressionType().getCanonicalType() instanceof CSimpleType) {
+        && CTypes.isScalarType(operand.getExpressionType().getCanonicalType())) {
 
       // Create a temporary union object and initialize it with a positional initializer list.
       // A single initializer initializes the first union member.
