d236d5a19f3b18947cd6d7851846ceccfe45f052
Daniel Raffler <daniel-raffler@gmx.de>
2026-02-07 08:27:09 +0100

    Make DssBlockAnalysis closable, and add a comment on why we need to close the distributed CPAs, and not just the underlying, wrapped CPAs

--- src/org/sosy_lab/cpachecker/core/algorithm/distributed_summaries/worker/DssAnalysisWorker.java
+++ src/org/sosy_lab/cpachecker/core/algorithm/distributed_summaries/worker/DssAnalysisWorker.java
@@ -35,7 +35,6 @@ import org.sosy_lab.cpachecker.core.algorithm.distributed_summaries.distributed_
 import org.sosy_lab.cpachecker.core.algorithm.distributed_summaries.distributed_cpa.composite.DistributedCompositeCPA;
 import org.sosy_lab.cpachecker.core.specification.Specification;
 import org.sosy_lab.cpachecker.exceptions.CPAException;
-import org.sosy_lab.cpachecker.util.CPAs;
 import org.sosy_lab.java_smt.api.SolverException;
 
 public class DssAnalysisWorker extends DssWorker implements AutoCloseable {
@@ -240,6 +239,6 @@ public class DssAnalysisWorker extends DssWorker implements AutoCloseable {
 
   @Override
   public void close() {
-    CPAs.closeCpaIfPossible(dssBlockAnalysis.getDcpa(), logger);
+    dssBlockAnalysis.close();
   }
 }
