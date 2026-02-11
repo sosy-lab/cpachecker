97d76d3ee9d1e61c66084bb887fdb4af55fbbb41
Matthias Kettl <matthias.kettl@sosy.ifi.lmu.de>
2026-02-11 14:56:55 +0100

    Make test program independent of ERROR or reach_error spec

--- test/programs/block_analysis/loop-two-reasons-for-infeasibility-required.c
+++ test/programs/block_analysis/loop-two-reasons-for-infeasibility-required.c
@@ -6,6 +6,8 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
+void reach_error() {}
+
 int main() {
   int i = 1;
   int p = 1;
@@ -14,5 +16,6 @@ int main() {
       if (p != 1) goto ERROR;
       return 0;
       ERROR:
+        reach_error();
         return 1;
 }
