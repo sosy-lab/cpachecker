49c17826ca396cbaa16cfd335a333649013e94b8
Noah König <noahkoenig@fedora.fritz.box>
2026-02-12 00:12:10 +0100

    remove block label from SeqExportStatement

--- src/org/sosy_lab/cpachecker/core/algorithm/mpor/sequentialization/ast/custom_statements/SeqBlockLabelStatement.java
+++ src/org/sosy_lab/cpachecker/core/algorithm/mpor/sequentialization/ast/custom_statements/SeqBlockLabelStatement.java
@@ -8,16 +8,13 @@
 
 package org.sosy_lab.cpachecker.core.algorithm.mpor.sequentialization.ast.custom_statements;
 
-import com.google.common.collect.ImmutableList;
 import org.sosy_lab.cpachecker.core.algorithm.mpor.sequentialization.strings.hard_coded.SeqSyntax;
-import org.sosy_lab.cpachecker.util.cwriter.export.statement.CExportStatement;
 import org.sosy_lab.cpachecker.util.cwriter.export.statement.CLabelStatement;
 
 /**
  * The label before a block of {@link SeqThreadStatement}s, e.g. {@code T0_0: stmt1; stmt2; ...}.
  */
-public record SeqBlockLabelStatement(String threadPrefix, int labelNumber)
-    implements SeqExportStatement {
+public record SeqBlockLabelStatement(String threadPrefix, int labelNumber) {
 
   public SeqBlockLabelStatement withLabelNumber(int pLabelNumber) {
     return new SeqBlockLabelStatement(threadPrefix, pLabelNumber);
@@ -25,10 +22,5 @@ public record SeqBlockLabelStatement(String threadPrefix, int labelNumber)
 
   public CLabelStatement toCLabelStatement() {
     return new CLabelStatement(threadPrefix + SeqSyntax.UNDERSCORE + labelNumber);
-  }
-
-  @Override
-  public ImmutableList<CExportStatement> toCExportStatements() {
-    return ImmutableList.of(new CLabelStatement(threadPrefix + SeqSyntax.UNDERSCORE + labelNumber));
   }
 }
--- src/org/sosy_lab/cpachecker/core/algorithm/mpor/sequentialization/ast/custom_statements/SeqExportStatement.java
+++ src/org/sosy_lab/cpachecker/core/algorithm/mpor/sequentialization/ast/custom_statements/SeqExportStatement.java
@@ -16,7 +16,7 @@ import org.sosy_lab.cpachecker.exceptions.UnrecognizedCodeException;
 import org.sosy_lab.cpachecker.util.cwriter.export.statement.CExportStatement;
 
 public sealed interface SeqExportStatement extends SeqASTNode
-    permits SeqBlockLabelStatement, SeqThreadStatementBlock, SeqThreadStatementClause {
+    permits SeqThreadStatementBlock, SeqThreadStatementClause {
 
   ImmutableList<CExportStatement> toCExportStatements();
 
