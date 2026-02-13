77ef955b6603518bddb1fc753e437d7c888ead73
Noah König <noahkoenig@fedora.fritz.box>
2026-02-13 15:14:14 +0100

    add CIntegerLiteralBase enum

--- src/org/sosy_lab/cpachecker/cfa/ast/c/CIntegerLiteralExpression.java
+++ src/org/sosy_lab/cpachecker/cfa/ast/c/CIntegerLiteralExpression.java
@@ -19,16 +19,47 @@ import org.sosy_lab.cpachecker.cfa.types.c.CType;
 public final class CIntegerLiteralExpression extends AIntegerLiteralExpression
     implements CLiteralExpression {
 
+  /**
+   * An enum for the base of an integer literal, e.g. binary for {@code 0b01010101}.
+   *
+   * <p>Reference: <a
+   * href="https://en.cppreference.com/w/c/language/integer_constant.html">https://en.cppreference.com/w/c/language/integer_constant.html</a>
+   */
+  public enum CIntegerLiteralBase {
+    BINARY("0b", 2),
+    OCTAL("0", 8),
+    DECIMAL("", 10),
+    HEXADECIMAL("0x", 16);
+
+    public final String prefix;
+
+    public final int radix;
+
+    CIntegerLiteralBase(String pPrefix, int pRadix) {
+      prefix = pPrefix;
+      radix = pRadix;
+    }
+  }
+
   @Serial private static final long serialVersionUID = 7691279268370356228L;
   public static final CIntegerLiteralExpression ONE = createDummyLiteral(1L, CNumericTypes.INT);
   public static final CIntegerLiteralExpression ZERO = createDummyLiteral(0L, CNumericTypes.INT);
+  private final CIntegerLiteralBase base;
 
   public static CIntegerLiteralExpression createDummyLiteral(long value, CType type) {
     return new CIntegerLiteralExpression(FileLocation.DUMMY, type, BigInteger.valueOf(value));
   }
 
+  public CIntegerLiteralExpression(
+      FileLocation pFileLocation, CType pType, BigInteger pValue, CIntegerLiteralBase pBase) {
+
+    super(pFileLocation, pType, pValue);
+    base = pBase;
+  }
+
   public CIntegerLiteralExpression(FileLocation pFileLocation, CType pType, BigInteger pValue) {
     super(pFileLocation, pType, pValue);
+    base = CIntegerLiteralBase.DECIMAL;
   }
 
   @Override
@@ -53,7 +84,22 @@ public final class CIntegerLiteralExpression extends AIntegerLiteralExpression
 
   @Override
   public String toASTString() {
-    StringBuilder result = new StringBuilder(getValue().toString());
+    StringBuilder result = new StringBuilder();
+
+    if (base.prefix.isEmpty()) {
+      // if there is no prefix, add the value including its '-' sign if the number is negative
+      result.append(getValue().toString(base.radix));
+
+    } else {
+      // if there is a prefix, add the '-' sign before the prefix, if the number is negative
+      if (getValue().signum() < 0) {
+        // append '-' before the base prefix
+        result.append("-");
+      }
+      result.append(base.prefix);
+      // use the absolute, so that no additional '-' is used after the prefix
+      result.append(getValue().abs().toString(base.radix));
+    }
 
     CType cType = getExpressionType();
     if (cType instanceof CSimpleType type) {
