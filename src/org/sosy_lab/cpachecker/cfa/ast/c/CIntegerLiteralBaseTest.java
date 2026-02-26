8271c2406d0dfbe86e4ac39242a6f72718ab7b53
Noah König <noahkoenig@fedora-2.fritz.box>
2026-02-26 10:39:25 -0500

    add CIntegerLiteralBase tests for negative values

--- src/org/sosy_lab/cpachecker/cfa/ast/c/CIntegerLiteralBaseTest.java
+++ src/org/sosy_lab/cpachecker/cfa/ast/c/CIntegerLiteralBaseTest.java
@@ -18,46 +18,109 @@ import org.sosy_lab.cpachecker.cfa.types.c.CNumericTypes;
 
 public class CIntegerLiteralBaseTest {
 
-  private static final BigInteger testValue =
-      BigInteger.ZERO.setBit(0).setBit(1).setBit(6).setBit(7).setBit(13).setBit(14);
+  /** Set bits from left to right: {@code 0110000011000011}. */
+  private static final BigInteger positiveTestValue =
+      BigInteger.ZERO.setBit(14).setBit(13).setBit(7).setBit(6).setBit(1).setBit(0);
+
+  /** Set bits from left to right: {@code 10101010}. */
+  private static final BigInteger negativeTestValue =
+      BigInteger.ZERO.setBit(7).setBit(5).setBit(3).setBit(1).negate();
 
   @Test
-  public void test_binary() {
+  public void test_positive_binary() {
     CIntegerLiteralExpression withBinaryBase =
         new CIntegerLiteralExpression(
-            FileLocation.DUMMY, CNumericTypes.SHORT_INT, testValue, CIntegerLiteralBase.BINARY);
+            FileLocation.DUMMY,
+            CNumericTypes.SHORT_INT,
+            positiveTestValue,
+            CIntegerLiteralBase.BINARY);
     // leading zeros are dropped, so e.g. "0b1" instead of "0b0000_0001"
     String expected = "0b110000011000011";
     assertThat(withBinaryBase.toASTString()).isEqualTo(expected);
   }
 
   @Test
-  public void test_octal() {
+  public void test_negative_binary() {
+    CIntegerLiteralExpression withBinaryBase =
+        new CIntegerLiteralExpression(
+            FileLocation.DUMMY,
+            CNumericTypes.SHORT_INT,
+            negativeTestValue,
+            CIntegerLiteralBase.BINARY);
+    // leading zeros are dropped, so e.g. "0b1" instead of "0b0000_0001"
+    String expected = "-0b10101010";
+    assertThat(withBinaryBase.toASTString()).isEqualTo(expected);
+  }
+
+  @Test
+  public void test_positive_octal() {
     CIntegerLiteralExpression withOctalBase =
         new CIntegerLiteralExpression(
-            FileLocation.DUMMY, CNumericTypes.SHORT_INT, testValue, CIntegerLiteralBase.OCTAL);
+            FileLocation.DUMMY,
+            CNumericTypes.SHORT_INT,
+            positiveTestValue,
+            CIntegerLiteralBase.OCTAL);
     String expected = "060303";
     assertThat(withOctalBase.toASTString()).isEqualTo(expected);
   }
 
   @Test
-  public void test_decimal() {
+  public void test_negative_octal() {
+    CIntegerLiteralExpression withBinaryBase =
+        new CIntegerLiteralExpression(
+            FileLocation.DUMMY,
+            CNumericTypes.SHORT_INT,
+            negativeTestValue,
+            CIntegerLiteralBase.OCTAL);
+    String expected = "-0252";
+    assertThat(withBinaryBase.toASTString()).isEqualTo(expected);
+  }
+
+  @Test
+  public void test_positive_decimal() {
     CIntegerLiteralExpression withDecimalBase =
         new CIntegerLiteralExpression(
-            FileLocation.DUMMY, CNumericTypes.INT, testValue, CIntegerLiteralBase.DECIMAL);
+            FileLocation.DUMMY,
+            CNumericTypes.SHORT_INT,
+            positiveTestValue,
+            CIntegerLiteralBase.DECIMAL);
     String expected = String.valueOf(1 + 2 + 64 + 128 + 8192 + 16384);
     assertThat(withDecimalBase.toASTString()).isEqualTo(expected);
   }
 
   @Test
-  public void test_hexadecimal() {
+  public void test_negative_decimal() {
+    CIntegerLiteralExpression withDecimalBase =
+        new CIntegerLiteralExpression(
+            FileLocation.DUMMY,
+            CNumericTypes.SHORT_INT,
+            negativeTestValue,
+            CIntegerLiteralBase.DECIMAL);
+    String expected = "-170";
+    assertThat(withDecimalBase.toASTString()).isEqualTo(expected);
+  }
+
+  @Test
+  public void test_positive_hexadecimal() {
     CIntegerLiteralExpression withHexadecimalBase =
         new CIntegerLiteralExpression(
             FileLocation.DUMMY,
             CNumericTypes.SHORT_INT,
-            testValue,
+            positiveTestValue,
             CIntegerLiteralBase.HEXADECIMAL);
     String expected = "0x60c3";
+    assertThat(withHexadecimalBase.toASTString()).isEqualTo(expected);
+  }
+
+  @Test
+  public void test_negative_hexadecimal() {
+    CIntegerLiteralExpression withHexadecimalBase =
+        new CIntegerLiteralExpression(
+            FileLocation.DUMMY,
+            CNumericTypes.SHORT_INT,
+            negativeTestValue,
+            CIntegerLiteralBase.HEXADECIMAL);
+    String expected = "-0xaa";
     assertThat(withHexadecimalBase.toASTString()).isEqualTo(expected);
   }
 }
