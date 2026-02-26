6d6096114534110782aca377bb16ba2255584f78
Marian Lingsch-Rosenfeld <Marian.Lingsch@sosy.ifi.lmu.de>
2026-02-26 17:20:03 +0100

    Add examples using pointers to check how the pointer aliasing works
    Response to: https://gitlab.com/sosy-lab/software/cpachecker/-/merge_requests/497#note_3116389256

---
+++ test/programs/simple/recursion/countup_local_pointer.c
@@ -0,0 +1,29 @@
+// This file is part of CPAchecker,
+// a tool for configurable software verification:
+// https://cpachecker.sosy-lab.org
+//
+// SPDX-FileCopyrightText: 2007-2020 Dirk Beyer <https://www.sosy-lab.org>
+//
+// SPDX-License-Identifier: Apache-2.0
+
+extern void reach_error();
+
+int g = 0;
+int max = 3;
+
+void count(int *c) {
+  if (*c < max) {
+    *c++;
+    int* a = c;
+    count();
+    a += 1;
+  }
+}
+
+
+int main() {
+  count(&g);
+  if (g != 2*max) {
+    reach_error();
+  }
+}
