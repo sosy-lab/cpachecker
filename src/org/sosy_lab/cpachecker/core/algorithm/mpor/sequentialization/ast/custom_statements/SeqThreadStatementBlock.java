16bf79d10f72572ca2d8a8d6c06039a85393be1b
Noah König <noahkoenig@fedora-2.fritz.box>
2026-02-27 12:54:31 -0500

    add sanity check when cloning block with new statements

--- src/org/sosy_lab/cpachecker/core/algorithm/mpor/sequentialization/ast/custom_statements/SeqThreadStatementBlock.java
+++ src/org/sosy_lab/cpachecker/core/algorithm/mpor/sequentialization/ast/custom_statements/SeqThreadStatementBlock.java
@@ -112,6 +112,9 @@ public final class SeqThreadStatementBlock implements SeqExportStatement {
   }
 
   public SeqThreadStatementBlock withStatements(ImmutableList<SeqThreadStatement> pStatements) {
+    checkArgument(
+        statements.size() == pStatements.size(),
+        "pStatements.size() must be equal to the existing statements.size()");
     return new SeqThreadStatementBlock(threadId, labelNumber, pStatements, nextThreadLabel);
   }
 
