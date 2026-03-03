2f40d6d75cb335d6269280bf9ed9ab80c32abc03
Rabea Lühmann <r.luehmann@campus.lmu.de>
2026-03-03 10:45:29 +0100

    remove cfa node field from acsl comment
    An acsl comment has a file location. An acsl annotation belongs to a cfa
    node.

--- src/org/sosy_lab/cpachecker/cfa/ast/acsl/parser/AcslComment.java
+++ src/org/sosy_lab/cpachecker/cfa/ast/acsl/parser/AcslComment.java
@@ -8,33 +8,9 @@
 
 package org.sosy_lab.cpachecker.cfa.ast.acsl.parser;
 
-import com.google.common.base.Verify;
-import java.util.Optional;
-import org.checkerframework.checker.nullness.qual.NonNull;
 import org.sosy_lab.cpachecker.cfa.ast.FileLocation;
-import org.sosy_lab.cpachecker.cfa.model.CFANode;
 
-public class AcslComment {
-
-  private final FileLocation fileLocation;
-  private final String commentString;
-  private @NonNull Optional<CFANode> cfaNode;
-
-  public AcslComment(FileLocation pFileLocation, String pCommentString) {
-    fileLocation = pFileLocation;
-    commentString = pCommentString;
-    cfaNode = Optional.empty();
-  }
-
-  public void updateCfaNode(@NonNull CFANode pCfaNode) {
-    // Ensure the Cfa Node for an Acsl Comment can only be set once
-    Verify.verify(cfaNode.isEmpty());
-    cfaNode = Optional.of(pCfaNode);
-  }
-
-  public FileLocation getFileLocation() {
-    return fileLocation;
-  }
+public record AcslComment(FileLocation fileLocation, String commentString) {
 
   public String getComment() {
     return commentString;
@@ -43,14 +19,5 @@ public class AcslComment {
   @Override
   public String toString() {
     return "'" + commentString + "'" + " at " + fileLocation;
-  }
-
-  @NonNull
-  public Optional<CFANode> getCfaNode() {
-    return cfaNode;
-  }
-
-  public Boolean hasCfaNode() {
-    return cfaNode.isPresent();
   }
 }
