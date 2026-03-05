a477008af1f5325cab8d0c07e75edbfb79453765
Johannes Schlüßlhuber <schluesslhuber@cip.ifi.lmu.de>
2026-03-05 16:42:54 +0100

    Update tail recursion now uses immutable lists with builders

--- src/org/sosy_lab/cpachecker/cfa/transformation/TailRecursionEliminationProgramTransformation.java
+++ src/org/sosy_lab/cpachecker/cfa/transformation/TailRecursionEliminationProgramTransformation.java
@@ -11,10 +11,11 @@ package org.sosy_lab.cpachecker.cfa.transformation;
 import com.google.common.collect.FluentIterable;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Iterables;
 import com.google.common.graph.Traverser;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Optional;
+import org.checkerframework.checker.nullness.qual.NonNull;
 import org.sosy_lab.cpachecker.cfa.CFA;
 import org.sosy_lab.cpachecker.cfa.ast.AParameterDeclaration;
 import org.sosy_lab.cpachecker.cfa.ast.c.CExpression;
@@ -31,6 +32,7 @@ public class TailRecursionEliminationProgramTransformation extends ProgramTransf
   public Optional<SubCFA> transform(CFA pCFA, CFANode pNode) {
 
     // TODO check transformation conditions
+    // check 1: are we at the start of a function with a return node
     if (!(pNode instanceof FunctionEntryNode)) {
       return Optional.empty();
     }
@@ -39,49 +41,93 @@ public class TailRecursionEliminationProgramTransformation extends ProgramTransf
     }
     CFANode exitNode = ((FunctionEntryNode) pNode).getExitNode().get();
     String functionName = pNode.getFunctionName();
-
+    // check 2: is the last operation a recursive function call
     FluentIterable<CFAEdge> enteringEdges = exitNode.getEnteringEdges();
     for(CFAEdge edge : enteringEdges) {
       CReturnStatement returnStatement = ((CReturnStatementEdge) edge).getReturnStatement();
-      // TODO return expressions checken
+      // TODO check return expressions
       if (returnStatement.getReturnValue().isEmpty()) {
         return  Optional.empty();
       }
       CExpression returnExpression = returnStatement.getReturnValue().get();
     }
+    // check 3: the first statement must be a conditional check of the exit condition
 
     // TODO perform transformation
-    ArrayList<CFANode> nodes = new ArrayList<>();
-    ArrayList<CFAEdge> edges = new ArrayList<>();
+    CFANode newEntryNode = null;
+    CFANode newExitNode = null;
+    ImmutableList.Builder<CFANode> nodes = ImmutableList.builder();
+    ImmutableList.Builder<CFAEdge> edges = ImmutableList.builder();
     HashMap<Integer, Integer> nodeMap = new HashMap<>();
     Traverser<CFANode> cfaNetworkTraverser = Traverser.forGraph(pCFA.asGraph());
     Iterable<CFANode> cfaNodeIterable = cfaNetworkTraverser.breadthFirst(pNode);
     ImmutableList<? extends AParameterDeclaration> parameters = pNode.getFunction().getParameters();
-    int parameterNum = parameters.size();
+    cfaNodeIterable = Iterables.filter(cfaNodeIterable, (CFANode node) -> {
+      assert node != null;
+      return node.getFunctionName().equals(functionName);
+    });
 
+    // add new nodes
     for(CFANode currentNode : cfaNodeIterable) {
-      if (currentNode.getFunctionName().equals(functionName)) {
-        if (currentNode.getNodeNumber() == exitNode.getNodeNumber()) {
-          nodes.add(CFANode.newDummyCFANode(functionName));
-          nodeMap.put(currentNode.getNodeNumber(), nodes.getLast().getNodeNumber());
-          break;
-        } else if (currentNode.getNodeNumber() == pNode.getNodeNumber()) {
-          nodes.add(CFANode.newDummyCFANode(functionName));
-          nodes.getLast().setLoopStart();
-          nodeMap.put(currentNode.getNodeNumber(), nodes.getLast().getNodeNumber());
+      CFANode newNode = CFANode.newDummyCFANode(functionName);
+      nodeMap.put(currentNode.getNodeNumber(), newNode.getNodeNumber());
+      if (currentNode.getNodeNumber() == exitNode.getNodeNumber()) {
+        newExitNode = newNode;
+      } else if (currentNode.getNodeNumber() == pNode.getNodeNumber()) {
+        newNode.setLoopStart();
+        newEntryNode = newNode;
+      }
+      nodes.add(newNode);
+    }
+    for (int i = 0; i < parameters.size(); i++) {
+      CFANode newNode = CFANode.newDummyCFANode(functionName);
+      nodes.add(newNode);
+    }
+    ImmutableList<CFANode> nodesList = nodes.build();
+
+    // add new edges
+    for(CFANode currentNode : cfaNodeIterable) {  // TODO can Iterables be reused??
+      for(CFAEdge currentEdge : currentNode.getAllLeavingEdges()){
+        if (currentEdge.getSuccessor().getFunctionName().equals(functionName)) {
+          // TODO check if edge is the recursive function call
+          // TODO if true skip edge copying and add new edges for parameter nodes
+          int newPredecessorNodeIndex = getNodeIndex(nodeMap.get(currentNode.getNodeNumber()), nodesList).orElseThrow();
+          Optional<Integer> newSuccessorNodeIndex = getNodeIndex(nodeMap.get(currentEdge.getSuccessor().getNodeNumber()), nodesList);
+          if (newSuccessorNodeIndex.isPresent()) {
+            edges.add(
+                ProgramTransformationCFAEdgeCreator.copyCFAEdge(
+                    currentEdge, nodesList.get(newPredecessorNodeIndex), nodesList.get(newSuccessorNodeIndex.get())));
+          } else {
+            // TODO how to handle jumps out of the function?
+            // TODO exclude normal function exit
+            edges.add(
+                ProgramTransformationCFAEdgeCreator.copyCFAEdge(
+                    currentEdge, nodesList.get(newPredecessorNodeIndex), currentEdge.getSuccessor()));
+          }
         }
       }
     }
+    ImmutableList<CFAEdge> edgesList = edges.build();
+
 
     return Optional.of(new SubCFA(
         pNode,
         exitNode,
-        nodes.getFirst(),
-        nodes.getLast(),
+        newEntryNode,
+        newExitNode,
         ProgramTransformationEnum.TAIL_RECURSION_ELIMINATION,
         ProgramTransformationBehaviour.PRECISE,
-        ImmutableSet.copyOf(nodes),
-        ImmutableSet.copyOf(edges)
+        ImmutableSet.copyOf(nodesList),
+        ImmutableSet.copyOf(edgesList)
     ));
+  }
+
+  private static Optional<Integer> getNodeIndex(int nodeNumber, @NonNull ImmutableList<CFANode> nodeList) {
+    for (int i = 0; i < nodeList.size(); i++) {
+      if (nodeList.get(i).getNodeNumber() == nodeNumber) {
+        return Optional.of(i);
+      }
+    }
+    return Optional.empty();
   }
 }
