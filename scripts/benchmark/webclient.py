6f8ddea4acf5044912b187fb9151646ca29fe82a
erya_wang <eryawang620@gmail.com>
2026-03-22 23:01:38 +0100

    Revert "normalize requirements"
    This reverts commit 3754ca05e738cef49c674d8083fdcb43abc73624.

--- scripts/benchmark/webclient.py
+++ scripts/benchmark/webclient.py
@@ -666,16 +666,6 @@ class WebInterface:
         counter=0,
     ):
         params = []
-        # normalize requirements
-        if requirements is None:
-            requirements = {}
-        if not isinstance(requirements, dict):
-            requirements = {}
-
-        # remove None values
-        requirements = {
-            k: v for k, v in requirements.items() if v is not None
-        }
         opened_files = []  # open file handles are passed to the request library
 
         for programPath in run.sourcefiles:
