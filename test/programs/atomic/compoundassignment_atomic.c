73085d13d40a86281bb8591ce1e9f5acad3ad5f5
Csanád Telbisz <csanadtelbisz@gmail.com>
2026-03-26 17:48:05 +0100

    atomic operations test

---
+++ test/programs/atomic/compoundassignment_atomic.c
@@ -0,0 +1,26 @@
+#include <pthread.h>
+
+extern void reach_error(void);
+extern void abort(void);
+
+_Atomic int counter = 0;
+
+void *thread(void *arg) {
+  counter += 1;
+  return NULL;
+}
+
+int main() {
+  pthread_t t1, t2;
+
+  pthread_create(&t1, NULL, thread, NULL);
+  pthread_create(&t2, NULL, thread, NULL);
+  pthread_join(t1, NULL);
+  pthread_join(t2, NULL);
+  if (counter != 2) {
+    // this point is only reachable if the increment is not interpreted as atomic
+    ERROR: {reach_error();abort();}
+  }
+
+  return 0;
+}
---
+++ test/programs/atomic/postincrement_nonatomic.c
@@ -0,0 +1,33 @@
+#include <pthread.h>
+
+extern void reach_error(void);
+extern void abort(void);
+
+int counter = 0;
+int x = 0;
+int y = 0;
+
+void *thread1(void *arg) {
+  x = counter++;
+  return NULL;
+}
+
+void *thread2(void *arg) {
+  y = counter++;
+  return NULL;
+}
+
+int main() {
+  pthread_t t1, t2;
+
+  pthread_create(&t1, NULL, thread1, NULL);
+  pthread_create(&t2, NULL, thread2, NULL);
+  pthread_join(t1, NULL);
+  pthread_join(t2, NULL);
+  if (x == y) {
+    // this point is only reachable if the increment is not interpreted as atomic
+    ERROR: {reach_error();abort();}
+  }
+
+  return 0;
+}
