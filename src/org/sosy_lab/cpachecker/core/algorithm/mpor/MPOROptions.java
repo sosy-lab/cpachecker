08e9b830b1395834abb9eb5219a0b1462fd808c9
Noah König <noahkoenig@fedora-2.fritz.box>
2026-03-27 10:29:43 +0100

    update MPOROptions descriptions

--- src/org/sosy_lab/cpachecker/core/algorithm/mpor/MPOROptions.java
+++ src/org/sosy_lab/cpachecker/core/algorithm/mpor/MPOROptions.java
@@ -25,202 +25,221 @@ public class MPOROptions {
 
   // using Optional for @Option is not allowed, so we use 'NONE' for enums that can be disabled.
 
-  @Option(secure = true, description = "allow input programs that write pointer variables?")
+  @Option(secure = true, description = "Allow input programs that write pointer variables?")
   private boolean allowPointerWrites = true;
 
   @Option(
       secure = true,
       description =
-          "merge statements between __VERIFIER_atomic_begin() and __VERIFIER_atomic_end() via goto?"
-              + " setting this to false does not model the input programs behavior correctly.")
+          "Merge statements between __VERIFIER_atomic_begin() and __VERIFIER_atomic_end() with goto"
+              + " statements? Setting this to false does not model the input programs behavior"
+              + " correctly.")
   private boolean atomicBlockMerge = true;
 
-  @Option(secure = true, description = "the encoding of the partial order reduction bit vectors.")
+  @Option(
+      secure = true,
+      description =
+          "The encoding of bit vectors that are used to instrument the output program with partial"
+              + " order reduction.")
   private SeqBitVectorEncoding bitVectorEncoding = SeqBitVectorEncoding.NONE;
 
   @Option(
       secure = false,
       description =
-          "the style preset used by clang-format to format the output program. use NONE to disable"
+          "The style preset used by clang-format to format the output program. Use NONE to disable"
               + " formatting.")
   private ClangFormatStyle clangFormatStyle = ClangFormatStyle.WEBKIT;
 
   @Option(
       secure = true,
-      description = "include comments with trivia explaining the sequentialization?")
+      description =
+          "Include comments in the output program that explain the sequentialization and its"
+              + " components?")
   private boolean comments = false;
 
   @Option(
       secure = true,
       description =
-          "make labels for thread statements consecutive? i.e. 0 to n - 1 where n is the number of"
-              + " statements. disabling may result in the first statement being unreachable, but"
-              + " can be useful for debugging.")
+          "Make labels for thread statements consecutive, i.e., in [1; N] where N is the number of"
+              + " statements? Disabling this option may result in the first statement being"
+              + " unreachable, but can be useful for debugging.")
   private boolean consecutiveLabels = true;
 
   @Option(
       secure = true,
       description =
-          "defines the syntax in which the next statement of a thread simulation is chosen.")
+          "Defines the program syntax in which the next statement(s) of a thread simulation is"
+              + " selected.")
   private MultiSelectionStatementEncoding controlEncodingStatement =
       MultiSelectionStatementEncoding.SWITCH_CASE;
 
   @Option(
       secure = true,
-      description = "defines the syntax in which the next thread executing a statement is chosen.")
+      description =
+          "Defines the program syntax in which the next thread executing a statement (or multiple"
+              + " statements) is selected.")
   private MultiSelectionStatementEncoding controlEncodingThread =
       MultiSelectionStatementEncoding.NONE;
 
   @Option(
       secure = true,
       description =
-          "include original function declarations from input file? including them may result in"
-              + " unsound analysis (e.g. false alarms for CBMC + ignored function calls through"
-              + " function pointers for CPAchecker)")
+          "Include original function declarations from the input program? Including them may result"
+              + " in unsound analysis (e.g. false alarms for CBMC, or ignored function calls"
+              + " through function pointers for CPAchecker).")
   private boolean inputFunctionDeclarations = false;
 
   @Option(
       secure = true,
       description =
-          "include original type declarations from input file? disabling may result in parse errors"
-              + " for the output program.")
+          "Include original type declarations from the input file? Disabling may result in parse"
+              + " errors for the output program.")
   private boolean inputTypeDeclarations = true;
 
   @Option(
       secure = true,
-      description = "link commuting statements via goto to reduce the state space?")
+      description = "Link commuting statements with goto statements to reduce the state space?")
   private boolean linkReduction = true;
 
   @Option(
       secure = true,
       description =
-          "the number of loop iterations to perform thread simulations. use 0 for an infinite loop"
-              + " (while (1)). any number other than 0 is unsound, because the entire state space"
+          "The number of loop iterations to perform thread simulations. Use 0 for an infinite loop"
+              + " (while (1)). Any number other than 0 is unsound, because the entire state space"
               + " is not searched.")
   private int loopIterations = 0;
 
   @Option(
       secure = true,
       description =
-          "unroll the loop used for thread simulation? not unrolling loops can be unsound, e.g. for"
-              + " CBMC. only use with loopIterations > 0.")
+          "Unroll the loop used for the thread simulation? Disabling this option can be unsound for"
+              + " CBMC. Enabling this option is only possible with loopIterations > 0.")
   private boolean loopUnrolling = false;
 
   @Option(
       secure = true,
       description =
-          "removes backward goto, i.e. jumping to a line higher up in the program, by reordering"
-              + " statements. only works for if-else constructs, not loops. use noBackwardLoopGoto"
-              + " to ensure that backward loop goto are removed.")
+          "Removes backward goto, i.e., jumping to a line higher up in the program, by reordering"
+              + " statements. Only works for if-else constructs, not loops. Use noBackwardLoopGoto"
+              + " to ensure that backward goto from loops are removed too.")
   private boolean noBackwardGoto = true;
 
   @Option(
       secure = true,
       description =
-          "removes backward goto from loops. this option is independent from noBackwardGoto.")
+          "Removes backward goto from loops. This option is independent from noBackwardGoto.")
   private boolean noBackwardLoopGoto = true;
 
   @Option(
       secure = true,
       description =
-          "use signed __VERIFIER_nondet_int() instead of unsigned __VERIFIER_nondet_uint()?")
+          "Use signed __VERIFIER_nondet_int() instead of unsigned __VERIFIER_nondet_uint()?")
   private boolean nondeterminismSigned = false;
 
-  @Option(secure = true, description = "the source(s) of nondeterminism in the sequentialization.")
+  @Option(secure = true, description = "The source(s) of nondeterminism in the sequentialization.")
   private NondeterminismSource nondeterminismSource = NondeterminismSource.NUM_STATEMENTS;
 
   @Option(
       secure = true,
       description =
-          "prune and simplify bit vector evaluation expressions based on perfect knowledge? e.g."
+          "Prune and simplify bit vector evaluation expressions based on perfect knowledge? E.g.,"
               + " if it is known that the left hand side in an & expression is 0, then the entire"
               + " evaluation can be pruned.")
   private boolean pruneBitVectorEvaluations = false;
 
   @Option(
       secure = true,
-      description = "prune empty statements (with only pc writes) from the sequentialization?")
+      description =
+          "Prune empty statements with no semantics from the input program from the"
+              + " sequentialization?")
   private boolean pruneEmptyStatements = true;
 
   @Option(
       secure = true,
       description =
-          "only bit vectors for memory locations that are reachable for a thread are included,"
-              + " reducing the amount of variables, evaluations, and writes in the output program.")
+          "Prune the sparse bit vectors so that the scalar bool variables for the bit vector are"
+              + " only included if their respective memory location is reachable for their"
+              + " corresponding thread. Reduces the amount of variables, evaluations, and writes in"
+              + " the output program.")
   private boolean pruneSparseBitVectors = false;
 
   @Option(
       secure = true,
       description =
-          "bit vectors are only written to 0 if the corresponding memory location is not reachable"
-              + " anymore, removing all unnecessary writes to 1 in the output program.")
+          "Prune unnecessary sparse bit vector writes to 1 in the output program? Sparse bit"
+              + " vectors for the reachability of a memory location are initialized to 1, and only"
+              + " written to 0 if the corresponding memory location is not reachable anymore from a"
+              + " given location, so any additional write to 1 is unnecessary and can be pruned"
+              + " with this option.")
   private boolean pruneSparseBitVectorWrites = false;
 
   @Option(
       secure = true,
       description =
-          "ignore that the current thread should not execute if it is not in conflict with any"
+          "Ignore that the current thread should not execute if it is not in conflict with any"
               + " other thread?")
   private boolean reduceIgnoreSleep = false;
 
   @Option(
       secure = true,
-      description = "enforce an execution order when the current and previous thread commute?")
+      description = "Enforce an execution order if the current and previous thread commute?")
   private boolean reduceLastThreadOrder = false;
 
   @Option(
       secure = true,
       description =
           "Continue executing the current thread if it is the only active thread. This option"
-              + " utilizes a thread_count ghost variable which is incremented for each created"
-              + " thread and decremented for each terminated thread. These increments and decrement"
-              + " are placed inside a possibly infinite loop, so when analyzing for overflows, it"
-              + " may be more efficient to disable this option.")
+              + " utilizes a thread_count ghost variable which is initialized to 1 and incremented"
+              + " for each created thread and decremented for each terminated thread. These"
+              + " increments and decrement are placed inside a possibly infinite loop, so when"
+              + " analyzing for overflows, it may be more efficient to disable this option.")
   private boolean reduceSingleActiveThread = true;
 
   @Option(
       secure = true,
       description =
-          "continue executing the current thread until it is in conflict with at least another"
+          "Continue executing the current thread until it is in conflict with at least another"
               + " thread?")
   private boolean reduceUntilConflict = false;
 
   @Option(
       secure = true,
       description =
-          "how to determine if two threads commute from a location. READ_AND_WRITE reduces the"
-              + " state space more than ACCESS_ONLY, but introduces additional overhead (i.e."
+          "How to determine if two threads commute from a location. READ_AND_WRITE reduces the"
+              + " state space more than ACCESS_ONLY, but introduces additional overhead (i.e.,"
               + " number of variables, assignments, and expression evaluations)")
   private ReductionMode reductionMode = ReductionMode.NONE;
 
   @Option(
       secure = true,
       description =
-          "use separate int values (scalars) for tracking thread pcs instead of int arrays?")
+          "Use separate int values (scalars) for tracking thread program counters instead of a"
+              + " single int array?")
   private boolean scalarPc = true;
 
-  @Option(secure = true, description = "use shortened variable names, e.g. THREAD0 -> T0")
+  @Option(secure = true, description = "Use shortened variable names, e.g., THREAD0 -> T0.")
   private boolean shortVariableNames = true;
 
   @Option(
       secure = true,
       description =
-          "check if all goto statements jump forward, i.e. to a higher line of code in the"
+          "Check if all goto statements jump forward, i.e., to a higher line of code in the"
               + " program?")
   private boolean validateNoBackwardGoto = true;
 
   @Option(
       secure = true,
       description =
-          "check if CPAchecker can parse sequentialization? note that it may take several seconds"
-              + " to parse a program")
+          "Check if CPAchecker can parse output program? Note that it may take several seconds"
+              + " to parse a program.")
   private boolean validateParse = true;
 
   @Option(
       secure = true,
       description =
-          "check if all label pc (except initial) are target pc and all target pc (except "
-              + " termination) are label pc within a thread simulation?")
+          "Check (1) if all label program counter (except initial program counters) are also"
+              + " targeted and (2) that all target program counter (except terminating program"
+              + " counters) are also labels within a thread simulation?")
   private boolean validatePc = true;
 
   /**
