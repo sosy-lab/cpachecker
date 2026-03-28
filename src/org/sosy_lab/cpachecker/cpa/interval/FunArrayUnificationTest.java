d9eeeb4b0593c0151c4b181e4d763cab68955a59
Maximilian Hofstetter <maximilian.hofstetter@gmx.net>
2026-03-28 08:00:52 +0100

    FunArray: Refactor test

--- src/org/sosy_lab/cpachecker/cpa/interval/FunArrayUnificationTest.java
+++ src/org/sosy_lab/cpachecker/cpa/interval/FunArrayUnificationTest.java
@@ -15,49 +15,63 @@ import org.junit.Test;
 import org.sosy_lab.cpachecker.cpa.interval.FunArrayBuilder.FunArrayBuilderException;
 
 public class FunArrayUnificationTest {
-  @Test
-  public void testExample8FromPaper() throws FunArrayBuilderException {
 
-    FunArray arrayA = FunArrayBuilder
-        .firstBound(exp(0), exp("i"))
-        .value(Interval.UNBOUND)
-        .bound(exp("n"))
-        .build();
-
-    FunArray arrayB = FunArrayBuilder
-        .firstBound(exp(0), exp("i", -1))
-        .value(0)
-        .bound(exp(1), exp("i"))
-        .value(Interval.UNBOUND)
-        .bound(exp("n"))
-        .mayBeEmpty()
-        .build();
-
-    var unification = new FunArrayUnification(arrayA, arrayB);
+  private void testUnification(
+      FunArray initialA,
+      FunArray initialB,
+      FunArray expectedResultA,
+      FunArray expectedResultB
+  ) {
+    var unification = new FunArrayUnification(initialA, initialB);
     var result = unification.unify(Interval.EMPTY, Interval.EMPTY);
 
     FunArray resultA = result.resultA();
     FunArray resultB = result.resultB();
 
-    FunArray expectedResultA = FunArrayBuilder
-        .firstBound(exp(0))
-        .value(Interval.EMPTY)
-        .bound(exp("i"))
-        .mayBeEmpty()
-        .value(Interval.UNBOUND)
-        .bound(exp("n"))
-        .build();
-
-    FunArray expectedResultB = FunArrayBuilder
-        .firstBound(exp(0))
-        .value(0)
-        .bound(exp("i"))
-        .value(Interval.UNBOUND)
-        .bound(exp("n"))
-        .mayBeEmpty()
-        .build();
-
     assertThat(resultA).isEqualTo(expectedResultA);
     assertThat(resultB).isEqualTo(expectedResultB);
+  }
+
+  /*
+   * Tests example 8 from the Cousout, Cousot and Logozzo Paper.
+   */
+  @Test
+  public void testExample8FromPaper() throws FunArrayBuilderException {
+
+    testUnification(
+        // {0 i} ⊤ {n}
+        FunArrayBuilder
+            .firstBound(exp(0), exp("i"))
+            .value(Interval.UNBOUND)
+            .bound(exp("n"))
+            .build(),
+        // {0 i-1} 0 {1 i} ⊤ {n}?
+        FunArrayBuilder
+            .firstBound(exp(0), exp("i", -1))
+            .value(0)
+            .bound(exp(1), exp("i"))
+            .value(Interval.UNBOUND)
+            .bound(exp("n"))
+            .mayBeEmpty()
+            .build(),
+        // {0} ⊥ {i}? ⊤ {n}
+        FunArrayBuilder
+            .firstBound(exp(0))
+            .value(Interval.EMPTY)
+            .bound(exp("i"))
+            .mayBeEmpty()
+            .value(Interval.UNBOUND)
+            .bound(exp("n"))
+            .build(),
+        // {0} 0 {i} ⊤ {n}?
+        FunArrayBuilder
+            .firstBound(exp(0))
+            .value(0)
+            .bound(exp("i"))
+            .value(Interval.UNBOUND)
+            .bound(exp("n"))
+            .mayBeEmpty()
+            .build()
+    );
   }
 }
