65b930c98ef7c10c35ae216884e434d0e35d8aa7
Marian Lingsch-Rosenfeld <Marian.Lingsch@sosy.ifi.lmu.de>
2026-03-30 12:08:21 +0200

    Port the visitor to obtain nodes for all labels from the branch transformation-c-to-svlib

---
+++ src/org/sosy_lab/cpachecker/cfa/parser/svlib/SvLibLabelCollectingVisitor.java
@@ -0,0 +1,130 @@
+// This file is part of CPAchecker,
+// a tool for configurable software verification:
+// https://cpachecker.sosy-lab.org
+//
+// SPDX-FileCopyrightText: 2026 Dirk Beyer <https://www.sosy-lab.org>
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package org.sosy_lab.cpachecker.cfa.parser.svlib;
+
+import com.google.common.collect.ImmutableMap;
+import java.util.Optional;
+import org.sosy_lab.cpachecker.cfa.model.CFANode;
+import org.sosy_lab.cpachecker.cfa.parser.svlib.ast.SvLibProcedureDeclaration;
+import org.sosy_lab.cpachecker.cfa.parser.svlib.ast.statements.SvLibAssignmentStatement;
+import org.sosy_lab.cpachecker.cfa.parser.svlib.ast.statements.SvLibAssumeStatement;
+import org.sosy_lab.cpachecker.cfa.parser.svlib.ast.statements.SvLibBreakStatement;
+import org.sosy_lab.cpachecker.cfa.parser.svlib.ast.statements.SvLibChoiceStatement;
+import org.sosy_lab.cpachecker.cfa.parser.svlib.ast.statements.SvLibContinueStatement;
+import org.sosy_lab.cpachecker.cfa.parser.svlib.ast.statements.SvLibGotoStatement;
+import org.sosy_lab.cpachecker.cfa.parser.svlib.ast.statements.SvLibHavocStatement;
+import org.sosy_lab.cpachecker.cfa.parser.svlib.ast.statements.SvLibIfStatement;
+import org.sosy_lab.cpachecker.cfa.parser.svlib.ast.statements.SvLibLabelStatement;
+import org.sosy_lab.cpachecker.cfa.parser.svlib.ast.statements.SvLibProcedureCallStatement;
+import org.sosy_lab.cpachecker.cfa.parser.svlib.ast.statements.SvLibReturnStatement;
+import org.sosy_lab.cpachecker.cfa.parser.svlib.ast.statements.SvLibSequenceStatement;
+import org.sosy_lab.cpachecker.cfa.parser.svlib.ast.statements.SvLibStatement;
+import org.sosy_lab.cpachecker.cfa.parser.svlib.ast.statements.SvLibStatementVisitor;
+import org.sosy_lab.cpachecker.cfa.parser.svlib.ast.statements.SvLibWhileStatement;
+import org.sosy_lab.cpachecker.exceptions.NoException;
+
+public class SvLibLabelCollectingVisitor
+    implements SvLibStatementVisitor<Optional<CFANode>, NoException> {
+
+  private final SvLibProcedureDeclaration procedure;
+  private final ImmutableMap.Builder<String, CFANode> labelsToNodes;
+
+  public SvLibLabelCollectingVisitor(
+      SvLibProcedureDeclaration pProcedure, ImmutableMap.Builder<String, CFANode> pLabelsToNodes) {
+    procedure = pProcedure;
+    labelsToNodes = pLabelsToNodes;
+  }
+
+  @Override
+  public Optional<CFANode> visit(SvLibAssignmentStatement pSvLibAssignmentStatement)
+      throws NoException {
+    return Optional.empty();
+  }
+
+  @Override
+  public Optional<CFANode> visit(SvLibHavocStatement pSvLibHavocStatement) throws NoException {
+    return Optional.empty();
+  }
+
+  @Override
+  public Optional<CFANode> visit(SvLibProcedureCallStatement pSvLibProcedureCallStatement)
+      throws NoException {
+    return Optional.empty();
+  }
+
+  @Override
+  public Optional<CFANode> visit(SvLibSequenceStatement pSvLibSequenceStatement)
+      throws NoException {
+    for (SvLibStatement subStatement : pSvLibSequenceStatement.getStatements()) {
+      subStatement.accept(this);
+    }
+
+    return Optional.empty();
+  }
+
+  @Override
+  public Optional<CFANode> visit(SvLibAssumeStatement pSvLibAssumeStatement) throws NoException {
+    return Optional.empty();
+  }
+
+  @Override
+  public Optional<CFANode> visit(SvLibWhileStatement pSvLibWhileStatement) throws NoException {
+    pSvLibWhileStatement.getBody().accept(this);
+
+    return Optional.empty();
+  }
+
+  @Override
+  public Optional<CFANode> visit(SvLibIfStatement pSvLibIfStatement) throws NoException {
+    pSvLibIfStatement.getThenBranch().accept(this);
+    if (pSvLibIfStatement.getElseBranch().isPresent()) {
+      SvLibStatement elseBranch = pSvLibIfStatement.getElseBranch().orElseThrow();
+      elseBranch.accept(this);
+    }
+
+    return Optional.empty();
+  }
+
+  @Override
+  public Optional<CFANode> visit(SvLibBreakStatement pSvLibBreakStatement) throws NoException {
+    return Optional.empty();
+  }
+
+  @Override
+  public Optional<CFANode> visit(SvLibContinueStatement pSvLibContinueStatement)
+      throws NoException {
+    return Optional.empty();
+  }
+
+  @Override
+  public Optional<CFANode> visit(SvLibReturnStatement pSvLibReturnStatement) throws NoException {
+    return Optional.empty();
+  }
+
+  @Override
+  public Optional<CFANode> visit(SvLibGotoStatement pSvLibGotoStatement) throws NoException {
+    return Optional.empty();
+  }
+
+  @Override
+  public Optional<CFANode> visit(SvLibLabelStatement pSvLibLabelStatement) throws NoException {
+    CFANode dummyNode = new CFANode(procedure.toSimpleDeclaration());
+    labelsToNodes.put(pSvLibLabelStatement.getLabel(), dummyNode);
+    return Optional.empty();
+  }
+
+  @Override
+  public Optional<CFANode> visit(SvLibChoiceStatement pSvLibChoiceStatement) throws NoException {
+    for (SvLibStatement possibleChoice : pSvLibChoiceStatement.getChoices()) {
+      possibleChoice.accept(this);
+    }
+
+    return Optional.empty();
+  }
+}
