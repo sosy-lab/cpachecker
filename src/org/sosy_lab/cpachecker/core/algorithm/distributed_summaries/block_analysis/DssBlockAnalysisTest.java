6207fd443a1c480e6cceedf83a82c6ec88e3b317
Manuel Pietsch <manuel.pietsch@sosy.ifi.lmu.de>
2026-03-30 13:19:53 +0200

    Make it an actual test

--- src/org/sosy_lab/cpachecker/core/algorithm/distributed_summaries/block_analysis/DssBlockAnalysisTest.java
+++ src/org/sosy_lab/cpachecker/core/algorithm/distributed_summaries/block_analysis/DssBlockAnalysisTest.java
@@ -11,9 +11,15 @@ package org.sosy_lab.cpachecker.core.algorithm.distributed_summaries.block_analy
 import static org.junit.Assert.fail;
 
 import com.google.common.collect.ImmutableList;
+import com.google.common.truth.Truth;
 import java.nio.file.Path;
 import java.util.Collection;
+import java.util.List;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
 import org.sosy_lab.common.ShutdownManager;
 import org.sosy_lab.common.ShutdownNotifier;
 import org.sosy_lab.common.configuration.Configuration;
@@ -26,36 +32,121 @@ import org.sosy_lab.cpachecker.core.algorithm.distributed_summaries.communicatio
 import org.sosy_lab.cpachecker.core.algorithm.distributed_summaries.communication.messages.DssViolationConditionMessage;
 import org.sosy_lab.cpachecker.core.algorithm.distributed_summaries.decomposition.ImportDecomposition;
 import org.sosy_lab.cpachecker.core.algorithm.distributed_summaries.decomposition.graph.BlockGraph;
+import org.sosy_lab.cpachecker.core.algorithm.distributed_summaries.decomposition.graph.BlockGraphModification;
+import org.sosy_lab.cpachecker.core.algorithm.distributed_summaries.decomposition.graph.BlockGraphModification.Modification;
 import org.sosy_lab.cpachecker.core.algorithm.distributed_summaries.decomposition.graph.BlockNode;
 import org.sosy_lab.cpachecker.core.algorithm.distributed_summaries.worker.DssAnalysisOptions;
 import org.sosy_lab.cpachecker.core.specification.Specification;
 import org.sosy_lab.cpachecker.util.test.TestDataTools;
 
+@RunWith(Parameterized.class)
 public class DssBlockAnalysisTest {
+
+  @Parameters(name = "{2} in {0} with ({1} until M{3}.json)")
+  public static List<Object[]> getParameters() {
+    return ImmutableList.of(
+        new Object[] {
+          "doc/examples/example.c", "output/block_analysis", "L2", 40, new Responses(null, null)
+        });
+  }
+
+  @Parameter(0)
+  String program;
+
+  @Parameter(1)
+  String folder;
+
+  @Parameter(2)
+  String blockID;
+
+  @Parameter(3)
+  int lastMessage;
+
+  // TODO load from files as well?
+  @Parameter(4)
+  Responses expected;
 
   @Test
   public void test() throws Exception {
 
-    var test = getAnalysisResponse("doc/examples/example.c", "output/block_analysis", "L2", 40);
+    Responses rs = getAnalysisResponse(program, folder, blockID, lastMessage);
+
+    if(expected.postAnalysis != null) {
+      expected.postAnalysis.forEach(
+          exp ->
+              Truth.assertWithMessage(
+                      "Expected postcondition analysis to cover message %s, but got %s",
+                      exp, rs.postAnalysis)
+                  .that(rs.postAnalysis.stream().anyMatch(act -> covers(exp, act)))
+                  .isTrue());
+    }
+
+    if (expected.violationAnalysis != null) {
+      expected.violationAnalysis.forEach(
+          exp ->
+              Truth.assertWithMessage(
+                      "Expected violation analysis to cover message %s, but got %s",
+                      exp, rs.violationAnalysis)
+                  .that(rs.violationAnalysis.stream().anyMatch(act -> covers(exp, act)))
+                  .isTrue());
+    }
 
     fail("Not yet implemented");
+  }
+
+  private boolean covers(DssMessage pExp, DssMessage pAct) {
+    // TODO how to do this?? I do not want to enforce everything to be the same!
+    return true;
   }
 
   public record Responses(
       Collection<DssMessage> postAnalysis, Collection<DssMessage> violationAnalysis) {}
 
+  /**
+   * repeats the analysis for a given block with given received messages
+   *
+   * <p>How to create a usable folder:
+   *
+   * <p>cpachecker --dss --option distributedSummaries.debug=true <file>
+   *
+   * <p>cpachecker --dss --option distributedSummaries.decomposition.generateBlockGraphOnly=true
+   * <file>
+   *
+   * <p>The second part is needed because debug exports the blocks after modifying the graph, which
+   * means we can not import it.
+   *
+   * @param program the program that is analyzed
+   * @param blockAnalysisFolder the folder where the debug information is stored
+   * @param blockID the block for which to run the analysis
+   * @param lastMessageNumber analyze after this message
+   * @return the responses to the last postCondition and the last violation condition message in the
+   *     folder
+   */
   public static Responses getAnalysisResponse(
       String program, String blockAnalysisFolder, String blockID, int lastMessageNumber)
       throws Exception {
     CFA cfa = TestUtil.buildTestCFA(program);
-    BlockNode node = getBlockNode(cfa, blockAnalysisFolder, blockID);
-    DssBlockAnalysis analysis = createAnalysis(cfa, node);
+    Modification mod = getModifiedBlockGraph(cfa, blockAnalysisFolder);
+
+    BlockNode node =
+        mod.blockGraph().getNodes().stream()
+            .filter(b -> b.getId().equals(blockID))
+            .findAny()
+            .orElseThrow(
+                () ->
+                    new IllegalArgumentException(
+                        "No block with id '" + blockID + "' found in the block graph."));
+
+    DssBlockAnalysis analysis = createAnalysis(mod.cfa(), node);
 
     DssViolationConditionMessage lastViolation = null;
     DssPostConditionMessage lastPrecondition = null;
     for(int i = 0; i <= lastMessageNumber; i++) {
-      DssMessage curr =
-          DssMessage.fromJson(Path.of(blockAnalysisFolder, "messages", "M" + i + ".json"));
+      Path path = Path.of(blockAnalysisFolder, "messages", "M" + i + ".json");
+      if (!path.toFile().exists()) {
+        continue;
+      }
+      DssMessage curr = DssMessage.fromJson(path);
 
       if (curr instanceof DssPostConditionMessage p
           && node.getPredecessorIds().contains(p.getSenderId())) {
@@ -108,22 +199,21 @@ public class DssBlockAnalysisTest {
     return new DssBlockAnalysis(log, pNode, pCfa, spec, config, options, messageFactory, shut);
   }
 
-  private static BlockNode getBlockNode(CFA pCfa, String pBlockAnalysisFolder, String pBlockID)
+  private static Modification getModifiedBlockGraph(CFA pCfa, String pBlockAnalysisFolder)
       throws Exception {
     ImportDecomposition decomp =
         new ImportDecomposition(Path.of(pBlockAnalysisFolder, "blocks.json"));
     BlockGraph graph = decomp.decompose(pCfa);
 
-    BlockNode blockNode =
-        graph.getNodes().stream()
-            .filter(b -> b.getId().equals(pBlockID))
-            .findAny()
-            .orElseThrow(
-                () ->
-                    new IllegalArgumentException(
-                        "No block with id '" + pBlockID + "' found in the block graph."));
+    Configuration config =
+        TestDataTools.configurationForTest()
+            .loadFromFile(TestUtil.DSS_FORWARD_CONFIGURATION_FILE)
+            .build();
+    Modification mod =
+        BlockGraphModification.instrumentCFA(
+            pCfa, graph, config, LogManager.createTestLogManager());
 
-    return blockNode;
+    return mod;
   }
 
 
