03a562bb01a41364cc23967c7a7bb9df988d88dc
Johannes Schlüßlhuber <schluesslhuber@cip.ifi.lmu.de>
2026-04-08 14:44:01 +0200

    Add test file for tail recursion integration test

---
+++ test/programs/program_transformation/tail_recursion_add.c
@@ -0,0 +1,33 @@
+// This file is part of CPAchecker,
+// a tool for configurable software verification:
+// https://cpachecker.sosy-lab.org
+//
+// SPDX-FileCopyrightText: 2026 Dirk Beyer <https://www.sosy-lab.org>
+//
+// SPDX-License-Identifier: Apache-2.0
+
+unsigned int rand_unsigned_int();
+
+unsigned int add(unsigned int x, unsigned int y){
+  if(y == 0){
+    return x;
+  }else{
+    return add(x+1, y-1);
+  }
+}
+
+int main(){
+
+  unsigned int x = rand_unsigned_int();
+  unsigned int y = rand_unsigned_int();
+
+  unsigned int z = add(x, y);
+
+  if(z == (x + y)){
+    return 1;
+  }else{
+    ERROR: return 0;
+  }
+
+}
+
