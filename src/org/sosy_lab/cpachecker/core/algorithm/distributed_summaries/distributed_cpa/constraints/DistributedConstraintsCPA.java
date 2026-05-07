6361eba58d712b5c6c3fff9670284d19841193a6
Matthias Kettl <matthias.kettl@sosy.ifi.lmu.de>
2026-05-07 16:12:25 +0200

    Rename methods to updated interface

--- src/org/sosy_lab/cpachecker/core/algorithm/distributed_summaries/distributed_cpa/constraints/DistributedConstraintsCPA.java
+++ src/org/sosy_lab/cpachecker/core/algorithm/distributed_summaries/distributed_cpa/constraints/DistributedConstraintsCPA.java
@@ -79,11 +79,6 @@ public class DistributedConstraintsCPA implements ForwardingDistributedConfigura
   }
 
   @Override
-  public int programCounterHash(AbstractState pAbstractState) {
-    return Objects.hash(this, pAbstractState);
-  }
-
-  @Override
   public ProceedOperator getProceedOperator() {
     return proceedOperator;
   }
@@ -122,6 +117,11 @@ public class DistributedConstraintsCPA implements ForwardingDistributedConfigura
   public boolean isMostGeneralBlockEntryState(AbstractState pAbstractState) {
     ConstraintsState constraintsState = (ConstraintsState) pAbstractState;
     return constraintsState.isTop();
+  }
+
+  @Override
+  public int computeProgramPointHash(AbstractState pAbstractState) {
+    return Objects.hash(this, pAbstractState);
   }
 
   @Override
