2dd3bfd6a4808e9fbc8afdcb83395b39560e5fc3
Paulína Ayaziová <paulina.ayazi@gmail.com>
2026-05-19 15:16:26 +0200

    Strengthen symbolic state with block state

--- src/org/sosy_lab/cpachecker/cpa/symbolicExecution/SymbolicExecutionTransferRelation.java
+++ src/org/sosy_lab/cpachecker/cpa/symbolicExecution/SymbolicExecutionTransferRelation.java
@@ -8,18 +8,30 @@
 
 package org.sosy_lab.cpachecker.cpa.symbolicExecution;
 
+import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Iterables;
 import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map.Entry;
+import java.util.Set;
+import org.checkerframework.checker.nullness.qual.NonNull;
 import org.checkerframework.checker.nullness.qual.Nullable;
 import org.sosy_lab.cpachecker.cfa.model.CFAEdge;
 import org.sosy_lab.cpachecker.core.defaults.SingleEdgeTransferRelation;
 import org.sosy_lab.cpachecker.core.interfaces.AbstractState;
 import org.sosy_lab.cpachecker.core.interfaces.Precision;
 import org.sosy_lab.cpachecker.core.interfaces.TransferRelation;
+import org.sosy_lab.cpachecker.cpa.block.BlockState;
+import org.sosy_lab.cpachecker.cpa.constraints.constraint.Constraint;
 import org.sosy_lab.cpachecker.cpa.constraints.domain.ConstraintsState;
 import org.sosy_lab.cpachecker.cpa.value.ValueAnalysisState;
+import org.sosy_lab.cpachecker.cpa.value.ValueAnalysisState.ValueAndType;
 import org.sosy_lab.cpachecker.exceptions.CPATransferException;
+import org.sosy_lab.cpachecker.util.AbstractStates;
+import org.sosy_lab.cpachecker.util.states.MemoryLocation;
 
 public class SymbolicExecutionTransferRelation extends SingleEdgeTransferRelation {
 
@@ -61,20 +73,38 @@ public class SymbolicExecutionTransferRelation extends SingleEdgeTransferRelatio
       Precision precision)
       throws CPATransferException, InterruptedException {
     SymbolicExecutionState symbolicExecutionState = (SymbolicExecutionState) state;
+
+    // Some states can strengthen the CS and VS separately
+    Set<AbstractState> strengthenSeparately = new HashSet<>();
+
+    for (AbstractState strengtheningState : otherStates) {
+      if (strengtheningState instanceof BlockState blockState) {
+        SymbolicExecutionState newState =
+            strengthenWithBlockState(symbolicExecutionState, blockState, cfaEdge, precision);
+        if (newState == null) {
+          return ImmutableSet.of();
+        }
+        symbolicExecutionState = newState;
+      } else {
+        strengthenSeparately.add(strengtheningState);
+      }
+    }
     Collection<? extends AbstractState> valueStates =
         valueAnalysisTransferRelation.strengthen(
             symbolicExecutionState.valueAnalysisState(),
             Iterables.concat(
-                otherStates, ImmutableList.of(symbolicExecutionState.constraintsState())),
+                strengthenSeparately, ImmutableList.of(symbolicExecutionState.constraintsState())),
             cfaEdge,
             precision);
     Collection<? extends AbstractState> constraintStates =
         constraintsTransferRelation.strengthen(
-            symbolicExecutionState.valueAnalysisState(),
+            symbolicExecutionState.constraintsState(),
             Iterables.concat(
-                otherStates, ImmutableList.of(symbolicExecutionState.valueAnalysisState())),
+                strengthenSeparately,
+                ImmutableList.of(symbolicExecutionState.valueAnalysisState())),
             cfaEdge,
             precision);
+
     ImmutableList.Builder<SymbolicExecutionState> strengthenedStates = ImmutableList.builder();
     for (AbstractState valueState : valueStates) {
       for (AbstractState constraintState : constraintStates) {
@@ -84,5 +114,67 @@ public class SymbolicExecutionTransferRelation extends SingleEdgeTransferRelatio
       }
     }
     return strengthenedStates.build();
+  }
+
+  public SymbolicExecutionState strengthenWithBlockState(
+      final SymbolicExecutionState pStateToStrengthen,
+      final BlockState pBlockState,
+      final CFAEdge pCfaEdge,
+      Precision pPrecision)
+      throws CPATransferException, InterruptedException {
+
+    if (!pBlockState.isTarget()) {
+      return pStateToStrengthen;
+    }
+
+    List<? extends @NonNull AbstractState> violations = pBlockState.getViolationConditions();
+
+    SymbolicExecutionState violation =
+        AbstractStates.extractStateByType(violations.getFirst(), SymbolicExecutionState.class);
+    Preconditions.checkNotNull(violation);
+    return strengthenWithSymbolicState(pStateToStrengthen, violation, pCfaEdge, pPrecision);
+  }
+
+  public SymbolicExecutionState strengthenWithSymbolicState(
+      final SymbolicExecutionState pStateToStrengthen,
+      final SymbolicExecutionState pViolation,
+      final CFAEdge pCfaEdge,
+      Precision pPrecision)
+      throws CPATransferException, InterruptedException {
+
+    SymbolicExecutionState newViolation =
+        pViolation.renameIDs(pStateToStrengthen.getSymbolicIdentifiers());
+
+    List<Constraint> addConstraints =
+        ValueAnalysisState.compareInConstraint(
+            pStateToStrengthen.valueAnalysisState(), newViolation.valueAnalysisState());
+    addConstraints.addAll(newViolation.constraintsState());
+
+    ConstraintsState newConstraintsState = new ConstraintsState(new HashSet<>(addConstraints));
+
+    // Call strengthen of CS with CS
+    Collection<? extends AbstractState> newCS =
+        constraintsTransferRelation.strengthen(
+            pStateToStrengthen.constraintsState(),
+            List.of(newConstraintsState),
+            pCfaEdge,
+            pPrecision);
+    if (newCS.isEmpty()) return null;
+    Preconditions.checkArgument(newCS.size() == 1);
+
+    for (Entry<MemoryLocation, ValueAndType> entry :
+        newViolation.valueAnalysisState().getConstants()) {
+      if (!pStateToStrengthen.valueAnalysisState().contains(entry.getKey())) {
+        pStateToStrengthen
+            .valueAnalysisState()
+            .assignConstant(
+                entry.getKey(), entry.getValue().getValue(), entry.getValue().getType());
+      }
+    }
+
+    // return new SE
+    return new SymbolicExecutionState(
+        pStateToStrengthen.valueAnalysisState(),
+        (ConstraintsState) Iterables.getOnlyElement(newCS));
   }
 }
