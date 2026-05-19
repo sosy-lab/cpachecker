331fce72bbb2b7509cf8cb913c6c509408ffecaa
Paulína Ayaziová <paulina.ayazi@gmail.com>
2026-05-19 15:14:19 +0200

    Add functions for renaming symbolic state

--- src/org/sosy_lab/cpachecker/cpa/symbolicExecution/SymbolicExecutionState.java
+++ src/org/sosy_lab/cpachecker/cpa/symbolicExecution/SymbolicExecutionState.java
@@ -8,10 +8,44 @@
 
 package org.sosy_lab.cpachecker.cpa.symbolicExecution;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map.Entry;
+import java.util.Set;
 import org.sosy_lab.cpachecker.core.interfaces.AbstractState;
+import org.sosy_lab.cpachecker.cpa.constraints.constraint.Constraint;
 import org.sosy_lab.cpachecker.cpa.constraints.domain.ConstraintsState;
 import org.sosy_lab.cpachecker.cpa.value.ValueAnalysisState;
+import org.sosy_lab.cpachecker.cpa.value.ValueAnalysisState.ValueAndType;
+import org.sosy_lab.cpachecker.cpa.value.symbolic.type.SymbolicIdentifier;
+import org.sosy_lab.cpachecker.cpa.value.symbolic.type.SymbolicValue;
+import org.sosy_lab.cpachecker.cpa.value.symbolic.util.SymbolicIdentifierRenamer;
+import org.sosy_lab.cpachecker.cpa.value.symbolic.util.SymbolicValues;
+import org.sosy_lab.cpachecker.util.states.MemoryLocation;
 
 public record SymbolicExecutionState(
     ValueAnalysisState valueAnalysisState, ConstraintsState constraintsState)
-    implements AbstractState {}
+    implements AbstractState {
+
+  public SymbolicExecutionState renameIDs(Set<SymbolicIdentifier> toRename) {
+    SymbolicIdentifierRenamer visitor = new SymbolicIdentifierRenamer(new HashMap<>(), toRename);
+    return new SymbolicExecutionState(
+        valueAnalysisState().renameIDs(visitor), constraintsState().renameIDs(visitor));
+  }
+
+  public Set<SymbolicIdentifier> getSymbolicIdentifiers() {
+    Set<SymbolicIdentifier> identifiers = new HashSet<>();
+
+    for (Constraint constraint : constraintsState) {
+      assert constraint != null;
+      identifiers.addAll(SymbolicValues.getContainedSymbolicIdentifiers(constraint));
+    }
+
+    for (Entry<MemoryLocation, ValueAndType> constant : valueAnalysisState.getConstants()) {
+      if (constant.getValue().getValue() instanceof SymbolicValue symVal) {
+        identifiers.addAll(SymbolicValues.getContainedSymbolicIdentifiers(symVal));
+      }
+    }
+    return identifiers;
+  }
+}
