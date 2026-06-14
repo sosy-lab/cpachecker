d9505b76f3a0ab97e1aee271d9c468c9131f99e9
Johannes Schlüßlhuber <schluesslhuber@cip.ifi.lmu.de>
2026-06-14 11:31:32 +0200

    Add methods for entering and leaving a program transformation to ProgramTransformationRecoveryUtils

--- src/org/sosy_lab/cpachecker/cfa/transformation/ProgramTransformationRecoveryUtils.java
+++ src/org/sosy_lab/cpachecker/cfa/transformation/ProgramTransformationRecoveryUtils.java
@@ -10,13 +10,22 @@ package org.sosy_lab.cpachecker.cfa.transformation;
 
 import java.util.ArrayList;
 import java.util.List;
+import org.sosy_lab.cpachecker.cfa.DummyCFAEdge;
 import org.sosy_lab.cpachecker.core.interfaces.AbstractState;
+import org.sosy_lab.cpachecker.core.reachedset.ReachedSet;
 import org.sosy_lab.cpachecker.cpa.arg.ARGState;
 import org.sosy_lab.cpachecker.cpa.composite.CompositeState;
 import org.sosy_lab.cpachecker.cpa.location.LocationState;
 
 public class ProgramTransformationRecoveryUtils {
 
+  /**
+   * Copy all wrapped states from pARGState, with a new LocationState and parent ARGState.
+   * @param pARGState the ARGState to copy from
+   * @param pLocationState the new LocationState
+   * @param pParentState the new parent ARGState
+   * @return newly created ARGState
+   */
   public static ARGState argStateWithLocation(
       ARGState pARGState, LocationState pLocationState, ARGState pParentState) {
     CompositeState currentCompositeState = ((CompositeState) pARGState.getWrappedState());
@@ -30,5 +39,43 @@ public class ProgramTransformationRecoveryUtils {
       }
     }
     return new ARGState(new CompositeState(newWrappedStates), pParentState);
+  }
+
+  /**
+   * Removes the initial ARGState in a program transformation, after a dummy entry edge.
+   * @param pPreviousARGState the ARGState before entering the program transformation
+   * @param pCurrentARGState the first ARGState in a program transformation
+   * @param reached the reached set
+   * @return the previous ARGState now connected to the former children of the removed ARGState
+   */
+  public static ARGState handleEntry(
+      ARGState pPreviousARGState,
+      ARGState pCurrentARGState,
+      ReachedSet reached) {
+    assert(pPreviousARGState.getEdgeToChild(pCurrentARGState) instanceof DummyCFAEdge);
+    List<ARGState> childStates = List.copyOf(pCurrentARGState.getChildren());
+    for (int i = 0; i < childStates.size(); i++) {
+      childStates.get(i).addParent(pPreviousARGState);
+    }
+    reached.remove(pCurrentARGState);
+    pCurrentARGState.removeFromARG();
+    return pPreviousARGState;
+  }
+
+  /**
+   * Removes the last state before exiting a program transformation from the ARG and reached set.
+   * @param pPreviousARGState previous ARGState
+   * @param pCurrentARGState the last ARGState in this program transformation
+   * @param reached the reached set
+   */
+  public static void handleExit(
+      ARGState pPreviousARGState,
+      ARGState pCurrentARGState,
+      ReachedSet reached) {
+    assert (pCurrentARGState.getChildren().size() == 1);
+    ARGState childState = pCurrentARGState.getChildren().getFirst();
+    childState.addParent(pPreviousARGState);
+    reached.remove(pCurrentARGState);
+    pCurrentARGState.removeFromARG();
   }
 }
