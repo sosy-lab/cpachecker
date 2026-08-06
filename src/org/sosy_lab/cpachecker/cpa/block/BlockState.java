ac5950a7474f86653f6f3b295408d324db25ef85
Matthias Kettl <matthias.kettl@sosy.ifi.lmu.de>
2026-08-06 15:43:24 +0200

    Remove accidentially commited test file and use adjust import

--- src/org/sosy_lab/cpachecker/cpa/block/BlockState.java
+++ src/org/sosy_lab/cpachecker/cpa/block/BlockState.java
@@ -31,7 +31,7 @@ import org.sosy_lab.cpachecker.core.interfaces.AbstractState;
 import org.sosy_lab.cpachecker.core.interfaces.FormulaReportingState;
 import org.sosy_lab.cpachecker.core.interfaces.Partitionable;
 import org.sosy_lab.cpachecker.core.interfaces.Targetable;
-import org.sosy_lab.cpachecker.cpa.path.PathState;
+import org.sosy_lab.cpachecker.cpa.pathrestriction.PathRestrictionState;
 import org.sosy_lab.cpachecker.cpa.pathrestriction.SegmentedPaths;
 import org.sosy_lab.cpachecker.util.AbstractStates;
 import org.sosy_lab.cpachecker.util.predicates.smt.BooleanFormulaManagerView;
@@ -57,7 +57,7 @@ public class BlockState
   private List<? extends AbstractState> violationConditions;
   private final SegmentedPaths witness;
 
-  private final Optional<PathState> witnessCheckPathState;
+  private final Optional<PathRestrictionState> witnessCheckPathState;
 
   public BlockState(
       CFANode pNode,
@@ -66,7 +66,7 @@ public class BlockState
       List<? extends AbstractState> pViolationConditions,
       BlockGraphPath pHistory,
       SegmentedPaths pWitness,
-      PathState pWitnessCheckPathState) {
+      PathRestrictionState pWitnessCheckPathState) {
     Preconditions.checkArgument(
         pType == BlockStateType.WITNESS || pWitnessCheckPathState == null,
         "Added path state while not being in Witnes state");
