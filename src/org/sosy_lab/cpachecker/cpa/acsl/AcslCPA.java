8f377668ff8705c792ff3812e2fe8836b7a4305a
Sarah Koch <Koch.Sa@campus.lmu.de>
2026-08-07 16:26:16 +0200

    Fixed using the wrong type of constructor

--- src/org/sosy_lab/cpachecker/cpa/acsl/AcslCPA.java
+++ src/org/sosy_lab/cpachecker/cpa/acsl/AcslCPA.java
@@ -8,30 +8,32 @@
 
 package org.sosy_lab.cpachecker.cpa.acsl;
 
-import org.checkerframework.checker.nullness.qual.Nullable;
-import org.sosy_lab.common.configuration.Options;
+import org.sosy_lab.common.configuration.Configuration;
+import org.sosy_lab.common.log.LogManager;
+import org.sosy_lab.cpachecker.cfa.CFA;
 import org.sosy_lab.cpachecker.cfa.model.CFANode;
 import org.sosy_lab.cpachecker.core.defaults.AbstractCPA;
 import org.sosy_lab.cpachecker.core.defaults.AutomaticCPAFactory;
-import org.sosy_lab.cpachecker.core.interfaces.AbstractDomain;
 import org.sosy_lab.cpachecker.core.interfaces.AbstractState;
 import org.sosy_lab.cpachecker.core.interfaces.CPAFactory;
 import org.sosy_lab.cpachecker.core.interfaces.ConfigurableProgramAnalysis;
 import org.sosy_lab.cpachecker.core.interfaces.StateSpacePartition;
-import org.sosy_lab.cpachecker.core.interfaces.TransferRelation;
 
 /**
  * This is a CPA whose goal is to find and inject ACSL invariants. For now this class is used for
  * testing and proof of concept.
  */
-@Options(prefix = "cpa.acsl")
+@SuppressWarnings("unused")
 public class AcslCPA extends AbstractCPA implements ConfigurableProgramAnalysis {
-  protected AcslCPA(
-      String mergeType,
-      String stopType,
-      AbstractDomain domain,
-      @Nullable TransferRelation transfer) {
-    super(mergeType, stopType, domain, transfer);
+
+  private final CFA cfa;
+  private final LogManager logger;
+
+  private AcslCPA(CFA pCFA, LogManager pLogManager, Configuration pConfig) {
+    super("sep", "sep", null);
+    this.cfa = pCFA;
+    this.logger = pLogManager;
+    System.out.println("AcslCPA created.");
   }
 
   public static CPAFactory factory() {
