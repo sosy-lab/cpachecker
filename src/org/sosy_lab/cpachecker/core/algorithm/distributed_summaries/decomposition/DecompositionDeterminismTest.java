f309d1306f8954dd7f0245c62673715180740523
Matthias Kettl <matthias.kettl@sosy.ifi.lmu.de>
2026-08-14 16:20:52 +0200

    Use var instead of int

--- src/org/sosy_lab/cpachecker/core/algorithm/distributed_summaries/decomposition/DecompositionDeterminismTest.java
+++ src/org/sosy_lab/cpachecker/core/algorithm/distributed_summaries/decomposition/DecompositionDeterminismTest.java
@@ -59,7 +59,7 @@ public class DecompositionDeterminismTest {
     CFA firstCfa = TestUtil.buildTestCFA(path);
     List<String> first = fingerprint(createDecomposition(firstCfa, pInline).decompose(firstCfa));
 
-    int unused = Objects.hash(new Object(), new Object(), new Object());
+    var unused = Objects.hash(new Object(), new Object(), new Object());
 
     CFA secondCfa = TestUtil.buildTestCFA(path);
     List<String> second = fingerprint(createDecomposition(secondCfa, pInline).decompose(secondCfa));
