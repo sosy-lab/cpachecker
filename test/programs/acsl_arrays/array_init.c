ee84126e1dd9d9558938f8f49fbf5dfe941593e7
Sarah Koch <Koch.Sa@campus.lmu.de>
2026-08-14 13:22:17 +0200

    add missing licensing info

--- test/programs/acsl_arrays/array_init.c
+++ test/programs/acsl_arrays/array_init.c
@@ -1,3 +1,11 @@
+// This file is part of CPAchecker,
+// a tool for configurable software verification:
+// https://cpachecker.sosy-lab.org
+//
+// SPDX-FileCopyrightText: 2026 Dirk Beyer <https://www.sosy-lab.org>
+//
+// SPDX-License-Identifier: Apache-2.0
+
 extern unsigned __VERIFIER_nondet_uint();
 extern void __VERIFIER_assume(int);
 extern void __assert_fail(const char *assertion, const char *file,
