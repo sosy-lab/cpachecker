0371998011d14383bd5e6fd902585ca9c5909960
Philipp Wendler <uni@philippwendler.de>
2026-08-20 09:27:21 +0200

    Remove fiddling with paths of Python modules
    This was supposed to allow adding BenchExec tool-info modules
    to scripts/benchmark/tools/.
    But we never used that, we want all our tool-info modules in the
    BenchExec repository, and we can anyway use fully-qualified module names
    if we want to add modules locally.

--- scripts/benchmark.py
+++ scripts/benchmark.py
@@ -25,15 +25,8 @@ from benchexec import __version__
 import benchexec.benchexec
 import benchexec.model
 import benchexec.tooladapter
-import benchexec.tools
 import benchexec.util
 from benchmark import vcloudutil
-
-# Add ./benchmark/tools to __path__ of benchexec.tools package
-# such that additional tool-wrapper modules can be placed in this directory.
-benchexec.tools.__path__ = [
-    os.path.join(os.path.dirname(__file__), "benchmark", "tools")
-] + benchexec.tools.__path__
 
 _ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
 
