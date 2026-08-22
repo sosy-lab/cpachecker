077e54c77ec5b6c3f34bf5f00b2acf14b5d37e2b
Marian Lingsch-Rosenfeld <Marian.Lingsch-Rosenfeld@sosy.ifi.lmu.de>
2026-08-22 15:42:29 +0200

    Rename class since we are unifying all witnesses v2* classes

--- src/org/sosy_lab/cpachecker/cpa/automaton/AutomatonWitnessV2ValidationIntegrationTest.java
+++ src/org/sosy_lab/cpachecker/cpa/automaton/AutomatonWitnessV2ValidationIntegrationTest.java
@@ -14,7 +14,7 @@ import org.junit.Test;
 import org.sosy_lab.cpachecker.core.CPAcheckerResult.Result;
 import org.sosy_lab.cpachecker.util.test.IntegrationTestRunner;
 
-public final class AutomatonWitnessV2d2ValidationIntegrationTest {
+public final class AutomatonWitnessV2ValidationIntegrationTest {
 
   @BeforeClass
   public static void skipUnlessExtendedTestsEnabled() {
