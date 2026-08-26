e70229da438b8867d507123c255cab5a60e58e7c
Manuel Pietsch <manuel.pietsch@sosy.ifi.lmu.de>
2026-08-26 09:42:50 +0200

    Format Source

--- src/org/sosy_lab/cpachecker/core/algorithm/distributed_summaries/distributed_cpa/predicate/DistributedPredicateCPATest.java
+++ src/org/sosy_lab/cpachecker/core/algorithm/distributed_summaries/distributed_cpa/predicate/DistributedPredicateCPATest.java
@@ -194,7 +194,8 @@ public class DistributedPredicateCPATest {
                 .getBooleanFormulaManager()
                 .toConjunctionArgs(pathFormula.getFormula(), true));
     AbstractionPredicate globalPredicate =
-        cpa.getAbstractionManager().makePredicate(formulaManagerView.uninstantiate(atoms.getFirst()));
+        cpa.getAbstractionManager()
+            .makePredicate(formulaManagerView.uninstantiate(atoms.getFirst()));
     AbstractionPredicate localPredicate =
         cpa.getAbstractionManager().makePredicate(formulaManagerView.uninstantiate(atoms.get(1)));
 
