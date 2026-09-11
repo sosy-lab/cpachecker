d96f49191161bbbb55468c4636ad2da01a2c546f
AkshayWarrier <aksmen121@gmail.com>
2026-09-11 18:23:47 +0200

    Fix task definitions of math tasks

--- test/programs/simple/math/integer_abs_loop_false.c
+++ test/programs/simple/math/integer_abs_loop_false.c
@@ -19,7 +19,7 @@ int main(void) {
   long long sum = 0;
   int i = -3;
 
-  // i takes the values -3, -2, -1, 0, 1, 2, so
+  // i takes the values -3, -2, -1, 0, 1 so
   // abs(-3)+abs(-2)+abs(-1)+abs(0)+abs(1) = 3+2+1+0+1 = 7.
   while (i < 2) {
     sum += abs(i);
