ca38c8313c0139c921647bd5f4664af197a5ca29
BaierD <daniel.baier@sosy.ifi.lmu.de>
2026-09-18 00:35:29 +0200

    Update SMG2 SMGMergeStatus JavaDoc

--- src/org/sosy_lab/cpachecker/util/smg/join/SMGMergeStatus.java
+++ src/org/sosy_lab/cpachecker/util/smg/join/SMGMergeStatus.java
@@ -8,13 +8,30 @@
 
 package org.sosy_lab.cpachecker.util.smg.join;
 
-/** The join of two SMG returns a status flag. */
+/**
+ * The join of two SMGs returns a status flag, arguing about the relation of the two graphs and
+ * their stack frames, but excluding value interpretations.
+ */
 public enum SMGMergeStatus {
+  /**
+   * Two SMGs are considered semantically equivalent if they are structurally identical modulo
+   * internal node (value/object) identifiers. Concretely, there must be a one-to-one mapping of the
+   * SPCs and SMGs, preserving object attributes: kind, size, level, validity, and SLS/DLS metadata,
+   * as well as all has-value edges (but not their values), points-to edges (including their
+   * targets), offsets, types, target specifiers, as well as the stack frame. The equality of values
+   * and or their constraints is NOT included in this! This does not generalize SMGs or checks
+   * whether one SMG entails another.
+   */
   EQUAL("≃"),
-  /** The left state is entailed (contained) in the right state, e.g. 3+ ⊏ 2+. */
+  /**
+   * The left SMG is entailed (contained) in the right SMG, e.g. 3+ ⊏ 2+. Stack frames are equal.
+   */
   LEFT_ENTAILED_IN_RIGHT("⊏"),
-  /** The right state is entailed (contained) in the left state, e.g. 2+ ⊐ 3+. */
+  /**
+   * The right SMG is entailed (contained) in the left SMG, e.g. 2+ ⊐ 3+. Stack frames are equal.
+   */
   RIGHT_ENTAILED_IN_LEFT("⊐"),
+  /** The resulting SMG is neither EQUAL, LEFT_ENTAILED_IN_RIGHT, nor RIGHT_ENTAILED_IN_LEFT. */
   INCOMPARABLE("⋈");
 
   private final String symbol;
